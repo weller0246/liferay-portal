4108490e7de35fcd05a513d64fc03a0efaddb52d
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-02-10 10:32:10 +0100

    LPS-145927 Depending on validation this is how it should work:         * If no validation is performed locale should be created, added to the locales cached and returned.         * If validation is performed locale is added to the cache, but only returned if validation is successful.

--- portal-kernel/src/com/liferay/portal/kernel/util/LocaleUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/util/LocaleUtil.java
@@ -221,21 +221,7 @@ public class LocaleUtil {
 		return StringUtil.equalsIgnoreCase(languageId1, languageId2);
 	}
 
-	private Locale _fromLanguageId(String languageId, boolean validate) {
-		return _fromLanguageId(languageId, validate, true);
-	}
-
-	private Locale _fromLanguageId(
-		String languageId, boolean validate, boolean useDefault) {
-
-		if (languageId == null) {
-			if (useDefault) {
-				return _getDefault();
-			}
-
-			return null;
-		}
-
+	private Locale _fromLanguageId(String languageId) {
 		Locale locale = _locales.get(languageId);
 
 		if (locale != null) {
@@ -279,19 +265,38 @@ public class LocaleUtil {
 			}
 		}
 
-		if (validate && !LanguageUtil.isAvailableLocale(locale)) {
-			locale = null;
+		_locales.put(languageId, locale);
 
+		return locale;
+	}
+
+	private Locale _fromLanguageId(String languageId, boolean validate) {
+		return _fromLanguageId(languageId, validate, true);
+	}
+
+	private Locale _fromLanguageId(
+		String languageId, boolean validate, boolean useDefault) {
+
+		if (languageId == null) {
+			if (useDefault) {
+				return _getDefault();
+			}
+
+			return null;
+		}
+
+		Locale locale = _fromLanguageId(languageId);
+
+		if (validate && !LanguageUtil.isAvailableLocale(locale)) {
 			if (_log.isWarnEnabled()) {
 				_log.warn(languageId + " is not a valid language id");
 			}
-		}
-		else {
-			_locales.put(languageId, locale);
-		}
 
-		if ((locale == null) && useDefault) {
-			locale = _locale;
+			if (useDefault) {
+				return _locale;
+			}
+
+			return null;
 		}
 
 		return locale;
