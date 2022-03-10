bb31ff4affe977615b64a56f0e05a839b3de76d4
Evan Thibodeau <evan.thibodeau@liferay.com>
2022-03-10 10:50:56 +0400

    COMMERCE-8937 - Remove unused methods.

--- modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/asset/display/page/portlet/helper/AssetDisplayPageFriendlyURLResolverHelper.java
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/asset/display/page/portlet/helper/AssetDisplayPageFriendlyURLResolverHelper.java
@@ -14,23 +14,7 @@
 
 package com.liferay.commerce.product.content.web.internal.asset.display.page.portlet.helper;
 
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutConstants;
-import com.liferay.portal.kernel.model.LayoutTemplate;
-import com.liferay.portal.kernel.model.LayoutTypePortlet;
-import com.liferay.portal.kernel.portlet.PortletIdCodec;
-import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.LayoutLocalService;
-import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.StringUtil;
-
-import java.util.List;
-
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Ivica Cardic
@@ -40,104 +24,8 @@ import org.osgi.service.component.annotations.Reference;
 )
 public class AssetDisplayPageFriendlyURLResolverHelper {
 
-	public long getPlidFromPortletId(
-		List<Layout> layouts, String portletId, long scopeGroupId) {
-
-		for (Layout layout : layouts) {
-			LayoutTypePortlet layoutTypePortlet =
-				(LayoutTypePortlet)layout.getLayoutType();
-
-			if (_hasNonstaticPortletId(layoutTypePortlet, portletId) &&
-				(_portal.getScopeGroupId(layout, portletId) == scopeGroupId)) {
-
-				return layout.getPlid();
-			}
-		}
-
-		return LayoutConstants.DEFAULT_PLID;
-	}
-
-	public long getPlidFromPortletId(
-		long groupId, boolean privateLayout, String portletId) {
-
-		long scopeGroupId = groupId;
-
-		try {
-			Group group = _groupLocalService.getGroup(groupId);
-
-			if (group.isLayout()) {
-				Layout scopeLayout = _layoutLocalService.getLayout(
-					group.getClassPK());
-
-				groupId = scopeLayout.getGroupId();
-			}
-
-			String[] validLayoutTypes = {
-				LayoutConstants.TYPE_PORTLET,
-				LayoutConstants.TYPE_FULL_PAGE_APPLICATION,
-				LayoutConstants.TYPE_PANEL, LayoutConstants.TYPE_CONTENT
-			};
-
-			for (String layoutType : validLayoutTypes) {
-				long plid = getPlidFromPortletId(
-					_layoutLocalService.getLayouts(
-						groupId, privateLayout, layoutType),
-					portletId, scopeGroupId);
-
-				if (plid != LayoutConstants.DEFAULT_PLID) {
-					return plid;
-				}
-			}
-		}
-		catch (Exception exception) {
-			if (_log.isWarnEnabled()) {
-				_log.warn(exception);
-			}
-		}
-
-		return LayoutConstants.DEFAULT_PLID;
-	}
-
 	public int getURLSeparatorLength(String urlSeparator) {
 		return urlSeparator.length();
 	}
-
-	private boolean _hasNonstaticPortletId(
-		LayoutTypePortlet layoutTypePortlet, String portletId) {
-
-		LayoutTemplate layoutTemplate = layoutTypePortlet.getLayoutTemplate();
-
-		for (String columnId : layoutTemplate.getColumns()) {
-			String[] columnValues = StringUtil.split(
-				layoutTypePortlet.getTypeSettingsProperty(columnId));
-
-			for (String nonstaticPortletId : columnValues) {
-				if (nonstaticPortletId.equals(portletId)) {
-					return true;
-				}
-
-				String decodedNonstaticPortletName =
-					PortletIdCodec.decodePortletName(nonstaticPortletId);
-
-				if (decodedNonstaticPortletName.equals(portletId)) {
-					return true;
-				}
-			}
-		}
-
-		return false;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		AssetDisplayPageFriendlyURLResolverHelper.class);
-
-	@Reference
-	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private LayoutLocalService _layoutLocalService;
-
-	@Reference
-	private Portal _portal;
 
 }
