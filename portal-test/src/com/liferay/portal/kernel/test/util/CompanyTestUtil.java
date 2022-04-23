146dab859c57c1c329cbac10cc900721f66bc9ca
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-04-23 08:40:05 +0200

    LPS-145927 Do not validate in the middle of a languages change.

--- portal-test/src/com/liferay/portal/kernel/test/util/CompanyTestUtil.java
+++ portal-test/src/com/liferay/portal/kernel/test/util/CompanyTestUtil.java
@@ -95,7 +95,7 @@ public class CompanyTestUtil {
 		CompanyThreadLocal.setCompanyId(companyId);
 
 		LocaleThreadLocal.setDefaultLocale(
-			LocaleUtil.fromLanguageId(defaultLanguageId));
+			LocaleUtil.fromLanguageId(defaultLanguageId, false));
 	}
 
 }
