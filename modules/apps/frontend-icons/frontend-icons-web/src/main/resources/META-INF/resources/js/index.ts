d9a54f433c46f18a26c28da8237a28a9064630df
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2022-04-25 14:59:53 +0400

    LPS-145112 - Hardcode path and system pack name in javascript util file rather than using a DynamicInclude

--- modules/apps/frontend-icons/frontend-icons-web/src/main/resources/META-INF/resources/js/index.ts
+++ modules/apps/frontend-icons/frontend-icons-web/src/main/resources/META-INF/resources/js/index.ts
@@ -12,15 +12,20 @@
  * details.
  */
 
-const ICONS_CONFIG = (window as any)._FRONTEND_ICONS;
+/**
+ * These constants should be identical to the constants used at
+ * `portal-kernel/src/com/liferay/portal/kernel/icons/IconsUtil.java`
+ */
+const BASE_PATH = '/o/icons';
+const SYSTEM_ICON_PACK = 'clay';
 
 export function getSpritemapPath(iconPack?: String) {
 	const packOrSite = iconPack ? 'pack' : 'site';
 	const iconPackOrSiteId = iconPack || Liferay.ThemeDisplay.getSiteGroupId();
 
-	return `${ICONS_CONFIG.basePath}/${packOrSite}/${iconPackOrSiteId}.svg`;
+	return `${BASE_PATH}/${packOrSite}/${iconPackOrSiteId}.svg`;
 }
 
 export function getSystemSpritemapPath() {
-	return ICONS_CONFIG.systemSpritemapPath;
+	return getSpritemapPath(SYSTEM_ICON_PACK);
 }
