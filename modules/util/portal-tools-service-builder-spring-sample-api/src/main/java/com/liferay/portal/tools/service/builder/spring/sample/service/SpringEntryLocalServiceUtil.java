08fa66740d1a4a619315dfa89a40ffe7b7cc63c9
Kyle Miho <kyle.miho@liferay.com>
2022-04-28 13:29:17 -0700

    LRQA-72287 Move and rename sample test modules

---
+++ modules/util/portal-tools-service-builder-spring-sample-api/src/main/java/com/liferay/portal/tools/service/builder/spring/sample/service/SpringEntryLocalServiceUtil.java
@@ -0,0 +1,328 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.tools.service.builder.spring.sample.service;
+
+import com.liferay.petra.sql.dsl.query.DSLQuery;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.PersistedModel;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.tools.service.builder.spring.sample.model.SpringEntry;
+
+import java.io.Serializable;
+
+import java.util.List;
+
+/**
+ * Provides the local service utility for SpringEntry. This utility wraps
+ * <code>com.liferay.portal.tools.service.builder.spring.sample.service.impl.SpringEntryLocalServiceImpl</code> and
+ * is an access point for service operations in application layer code running
+ * on the local server. Methods of this service will not have security checks
+ * based on the propagated JAAS credentials because this service can only be
+ * accessed from within the same VM.
+ *
+ * @author Brian Wing Shun Chan
+ * @see SpringEntryLocalService
+ * @generated
+ */
+public class SpringEntryLocalServiceUtil {
+
+	/*
+	 * NOTE FOR DEVELOPERS:
+	 *
+	 * Never modify this class directly. Add custom service methods to <code>com.liferay.portal.tools.service.builder.spring.sample.service.impl.SpringEntryLocalServiceImpl</code> and rerun ServiceBuilder to regenerate this class.
+	 */
+
+	/**
+	 * Adds the spring entry to the database. Also notifies the appropriate model listeners.
+	 *
+	 * <p>
+	 * <strong>Important:</strong> Inspect SpringEntryLocalServiceImpl for overloaded versions of the method. If provided, use these entry points to the API, as the implementation logic may require the additional parameters defined there.
+	 * </p>
+	 *
+	 * @param springEntry the spring entry
+	 * @return the spring entry that was added
+	 */
+	public static SpringEntry addSpringEntry(SpringEntry springEntry) {
+		return getService().addSpringEntry(springEntry);
+	}
+
+	/**
+	 * @throws PortalException
+	 */
+	public static PersistedModel createPersistedModel(
+			Serializable primaryKeyObj)
+		throws PortalException {
+
+		return getService().createPersistedModel(primaryKeyObj);
+	}
+
+	/**
+	 * Creates a new spring entry with the primary key. Does not add the spring entry to the database.
+	 *
+	 * @param SpringEntryId the primary key for the new spring entry
+	 * @return the new spring entry
+	 */
+	public static SpringEntry createSpringEntry(long SpringEntryId) {
+		return getService().createSpringEntry(SpringEntryId);
+	}
+
+	/**
+	 * @throws PortalException
+	 */
+	public static PersistedModel deletePersistedModel(
+			PersistedModel persistedModel)
+		throws PortalException {
+
+		return getService().deletePersistedModel(persistedModel);
+	}
+
+	/**
+	 * Deletes the spring entry with the primary key from the database. Also notifies the appropriate model listeners.
+	 *
+	 * <p>
+	 * <strong>Important:</strong> Inspect SpringEntryLocalServiceImpl for overloaded versions of the method. If provided, use these entry points to the API, as the implementation logic may require the additional parameters defined there.
+	 * </p>
+	 *
+	 * @param SpringEntryId the primary key of the spring entry
+	 * @return the spring entry that was removed
+	 * @throws PortalException if a spring entry with the primary key could not be found
+	 */
+	public static SpringEntry deleteSpringEntry(long SpringEntryId)
+		throws PortalException {
+
+		return getService().deleteSpringEntry(SpringEntryId);
+	}
+
+	/**
+	 * Deletes the spring entry from the database. Also notifies the appropriate model listeners.
+	 *
+	 * <p>
+	 * <strong>Important:</strong> Inspect SpringEntryLocalServiceImpl for overloaded versions of the method. If provided, use these entry points to the API, as the implementation logic may require the additional parameters defined there.
+	 * </p>
+	 *
+	 * @param springEntry the spring entry
+	 * @return the spring entry that was removed
+	 */
+	public static SpringEntry deleteSpringEntry(SpringEntry springEntry) {
+		return getService().deleteSpringEntry(springEntry);
+	}
+
+	public static <T> T dslQuery(DSLQuery dslQuery) {
+		return getService().dslQuery(dslQuery);
+	}
+
+	public static int dslQueryCount(DSLQuery dslQuery) {
+		return getService().dslQueryCount(dslQuery);
+	}
+
+	public static DynamicQuery dynamicQuery() {
+		return getService().dynamicQuery();
+	}
+
+	/**
+	 * Performs a dynamic query on the database and returns the matching rows.
+	 *
+	 * @param dynamicQuery the dynamic query
+	 * @return the matching rows
+	 */
+	public static <T> List<T> dynamicQuery(DynamicQuery dynamicQuery) {
+		return getService().dynamicQuery(dynamicQuery);
+	}
+
+	/**
+	 * Performs a dynamic query on the database and returns a range of the matching rows.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to <code>com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS</code> will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent, then the query will include the default ORDER BY logic from <code>com.liferay.portal.tools.service.builder.spring.sample.model.impl.SpringEntryModelImpl</code>.
+	 * </p>
+	 *
+	 * @param dynamicQuery the dynamic query
+	 * @param start the lower bound of the range of model instances
+	 * @param end the upper bound of the range of model instances (not inclusive)
+	 * @return the range of matching rows
+	 */
+	public static <T> List<T> dynamicQuery(
+		DynamicQuery dynamicQuery, int start, int end) {
+
+		return getService().dynamicQuery(dynamicQuery, start, end);
+	}
+
+	/**
+	 * Performs a dynamic query on the database and returns an ordered range of the matching rows.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to <code>com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS</code> will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent, then the query will include the default ORDER BY logic from <code>com.liferay.portal.tools.service.builder.spring.sample.model.impl.SpringEntryModelImpl</code>.
+	 * </p>
+	 *
+	 * @param dynamicQuery the dynamic query
+	 * @param start the lower bound of the range of model instances
+	 * @param end the upper bound of the range of model instances (not inclusive)
+	 * @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	 * @return the ordered range of matching rows
+	 */
+	public static <T> List<T> dynamicQuery(
+		DynamicQuery dynamicQuery, int start, int end,
+		OrderByComparator<T> orderByComparator) {
+
+		return getService().dynamicQuery(
+			dynamicQuery, start, end, orderByComparator);
+	}
+
+	/**
+	 * Returns the number of rows matching the dynamic query.
+	 *
+	 * @param dynamicQuery the dynamic query
+	 * @return the number of rows matching the dynamic query
+	 */
+	public static long dynamicQueryCount(DynamicQuery dynamicQuery) {
+		return getService().dynamicQueryCount(dynamicQuery);
+	}
+
+	/**
+	 * Returns the number of rows matching the dynamic query.
+	 *
+	 * @param dynamicQuery the dynamic query
+	 * @param projection the projection to apply to the query
+	 * @return the number of rows matching the dynamic query
+	 */
+	public static long dynamicQueryCount(
+		DynamicQuery dynamicQuery,
+		com.liferay.portal.kernel.dao.orm.Projection projection) {
+
+		return getService().dynamicQueryCount(dynamicQuery, projection);
+	}
+
+	public static SpringEntry fetchSpringEntry(long SpringEntryId) {
+		return getService().fetchSpringEntry(SpringEntryId);
+	}
+
+	/**
+	 * Returns the spring entry with the matching UUID and company.
+	 *
+	 * @param uuid the spring entry's UUID
+	 * @param companyId the primary key of the company
+	 * @return the matching spring entry, or <code>null</code> if a matching spring entry could not be found
+	 */
+	public static SpringEntry fetchSpringEntryByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		return getService().fetchSpringEntryByUuidAndCompanyId(uuid, companyId);
+	}
+
+	public static com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery
+		getActionableDynamicQuery() {
+
+		return getService().getActionableDynamicQuery();
+	}
+
+	public static
+		com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery
+			getIndexableActionableDynamicQuery() {
+
+		return getService().getIndexableActionableDynamicQuery();
+	}
+
+	/**
+	 * Returns the OSGi service identifier.
+	 *
+	 * @return the OSGi service identifier
+	 */
+	public static String getOSGiServiceIdentifier() {
+		return getService().getOSGiServiceIdentifier();
+	}
+
+	/**
+	 * @throws PortalException
+	 */
+	public static PersistedModel getPersistedModel(Serializable primaryKeyObj)
+		throws PortalException {
+
+		return getService().getPersistedModel(primaryKeyObj);
+	}
+
+	/**
+	 * Returns a range of all the spring entries.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to <code>com.liferay.portal.kernel.dao.orm.QueryUtil#ALL_POS</code> will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent, then the query will include the default ORDER BY logic from <code>com.liferay.portal.tools.service.builder.spring.sample.model.impl.SpringEntryModelImpl</code>.
+	 * </p>
+	 *
+	 * @param start the lower bound of the range of spring entries
+	 * @param end the upper bound of the range of spring entries (not inclusive)
+	 * @return the range of spring entries
+	 */
+	public static List<SpringEntry> getSpringEntries(int start, int end) {
+		return getService().getSpringEntries(start, end);
+	}
+
+	/**
+	 * Returns the number of spring entries.
+	 *
+	 * @return the number of spring entries
+	 */
+	public static int getSpringEntriesCount() {
+		return getService().getSpringEntriesCount();
+	}
+
+	/**
+	 * Returns the spring entry with the primary key.
+	 *
+	 * @param SpringEntryId the primary key of the spring entry
+	 * @return the spring entry
+	 * @throws PortalException if a spring entry with the primary key could not be found
+	 */
+	public static SpringEntry getSpringEntry(long SpringEntryId)
+		throws PortalException {
+
+		return getService().getSpringEntry(SpringEntryId);
+	}
+
+	/**
+	 * Returns the spring entry with the matching UUID and company.
+	 *
+	 * @param uuid the spring entry's UUID
+	 * @param companyId the primary key of the company
+	 * @return the matching spring entry
+	 * @throws PortalException if a matching spring entry could not be found
+	 */
+	public static SpringEntry getSpringEntryByUuidAndCompanyId(
+			String uuid, long companyId)
+		throws PortalException {
+
+		return getService().getSpringEntryByUuidAndCompanyId(uuid, companyId);
+	}
+
+	/**
+	 * Updates the spring entry in the database or adds it if it does not yet exist. Also notifies the appropriate model listeners.
+	 *
+	 * <p>
+	 * <strong>Important:</strong> Inspect SpringEntryLocalServiceImpl for overloaded versions of the method. If provided, use these entry points to the API, as the implementation logic may require the additional parameters defined there.
+	 * </p>
+	 *
+	 * @param springEntry the spring entry
+	 * @return the spring entry that was updated
+	 */
+	public static SpringEntry updateSpringEntry(SpringEntry springEntry) {
+		return getService().updateSpringEntry(springEntry);
+	}
+
+	public static SpringEntryLocalService getService() {
+		return _service;
+	}
+
+	private static volatile SpringEntryLocalService _service;
+
+}
