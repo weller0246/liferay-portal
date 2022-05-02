24199b48a718e011a17f4d7e7ba25b70f12df1ea
CalumR23 <searagan@gmail.com>
2022-05-02 16:12:11 -0700

    POSHI-328 Add util methods to utilize terminal commands

--- modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/util/ExecUtil.java
+++ modules/test/poshi/poshi-runner/src/main/java/com/liferay/poshi/runner/util/ExecUtil.java
@@ -16,17 +16,37 @@ package com.liferay.poshi.runner.util;
 
 import com.liferay.poshi.core.util.OSDetector;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeoutException;
 
 /**
  * @author Michael Hashimoto
  */
 public class ExecUtil {
+
+	public static String executeCommand(String command)
+		throws IOException, TimeoutException {
+
+		for (String terminalCommand : _terminalCommandWhiteList) {
+			if (!command.startsWith(terminalCommand)) {
+				throw new RuntimeException(
+					"Unable to utilize command. Please use git, grep, or sed.");
+			}
+		}
+
+		Process process = executeCommands(
+			true, new File("."), 1000 * 60 * 15, command);
+
+		return readInputStream(process.getInputStream(), true);
+	}
 
 	public static Process executeCommands(
 			boolean exitOnFirstFail, File baseDir, long timeout,
@@ -138,6 +158,33 @@ public class ExecUtil {
 			true, new File("."), _BASH_COMMAND_TIMEOUT_DEFAULT, commands);
 	}
 
+	public static String executeScript(String filePath) throws IOException {
+		List<String> commands = new ArrayList<>();
+		String line = null;
+
+		File file = new File(filePath);
+
+		BufferedReader bufferedReader = new BufferedReader(
+			new FileReader(file));
+
+		while ((line = bufferedReader.readLine()) != null) {
+			commands.add(line);
+		}
+
+		StringBuilder sb = new StringBuilder();
+
+		try {
+			for (String command : commands) {
+				sb.append(executeCommand(command));
+			}
+		}
+		catch (IOException | TimeoutException exception) {
+			throw new RuntimeException(exception);
+		}
+
+		return sb.toString();
+	}
+
 	public static String readInputStream(InputStream inputStream)
 		throws IOException {
 
@@ -189,5 +236,8 @@ public class ExecUtil {
 	}
 
 	private static final long _BASH_COMMAND_TIMEOUT_DEFAULT = 1000 * 60 * 60;
+
+	private static final List<String> _terminalCommandWhiteList = Arrays.asList(
+		"git", "grep", "sed");
 
 }
