2966f612f1cfed63dbcef6499c6e01b7061f2c47
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-05-05 09:14:12 +0100

    LPS-158043 Implement Subscribe/Unsubscribe for Journal Articles.

---
+++ modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/SubscribeJournalArticleContentDashboardItemAction.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.journal.internal.item.action;
+
+import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Cristina González
+ */
+public class SubscribeJournalArticleContentDashboardItemAction
+	implements ContentDashboardItemAction {
+
+	public SubscribeJournalArticleContentDashboardItemAction(
+		HttpServletRequest httpServletRequest, JournalArticle journalArticle,
+		Language language, Portal portal,
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		_httpServletRequest = httpServletRequest;
+		_journalArticle = journalArticle;
+		_language = language;
+		_portal = portal;
+		_requestBackedPortletURLFactory = requestBackedPortletURLFactory;
+	}
+
+	@Override
+	public String getIcon() {
+		return "bell-on";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "subscribe");
+	}
+
+	@Override
+	public String getName() {
+		return "subscribe";
+	}
+
+	@Override
+	public Type getType() {
+		return Type.SUBSCRIBE;
+	}
+
+	@Override
+	public String getURL() {
+		return PortletURLBuilder.create(
+			_requestBackedPortletURLFactory.createActionURL(
+				JournalPortletKeys.JOURNAL)
+		).setActionName(
+			"/journal/subscribe_article"
+		).setRedirect(
+			ParamUtil.getString(_httpServletRequest, "backURL")
+		).setParameter(
+			"articleId", _journalArticle.getResourcePrimKey()
+		).buildString();
+	}
+
+	@Override
+	public String getURL(Locale locale) {
+		return getURL();
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final JournalArticle _journalArticle;
+	private final Language _language;
+	private final Portal _portal;
+	private final RequestBackedPortletURLFactory
+		_requestBackedPortletURLFactory;
+
+}
---
+++ modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/provider/UnsubscribeJournalArticleContentDashboardItemActionProvider.java
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.journal.internal.item.action.provider;
+
+import com.liferay.content.dashboard.item.action.ContentDashboardItemAction;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemActionProvider;
+import com.liferay.content.dashboard.journal.internal.item.action.UnsubscribeJournalArticleContentDashboardItemAction;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.subscription.service.SubscriptionLocalService;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = ContentDashboardItemActionProvider.class)
+public class UnsubscribeJournalArticleContentDashboardItemActionProvider
+	implements ContentDashboardItemActionProvider<JournalArticle> {
+
+	@Override
+	public ContentDashboardItemAction getContentDashboardItemAction(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest) {
+
+		if (!isShow(journalArticle, httpServletRequest)) {
+			return null;
+		}
+
+		return new UnsubscribeJournalArticleContentDashboardItemAction(
+			httpServletRequest, journalArticle, _language, _portal,
+			RequestBackedPortletURLFactoryUtil.create(httpServletRequest));
+	}
+
+	@Override
+	public String getKey() {
+		return "unsubscribe";
+	}
+
+	@Override
+	public ContentDashboardItemAction.Type getType() {
+		return ContentDashboardItemAction.Type.UNSUBSCRIBE;
+	}
+
+	@Override
+	public boolean isShow(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest) {
+
+		return _subscriptionLocalService.isSubscribed(
+			journalArticle.getCompanyId(),
+			_portal.getUserId(httpServletRequest),
+			JournalArticle.class.getName(),
+			journalArticle.getResourcePrimKey());
+	}
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private SubscriptionLocalService _subscriptionLocalService;
+
+}
