20157eaf92df57a4893ad5ce0b9cf9e83f835bfd
gustavolimav <gustavo.lima@liferay.com>
2022-05-06 10:01:04 -0300

    LPS-152644 notifications-admin-service: Add logic to NotificationsQueueEntryLocalServiceImpl
    https://issues.liferay.com/browse/LPS-152644

--- modules/apps/notifications-admin/notifications-admin-service/src/main/java/com/liferay/notifications/admin/service/impl/NotificationsQueueEntryLocalServiceImpl.java
+++ modules/apps/notifications-admin/notifications-admin-service/src/main/java/com/liferay/notifications/admin/service/impl/NotificationsQueueEntryLocalServiceImpl.java
@@ -14,13 +14,36 @@
 
 package com.liferay.notifications.admin.service.impl;
 
+import com.liferay.mail.kernel.model.MailMessage;
+import com.liferay.mail.kernel.service.MailService;
+import com.liferay.notifications.admin.model.NotificationsQueueEntry;
 import com.liferay.notifications.admin.service.base.NotificationsQueueEntryLocalServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexable;
+import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.mail.internet.InternetAddress;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Gabriel Albuquerque
+ * @author Gustavo Lima
  */
 @Component(
 	property = "model.class.name=com.liferay.notifications.admin.model.NotificationsQueueEntry",
@@ -28,4 +51,244 @@ import org.osgi.service.component.annotations.Component;
 )
 public class NotificationsQueueEntryLocalServiceImpl
 	extends NotificationsQueueEntryLocalServiceBaseImpl {
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public NotificationsQueueEntry addNotificationsQueueEntry(
+			long userId, long groupId, String className, long classPK,
+			long notificationsTemplateId, String from, String fromName,
+			String to, String toName, String cc, String bcc, String subject,
+			String body, double priority)
+		throws PortalException {
+
+		User user = userLocalService.getUser(userId);
+
+		long notificationsQueueEntryId = counterLocalService.increment();
+
+		NotificationsQueueEntry notificationsQueueEntry =
+			notificationsQueueEntryPersistence.create(
+				notificationsQueueEntryId);
+
+		notificationsQueueEntry.setGroupId(groupId);
+		notificationsQueueEntry.setCompanyId(user.getCompanyId());
+		notificationsQueueEntry.setUserId(user.getUserId());
+		notificationsQueueEntry.setUserName(user.getFullName());
+		notificationsQueueEntry.setClassName(className);
+		notificationsQueueEntry.setNotificationsQueueEntryId(
+			notificationsTemplateId);
+		notificationsQueueEntry.setClassPK(classPK);
+		notificationsQueueEntry.setFrom(from);
+		notificationsQueueEntry.setFromName(fromName);
+		notificationsQueueEntry.setTo(to);
+		notificationsQueueEntry.setToName(toName);
+		notificationsQueueEntry.setCc(cc);
+		notificationsQueueEntry.setBcc(bcc);
+		notificationsQueueEntry.setSubject(subject);
+		notificationsQueueEntry.setBody(body);
+		notificationsQueueEntry.setPriority(priority);
+
+		return notificationsQueueEntryPersistence.update(
+			notificationsQueueEntry);
+	}
+
+	@Override
+	public void deleteNotificationsQueueEntries(Date sentDate) {
+		notificationsQueueEntryPersistence.removeByLtSentDate(sentDate);
+	}
+
+	@Override
+	public void deleteNotificationsQueueEntries(long groupId)
+		throws PortalException {
+
+		List<NotificationsQueueEntry> notificationsQueueEntries =
+			notificationsQueueEntryPersistence.findByGroupId(groupId);
+
+		for (NotificationsQueueEntry notificationsQueueEntry :
+				notificationsQueueEntries) {
+
+			notificationsQueueEntryLocalService.deleteNotificationsQueueEntry(
+				notificationsQueueEntry);
+		}
+	}
+
+	@Override
+	public NotificationsQueueEntry deleteNotificationsQueueEntry(
+			long notificationsQueueEntryId)
+		throws PortalException {
+
+		NotificationsQueueEntry notificationsQueueEntry =
+			notificationsQueueEntryPersistence.findByPrimaryKey(
+				notificationsQueueEntryId);
+
+		return notificationsQueueEntryLocalService.
+			deleteNotificationsQueueEntry(notificationsQueueEntry);
+	}
+
+	@Indexable(type = IndexableType.DELETE)
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public NotificationsQueueEntry deleteNotificationsQueueEntry(
+		NotificationsQueueEntry notificationsQueueEntry) {
+
+		notificationsQueueEntryPersistence.remove(notificationsQueueEntry);
+
+		return notificationsQueueEntry;
+	}
+
+	@Override
+	public List<NotificationsQueueEntry> getNotificationsQueueEntries(
+		boolean sent) {
+
+		return notificationsQueueEntryPersistence.findBySent(sent);
+	}
+
+	@Override
+	public List<NotificationsQueueEntry> getNotificationsQueueEntries(
+		long groupId, String className, long classPK, boolean sent, int start,
+		int end, OrderByComparator<NotificationsQueueEntry> orderByComparator) {
+
+		return notificationsQueueEntryPersistence.findByG_C_C_S(
+			groupId, classNameLocalService.getClassNameId(className), classPK,
+			sent, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getNotificationsQueueEntriesCount(long groupId) {
+		return notificationsQueueEntryPersistence.countByGroupId(groupId);
+	}
+
+	@Override
+	public int getNotificationsQueueEntriesCount(
+		long groupId, String className, long classPK, boolean sent) {
+
+		return notificationsQueueEntryPersistence.countByG_C_C_S(
+			groupId, classNameLocalService.getClassNameId(className), classPK,
+			sent);
+	}
+
+	@Override
+	public NotificationsQueueEntry resendNotificationsQueueEntry(
+			long notificationsQueueEntryId)
+		throws PortalException {
+
+		return notificationsQueueEntryLocalService.updateSent(
+			notificationsQueueEntryId, false);
+	}
+
+	public void sendNotificationsQueueEntries() throws Exception {
+		List<NotificationsQueueEntry> notificationsQueueEntries =
+			notificationsQueueEntryPersistence.findBySent(false);
+
+		for (NotificationsQueueEntry notificationsQueueEntry :
+				notificationsQueueEntries) {
+
+			InternetAddress from = new InternetAddress(
+				notificationsQueueEntry.getFrom(),
+				notificationsQueueEntry.getFromName());
+			InternetAddress to = new InternetAddress(
+				notificationsQueueEntry.getTo(),
+				notificationsQueueEntry.getToName());
+
+			MailMessage mailMessage = new MailMessage(
+				from, to, notificationsQueueEntry.getSubject(),
+				notificationsQueueEntry.getBody(), true);
+
+			List<InternetAddress> bccInternetAddresses = new ArrayList<>();
+			List<InternetAddress> ccInternetAddresses = new ArrayList<>();
+
+			String[] bccAddresses = StringUtil.split(
+				notificationsQueueEntry.getBcc());
+			String[] ccAddresses = StringUtil.split(
+				notificationsQueueEntry.getCc());
+
+			for (String bccAddress : bccAddresses) {
+				bccInternetAddresses.add(new InternetAddress(bccAddress));
+			}
+
+			for (String ccAddress : ccAddresses) {
+				ccInternetAddresses.add(new InternetAddress(ccAddress));
+			}
+
+			mailMessage.setBCC(
+				bccInternetAddresses.toArray(new InternetAddress[0]));
+			mailMessage.setCC(
+				ccInternetAddresses.toArray(new InternetAddress[0]));
+
+			try {
+				_mailService.sendEmail(mailMessage);
+
+				notificationsQueueEntryLocalService.updateSent(
+					notificationsQueueEntry.getNotificationsQueueEntryId(),
+					true);
+			}
+			catch (Exception exception) {
+				if (_log.isDebugEnabled()) {
+					_log.debug(exception);
+				}
+			}
+		}
+	}
+
+	@Override
+	public void updateNotificationsQueueEntriesTemplateIds(
+		long notificationsTemplateId) {
+
+		List<NotificationsQueueEntry> notificationsQueueEntries =
+			notificationsQueueEntryPersistence.findByNotificationsTemplateId(
+				notificationsTemplateId);
+
+		for (NotificationsQueueEntry notificationsQueueEntry :
+				notificationsQueueEntries) {
+
+			updateNotificationsQueueEntry(notificationsQueueEntry, 0);
+		}
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public NotificationsQueueEntry updateSent(
+			long notificationsQueueEntryId, boolean sent)
+		throws PortalException {
+
+		NotificationsQueueEntry notificationsQueueEntry =
+			notificationsQueueEntryPersistence.findByPrimaryKey(
+				notificationsQueueEntryId);
+
+		notificationsQueueEntry.setSent(sent);
+
+		if (sent) {
+			notificationsQueueEntry.setSentDate(new Date());
+		}
+		else {
+			notificationsQueueEntry.setSentDate(null);
+		}
+
+		return notificationsQueueEntryPersistence.update(
+			notificationsQueueEntry);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	protected NotificationsQueueEntry updateNotificationsQueueEntry(
+		NotificationsQueueEntry notificationsQueueEntry,
+		long notificationsTemplateId) {
+
+		notificationsQueueEntry.setNotificationsTemplateId(
+			notificationsTemplateId);
+
+		return notificationsQueueEntryPersistence.update(
+			notificationsQueueEntry);
+	}
+
+	@Reference
+	protected ClassNameLocalService classNameLocalService;
+
+	@Reference
+	protected UserLocalService userLocalService;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		NotificationsQueueEntryLocalServiceImpl.class);
+
+	@Reference
+	private MailService _mailService;
+
 }
