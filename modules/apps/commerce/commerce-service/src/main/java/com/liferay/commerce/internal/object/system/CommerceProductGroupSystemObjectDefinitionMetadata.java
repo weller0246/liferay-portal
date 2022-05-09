ad15d083fc21665f84e4cc17a98ce8d9b756e437
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-05-09 16:07:16 -0300

    LPS-152408 create Commerce Product Group system object

---
+++ modules/apps/commerce/commerce-service/src/main/java/com/liferay/commerce/internal/object/system/CommerceProductGroupSystemObjectDefinitionMetadata.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.object.system;
+
+import com.liferay.commerce.pricing.model.CommercePricingClass;
+import com.liferay.commerce.pricing.model.CommercePricingClassTable;
+import com.liferay.object.constants.ObjectDefinitionConstants;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.system.BaseSystemObjectDefinitionMetadata;
+import com.liferay.object.system.SystemObjectDefinitionMetadata;
+import com.liferay.petra.sql.dsl.Column;
+import com.liferay.petra.sql.dsl.Table;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Gleice Lisbino
+ */
+@Component(enabled = true, service = SystemObjectDefinitionMetadata.class)
+public class CommerceProductGroupSystemObjectDefinitionMetadata
+	extends BaseSystemObjectDefinitionMetadata {
+
+	@Override
+	public Map<Locale, String> getLabelMap() {
+		return createLabelMap("model.resource.com.liferay.commerce.pricing.model.CommercePricingClass");
+	}
+
+	@Override
+	public Class<?> getModelClass() {
+		return CommercePricingClass.class;
+	}
+
+	@Override
+	public List<ObjectField> getObjectFields() {
+				return Arrays.asList(
+				createObjectField("Text", "String", "name", "title", false),
+				createObjectField("Integer", "Integer", "number-of-products", "productsCount", false));
+	}
+
+	@Override
+	public Map<Locale, String> getPluralLabelMap() {
+		return createLabelMap("product-groups");
+	}
+
+	@Override
+	public Column<?, Long> getPrimaryKeyColumn() {
+		return CommercePricingClassTable.INSTANCE.commercePricingClassId;
+	}
+
+	@Override
+	public String getRESTContextPath() {
+		return "headless-commerce-admin-catalog/v1.0/product-groups";
+	}
+
+	@Override
+	public String getScope() {
+		return ObjectDefinitionConstants.SCOPE_COMPANY;
+	}
+
+	@Override
+	public Table getTable() {
+		return CommercePricingClassTable.INSTANCE;
+	}
+
+	@Override
+	public int getVersion() {
+		return 1;
+	}
+
+}
