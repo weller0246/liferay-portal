f15c0687029c59f0b8bbc11a9fa7216518ed6cc3
Sandro Chinea <sandrochinea@gmail.com>
2022-05-10 11:55:48 +0100

    LPS-152455 Implement useHasInputChild hook

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/useHasInputChild.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/useHasInputChild.js
@@ -12,6 +12,92 @@
  * details.
  */
 
-export default function useHasInputChild() {
-	return () => false;
+import {useCallback} from 'react';
+
+import {FREEMARKER_FRAGMENT_ENTRY_PROCESSOR} from '../config/constants/freemarkerFragmentEntryProcessor';
+import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
+import {useSelectorCallback, useSelectorRef} from '../contexts/StoreContext';
+import selectFormConfiguration from '../selectors/selectFormConfiguration';
+import FormService from '../services/FormService';
+import useCache from './useCache';
+
+function isMappedToRequiredInput(fragmentEntryLink, formFields) {
+	const flattenedFields = formFields.flatMap((fieldSet) => fieldSet.fields);
+
+	const fieldId =
+		fragmentEntryLink.editableValues[FREEMARKER_FRAGMENT_ENTRY_PROCESSOR]
+			.inputFieldId;
+
+	return flattenedFields.some(
+		(field) => field.key === fieldId && field.required
+	);
+}
+
+function hasRequiredInputChild(
+	item,
+	layoutData,
+	fragmentEntryLinks,
+	formFields
+) {
+	return item.children.some((childId) => {
+		const child = layoutData.items[childId];
+
+		if (child.type !== LAYOUT_DATA_ITEM_TYPES.fragment) {
+			return hasRequiredInputChild(
+				child,
+				layoutData,
+				fragmentEntryLinks,
+				formFields
+			);
+		}
+
+		const fragmentEntryLink =
+			fragmentEntryLinks[child.config.fragmentEntryLinkId];
+
+		return (
+			isMappedToRequiredInput(fragmentEntryLink, formFields) ||
+			hasRequiredInputChild(
+				child,
+				layoutData,
+				fragmentEntryLinks,
+				formFields
+			)
+		);
+	});
+}
+
+export default function useHasInputChild(itemId) {
+	const layoutDataRef = useSelectorRef((state) => state.layoutData);
+	const fragmentEntryLinksRef = useSelectorRef(
+		(state) => state.fragmentEntryLinks
+	);
+
+	const formConfiguration = useSelectorCallback(
+		(state) =>
+			selectFormConfiguration(
+				state.layoutData?.items[itemId],
+				state.layoutData
+			),
+		[itemId]
+	);
+
+	const {classNameId, classTypeId} = formConfiguration;
+
+	const formFields = useCache({
+		fetcher: () => FormService.getFormFields({classNameId, classTypeId}),
+		key: ['formFields', classNameId, classTypeId],
+	});
+
+	return useCallback(
+		() =>
+			formFields
+				? hasRequiredInputChild(
+						layoutDataRef.current?.items[itemId],
+						layoutDataRef.current,
+						fragmentEntryLinksRef.current,
+						formFields
+				  )
+				: false,
+		[formFields, layoutDataRef, fragmentEntryLinksRef, itemId]
+	);
 }
