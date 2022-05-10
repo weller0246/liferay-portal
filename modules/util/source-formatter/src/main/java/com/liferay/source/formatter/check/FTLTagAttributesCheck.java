ce78d105573c90ee90196e05ff15214a584d0cb0
Qi Zhang <qi.zhang@liferay.com>
2022-05-10 08:47:56 +0800

    LPS-153192 Add logic for enforce macro tag style

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/FTLTagAttributesCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/FTLTagAttributesCheck.java
@@ -20,6 +20,11 @@ import com.liferay.portal.kernel.io.unsync.UnsyncBufferedReader;
 import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
 import com.liferay.portal.kernel.util.StringUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Hugo Huijser
  */
@@ -38,6 +43,138 @@ public class FTLTagAttributesCheck extends BaseTagAttributesCheck {
 		content = formatIncorrectLineBreak(fileName, content);
 
 		content = _formatTagAttributes(absolutePath, content);
+
+		content = _formatMacroTagAttributes(content);
+
+		return content;
+	}
+
+	private String[] _checkAttributeArray(String[] attributeArray) {
+		if (attributeArray.length == 1) {
+			return attributeArray;
+		}
+
+		List<String> attributes = new ArrayList<>();
+
+		boolean continueFlg = false;
+
+		int newArrayIndex = 0;
+
+		for (int i = 0; i < attributeArray.length; i++) {
+			if (continueFlg) {
+				continueFlg = false;
+
+				continue;
+			}
+
+			String attribute = attributeArray[i];
+
+			if (StringUtil.equals(attribute, StringPool.EQUAL)) {
+				if ((i == 0) || ((i + 1) == attributeArray.length)) {
+					return new String[0];
+				}
+
+				newArrayIndex--;
+
+				attributes.set(
+					newArrayIndex,
+					StringBundler.concat(
+						attributes.get(newArrayIndex), StringPool.SPACE,
+						attribute, StringPool.SPACE, attributeArray[i + 1]));
+
+				newArrayIndex++;
+				continueFlg = true;
+			}
+			else {
+				attributes.add(attribute);
+				newArrayIndex++;
+			}
+		}
+
+		return attributes.toArray(new String[0]);
+	}
+
+	private boolean _checkTab(String indent, String attribute) {
+		if (!attribute.startsWith(indent)) {
+			return false;
+		}
+
+		int index = attribute.indexOf(indent);
+
+		String substringResult = attribute.substring(index + indent.length());
+
+		return !substringResult.startsWith(StringPool.TAB);
+	}
+
+	private String _formatMacroTagAttributes(String content) {
+		Matcher matcher = _macroTagAttributePattern.matcher(content);
+
+		while (matcher.find()) {
+			String indent = matcher.group(1) + StringPool.TAB;
+
+			String attributes = matcher.group(2);
+
+			String[] rowAttributeArray = attributes.split(StringPool.NEW_LINE);
+
+			int rowNumber = 0;
+
+			for (String rowAttribute : rowAttributeArray) {
+				rowNumber++;
+
+				String[] attributeArray = rowAttribute.split(StringPool.SPACE);
+
+				if ((attributeArray.length == 1) &&
+					((rowNumber == 1) ||
+					 _checkTab(indent, attributeArray[0]))) {
+
+					continue;
+				}
+
+				attributeArray = _checkAttributeArray(attributeArray);
+
+				if (attributeArray.length == 0) {
+					continue;
+				}
+
+				StringBundler sb = new StringBundler();
+
+				for (int i = 0; i < attributeArray.length; i++) {
+					String attribute = attributeArray[i];
+
+					if ((rowNumber != 1) || (i != 0)) {
+						sb.append(indent);
+					}
+
+					sb.append(StringUtil.trimLeading(attribute));
+					sb.append(StringPool.NEW_LINE);
+				}
+
+				if (sb.index() > 0) {
+					sb.setIndex(sb.index() - 1);
+				}
+
+				String replace = sb.toString();
+
+				if ((attributeArray.length == 1) &&
+					StringUtil.equals(rowAttribute, replace)) {
+
+					continue;
+				}
+
+				return StringUtil.replaceFirst(
+					content, rowAttribute, replace, matcher.start(2));
+			}
+
+			int tagLine = getLineNumber(content, matcher.start(1));
+			int tagEndLine = getLineNumber(content, matcher.end(2));
+
+			if (!attributes.endsWith(StringPool.NEW_LINE) &&
+				(tagLine != tagEndLine)) {
+
+				return StringUtil.insert(
+					content, StringPool.NEW_LINE, matcher.end(2));
+			}
+		}
 
 		return content;
 	}
@@ -76,5 +213,8 @@ public class FTLTagAttributesCheck extends BaseTagAttributesCheck {
 
 		return content;
 	}
+
+	private static final Pattern _macroTagAttributePattern = Pattern.compile(
+		"(\t*)<#macro ([\\s\\S]*?)>");
 
 }
