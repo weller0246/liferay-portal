6850f12e1c3e4e2faceff60ce1ee259bb6a6546f
Sandro Chinea <sandrochinea@gmail.com>
2022-05-10 16:06:30 +0100

    LPS-152455 Implement useIsSomeFormIncomplete hook

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/useIsSomeFormIncomplete.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/useIsSomeFormIncomplete.js
@@ -12,6 +12,105 @@
  * details.
  */
 
+import {FREEMARKER_FRAGMENT_ENTRY_PROCESSOR} from '../config/constants/freemarkerFragmentEntryProcessor';
+import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
+import {useSelectorRef} from '../contexts/StoreContext';
+import FormService from '../services/FormService';
+import {getCacheItem, getCacheKey} from './cache';
+import getFragmentItem from './getFragmentItem';
+import {isLayoutDataItemDeleted} from './isLayoutDataItemDeleted';
+
+function getDescendantIds(layoutData, itemId) {
+	const item = layoutData.items[itemId];
+
+	const descendantIds = [...item.children];
+
+	item.children.forEach((childId) => {
+		if (!isLayoutDataItemDeleted(layoutData, childId)) {
+			descendantIds.push(...getDescendantIds(layoutData, childId));
+		}
+	});
+
+	return descendantIds;
+}
+
+function getFormItems(layoutData) {
+	return Object.values(layoutData.items).filter(
+		(item) =>
+			item.type === LAYOUT_DATA_ITEM_TYPES.form &&
+			item.config.classNameId !== '0' &&
+			!isLayoutDataItemDeleted(layoutData, item.itemId)
+	);
+}
+
 export default function useIsSomeFormIncomplete() {
-	return () => false;
+	const stateRef = useSelectorRef((state) => state);
+
+	return () => {
+		const {fragmentEntryLinks, layoutData} = stateRef.current;
+
+		const forms = getFormItems(layoutData);
+
+		if (!forms.length) {
+			return Promise.resolve(false);
+		}
+
+		const promises = forms.map((form) => {
+			const {
+				config: {classNameId, classTypeId},
+				itemId,
+			} = form;
+
+			const payload = {
+				classNameId,
+				classTypeId,
+			};
+
+			const cacheKey = getCacheKey([
+				'formFields',
+				classNameId,
+				classTypeId,
+			]);
+
+			const {data: fields} = getCacheItem(cacheKey);
+
+			const promise = fields
+				? Promise.resolve({fields, itemId})
+				: FormService.getFormFields(payload).then((fields) => ({
+						fields,
+						itemId,
+				  }));
+
+			return promise;
+		});
+
+		return Promise.all(promises).then((forms) =>
+			forms.some(({fields, itemId}) => {
+				const requiredFields = fields
+					.flatMap((fieldSet) => fieldSet.fields)
+					.filter((field) => field.required);
+
+				const descendantIds = getDescendantIds(layoutData, itemId);
+
+				return requiredFields.some(
+					(field) =>
+						!Object.values(fragmentEntryLinks).some(
+							(fragmentEntryLink) => {
+								const item = getFragmentItem(
+									layoutData,
+									fragmentEntryLink.fragmentEntryLinkId
+								);
+
+								return (
+									fragmentEntryLink.editableValues?.[
+										FREEMARKER_FRAGMENT_ENTRY_PROCESSOR
+									]?.inputFieldId === field.key &&
+									descendantIds.includes(item.itemId)
+								);
+							}
+						)
+				);
+			})
+		);
+	};
 }
