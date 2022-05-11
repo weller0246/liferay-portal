88c5fc4fadb9a066248719df8deef0d029c26be9
Noor Najjar <noor.najjar@liferay.com>
2022-05-11 15:17:32 -0700

    LPS-149277 Create test class for JournalHelper and add test case for new method

---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/util/test/JournalHelperTest.java
@@ -0,0 +1,131 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.util.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.constants.JournalFolderConstants;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.journal.util.JournalHelper;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.portlet.constants.FriendlyURLResolverConstants;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Noor Najjar
+ */
+@RunWith(Arquillian.class)
+public class JournalHelperTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		Layout layout = LayoutTestUtil.addTypePortletLayout(_group);
+
+		_themeDisplay = new ThemeDisplay();
+
+		_themeDisplay.setCompany(
+			_companyLocalService.getCompany(_group.getCompanyId()));
+		_themeDisplay.setLayout(layout);
+		_themeDisplay.setLocale(LocaleUtil.US);
+		_themeDisplay.setLookAndFeel(
+			layout.getTheme(), layout.getColorScheme());
+
+		User user = _userLocalService.getUser(_group.getCreatorUserId());
+
+		_themeDisplay.setRealUser(user);
+
+		HttpServletRequest httpServletRequest = new MockHttpServletRequest();
+
+		httpServletRequest.setAttribute(WebKeys.THEME_DISPLAY, _themeDisplay);
+
+		_themeDisplay.setRequest(httpServletRequest);
+
+		_themeDisplay.setResponse(new MockHttpServletResponse());
+		_themeDisplay.setScopeGroupId(_group.getGroupId());
+		_themeDisplay.setSiteGroupId(_group.getGroupId());
+		_themeDisplay.setUser(user);
+	}
+
+	@Test
+	public void testBuildURLPattern() throws Exception {
+		JournalArticle article = JournalTestUtil.addArticle(
+			_group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID, "Test",
+			RandomTestUtil.randomString());
+
+		Layout layout = _themeDisplay.getLayout();
+
+		Assert.assertEquals(
+			getArticleURL(),
+			_journalHelper.buildURLPattern(
+				article, layout.isPrivateLayout(), _themeDisplay,
+				_themeDisplay.getLocale(),
+				FriendlyURLResolverConstants.URL_SEPARATOR_JOURNAL_ARTICLE));
+	}
+
+	protected String getArticleURL() {
+		return StringBundler.concat(
+			"/web", _group.getFriendlyURL(),
+			FriendlyURLResolverConstants.URL_SEPARATOR_JOURNAL_ARTICLE, "test");
+	}
+
+	@Inject
+	private CompanyLocalService _companyLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private JournalHelper _journalHelper;
+
+	private ThemeDisplay _themeDisplay;
+
+	@Inject
+	private UserLocalService _userLocalService;
+
+}
