ccc0d4c3cf710dd8f74689ec494eba682cdf0d49
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-05-11 17:53:51 +0200

    COMMERCE-9020 Updated resource implementation

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-site-setting-impl/src/main/java/com/liferay/headless/commerce/admin/site/setting/internal/resource/v1_0/MeasurementUnitResourceImpl.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-admin-site-setting-impl/src/main/java/com/liferay/headless/commerce/admin/site/setting/internal/resource/v1_0/MeasurementUnitResourceImpl.java
@@ -14,9 +14,34 @@
 
 package com.liferay.headless.commerce.admin.site.setting.internal.resource.v1_0;
 
+import com.liferay.commerce.product.constants.CPMeasurementUnitConstants;
+import com.liferay.commerce.product.exception.CPMeasurementUnitTypeException;
+import com.liferay.commerce.product.exception.DuplicateCPMeasurementUnitExternalReferenceCodeException;
+import com.liferay.commerce.product.exception.DuplicateCPMeasurementUnitKeyException;
+import com.liferay.commerce.product.exception.NoSuchCPMeasurementUnitException;
+import com.liferay.commerce.product.model.CPMeasurementUnit;
+import com.liferay.commerce.product.service.CPMeasurementUnitService;
 import com.liferay.headless.commerce.admin.site.setting.dto.v1_0.MeasurementUnit;
-import com.liferay.headless.commerce.admin.site.setting.internal.helper.v1_0.MeasurementUnitHelper;
+import com.liferay.headless.commerce.admin.site.setting.internal.dto.v1_0.converter.MeasurementUnitDTOConverter;
 import com.liferay.headless.commerce.admin.site.setting.resource.v1_0.MeasurementUnitResource;
+import com.liferay.headless.commerce.core.util.LanguageUtils;
+import com.liferay.headless.commerce.core.util.ServiceContextHelper;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.search.filter.Filter;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
+import com.liferay.portal.vulcan.pagination.Page;
+import com.liferay.portal.vulcan.pagination.Pagination;
+import com.liferay.portal.vulcan.util.TransformUtil;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
 
 import javax.ws.rs.core.Response;
 
@@ -26,6 +51,7 @@ import org.osgi.service.component.annotations.ServiceScope;
 
 /**
  * @author Zoltán Takács
+ * @author Crescenzo Rega
  */
 @Component(
 	enabled = false,
@@ -36,8 +62,100 @@ public class MeasurementUnitResourceImpl
 	extends BaseMeasurementUnitResourceImpl {
 
 	@Override
-	public Response deleteMeasurementUnit(Long id) throws Exception {
-		_measurementUnitHelper.deleteMeasurementUnit(id);
+	public void deleteMeasurementUnit(Long id) throws Exception {
+		_findById(id);
+
+		_cpMeasurementUnitService.deleteCPMeasurementUnit(id);
+	}
+
+	@Override
+	public void deleteMeasurementUnitByExternalReferenceCode(
+			String externalReferenceCode)
+		throws Exception {
+
+		CPMeasurementUnit cpMeasurementUnit = _findByExternalReferenceCode(
+			externalReferenceCode);
+
+		_cpMeasurementUnitService.deleteCPMeasurementUnit(
+			cpMeasurementUnit.getCPMeasurementUnitId());
+	}
+
+	@Override
+	public void deleteMeasurementUnitByKey(String key) throws Exception {
+		CPMeasurementUnit cpMeasurementUnit = _findByKey(key);
+
+		_cpMeasurementUnitService.deleteCPMeasurementUnit(
+			cpMeasurementUnit.getCPMeasurementUnitId());
+	}
+
+	@Override
+	public MeasurementUnit getMeasurementUnit(Long id) throws Exception {
+		return _toMeasurementUnit(_findById(id));
+	}
+
+	@Override
+	public MeasurementUnit getMeasurementUnitByExternalReferenceCode(
+			String externalReferenceCode)
+		throws Exception {
+
+		return _toMeasurementUnit(
+			_findByExternalReferenceCode(externalReferenceCode));
+	}
+
+	@Override
+	public MeasurementUnit getMeasurementUnitByKey(String key)
+		throws Exception {
+
+		return _toMeasurementUnit(_findByKey(key));
+	}
+
+	@Override
+	public Page<MeasurementUnit> getMeasurementUnitsByType(
+			String measurementUnitType, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		try {
+			int type = _getType(measurementUnitType);
+
+			return Page.of(
+				TransformUtil.transform(
+					_cpMeasurementUnitService.getCPMeasurementUnitsByType(
+						contextCompany.getCompanyId(), type,
+						pagination.getStartPosition(),
+						pagination.getEndPosition(), null),
+					this::_toMeasurementUnit),
+				pagination,
+				_cpMeasurementUnitService.getCPMeasurementUnitsCount(
+					contextCompany.getCompanyId(), type));
+		}
+		catch (CPMeasurementUnitTypeException cpMeasurementUnitTypeException) {
+			return Page.of(Collections.emptyList());
+		}
+	}
+
+	@Override
+	public Page<MeasurementUnit> getMeasurementUnitsPage(
+			Filter filter, Pagination pagination, Sort[] sorts)
+		throws Exception {
+
+		return Page.of(
+			TransformUtil.transform(
+				_cpMeasurementUnitService.getCPMeasurementUnits(
+					contextCompany.getCompanyId(),
+					pagination.getStartPosition(), pagination.getEndPosition(),
+					null),
+				this::_toMeasurementUnit),
+			pagination,
+			_cpMeasurementUnitService.getCPMeasurementUnitsCount(
+				contextCompany.getCompanyId()));
+	}
+
+	@Override
+	public Response patchMeasurementUnit(
+			Long id, MeasurementUnit measurementUnit)
+		throws Exception {
+
+		_updateMeasurementUnit(measurementUnit, _findById(id));
 
 		Response.ResponseBuilder responseBuilder = Response.ok();
 
@@ -45,11 +163,222 @@ public class MeasurementUnitResourceImpl
 	}
 
 	@Override
-	public MeasurementUnit getMeasurementUnit(Long id) throws Exception {
-		return _measurementUnitHelper.getMeasurementUnit(id);
+	public Response patchMeasurementUnitByExternalReferenceCode(
+			String externalReferenceCode, MeasurementUnit measurementUnit)
+		throws Exception {
+
+		_updateMeasurementUnit(
+			measurementUnit,
+			_findByExternalReferenceCode(externalReferenceCode));
+
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		return responseBuilder.build();
+	}
+
+	@Override
+	public Response patchMeasurementUnitByKey(
+			String key, MeasurementUnit measurementUnit)
+		throws Exception {
+
+		_updateMeasurementUnit(measurementUnit, _findByKey(key));
+
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		return responseBuilder.build();
+	}
+
+	@Override
+	public MeasurementUnit postMeasurementUnit(MeasurementUnit measurementUnit)
+		throws Exception {
+
+		try {
+			return _toMeasurementUnit(
+				_cpMeasurementUnitService.addCPMeasurementUnit(
+					measurementUnit.getExternalReferenceCode(),
+					LanguageUtils.getLocalizedMap(measurementUnit.getName()),
+					measurementUnit.getKey(), measurementUnit.getRate(),
+					_getPrimary(
+						measurementUnit.getPrimary(),
+						_getType(measurementUnit.getType())),
+					measurementUnit.getPriority(),
+					_getType(measurementUnit.getType()),
+					_serviceContextHelper.getServiceContext(contextUser)));
+		}
+		catch (Exception exception) {
+			if (exception instanceof
+					DuplicateCPMeasurementUnitExternalReferenceCodeException) {
+
+				throw new DuplicateCPMeasurementUnitExternalReferenceCodeException(
+					"There is another measurement unit with external " +
+						"reference code " +
+							measurementUnit.getExternalReferenceCode());
+			}
+
+			if (exception instanceof DuplicateCPMeasurementUnitKeyException) {
+				throw new DuplicateCPMeasurementUnitKeyException(
+					"There is another measurement unit with key " +
+						measurementUnit.getKey());
+			}
+
+			throw new Exception(exception.getMessage(), exception.getCause());
+		}
+	}
+
+	private CPMeasurementUnit _findByExternalReferenceCode(
+			String externalReferenceCode)
+		throws Exception {
+
+		CPMeasurementUnit cpMeasurementUnit =
+			_cpMeasurementUnitService.
+				fetchCPMeasurementUnitByExternalReferenceCode(
+					contextCompany.getCompanyId(), externalReferenceCode);
+
+		if (cpMeasurementUnit == null) {
+			throw new NoSuchCPMeasurementUnitException(
+				"Unable to find measurement unit with external reference " +
+					"code " + externalReferenceCode);
+		}
+
+		return cpMeasurementUnit;
+	}
+
+	private CPMeasurementUnit _findById(Long id) throws Exception {
+		CPMeasurementUnit cpMeasurementUnit =
+			_cpMeasurementUnitService.fetchCPMeasurementUnit(id);
+
+		if (cpMeasurementUnit == null) {
+			throw new NoSuchCPMeasurementUnitException(
+				"Unable to find measurement unit with id " + id);
+		}
+
+		return cpMeasurementUnit;
+	}
+
+	private CPMeasurementUnit _findByKey(String key) throws Exception {
+		CPMeasurementUnit cpMeasurementUnit =
+			_cpMeasurementUnitService.fetchCPMeasurementUnitByKey(
+				contextCompany.getCompanyId(), key);
+
+		if (cpMeasurementUnit == null) {
+			throw new NoSuchCPMeasurementUnitException(
+				"Unable to find measurement unit with key " + key);
+		}
+
+		return cpMeasurementUnit;
+	}
+
+	private Boolean _getPrimary(Boolean primary, int type) throws Exception {
+		CPMeasurementUnit cpMeasurementUnit =
+			_cpMeasurementUnitService.fetchPrimaryCPMeasurementUnitByType(
+				contextCompany.getCompanyId(), type);
+
+		if (cpMeasurementUnit == null) {
+			return Boolean.TRUE;
+		}
+
+		if (primary == null) {
+			return Boolean.FALSE;
+		}
+
+		return primary;
+	}
+
+	private int _getType(String measurementUnitType) throws Exception {
+		if (Validator.isNumber(measurementUnitType) &&
+			CPMeasurementUnitConstants.typesMap.containsKey(
+				GetterUtil.getInteger(measurementUnitType))) {
+
+			return GetterUtil.getInteger(measurementUnitType);
+		}
+
+		if (CPMeasurementUnitConstants.typesMap.containsValue(
+				measurementUnitType)) {
+
+			Set<Map.Entry<Integer, String>> entries =
+				CPMeasurementUnitConstants.typesMap.entrySet();
+
+			Stream<Map.Entry<Integer, String>> stream = entries.stream();
+
+			return stream.filter(
+				type -> type.getValue(
+				).equalsIgnoreCase(
+					measurementUnitType
+				)
+			).map(
+				Map.Entry::getKey
+			).findFirst(
+			).orElse(
+				-1
+			);
+		}
+
+		throw new CPMeasurementUnitTypeException(
+			"Unable to find measurement unit with type " + measurementUnitType);
+	}
+
+	private MeasurementUnit _toMeasurementUnit(
+			CPMeasurementUnit cpMeasurementUnit)
+		throws Exception {
+
+		return _measurementUnitDTOConverter.toDTO(
+			new DefaultDTOConverterContext(
+				contextAcceptLanguage.isAcceptAllLanguages(),
+				HashMapBuilder.put(
+					"delete",
+					addAction(
+						ActionKeys.DELETE, cpMeasurementUnit,
+						"deleteMeasurementUnit")
+				).put(
+					"get",
+					addAction(
+						ActionKeys.VIEW, cpMeasurementUnit,
+						"getMeasurementUnit")
+				).put(
+					"update",
+					addAction(
+						ActionKeys.UPDATE, cpMeasurementUnit,
+						"patchMeasurementUnit")
+				).build(),
+				_dtoConverterRegistry,
+				cpMeasurementUnit.getCPMeasurementUnitId(),
+				contextAcceptLanguage.getPreferredLocale(), contextUriInfo,
+				contextUser));
+	}
+
+	private void _updateMeasurementUnit(
+			MeasurementUnit measurementUnit,
+			CPMeasurementUnit cpMeasurementUnit)
+		throws Exception {
+
+		_cpMeasurementUnitService.updateCPMeasurementUnit(
+			GetterUtil.get(
+				measurementUnit.getExternalReferenceCode(),
+				cpMeasurementUnit.getExternalReferenceCode()),
+			cpMeasurementUnit.getCPMeasurementUnitId(),
+			LanguageUtils.getLocalizedMap(measurementUnit.getName()),
+			GetterUtil.get(
+				measurementUnit.getKey(), cpMeasurementUnit.getKey()),
+			GetterUtil.get(
+				measurementUnit.getRate(), cpMeasurementUnit.getRate()),
+			GetterUtil.get(
+				measurementUnit.getPrimary(), cpMeasurementUnit.isPrimary()),
+			GetterUtil.get(
+				measurementUnit.getPriority(), cpMeasurementUnit.getPriority()),
+			_getType(measurementUnit.getType()),
+			_serviceContextHelper.getServiceContext(contextUser));
 	}
 
 	@Reference
-	private MeasurementUnitHelper _measurementUnitHelper;
+	private CPMeasurementUnitService _cpMeasurementUnitService;
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private MeasurementUnitDTOConverter _measurementUnitDTOConverter;
+
+	@Reference
+	private ServiceContextHelper _serviceContextHelper;
 
 }
