ccf056b408d75d804874e6465dd1edf4b2d3597f
Charlotte Wong <chiharutaru@gmail.com>
2022-05-11 10:42:54 -0700

    LRCI-2865 Add conditional logic for rendering Jenkins report based on retesting

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/RootCauseAnalysisToolBuild.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/RootCauseAnalysisToolBuild.java
@@ -234,6 +234,56 @@ public class RootCauseAnalysisToolBuild extends DefaultTopLevelBuild {
 		List<LocalGitCommit> historicalLocalGitCommits =
 			_workspaceGitRepository.getHistoricalLocalGitCommits();
 
+		LocalGitCommit retestLocalGitCommit = historicalLocalGitCommits.get(0);
+
+		String firstSHA = retestLocalGitCommit.getSHA();
+
+		String secondSHA = null;
+
+		if (historicalLocalGitCommits.get(1) != null) {
+			retestLocalGitCommit = historicalLocalGitCommits.get(1);
+
+			secondSHA = retestLocalGitCommit.getSHA();
+		}
+
+		if ((historicalLocalGitCommits.size() > 1) &&
+			firstSHA.equals(secondSHA)) {
+
+			for (int i = 0; i < historicalLocalGitCommits.size(); i++) {
+				LocalGitCommit localGitCommit = historicalLocalGitCommits.get(
+					i);
+
+				String sha = localGitCommit.getSHA();
+
+				for (BuildData buildData : buildDataList) {
+					if (buildData instanceof PortalBuildData) {
+						PortalBuildData currentPortalBuildData =
+							(PortalBuildData)buildData;
+
+						if (sha.equals(
+								currentPortalBuildData.getPortalBranchSHA())) {
+
+							if (currentPortalBuildData != null) {
+								gitCommitGroup = new GitCommitGroup(
+									currentPortalBuildData);
+
+								gitCommitGroups.add(gitCommitGroup);
+							}
+							else if (i == 0) {
+								gitCommitGroup = new GitCommitGroup(null);
+
+								gitCommitGroups.add(gitCommitGroup);
+							}
+
+							gitCommitGroup.add(localGitCommit);
+						}
+					}
+				}
+			}
+
+			return gitCommitGroups;
+		}
+
 		for (int i = 0; i < historicalLocalGitCommits.size(); i++) {
 			LocalGitCommit localGitCommit = historicalLocalGitCommits.get(i);
 
