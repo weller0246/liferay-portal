042aa72992b5f3e3cc49a03abc5f57a306e6089a
Matija Petanjek <matija.petanjek@liferay.com>
2022-05-11 16:36:11 +0200

    COMMERCE-9206 batch-engine-api - extract relevant properties to company scoped config and keep the rest in system scoped

--- modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/configuration/BatchEngineTaskCompanyConfiguration.java
+++ modules/apps/batch-engine/batch-engine-api/src/main/java/com/liferay/batch/engine/configuration/BatchEngineTaskCompanyConfiguration.java
@@ -19,26 +19,26 @@ import aQute.bnd.annotation.metatype.Meta;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
 
 /**
- * @author Ivica Cardic
+ * @author Matija Petanjek
  */
-@ExtendedObjectClassDefinition(category = "batch-engine")
-@Meta.OCD(
-	id = "com.liferay.batch.engine.configuration.BatchEngineTaskConfiguration",
-	localization = "content/Language",
-	name = "batch-engine-task-configuration-name"
+@ExtendedObjectClassDefinition(
+	category = "batch-engine",
+	scope = ExtendedObjectClassDefinition.Scope.COMPANY
 )
-public interface BatchEngineTaskConfiguration {
+@Meta.OCD(
+	id = "com.liferay.batch.engine.configuration.BatchEngineTaskCompanyConfiguration",
+	localization = "content/Language",
+	name = "batch-engine-task-company-configuration-name"
+)
+public interface BatchEngineTaskCompanyConfiguration {
 
-	@Meta.AD(
-		deflt = "14", name = "completed-tasks-cleaner-scan-interval",
-		required = false
-	)
-	public int completedTasksCleanerScanInterval();
+	@Meta.AD(deflt = ",", name = "csv-file-column-delimiter", required = false)
+	public String csvFileColumnDelimiter();
 
-	@Meta.AD(deflt = "30", name = "orphanage-threshold", required = false)
-	public int orphanageThreshold();
+	@Meta.AD(deflt = "100", name = "export-batch-size", required = false)
+	public int exportBatchSize();
 
-	@Meta.AD(deflt = "60", name = "orphan-scan-interval", required = false)
-	public int orphanScanInterval();
+	@Meta.AD(deflt = "100", name = "import-batch-size", required = false)
+	public int importBatchSize();
 
 }
