828610e32aeadcd153dc3a41025c7825eba6184d
Alan Huang <alan.huang@liferay.com>
2022-05-16 18:51:57 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/redirect/redirect-web/src/main/java/com/liferay/redirect/web/internal/portlet/RedirectPortlet.java
+++ modules/apps/redirect/redirect-web/src/main/java/com/liferay/redirect/web/internal/portlet/RedirectPortlet.java
@@ -53,7 +53,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + RedirectPortletKeys.REDIRECT,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
