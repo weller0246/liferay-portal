bf966368af145c6e0c5dd15dea4e3eb503530891
gustavolimav <gustavo.lima@liferay.com>
2022-05-16 16:21:38 -0300

    LPS-152788 Implement ResourceImpl

--- modules/apps/notification/notification-rest-impl/src/main/java/com/liferay/notification/rest/internal/resource/v1_0/NotificationTemplateResourceImpl.java
+++ modules/apps/notification/notification-rest-impl/src/main/java/com/liferay/notification/rest/internal/resource/v1_0/NotificationTemplateResourceImpl.java
@@ -14,9 +14,15 @@
 
 package com.liferay.notification.rest.internal.resource.v1_0;
 
+import com.liferay.notification.rest.dto.v1_0.NotificationTemplate;
 import com.liferay.notification.rest.resource.v1_0.NotificationTemplateResource;
+import com.liferay.notification.service.NotificationTemplateService;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -28,4 +34,132 @@ import org.osgi.service.component.annotations.ServiceScope;
 )
 public class NotificationTemplateResourceImpl
 	extends BaseNotificationTemplateResourceImpl {
+
+	@Override
+	public void deleteNotificationTemplate(Long notificationTemplateId)
+		throws Exception {
+
+		_notificationTemplateService.deleteNotificationTemplate(
+			notificationTemplateId);
+	}
+
+	@Override
+	public NotificationTemplate getNotificationTemplate(
+			Long notificationTemplateId)
+		throws Exception {
+
+		return _toNotificationTemplate(
+			_notificationTemplateService.getNotificationTemplate(
+				notificationTemplateId));
+	}
+
+	@Override
+	public NotificationTemplate postNotificationTemplate(
+			NotificationTemplate notificationTemplate)
+		throws Exception {
+
+		return _toNotificationTemplate(
+			_notificationTemplateService.addNotificationTemplate(
+				contextUser.getUserId(), notificationTemplate.getName(),
+				notificationTemplate.getDescription(),
+				notificationTemplate.getFrom(),
+				LocalizedMapUtil.getLocalizedMap(
+					notificationTemplate.getFromNameMap()),
+				notificationTemplate.getTo(), notificationTemplate.getCc(),
+				notificationTemplate.getBcc(), notificationTemplate.getEnable(),
+				LocalizedMapUtil.getLocalizedMap(
+					notificationTemplate.getSubjectMap()),
+				LocalizedMapUtil.getLocalizedMap(
+					notificationTemplate.getBodyMap())));
+	}
+
+	@Override
+	public NotificationTemplate putNotificationTemplate(
+			Long notificationTemplateId,
+			NotificationTemplate notificationTemplate)
+		throws Exception {
+
+		return _toNotificationTemplate(
+			_notificationTemplateService.updateNotificationTemplate(
+				notificationTemplateId, notificationTemplate.getName(),
+				notificationTemplate.getDescription(),
+				notificationTemplate.getFrom(),
+				LocalizedMapUtil.getLocalizedMap(
+					notificationTemplate.getFromNameMap()),
+				notificationTemplate.getTo(), notificationTemplate.getCc(),
+				notificationTemplate.getBcc(), notificationTemplate.getEnable(),
+				LocalizedMapUtil.getLocalizedMap(
+					notificationTemplate.getSubjectMap()),
+				LocalizedMapUtil.getLocalizedMap(
+					notificationTemplate.getBodyMap())));
+	}
+
+	private NotificationTemplate _toNotificationTemplate(
+		com.liferay.notification.model.NotificationTemplate
+			serviceBuilderNotificationTemplate) {
+
+		return new NotificationTemplate() {
+			{
+				actions = HashMapBuilder.put(
+					"delete",
+					addAction(
+						ActionKeys.DELETE, "deleteNotificationTemplate",
+						com.liferay.notification.model.NotificationTemplate.
+							class.getName(),
+						serviceBuilderNotificationTemplate.
+							getNotificationTemplateId())
+				).put(
+					"get",
+					addAction(
+						ActionKeys.VIEW, "getNotificationTemplate",
+						com.liferay.notification.model.NotificationTemplate.
+							class.getName(),
+						serviceBuilderNotificationTemplate.
+							getNotificationTemplateId())
+				).put(
+					"permissions",
+					addAction(
+						ActionKeys.PERMISSIONS, "patchNotificationTemplate",
+						com.liferay.notification.model.NotificationTemplate.
+							class.getName(),
+						serviceBuilderNotificationTemplate.
+							getNotificationTemplateId())
+				).put(
+					"update",
+					addAction(
+						ActionKeys.UPDATE, "putNotificationTemplate",
+						com.liferay.notification.model.NotificationTemplate.
+							class.getName(),
+						serviceBuilderNotificationTemplate.
+							getNotificationTemplateId())
+				).build();
+				bcc = serviceBuilderNotificationTemplate.getBcc();
+				bodyMap = LocalizedMapUtil.getI18nMap(
+					serviceBuilderNotificationTemplate.getBodyMap());
+				cc = serviceBuilderNotificationTemplate.getCc();
+				dateCreated =
+					serviceBuilderNotificationTemplate.getCreateDate();
+				dateModified =
+					serviceBuilderNotificationTemplate.getModifiedDate();
+				description =
+					serviceBuilderNotificationTemplate.getDescription();
+				enable = serviceBuilderNotificationTemplate.getEnabled();
+				from = serviceBuilderNotificationTemplate.getFrom();
+				fromNameMap = LocalizedMapUtil.getI18nMap(
+					serviceBuilderNotificationTemplate.getFromNameMap());
+				id =
+					serviceBuilderNotificationTemplate.
+						getNotificationTemplateId();
+				name = serviceBuilderNotificationTemplate.getName();
+				name_i18n = LocalizedMapUtil.getI18nMap(
+					serviceBuilderNotificationTemplate.getNameMap());
+				subjectMap = LocalizedMapUtil.getI18nMap(
+					serviceBuilderNotificationTemplate.getSubjectMap());
+			}
+		};
+	}
+
+	@Reference
+	private NotificationTemplateService _notificationTemplateService;
+
 }
