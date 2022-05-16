cb57369f1abfff41c25989bb8700d3b8aea2d6ed
gustavolimav <gustavo.lima@liferay.com>
2022-05-16 18:08:41 -0300

    LPS-152788 Add rest test Coverage

--- modules/apps/notification/notification-rest-test/src/testIntegration/java/com/liferay/notification/rest/resource/v1_0/test/NotificationTemplateResourceTest.java
+++ modules/apps/notification/notification-rest-test/src/testIntegration/java/com/liferay/notification/rest/resource/v1_0/test/NotificationTemplateResourceTest.java
@@ -15,15 +15,123 @@
 package com.liferay.notification.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.notification.rest.client.dto.v1_0.NotificationTemplate;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.vulcan.util.LocalizedMapUtil;
 
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Gabriel Albuquerque
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class NotificationTemplateResourceTest
 	extends BaseNotificationTemplateResourceTestCase {
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetNotificationTemplate() throws Exception {
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetNotificationTemplateNotFound() {
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLGetNotificationTemplatesPage() throws Exception {
+	}
+
+	@Override
+	protected NotificationTemplate randomNotificationTemplate()
+		throws Exception {
+
+		NotificationTemplate notificationTemplate =
+			super.randomNotificationTemplate();
+
+		notificationTemplate.setFromNameMap(
+			LocalizedMapUtil.getI18nMap(
+				RandomTestUtil.randomLocaleStringMap()));
+		notificationTemplate.setBodyMap(
+			LocalizedMapUtil.getI18nMap(
+				RandomTestUtil.randomLocaleStringMap()));
+		notificationTemplate.setSubjectMap(
+			LocalizedMapUtil.getI18nMap(
+				RandomTestUtil.randomLocaleStringMap()));
+
+		return notificationTemplate;
+	}
+
+	@Override
+	protected NotificationTemplate
+			testDeleteNotificationTemplate_addNotificationTemplate()
+		throws Exception {
+
+		return _addNotificationTemplate(randomNotificationTemplate());
+	}
+
+	@Override
+	protected NotificationTemplate
+			testGetNotificationTemplate_addNotificationTemplate()
+		throws Exception {
+
+		return _addNotificationTemplate(randomNotificationTemplate());
+	}
+
+	@Override
+	protected NotificationTemplate
+			testGetNotificationTemplatesPage_addNotificationTemplate(
+				NotificationTemplate notificationTemplate)
+		throws Exception {
+
+		return _addNotificationTemplate(notificationTemplate);
+	}
+
+	@Override
+	protected NotificationTemplate
+			testGraphQLNotificationTemplate_addNotificationTemplate()
+		throws Exception {
+
+		return _addNotificationTemplate(randomNotificationTemplate());
+	}
+
+	@Override
+	protected NotificationTemplate
+			testPatchNotificationTemplate_addNotificationTemplate()
+		throws Exception {
+
+		return _addNotificationTemplate(randomNotificationTemplate());
+	}
+
+	@Override
+	protected NotificationTemplate
+			testPostNotificationTemplate_addNotificationTemplate(
+				NotificationTemplate notificationTemplate)
+		throws Exception {
+
+		return _addNotificationTemplate(notificationTemplate);
+	}
+
+	@Override
+	protected NotificationTemplate
+			testPutNotificationTemplate_addNotificationTemplate()
+		throws Exception {
+
+		return _addNotificationTemplate(randomNotificationTemplate());
+	}
+
+	private NotificationTemplate _addNotificationTemplate(
+			NotificationTemplate notificationTemplate)
+		throws Exception {
+
+		return notificationTemplateResource.postNotificationTemplate(
+			notificationTemplate);
+	}
+
 }
