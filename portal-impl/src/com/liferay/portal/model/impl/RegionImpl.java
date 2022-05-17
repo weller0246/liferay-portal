015e4a233795fe73b808c779900e4b7a2bb73613
pei-jung <pei-jung.lan@liferay.com>
2022-05-16 17:41:21 -0700

    LPS-153684 (portal-impl) - add title to region json object

--- portal-impl/src/com/liferay/portal/model/impl/RegionImpl.java
+++ portal-impl/src/com/liferay/portal/model/impl/RegionImpl.java
@@ -14,8 +14,40 @@
 
 package com.liferay.portal.model.impl;
 
+import com.liferay.portal.kernel.json.JSON;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
+
 /**
  * @author Brian Wing Shun Chan
  */
+@JSON(strict = true)
 public class RegionImpl extends RegionBaseImpl {
+
+	@JSON
+	@Override
+	public String getTitle() {
+		String title = getTitle(
+			LocaleUtil.toLanguageId(getLocale(getTitleCurrentLanguageId())),
+			true);
+
+		if (Validator.isNotNull(title)) {
+			return title;
+		}
+
+		return getName();
+	}
+
+	@Override
+	public String getTitleCurrentLanguageId() {
+		return _titleCurrentLanguageId;
+	}
+
+	@Override
+	public void setTitleCurrentLanguageId(String languageId) {
+		_titleCurrentLanguageId = languageId;
+	}
+
+	private String _titleCurrentLanguageId;
+
 }
