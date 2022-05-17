f8d0efb79e0aad0ce71acdd4dde84ecaf0e2b65c
Joana Lima <joana.lima@liferay.com>
2022-05-17 17:59:36 -0300

    LPS-152582 Create Commission Chart Layout

---
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/src/common/components/line-chart/index.js
@@ -0,0 +1,123 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayChart from '@clayui/charts';
+import ClayIcon from '@clayui/icon';
+import classNames from 'classnames';
+import React from 'react';
+
+import ClayIconProvider from '../../../common/context/ClayIconProvider';
+
+import './index.css';
+
+const LineChart = ({
+	axispaddingleft = 0.1,
+	axispaddingright = 0.5,
+	chartData,
+	percentual,
+	value,
+	getDataDate,
+	getPeriod,
+	height = 150,
+	patternColor = ['#4BC286'],
+	pointRadius = 1.5,
+	showaxisx = false,
+	showaxisy = false,
+	width = 200,
+}) => {
+	return (
+		<ClayIconProvider>
+			<div className="align-items-center d-flex flex-wrap justify-content-center line-chart-container mt-3 py-1">
+				{chartData && (
+					<ClayChart
+						axis={{
+							x: {
+								padding: {
+									left: axispaddingleft,
+									right: axispaddingright,
+								},
+								show: showaxisx,
+							},
+							y: {
+								show: showaxisy,
+							},
+						}}
+						color={{
+							pattern: patternColor,
+						}}
+						data={chartData}
+						legend={{
+							show: false,
+						}}
+						line={{
+							classes: ['line-class-data'],
+						}}
+						point={{
+							r: pointRadius,
+						}}
+						size={{
+							height,
+							width,
+						}}
+						tooltip={{
+							contents: (data) => {
+								const title = Liferay.Language.get(data[0].id);
+
+								const value = data[0].value.toFixed(1);
+
+								return `<div class="line-chart-tooltip bg-neutral-0 d-flex font-weight-bold rounded-sm p-2"><span class="d-flex font-weight-normal mr-2 w-100">${title}</span> $${value}</div>`;
+							},
+						}}
+					/>
+				)}
+
+				<div className="d-flex h-100 justify-content-end line-chart-legend mt-5">
+					<div className="font-weight-normal mb-2 text-neutral-8 text-paragraph-sm">
+						{getDataDate}
+					</div>
+
+					<div className="font-weight-bolder h5 line-chart-screen mb-3">
+						{new Intl.NumberFormat('en-US', {
+							currency: 'USD',
+							style: 'currency',
+						}).format(value)}
+					</div>
+
+					<div
+						className={classNames(
+							'text-paragraph-sm font-weight-bolder',
+							{
+								'line-chart-icon-success-color':
+									percentual >= 0,
+								'text-danger': percentual < 0,
+							}
+						)}
+					>
+						{percentual > 0 && <ClayIcon symbol="caret-top" />}
+
+						{percentual === 0 && <ClayIcon symbol="hr" />}
+
+						{percentual < 0 && <ClayIcon symbol="caret-bottom" />}
+
+						{getPeriod === 1
+							? `${percentual}% MoM`
+							: `${percentual}% YoY`}
+					</div>
+				</div>
+			</div>
+		</ClayIconProvider>
+	);
+};
+
+export default LineChart;
---
+++ modules/apps/site-initializer/site-initializer-raylife-ap/src/main/resources/site-initializer/fragments/src/raylife-ap-react-fragments/fragments/dashboard-commission/index.js
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ClaySelect} from '@clayui/form';
+import React, {useState} from 'react';
+
+import LineChart from '../../../common/components/line-chart';
+
+export default function () {
+	const months = [
+		'January',
+		'February',
+		'March',
+		'April',
+		'May',
+		'June',
+		'July',
+		'August',
+		'September',
+		'October',
+		'November',
+		'December',
+	];
+
+	const d = new Date();
+	const month = months[d.getMonth()];
+
+	const [selectedFilterDate, setSelectedFilterDate] = useState('1');
+
+	const options = [
+		{
+			label: 'This Month',
+			value: '1',
+		},
+		{
+			label: '6 MO',
+			value: '2',
+		},
+		{
+			label: 'YTD',
+			value: '3',
+		},
+	];
+
+	const loadData = [
+		{
+			commissionValue: 20954.67,
+			dataColumns: ['Commission', 10000, 10954.67],
+			percentcommission: 15,
+			period: 1,
+			periodDate: month,
+		},
+		{
+			commissionValue: 69126.95,
+			dataColumns: ['Commission', 60, 20, 70, 10, 30, 50],
+			percentcommission: -10,
+			period: 2,
+			periodDate: 'Dec 2021 - May 2022',
+		},
+		{
+			commissionValue: 22633.29,
+			dataColumns: ['Commission', 60, 20, 70, 10, 30],
+			percentcommission: 0,
+			period: 3,
+			periodDate: 'Jan - May',
+		},
+	];
+
+	const getData = () => {
+		return loadData.filter(
+			(data) => data.period === Number(selectedFilterDate)
+		);
+	};
+
+	const chartData = {
+		columns: [getData()[0]?.dataColumns],
+		type: 'line',
+	};
+
+	const commissionPercentual = getData()[0]?.percentcommission;
+	const getDataDate = getData()[0]?.periodDate;
+	const commissionValue = getData()[0]?.commissionValue;
+	const getPeriod = getData()[0]?.period;
+
+	return (
+		<div className="d-flex dashboard-commission-container flex-column flex-shrink-0 pb-4 pt-3 px-3">
+			<div className="align-items-center d-flex dashboard-commission-header justify-content-between">
+				<div className="dashboard-commission-title font-weight-bolder h4">
+					Commission
+				</div>
+
+				<ClaySelect
+					className="dashboard-commission-select"
+					onChange={({target}) => {
+						setSelectedFilterDate(target.value);
+					}}
+					sizing="sm"
+					value={selectedFilterDate}
+				>
+					{options.map((item) => (
+						<ClaySelect.Option
+							key={item.value}
+							label={item.label}
+							value={item.value}
+						/>
+					))}
+				</ClaySelect>
+			</div>
+
+			{chartData.columns.length > 0 && (
+				<LineChart
+					chartData={chartData}
+					getDataDate={getDataDate}
+					getPeriod={getPeriod}
+					percentual={commissionPercentual}
+					value={commissionValue}
+				/>
+			)}
+
+			{chartData.columns.length === 0 && loadData && (
+				<div className="align-items-center d-flex flex-grow-1 justify-content-center">
+					<span>No Data Applications</span>
+				</div>
+			)}
+		</div>
+	);
+}
