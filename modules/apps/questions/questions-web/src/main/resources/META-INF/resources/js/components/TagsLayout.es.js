0bbef37eb82f8934d579b958373b8aae8a07de40
Lucas Soares <lucas.soares@liferay.com>
2022-05-17 18:09:50 -0300

    LPS-152317 Fix link to filter by tags

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/TagsLayout.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/components/TagsLayout.es.js
@@ -27,7 +27,6 @@ import SubscriptionButton from './SubscriptionButton.es';
 
 export default function TagsLayout({
 	context,
-	linkPage,
 	orderBy,
 	page,
 	pageSize,
@@ -36,16 +35,9 @@ export default function TagsLayout({
 }) {
 	return (
 		<div className="question-tags" key={tag.id}>
-			<div className="align-items-center card card-interactive card-interactive-primary card-type-template d-flex justify-content-between template-card-horizontal">
-				<div>
-					<Link
-						title={tag.name}
-						to={
-							linkPage === 'subscriptions'
-								? `/questions/${linkPage}/${tag.name}`
-								: '/tags'
-						}
-					>
+			<Link title={tag.name} to={`/questions/tag/${tag.name}`}>
+				<div className="align-items-center card card-interactive card-interactive-primary card-type-template d-flex justify-content-between template-card-horizontal">
+					<div>
 						<div className="card-body d-flex flex-column">
 							<div className="card-row">
 								<div className="autofit-row autofit-row-center autofit-row-expand">
@@ -91,46 +83,46 @@ export default function TagsLayout({
 								</div>
 							</div>
 						</div>
-					</Link>
-				</div>
+					</div>
 
-				<div className="c-pr-3">
-					{tag.actions.subscribe && (
-						<div className="autofit-col">
-							<div className="autofit-section">
-								<SubscriptionButton
-									isSubscribed={tag.subscribed}
-									onSubscription={() => {
-										deleteCacheKey(
-											getTagsOrderByDateCreatedQuery,
-											{
-												page,
-												pageSize,
-												search,
-												siteKey: context,
-											}
-										);
-										deleteCacheKey(
-											getTagsOrderByNumberOfUsagesQuery,
-											{
-												page,
-												pageSize,
-												search,
-												siteKey: context,
-											}
-										);
-									}}
-									queryVariables={{
-										keywordId: tag.id,
-									}}
-									subscribeQuery={subscribeTagQuery}
-									unsubscribeQuery={unsubscribeTagQuery}
-								/>
+					<div className="c-pr-3">
+						{tag.actions.subscribe && (
+							<div className="autofit-col">
+								<div className="autofit-section">
+									<SubscriptionButton
+										isSubscribed={tag.subscribed}
+										onSubscription={() => {
+											deleteCacheKey(
+												getTagsOrderByDateCreatedQuery,
+												{
+													page,
+													pageSize,
+													search,
+													siteKey: context,
+												}
+											);
+											deleteCacheKey(
+												getTagsOrderByNumberOfUsagesQuery,
+												{
+													page,
+													pageSize,
+													search,
+													siteKey: context,
+												}
+											);
+										}}
+										queryVariables={{
+											keywordId: tag.id,
+										}}
+										subscribeQuery={subscribeTagQuery}
+										unsubscribeQuery={unsubscribeTagQuery}
+									/>
+								</div>
 							</div>
-						</div>
-					)}
+						)}
+					</div>
 				</div>
-			</div>
+			</Link>
 		</div>
 	);
 }
