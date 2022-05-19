da9f5d3a1e78eec5df40a93bad8ba2c539ae5336
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-05-19 20:17:24 -0300

    LPS-153931 Apply to use in Actions

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/GroovyObjectActionExecutorImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/GroovyObjectActionExecutorImpl.java
@@ -17,12 +17,11 @@ package com.liferay.object.internal.action.executor;
 import com.liferay.object.action.executor.ObjectActionExecutor;
 import com.liferay.object.constants.ObjectActionExecutorConstants;
 import com.liferay.object.internal.action.util.ObjectActionDataConverterUtil;
+import com.liferay.object.runtime.scripting.executor.GroovyScriptingExecutor;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.scripting.Scripting;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 
 import java.util.HashSet;
-import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,10 +38,10 @@ public class GroovyObjectActionExecutorImpl implements ObjectActionExecutor {
 			JSONObject payloadJSONObject, long userId)
 		throws Exception {
 
-		_execute(
+		_groovyScriptingExecutor.execute(
 			ObjectActionDataConverterUtil.convertPayloadJSONObject(
 				payloadJSONObject),
-			parametersUnicodeProperties.get("script"));
+			new HashSet<>(), parametersUnicodeProperties.get("script"));
 	}
 
 	@Override
@@ -50,29 +49,7 @@ public class GroovyObjectActionExecutorImpl implements ObjectActionExecutor {
 		return ObjectActionExecutorConstants.KEY_GROOVY;
 	}
 
-	private void _execute(Map<String, Object> inputObjects, String script)
-		throws Exception {
-
-		Thread currentThread = Thread.currentThread();
-
-		ClassLoader contextClassLoader = currentThread.getContextClassLoader();
-
-		Class<?> clazz = getClass();
-
-		ClassLoader classLoader = clazz.getClassLoader();
-
-		try {
-			currentThread.setContextClassLoader(classLoader);
-
-			_scripting.eval(
-				null, inputObjects, new HashSet<>(), "groovy", script);
-		}
-		finally {
-			currentThread.setContextClassLoader(contextClassLoader);
-		}
-	}
-
 	@Reference
-	private Scripting _scripting;
+	private GroovyScriptingExecutor _groovyScriptingExecutor;
 
 }
