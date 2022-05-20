a66cdc596ac325feab19ab688dd5b77c4509ffbf
Joao Victor <joao.victor@liferay.com>
2022-05-20 15:45:41 -0300

    LPS-151046 Clean Role Name after change Role Type

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/shared-components/BaseRoleType.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/shared-components/BaseRoleType.js
@@ -206,13 +206,13 @@ const BaseRoleType = ({
 
 		setFilterRoleType(true);
 		setSelectedRoleType(event.target.value);
-		setSelectedRoleName(null);
+		setSelectedRoleName('');
 	};
 
 	const roleTypeItemClick = (item) => {
 		setSelectedRoleType(item);
 		setRoleTypeDropdownActive(false);
-		setSelectedRoleName(null);
+		setSelectedRoleName('');
 	};
 
 	const initialLoading = networkStatus === 1;
