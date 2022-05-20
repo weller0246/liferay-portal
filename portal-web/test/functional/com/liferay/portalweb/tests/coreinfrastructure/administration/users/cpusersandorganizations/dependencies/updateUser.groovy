6df3d1ccf8ede4ec37c4442c6a810d04e5b56f4a
Sharry Shi <sharry.shi@liferay.com>
2022-05-20 14:42:40 +0800

    LRQA-74948 Update functional test script

--- portal-web/test/functional/com/liferay/portalweb/tests/coreinfrastructure/administration/users/cpusersandorganizations/dependencies/updateUser.groovy
+++ portal-web/test/functional/com/liferay/portalweb/tests/coreinfrastructure/administration/users/cpusersandorganizations/dependencies/updateUser.groovy
@@ -13,8 +13,7 @@ try {
 	UserLocalServiceUtil.updateUser(
 		user.getUserId(), "", "",
 		"", false, user.getReminderQueryQuestion(),
-		user.getReminderQueryAnswer(), user.getScreenName(), "userea@liferay.com",
-		user.getFacebookId(), user.getOpenId(), true, null,
+		user.getReminderQueryAnswer(), user.getScreenName(), "userea@liferay.com", true, null,
 		user.getLanguageId(), user.getTimeZoneId(), user.getGreeting(),
 		user.getComments(), "", user.getMiddleName(), "",
 		0L, 0L,user.getMale(),
