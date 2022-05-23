e9f9db051c51bbd62f9e10532c62402930914e3f
Brian Chan <brian.chan@liferay.com>
2022-05-23 08:33:59 -0500

    LPS-153699 SF

--- modules/apps/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserDisplayContext.java
+++ modules/apps/asset/asset-browser-web/src/main/java/com/liferay/asset/browser/web/internal/display/context/AssetBrowserDisplayContext.java
@@ -102,7 +102,8 @@ public class AssetBrowserDisplayContext {
 
 		if (AssetBrowserWebConfigurationValues.SEARCH_WITH_DATABASE) {
 			long[] subtypeSelectionIds = ArrayUtil.filter(
-				new long[] {getSubtypeSelectionId()}, id -> id >= 0);
+				new long[] {getSubtypeSelectionId()},
+				subtypeSelectionId -> subtypeSelectionId >= 0);
 
 			assetBrowserSearch.setResultsAndTotal(
 				() -> AssetEntryLocalServiceUtil.getEntries(
