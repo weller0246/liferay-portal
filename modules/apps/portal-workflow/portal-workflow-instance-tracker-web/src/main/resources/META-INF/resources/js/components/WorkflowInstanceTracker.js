378db6c5ad0375a09fede9837848b4a8f2021560
michaelmdelcavalcanti <michael.cavalcanti@liferay.com>
2022-05-24 17:59:02 -0300

    LPS-145267 Add Accept-Language in GET to workflow-definitions

--- modules/apps/portal-workflow/portal-workflow-instance-tracker-web/src/main/resources/META-INF/resources/js/components/WorkflowInstanceTracker.js
+++ modules/apps/portal-workflow/portal-workflow-instance-tracker-web/src/main/resources/META-INF/resources/js/components/WorkflowInstanceTracker.js
@@ -38,6 +38,8 @@ export default function WorkflowInstanceTracker({workflowInstanceId}) {
 	const [visitedNodes, setVisitedNodes] = useState([]);
 	const [definitionElements, setDefinitionElements] = useState({});
 
+	const languageId = themeDisplay.getLanguageId().replace('_', '-');
+
 	useEffect(() => {
 		fetch(
 			`/o/headless-admin-workflow/v1.0/workflow-instances/${workflowInstanceId}`,
@@ -50,6 +52,9 @@ export default function WorkflowInstanceTracker({workflowInstanceId}) {
 				fetch(
 					`/o/headless-admin-workflow/v1.0/workflow-definitions/by-name/${data.workflowDefinitionName}`,
 					{
+						headers: {
+							'Accept-Language': languageId,
+						},
 						method: 'GET',
 						params: {
 							version: data.workflowDefinitionVersion,
