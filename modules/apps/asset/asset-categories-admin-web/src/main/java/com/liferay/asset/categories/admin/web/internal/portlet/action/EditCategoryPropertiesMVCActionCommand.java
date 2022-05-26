ecfefd648d32fa276824f0461ba4c89c0fb0d77e
Diego Hu <diego.hu@liferay.com>
2022-05-26 17:15:20 +0200

    LPS-153638 Create EditCategoryPropertiesMVCActionCommand, copy from AssetCategoryAdminPortlet

---
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/portlet/action/EditCategoryPropertiesMVCActionCommand.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.portlet.action;
+
+import com.liferay.asset.categories.admin.web.constants.AssetCategoriesAdminPortletKeys;
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetCategoryConstants;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Diego Hu
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AssetCategoriesAdminPortletKeys.ASSET_CATEGORIES_ADMIN,
+		"mvc.command.name=/asset_categories_admin/edit_category_properties"
+	},
+	service = MVCActionCommand.class
+)
+public class EditCategoryPropertiesMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long categoryId = ParamUtil.getLong(actionRequest, "categoryId");
+
+		AssetCategory category = _assetCategoryService.fetchCategory(
+			categoryId);
+
+		String[] categoryProperties = _getCategoryProperties(actionRequest);
+
+		ServiceContext serviceContext = ServiceContextFactory.getInstance(
+			AssetCategory.class.getName(), actionRequest);
+
+		_assetCategoryService.updateCategory(
+			categoryId, category.getParentCategoryId(), category.getTitleMap(),
+			category.getDescriptionMap(), category.getVocabularyId(),
+			categoryProperties, serviceContext);
+	}
+
+	private String[] _getCategoryProperties(ActionRequest actionRequest) {
+		int[] categoryPropertiesIndexes = StringUtil.split(
+			ParamUtil.getString(actionRequest, "categoryPropertiesIndexes"), 0);
+
+		String[] categoryProperties =
+			new String[categoryPropertiesIndexes.length];
+
+		for (int i = 0; i < categoryPropertiesIndexes.length; i++) {
+			int categoryPropertiesIndex = categoryPropertiesIndexes[i];
+
+			String key = ParamUtil.getString(
+				actionRequest, "key" + categoryPropertiesIndex);
+
+			if (Validator.isNull(key)) {
+				continue;
+			}
+
+			String value = ParamUtil.getString(
+				actionRequest, "value" + categoryPropertiesIndex);
+
+			categoryProperties[i] =
+				key + AssetCategoryConstants.PROPERTY_KEY_VALUE_SEPARATOR +
+					value;
+		}
+
+		return categoryProperties;
+	}
+
+	@Reference
+	private AssetCategoryService _assetCategoryService;
+
+}
