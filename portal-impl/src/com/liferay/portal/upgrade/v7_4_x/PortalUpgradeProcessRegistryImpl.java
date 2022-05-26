4f6be19f7d66b1489a0b4be3004497b117d3e92e
Rubén Pulido <ruben.pulido@liferay.com>
2022-05-27 00:01:04 +0200

    LPS-154398 Upgrade process adding faviconFileEntryId column to Layout and LayoutSet

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
@@ -92,6 +92,9 @@ public class PortalUpgradeProcessRegistryImpl
 
 		upgradeProcesses.put(
 			new Version(14, 0, 0), new UpgradeExternalReferenceCode());
+
+		upgradeProcesses.put(
+			new Version(14, 0, 1), new UpgradeFaviconFileEntryIdColumn());
 	}
 
 }
