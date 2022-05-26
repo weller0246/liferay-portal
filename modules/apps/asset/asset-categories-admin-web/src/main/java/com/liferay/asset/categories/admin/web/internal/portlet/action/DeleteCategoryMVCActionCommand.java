631278b4e856a37fe347ed8cd9fb6caad08a6f06
Diego Hu <diego.hu@liferay.com>
2022-05-26 08:17:37 +0200

    LPS-153635 Create DeleteCategoryMVCActionCommand

---
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/portlet/action/DeleteCategoryMVCActionCommand.java
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.categories.admin.web.internal.portlet.action;
+
+import com.liferay.asset.categories.admin.web.constants.AssetCategoriesAdminPortletKeys;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Diego Hu
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + AssetCategoriesAdminPortletKeys.ASSET_CATEGORIES_ADMIN,
+		"mvc.command.name=/asset_categories_admin/delete_category"
+	},
+	service = MVCActionCommand.class
+)
+public class DeleteCategoryMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		long[] deleteCategoryIds = null;
+
+		long categoryId = ParamUtil.getLong(actionRequest, "categoryId");
+
+		if (categoryId > 0) {
+			deleteCategoryIds = new long[] {categoryId};
+		}
+		else {
+			deleteCategoryIds = ParamUtil.getLongValues(
+				actionRequest, "rowIds");
+		}
+
+		_assetCategoryService.deleteCategories(deleteCategoryIds);
+	}
+
+	@Reference
+	private AssetCategoryService _assetCategoryService;
+
+}
