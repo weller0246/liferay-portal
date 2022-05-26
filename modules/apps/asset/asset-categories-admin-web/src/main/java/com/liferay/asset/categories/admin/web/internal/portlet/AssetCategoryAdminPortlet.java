67ad8b489a065388733a58c4d9b7e7977a0a4999
Diego Hu <diego.hu@liferay.com>
2022-05-26 16:14:32 +0200

    LPS-153637 Remove not needed steps

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/portlet/AssetCategoryAdminPortlet.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/portlet/AssetCategoryAdminPortlet.java
@@ -20,8 +20,6 @@ import com.liferay.asset.categories.admin.web.internal.constants.AssetCategories
 import com.liferay.asset.category.property.exception.CategoryPropertyKeyException;
 import com.liferay.asset.category.property.exception.CategoryPropertyValueException;
 import com.liferay.asset.category.property.exception.DuplicateCategoryPropertyException;
-import com.liferay.asset.category.property.model.AssetCategoryProperty;
-import com.liferay.asset.category.property.service.AssetCategoryPropertyLocalService;
 import com.liferay.asset.display.page.portlet.AssetDisplayPageEntryFormProcessor;
 import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
 import com.liferay.asset.kernel.NoSuchClassTypeException;
@@ -35,21 +33,13 @@ import com.liferay.asset.kernel.exception.NoSuchEntryException;
 import com.liferay.asset.kernel.exception.NoSuchVocabularyException;
 import com.liferay.asset.kernel.exception.VocabularyNameException;
 import com.liferay.asset.kernel.model.AssetCategory;
-import com.liferay.asset.kernel.model.AssetCategoryConstants;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetCategoryService;
 import com.liferay.asset.kernel.service.AssetVocabularyService;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
-import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
-import com.liferay.portal.kernel.service.ServiceContext;
-import com.liferay.portal.kernel.service.ServiceContextFactory;
-import com.liferay.portal.kernel.servlet.MultiSessionMessages;
 import com.liferay.portal.kernel.servlet.SessionErrors;
-import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.HtmlUtil;
-import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
@@ -57,8 +47,6 @@ import com.liferay.portal.kernel.util.WebKeys;
 import java.io.IOException;
 
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 import java.util.Map;
 
 import javax.portlet.ActionRequest;
@@ -99,83 +87,6 @@ import org.osgi.service.component.annotations.Reference;
 	service = Portlet.class
 )
 public class AssetCategoryAdminPortlet extends MVCPortlet {
-
-	public void editCategory(
-			ActionRequest actionRequest, ActionResponse actionResponse)
-		throws Exception {
-
-		long categoryId = ParamUtil.getLong(actionRequest, "categoryId");
-
-		long parentCategoryId = ParamUtil.getLong(
-			actionRequest, "parentCategoryId");
-		Map<Locale, String> titleMap = LocalizationUtil.getLocalizationMap(
-			actionRequest, "title");
-		Map<Locale, String> descriptionMap =
-			LocalizationUtil.getLocalizationMap(actionRequest, "description");
-		long vocabularyId = ParamUtil.getLong(actionRequest, "vocabularyId");
-
-		ServiceContext serviceContext = ServiceContextFactory.getInstance(
-			AssetCategory.class.getName(), actionRequest);
-
-		hideDefaultSuccessMessage(actionRequest);
-
-		MultiSessionMessages.add(
-			actionRequest, actionResponse.getNamespace() + "requestProcessed");
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		AssetCategory category = null;
-
-		if (categoryId <= 0) {
-
-			// Add category
-
-			long groupId = ParamUtil.getLong(actionRequest, "groupId");
-
-			category = _assetCategoryService.addCategory(
-				groupId, parentCategoryId, titleMap, descriptionMap,
-				vocabularyId, null, serviceContext);
-
-			MultiSessionMessages.add(
-				actionRequest, "categoryAdded",
-				LanguageUtil.format(
-					_portal.getHttpServletRequest(actionRequest),
-					"x-was-created-successfully",
-					new Object[] {
-						HtmlUtil.escape(titleMap.get(themeDisplay.getLocale()))
-					}));
-		}
-		else {
-
-			// Update category
-
-			String[] categoryPropertiesArray = _getCategoryProperties(
-				_assetCategoryPropertyLocalService.getCategoryProperties(
-					categoryId));
-
-			category = _assetCategoryService.updateCategory(
-				categoryId, parentCategoryId, titleMap, descriptionMap,
-				vocabularyId, categoryPropertiesArray, serviceContext);
-
-			MultiSessionMessages.add(
-				actionRequest, "categoryUpdated",
-				LanguageUtil.format(
-					_portal.getHttpServletRequest(actionRequest),
-					"x-was-updated-successfully",
-					new Object[] {
-						HtmlUtil.escape(titleMap.get(themeDisplay.getLocale()))
-					}));
-		}
-
-		// Asset display page
-
-		_assetDisplayPageEntryFormProcessor.process(
-			AssetCategory.class.getName(), category.getCategoryId(),
-			actionRequest);
-
-		sendRedirect(actionRequest, actionResponse);
-	}
 
 	public void setCategoryDisplayPageTemplate(
 			ActionRequest actionRequest, ActionResponse actionResponse)
@@ -261,33 +172,11 @@ public class AssetCategoryAdminPortlet extends MVCPortlet {
 		return false;
 	}
 
-	private String[] _getCategoryProperties(
-		List<AssetCategoryProperty> categoryProperties) {
-
-		String[] categoryPropertiesArray =
-			new String[categoryProperties.size()];
-
-		for (int i = 0; i < categoryProperties.size(); i++) {
-			AssetCategoryProperty categoryProperty = categoryProperties.get(i);
-
-			categoryPropertiesArray[i] =
-				categoryProperty.getKey() +
-					AssetCategoryConstants.PROPERTY_KEY_VALUE_SEPARATOR +
-						categoryProperty.getValue();
-		}
-
-		return categoryPropertiesArray;
-	}
-
 	private volatile AssetCategoriesAdminWebConfiguration
 		_assetCategoriesAdminWebConfiguration;
 
 	@Reference
 	private AssetCategoryLocalService _assetCategoryLocalService;
-
-	@Reference
-	private AssetCategoryPropertyLocalService
-		_assetCategoryPropertyLocalService;
 
 	@Reference
 	private AssetCategoryService _assetCategoryService;
