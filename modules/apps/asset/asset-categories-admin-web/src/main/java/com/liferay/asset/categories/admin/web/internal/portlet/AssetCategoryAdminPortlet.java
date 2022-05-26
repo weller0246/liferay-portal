d355b61c210b1120ce6559ae80d8bbe42f8fc208
Diego Hu <diego.hu@liferay.com>
2022-05-26 17:39:17 +0200

    LPS-153639 Remove not needed steps

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/portlet/AssetCategoryAdminPortlet.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/portlet/AssetCategoryAdminPortlet.java
@@ -24,7 +24,6 @@ import com.liferay.asset.category.property.model.AssetCategoryProperty;
 import com.liferay.asset.category.property.service.AssetCategoryPropertyLocalService;
 import com.liferay.asset.display.page.portlet.AssetDisplayPageEntryFormProcessor;
 import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
-import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
 import com.liferay.asset.kernel.NoSuchClassTypeException;
 import com.liferay.asset.kernel.exception.AssetCategoryLimitException;
 import com.liferay.asset.kernel.exception.AssetCategoryNameException;
@@ -37,18 +36,10 @@ import com.liferay.asset.kernel.exception.NoSuchVocabularyException;
 import com.liferay.asset.kernel.exception.VocabularyNameException;
 import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetCategoryConstants;
-import com.liferay.asset.kernel.model.AssetRendererFactory;
-import com.liferay.asset.kernel.model.AssetVocabulary;
-import com.liferay.asset.kernel.model.AssetVocabularyConstants;
-import com.liferay.asset.kernel.model.ClassTypeReader;
 import com.liferay.asset.kernel.service.AssetCategoryLocalService;
 import com.liferay.asset.kernel.service.AssetCategoryService;
 import com.liferay.asset.kernel.service.AssetVocabularyService;
-import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
-import com.liferay.portal.kernel.exception.NoSuchModelException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 import com.liferay.portal.kernel.security.auth.PrincipalException;
@@ -64,7 +55,6 @@ import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
-import com.liferay.portlet.asset.util.AssetVocabularySettingsHelper;
 
 import java.io.IOException;
 
@@ -230,48 +220,6 @@ public class AssetCategoryAdminPortlet extends MVCPortlet {
 			categoryProperties, serviceContext);
 	}
 
-	public void editVocabulary(
-			ActionRequest actionRequest, ActionResponse actionResponse)
-		throws Exception {
-
-		long vocabularyId = ParamUtil.getLong(actionRequest, "vocabularyId");
-
-		Map<Locale, String> titleMap = LocalizationUtil.getLocalizationMap(
-			actionRequest, "title");
-		Map<Locale, String> descriptionMap =
-			LocalizationUtil.getLocalizationMap(actionRequest, "description");
-
-		AssetVocabulary vocabulary = null;
-
-		ServiceContext serviceContext = ServiceContextFactory.getInstance(
-			AssetVocabulary.class.getName(), actionRequest);
-
-		if (vocabularyId <= 0) {
-
-			// Add vocabulary
-
-			int visibilityType = ParamUtil.getInteger(
-				actionRequest, "visibilityType",
-				AssetVocabularyConstants.VISIBILITY_TYPE_PUBLIC);
-
-			vocabulary = _assetVocabularyService.addVocabulary(
-				serviceContext.getScopeGroupId(), StringPool.BLANK, titleMap,
-				descriptionMap, _getSettings(actionRequest), visibilityType,
-				serviceContext);
-		}
-		else {
-
-			// Update vocabulary
-
-			vocabulary = _assetVocabularyService.updateVocabulary(
-				vocabularyId, StringPool.BLANK, titleMap, descriptionMap,
-				_getSettings(actionRequest), serviceContext);
-		}
-
-		actionRequest.setAttribute(
-			WebKeys.REDIRECT, _getRedirectURL(actionResponse, vocabulary));
-	}
-
 	public void moveCategory(
 			ActionRequest actionRequest, ActionResponse actionResponse)
 		throws Exception {
@@ -428,74 +376,6 @@ public class AssetCategoryAdminPortlet extends MVCPortlet {
 		}
 
 		return categoryPropertiesArray;
-	}
-
-	private String _getRedirectURL(
-		ActionResponse actionResponse, AssetVocabulary vocabulary) {
-
-		return PortletURLBuilder.createRenderURL(
-			_portal.getLiferayPortletResponse(actionResponse)
-		).setMVCPath(
-			"/view.jsp"
-		).setParameter(
-			"vocabularyId", vocabulary.getVocabularyId()
-		).buildString();
-	}
-
-	private String _getSettings(ActionRequest actionRequest) throws Exception {
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		int[] indexes = StringUtil.split(
-			ParamUtil.getString(actionRequest, "indexes"), 0);
-
-		long[] classNameIds = new long[indexes.length];
-		long[] classTypePKs = new long[indexes.length];
-		boolean[] requireds = new boolean[indexes.length];
-
-		for (int i = 0; i < indexes.length; i++) {
-			int index = indexes[i];
-
-			classNameIds[i] = ParamUtil.getLong(
-				actionRequest, "classNameId" + index);
-
-			classTypePKs[i] = ParamUtil.getLong(
-				actionRequest,
-				StringBundler.concat(
-					"subtype", classNameIds[i], "-classNameId", index),
-				AssetCategoryConstants.ALL_CLASS_TYPE_PK);
-
-			if (classTypePKs[i] != -1) {
-				AssetRendererFactory<?> assetRendererFactory =
-					AssetRendererFactoryRegistryUtil.
-						getAssetRendererFactoryByClassNameId(classNameIds[i]);
-
-				ClassTypeReader classTypeReader =
-					assetRendererFactory.getClassTypeReader();
-
-				try {
-					classTypeReader.getClassType(
-						classTypePKs[i], themeDisplay.getLocale());
-				}
-				catch (NoSuchModelException noSuchModelException) {
-					throw new NoSuchClassTypeException(noSuchModelException);
-				}
-			}
-
-			requireds[i] = ParamUtil.getBoolean(
-				actionRequest, "required" + index);
-		}
-
-		AssetVocabularySettingsHelper vocabularySettingsHelper =
-			new AssetVocabularySettingsHelper();
-
-		vocabularySettingsHelper.setClassNameIdsAndClassTypePKs(
-			classNameIds, classTypePKs, requireds);
-
-		vocabularySettingsHelper.setMultiValued(
-			ParamUtil.getBoolean(actionRequest, "multiValued"));
-
-		return vocabularySettingsHelper.toString();
 	}
 
 	private volatile AssetCategoriesAdminWebConfiguration
