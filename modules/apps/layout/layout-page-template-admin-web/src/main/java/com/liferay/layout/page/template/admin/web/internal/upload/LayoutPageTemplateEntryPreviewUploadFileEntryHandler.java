81ac23c566631236166714f34b3b093ffe09bcca
Alicia García <alicia.garcia@liferay.com>
2022-05-27 15:14:11 +0200

    LPS-154705 use new class on LayoutPageTemplateEntryPreviewUploadFileEntryHandler

--- modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/upload/LayoutPageTemplateEntryPreviewUploadFileEntryHandler.java
+++ modules/apps/layout/layout-page-template-admin-web/src/main/java/com/liferay/layout/page/template/admin/web/internal/upload/LayoutPageTemplateEntryPreviewUploadFileEntryHandler.java
@@ -17,22 +17,14 @@ package com.liferay.layout.page.template.admin.web.internal.upload;
 import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.TempFileEntryUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.ImageEditorUploadHandler;
 import com.liferay.upload.UniqueFileNameProvider;
-import com.liferay.upload.UploadFileEntryHandler;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -45,100 +37,39 @@ import org.osgi.service.component.annotations.Reference;
 	service = LayoutPageTemplateEntryPreviewUploadFileEntryHandler.class
 )
 public class LayoutPageTemplateEntryPreviewUploadFileEntryHandler
-	implements UploadFileEntryHandler {
+	extends ImageEditorUploadHandler {
 
 	@Override
-	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
-		throws IOException, PortalException {
+	protected void checkPermissions(UploadPortletRequest uploadPortletRequest)
+		throws PortalException {
+
+		long layoutPageTemplateEntryId = ParamUtil.getLong(
+			uploadPortletRequest, "layoutPageTemplateEntryId");
 
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)uploadPortletRequest.getAttribute(
 				WebKeys.THEME_DISPLAY);
 
-		long layoutPageTemplateEntryId = ParamUtil.getLong(
-			uploadPortletRequest, "layoutPageTemplateEntryId");
-
 		_layoutPageTemplateEntryModelResourcePermission.check(
 			themeDisplay.getPermissionChecker(), layoutPageTemplateEntryId,
 			ActionKeys.UPDATE);
-
-		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
-
-		if (Validator.isNotNull(fileName)) {
-			try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
-					_PARAMETER_NAME)) {
-
-				return _addTempFileEntry(
-					fileName, inputStream, _PARAMETER_NAME,
-					uploadPortletRequest, themeDisplay);
-			}
-		}
-
-		return _editImageFileEntry(uploadPortletRequest, themeDisplay);
 	}
 
-	private FileEntry _addTempFileEntry(
-			String fileName, InputStream inputStream, String parameterName,
-			UploadPortletRequest uploadPortletRequest,
-			ThemeDisplay themeDisplay)
-		throws PortalException {
-
-		String uniqueFileName = _uniqueFileNameProvider.provide(
-			fileName, curFileName -> _exists(themeDisplay, curFileName));
-
-		return TempFileEntryUtil.addTempFileEntry(
-			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-			_TEMP_FOLDER_NAME, uniqueFileName, inputStream,
-			uploadPortletRequest.getContentType(parameterName));
+	@Override
+	protected DLAppService getDLAppService() {
+		return _dlAppService;
 	}
 
-	private FileEntry _editImageFileEntry(
-			UploadPortletRequest uploadPortletRequest,
-			ThemeDisplay themeDisplay)
-		throws IOException, PortalException {
-
-		long fileEntryId = ParamUtil.getLong(
-			uploadPortletRequest, "fileEntryId");
-
-		FileEntry fileEntry = _dlAppService.getFileEntry(fileEntryId);
-
-		try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
-				"imageBlob")) {
-
-			return _addTempFileEntry(
-				fileEntry.getFileName(), inputStream, "imageBlob",
-				uploadPortletRequest, themeDisplay);
-		}
+	@Override
+	protected String getFolderName() {
+		return LayoutPageTemplateEntryPreviewUploadFileEntryHandler.class.
+			getName();
 	}
 
-	private boolean _exists(ThemeDisplay themeDisplay, String curFileName) {
-		try {
-			FileEntry tempFileEntry = TempFileEntryUtil.getTempFileEntry(
-				themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-				_TEMP_FOLDER_NAME, curFileName);
-
-			if (tempFileEntry != null) {
-				return true;
-			}
-
-			return false;
-		}
-		catch (PortalException portalException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(portalException);
-			}
-
-			return false;
-		}
+	@Override
+	protected UniqueFileNameProvider getUniqueFileNameProvider() {
+		return _uniqueFileNameProvider;
 	}
-
-	private static final String _PARAMETER_NAME = "imageSelectorFileName";
-
-	private static final String _TEMP_FOLDER_NAME =
-		LayoutPageTemplateEntryPreviewUploadFileEntryHandler.class.getName();
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		LayoutPageTemplateEntryPreviewUploadFileEntryHandler.class);
 
 	@Reference
 	private DLAppService _dlAppService;
