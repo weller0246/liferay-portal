0835b4f625dc33a630d481840f4bc9d5e15ec2bb
Alicia García <alicia.garcia@liferay.com>
2022-05-27 15:13:54 +0200

    LPS-154705 use new class on FragmentEntryPreviewUploadFileEntryHandler

--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/upload/FragmentEntryPreviewUploadFileEntryHandler.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/upload/FragmentEntryPreviewUploadFileEntryHandler.java
@@ -14,22 +14,16 @@
 
 package com.liferay.fragment.web.internal.upload;
 
+import com.liferay.document.library.kernel.service.DLAppService;
 import com.liferay.fragment.constants.FragmentActionKeys;
 import com.liferay.fragment.constants.FragmentConstants;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
-import com.liferay.portal.kernel.util.TempFileEntryUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.upload.ImageEditorUploadHandler;
 import com.liferay.upload.UniqueFileNameProvider;
-import com.liferay.upload.UploadFileEntryHandler;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -41,11 +35,11 @@ import org.osgi.service.component.annotations.Reference;
 	immediate = true, service = FragmentEntryPreviewUploadFileEntryHandler.class
 )
 public class FragmentEntryPreviewUploadFileEntryHandler
-	implements UploadFileEntryHandler {
+	extends ImageEditorUploadHandler {
 
 	@Override
-	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
-		throws IOException, PortalException {
+	protected void checkPermissions(UploadPortletRequest uploadPortletRequest)
+		throws PrincipalException {
 
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)uploadPortletRequest.getAttribute(
@@ -54,51 +48,25 @@ public class FragmentEntryPreviewUploadFileEntryHandler
 		_portletResourcePermission.check(
 			themeDisplay.getPermissionChecker(), themeDisplay.getScopeGroup(),
 			FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES);
-
-		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
-		String contentType = uploadPortletRequest.getContentType(
-			_PARAMETER_NAME);
-
-		try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
-				_PARAMETER_NAME)) {
-
-			String uniqueFileName = _uniqueFileNameProvider.provide(
-				fileName, curFileName -> _exists(themeDisplay, curFileName));
-
-			return TempFileEntryUtil.addTempFileEntry(
-				themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-				_TEMP_FOLDER_NAME, uniqueFileName, inputStream, contentType);
-		}
 	}
 
-	private boolean _exists(ThemeDisplay themeDisplay, String curFileName) {
-		try {
-			FileEntry tempFileEntry = TempFileEntryUtil.getTempFileEntry(
-				themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-				_TEMP_FOLDER_NAME, curFileName);
-
-			if (tempFileEntry != null) {
-				return true;
-			}
-
-			return false;
-		}
-		catch (PortalException portalException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(portalException);
-			}
-
-			return false;
-		}
+	@Override
+	protected DLAppService getDLAppService() {
+		return _dlAppService;
 	}
 
-	private static final String _PARAMETER_NAME = "imageSelectorFileName";
+	@Override
+	protected String getFolderName() {
+		return FragmentEntryPreviewUploadFileEntryHandler.class.getName();
+	}
 
-	private static final String _TEMP_FOLDER_NAME =
-		FragmentEntryPreviewUploadFileEntryHandler.class.getName();
+	@Override
+	protected UniqueFileNameProvider getUniqueFileNameProvider() {
+		return _uniqueFileNameProvider;
+	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		FragmentEntryPreviewUploadFileEntryHandler.class);
+	@Reference
+	private DLAppService _dlAppService;
 
 	@Reference(
 		target = "(resource.name=" + FragmentConstants.RESOURCE_NAME + ")"
