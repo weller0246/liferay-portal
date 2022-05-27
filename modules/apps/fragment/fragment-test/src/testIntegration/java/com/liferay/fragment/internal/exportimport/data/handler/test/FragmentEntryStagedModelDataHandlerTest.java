7a7c57483130cabe4779a4995614a8f676e6cc89
Jonathan McCann <jonathan.mccann@liferay.com>
2022-05-27 08:58:09 -0700

    LPS-154882 Add test

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/exportimport/data/handler/test/FragmentEntryStagedModelDataHandlerTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/internal/exportimport/data/handler/test/FragmentEntryStagedModelDataHandlerTest.java
@@ -15,9 +15,11 @@
 package com.liferay.fragment.internal.exportimport.data.handler.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.document.library.kernel.exception.NoSuchFileEntryException;
 import com.liferay.exportimport.kernel.lar.ExportImportThreadLocal;
 import com.liferay.exportimport.test.util.lar.BaseStagedModelDataHandlerTestCase;
 import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.constants.FragmentPortletKeys;
 import com.liferay.fragment.model.FragmentCollection;
 import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.model.FragmentEntryLink;
@@ -25,16 +27,21 @@ import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalService;
 import com.liferay.fragment.util.FragmentTestUtil;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.model.StagedModel;
+import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.ContentTypes;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
@@ -69,6 +76,98 @@ public class FragmentEntryStagedModelDataHandlerTest
 		super.setUp();
 
 		_layout = LayoutTestUtil.addTypeContentLayout(stagingGroup);
+	}
+
+	@Test
+	public void testDeletePreviewFileEntryWithStagingEnabled()
+		throws Exception {
+
+		Map<String, List<StagedModel>> dependentStagedModelsMap =
+			addDependentStagedModelsMap(stagingGroup);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				stagingGroup.getGroupId(), TestPropsValues.getUserId());
+
+		StagedModel stagedModel = addStagedModel(
+			stagingGroup, dependentStagedModelsMap);
+
+		FragmentEntry fragmentEntry = (FragmentEntry)stagedModel;
+
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		Repository repository = PortletFileRepositoryUtil.addPortletRepository(
+			stagingGroup.getGroupId(), FragmentPortletKeys.FRAGMENT,
+			serviceContext);
+
+		FileEntry fileEntry = PortletFileRepositoryUtil.addPortletFileEntry(
+			stagingGroup.getGroupId(), TestPropsValues.getUserId(),
+			FragmentEntry.class.getName(), fragmentEntry.getFragmentEntryId(),
+			FragmentPortletKeys.FRAGMENT, repository.getDlFolderId(),
+			classLoader.getResourceAsStream(
+				"com/liferay/fragment/dependencies/liferay.png"),
+			RandomTestUtil.randomString(), ContentTypes.IMAGE_PNG, false);
+
+		stagedModel = _fragmentEntryLocalService.updateFragmentEntry(
+			fragmentEntry.getFragmentEntryId(), fileEntry.getFileEntryId());
+
+		try {
+			exportImportStagedModel(stagedModel);
+		}
+		finally {
+			ExportImportThreadLocal.setPortletImportInProcess(false);
+		}
+
+		StagedModel importedStagedModel = getStagedModel(
+			stagedModel.getUuid(), liveGroup);
+
+		FragmentEntry importedFragmentEntry =
+			(FragmentEntry)importedStagedModel;
+
+		long importedPreviewFileEntryId =
+			importedFragmentEntry.getPreviewFileEntryId();
+
+		fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
+			importedPreviewFileEntryId);
+
+		Assert.assertNotNull(fileEntry);
+
+		PortletFileRepositoryUtil.deletePortletFileEntry(
+			fileEntry.getFileEntryId());
+
+		stagedModel = _fragmentEntryLocalService.updateFragmentEntry(
+			fragmentEntry.getFragmentEntryId(), 0);
+
+		try {
+			exportImportStagedModel(stagedModel);
+		}
+		finally {
+			ExportImportThreadLocal.setPortletImportInProcess(false);
+		}
+
+		importedStagedModel = getStagedModel(stagedModel.getUuid(), liveGroup);
+
+		importedFragmentEntry = (FragmentEntry)importedStagedModel;
+
+		Assert.assertEquals(0, importedFragmentEntry.getPreviewFileEntryId());
+
+		fileEntry = null;
+
+		try {
+			fileEntry = PortletFileRepositoryUtil.getPortletFileEntry(
+				importedPreviewFileEntryId);
+		}
+		catch (NoSuchFileEntryException noSuchFileEntryException) {
+			Assert.assertEquals(
+				StringBundler.concat(
+					"No FileEntry exists with the key {fileEntryId=",
+					importedPreviewFileEntryId, "}"),
+				noSuchFileEntryException.getMessage());
+		}
+
+		Assert.assertNull(fileEntry);
 	}
 
 	@Test
