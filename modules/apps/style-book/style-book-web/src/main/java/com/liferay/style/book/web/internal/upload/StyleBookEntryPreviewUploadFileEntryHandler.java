ba2751428cb752d02bba2f298c487e2f1e5ad352
Alicia García <alicia.garcia@liferay.com>
2022-05-27 15:14:25 +0200

    LPS-154705 use new class on StyleBookEntryPreviewUploadFileEntryHandler

--- modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/upload/StyleBookEntryPreviewUploadFileEntryHandler.java
+++ modules/apps/style-book/style-book-web/src/main/java/com/liferay/style/book/web/internal/upload/StyleBookEntryPreviewUploadFileEntryHandler.java
@@ -15,24 +15,15 @@
 package com.liferay.style.book.web.internal.upload;
 
 import com.liferay.document.library.kernel.service.DLAppService;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.upload.UploadPortletRequest;
-import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.TempFileEntryUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.style.book.constants.StyleBookActionKeys;
 import com.liferay.style.book.constants.StyleBookConstants;
+import com.liferay.upload.ImageEditorUploadHandler;
 import com.liferay.upload.UniqueFileNameProvider;
-import com.liferay.upload.UploadFileEntryHandler;
-
-import java.io.IOException;
-import java.io.InputStream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -45,11 +36,11 @@ import org.osgi.service.component.annotations.Reference;
 	service = StyleBookEntryPreviewUploadFileEntryHandler.class
 )
 public class StyleBookEntryPreviewUploadFileEntryHandler
-	implements UploadFileEntryHandler {
+	extends ImageEditorUploadHandler {
 
 	@Override
-	public FileEntry upload(UploadPortletRequest uploadPortletRequest)
-		throws IOException, PortalException {
+	protected void checkPermissions(UploadPortletRequest uploadPortletRequest)
+		throws PrincipalException {
 
 		ThemeDisplay themeDisplay =
 			(ThemeDisplay)uploadPortletRequest.getAttribute(
@@ -58,83 +49,22 @@ public class StyleBookEntryPreviewUploadFileEntryHandler
 		_portletResourcePermission.check(
 			themeDisplay.getPermissionChecker(), themeDisplay.getScopeGroup(),
 			StyleBookActionKeys.MANAGE_STYLE_BOOK_ENTRIES);
-
-		String fileName = uploadPortletRequest.getFileName(_PARAMETER_NAME);
-
-		if (Validator.isNotNull(fileName)) {
-			try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
-					_PARAMETER_NAME)) {
-
-				return _addTempFileEntry(
-					fileName, inputStream, _PARAMETER_NAME,
-					uploadPortletRequest, themeDisplay);
-			}
-		}
-
-		return _editImageFileEntry(uploadPortletRequest, themeDisplay);
 	}
 
-	private FileEntry _addTempFileEntry(
-		String fileName, InputStream inputStream, String parameterName,
-		UploadPortletRequest uploadPortletRequest,
-		ThemeDisplay themeDisplay)
-		throws PortalException {
-
-		String uniqueFileName = _uniqueFileNameProvider.provide(
-			fileName, curFileName -> _exists(themeDisplay, curFileName));
-
-		return TempFileEntryUtil.addTempFileEntry(
-			themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-			_TEMP_FOLDER_NAME, uniqueFileName, inputStream,
-			uploadPortletRequest.getContentType(parameterName));
+	@Override
+	protected DLAppService getDLAppService() {
+		return _dlAppService;
 	}
 
-	private FileEntry _editImageFileEntry(
-			UploadPortletRequest uploadPortletRequest,
-			ThemeDisplay themeDisplay)
-		throws IOException, PortalException {
-
-		long fileEntryId = ParamUtil.getLong(
-			uploadPortletRequest, "fileEntryId");
-
-		FileEntry fileEntry = _dlAppService.getFileEntry(fileEntryId);
-
-		try (InputStream inputStream = uploadPortletRequest.getFileAsStream(
-				"imageBlob")) {
-			return _addTempFileEntry(
-				fileEntry.getFileName(), inputStream, "imageBlob",
-				uploadPortletRequest, themeDisplay);
-		}
+	@Override
+	protected String getFolderName() {
+		return StyleBookEntryPreviewUploadFileEntryHandler.class.getName();
 	}
 
-	private boolean _exists(ThemeDisplay themeDisplay, String curFileName) {
-		try {
-			FileEntry tempFileEntry = TempFileEntryUtil.getTempFileEntry(
-				themeDisplay.getScopeGroupId(), themeDisplay.getUserId(),
-				_TEMP_FOLDER_NAME, curFileName);
-
-			if (tempFileEntry != null) {
-				return true;
-			}
-
-			return false;
-		}
-		catch (PortalException portalException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(portalException);
-			}
-
-			return false;
-		}
+	@Override
+	protected UniqueFileNameProvider getUniqueFileNameProvider() {
+		return _uniqueFileNameProvider;
 	}
-
-	private static final String _PARAMETER_NAME = "imageSelectorFileName";
-
-	private static final String _TEMP_FOLDER_NAME =
-		StyleBookEntryPreviewUploadFileEntryHandler.class.getName();
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		StyleBookEntryPreviewUploadFileEntryHandler.class);
 
 	@Reference
 	private DLAppService _dlAppService;
