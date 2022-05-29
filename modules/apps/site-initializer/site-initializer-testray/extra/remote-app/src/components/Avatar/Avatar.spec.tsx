7a1cf98613f1032633f1816f1e541688553434d4
Keven Leone <keven.leone@liferay.com>
2022-05-29 20:56:35 -0300

    LPS-152783 Write unit test

---
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/components/Avatar/Avatar.spec.tsx
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {render} from '@testing-library/react';
+
+import {Avatar} from './Avatar';
+
+describe('Avatar', () => {
+	it('renders with success', () => {
+		const {asFragment} = render(<Avatar />);
+
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('renders with name prop and test initials', () => {
+		const {queryByText, rerender} = render(<Avatar name="Jane Doe" />);
+
+		expect(queryByText('JD')).toBeTruthy();
+		expect(queryByText('Jane Doe')).toBeFalsy();
+
+		rerender(<Avatar displayName name="Liferay User" />);
+
+		expect(queryByText('LU')).toBeTruthy();
+		expect(queryByText('Liferay User')).toBeTruthy();
+	});
+
+	it('renders with name prop and test initials', () => {
+		const {container} = render(
+			<Avatar name="Jane Doe" url="https://liferay.com/jane-doe.png" />
+		);
+
+		expect(container.querySelector('img')?.alt).toBe('Jane Doe');
+		expect(container.querySelector('img')?.src).toBe(
+			'https://liferay.com/jane-doe.png'
+		);
+	});
+});
---
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/components/Form/Footer/Footer.spec.tsx
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {fireEvent, render} from '@testing-library/react';
+
+import Footer from './Footer';
+
+describe('Footer', () => {
+	it('renders with success', () => {
+		const {asFragment, queryByText} = render(
+			<Footer onClose={() => null} onSubmit={() => null} />
+		);
+
+		expect(queryByText('Cancel')).toBeTruthy();
+		expect(queryByText('Save')).toBeTruthy();
+		expect(asFragment()).toMatchSnapshot();
+	});
+
+	it('renders and perform actions', () => {
+		const cancelFn = jest.fn();
+		const submitFn = jest.fn();
+
+		const {queryByText} = render(
+			<Footer onClose={cancelFn} onSubmit={submitFn} />
+		);
+
+		expect(cancelFn).toHaveBeenCalledTimes(0);
+		expect(submitFn).toHaveBeenCalledTimes(0);
+
+		const cancel = queryByText('Cancel');
+		const submit = queryByText('Save');
+
+		expect(cancel).toBeTruthy();
+
+		fireEvent.click(cancel as HTMLElement);
+		fireEvent.click(submit as HTMLElement);
+
+		expect(cancelFn).toHaveBeenCalledTimes(1);
+		expect(submitFn).toHaveBeenCalledTimes(1);
+	});
+});
