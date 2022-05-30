903460b04ed4f74ce2ddecbfaab22b43ee506f9b
hai.yu <hai.yu@liferay.com>
2022-05-30 17:35:31 +0800

    LPS-153797 Avoid running _hasValidAssetVocabularies() more times in the same request.

--- modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLViewEntriesDisplayContext.java
+++ modules/apps/document-library/document-library-web/src/main/java/com/liferay/document/library/web/internal/display/context/DLViewEntriesDisplayContext.java
@@ -346,6 +346,10 @@ public class DLViewEntriesDisplayContext {
 	private boolean _hasValidAssetVocabularies(long scopeGroupId)
 		throws PortalException {
 
+		if (_hasValidAssetVocabularies != null) {
+			return _hasValidAssetVocabularies;
+		}
+
 		List<AssetVocabulary> assetVocabularies =
 			AssetVocabularyServiceUtil.getGroupVocabularies(
 				PortalUtil.getCurrentAndAncestorSiteGroupIds(scopeGroupId));
@@ -363,11 +367,15 @@ public class DLViewEntriesDisplayContext {
 				assetVocabulary.getVocabularyId());
 
 			if (count > 0) {
-				return true;
+				_hasValidAssetVocabularies = true;
+
+				return _hasValidAssetVocabularies;
 			}
 		}
 
-		return false;
+		_hasValidAssetVocabularies = false;
+
+		return _hasValidAssetVocabularies;
 	}
 
 	private boolean _hasWorkflowDefinitionLink(FileEntry fileEntry) {
@@ -407,6 +415,7 @@ public class DLViewEntriesDisplayContext {
 		_dlPortletInstanceSettingsHelper;
 	private final DLRequestHelper _dlRequestHelper;
 	private final DLTrashHelper _dlTrashHelper;
+	private Boolean _hasValidAssetVocabularies;
 	private final HttpServletRequest _httpServletRequest;
 	private final LiferayPortletRequest _liferayPortletRequest;
 	private final LiferayPortletResponse _liferayPortletResponse;
