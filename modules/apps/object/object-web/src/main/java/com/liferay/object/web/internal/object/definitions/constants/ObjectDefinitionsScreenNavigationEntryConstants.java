6f9007171f718e864d9b0cdcdef659994dfc6379
Carlos Montenegro <carlos.montenegro@liferay.com>
2022-05-30 09:06:46 -0300

    LPS-154881 Create CATEGORY_KEY_STATES and ENTRY_KEY_STATES

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/constants/ObjectDefinitionsScreenNavigationEntryConstants.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/definitions/constants/ObjectDefinitionsScreenNavigationEntryConstants.java
@@ -32,6 +32,8 @@ public class ObjectDefinitionsScreenNavigationEntryConstants {
 
 	public static final String CATEGORY_KEY_RELATIONSHIPS = "relationships";
 
+	public static final String CATEGORY_KEY_STATE_MANAGER = "state-manager";
+
 	public static final String CATEGORY_KEY_VALIDATIONS = "validations";
 
 	public static final String CATEGORY_KEY_VIEWS = "views";
@@ -47,6 +49,8 @@ public class ObjectDefinitionsScreenNavigationEntryConstants {
 	public static final String ENTRY_KEY_OBJECTS = "objects";
 
 	public static final String ENTRY_KEY_RELATIONSHIPS = "relationships";
+
+	public static final String ENTRY_KEY_STATE_MANAGER = "state-manager";
 
 	public static final String ENTRY_KEY_VALIDATIONS = "validations";
 
