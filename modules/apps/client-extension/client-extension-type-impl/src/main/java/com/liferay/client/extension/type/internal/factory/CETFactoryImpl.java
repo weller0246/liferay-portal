63cb2fe5ba62391abd938b9e930814e5fc199583
Brian Chan <brian.chan@liferay.com>
2022-05-31 09:33:37 +0200

    LPS-155023 Stub out client-extension-type-api and client-extension-type-impl

---
+++ modules/apps/client-extension/client-extension-type-impl/src/main/java/com/liferay/client/extension/type/internal/factory/CETFactoryImpl.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.client.extension.type.internal.factory;
+
+import com.liferay.client.extension.model.ClientExtensionEntry;
+import com.liferay.client.extension.type.CETCustomElement;
+import com.liferay.client.extension.type.CETIFrame;
+import com.liferay.client.extension.type.factory.CETFactory;
+import com.liferay.client.extension.type.internal.CETCustomElementImpl;
+import com.liferay.client.extension.type.internal.CETIFrameImpl;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Brian Wing Shun Chan
+ */
+@Component(immediate = true, service = CETFactory.class)
+public class CETFactoryImpl implements CETFactory {
+
+	public CETCustomElement customElement(
+		ClientExtensionEntry clientExtensionEntry) {
+
+		return new CETCustomElementImpl();
+	}
+
+	public CETIFrame iFrame(ClientExtensionEntry clientExtensionEntry) {
+		return new CETIFrameImpl();
+	}
+
+}
