68d82a06d878018c32bc075c09f640624656ab16
Victor Galan <victor.galan@liferay.com>
2022-05-31 17:39:06 +0200

    LPS-155086 Add jsp to render theme css extension selector

---
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/js/ThemeCSSReplacementSelector.js
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ClayButtonWithIcon} from '@clayui/button';
+import ClayForm, {ClayInput} from '@clayui/form';
+import React, {useState} from 'react';
+
+export default function ThemeCSSReplacementSelector({
+	portletNamespace,
+	themeCSSExtensionName,
+}) {
+	const [extensionName, setExtensionName] = useState(themeCSSExtensionName);
+	const [extensionId, setExtensionId] = useState(themeCSSExtensionName);
+
+	const onClick = () => {
+		setExtensionName('test');
+		setExtensionName('1234');
+	};
+
+	return (
+		<>
+			<ClayInput
+				name="themeCSSReplacementExtensionId"
+				type="hidden"
+				value={extensionId}
+			/>
+			<ClayForm.Group>
+				<label
+					htmlFor={`${portletNamespace}themeCSSReplacementExtension`}
+				>
+					{Liferay.Language.get('extension')}
+				</label>
+
+				<ClayInput.Group className="w-50" small>
+					<ClayInput.GroupItem>
+						<ClayInput
+							id={`${portletNamespace}themeCSSReplacementExtension`}
+							onClick={onClick}
+							placeholder={Liferay.Language.get(
+								'no-theme-css-extension-loaded'
+							)}
+							readOnly
+							type="text"
+							value={extensionName}
+						/>
+					</ClayInput.GroupItem>
+
+					<ClayInput.GroupItem shrink>
+						{extensionName ? (
+							<>
+								<ClayButtonWithIcon
+									className="mr-2"
+									displayType="secondary"
+									onClick={onClick}
+									small
+									symbol="change"
+								/>
+
+								<ClayButtonWithIcon
+									displayType="secondary"
+									onClick={() => {
+										setExtensionName('');
+										setExtensionId('');
+									}}
+									small
+									symbol="trash"
+								/>
+							</>
+						) : (
+							<ClayButtonWithIcon
+								displayType="secondary"
+								onClick={onClick}
+								small
+								symbol="plus"
+							/>
+						)}
+					</ClayInput.GroupItem>
+				</ClayInput.Group>
+			</ClayForm.Group>
+		</>
+	);
+}
---
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/look_and_feel_theme_css.jsp
@@ -0,0 +1,36 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<clay:sheet-section>
+	<h3 class="sheet-subtitle"><liferay-ui:message key="theme-css-replacement" /></h3>
+
+	<clay:alert
+		displayType="info"
+		message='<%= LanguageUtil.get(request, "by-replacing-the-css-completely-you-will-be-overriding-all-the-declared-design-tokens-and-css-variables") %>'
+	/>
+
+	<p>
+		<liferay-ui:message key="use-this-client-extension-to-fully-replace-the-default-css-contained-in-the-theme" />
+	</p>
+
+	<div>
+		<react:component
+			module="js/ThemeCSSReplacementSelector"
+		/>
+	</div>
+</clay:sheet-section>
