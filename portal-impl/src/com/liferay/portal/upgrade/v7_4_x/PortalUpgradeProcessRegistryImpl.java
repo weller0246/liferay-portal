0eaad456b51e696aa7852705f39e2426928acd33
Samuel Ziemer <Samuel.Ziemer@liferay.com>
2022-05-31 09:36:41 -0700

    LPS-152963 Add upgrade process

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
@@ -95,6 +95,8 @@ public class PortalUpgradeProcessRegistryImpl
 
 		upgradeProcesses.put(
 			new Version(14, 0, 1), new UpgradeFaviconFileEntryIdColumn());
+
+		upgradeProcesses.put(new Version(14, 0, 2), new UpgradeRole());
 	}
 
 }
