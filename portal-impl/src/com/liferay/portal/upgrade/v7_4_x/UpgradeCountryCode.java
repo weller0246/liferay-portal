d8cc57418c82cc3a0273718938d0f4b263f9bd8e
pei-jung <pei-jung.lan@liferay.com>
2022-05-31 18:24:31 -0700

    LPS-154702 Upgrade regions

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeCountryCode.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/UpgradeCountryCode.java
@@ -38,6 +38,12 @@ public class UpgradeCountryCode extends UpgradeProcess {
 				"'democratic-republic-of-congo' where a2 = 'CD'");
 		runSQL("update Country set name = 'republic-of-congo' where a2 = 'CG'");
 
+		_upgradeCountry();
+		_upgradeRegion("ES", "Navarra", "NA");
+		_upgradeRegion("IT", "Napoli", "NA");
+	}
+
+	private void _upgradeCountry() throws Exception {
 		try (PreparedStatement preparedStatement1 =
 				AutoBatchPreparedStatementUtil.autoBatch(
 					connection.prepareStatement(
@@ -86,6 +92,60 @@ public class UpgradeCountryCode extends UpgradeProcess {
 				preparedStatement1.setBoolean(17, true);
 				preparedStatement1.setBoolean(18, false);
 				preparedStatement1.setBoolean(19, false);
+
+				preparedStatement1.addBatch();
+			}
+
+			preparedStatement1.executeBatch();
+		}
+	}
+
+	private void _upgradeRegion(
+			String countryA2, String regionName, String regionCode)
+		throws Exception {
+
+		try (PreparedStatement preparedStatement1 =
+				AutoBatchPreparedStatementUtil.autoBatch(
+					connection.prepareStatement(
+						StringBundler.concat(
+							"insert into Region (mvccVersion, uuid_, ",
+							"defaultLanguageId, regionId, companyId, userId, ",
+							"createDate, modifiedDate, countryId, active_, ",
+							"name, position, regionCode) values (0, ?, ?, ?, ",
+							"?, ?, ?, ?, ?, ?, ?, ?, ?)")));
+			PreparedStatement preparedStatement2 = connection.prepareStatement(
+				SQLTransformer.transform(
+					StringBundler.concat(
+						"select Country.countryId, User_.companyId, ",
+						"User_.languageId, User_.userId from User_ join ",
+						"Country on User_.companyId = Country.companyId where ",
+						"User_.defaultUser = [$TRUE$] and Country.a2 = '",
+						countryA2, "' and Country.countryId not in (select ",
+						"Country.countryId from Country join Region on ",
+						"Country.countryId = Region.countryId where ",
+						"Country.a2 = '", countryA2, "' and Region.regionCode ",
+						"= '", regionCode, "')")));
+			ResultSet resultSet = preparedStatement2.executeQuery()) {
+
+			if (resultSet.next()) {
+				Timestamp now = new Timestamp(System.currentTimeMillis());
+
+				preparedStatement1.setString(1, PortalUUIDUtil.generate());
+				preparedStatement1.setString(
+					2, resultSet.getString("User_.languageId"));
+				preparedStatement1.setLong(3, increment());
+				preparedStatement1.setLong(
+					4, resultSet.getLong("User_.companyId"));
+				preparedStatement1.setLong(
+					5, resultSet.getLong("User_.userId"));
+				preparedStatement1.setTimestamp(6, now);
+				preparedStatement1.setTimestamp(7, now);
+				preparedStatement1.setLong(
+					8, resultSet.getLong("Country.countryId"));
+				preparedStatement1.setBoolean(9, true);
+				preparedStatement1.setString(10, regionName);
+				preparedStatement1.setDouble(11, 0.0);
+				preparedStatement1.setString(12, regionCode);
 
 				preparedStatement1.addBatch();
 			}
