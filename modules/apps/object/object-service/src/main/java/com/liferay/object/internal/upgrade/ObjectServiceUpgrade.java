804df4e0192c19c4f6f7e49eec8b937187e72763
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-06-01 13:55:38 -0300

    LPS-154792 Add new upgrade step

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/ObjectServiceUpgrade.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/upgrade/ObjectServiceUpgrade.java
@@ -106,6 +106,11 @@ public class ObjectServiceUpgrade implements UpgradeStepRegistrator {
 			"3.9.0", "3.10.0",
 			new com.liferay.object.internal.upgrade.v3_10_0.
 				ObjectDefinitionUpgradeProcess());
+
+		registry.register(
+			"3.10.0", "3.11.0",
+			new com.liferay.object.internal.upgrade.v3_11_0.
+				ObjectActionUpgradeProcess());
 	}
 
 }
