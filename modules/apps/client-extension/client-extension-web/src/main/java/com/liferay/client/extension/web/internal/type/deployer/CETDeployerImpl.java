f5c5859488bb52c59584b7251c36c32ec2f49eae
Brian Chan <brian.chan@liferay.com>
2022-06-02 15:51:44 +0200

    LPS-155255 Use PortalUtil

--- modules/apps/client-extension/client-extension-web/src/main/java/com/liferay/client/extension/web/internal/type/deployer/CETDeployerImpl.java
+++ modules/apps/client-extension/client-extension-web/src/main/java/com/liferay/client/extension/web/internal/type/deployer/CETDeployerImpl.java
@@ -31,6 +31,7 @@ import com.liferay.portal.kernel.portlet.ConfigurationAction;
 import com.liferay.portal.kernel.portlet.FriendlyURLMapper;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.util.ArrayList;
@@ -117,11 +118,9 @@ public class CETDeployerImpl implements CETDeployer {
 	}
 
 	private String _getPortletId(CET cet) {
-		String primaryKey = cet.getPrimaryKey();
-
 		return "com_liferay_client_extension_web_internal_portlet_" +
 			"ClientExtensionEntryPortlet_" +
-			   primaryKey.replaceAll("[^a-zA-Z0-9_]", StringPool.UNDERLINE);
+				PortalUtil.getJsSafePortletId(cet.getPrimaryKey());
 	}
 
 	private ServiceRegistration<ConfigurationAction>
