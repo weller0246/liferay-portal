d0f1de5a4c5e407d629f25744b15844a5340c228
Kevin Tan <kevin.tan@liferay.com>
2022-06-02 14:16:03 -0700

    LPS-150870 portal-search-web: Add toggle between classic and new layout

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SortConfigurationOptions.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SortConfigurationOptions.js
@@ -12,12 +12,14 @@
  * details.
  */
 
+import ClayAlert from '@clayui/alert';
+import ClayButton from '@clayui/button';
 import ClayForm, {ClayInput, ClaySelect, ClayToggle} from '@clayui/form';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 import FieldList from './FieldList';
 
-export const ORDERS = {
+const ORDERS = {
 	ASC: {
 		label: Liferay.Language.get('ascending'),
 		value: 'ascending',
@@ -25,6 +27,17 @@ export const ORDERS = {
 	DESC: {
 		label: Liferay.Language.get('descending'),
 		value: 'descending',
+	},
+};
+
+const VIEWS = {
+	CLASSIC: {
+		label: Liferay.Language.get('classic'),
+		value: 'classic',
+	},
+	NEW: {
+		label: Liferay.Language.get('new'),
+		value: 'new',
 	},
 };
 
@@ -51,6 +64,27 @@ const getOrderFromFieldName = (fieldName) => {
 };
 
 /**
+ * Gets the relevance field object.
+ * @param {Array} fieldsJSONArray
+ * @returns {object}
+ */
+const getRelevanceFieldObject = (fieldsJSONArray) => {
+	let relevanceField = null;
+
+	fieldsJSONArray.some((item) => {
+		if (item.field === '') {
+			relevanceField = item;
+
+			return true;
+		}
+
+		return false;
+	});
+
+	return relevanceField;
+};
+
+/**
  * Cleans up the fields array by removing those that have empty indexed
  * field names.
  * @param {Array} fields The list of fields.
@@ -69,6 +103,27 @@ const removeOrderFromFieldName = (fieldName) => {
 	return fieldName.slice(-1) === '+' || fieldName.slice(-1) === '-'
 		? fieldName.slice(0, -1)
 		: fieldName;
+};
+
+/**
+ * Transforms the preference key fields format into a format easier to work with
+ * in the component. Adds an `id` and `order` property.
+ * @param {Array} fieldsJSONArray
+ * @returns {Array}
+ */
+const transformFieldsJSONArrayToFieldsArray = (fieldsJSONArray) => {
+	const fieldsJSONArrayWithRemovedRelevance = fieldsJSONArray.filter(
+		({field}) => field !== ''
+	);
+
+	return fieldsJSONArrayWithRemovedRelevance.map(
+		({field: fieldName, label}, index) => ({
+			field: removeOrderFromFieldName(fieldName),
+			id: index, // For FieldList item `key` when reordering.
+			label,
+			order: getOrderFromFieldName(fieldName),
+		})
+	);
 };
 
 function Inputs({onChange, value}) {
@@ -138,88 +193,248 @@ function SortConfigurationOptions({
 	],
 	namespace = '',
 }) {
-	const hasRelevance = fieldsJSONArray[0].field === '';
+	const relevanceField = getRelevanceFieldObject(fieldsJSONArray);
 
-	const [relevanceLabel, setRelevanceLabel] = useState(
-		hasRelevance ? fieldsJSONArray[0].label : 'relevance'
-	);
-	const [relevanceOn, setRelevanceOn] = useState(hasRelevance);
-
+	const [classicFields, setClassicFields] = useState(fieldsJSONArray);
 	const [fields, setFields] = useState(
-		(hasRelevance ? fieldsJSONArray.slice(1) : fieldsJSONArray).map(
-			({field: fieldName, label}, index) => ({
-				field: removeOrderFromFieldName(fieldName),
-				id: index, // For FieldList item `key` when reordering.
-				label,
-				order: getOrderFromFieldName(fieldName),
-			})
-		)
+		transformFieldsJSONArrayToFieldsArray(fieldsJSONArray)
 	);
+	const [relevanceLabel, setRelevanceLabel] = useState(
+		relevanceField ? relevanceField.label : 'relevance'
+	);
+	const [relevanceOn, setRelevanceOn] = useState(!!relevanceField);
+	const [showAlert, setShowAlert] = useState(true);
+	const [view, setView] = useState(VIEWS.NEW);
+
+	useEffect(() => {
+		if (view.value !== VIEWS.CLASSIC.value) {
+			return;
+		}
+
+		AUI().use('liferay-auto-fields', () => {
+			new Liferay.AutoFields({
+				contentBox: `#${namespace}fieldsId`,
+				namespace: `${namespace}`,
+			}).render();
+		});
+	}, [namespace, view]);
+
+	const _handleChangeClassicValue = (index, property) => (event) => {
+		const newClassicFields = [...classicFields];
+
+		newClassicFields[index][property] = event.target.value;
+
+		setClassicFields(newClassicFields);
+	};
+
+	const _handleCloseAlert = () => {
+		setShowAlert(false);
+	};
+
+	const _handleSwitchView = () => {
+		if (view.value === VIEWS.NEW.value) {
+			setClassicFields(
+				relevanceOn
+					? [
+							{field: '', label: relevanceLabel},
+							...addOrdersToFieldNames(removeEmptyFields(fields)),
+					  ]
+					: addOrdersToFieldNames(removeEmptyFields(fields))
+			);
+
+			setView(VIEWS.CLASSIC);
+		}
+		else {
+			const newFields = [];
+
+			const autoFieldFormRows = [
+				...document.getElementsByClassName('field-form-row'),
+			].filter((element) => {
+				return !element.hidden;
+			});
+
+			autoFieldFormRows.forEach((item) => {
+				const field = item.getElementsByClassName('sort-field-input')[0]
+					.value;
+				const label = item.getElementsByClassName('label-input')[0]
+					.value;
+
+				newFields.push({
+					field,
+					label,
+				});
+			});
+
+			setFields(transformFieldsJSONArrayToFieldsArray(newFields));
+
+			const classicRelevanceField = getRelevanceFieldObject(newFields);
+
+			setRelevanceLabel(
+				classicRelevanceField
+					? classicRelevanceField.label
+					: 'relevance'
+			);
+			setRelevanceOn(!!classicRelevanceField);
+
+			setView(VIEWS.NEW);
+		}
+	};
 
 	return (
 		<div className="sort-configurations-options">
-			<input
-				name={`${namespace}${fieldsInputName}`}
-				type="hidden"
-				value={JSON.stringify(
-					relevanceOn
-						? [
-								{field: '', label: relevanceLabel},
-								...addOrdersToFieldNames(
-									removeEmptyFields(fields)
-								),
-						  ]
-						: addOrdersToFieldNames(removeEmptyFields(fields))
-				)}
-			/>
+			<div className="view-switcher">
+				<ClayButton
+					borderless
+					displayType="secondary"
+					onClick={_handleSwitchView}
+					small
+				>
+					{Liferay.Util.sub(
+						Liferay.Language.get('switch-to-x-view'),
+						[
+							view.value === VIEWS.NEW.value
+								? VIEWS.CLASSIC.label
+								: VIEWS.NEW.label,
+						]
+					)}
+				</ClayButton>
+			</div>
 
-			<ClayForm.Group className="field-item relevance">
-				<ClayInput.Group>
-					<ClayInput.GroupItem>
-						<label htmlFor="relevance">
-							{Liferay.Language.get('display-label')}
-						</label>
+			{view.value === VIEWS.NEW.value && (
+				<>
+					<ClayForm.Group className="field-item relevance">
+						<ClayInput.Group>
+							<ClayInput.GroupItem>
+								<label htmlFor="relevance">
+									{Liferay.Language.get('display-label')}
+								</label>
 
-						<ClayInput
-							id="relevance"
-							onChange={(event) =>
-								setRelevanceLabel(event.target.value)
-							}
-							type="text"
-							value={relevanceLabel}
-						/>
+								<ClayInput
+									id="relevance"
+									onChange={(event) =>
+										setRelevanceLabel(event.target.value)
+									}
+									type="text"
+									value={relevanceLabel}
+								/>
 
-						<div className="text-secondary">
-							{Liferay.Language.get(
-								'relevance-can-be-turned-on-or-off-but-not-removed'
-							)}
+								<div className="text-secondary">
+									{Liferay.Language.get(
+										'relevance-can-be-turned-on-or-off-but-not-removed'
+									)}
+								</div>
+							</ClayInput.GroupItem>
+
+							<ClayInput.GroupItem shrink>
+								<ClayToggle
+									label={
+										relevanceOn
+											? Liferay.Language.get('on')
+											: Liferay.Language.get('off')
+									}
+									onToggle={() =>
+										setRelevanceOn(!relevanceOn)
+									}
+									toggled={relevanceOn}
+								/>
+							</ClayInput.GroupItem>
+						</ClayInput.Group>
+					</ClayForm.Group>
+
+					<FieldList
+						initialValue={{
+							field: '',
+							label: '',
+							order: ORDERS.ASC.value,
+						}}
+						onChange={setFields}
+						renderInputs={(props) => <Inputs {...props} />}
+						value={fields}
+					/>
+
+					<input
+						name={`${namespace}${fieldsInputName}`}
+						type="hidden"
+						value={JSON.stringify(
+							relevanceOn
+								? [
+										{field: '', label: relevanceLabel},
+										...addOrdersToFieldNames(
+											removeEmptyFields(fields)
+										),
+								  ]
+								: addOrdersToFieldNames(
+										removeEmptyFields(fields)
+								  )
+						)}
+					/>
+				</>
+			)}
+
+			{view.value === VIEWS.CLASSIC.value && showAlert && (
+				<ClayAlert
+					displayType="info"
+					onClose={_handleCloseAlert}
+					title={Liferay.Language.get('info')}
+				>
+					{Liferay.Language.get(
+						'the-classic-view-will-be-removed-in-a-future-version'
+					)}
+				</ClayAlert>
+			)}
+
+			{view.value === VIEWS.CLASSIC.value && (
+				<span id={`${namespace}fieldsId`}>
+					{classicFields.map(({field, label}, index) => (
+						<div
+							className="field-form-row lfr-form-row lfr-form-row-inline"
+							key={index}
+						>
+							<div className="row-fields">
+								<div className="form-group">
+									<label htmlFor={`label_${index}`}>
+										{Liferay.Language.get('label')}
+									</label>
+
+									<ClayInput
+										className="label-input"
+										id={`label_${index}`}
+										onChange={_handleChangeClassicValue(
+											index,
+											'label'
+										)}
+										type="text"
+										value={label}
+									/>
+								</div>
+
+								<div className="form-group">
+									<label htmlFor={`field_${index}`}>
+										{Liferay.Language.get('field')}
+									</label>
+
+									<ClayInput
+										className="sort-field-input"
+										id={`field_${index}`}
+										onChange={_handleChangeClassicValue(
+											index,
+											'field'
+										)}
+										type="text"
+										value={field}
+									/>
+								</div>
+							</div>
 						</div>
-					</ClayInput.GroupItem>
+					))}
 
-					<ClayInput.GroupItem shrink>
-						<ClayToggle
-							label={
-								relevanceOn
-									? Liferay.Language.get('on')
-									: Liferay.Language.get('off')
-							}
-							onToggle={() => setRelevanceOn(!relevanceOn)}
-							toggled={relevanceOn}
-						/>
-					</ClayInput.GroupItem>
-				</ClayInput.Group>
-			</ClayForm.Group>
-
-			<FieldList
-				initialValue={{
-					field: '',
-					label: '',
-					order: ORDERS.ASC.value,
-				}}
-				onChange={setFields}
-				renderInputs={(props) => <Inputs {...props} />}
-				value={fields}
-			/>
+					<input
+						name={`${namespace}${fieldsInputName}`}
+						type="hidden"
+						value={JSON.stringify(classicFields)}
+					/>
+				</span>
+			)}
 		</div>
 	);
 }
