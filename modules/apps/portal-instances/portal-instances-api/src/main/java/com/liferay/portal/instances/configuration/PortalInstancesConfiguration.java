55a34e845cef7a1ef83de94563b3d46c00604669
Raymond Augé <raymond.auge@liferay.com>
2022-06-02 09:46:32 +0200

    LPS-161850 As OPS I want to provision virtual instances via configuration

---
+++ modules/apps/portal-instances/portal-instances-api/src/main/java/com/liferay/portal/instances/configuration/PortalInstancesConfiguration.java
@@ -0,0 +1,43 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.instances.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+/**
+ * @author Raymond Augé
+ */
+@Meta.OCD(
+	factory = true,
+	id = "com.liferay.portal.instances.configuration.PortalInstancesConfiguration"
+)
+public interface PortalInstancesConfiguration {
+
+	@Meta.AD(deflt = "true", required = false, type = Meta.Type.Boolean)
+	public boolean active();
+
+	@Meta.AD(required = false, type = Meta.Type.Integer)
+	public int maxUsers();
+
+	@Meta.AD(type = Meta.Type.String)
+	public String mx();
+
+	@Meta.AD(required = false, type = Meta.Type.String)
+	public String siteInitializerKey();
+
+	@Meta.AD(type = Meta.Type.String)
+	public String virtualHostname();
+
+}
---
+++ modules/apps/portal-instances/portal-instances-service/src/main/java/com/liferay/portal/instances/internal/configuration/PortalInstancesConfigurationFactory.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.instances.internal.configuration;
+
+import com.liferay.petra.lang.SafeCloseable;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.instances.configuration.PortalInstancesConfiguration;
+import com.liferay.portal.instances.service.PortalInstancesLocalService;
+import com.liferay.portal.kernel.exception.NoSuchCompanyException;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ConfigurationPolicy;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Raymond Augé
+ */
+@Component(
+	configurationPid = "com.liferay.portal.instances.configuration.PortalInstancesConfiguration",
+	configurationPolicy = ConfigurationPolicy.REQUIRE, immediate = true,
+	service = PortalInstancesConfigurationFactory.class
+)
+public class PortalInstancesConfigurationFactory {
+
+	@Activate
+	protected void activate(Map<String, Object> properties)
+		throws PortalException {
+
+		PortalInstancesConfiguration portalInstancesConfiguration =
+			ConfigurableUtil.createConfigurable(
+				PortalInstancesConfiguration.class, properties);
+
+		String webId = _getWebId(properties);
+		String virtualHostname = portalInstancesConfiguration.virtualHostname();
+		String mx = portalInstancesConfiguration.mx();
+		int maxUsers = portalInstancesConfiguration.maxUsers();
+		boolean active = portalInstancesConfiguration.active();
+
+		Company company = null;
+
+		try {
+			company = _companyLocalService.getCompanyByWebId(webId);
+		}
+		catch (NoSuchCompanyException noSuchCompanyException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(noSuchCompanyException);
+			}
+		}
+
+		if (company == null) {
+
+			// Add instance
+
+			company = _companyLocalService.addCompany(
+				null, webId, virtualHostname, mx, maxUsers, active);
+
+			String siteInitializerKey =
+				portalInstancesConfiguration.siteInitializerKey();
+
+			try (SafeCloseable safeCloseable =
+					CompanyThreadLocal.setWithSafeCloseable(
+						company.getCompanyId())) {
+
+				_portalInstancesLocalService.initializePortalInstance(
+					company.getCompanyId(), siteInitializerKey,
+					_servletContext);
+			}
+		}
+		else {
+
+			// Update instance
+
+			if (company.getCompanyId() ==
+					_portalInstancesLocalService.getDefaultCompanyId()) {
+
+				active = true;
+			}
+
+			_companyLocalService.updateCompany(
+				company.getCompanyId(), virtualHostname, mx, maxUsers, active);
+		}
+
+		_portalInstancesLocalService.synchronizePortalInstances();
+	}
+
+	private String _getWebId(Map<String, Object> properties) {
+		String pid = GetterUtil.getString(
+			properties.get(Constants.SERVICE_PID));
+
+		int index = pid.indexOf('~');
+
+		if (index > 0) {
+			pid = pid.substring(index + 1);
+		}
+
+		return pid;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PortalInstancesConfigurationFactory.class);
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private PortalInstancesLocalService _portalInstancesLocalService;
+
+	@Reference(
+		target = "(&(original.bean=true)(bean.id=javax.servlet.ServletContext))"
+	)
+	private ServletContext _servletContext;
+
+}
