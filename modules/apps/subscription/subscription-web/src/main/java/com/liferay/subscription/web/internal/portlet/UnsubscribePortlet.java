c768847f4243117d19c8b37a2663b44d19ebe924
Alan Huang <alan.huang@liferay.com>
2022-06-05 16:03:52 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/subscription/subscription-web/src/main/java/com/liferay/subscription/web/internal/portlet/UnsubscribePortlet.java
+++ modules/apps/subscription/subscription-web/src/main/java/com/liferay/subscription/web/internal/portlet/UnsubscribePortlet.java
@@ -41,7 +41,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.template-path=/META-INF/resources/",
 		"javax.portlet.name=" + SubscriptionPortletKeys.UNSUBSCRIBE,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=guest,power-user,user"
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/MyOrganizationsPortlet.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/portlet/MyOrganizationsPortlet.java
@@ -56,7 +56,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + UsersAdminPortletKeys.MY_ORGANIZATIONS,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
