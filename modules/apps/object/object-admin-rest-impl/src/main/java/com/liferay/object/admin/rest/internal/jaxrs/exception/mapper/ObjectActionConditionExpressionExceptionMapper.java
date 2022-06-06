05fbd3281b20df2ae93f6be12f45af3ec06ab2c1
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-06-06 09:42:16 -0300

    LPS-155427 Add exception mapper

---
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectActionConditionExpressionExceptionMapper.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.admin.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectActionConditionExpressionException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Carolina Barbosa
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Object.Admin.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectActionConditionExpressionExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class ObjectActionConditionExpressionExceptionMapper
+	extends BaseExceptionMapper<ObjectActionConditionExpressionException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectActionConditionExpressionException
+			objectActionConditionExpressionException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			objectActionConditionExpressionException.getMessage());
+	}
+
+}
