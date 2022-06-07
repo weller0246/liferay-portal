ac5c1bcd92bb4ca1d2b7ebae44374377b735812b
Diego Hu <diego.hu@liferay.com>
2022-06-07 17:23:53 +0200

    LPS-153304 Register upgrade step

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/FragmentServiceUpgrade.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/internal/upgrade/FragmentServiceUpgrade.java
@@ -151,6 +151,11 @@ public class FragmentServiceUpgrade implements UpgradeStepRegistrator {
 			"2.9.0", "2.9.1",
 			new com.liferay.fragment.internal.upgrade.v2_9_1.
 				FragmentEntryLinkUpgradeProcess());
+
+		registry.register(
+			"2.9.1", "2.9.2",
+			new com.liferay.fragment.internal.upgrade.v2_9_2.
+				FragmentCompositionUpgradeProcess());
 	}
 
 	@Reference
