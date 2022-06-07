a070b4ec7176cbd8bacd15d80f6a1023795de8d7
Rubén Pulido <ruben.pulido@liferay.com>
2022-06-07 15:02:04 +0200

    LPS-154522 Create interface InfoItemCreator similar to InfoItemFieldValuesUpdater

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/item/creator/InfoItemCreator.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.item.creator;
+
+import com.liferay.info.exception.InfoFormException;
+import com.liferay.info.item.InfoItemFieldValues;
+
+/**
+ * @author Rubén Pulido
+ */
+public interface InfoItemCreator<T> {
+
+	public T createFromInfoItemFieldValues(
+			InfoItemFieldValues infoItemFieldValues)
+		throws InfoFormException;
+
+}
