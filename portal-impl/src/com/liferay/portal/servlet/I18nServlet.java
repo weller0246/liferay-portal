46af10c50c49c71a1001bf621b2065151e78c09a
Jose L. Bango <jose.bango@liferay.com>
2022-06-07 16:40:52 +0200

    LPS-154862 Rename variable 'siteDefaultLocale'. That name can be misleading since it isn't always the site default locale

--- portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
+++ portal-impl/src/com/liferay/portal/servlet/I18nServlet.java
@@ -156,7 +156,7 @@ public class I18nServlet extends HttpServlet {
 			i18nLanguageCode = i18nLanguageId.substring(0, pos);
 		}
 
-		Locale siteDefaultLocale = LanguageUtil.getLocale(i18nLanguageCode);
+		Locale targetLocale = LanguageUtil.getLocale(i18nLanguageCode);
 
 		Group siteGroup = null;
 
@@ -188,27 +188,26 @@ public class I18nServlet extends HttpServlet {
 					return null;
 				}
 
-				siteDefaultLocale = LanguageUtil.getLocale(
+				targetLocale = LanguageUtil.getLocale(
 					siteGroup.getGroupId(), i18nLanguageCode);
 
-				if ((siteDefaultLocale == null) &&
+				if ((targetLocale == null) &&
 					PortalUtil.isGroupControlPanelPath(path)) {
 
-					siteDefaultLocale = LanguageUtil.getLocale(
-						i18nLanguageCode);
+					targetLocale = LanguageUtil.getLocale(i18nLanguageCode);
 				}
 			}
 		}
 
-		if (siteDefaultLocale == null) {
+		if (targetLocale == null) {
 			if (PropsValues.LOCALE_USE_DEFAULT_IF_NOT_AVAILABLE) {
-				siteDefaultLocale = PortalUtil.getSiteDefaultLocale(siteGroup);
+				targetLocale = PortalUtil.getSiteDefaultLocale(siteGroup);
 
-				i18nLanguageCode = siteDefaultLocale.getLanguage();
+				i18nLanguageCode = targetLocale.getLanguage();
 
 				i18nPath = StringPool.SLASH + i18nLanguageCode;
 
-				i18nLanguageId = LocaleUtil.toLanguageId(siteDefaultLocale);
+				i18nLanguageId = LocaleUtil.toLanguageId(targetLocale);
 			}
 			else {
 				return null;
@@ -216,7 +215,7 @@ public class I18nServlet extends HttpServlet {
 		}
 		else {
 			String siteDefaultLanguageId = LocaleUtil.toLanguageId(
-				siteDefaultLocale);
+				targetLocale);
 
 			if (siteDefaultLanguageId.startsWith(i18nLanguageId)) {
 				i18nPath = StringPool.SLASH + i18nLanguageCode;
