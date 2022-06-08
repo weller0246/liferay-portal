fd544e579eb04dd442cc628676c278b49617cb77
clara izquierdo <claraizquierdosanz@gmail.com>
2022-06-08 16:57:40 +0200

    LPS-153487 Fix content performance panel initialization

--- modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/AnalyticsReportsApp.js
+++ modules/dxp/apps/analytics/analytics-reports-web/src/main/resources/META-INF/resources/js/AnalyticsReportsApp.js
@@ -54,7 +54,7 @@ export default function AnalyticsReportsApp({context, portletNamespace}) {
 
 	useEffect(() => {
 		if (analyticsReportsPanelToggle) {
-			const sidenavInstance = Liferay.SideNavigation.initialize(
+			const sidenavInstance = Liferay.SideNavigation.instance(
 				analyticsReportsPanelToggle
 			);
 
