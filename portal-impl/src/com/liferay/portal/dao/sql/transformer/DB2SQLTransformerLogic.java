2724836d7bff03ac04adff8d0ba0fa3744501619
ricardocousolr <ricardo.couso@liferay.com>
2022-06-09 11:25:53 +0200

    LPS-155624 Revert "LPS-127348 Use one regex to match ? which is not in quote"
    Queries like SELECT somecolumn FROM sometable WHERE somecolumn = ? shouldn't be
    transformed, especially is somecolumn is a double.
    
    This reverts commit 742800639a2ee003bd81edfcf8ddbe92212727f1.

--- portal-impl/src/com/liferay/portal/dao/sql/transformer/DB2SQLTransformerLogic.java
+++ portal-impl/src/com/liferay/portal/dao/sql/transformer/DB2SQLTransformerLogic.java
@@ -19,6 +19,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.internal.dao.sql.transformer.SQLFunctionTransformer;
 import com.liferay.portal.kernel.dao.db.DB;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import java.util.function.Function;
 import java.util.regex.Matcher;
@@ -36,7 +37,8 @@ public class DB2SQLTransformerLogic extends BaseSQLTransformerLogic {
 			getBooleanFunction(), getCastClobTextFunction(),
 			getCastLongFunction(), getCastTextFunction(), getConcatFunction(),
 			getDropTableIfExistsTextFunction(), getIntegerDivisionFunction(),
-			getNullDateFunction(), _getQuestionMarkFunction()
+			getNullDateFunction(), _getCaseWhenThenFunction(),
+			_getLikeFunction(), _getSelectFunction()
 		};
 
 		if (!db.isSupportsStringCaseSensitiveQuery()) {
@@ -69,17 +71,77 @@ public class DB2SQLTransformerLogic extends BaseSQLTransformerLogic {
 		return matcher.replaceAll(dropTableIfExists);
 	}
 
-	private Function<String, String> _getQuestionMarkFunction() {
-		return (String sql) -> {
-			Matcher matcher = _questionMarkPattern.matcher(sql);
+	private Function<String, String> _getCaseWhenThenFunction() {
+		return (String sql) -> _replaceQuestionParameterMarker(
+			_caseWhenThenPattern.matcher(sql), sql);
+	}
 
-			return matcher.replaceAll(" COALESCE(CAST(? AS VARCHAR(2000)),'')");
+	private Function<String, String> _getLikeFunction() {
+		return (String sql) -> {
+			Matcher matcher = _likePattern.matcher(sql);
+
+			return matcher.replaceAll(
+				"LIKE COALESCE(CAST(? AS VARCHAR(2000)),'')");
 		};
 	}
 
-	private static final Pattern _questionMarkPattern = Pattern.compile(
-		"((?![\\'|\\\"][\\w\\s]*[\\\\'|\\\\\"]*[\\w\\s]*) \\?" +
-			"(?![\\w\\s]*[\\\\'|\\\\\"]*[\\w\\s]*[\\'|\\\"]))",
-		Pattern.CASE_INSENSITIVE);
+	private Function<String, String> _getSelectFunction() {
+		return (String sql) -> _replaceQuestionParameterMarker(
+			_selectPattern.matcher(sql), sql);
+	}
+
+	private String _replaceQuestionParameterMarker(
+		Matcher matcher, String sql) {
+
+		int index = 0;
+
+		StringBundler sb = new StringBundler();
+
+		while (matcher.find()) {
+			if (matcher.start() > index) {
+				sb.append(sql.substring(index, matcher.start()));
+			}
+
+			sb.append(
+				StringUtil.replace(
+					matcher.group(),
+					new String[] {
+						StringBundler.concat(
+							StringPool.SPACE, StringPool.QUESTION,
+							StringPool.COMMA),
+						StringBundler.concat(
+							StringPool.SPACE, StringPool.QUESTION,
+							StringPool.SPACE)
+					},
+					new String[] {
+						StringBundler.concat(
+							StringPool.SPACE,
+							_QUESTION_PARAMETER_MARKER_REPLACEMENT,
+							StringPool.COMMA),
+						StringBundler.concat(
+							StringPool.SPACE,
+							_QUESTION_PARAMETER_MARKER_REPLACEMENT,
+							StringPool.SPACE)
+					}));
+
+			index = matcher.end();
+		}
+
+		if (index < (sql.length() - 1)) {
+			sb.append(sql.substring(index));
+		}
+
+		return sb.toString();
+	}
+
+	private static final String _QUESTION_PARAMETER_MARKER_REPLACEMENT =
+		"COALESCE(CAST(? AS VARCHAR(2000)),'')";
+
+	private static final Pattern _caseWhenThenPattern = Pattern.compile(
+		"\\bcase when.+?end\\b", Pattern.CASE_INSENSITIVE);
+	private static final Pattern _likePattern = Pattern.compile(
+		"LIKE \\?", Pattern.CASE_INSENSITIVE);
+	private static final Pattern _selectPattern = Pattern.compile(
+		"select .+?from ", Pattern.CASE_INSENSITIVE);
 
 }
