67f620cd4f78b70a35881014dfa1558a7b4d99a5
Manuel Rives <manuel.rives@liferay.com>
2022-06-09 13:34:26 +0200

    LPS-155705 Added WEBP format support to ImageMagic scaling.

--- modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMImageMagickImageScaler.java
+++ modules/apps/adaptive-media/adaptive-media-image-impl/src/main/java/com/liferay/adaptive/media/image/internal/scaler/AMImageMagickImageScaler.java
@@ -46,7 +46,8 @@ import org.osgi.service.component.annotations.Reference;
  * @author Eric Yan
  */
 @Component(
-	immediate = true, property = "mime.type=image/heic",
+	immediate = true,
+	property = {"mime.type=image/heic", "mime.type=image/webp"},
 	service = AMImageScaler.class
 )
 public class AMImageMagickImageScaler implements AMImageScaler {
