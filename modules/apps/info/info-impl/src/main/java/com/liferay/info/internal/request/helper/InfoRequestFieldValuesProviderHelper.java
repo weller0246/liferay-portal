85961e937e19241b738c4a190b1c1f3cbf5aa848
Rubén Pulido <ruben.pulido@liferay.com>
2022-06-09 15:35:10 +0200

    LPS-154522 Create InfoRequestFieldValuesProviderHelper

---
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
@@ -0,0 +1,117 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.internal.request.helper;
+
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.TextInfoFieldType;
+import com.liferay.info.form.InfoForm;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Rubén Pulido
+ */
+public class InfoRequestFieldValuesProviderHelper {
+
+	public InfoRequestFieldValuesProviderHelper(
+		InfoItemServiceTracker infoItemServiceTracker) {
+
+		_infoItemServiceTracker = infoItemServiceTracker;
+	}
+
+	public <T> List<InfoFieldValue<Object>> getInfoFieldValues(
+		HttpServletRequest httpServletRequest) {
+
+		List<InfoFieldValue<Object>> infoFieldValues = new ArrayList<>();
+
+		String className = PortalUtil.getClassName(
+			ParamUtil.getLong(httpServletRequest, "classNameId"));
+
+		Map<String, String[]> parameterMap =
+			httpServletRequest.getParameterMap();
+
+		for (InfoField infoField : _getInfoFields(className)) {
+			if (ArrayUtil.isNotEmpty(parameterMap.get(infoField.getName()))) {
+				String[] values = parameterMap.get(infoField.getName());
+
+				for (String value : values) {
+					infoFieldValues.add(_getInfoFieldValue(infoField, value));
+				}
+			}
+		}
+
+		return infoFieldValues;
+	}
+
+	private <T> List<InfoField> _getInfoFields(String className) {
+		InfoItemFormProvider<T> infoItemFormProvider =
+			_infoItemServiceTracker.getFirstInfoItemService(
+				InfoItemFormProvider.class, className);
+
+		InfoForm infoForm = infoItemFormProvider.getInfoForm();
+
+		return infoForm.getAllInfoFields();
+	}
+
+	private InfoFieldValue<Object> _getInfoFieldValue(
+		InfoField infoField, Locale locale, Object object) {
+
+		if (infoField.isLocalizable()) {
+			return new InfoFieldValue<>(
+				infoField,
+				InfoLocalizedValue.builder(
+				).defaultLocale(
+					locale
+				).value(
+					locale, object
+				).build());
+		}
+
+		return new InfoFieldValue<>(infoField, object);
+	}
+
+	private InfoFieldValue<Object> _getInfoFieldValue(
+		InfoField infoField, String value) {
+
+		if (infoField.getInfoFieldType() instanceof TextInfoFieldType) {
+			return _getTextInfoFieldValue(infoField, value);
+		}
+
+		return null;
+	}
+
+	private InfoFieldValue<Object> _getTextInfoFieldValue(
+		InfoField<TextInfoFieldType> infoField, String value) {
+
+		return _getInfoFieldValue(
+			infoField, LocaleThreadLocal.getThemeDisplayLocale(), value);
+	}
+
+	private final InfoItemServiceTracker _infoItemServiceTracker;
+
+}
