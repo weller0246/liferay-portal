af436e6a63565ad2f1e9b6eac0dcd2561260280e
Victor Galan <victor.galan@liferay.com>
2022-06-09 10:35:31 +0200

    LPS-155627 Add Dropdown for showing the options

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-configuration-fields/CSSClassSelectorField.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/fragment-configuration-fields/CSSClassSelectorField.js
@@ -12,11 +12,14 @@
  * details.
  */
 
+import ClayDropDown from '@clayui/drop-down';
 import ClayForm from '@clayui/form';
+import ClayLabel from '@clayui/label';
 import ClayMultiSelect from '@clayui/multi-select';
-import React, {useMemo, useState} from 'react';
+import React, {useMemo, useRef, useState} from 'react';
 
 import useControlledState from '../../../core/hooks/useControlledState';
+import {useSelector} from '../../contexts/StoreContext';
 import {useId} from '../../utils/useId';
 
 export default function CSSClassSelectorField({
@@ -40,37 +43,109 @@ export default function CSSClassSelectorField({
 	const cssClassesInputId = useId();
 	const helpTextId = useId();
 
+	const multiSelectRef = useRef();
+
 	return (
-		<ClayForm.Group small>
-			<label htmlFor={cssClassesInputId}>
-				{Liferay.Language.get('css-classes')}
-			</label>
+		<>
+			<ClayForm.Group small>
+				<label htmlFor={cssClassesInputId}>
+					{Liferay.Language.get('css-classes')}
+				</label>
 
-			<ClayMultiSelect
-				id={cssClassesInputId}
-				items={items}
-				onChange={setValue}
-				onItemsChange={(items) => {
-					setItems(items);
+				<ClayMultiSelect
+					id={cssClassesInputId}
+					items={items}
+					onChange={setValue}
+					onItemsChange={(items) => {
+						setItems(items);
 
-					onValueSelect(
-						field.name,
-						items.map((item) => item.value)
-					);
-				}}
-				placeholder={
-					items.length > 0
-						? null
-						: Liferay.Language.get('type-to-add-a-class')
-				}
-				value={value}
-			/>
+						onValueSelect(
+							field.name,
+							items.map((item) => item.value)
+						);
+					}}
+					placeholder={
+						items.length > 0
+							? null
+							: Liferay.Language.get('type-to-add-a-class')
+					}
+					ref={multiSelectRef}
+					value={value}
+				/>
 
-			<div className="mt-1 small text-secondary" id={helpTextId}>
-				{Liferay.Language.get(
-					'use-a-space-enter-or-comma-to-add-multiple-classes'
+				<div className="mt-1 small text-secondary" id={helpTextId}>
+					{Liferay.Language.get(
+						'use-a-space-enter-or-comma-to-add-multiple-classes'
+					)}
+				</div>
+			</ClayForm.Group>
+			<CSSClassSelectorDropDown />
+		</>
+	);
+}
+
+function CSSClassSelectorDropDown({
+	active,
+	cssClass,
+	multiSelectRef,
+	onItemClick,
+	onSetActive,
+}) {
+	const dropdownRef = useRef();
+
+	const availableCssClasses = useSelector((state) => {
+		const layoutData = state.layoutData;
+
+		const cssClasses = new Set();
+
+		layoutData.items
+			?.flatMap((item) => item.config?.cssClasses ?? [])
+			.forEach((cssClass) => {
+				cssClasses.add(cssClass);
+			});
+
+		return cssClasses;
+	});
+
+	return (
+		<ClayDropDown.Menu
+			active={active}
+			alignElementRef={multiSelectRef}
+			onSetActive={onSetActive}
+			ref={dropdownRef}
+		>
+			<ClayDropDown.ItemList>
+				<ClayDropDown.Group header={Liferay.Language.get('new-class')}>
+					<ClayDropDown.Item
+						className="align-items-center d-flex text-3"
+						onClick={() => onItemClick(cssClass)}
+					>
+						{Liferay.Language.get('create')}
+
+						<ClayLabel className="ml-2" displayType="secondary">
+							{cssClass}
+						</ClayLabel>
+					</ClayDropDown.Item>
+				</ClayDropDown.Group>
+
+				{availableCssClasses.length > 0 && (
+					<ClayDropDown.Group
+						header={Liferay.Language.get('existing-classes')}
+					>
+						{availableCssClasses.map((availableCssClass) => (
+							<ClayDropDown.Item
+								className="align-items-center d-flex text-3"
+								key={availableCssClass}
+								onClick={() => onItemClick(availableCssClass)}
+							>
+								<ClayLabel displayType="secondary">
+									{availableCssClass}
+								</ClayLabel>
+							</ClayDropDown.Item>
+						))}
+					</ClayDropDown.Group>
 				)}
-			</div>
-		</ClayForm.Group>
+			</ClayDropDown.ItemList>
+		</ClayDropDown.Menu>
 	);
 }
