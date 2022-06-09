0edf85ac2be6fadfdd25fee15777d0c65d7c44f7
Rubén Pulido <ruben.pulido@liferay.com>
2022-06-09 15:36:09 +0200

    LPS-154522 Add support for DateInfoFieldType

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/helper/InfoRequestFieldValuesProviderHelper.java
@@ -16,17 +16,24 @@ package com.liferay.info.internal.request.helper;
 
 import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.field.type.DateInfoFieldType;
 import com.liferay.info.field.type.TextInfoFieldType;
 import com.liferay.info.form.InfoForm;
 import com.liferay.info.item.InfoItemServiceTracker;
 import com.liferay.info.item.provider.InfoItemFormProvider;
 import com.liferay.info.localized.InfoLocalizedValue;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
 
+import java.text.ParseException;
+
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -68,6 +75,27 @@ public class InfoRequestFieldValuesProviderHelper {
 		return infoFieldValues;
 	}
 
+	private InfoFieldValue<Object> _getDateInfoFieldValue(
+		InfoField<DateInfoFieldType> infoField, String value) {
+
+		Locale locale = LocaleThreadLocal.getThemeDisplayLocale();
+
+		Date date;
+
+		try {
+			date = DateUtil.parseDate("yyyy-MM-dd", value, locale);
+		}
+		catch (ParseException parseException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(parseException);
+			}
+
+			return null;
+		}
+
+		return _getInfoFieldValue(infoField, locale, date);
+	}
+
 	private <T> List<InfoField> _getInfoFields(String className) {
 		InfoItemFormProvider<T> infoItemFormProvider =
 			_infoItemServiceTracker.getFirstInfoItemService(
@@ -98,6 +126,10 @@ public class InfoRequestFieldValuesProviderHelper {
 	private InfoFieldValue<Object> _getInfoFieldValue(
 		InfoField infoField, String value) {
 
+		if (infoField.getInfoFieldType() instanceof DateInfoFieldType) {
+			return _getDateInfoFieldValue(infoField, value);
+		}
+
 		if (infoField.getInfoFieldType() instanceof TextInfoFieldType) {
 			return _getTextInfoFieldValue(infoField, value);
 		}
@@ -111,6 +143,9 @@ public class InfoRequestFieldValuesProviderHelper {
 		return _getInfoFieldValue(
 			infoField, LocaleThreadLocal.getThemeDisplayLocale(), value);
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		InfoRequestFieldValuesProviderHelper.class);
 
 	private final InfoItemServiceTracker _infoItemServiceTracker;
 
