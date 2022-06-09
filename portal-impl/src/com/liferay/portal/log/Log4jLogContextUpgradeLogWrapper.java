2d4f9d45854cba9f05d276b81c5529376de055a4
Luis Ortiz <luis.ortiz@liferay.com>
2022-06-09 09:49:29 +0200

    LPS-155004 Adding new log4j wrapper for adding thread context information for upgrade classes

---
+++ portal-impl/src/com/liferay/portal/log/Log4jLogContextUpgradeLogWrapper.java
@@ -0,0 +1,207 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.log;
+
+import com.liferay.portal.events.StartupHelperUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogWrapper;
+
+import org.apache.logging.log4j.ThreadContext;
+
+/**
+ * @author Luis Ortiz
+ */
+public class Log4jLogContextUpgradeLogWrapper extends LogWrapper {
+
+	public Log4jLogContextUpgradeLogWrapper(Log log) {
+		super(log);
+
+		setLogWrapperClassName(
+			Log4jLogContextUpgradeLogWrapper.class.getName());
+	}
+
+	@Override
+	public void debug(Object message) {
+		_populateThreadContext();
+
+		super.debug(message);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void debug(Object message, Throwable throwable) {
+		_populateThreadContext();
+
+		super.debug(message, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void debug(Throwable throwable) {
+		_populateThreadContext();
+
+		super.debug(null, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void error(Object message) {
+		_populateThreadContext();
+
+		super.error(message);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void error(Object message, Throwable throwable) {
+		_populateThreadContext();
+
+		super.error(message, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void error(Throwable throwable) {
+		_populateThreadContext();
+
+		super.error(null, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void fatal(Object message) {
+		_populateThreadContext();
+
+		super.fatal(message);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void fatal(Object message, Throwable throwable) {
+		_populateThreadContext();
+
+		super.fatal(message, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void fatal(Throwable throwable) {
+		_populateThreadContext();
+
+		super.fatal(null, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void info(Object message) {
+		_populateThreadContext();
+
+		super.info(message);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void info(Object message, Throwable throwable) {
+		_populateThreadContext();
+
+		super.info(message, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void info(Throwable throwable) {
+		_populateThreadContext();
+
+		super.info(null, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void trace(Object message) {
+		_populateThreadContext();
+
+		super.trace(message);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void trace(Object message, Throwable throwable) {
+		_populateThreadContext();
+
+		super.trace(message, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void trace(Throwable throwable) {
+		_populateThreadContext();
+
+		super.trace(null, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void warn(Object message) {
+		_populateThreadContext();
+
+		super.warn(message);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void warn(Object message, Throwable throwable) {
+		_populateThreadContext();
+
+		super.warn(message, throwable);
+
+		_cleanThreadContext();
+	}
+
+	@Override
+	public void warn(Throwable throwable) {
+		_populateThreadContext();
+
+		super.warn(null, throwable);
+
+		_cleanThreadContext();
+	}
+
+	private void _cleanThreadContext() {
+		ThreadContext.clearMap();
+	}
+
+	private void _populateThreadContext() {
+		if (StartupHelperUtil.isUpgrading()) {
+			ThreadContext.put("UpgradeLog", "UpgradeLog");
+		}
+	}
+
+}
--- portal-impl/src/com/liferay/portal/log/Log4jLogFactoryImpl.java
+++ portal-impl/src/com/liferay/portal/log/Log4jLogFactoryImpl.java
@@ -14,8 +14,14 @@
 
 package com.liferay.portal.log;
 
+import com.liferay.portal.dao.db.BaseDB;
+import com.liferay.portal.kernel.dao.db.BaseDBProcess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactory;
+import com.liferay.portal.kernel.upgrade.BaseUpgradeCallable;
+import com.liferay.portal.kernel.util.LoggingTimer;
+import com.liferay.portal.tools.DBUpgrader;
+import com.liferay.portal.verify.VerifyProperties;
 
 import org.apache.logging.log4j.LogManager;
 
@@ -31,8 +37,49 @@ public class Log4jLogFactoryImpl implements LogFactory {
 
 	@Override
 	public Log getLog(String name) {
-		return new Log4jLogContextLogWrapper(
+		Log log = new Log4jLogContextLogWrapper(
 			new Log4jLogImpl(LogManager.getLogger(name)));
+
+		if (_isUpgradeClass(name)) {
+			log = new Log4jLogContextUpgradeLogWrapper(log);
+		}
+
+		return log;
 	}
+
+	private boolean _isUpgradeClass(String name) {
+		try {
+			Thread thread = Thread.currentThread();
+
+			Class<?> clazz = Class.forName(
+				name, true, thread.getContextClassLoader());
+
+			for (Class<?> baseClazz : _BASE_CLASSES) {
+				if (baseClazz.isAssignableFrom(clazz)) {
+					return true;
+				}
+			}
+
+			for (Class<?> staticClazz : _STATIC_CLASSES) {
+				if (name.equals(staticClazz)) {
+					return true;
+				}
+			}
+		}
+		catch (ClassNotFoundException classNotFoundException) {
+			System.out.println(classNotFoundException);
+		}
+
+		return false;
+	}
+
+	private static final Class<?>[] _BASE_CLASSES = {
+		BaseDBProcess.class, BaseUpgradeCallable.class, LoggingTimer.class,
+		BaseDB.class
+	};
+
+	private static final Class<?>[] _STATIC_CLASSES = {
+		DBUpgrader.class, VerifyProperties.class
+	};
 
 }
