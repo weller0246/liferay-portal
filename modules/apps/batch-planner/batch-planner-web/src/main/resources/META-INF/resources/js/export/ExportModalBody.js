54dc1280d5befe1b2cab90bf9a1a3cbc094cff05
Igor Beslic <igor.beslic@liferay.com>
2022-06-10 10:35:09 +0200

    COMMERCE-7553 clean code - this looks like junior developer has no mentor to teaches him to properly code

--- modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/export/ExportModalBody.js
+++ modules/apps/batch-planner/batch-planner-web/src/main/resources/META-INF/resources/js/export/ExportModalBody.js
@@ -13,7 +13,6 @@
  */
 
 import ClayForm from '@clayui/form';
-import ClayIcon from '@clayui/icon';
 import ClayLabel from '@clayui/label';
 import ClayModal from '@clayui/modal';
 import ClayProgressBar from '@clayui/progress-bar';
@@ -36,7 +35,7 @@ const ExportModalBody = ({errorMessage, percentage, readyToDownload}) => {
 		label = Liferay.Language.get('completed');
 	}
 	else if (errorMessage) {
-		title = Liferay.Language.get('error');
+		title = errorMessage;
 		labelType = 'danger';
 		label = Liferay.Language.get('failed');
 	}
@@ -51,49 +50,17 @@ const ExportModalBody = ({errorMessage, percentage, readyToDownload}) => {
 			<ClayForm.Group
 				className={classnames({'has-error': !!errorMessage})}
 			>
-				<div>
-					{title}
+				<ClayForm.FeedbackGroup>
+					<ClayForm.FeedbackItem>{title}</ClayForm.FeedbackItem>
 
-					<div className="align-items-start d-flex pb-2 pt-2">
-						<ClayIcon
-							className="mr-2 mt-1"
-							symbol="document-default"
-						/>
+					<ClayForm.FeedbackItem>
+						{EXPORT_FILE_NAME}
+					</ClayForm.FeedbackItem>
 
-						<div>
-							<p className="mb-0">{EXPORT_FILE_NAME}</p>
+					<ClayLabel displayType={labelType}>{label}</ClayLabel>
+				</ClayForm.FeedbackGroup>
 
-							<ClayLabel displayType={labelType}>
-								{label}
-							</ClayLabel>
-						</div>
-					</div>
-				</div>
-
-				<div
-					className="progress-container"
-					data-percentage={readyToDownload ? 100 : percentage}
-					data-title={
-						readyToDownload
-							? Liferay.Language.get('completed')
-							: Liferay.Language.get('in-progress')
-					}
-				>
-					<ClayProgressBar
-						value={readyToDownload ? 100 : percentage}
-						warn={!!errorMessage}
-					/>
-				</div>
-
-				{errorMessage && (
-					<ClayForm.FeedbackGroup>
-						<ClayForm.FeedbackItem>
-							<ClayForm.FeedbackIndicator symbol="exclamation-full" />
-
-							{errorMessage}
-						</ClayForm.FeedbackItem>
-					</ClayForm.FeedbackGroup>
-				)}
+				<ClayProgressBar value={percentage} warn={!!errorMessage} />
 			</ClayForm.Group>
 		</ClayModal.Body>
 	);
