5301ccc00417d690c99f2675460e61105f9048ef
Joao Marcelo Alves <joao.alves@liferay.com>
2022-06-13 16:26:52 -0300

    LPS-154327 Basic Remote App Setup

--- modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/common/utils/constants.js
+++ modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/common/utils/constants.js
@@ -12,30 +12,19 @@
  * details.
  */
 
-import _axios from 'axios';
-
-import {Liferay} from './liferay';
-
-const {REACT_APP_LIFERAY_HOST = window.location.origin} = process.env;
-
-const baseFetch = async (url: string, options = {}) => {
-	// eslint-disable-next-line @liferay/portal/no-global-fetch
-	return fetch(REACT_APP_LIFERAY_HOST + '/' + url, {
-		headers: {
-			'Content-Type': 'application/json',
-			'x-csrf-token': Liferay.authToken,
-		},
-		...options,
-	});
+export const CONSTANTS = {
+	MONTHS_ABREVIATIONS: [
+		'Jan',
+		'Feb',
+		'Mar',
+		'Apr',
+		'May',
+		'Jun',
+		'Jul',
+		'Aug',
+		'Sep',
+		'Oct',
+		'Nov',
+		'Dec',
+	],
 };
-
-const axios = _axios.create({
-	baseURL: REACT_APP_LIFERAY_HOST,
-	headers: {
-		'x-csrf-token': Liferay.authToken,
-	},
-});
-
-export {axios};
-
-export default baseFetch;
