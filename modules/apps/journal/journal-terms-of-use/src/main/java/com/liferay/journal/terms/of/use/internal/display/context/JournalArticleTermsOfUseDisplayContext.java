e61e07365e150547fb36e883baf74cab951f7eff
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-06-13 14:58:12 +0200

    LPS-155878 Adds new display context to obtain the settings and use the configuration at company level for default values

---
+++ modules/apps/journal/journal-terms-of-use/src/main/java/com/liferay/journal/terms/of/use/internal/display/context/JournalArticleTermsOfUseDisplayContext.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.terms.of.use.internal.display.context;
+
+import com.liferay.journal.configuration.JournalServiceConfiguration;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.portal.kernel.settings.CompanyServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsFactoryUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class JournalArticleTermsOfUseDisplayContext {
+
+	public JournalArticleTermsOfUseDisplayContext(
+			JournalServiceConfiguration journalServiceConfiguration,
+			ThemeDisplay themeDisplay)
+		throws Exception {
+
+		_journalServiceConfiguration = journalServiceConfiguration;
+		_themeDisplay = themeDisplay;
+
+		_settings = SettingsFactoryUtil.getSettings(
+			new CompanyServiceSettingsLocator(
+				themeDisplay.getCompanyId(),
+				JournalServiceConfiguration.class.getName()));
+	}
+
+	public JournalArticle getJournalArticle() {
+		long termsOfUseJournalArticleGroupId =
+			getTermsOfUseJournalArticleGroupId();
+		String termsOfUseJournalArticleId = getTermsOfUseJournalArticleId();
+
+		if ((termsOfUseJournalArticleGroupId > 0) &&
+			Validator.isNotNull(termsOfUseJournalArticleId)) {
+
+			return JournalArticleLocalServiceUtil.fetchArticle(
+				termsOfUseJournalArticleGroupId, termsOfUseJournalArticleId);
+		}
+
+		return null;
+	}
+
+	public long getTermsOfUseJournalArticleGroupId() {
+		return GetterUtil.getLong(
+			_settings.getValue(
+				_TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID_CONFIGURATION_PROPERTY,
+				String.valueOf(
+					_journalServiceConfiguration.
+						termsOfUseJournalArticleGroupId())));
+	}
+
+	public String getTermsOfUseJournalArticleGroupIdConfigurationProperty() {
+		return _TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID_CONFIGURATION_PROPERTY;
+	}
+
+	public String getTermsOfUseJournalArticleId() {
+		return _settings.getValue(
+			_TERMS_OF_USE_JOURNAL_ARTICLE_ID_CONFIGURATION_PROPERTY,
+			String.valueOf(
+				_journalServiceConfiguration.termsOfUseJournalArticleId()));
+	}
+
+	public String getTermsOfUseJournalArticleIdConfigurationProperty() {
+		return _TERMS_OF_USE_JOURNAL_ARTICLE_ID_CONFIGURATION_PROPERTY;
+	}
+
+	private static final String
+		_TERMS_OF_USE_JOURNAL_ARTICLE_GROUP_ID_CONFIGURATION_PROPERTY =
+			"terms.of.use.journal.article.group.id";
+
+	private static final String
+		_TERMS_OF_USE_JOURNAL_ARTICLE_ID_CONFIGURATION_PROPERTY =
+			"terms.of.use.journal.article.id";
+
+	private final JournalServiceConfiguration _journalServiceConfiguration;
+	private final Settings _settings;
+	private final ThemeDisplay _themeDisplay;
+
+}
