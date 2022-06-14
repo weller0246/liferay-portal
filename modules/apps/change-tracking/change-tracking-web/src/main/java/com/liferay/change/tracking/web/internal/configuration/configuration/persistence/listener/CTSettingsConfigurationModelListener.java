0cc11bbe1e904ebd1102b4d8e3f2d63fff967580
David Truong <david.truong@liferay.com>
2022-06-14 15:50:26 -0500

    LPS-154710 add helper for web module

---
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/configuration/configuration/persistence/listener/CTSettingsConfigurationModelListener.java
@@ -0,0 +1,192 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.web.internal.configuration.configuration.persistence.listener;
+
+import com.liferay.change.tracking.configuration.CTSettingsConfiguration;
+import com.liferay.change.tracking.constants.CTPortletKeys;
+import com.liferay.change.tracking.exception.CTStagingEnabledException;
+import com.liferay.change.tracking.model.CTCollection;
+import com.liferay.change.tracking.model.CTPreferences;
+import com.liferay.change.tracking.model.CTPreferencesTable;
+import com.liferay.change.tracking.service.CTCollectionLocalService;
+import com.liferay.change.tracking.service.CTPreferencesLocalService;
+import com.liferay.change.tracking.web.internal.scheduler.PublishScheduler;
+import com.liferay.petra.sql.dsl.DSLQueryFactoryUtil;
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListener;
+import com.liferay.portal.configuration.persistence.listener.ConfigurationModelListenerException;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.GroupTable;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.permission.PortletPermission;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+import com.liferay.portal.util.PropsValues;
+
+import java.util.Dictionary;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author David Truong
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.change.tracking.configuration.CTSettingsConfiguration",
+	service = ConfigurationModelListener.class
+)
+public class CTSettingsConfigurationModelListener
+	implements ConfigurationModelListener {
+
+	@Override
+	public void onAfterSave(String pid, Dictionary<String, Object> properties)
+		throws ConfigurationModelListenerException {
+
+		boolean enabled = GetterUtil.getBoolean(properties.get("enabled"));
+
+		if (!enabled) {
+			try {
+				long companyId = GetterUtil.getLong(
+					properties.get("companyId"));
+
+				_cleanUpCTPreferences(companyId);
+
+				_cleanUpScheduledPublications(companyId);
+			}
+			catch (PortalException portalException) {
+				throw new ConfigurationModelListenerException(
+					portalException, CTSettingsConfiguration.class, getClass(),
+					properties);
+			}
+		}
+	}
+
+	@Override
+	public void onBeforeSave(String pid, Dictionary<String, Object> properties)
+		throws ConfigurationModelListenerException {
+
+		boolean enabled = GetterUtil.getBoolean(properties.get("enabled"));
+		boolean sandboxEnabled = GetterUtil.getBoolean(
+			properties.get("sandboxEnabled"));
+
+		if (!enabled && sandboxEnabled) {
+			properties.put("sandboxEnabled", false);
+		}
+
+		if (enabled) {
+			try {
+				_portletPermission.check(
+					PermissionThreadLocal.getPermissionChecker(),
+					CTPortletKeys.PUBLICATIONS, ActionKeys.CONFIGURATION);
+
+				long companyId = GetterUtil.getLong(
+					properties.get("companyId"));
+
+				_checkStagingEnabled(companyId);
+			}
+			catch (PortalException portalException) {
+				throw new ConfigurationModelListenerException(
+					portalException, CTSettingsConfiguration.class, getClass(),
+					properties);
+			}
+		}
+	}
+
+	private void _checkStagingEnabled(long companyId) throws PortalException {
+		for (Group group :
+				_groupLocalService.<List<Group>>dslQuery(
+					DSLQueryFactoryUtil.select(
+						GroupTable.INSTANCE
+					).from(
+						GroupTable.INSTANCE
+					).where(
+						GroupTable.INSTANCE.companyId.eq(
+							companyId
+						).and(
+							GroupTable.INSTANCE.liveGroupId.neq(
+								GroupConstants.DEFAULT_LIVE_GROUP_ID
+							).or(
+								GroupTable.INSTANCE.typeSettings.like(
+									"%staged=true%")
+							).withParentheses()
+						)
+					))) {
+
+			if (group.isStaged() || group.isStagingGroup()) {
+				throw new CTStagingEnabledException();
+			}
+		}
+	}
+
+	private void _cleanUpCTPreferences(long companyId) {
+		for (CTPreferences ctPreferences :
+				_ctPreferencesLocalService.<List<CTPreferences>>dslQuery(
+					DSLQueryFactoryUtil.select(
+						CTPreferencesTable.INSTANCE
+					).from(
+						CTPreferencesTable.INSTANCE
+					).where(
+						CTPreferencesTable.INSTANCE.companyId.eq(companyId)
+					))) {
+
+			_ctPreferencesLocalService.deleteCTPreferences(ctPreferences);
+		}
+	}
+
+	private void _cleanUpScheduledPublications(long companyId)
+		throws PortalException {
+
+		if (PropsValues.SCHEDULER_ENABLED) {
+			List<CTCollection> ctCollections =
+				_ctCollectionLocalService.getCTCollections(
+					companyId, WorkflowConstants.STATUS_SCHEDULED,
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+			for (CTCollection ctCollection : ctCollections) {
+				_publishScheduler.unschedulePublish(
+					ctCollection.getCtCollectionId());
+			}
+		}
+	}
+
+	@Reference
+	private CTCollectionLocalService _ctCollectionLocalService;
+
+	@Reference
+	private CTPreferencesLocalService _ctPreferencesLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private PortletPermission _portletPermission;
+
+	@Reference(
+		cardinality = ReferenceCardinality.OPTIONAL,
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY
+	)
+	private volatile PublishScheduler _publishScheduler;
+
+}
---
+++ modules/apps/change-tracking/change-tracking-web/src/main/java/com/liferay/change/tracking/web/internal/configuration/helper/CTSettingsConfigurationHelper.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.change.tracking.web.internal.configuration.helper;
+
+import com.liferay.change.tracking.configuration.CTSettingsConfiguration;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+
+import java.util.Collections;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author David Truong
+ */
+@Component(immediate = true, service = CTSettingsConfigurationHelper.class)
+public class CTSettingsConfigurationHelper {
+
+	public void delete(long companyId) throws PortalException {
+		_configurationProvider.deleteCompanyConfiguration(
+			CTSettingsConfiguration.class, companyId);
+	}
+
+	public boolean enabled(long companyId) {
+		CTSettingsConfiguration configuration = _getConfiguration(companyId);
+
+		return configuration.enabled();
+	}
+
+	public CTSettingsConfiguration getCTSettingsConfiguration(long companyId) {
+		return _getConfiguration(companyId);
+	}
+
+	public boolean sandboxEnabled(long companyId) {
+		CTSettingsConfiguration configuration = _getConfiguration(companyId);
+
+		return configuration.sandboxEnabled();
+	}
+
+	public void save(long companyId, boolean enabled, boolean sandboxEnabled)
+		throws PortalException {
+
+		_configurationProvider.saveCompanyConfiguration(
+			CTSettingsConfiguration.class, companyId,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"enabled", enabled
+			).put(
+				"sandboxEnabled", sandboxEnabled
+			).build());
+	}
+
+	private CTSettingsConfiguration _getConfiguration(long companyId) {
+		CTSettingsConfiguration configuration =
+			ConfigurableUtil.createConfigurable(
+				CTSettingsConfiguration.class, Collections.emptyMap());
+
+		try {
+			configuration = _configurationProvider.getCompanyConfiguration(
+				CTSettingsConfiguration.class, companyId);
+		}
+		catch (ConfigurationException configurationException) {
+			_log.error(configurationException);
+		}
+
+		return configuration;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CTSettingsConfigurationHelper.class.getName());
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+}
