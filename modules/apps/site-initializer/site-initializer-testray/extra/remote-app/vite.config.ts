6bd8c9e8cbc1b63b65f1def52a7ac6e0256c394e
Keven Leone <keven.leone@liferay.com>
2022-06-15 16:49:08 -0300

    LPS-156137 Replace CRA for Vite

---
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/vite.config.ts
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import {defineConfig} from 'vite';
+import {UserConfigExport as VitestConfigExport} from 'vitest/config';
+
+export default defineConfig({
+	build: {outDir: 'build'},
+	plugins: [react()],
+	test: {
+		coverage: {
+			all: true,
+			exclude: [
+				path.resolve(__dirname),
+				'src/graphql/fragments',
+				path.resolve(__dirname),
+				'src/graphql/queries',
+				path.resolve(__dirname),
+				'src/graphql/mutations',
+			],
+			include: [path.resolve(__dirname), 'src'],
+		},
+		environment: 'jsdom',
+		exclude: ['node_modules', 'build'],
+		globals: true,
+		include: ['**/(*.)?{test,spec}.{ts,tsx}'],
+		setupFiles: ['./src/setupTests.ts'],
+	},
+} as VitestConfigExport);
