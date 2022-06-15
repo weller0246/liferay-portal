2e527daa90b4df5ce5419e1be500023df6d28a2d
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-06-15 13:57:19 +0200

    LPS-155701 SF

--- modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/struts/AddInfoItemStrutsAction.java
+++ modules/apps/info/info-impl/src/main/java/com/liferay/info/internal/request/struts/AddInfoItemStrutsAction.java
@@ -129,7 +129,7 @@ public class AddInfoItemStrutsAction implements StrutsAction {
 		}
 		catch (Exception exception) {
 			if (_log.isDebugEnabled()) {
-				_log.debug("Unable to add info item", exception);
+				_log.debug(exception);
 			}
 
 			InfoFormException infoFormException = new InfoFormException();
