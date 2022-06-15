1efbd9ef2dcaa3ff96c78c34ca2a5aca486e90a1
Alan Huang <alan.huang@liferay.com>
2022-06-16 01:06:59 +0800

    LPS-150378 SF, inline

--- modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/TOCTOUTest.java
+++ modules/apps/oauth2-provider/oauth2-provider-test/src/testIntegration/java/com/liferay/oauth2/provider/client/test/TOCTOUTest.java
@@ -111,13 +111,13 @@ public class TOCTOUTest extends BaseClientTestCase {
 		// Try again with a fresh narrowed down token for "everything.read".
 		// It should still fail (admin TOCTOU protection when narrowing down).
 
-		token = getToken(
-			"oauthTestApplicationCode", null,
-			getAuthorizationCodeBiFunction(
-				"test@liferay.com", "test", null, "everything.read"),
-			this::parseTokenString);
-
-		webTarget2InvocationBuilder = authorize(webTarget2.request(), token);
+		webTarget2InvocationBuilder = authorize(
+			webTarget2.request(),
+			getToken(
+				"oauthTestApplicationCode", null,
+				getAuthorizationCodeBiFunction(
+					"test@liferay.com", "test", null, "everything.read"),
+				this::parseTokenString));
 
 		try (LogCapture logCapture = LoggerTestUtil.configureLog4JLogger(
 				"portal_web.docroot.errors.code_jsp", LoggerTestUtil.WARN)) {
