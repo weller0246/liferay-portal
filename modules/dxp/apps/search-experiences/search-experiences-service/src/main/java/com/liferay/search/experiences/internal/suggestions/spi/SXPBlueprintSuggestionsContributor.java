2d3ede959a8f76a1d23462155089a281c6571ace
Petteri Karttunen <petteri.karttunen@liferay.com>
2022-06-16 10:14:28 +0300

    LPS-156112 search-experiences-service: Fields attribute is a list

--- modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/suggestions/spi/SXPBlueprintSuggestionsContributor.java
+++ modules/dxp/apps/search-experiences/search-experiences-service/src/main/java/com/liferay/search/experiences/internal/suggestions/spi/SXPBlueprintSuggestionsContributor.java
@@ -34,6 +34,7 @@ import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -221,7 +222,7 @@ public class SXPBlueprintSuggestionsContributor
 	}
 
 	private Map<String, Object> _getFieldValues(
-		Document document, String[] fieldNames, Locale locale) {
+		Document document, List<String> fieldNames, Locale locale) {
 
 		Map<String, Object> fieldValues = new HashMap<>();
 
@@ -286,7 +287,7 @@ public class SXPBlueprintSuggestionsContributor
 	}
 
 	private Suggestion _getSuggestion(
-		String[] fieldNames, boolean includeAssetSearchSummary,
+		List<String> fieldNames, boolean includeAssetSearchSummary,
 		boolean includeAssetURL, Layout layout,
 		LiferayPortletRequest liferayPortletRequest,
 		LiferayPortletResponse liferayPortletResponse,
@@ -302,7 +303,7 @@ public class SXPBlueprintSuggestionsContributor
 
 		Document document = searchHit.getDocument();
 
-		if (fieldNames.length > 0) {
+		if (ListUtil.isNotEmpty(fieldNames)) {
 			suggestionBuilder.attribute(
 				"fields",
 				_getFieldValues(
@@ -363,8 +364,7 @@ public class SXPBlueprintSuggestionsContributor
 
 		List<Suggestion> suggestions = new ArrayList<>();
 
-		String[] fieldNames = GetterUtil.getStringValues(
-			attributes.get("fields"));
+		List<String> fieldNames = (List<String>)attributes.get("fields");
 		String fieldValueSeparator = MapUtil.getString(
 			attributes, "fieldValueSeparator");
 		boolean includeAssetSearchSummary = MapUtil.getBoolean(
