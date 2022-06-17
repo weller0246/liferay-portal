3e64739cb59d7d5a2928e8a08f3d0210b9f6e971
Rubén Pulido <ruben.pulido@liferay.com>
2022-06-17 15:21:40 +0200

    LPS-156299 Create ObjectEntry from InfoItemFieldValues

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/creator/ObjectEntryInfoItemCreator.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/info/item/creator/ObjectEntryInfoItemCreator.java
@@ -15,9 +15,24 @@
 package com.liferay.object.web.internal.info.item.creator;
 
 import com.liferay.info.exception.InfoFormException;
+import com.liferay.info.field.InfoField;
+import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.item.creator.InfoItemCreator;
+import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.service.ObjectEntryService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+
+import java.io.Serializable;
+
+import java.util.Collection;
 
 /**
  * @author Eudaldo Alonso
@@ -25,12 +40,58 @@ import com.liferay.object.model.ObjectEntry;
 public class ObjectEntryInfoItemCreator
 	implements InfoItemCreator<ObjectEntry> {
 
+	public ObjectEntryInfoItemCreator(
+		ObjectDefinition objectDefinition,
+		ObjectEntryService objectEntryService) {
+
+		_objectDefinition = objectDefinition;
+		_objectEntryService = objectEntryService;
+	}
+
 	@Override
 	public ObjectEntry createFromInfoItemFieldValues(
 			InfoItemFieldValues infoItemFieldValues)
 		throws InfoFormException {
 
-		return null;
+		try {
+			Collection<InfoFieldValue<Object>> infoFieldValues =
+				infoItemFieldValues.getInfoFieldValues();
+
+			HashMapBuilder.HashMapWrapper<String, Serializable> hashMapWrapper =
+				HashMapBuilder.create(infoFieldValues.size());
+
+			ServiceContext serviceContext =
+				ServiceContextThreadLocal.getServiceContext();
+
+			ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+			for (InfoFieldValue<Object> infoFieldValue : infoFieldValues) {
+				InfoField infoField = infoFieldValue.getInfoField();
+
+				hashMapWrapper.put(
+					infoField.getName(),
+					(Serializable)infoFieldValue.getValue(
+						themeDisplay.getLocale()));
+			}
+
+			return _objectEntryService.addObjectEntry(
+				themeDisplay.getScopeGroupId(),
+				_objectDefinition.getObjectDefinitionId(),
+				hashMapWrapper.build(), serviceContext);
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(portalException);
+			}
+
+			throw new InfoFormException();
+		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ObjectEntryInfoItemCreator.class);
+
+	private final ObjectDefinition _objectDefinition;
+	private final ObjectEntryService _objectEntryService;
 
 }
