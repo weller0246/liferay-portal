a62e36998f224df0cd432a2a9a4bf9616feba17e
lazhcsilva <lazhcsilva@gmail.com>
2022-06-17 15:10:49 -0300

    LPS-147632 Removing auto-fill empty when selecting Role

--- modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/shared-components/BaseRole.js
+++ modules/dxp/apps/portal-workflow/portal-workflow-kaleo-designer-web/src/main/resources/META-INF/resources/designer/js/definition-builder/diagram-builder/components/sidebar/sections/shared-components/BaseRole.js
@@ -56,7 +56,6 @@ export default function BaseRole({
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [defaultFieldValue]);
 
-	const initialLoading = networkStatus === 1;
 	const loading = networkStatus < 4;
 	const error = networkStatus === 5;
 
@@ -104,7 +103,7 @@ export default function BaseRole({
 					/>
 
 					<ClayAutocomplete.DropDown
-						active={(!!resource && active) || initialLoading}
+						active={!!resource && active}
 						closeOnClickOutside
 						onSetActive={setActive}
 					>
