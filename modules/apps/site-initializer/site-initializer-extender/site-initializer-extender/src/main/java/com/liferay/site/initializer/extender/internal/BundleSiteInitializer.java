b43c09eb590ccc5b9e4fcff9fea8f2d62fc0ee72
Jair Medeiros <jair.medeiros@liferay.com>
2022-06-18 21:43:30 -0300

    LPS-152403 Improve Java code and add new fields for Customer Portal

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -1092,6 +1092,12 @@ public class BundleSiteInitializer implements SiteInitializer {
 		).build();
 	}
 
+	private String _getExpandoPropertyKey(String entryKey) {
+		String[] keyParts = entryKey.split("(?=\\p{Upper})");
+
+		return StringUtil.merge(keyParts, StringPool.DASH).toLowerCase();
+	}
+
 	private void _addExpandoColumns(ServiceContext serviceContext)
 		throws Exception {
 
@@ -1129,7 +1135,7 @@ public class BundleSiteInitializer implements SiteInitializer {
 					for (Map.Entry<String, Object> entry :
 						jsonObjectProperties.toMap().entrySet()) {
 						unicodeProperties.setProperty(
-							entry.getKey(),
+								_getExpandoPropertyKey(entry.getKey()),
 							entry.getValue().toString());
 					}
 
