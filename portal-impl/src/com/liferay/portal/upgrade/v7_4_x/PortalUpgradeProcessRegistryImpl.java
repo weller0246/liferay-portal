00b3b078ca5fd6ee74daab06fe5f5add30a6fd42
Brian Chan <brian.chan@liferay.com>
2022-06-19 13:34:09 -0700

    LPS-155786 my bad

--- portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_4_x/PortalUpgradeProcessRegistryImpl.java
@@ -15,7 +15,7 @@
 package com.liferay.portal.upgrade.v7_4_x;
 
 import com.liferay.portal.kernel.upgrade.CTModelUpgradeProcess;
-import com.liferay.portal.kernel.upgrade.DummyUpgradeStep;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeProcess;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.version.Version;
 import com.liferay.portal.upgrade.util.PortalUpgradeProcessRegistry;
@@ -101,7 +101,7 @@ public class PortalUpgradeProcessRegistryImpl
 
 		upgradeProcesses.put(new Version(15, 0, 0), new UpgradeOrgGroupRole());
 
-		upgradeProcesses.put(new Version(16, 0, 0), new DummyUpgradeStep());
+		upgradeProcesses.put(new Version(16, 0, 0), new DummyUpgradeProcess());
 	}
 
 }
