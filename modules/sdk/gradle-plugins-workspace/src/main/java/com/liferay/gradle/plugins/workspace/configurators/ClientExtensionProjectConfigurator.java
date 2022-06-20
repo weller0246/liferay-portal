aadafaaed67fe23ab77c5027fb289e304f2ff9c6
Simon.Jiang <simon.jiang@liferay.com>
2022-06-20 09:15:28 +0800

    LCD-14295 should not add client-extension.yaml in root dir of workspace

--- modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/ClientExtensionProjectConfigurator.java
+++ modules/sdk/gradle-plugins-workspace/src/main/java/com/liferay/gradle/plugins/workspace/configurators/ClientExtensionProjectConfigurator.java
@@ -51,6 +51,7 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.concurrent.Callable;
 
@@ -202,7 +203,9 @@ public class ClientExtensionProjectConfigurator
 					Path clientExtensionPath = dirPath.resolve(
 						_CLIENT_EXTENSION_YAML);
 
-					if (Files.exists(clientExtensionPath)) {
+					if (Files.exists(clientExtensionPath) &&
+						!Objects.equals(dirPath, rootDir.toPath())) {
+
 						projectDirs.add(dirPath.toFile());
 
 						return FileVisitResult.SKIP_SUBTREE;
