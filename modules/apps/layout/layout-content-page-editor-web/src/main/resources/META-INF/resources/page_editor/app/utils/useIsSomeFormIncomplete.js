21698a226791e1be001046343975d39cc65abaf6
Sandro Chinea <sandrochinea@gmail.com>
2022-06-20 17:02:45 +0100

    LPS-156092 Use constant

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/useIsSomeFormIncomplete.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/useIsSomeFormIncomplete.js
@@ -16,7 +16,7 @@ import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
 import {useGlobalContext} from '../contexts/GlobalContext';
 import {useSelectorRef} from '../contexts/StoreContext';
 import FormService from '../services/FormService';
-import {getCacheItem, getCacheKey} from './cache';
+import {CACHE_KEYS, getCacheItem, getCacheKey} from './cache';
 import hasRequiredInputChild from './hasRequiredInputChild';
 import hasSubmitChild from './hasSubmitChild';
 import {isLayoutDataItemDeleted} from './isLayoutDataItemDeleted';
@@ -63,7 +63,7 @@ export default function useIsSomeFormIncomplete() {
 			};
 
 			const cacheKey = getCacheKey([
-				'formFields',
+				CACHE_KEYS.formFields,
 				classNameId,
 				classTypeId,
 			]);
