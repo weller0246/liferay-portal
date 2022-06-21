70419ffe9b645b1ff6f93290eb887546e82b9675
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-06-21 14:58:43 +0200

    LPS-156404 Add struts action to handle status

---
+++ modules/apps/layout/layout-utility-page/layout-utility-page-status/src/main/java/com/liferay/layout/utility/page/status/internal/struts/StatusStrutsAction.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.utility.page.status.internal.struts;
+
+import com.liferay.portal.kernel.struts.StrutsAction;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	immediate = true, property = "path=/portal/status",
+	service = StrutsAction.class
+)
+public class StatusStrutsAction implements StrutsAction {
+
+	@Override
+	public String execute(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher("/status.jsp");
+
+		requestDispatcher.include(httpServletRequest, httpServletResponse);
+
+		return null;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.layout.utility.page.status)"
+	)
+	private ServletContext _servletContext;
+
+}
