46142e0dd1cce5ed0bc4ff81140a0676b96a8dde
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-06-22 15:28:09 -0300

    LPS-156234 SF

--- modules/apps/object/object-api/src/main/java/com/liferay/object/exception/NoSuchObjectStateTransitionException.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/exception/NoSuchObjectStateTransitionException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.object.exception;
 
 import com.liferay.portal.kernel.exception.NoSuchModelException;
@@ -27,7 +28,9 @@ public class NoSuchObjectStateTransitionException extends NoSuchModelException {
 		super(msg);
 	}
 
-	public NoSuchObjectStateTransitionException(String msg, Throwable throwable) {
+	public NoSuchObjectStateTransitionException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
