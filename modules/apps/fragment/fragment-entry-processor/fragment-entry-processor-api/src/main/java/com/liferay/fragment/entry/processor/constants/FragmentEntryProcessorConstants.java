e28ce52b21c51a81f176d68881210ac1ca45d82c
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-06-22 09:36:34 +0100

    LPS-156377 Adds new class with key processor constants

---
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-api/src/main/java/com/liferay/fragment/entry/processor/constants/FragmentEntryProcessorConstants.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.constants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class FragmentEntryProcessorConstants {
+
+	public static final String KEY_BACKGROUND_IMAGE_FRAGMENT_ENTRY_PROCESSOR =
+		"com.liferay.fragment.entry.processor.background.image." +
+			"BackgroundImageFragmentEntryProcessor";
+
+	public static final String KEY_EDITABLE_FRAGMENT_ENTRY_PROCESSOR =
+		"com.liferay.fragment.entry.processor.editable." +
+			"EditableFragmentEntryProcessor";
+
+	public static final String KEY_FREEMARKER_FRAGMENT_ENTRY_PROCESSOR =
+		"com.liferay.fragment.entry.processor.freemarker." +
+			"FreeMarkerFragmentEntryProcessor";
+
+	public static final String KEY_STYLES_FRAGMENT_ENTRY_PROCESSOR =
+		"com.liferay.fragment.entry.processor.styles." +
+			"StylesFragmentEntryProcessor";
+
+}
