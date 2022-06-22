9dc6d4eae60737aeb03d70a080d69295bd1ed964
marcelabc <marcela.cunha@liferay.com>
2022-06-22 18:00:26 -0300

    LPS-156644 Implement ObjectFieldBusinessType for Aggregation field

---
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/AggregationObjectFieldBusinessType.java
@@ -0,0 +1,111 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.internal.field.business.type;
+
+import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
+import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.field.business.type.ObjectFieldBusinessType;
+import com.liferay.object.field.render.ObjectFieldRenderingContext;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.service.ObjectFieldSettingLocalService;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.SetUtil;
+
+import java.util.Locale;
+import java.util.Map;
+import java.util.Set;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marcela Cunha
+ */
+@Component(
+	immediate = true,
+	property = "object.field.business.type.key=" + ObjectFieldConstants.BUSINESS_TYPE_AGGREGATION,
+	service = {
+		AggregationObjectFieldBusinessType.class, ObjectFieldBusinessType.class
+	}
+)
+public class AggregationObjectFieldBusinessType
+	implements ObjectFieldBusinessType {
+
+	@Override
+	public Set<String> getAllowedObjectFieldSettingsNames() {
+		return SetUtil.fromArray("function", "relatedObject", "summarizeField");
+	}
+
+	@Override
+	public String getDBType() {
+		return ObjectFieldConstants.DB_TYPE_STRING;
+	}
+
+	@Override
+	public String getDDMFormFieldTypeName() {
+		return DDMFormFieldTypeConstants.TEXT;
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		return LanguageUtil.get(
+			ResourceBundleUtil.getModuleAndPortalResourceBundle(
+				locale, getClass()),
+			"summarize-data-values");
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(
+			ResourceBundleUtil.getModuleAndPortalResourceBundle(
+				locale, getClass()),
+			"aggregation");
+	}
+
+	@Override
+	public String getName() {
+		return ObjectFieldConstants.BUSINESS_TYPE_AGGREGATION;
+	}
+
+	@Override
+	public Map<String, Object> getProperties(
+		ObjectField objectField,
+		ObjectFieldRenderingContext objectFieldRenderingContext) {
+
+		Map<String, Object> properties = HashMapBuilder.<String, Object>put(
+			"readOnly", true
+		).build();
+
+		ListUtil.isNotEmptyForEach(
+			_objectFieldSettingLocalService.getObjectFieldSettings(
+				objectField.getObjectFieldId()),
+			objectFieldSetting -> properties.put(
+				objectFieldSetting.getName(), objectFieldSetting.getValue()));
+
+		return properties;
+	}
+
+	@Override
+	public Set<String> getRequiredObjectFieldSettingsNames() {
+		return SetUtil.fromArray("function", "relatedObject", "summarizeField");
+	}
+
+	@Reference
+	private ObjectFieldSettingLocalService _objectFieldSettingLocalService;
+
+}
