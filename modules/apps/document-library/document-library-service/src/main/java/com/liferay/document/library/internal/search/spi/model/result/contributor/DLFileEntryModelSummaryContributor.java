518a19ed2ee5b0dd66eff7e12fdca6d7e8616f79
Alicia García <alicia.garcia@liferay.com>
2022-06-22 09:22:40 +0200

    LPS-155899 if title is blank and locale is different from default locale, return title from default locale

--- modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/result/contributor/DLFileEntryModelSummaryContributor.java
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/search/spi/model/result/contributor/DLFileEntryModelSummaryContributor.java
@@ -18,6 +18,7 @@ import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.search.Document;
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.search.spi.model.result.contributor.ModelSummaryContributor;
 
@@ -39,6 +40,9 @@ public class DLFileEntryModelSummaryContributor
 	public Summary getSummary(
 		Document document, Locale locale, String snippet) {
 
+		Locale defaultLocale = LocaleUtil.fromLanguageId(
+			document.get("defaultLanguageId"));
+
 		String prefix = Field.SNIPPET + StringPool.UNDERLINE;
 
 		String content = document.get(
@@ -50,6 +54,11 @@ public class DLFileEntryModelSummaryContributor
 		}
 
 		String title = document.get(locale, prefix + Field.TITLE, Field.TITLE);
+
+		if (Validator.isNull(title) && !locale.equals(defaultLocale)) {
+			title = document.get(
+				defaultLocale, prefix + Field.TITLE, Field.TITLE);
+		}
 
 		Summary summary = new Summary(title, content);
 
