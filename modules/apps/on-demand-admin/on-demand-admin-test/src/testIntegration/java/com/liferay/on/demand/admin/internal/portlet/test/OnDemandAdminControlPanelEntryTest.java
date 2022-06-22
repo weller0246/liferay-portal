c6de785b56d64ff4f4b50e2dc1bd950d269649d9
pei-jung <pei-jung.lan@liferay.com>
2022-06-22 14:20:59 -0700

    LPS-156154 Add test cases to assert the following cases:
    1. a non-admin user with the granted permissions in the default instance can access the portlet
    2. a non-admin user with the granted permissions in a non-default instance can not access the portlet

--- modules/apps/on-demand-admin/on-demand-admin-test/src/testIntegration/java/com/liferay/on/demand/admin/internal/portlet/test/OnDemandAdminControlPanelEntryTest.java
+++ modules/apps/on-demand-admin/on-demand-admin-test/src/testIntegration/java/com/liferay/on/demand/admin/internal/portlet/test/OnDemandAdminControlPanelEntryTest.java
@@ -17,12 +17,23 @@ package com.liferay.on.demand.admin.internal.portlet.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.on.demand.admin.constants.OnDemandAdminPortletKeys;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.role.RoleConstants;
 import com.liferay.portal.kernel.portlet.ControlPanelEntry;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.RoleTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.test.rule.Inject;
@@ -52,6 +63,32 @@ public class OnDemandAdminControlPanelEntryTest {
 				_permissionCheckerFactory.create(TestPropsValues.getUser()),
 				null, _onDemandAdminPortlet));
 
+		Group defaultCompanyGroup = _groupLocalService.getGroup(
+			TestPropsValues.getCompanyId(), GroupConstants.CONTROL_PANEL);
+		User defaultCompanyUser = UserTestUtil.addUser();
+
+		Assert.assertFalse(
+			_onDemandAdminControlPanelEntry.hasAccessPermission(
+				_permissionCheckerFactory.create(defaultCompanyUser),
+				defaultCompanyGroup, _onDemandAdminPortlet));
+
+		Role defaultCompanyRole = RoleTestUtil.addRole(
+			RoleConstants.TYPE_REGULAR);
+
+		RoleTestUtil.addResourcePermission(
+			defaultCompanyRole, OnDemandAdminPortletKeys.ON_DEMAND_ADMIN,
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(TestPropsValues.getCompanyId()),
+			ActionKeys.ACCESS_IN_CONTROL_PANEL);
+
+		_userLocalService.addRoleUser(
+			defaultCompanyRole.getRoleId(), defaultCompanyUser);
+
+		Assert.assertTrue(
+			_onDemandAdminControlPanelEntry.hasAccessPermission(
+				_permissionCheckerFactory.create(defaultCompanyUser),
+				defaultCompanyGroup, _onDemandAdminPortlet));
+
 		Company company = CompanyTestUtil.addCompany();
 
 		User companyAdminUser = UserTestUtil.addCompanyAdminUser(company);
@@ -60,7 +97,37 @@ public class OnDemandAdminControlPanelEntryTest {
 			_onDemandAdminControlPanelEntry.hasAccessPermission(
 				_permissionCheckerFactory.create(companyAdminUser), null,
 				_onDemandAdminPortlet));
+
+		Group group = _groupLocalService.getGroup(
+			company.getCompanyId(), GroupConstants.CONTROL_PANEL);
+		User user = UserTestUtil.addUser(company);
+
+		Assert.assertFalse(
+			_onDemandAdminControlPanelEntry.hasAccessPermission(
+				_permissionCheckerFactory.create(user), group,
+				_onDemandAdminPortlet));
+
+		Role role = _roleLocalService.addRole(
+			companyAdminUser.getUserId(), null, 0,
+			RandomTestUtil.randomString(), null, null,
+			RoleConstants.TYPE_REGULAR, null, null);
+
+		RoleTestUtil.addResourcePermission(
+			role, OnDemandAdminPortletKeys.ON_DEMAND_ADMIN,
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(company.getCompanyId()),
+			ActionKeys.ACCESS_IN_CONTROL_PANEL);
+
+		_userLocalService.addRoleUser(role.getRoleId(), user);
+
+		Assert.assertFalse(
+			_onDemandAdminControlPanelEntry.hasAccessPermission(
+				_permissionCheckerFactory.create(user), group,
+				_onDemandAdminPortlet));
 	}
+
+	@Inject
+	private GroupLocalService _groupLocalService;
 
 	@Inject(
 		filter = "javax.portlet.name=" + OnDemandAdminPortletKeys.ON_DEMAND_ADMIN
@@ -74,5 +141,11 @@ public class OnDemandAdminControlPanelEntryTest {
 
 	@Inject
 	private PermissionCheckerFactory _permissionCheckerFactory;
+
+	@Inject
+	private RoleLocalService _roleLocalService;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 }
