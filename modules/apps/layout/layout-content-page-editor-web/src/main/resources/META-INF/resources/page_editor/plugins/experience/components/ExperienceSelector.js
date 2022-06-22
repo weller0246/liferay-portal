e410e9b0c2f399f199ea8187186a0c8c02071fef
Alan Huang <alan.huang@liferay.com>
2022-06-22 14:47:36 +0800

    LPS-152539 Auto SF

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/components/ExperienceSelector.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/plugins/experience/components/ExperienceSelector.js
@@ -470,9 +470,7 @@ const ExperiencesSelectorHeader = ({
 						)}
 					</strong>
 
-					{Liferay.Language.get(
-						'to-enable,-go-to-instance-settings'
-					)}
+					{Liferay.Language.get('to-enable,-go-to-instance-settings')}
 				</ClayAlert>
 			) : (
 				<ClayAlert
