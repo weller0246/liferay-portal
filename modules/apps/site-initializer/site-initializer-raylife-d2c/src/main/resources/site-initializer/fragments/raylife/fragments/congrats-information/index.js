e956bed7ce9c63237c8d6a90b13cfe470b6187c8
Joana Lima <joana.lima@liferay.com>
2022-06-23 10:24:18 -0300

    LPS-156525 SF

--- modules/apps/site-initializer/site-initializer-raylife-d2c/src/main/resources/site-initializer/fragments/raylife/fragments/congrats-information/index.js
+++ modules/apps/site-initializer/site-initializer-raylife-d2c/src/main/resources/site-initializer/fragments/raylife/fragments/congrats-information/index.js
@@ -172,7 +172,8 @@ const main = async () => {
 			fragmentElement.querySelector('#congrats-price'),
 			`$${Number(quoteComparison.price || 0).toLocaleString('en-US')}`
 		);
-	} else {
+	}
+	else {
 		const discountInPrice = quoteComparison.price * 0.05;
 
 		const discountDescription = `You saved 5% ($${Number(
