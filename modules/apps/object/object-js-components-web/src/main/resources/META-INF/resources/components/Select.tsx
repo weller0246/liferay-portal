e1a77681762d0bf8aa38f092fa9a9d3f81ddeeb8
Nara Andrade <nara.andrade@liferay.com>
2022-06-28 16:36:50 -0300

    LPS-157017 Compare value with index instead of label

--- modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/Select.tsx
+++ modules/apps/object/object-js-components-web/src/main/resources/META-INF/resources/components/Select.tsx
@@ -68,7 +68,7 @@ export function Select({
 					<ClaySelect.Option
 						key={index}
 						label={label}
-						selected={value === label}
+						selected={value === index}
 						value={index}
 					/>
 				))}
