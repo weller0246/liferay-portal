bc15bfdb7bd77c768ce0debd21d96f281f6a7a1a
Javier de Arcos <javier.dearcos@liferay.com>
2022-06-29 09:59:03 +0200

    LPS-153319 Create new interface ExtensionProvider to add extensions to existing entities
    Co-authored-by: Carlos Correa <carlos.correa@liferay.com>

---
+++ modules/apps/portal-vulcan/portal-vulcan-api/src/main/java/com/liferay/portal/vulcan/extension/ExtensionProvider.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.vulcan.extension;
+
+import java.io.Serializable;
+
+import java.util.Collection;
+import java.util.Map;
+
+/**
+ * @author Javier de Arcos
+ */
+public interface ExtensionProvider {
+
+	public Map<String, Serializable> getExtendedProperties(
+			long companyId, Object entity)
+		throws Exception;
+
+	public Collection<String> getFilteredPropertyNames(
+		long companyId, Object entity);
+
+	public boolean isExtensionFor(long companyId, String className);
+
+	public void setExtendedProperties(
+			long companyId, Object entity,
+			Map<String, Serializable> extensionProviderExtendedProperties)
+		throws Exception;
+
+}
