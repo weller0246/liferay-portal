baf73b9f77bbcfe9f50b249ce1dae2484038e676
Brian Chan <brian.chan@liferay.com>
2022-06-28 21:39:24 -0700

    LCD-14229 Opt for new dirs

--- workspaces/sample-lxc-sm-workspace/lxc/extensions/able-remote-app/src/routes/hello-bar/pages/HelloBar.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelloBar = () => (
-	<div className="hello-bar">
-		<h1>Hello Bar</h1>
-	</div>
-);
-
-export default HelloBar;
--- workspaces/sample-lxc-workspace/lxc/extensions/able-remote-app/src/routes/hello-foo/pages/HelloFoo.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const HelloFoo = () => (
-	<div className="hello-foo">
-		<h1>Hello Foo</h1>
-	</div>
-);
-
-export default HelloFoo;
