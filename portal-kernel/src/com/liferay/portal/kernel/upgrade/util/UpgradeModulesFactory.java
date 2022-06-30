48068af64ff3e5e6c9b89970a9185fb2263e9d77
arthurchan35 <arthurchan35@gmail.com>
2022-06-30 14:18:25 +0800

    LPS-157265 Do not use bean

--- portal-kernel/src/com/liferay/portal/kernel/upgrade/util/UpgradeModulesFactory.java
+++ portal-kernel/src/com/liferay/portal/kernel/upgrade/util/UpgradeModulesFactory.java
@@ -17,9 +17,34 @@ package com.liferay.portal.kernel.upgrade.util;
 /**
  * @author Arthur Chan
  */
-public interface UpgradeModulesFactory {
+public class UpgradeModulesFactory {
 
-	public UpgradeModules create(
-		String[] bundleSymbolicNames, String[][] convertedLegacyModules);
+	public static UpgradeModules create(
+		String[] bundleSymbolicNames, String[][] convertedLegacyModules) {
+
+		return new UpgradeModules() {
+
+			@Override
+			public String[] getBundleSymbolicNames() {
+				if (bundleSymbolicNames == null) {
+					return new String[0];
+				}
+
+				return bundleSymbolicNames;
+			}
+
+			@Override
+			public String[][] getConvertedLegacyModules() {
+				if ((convertedLegacyModules == null) ||
+					(convertedLegacyModules[0] == null)) {
+
+					return new String[0][0];
+				}
+
+				return convertedLegacyModules;
+			}
+
+		};
+	}
 
 }
