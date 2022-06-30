dbe2921e50e570561f259f5dd2b2a8102d4a42ee
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-06-30 10:56:50 +0200

    LPS-157565 Add an util class to access to the configured maximum file size for guest users

---
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/util/ObjectConfigurationUtil.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.web.internal.util;
+
+import com.liferay.object.configuration.ObjectConfiguration;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@Component(
+	configurationPid = "com.liferay.object.configuration.ObjectConfiguration",
+	immediate = true, service = {}
+)
+public class ObjectConfigurationUtil {
+
+	public static int maximumFileSizeForGuestUsers() {
+		return _objectConfiguration.maximumFileSizeForGuestUsers();
+	}
+
+	@Activate
+	@Modified
+	protected void activate(Map<String, Object> properties) {
+		_objectConfiguration = ConfigurableUtil.createConfigurable(
+			ObjectConfiguration.class, properties);
+	}
+
+	private static volatile ObjectConfiguration _objectConfiguration;
+
+}
