740b8a0d73fc7c9a528b4761a11ff0d000ba2536
Jorge Díaz <jorge.diaz@liferay.com>
2022-07-03 20:40:24 +0200

    LPS-157466 If the extend session returns status==500 that means the session couldn't be extended because it was expired in the server side, we have to execute expire logic to display a warning to the user and stop extending the session

--- modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
+++ modules/apps/frontend-js/frontend-js-aui-web/src/main/resources/META-INF/resources/liferay/session.js
@@ -103,7 +103,13 @@ AUI.add(
 					instance.set('timestamp');
 
 					if (event.src === SRC) {
-						Liferay.Util.fetch(URL_BASE + 'extend_session');
+						Liferay.Util.fetch(URL_BASE + 'extend_session').then(
+							(response) => {
+								if (response.status === 500) {
+									instance.expire();
+								}
+							}
+						);
 					}
 				},
 
