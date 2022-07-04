276118946e2b2e9610f2b378319caa771d42fed9
Clarice Brasil <clarice.brasil@liferay.com>
2022-07-03 22:37:57 -0300

    LPS-155364 Fix SLA Card hooks and utils

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SlaCardList/hooks/useSlaCards.js
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {useMemo} from 'react';
+import {SLA_CARD_NAMES} from '../../../../../common/utils/constants/slaCardNames';
+import getSlaCard from '../utils/getSlaCard';
+
+const useSlaCards = (koroneikiAccount) =>
+	useMemo(() => {
+		const {
+			slaCurrent,
+			slaCurrentEndDate,
+			slaCurrentStartDate,
+			slaExpired,
+			slaExpiredEndDate,
+			slaExpiredStartDate,
+			slaFuture,
+			slaFutureEndDate,
+			slaFutureStartDate,
+		} = koroneikiAccount;
+
+		const slaCards = [];
+
+		if (slaCurrent) {
+			slaCards.push(
+				getSlaCard(
+					slaCurrent === slaFuture
+						? slaFutureEndDate
+						: slaCurrentEndDate,
+					slaCurrent === slaExpired
+						? slaExpiredStartDate
+						: slaCurrentStartDate,
+					slaCurrent.split(' ')[0],
+					SLA_CARD_NAMES.current
+				)
+			);
+		}
+
+		if (!!slaExpired && slaExpired !== slaCurrent) {
+			slaCards.push(
+				getSlaCard(
+					slaExpiredEndDate,
+					slaExpiredStartDate,
+					slaExpired,
+					SLA_CARD_NAMES.expired
+				)
+			);
+		}
+
+		if (!!slaFuture && slaFuture !== slaCurrent) {
+			slaCards.push(
+				getSlaCard(
+					slaFutureEndDate,
+					slaFutureStartDate,
+					slaFuture,
+					SLA_CARD_NAMES.future
+				)
+			);
+		}
+
+		return slaCards;
+	}, [koroneikiAccount]);
+
+export default useSlaCards;
---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SlaCardList/utils/getSlaCard.js
@@ -0,0 +1,22 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {FORMAT_DATE} from '../../../../../common/utils/constants/slaCardDate';
+import getDateCustomFormat from '../../../../../common/utils/getDateCustomFormat';
+
+export default function getSlaCard(endDate, startDate, title, label) {
+	return {
+		endDate: getDateCustomFormat(endDate, FORMAT_DATE, 'en-US'),
+		label,
+		startDate: getDateCustomFormat(startDate, FORMAT_DATE, 'en-US'),
+		title: title.split(' ')[0],
+	};
+}
