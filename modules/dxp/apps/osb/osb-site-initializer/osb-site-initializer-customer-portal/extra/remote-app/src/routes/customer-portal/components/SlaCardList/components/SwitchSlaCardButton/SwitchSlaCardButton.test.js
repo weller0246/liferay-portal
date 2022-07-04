c118f29bbeefff0d74fff8fa1412cab562aadb70
Clarice Brasil <clarice.brasil@liferay.com>
2022-07-03 22:35:50 -0300

    LPS-155364 Fix switch sla card button component

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/components/SlaCardList/components/SwitchSlaCardButton/SwitchSlaCardButton.test.js
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ('License'). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import React from 'react';
+import SwitchSlaCardButton from '.';
+
+describe('SwitchSlaCardButton', () => {
+	it('changes SlaCard when clicking the button', async () => {
+		const functionMock = jest.fn();
+		const user = userEvent.setup();
+
+		render(<SwitchSlaCardButton handleSlaCardClick={functionMock} />);
+
+		expect(screen.getByRole('button')).toBeInTheDocument();
+		await user.click(screen.getByRole('button'));
+		expect(functionMock).toHaveBeenCalled();
+	});
+});
