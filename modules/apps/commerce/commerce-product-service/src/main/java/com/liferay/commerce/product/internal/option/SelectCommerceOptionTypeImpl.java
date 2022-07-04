356b0a897bb3d900c9a56edc546caf3699774e26
Alessio Rendina <asilcomando@gmail.com>
2022-07-04 15:32:55 +0200

    COMMERCE-9384 Added all commerce option type implementations

---
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/option/SelectCommerceOptionTypeImpl.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.option;
+
+import com.liferay.commerce.product.option.CommerceOptionType;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"commerce.option.type.display.order:Integer=200",
+		"commerce.option.type.key=" + SelectCommerceOptionTypeImpl.KEY
+	},
+	service = CommerceOptionType.class
+)
+public class SelectCommerceOptionTypeImpl
+	extends BaseCommerceOptionTypeImpl implements CommerceOptionType {
+
+	public static final String KEY = "select";
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "select-from-list");
+	}
+
+	@Override
+	public void render(
+			long cpDefinitionOptionRelId, long cpDefinitionOptionValueRelId,
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+	}
+
+}
