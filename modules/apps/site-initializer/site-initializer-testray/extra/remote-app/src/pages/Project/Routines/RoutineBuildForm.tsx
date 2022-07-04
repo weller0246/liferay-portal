7ac35c5fcc5688ba8b2fa218d9444295a0540814
Keven Leone <keven.leone@liferay.com>
2022-07-04 17:13:15 -0300

    LPS-157548 Add SelectCase for RoutineBuildForm

--- modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/RoutineBuildForm.tsx
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/RoutineBuildForm.tsx
@@ -15,12 +15,14 @@
 import {useQuery} from '@apollo/client';
 import ClayButton from '@clayui/button';
 import ClayForm, {ClayCheckbox} from '@clayui/form';
+import {useState} from 'react';
 import {useForm} from 'react-hook-form';
 
 import Form from '../../../components/Form';
 import Container from '../../../components/Layout/Container';
 import {
 	CTypePagination,
+	TestrayCase,
 	TestrayRoutine,
 	getRoutines,
 } from '../../../graphql/queries';
@@ -28,9 +30,14 @@ import {
 	TestrayProductVersion,
 	getProductVersions,
 } from '../../../graphql/queries/testrayProductVersion';
+import {useHeader} from '../../../hooks';
 import useFormActions from '../../../hooks/useFormActions';
+import useFormModal from '../../../hooks/useFormModal';
 import i18n from '../../../i18n';
 import yupSchema, {yupResolver} from '../../../schema/yup';
+import {searchUtil} from '../../../util/search';
+import {CaseListView} from '../Cases';
+import SuiteFormSelectModal from '../Suites/modal';
 
 type RoutineBuildData = {
 	description: string;
@@ -42,6 +49,8 @@ type RoutineBuildData = {
 };
 
 const RoutineBuildForm = () => {
+	const [cases, setCases] = useState<number[]>([]);
+
 	const {data: routinesData} = useQuery<
 		CTypePagination<'routines', TestrayRoutine>
 	>(getRoutines);
@@ -53,9 +62,16 @@ const RoutineBuildForm = () => {
 	const routines = routinesData?.c.routines.items || [];
 	const productVersions = productVersionsData?.c.productVersions.items || [];
 
+	useHeader({timeout: 20, useTabs: []});
+
 	const {
 		form: {onClose},
 	} = useFormActions();
+
+	const {modal} = useFormModal({
+		onSave: (newCases) =>
+			setCases((prevCases) => [...new Set([...prevCases, ...newCases])]),
+	});
 
 	const {
 		formState: {errors},
@@ -80,6 +96,12 @@ const RoutineBuildForm = () => {
 	return (
 		<Container className="container">
 			<ClayForm className="container pt-2">
+				<ClayCheckbox
+					checked={template}
+					label={i18n.translate('template')}
+					onChange={() => setValue('template', !template)}
+				/>
+
 				<Form.Input
 					{...inputProps}
 					label={i18n.translate('name')}
@@ -121,35 +143,83 @@ const RoutineBuildForm = () => {
 					type="textarea"
 				/>
 
-				<ClayCheckbox
-					checked={template}
-					label={i18n.translate('template')}
-					onChange={() => setValue('template', !template)}
-				/>
+				<h3>{i18n.translate('runs')}</h3>
+
+				<Form.Divider />
 
 				<ClayButton.Group className="mb-4">
-					<ClayButton displayType="secondary">
-						{i18n.translate('select-cases')}
+					<ClayButton
+						displayType="secondary"
+						onClick={() => modal.open()}
+					>
+						{i18n.translate('add-option')}
 					</ClayButton>
 
-					<ClayButton className="ml-3" displayType="secondary">
-						{i18n.translate('select-case-parameters')}
+					<ClayButton className="ml-1" displayType="secondary">
+						{i18n.translate('select-stacks')}
 					</ClayButton>
 				</ClayButton.Group>
 
-				<div>
-					<ClayButton.Group spaced>
-						<ClayButton
-							displayType="secondary"
-							onClick={() => onClose()}
-						>
-							{i18n.translate('close')}
-						</ClayButton>
+				<SuiteFormSelectModal modal={modal} type="select-cases" />
 
-						<ClayButton displayType="primary">
-							{i18n.translate('save')}
-						</ClayButton>
-					</ClayButton.Group>
+				<h3>{i18n.translate('cases')}</h3>
+
+				<Form.Divider />
+
+				<ClayButton.Group className="mb-4">
+					<ClayButton
+						displayType="secondary"
+						onClick={() => modal.open()}
+					>
+						{i18n.translate('add-cases')}
+					</ClayButton>
+
+					<ClayButton className="ml-1" displayType="secondary">
+						{i18n.translate('add-suites')}
+					</ClayButton>
+				</ClayButton.Group>
+
+				{!!cases.length && (
+					<CaseListView
+						listViewProps={{
+							managementToolbarProps: {visible: false},
+							tableProps: {
+								actions: [
+									{
+										action: ({id}: TestrayCase) =>
+											setCases((prevCases) =>
+												prevCases.filter(
+													(prevCase: number) =>
+														prevCase !== id
+												)
+											),
+										name: i18n.translate('delete'),
+									},
+								],
+								columns: [
+									{
+										key: 'priority',
+										value: i18n.translate('priority'),
+									},
+									{
+										key: 'component',
+										render: (component) => component.name,
+										value: i18n.translate('component'),
+									},
+									{
+										key: 'name',
+										size: 'md',
+										value: i18n.translate('name'),
+									},
+								],
+							},
+							variables: {filter: searchUtil.in('id', cases)},
+						}}
+					/>
+				)}
+
+				<div className="mt-4">
+					<Form.Footer onClose={onClose} onSubmit={() => null} />
 				</div>
 			</ClayForm>
 		</Container>
