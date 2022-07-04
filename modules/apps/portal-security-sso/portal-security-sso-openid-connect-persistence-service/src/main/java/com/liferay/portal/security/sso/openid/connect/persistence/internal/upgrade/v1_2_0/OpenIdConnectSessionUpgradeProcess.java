a255f392ca4d23e3b2b435c5888f15212ded96f2
arthurchan35 <arthurchan35@gmail.com>
2022-07-04 13:40:38 +0800

    LPS-155600 Upgrade process for OIDC session

---
+++ modules/apps/portal-security-sso/portal-security-sso-openid-connect-persistence-service/src/main/java/com/liferay/portal/security/sso/openid/connect/persistence/internal/upgrade/v1_2_0/OpenIdConnectSessionUpgradeProcess.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.security.sso.openid.connect.persistence.internal.upgrade.v1_2_0;
+
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.Base64;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.net.URI;
+
+import java.security.MessageDigest;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import java.util.Dictionary;
+
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+
+/**
+ * @author Arthur Chan
+ */
+public class OpenIdConnectSessionUpgradeProcess extends UpgradeProcess {
+
+	public OpenIdConnectSessionUpgradeProcess(
+		ConfigurationAdmin configurationAdmin) {
+
+		_configurationAdmin = configurationAdmin;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		alterTableAddColumn(
+			"OpenIdConnectSession", "authServerWellKnownURI",
+			"VARCHAR(256) null");
+		alterTableAddColumn(
+			"OpenIdConnectSession", "clientId", "VARCHAR(256) null");
+
+		alterTableDropColumn("OpenIdConnectSession", "providerName");
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"select openIdConnectSessionId, configurationPid from " +
+					"OpenIdConnectSession");
+			ResultSet resultSet = preparedStatement.executeQuery()) {
+
+			while (resultSet.next()) {
+				long openIdConnectSessionId = resultSet.getLong(
+					"openIdConnectSessionId");
+
+				String configurationPid = resultSet.getString(
+					"configurationPid");
+
+				try {
+					_upgradeOpenIdConnectSession(
+						openIdConnectSessionId, configurationPid);
+				}
+				catch (Exception exception) {
+					if (_log.isDebugEnabled()) {
+						_log.debug(exception);
+					}
+
+					runSQL(
+						"delete from OpenIdConnectSession where " +
+							"openIdConnectSessionId = " +
+								openIdConnectSessionId);
+				}
+			}
+		}
+
+		alterTableDropColumn("OpenIdConnectSession", "configurationPid");
+	}
+
+	private String _generateLocalWellKnownURI(
+			String issuer, String tokenEndPoint)
+		throws Exception {
+
+		MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+
+		URI issuerURI = URI.create(issuer);
+
+		return StringBundler.concat(
+			issuerURI.getScheme(), "://", issuerURI.getAuthority(),
+			"/.well-known/openid-configuration", issuerURI.getPath(), '/',
+			Base64.encodeToURL(messageDigest.digest(tokenEndPoint.getBytes())),
+			"/local");
+	}
+
+	private void _upgradeOpenIdConnectSession(
+			long openIdConnectSessionId, String configurationPid)
+		throws Exception {
+
+		Configuration configuration = _configurationAdmin.getConfiguration(
+			configurationPid, "?");
+
+		Dictionary<String, ?> properties = configuration.getProperties();
+
+		String openIdConnectClientId = GetterUtil.getString(
+			properties.get("openIdConnectClientId"));
+
+		String discoveryEndPoint = GetterUtil.getString(
+			properties.get("discoveryEndPoint"));
+
+		if (Validator.isNull(discoveryEndPoint)) {
+			discoveryEndPoint = _generateLocalWellKnownURI(
+				GetterUtil.getString(properties.get("issuerURL")),
+				GetterUtil.getString(properties.get("tokenEndPoint")));
+		}
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"update OpenIdConnectSession set authServerWellKnownURI = ?, " +
+					"clientId = ? WHERE openIdConnectSessionId = ?")) {
+
+			preparedStatement.setString(1, discoveryEndPoint);
+			preparedStatement.setString(2, openIdConnectClientId);
+			preparedStatement.setLong(3, openIdConnectSessionId);
+
+			preparedStatement.execute();
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		OpenIdConnectSessionUpgradeProcess.class);
+
+	private final ConfigurationAdmin _configurationAdmin;
+
+}
