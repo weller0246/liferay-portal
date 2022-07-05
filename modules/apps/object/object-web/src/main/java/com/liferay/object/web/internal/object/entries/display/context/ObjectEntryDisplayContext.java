bf2d527ee1f86fa4ca72808fb70de214365b248a
Thien Quach <thien.quach@codeenginestudio.com>
2022-07-05 17:55:37 +0700

    LPS-157244: Decimal numbers auto increment in the decimal fields after save

--- modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContext.java
+++ modules/apps/object/object-web/src/main/java/com/liferay/object/web/internal/object/entries/display/context/ObjectEntryDisplayContext.java
@@ -29,6 +29,7 @@ import com.liferay.dynamic.data.mapping.model.LocalizedValue;
 import com.liferay.dynamic.data.mapping.model.UnlocalizedValue;
 import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
 import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.util.NumericDDMFormFieldUtil;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenuBuilder;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.NavigationItem;
@@ -97,6 +98,8 @@ import com.liferay.portal.vulcan.dto.converter.DTOConverterContext;
 import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.util.TransformUtil;
 import com.liferay.taglib.servlet.PipingServletResponseFactory;
+
+import java.text.DecimalFormat;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -1054,6 +1057,14 @@ public class ObjectEntryDisplayContext {
 				new UnlocalizedValue(listEntry.getKey()));
 		}
 		else {
+			if (value instanceof Double) {
+				DecimalFormat defaultDecimalFormat =
+					NumericDDMFormFieldUtil.getDecimalFormat(
+						_objectRequestHelper.getLocale());
+
+				value = defaultDecimalFormat.format(value);
+			}
+
 			ddmFormFieldValue.setValue(
 				new UnlocalizedValue(String.valueOf(value)));
 		}
