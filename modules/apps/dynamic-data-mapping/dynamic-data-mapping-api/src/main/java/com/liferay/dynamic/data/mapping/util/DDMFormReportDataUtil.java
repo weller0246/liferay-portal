036daf7ca648977a91e3dcb864d45096e386c469
Mirna Gama <mirnabeatrizgamav@gmail.com>
2022-07-06 11:17:49 -0300

    LPS-157455 Get the user portal language every time the form report is displayed

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormReportDataUtil.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-api/src/main/java/com/liferay/dynamic/data/mapping/util/DDMFormReportDataUtil.java
@@ -35,6 +35,7 @@ import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.LocaleThreadLocal;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Time;
@@ -255,6 +256,13 @@ public class DDMFormReportDataUtil {
 	}
 
 	private static String _getValue(Value value) {
+		Locale portalUserLanguage = LocaleThreadLocal.getThemeDisplayLocale();
+		Map<Locale, String> languages = value.getValues();
+
+		if (languages.containsKey(portalUserLanguage)) {
+			return value.getString(portalUserLanguage);
+		}
+
 		return value.getString(value.getDefaultLocale());
 	}
 
