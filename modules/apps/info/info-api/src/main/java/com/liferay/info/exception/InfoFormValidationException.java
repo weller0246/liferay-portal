a45134d26e134b47962a266b3b806ce18ef64c98
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-07-06 09:18:59 +0200

    LPS-157683 Adapt usages

--- modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormValidationException.java
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoFormValidationException.java
@@ -100,13 +100,14 @@ public class InfoFormValidationException extends InfoFormException {
 
 		@Override
 		public String getLocalizedMessage(Locale locale) {
-			return LanguageUtil.get(locale, "value-exceeds-minimum-value-of-x");
+			return LanguageUtil.get(
+				locale, "value-falls-bellow-the-minimum-value-of-x");
 		}
 
 		@Override
 		public String getLocalizedMessage(String fieldLabel, Locale locale) {
 			return LanguageUtil.format(
-				locale, "value-exceeds-minimum-value-of-x-for-field-x",
+				locale, "value-falls-bellow-the-minimum-value-of-x-for-field-x",
 				new String[] {String.valueOf(_minValue), fieldLabel}, false);
 		}
 
