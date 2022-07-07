fe2a9ef8e408c61353ac92975d1f747933827e85
Keven Leone <keven.leone@liferay.com>
2022-07-07 19:02:16 -0300

    LPS-157855 Remove RoutineBuildModal since no longer used

--- modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/Routine.tsx
+++ modules/apps/site-initializer/site-initializer-testray/extra/remote-app/src/pages/Project/Routines/Routine.tsx
@@ -26,11 +26,10 @@ import {filters} from '../../../schema/filter';
 import {BUILD_STATUS} from '../../../util/constants';
 import dayjs from '../../../util/date';
 import {searchUtil} from '../../../util/search';
-import RoutineBuildModal from './RoutineBuildModal';
 import useRoutineActions from './useRoutineActions';
 
 const Routine = () => {
-	const {actionsRoutine, formModal} = useRoutineActions();
+	const {actionsBuild, formModal} = useRoutineActions();
 	const {barChart, colors} = useBuildHistory();
 	const {routineId} = useParams();
 	const navigate = useNavigate();
@@ -48,13 +47,13 @@ const Routine = () => {
 					},
 				}}
 				managementToolbarProps={{
-					addButton: () => navigate('update'),
+					addButton: () => navigate('create'),
 					filterFields: filters.build.index as any,
 					title: i18n.translate('build-history'),
 				}}
 				query={getBuilds}
 				tableProps={{
-					actions: actionsRoutine,
+					actions: actionsBuild,
 					columns: [
 						{
 							key: 'status',
@@ -242,8 +241,6 @@ const Routine = () => {
 					</div>
 				)}
 			</ListView>
-
-			<RoutineBuildModal modal={formModal.modal} />
 		</Container>
 	);
 };
