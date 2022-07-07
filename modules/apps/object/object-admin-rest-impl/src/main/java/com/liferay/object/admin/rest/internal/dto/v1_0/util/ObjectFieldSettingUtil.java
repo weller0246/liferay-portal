d7f5394f22899c702e9c5051495cab99ce2aba34
marcelabc <marcela.cunha@liferay.com>
2022-07-07 20:57:01 -0300

    LPS-157168 IF/ELSE because value type is different when it called by the client or by the restAPI

--- modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectFieldSettingUtil.java
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/dto/v1_0/util/ObjectFieldSettingUtil.java
@@ -78,21 +78,32 @@ public class ObjectFieldSettingUtil {
 
 			List<ObjectFilter> objectFilters = new ArrayList<>();
 
-			for (Map<String, Object> value :
-					(List<Map<String, Object>>)objectFieldSetting.getValue()) {
+			List<Object> values;
+
+			if (objectFieldSetting.getValue() instanceof Object[]) {
+				values = ListUtil.fromArray(
+					(Object[])objectFieldSetting.getValue());
+			}
+			else {
+				values = (List<Object>)objectFieldSetting.getValue();
+			}
+
+			for (Object value : values) {
+				Map<String, Object> valueMap = (Map<String, Object>)value;
 
 				ObjectFilter objectFilter =
 					objectFilterLocalService.createObjectFilter(0L);
 
 				objectFilter.setFilterBy(
-					String.valueOf(value.get(ObjectFilterConstants.FILTER_BY)));
+					String.valueOf(
+						valueMap.get(ObjectFilterConstants.FILTER_BY)));
 				objectFilter.setFilterType(
 					String.valueOf(
-						value.get(ObjectFilterConstants.FILTER_TYPE)));
+						valueMap.get(ObjectFilterConstants.FILTER_TYPE)));
 				objectFilter.setJson(
 					String.valueOf(
 						JSONFactoryUtil.createJSONObject(
-							(Map)value.get(ObjectFilterConstants.JSON))));
+							(Map)valueMap.get(ObjectFilterConstants.JSON))));
 
 				objectFilters.add(objectFilter);
 			}
