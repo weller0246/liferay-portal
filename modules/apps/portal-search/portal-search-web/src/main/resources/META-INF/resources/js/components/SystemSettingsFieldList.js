c16492d590310ba66cb6a71c49c447c14fcfe891
Petteri Karttunen <petteri.karttunen@liferay.com>
2022-07-08 16:36:06 +0300

    LPS-155158 portal-search-web: Create custom system settings field list component
    Follows the look of other system settings with add/remove fields

---
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SystemSettingsFieldList.js
@@ -0,0 +1,108 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {ClayButtonWithIcon} from '@clayui/button';
+import ClayForm from '@clayui/form';
+import React, {useState} from 'react';
+
+const ADD_DELETE_BUTTON_STYLING = {
+	borderRadius: '10px',
+	height: '18px',
+	padding: '2px',
+	position: 'absolute',
+	width: '18px',
+};
+
+function escapeCommas(value) {
+	return value.replaceAll(',', '\\,');
+}
+
+function SystemSettingsFieldList({
+	fieldLabel,
+	fieldName,
+	initialValue,
+	namespace,
+}) {
+	const [value, setValue] = useState(initialValue || []);
+
+	const _handleAddItem = (index) => () => {
+		setValue([...value.slice(0, index + 1), '', ...value.slice(index + 1)]);
+	};
+
+	const _handleRemoveItem = (index) => () => {
+		setValue(value.filter((item, itemIndex) => index !== itemIndex));
+	};
+
+	const _handleValueChange = (index) => (event) => {
+		const newValue = value.map((item, itemIndex) => {
+			if (index === itemIndex) {
+				return event.target.value;
+			}
+
+			return item;
+		});
+
+		setValue(newValue);
+	};
+
+	return (
+		<div className="system-settings-field-list-container">
+			{value.map((item, index) => (
+				<React.Fragment key={index}>
+					<ClayForm.Group>
+						{index > 0 && (
+							<ClayButtonWithIcon
+								className="system-settings-field-list-delete-button"
+								onClick={_handleRemoveItem(index)}
+								style={{
+									...ADD_DELETE_BUTTON_STYLING,
+									right: '30px',
+								}}
+								symbol="hr"
+								title={Liferay.Language.get('remove')}
+							/>
+						)}
+
+						<ClayButtonWithIcon
+							className="system-settings-field-list-add-button"
+							onClick={_handleAddItem(index)}
+							style={{...ADD_DELETE_BUTTON_STYLING, right: '8px'}}
+							symbol="plus"
+							title={Liferay.Language.get('duplicate')}
+						/>
+
+						<label htmlFor={`${fieldName}${index}`}>
+							{fieldLabel}
+						</label>
+
+						<textarea
+							className="form-control"
+							id={`${fieldName}${index}`}
+							onChange={_handleValueChange(index)}
+							value={item}
+						/>
+					</ClayForm.Group>
+				</React.Fragment>
+			))}
+
+			<input
+				name={`${namespace}${fieldName}`}
+				type="hidden"
+				value={value.map((item) => escapeCommas(item)).join(',')}
+			/>
+		</div>
+	);
+}
+
+export default SystemSettingsFieldList;
---
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/search/bar/portlet_instance_configuration.jsp
@@ -0,0 +1,61 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+
+<%@ taglib uri="http://java.sun.com/portlet_2_0" prefix="portlet" %>
+
+<%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
+taglib uri="http://liferay.com/tld/react" prefix="react" %>
+
+<%@ page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
+page import="com.liferay.portal.kernel.util.HashMapBuilder" %><%@
+page import="com.liferay.portal.search.web.internal.search.bar.portlet.display.context.SearchBarPortletInstanceConfigurationDisplayContext" %>
+
+<portlet:defineObjects />
+
+<%
+SearchBarPortletInstanceConfigurationDisplayContext searchBarPortletInstanceConfigurationDisplayContext = (SearchBarPortletInstanceConfigurationDisplayContext)request.getAttribute(SearchBarPortletInstanceConfigurationDisplayContext.class.getName());
+%>
+
+<aui:input name="displayStyleGroupId" value="<%= searchBarPortletInstanceConfigurationDisplayContext.getDisplayStyleGroupId() %>" />
+
+<aui:input name="displayStyle" value="<%= searchBarPortletInstanceConfigurationDisplayContext.getDisplayStyle() %>" />
+
+<c:if test="<%= searchBarPortletInstanceConfigurationDisplayContext.isSuggestionsConfigurationVisible() %>">
+	<aui:input name="enableSuggestions" type="checkbox" value="<%= searchBarPortletInstanceConfigurationDisplayContext.isEnableSuggestions() %>" />
+
+	<div>
+		<span aria-hidden="true" class="loading-animation loading-animation-sm"></span>
+
+		<react:component
+			module="js/components/SystemSettingsFieldList"
+			props='<%=
+				HashMapBuilder.<String, Object>put(
+					"fieldLabel", LanguageUtil.get(request, "suggestions-contributor-configuration")
+				).put(
+					"fieldName", "suggestionsContributorConfiguration"
+				).put(
+					"initialValue", searchBarPortletInstanceConfigurationDisplayContext.getSuggestionsContributorConfigurations()
+				).put(
+					"namespace", liferayPortletResponse.getNamespace()
+				).build()
+			%>'
+		/>
+	</div>
+
+	<aui:input label="character-threshold-for-displaying-suggestions" name="suggestionsDisplayThreshold" value="<%= searchBarPortletInstanceConfigurationDisplayContext.getSuggestionsDisplayThreshold() %>" />
+</c:if>
