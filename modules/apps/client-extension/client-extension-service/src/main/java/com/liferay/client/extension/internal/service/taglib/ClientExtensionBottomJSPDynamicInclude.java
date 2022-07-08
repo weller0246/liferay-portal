a923b7169a26a31c2991ab79691fcd2d381af562
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-07-08 19:11:05 +0200

    LPS-157918 Moves common logic to a new class

--- modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/service/taglib/ClientExtensionBottomJSPDynamicInclude.java
+++ modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/service/taglib/ClientExtensionBottomJSPDynamicInclude.java
@@ -15,18 +15,13 @@
 package com.liferay.client.extension.internal.service.taglib;
 
 import com.liferay.client.extension.constants.ClientExtensionEntryConstants;
+import com.liferay.client.extension.internal.service.taglib.util.ClientExtensionDynamicIncludeUtil;
 import com.liferay.client.extension.model.ClientExtensionEntryRel;
-import com.liferay.client.extension.service.ClientExtensionEntryRelLocalService;
 import com.liferay.client.extension.type.GlobalJSCET;
 import com.liferay.client.extension.type.manager.CETManager;
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.dao.orm.QueryUtil;
-import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.LayoutSet;
-import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
-import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.kernel.util.Validator;
@@ -35,7 +30,6 @@ import com.liferay.portal.kernel.util.WebKeys;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -63,8 +57,13 @@ public class ClientExtensionBottomJSPDynamicInclude implements DynamicInclude {
 
 		PrintWriter printWriter = httpServletResponse.getWriter();
 
+		List<ClientExtensionEntryRel> clientExtensionEntryRels =
+			ClientExtensionDynamicIncludeUtil.getClientExtensionEntryRels(
+				themeDisplay.getLayout(),
+				ClientExtensionEntryConstants.TYPE_GLOBAL_JS);
+
 		for (ClientExtensionEntryRel clientExtensionEntryRel :
-				_getClientExtensionEntryRels(themeDisplay.getLayout())) {
+				clientExtensionEntryRels) {
 
 			GlobalJSCET globalJSCET = (GlobalJSCET)_cetManager.getCET(
 				clientExtensionEntryRel.getCompanyId(),
@@ -112,53 +111,7 @@ public class ClientExtensionBottomJSPDynamicInclude implements DynamicInclude {
 		dynamicIncludeRegistry.register("/html/common/themes/bottom.jsp#post");
 	}
 
-	private List<ClientExtensionEntryRel> _getClientExtensionEntryRels(
-		Layout layout) {
-
-		List<ClientExtensionEntryRel> clientExtensionEntryRels =
-			new ArrayList<>(
-				_clientExtensionEntryRelLocalService.
-					getClientExtensionEntryRels(
-						_portal.getClassNameId(Layout.class), layout.getPlid(),
-						ClientExtensionEntryConstants.TYPE_GLOBAL_JS,
-						QueryUtil.ALL_POS, QueryUtil.ALL_POS));
-
-		Layout masterLayout = _layoutLocalService.fetchLayout(
-			layout.getMasterLayoutPlid());
-
-		if (masterLayout != null) {
-			clientExtensionEntryRels.addAll(
-				_clientExtensionEntryRelLocalService.
-					getClientExtensionEntryRels(
-						_portal.getClassNameId(Layout.class),
-						masterLayout.getPlid(),
-						ClientExtensionEntryConstants.TYPE_GLOBAL_JS,
-						QueryUtil.ALL_POS, QueryUtil.ALL_POS));
-		}
-
-		LayoutSet layoutSet = layout.getLayoutSet();
-
-		clientExtensionEntryRels.addAll(
-			_clientExtensionEntryRelLocalService.getClientExtensionEntryRels(
-				_portal.getClassNameId(LayoutSet.class),
-				layoutSet.getLayoutSetId(),
-				ClientExtensionEntryConstants.TYPE_GLOBAL_JS, QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS));
-
-		return clientExtensionEntryRels;
-	}
-
 	@Reference
 	private CETManager _cetManager;
-
-	@Reference
-	private ClientExtensionEntryRelLocalService
-		_clientExtensionEntryRelLocalService;
-
-	@Reference
-	private LayoutLocalService _layoutLocalService;
-
-	@Reference
-	private Portal _portal;
 
 }
---
+++ modules/apps/client-extension/client-extension-service/src/main/java/com/liferay/client/extension/internal/service/taglib/util/ClientExtensionDynamicIncludeUtil.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.client.extension.internal.service.taglib.util;
+
+import com.liferay.client.extension.model.ClientExtensionEntryRel;
+import com.liferay.client.extension.service.ClientExtensionEntryRelLocalServiceUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class ClientExtensionDynamicIncludeUtil {
+
+	public static List<ClientExtensionEntryRel> getClientExtensionEntryRels(
+		Layout layout, String type) {
+
+		List<ClientExtensionEntryRel> clientExtensionEntryRels =
+			new ArrayList<>(
+				ClientExtensionEntryRelLocalServiceUtil.
+					getClientExtensionEntryRels(
+						PortalUtil.getClassNameId(Layout.class),
+						layout.getPlid(), type));
+
+		Layout masterLayout = LayoutLocalServiceUtil.fetchLayout(
+			layout.getMasterLayoutPlid());
+
+		if (masterLayout != null) {
+			clientExtensionEntryRels.addAll(
+				ClientExtensionEntryRelLocalServiceUtil.
+					getClientExtensionEntryRels(
+						PortalUtil.getClassNameId(Layout.class),
+						masterLayout.getPlid(), type));
+		}
+
+		LayoutSet layoutSet = layout.getLayoutSet();
+
+		clientExtensionEntryRels.addAll(
+			ClientExtensionEntryRelLocalServiceUtil.getClientExtensionEntryRels(
+				PortalUtil.getClassNameId(LayoutSet.class),
+				layoutSet.getLayoutSetId(), type));
+
+		return clientExtensionEntryRels;
+	}
+
+}
