8f5549da7ce21bbfcad0901b417753ca38157a23
Alberto Chaparro <alberto.chaparro@liferay.com>
2022-07-08 17:02:46 +0200

    LPS-157670 Some parts of these verifiers only need to run for 6.1

--- modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/verify/BookmarksServiceVerifyProcess.java
+++ modules/apps/bookmarks/bookmarks-service/src/main/java/com/liferay/bookmarks/internal/verify/BookmarksServiceVerifyProcess.java
@@ -14,13 +14,25 @@
 
 package com.liferay.bookmarks.internal.verify;
 
+import com.liferay.bookmarks.constants.BookmarksPortletKeys;
 import com.liferay.bookmarks.model.BookmarksFolder;
 import com.liferay.bookmarks.service.BookmarksFolderLocalService;
+import com.liferay.exportimport.kernel.staging.Staging;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LoggingTimer;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.verify.VerifyProcess;
 
 import java.util.List;
@@ -43,8 +55,57 @@ public class BookmarksServiceVerifyProcess extends VerifyProcess {
 
 	@Override
 	protected void doVerify() throws Exception {
+		if (GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-157670"))) {
+			updateStagedPortletNames();
+		}
+
 		_updateFolderAssets();
 		_verifyTree();
+	}
+
+	protected void updateStagedPortletNames() throws PortalException {
+		ActionableDynamicQuery groupActionableDynamicQuery =
+			_groupLocalService.getActionableDynamicQuery();
+
+		groupActionableDynamicQuery.setAddCriteriaMethod(
+			dynamicQuery -> {
+				Property siteProperty = PropertyFactoryUtil.forName("site");
+
+				dynamicQuery.add(siteProperty.eq(Boolean.TRUE));
+			});
+
+		groupActionableDynamicQuery.setPerformActionMethod(
+			(ActionableDynamicQuery.PerformActionMethod<Group>)group -> {
+				UnicodeProperties typeSettingsUnicodeProperties =
+					group.getTypeSettingsProperties();
+
+				if (typeSettingsUnicodeProperties == null) {
+					return;
+				}
+
+				String propertyKey = _staging.getStagedPortletId(
+					BookmarksPortletKeys.BOOKMARKS);
+
+				String propertyValue =
+					typeSettingsUnicodeProperties.getProperty(propertyKey);
+
+				if (Validator.isNull(propertyValue)) {
+					return;
+				}
+
+				typeSettingsUnicodeProperties.remove(propertyKey);
+
+				propertyKey = _staging.getStagedPortletId(
+					BookmarksPortletKeys.BOOKMARKS_ADMIN);
+
+				typeSettingsUnicodeProperties.put(propertyKey, propertyValue);
+
+				group.setTypeSettingsProperties(typeSettingsUnicodeProperties);
+
+				_groupLocalService.updateGroup(group);
+			});
+
+		groupActionableDynamicQuery.performActions();
 	}
 
 	private void _updateFolderAssets() throws Exception {
@@ -95,5 +156,11 @@ public class BookmarksServiceVerifyProcess extends VerifyProcess {
 
 	@Reference
 	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private Staging _staging;
 
 }
