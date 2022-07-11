4146ee1521d88e1ea041c6cda5cb076dfa61989b
Qi Zhang <qi.zhang@liferay.com>
2022-07-11 12:36:46 +0800

    LPS-157531 Add logic for enforce using PortalUtil.getPathProxy()

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPStaticResourceURLCheck.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.source.formatter.check.util.JavaSourceUtil;
+
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author Qi Zhang
+ */
+public class JSPStaticResourceURLCheck extends BaseFileCheck {
+
+	@Override
+	protected String doProcess(
+		String fileName, String absolutePath, String content) {
+
+		Matcher matcher = _staticResourceURLParamPattern.matcher(content);
+
+		while (matcher.find()) {
+			int x = matcher.start();
+
+			String expression = null;
+
+			while (true) {
+				x = content.indexOf(StringPool.CLOSE_PARENTHESIS, x);
+
+				if (x == -1) {
+					return content;
+				}
+
+				expression = content.substring(matcher.start(), x + 1);
+
+				int level = getLevel(
+					expression, StringPool.OPEN_PARENTHESIS,
+					StringPool.CLOSE_PARENTHESIS);
+
+				if (level == 0) {
+					break;
+				}
+
+				x++;
+			}
+
+			List<String> parameters = JavaSourceUtil.getParameterList(
+				expression);
+
+			if (ListUtil.isEmpty(parameters) || (parameters.size() < 2)) {
+				continue;
+			}
+
+			String secondParameter = parameters.get(1);
+
+			if (!secondParameter.contains("getContextPath()") ||
+				(secondParameter.contains("PortalUtil.getPathProxy()") &&
+				 secondParameter.contains("getContextPath()"))) {
+
+				continue;
+			}
+
+			int insertPos =
+				matcher.start() + expression.indexOf(secondParameter);
+
+			return StringUtil.insert(
+				content, "PortalUtil.getPathProxy() + ", insertPos);
+		}
+
+		return content;
+	}
+
+	private static final Pattern _staticResourceURLParamPattern =
+		Pattern.compile("PortalUtil.getStaticResourceURL\\(");
+
+}
