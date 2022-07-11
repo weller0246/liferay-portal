1851e6229e911e1f609346e0444bfe5b5dfc9b3e
Brian Chan <brian.chan@liferay.com>
2022-07-10 20:06:48 -0700

    Revert "LPS-157670 Some parts of these verifiers only need to run for 6.1"
    This reverts commit 996f03f110176cc7f362c6e235e50bf9f14f02d3.

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/verify/MessageBoardsServiceVerifyProcess.java
@@ -14,24 +14,10 @@
 
 package com.liferay.message.boards.internal.verify;
 
-import com.liferay.exportimport.kernel.staging.Staging;
-import com.liferay.message.boards.constants.MBPortletKeys;
 import com.liferay.message.boards.internal.verify.model.MBDiscussionVerifiableModel;
 import com.liferay.message.boards.internal.verify.model.MBThreadFlagVerifiableModel;
-import com.liferay.message.boards.internal.verify.model.MBThreadVerifiableModel;
-import com.liferay.portal.kernel.dao.orm.ActionableDynamicQuery;
-import com.liferay.portal.kernel.dao.orm.Property;
-import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
-import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Release;
-import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LoggingTimer;
-import com.liferay.portal.kernel.util.PropsUtil;
-import com.liferay.portal.kernel.util.UnicodeProperties;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.verify.VerifyAuditedModel;
 import com.liferay.portal.verify.VerifyGroupedModel;
 import com.liferay.portal.verify.VerifyProcess;
 
@@ -53,12 +39,7 @@ public class MessageBoardsServiceVerifyProcess extends VerifyProcess {
 
 	@Override
 	protected void doVerify() throws Exception {
-		if (GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-157670"))) {
-			updateStagedPortletNames();
-			verifyAuditedModels();
-		}
-
-		verifyGroupedModels();
+		_verifyGroupedModels();
 	}
 
 	@Reference(
@@ -68,75 +49,13 @@ public class MessageBoardsServiceVerifyProcess extends VerifyProcess {
 	protected void setRelease(Release release) {
 	}
 
-	protected void updateStagedPortletNames() throws PortalException {
-		ActionableDynamicQuery groupActionableDynamicQuery =
-			_groupLocalService.getActionableDynamicQuery();
-
-		groupActionableDynamicQuery.setAddCriteriaMethod(
-			dynamicQuery -> {
-				Property siteProperty = PropertyFactoryUtil.forName("site");
-
-				dynamicQuery.add(siteProperty.eq(Boolean.TRUE));
-			});
-		groupActionableDynamicQuery.setPerformActionMethod(
-			(ActionableDynamicQuery.PerformActionMethod<Group>)group -> {
-				UnicodeProperties typeSettingsUnicodeProperties =
-					group.getTypeSettingsProperties();
-
-				if (typeSettingsUnicodeProperties == null) {
-					return;
-				}
-
-				String propertyKey = _staging.getStagedPortletId(
-					MBPortletKeys.MESSAGE_BOARDS);
-
-				String propertyValue =
-					typeSettingsUnicodeProperties.getProperty(propertyKey);
-
-				if (Validator.isNull(propertyValue)) {
-					return;
-				}
-
-				typeSettingsUnicodeProperties.remove(propertyKey);
-
-				propertyKey = _staging.getStagedPortletId(
-					MBPortletKeys.MESSAGE_BOARDS_ADMIN);
-
-				typeSettingsUnicodeProperties.put(propertyKey, propertyValue);
-
-				group.setTypeSettingsProperties(typeSettingsUnicodeProperties);
-
-				_groupLocalService.updateGroup(group);
-			});
-
-		groupActionableDynamicQuery.performActions();
-	}
-
-	protected void verifyAuditedModels() throws Exception {
-		try (LoggingTimer loggingTimer = new LoggingTimer()) {
-			_verifyAuditedModel.verify(
-				new MBDiscussionVerifiableModel(),
-				new MBThreadVerifiableModel(),
-				new MBThreadFlagVerifiableModel());
-		}
-	}
-
-	protected void verifyGroupedModels() throws Exception {
+	private void _verifyGroupedModels() throws Exception {
 		try (LoggingTimer loggingTimer = new LoggingTimer()) {
 			_verifyGroupedModel.verify(
 				new MBDiscussionVerifiableModel(),
 				new MBThreadFlagVerifiableModel());
 		}
 	}
-
-	@Reference
-	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private Staging _staging;
-
-	@Reference
-	private VerifyAuditedModel _verifyAuditedModel;
 
 	private final VerifyGroupedModel _verifyGroupedModel =
 		new VerifyGroupedModel();
