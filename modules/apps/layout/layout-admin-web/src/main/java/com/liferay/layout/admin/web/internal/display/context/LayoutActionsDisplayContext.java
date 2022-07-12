f87dfed1faf05a4835c458175c7b4e61d018b072
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-07-12 11:12:53 +0200

    LPS-157995 Add action to configure the page. Logic for showing the action comes from miller column, and the URL that is generated comes from ManageLayoutProductNavigationControlMenuEntry

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutActionsDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutActionsDisplayContext.java
@@ -14,8 +14,22 @@
 
 package com.liferay.layout.admin.web.internal.display.context;
 
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalServiceUtil;
+import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.List;
+
+import javax.portlet.PortletRequest;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -29,6 +43,69 @@ public class LayoutActionsDisplayContext {
 
 		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getDropdownItems() {
+		Layout layout = _getLayout();
+
+		return DropdownItemListBuilder.addGroup(
+			dropdownGroupItem -> {
+				dropdownGroupItem.setDropdownItems(
+					DropdownItemListBuilder.add(
+						() -> _isShowConfigureAction(layout),
+						dropdownItem -> {
+							dropdownItem.setHref(
+								_getConfigureLayoutURL(layout));
+
+							dropdownItem.setIcon("cog");
+							dropdownItem.setLabel(
+								LanguageUtil.get(
+									_httpServletRequest, "configure"));
+						}
+					).build());
+				dropdownGroupItem.setSeparator(true);
+			}
+		).build();
+	}
+
+	private String _getConfigureLayoutURL(Layout layout) {
+		String currentURL = PortalUtil.getCurrentURL(_httpServletRequest);
+
+		return PortletURLBuilder.create(
+			PortalUtil.getControlPanelPortletURL(
+				_httpServletRequest, LayoutAdminPortletKeys.GROUP_PAGES,
+				PortletRequest.RENDER_PHASE)
+		).setMVCRenderCommandName(
+			"/layout_admin/edit_layout"
+		).setRedirect(
+			currentURL
+		).setBackURL(
+			currentURL
+		).setParameter(
+			"groupId", layout.getGroupId()
+		).setParameter(
+			"privateLayout", layout.isPrivateLayout()
+		).setParameter(
+			"selPlid", layout.getPlid()
+		).buildPortletURL(
+		).toString();
+	}
+
+	private Layout _getLayout() {
+		Layout layout = _themeDisplay.getLayout();
+
+		if (layout.isDraftLayout()) {
+			layout = LayoutLocalServiceUtil.fetchLayout(layout.getClassPK());
+		}
+
+		return layout;
+	}
+
+	private boolean _isShowConfigureAction(Layout layout)
+		throws PortalException {
+
+		return LayoutPermissionUtil.containsLayoutUpdatePermission(
+			_themeDisplay.getPermissionChecker(), layout);
 	}
 
 	private final HttpServletRequest _httpServletRequest;
