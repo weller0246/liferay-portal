f2dcefb0e6bee20ad1475594c7142d41f045b93e
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-07-12 18:07:38 +0200

    LPS-120353 Creates new ActionDropdownItemsProvider for layout set prototype actions

---
+++ modules/apps/layout/layout-set-prototype-web/src/main/java/com/liferay/layout/set/prototype/web/internal/servlet/taglib/util/LayoutSetPrototypeActionDropdownItemsProvider.java
@@ -0,0 +1,293 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.set.prototype.web.internal.servlet.taglib.util;
+
+import com.liferay.application.list.constants.ApplicationListWebKeys;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.application.list.display.context.logic.PanelCategoryHelper;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.petra.portlet.url.builder.PortletURLBuilder;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.LayoutSetPrototype;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.permission.LayoutSetPrototypePermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.security.PermissionsURLTag;
+
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class LayoutSetPrototypeActionDropdownItemsProvider {
+
+	public LayoutSetPrototypeActionDropdownItemsProvider(
+		LayoutSetPrototype layoutSetPrototype, RenderRequest renderRequest,
+		RenderResponse renderResponse) {
+
+		_layoutSetPrototype = layoutSetPrototype;
+		_renderResponse = renderResponse;
+
+		_httpServletRequest = PortalUtil.getHttpServletRequest(renderRequest);
+
+		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getActionDropdownItems() throws Exception {
+		boolean hasUpdatePermission = LayoutSetPrototypePermissionUtil.contains(
+			_themeDisplay.getPermissionChecker(),
+			_layoutSetPrototype.getLayoutSetPrototypeId(), ActionKeys.UPDATE);
+
+		return new DropdownItemList() {
+			{
+				if (hasUpdatePermission) {
+					Group group = _layoutSetPrototype.getGroup();
+
+					PortletURL siteAdministrationURL =
+						_getSiteAdministrationURL(group);
+
+					if (siteAdministrationURL != null) {
+						add(
+							dropdownItem -> {
+								dropdownItem.setHref(
+									siteAdministrationURL.toString());
+								dropdownItem.setLabel(
+									LanguageUtil.get(
+										_httpServletRequest, "manage"));
+							});
+					}
+
+					if (_layoutSetPrototype.isActive() && !group.isGuest()) {
+						add(_getDeactivateActionUnsafeConsumer());
+					}
+					else if (!_layoutSetPrototype.isActive()) {
+						add(_getActivateActionUnsafeConsumer());
+					}
+
+					boolean readyForPropagation = GetterUtil.getBoolean(
+						_layoutSetPrototype.getSettingsProperty(
+							"readyForPropagation"),
+						true);
+
+					if (readyForPropagation && !group.isGuest()) {
+						add(_getDisablePropagationActionUnsafeConsumer());
+					}
+					else if (!readyForPropagation) {
+						add(_getReadyForPropagationActionUnsafeConsumer());
+					}
+				}
+
+				if (LayoutSetPrototypePermissionUtil.contains(
+						_themeDisplay.getPermissionChecker(),
+						_layoutSetPrototype.getLayoutSetPrototypeId(),
+						ActionKeys.PERMISSIONS)) {
+
+					add(_getPermissionsActionUnsafeConsumer());
+				}
+
+				if (LayoutSetPrototypePermissionUtil.contains(
+						_themeDisplay.getPermissionChecker(),
+						_layoutSetPrototype.getLayoutSetPrototypeId(),
+						ActionKeys.DELETE)) {
+
+					add(_getDeleteActionUnsafeConsumer());
+				}
+			}
+		};
+	}
+
+	private UnsafeConsumer<DropdownItem, Exception>
+		_getActivateActionUnsafeConsumer() {
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "activate");
+			dropdownItem.putData(
+				"activateURL",
+				PortletURLBuilder.createActionURL(
+					_renderResponse
+				).setActionName(
+					"updateLayoutSetPrototypeAction"
+				).setRedirect(
+					_themeDisplay.getURLCurrent()
+				).setParameter(
+					"active", true
+				).setParameter(
+					"layoutSetPrototypeId",
+					_layoutSetPrototype.getLayoutSetPrototypeId()
+				).buildString());
+			dropdownItem.setLabel(
+				LanguageUtil.get(_httpServletRequest, "activate"));
+		};
+	}
+
+	private UnsafeConsumer<DropdownItem, Exception>
+		_getDeactivateActionUnsafeConsumer() {
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "deactivate");
+			dropdownItem.putData(
+				"deactivateURL",
+				PortletURLBuilder.createActionURL(
+					_renderResponse
+				).setActionName(
+					"updateLayoutSetPrototypeAction"
+				).setRedirect(
+					_themeDisplay.getURLCurrent()
+				).setParameter(
+					"active", false
+				).setParameter(
+					"layoutSetPrototypeId",
+					_layoutSetPrototype.getLayoutSetPrototypeId()
+				).buildString());
+			dropdownItem.setLabel(
+				LanguageUtil.get(_httpServletRequest, "deactivate"));
+		};
+	}
+
+	private UnsafeConsumer<DropdownItem, Exception>
+		_getDeleteActionUnsafeConsumer() {
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "delete");
+			dropdownItem.putData(
+				"deleteURL",
+				PortletURLBuilder.createActionURL(
+					_renderResponse
+				).setActionName(
+					"deleteLayoutSetPrototypes"
+				).setParameter(
+					"layoutSetPrototypeId",
+					_layoutSetPrototype.getLayoutSetPrototypeId()
+				).buildString());
+			dropdownItem.setIcon("trash");
+			dropdownItem.setLabel(
+				LanguageUtil.get(_httpServletRequest, "delete"));
+		};
+	}
+
+	private UnsafeConsumer<DropdownItem, Exception>
+		_getDisablePropagationActionUnsafeConsumer() {
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "disablePropagation");
+			dropdownItem.putData(
+				"disablePropagationURL",
+				PortletURLBuilder.createActionURL(
+					_renderResponse
+				).setActionName(
+					"updateLayoutSetPrototypeAction"
+				).setRedirect(
+					_themeDisplay.getURLCurrent()
+				).setParameter(
+					"layoutSetPrototypeId",
+					_layoutSetPrototype.getLayoutSetPrototypeId()
+				).setParameter(
+					"readyForPropagation", false
+				).buildString());
+			dropdownItem.setLabel(
+				LanguageUtil.get(_httpServletRequest, "disable-propagation"));
+		};
+	}
+
+	private UnsafeConsumer<DropdownItem, Exception>
+			_getPermissionsActionUnsafeConsumer()
+		throws Exception {
+
+		String permissionsURL = PermissionsURLTag.doTag(
+			StringPool.BLANK, LayoutSetPrototype.class.getName(),
+			HtmlUtil.escape(
+				_layoutSetPrototype.getName(_themeDisplay.getLocale())),
+			null, String.valueOf(_layoutSetPrototype.getLayoutSetPrototypeId()),
+			LiferayWindowState.POP_UP.toString(), null, _httpServletRequest);
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "permissions");
+			dropdownItem.putData("permissionsURL", permissionsURL);
+			dropdownItem.setIcon("password-policies");
+			dropdownItem.setLabel(
+				LanguageUtil.get(_httpServletRequest, "permissions"));
+		};
+	}
+
+	private UnsafeConsumer<DropdownItem, Exception>
+		_getReadyForPropagationActionUnsafeConsumer() {
+
+		return dropdownItem -> {
+			dropdownItem.putData("action", "readyForPropagation");
+			dropdownItem.putData(
+				"readyForPropagationURL",
+				PortletURLBuilder.createActionURL(
+					_renderResponse
+				).setActionName(
+					"updateLayoutSetPrototypeAction"
+				).setRedirect(
+					_themeDisplay.getURLCurrent()
+				).setParameter(
+					"layoutSetPrototypeId",
+					_layoutSetPrototype.getLayoutSetPrototypeId()
+				).setParameter(
+					"readyForPropagation", true
+				).buildString());
+			dropdownItem.setLabel(
+				LanguageUtil.get(_httpServletRequest, "ready-for-propagation"));
+		};
+	}
+
+	private PortletURL _getSiteAdministrationURL(Group group) {
+		PanelCategoryHelper panelCategoryHelper =
+			(PanelCategoryHelper)_httpServletRequest.getAttribute(
+				ApplicationListWebKeys.PANEL_CATEGORY_HELPER);
+
+		String portletId = panelCategoryHelper.getFirstPortletId(
+			PanelCategoryKeys.SITE_ADMINISTRATION,
+			_themeDisplay.getPermissionChecker(), group);
+
+		if (Validator.isNotNull(portletId)) {
+			return PortletURLBuilder.create(
+				PortalUtil.getControlPanelPortletURL(
+					_httpServletRequest, group, portletId, 0, 0,
+					PortletRequest.RENDER_PHASE)
+			).setRedirect(
+				_themeDisplay.getURLCurrent()
+			).buildPortletURL();
+		}
+
+		return null;
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final LayoutSetPrototype _layoutSetPrototype;
+	private final RenderResponse _renderResponse;
+	private final ThemeDisplay _themeDisplay;
+
+}
--- modules/apps/layout/layout-set-prototype-web/src/main/resources/META-INF/resources/init.jsp
+++ modules/apps/layout/layout-set-prototype-web/src/main/resources/META-INF/resources/init.jsp
@@ -21,25 +21,18 @@
 <%@ taglib uri="http://liferay.com/tld/aui" prefix="aui" %><%@
 taglib uri="http://liferay.com/tld/clay" prefix="clay" %><%@
 taglib uri="http://liferay.com/tld/frontend" prefix="liferay-frontend" %><%@
-taglib uri="http://liferay.com/tld/security" prefix="liferay-security" %><%@
 taglib uri="http://liferay.com/tld/theme" prefix="liferay-theme" %><%@
 taglib uri="http://liferay.com/tld/ui" prefix="liferay-ui" %><%@
 taglib uri="http://liferay.com/tld/util" prefix="liferay-util" %>
 
-<%@ page import="com.liferay.application.list.constants.ApplicationListWebKeys" %><%@
-page import="com.liferay.application.list.constants.PanelCategoryKeys" %><%@
-page import="com.liferay.application.list.display.context.logic.PanelCategoryHelper" %><%@
-page import="com.liferay.layout.set.prototype.constants.LayoutSetPrototypePortletKeys" %><%@
+<%@ page import="com.liferay.layout.set.prototype.constants.LayoutSetPrototypePortletKeys" %><%@
 page import="com.liferay.layout.set.prototype.web.internal.display.context.LayoutSetPrototypeDisplayContext" %><%@
-page import="com.liferay.petra.portlet.url.builder.PortletURLBuilder" %><%@
 page import="com.liferay.petra.string.StringPool" %><%@
-page import="com.liferay.portal.kernel.dao.search.ResultRow" %><%@
 page import="com.liferay.portal.kernel.exception.NoSuchLayoutSetPrototypeException" %><%@
 page import="com.liferay.portal.kernel.exception.RequiredLayoutSetPrototypeException" %><%@
 page import="com.liferay.portal.kernel.language.LanguageUtil" %><%@
 page import="com.liferay.portal.kernel.model.Group" %><%@
 page import="com.liferay.portal.kernel.model.LayoutSetPrototype" %><%@
-page import="com.liferay.portal.kernel.portlet.LiferayWindowState" %><%@
 page import="com.liferay.portal.kernel.security.permission.ActionKeys" %><%@
 page import="com.liferay.portal.kernel.service.LayoutSetPrototypeLocalServiceUtil" %><%@
 page import="com.liferay.portal.kernel.service.LayoutSetPrototypeServiceUtil" %><%@
@@ -51,7 +44,6 @@ page import="com.liferay.portal.kernel.util.ParamUtil" %><%@
 page import="com.liferay.portal.kernel.util.PortalUtil" %><%@
 page import="com.liferay.portal.kernel.util.UnicodeProperties" %><%@
 page import="com.liferay.portal.kernel.util.Validator" %><%@
-page import="com.liferay.portal.kernel.util.WebKeys" %><%@
 page import="com.liferay.portal.model.impl.LayoutSetPrototypeImpl" %><%@
 page import="com.liferay.portal.util.PropsValues" %><%@
 page import="com.liferay.sites.kernel.util.SitesUtil" %>
@@ -59,8 +51,7 @@ page import="com.liferay.sites.kernel.util.SitesUtil" %>
 <%@ page import="java.util.Date" %><%@
 page import="java.util.Set" %>
 
-<%@ page import="javax.portlet.PortletRequest" %><%@
-page import="javax.portlet.PortletURL" %><%@
+<%@ page import="javax.portlet.PortletURL" %><%@
 page import="javax.portlet.WindowState" %>
 
 <liferay-frontend:defineObjects />
