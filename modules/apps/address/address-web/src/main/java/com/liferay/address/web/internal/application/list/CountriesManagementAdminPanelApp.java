2e4e86491a06a75e7e15d54481c7f820c66b5834
pei-jung <pei-jung.lan@liferay.com>
2022-07-11 19:03:30 -0700

    LPS-122223 Display portlet under Control Panel > Configuration

---
+++ modules/apps/address/address-web/src/main/java/com/liferay/address/web/internal/application/list/CountriesManagementAdminPanelApp.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.address.web.internal.application.list;
+
+import com.liferay.address.web.internal.constants.AddressPortletKeys;
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.util.PortletCategoryKeys;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pei-Jung Lan
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=1100",
+		"panel.category.key=" + PortletCategoryKeys.CONTROL_PANEL_CONFIGURATION
+	},
+	service = PanelApp.class
+)
+public class CountriesManagementAdminPanelApp extends BasePanelApp {
+
+	@Override
+	public String getPortletId() {
+		return AddressPortletKeys.COUNTRIES_MANAGEMENT_ADMIN;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + AddressPortletKeys.COUNTRIES_MANAGEMENT_ADMIN + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
