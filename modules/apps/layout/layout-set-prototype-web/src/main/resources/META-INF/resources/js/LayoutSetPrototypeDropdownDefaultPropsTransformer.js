4ebdd975a35ce976dfc16083d6e5ac2438383829
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-07-12 18:17:54 +0200

    LPS-120353 Adds js implementation of actions

---
+++ modules/apps/layout/layout-set-prototype-web/src/main/resources/META-INF/resources/js/LayoutSetPrototypeDropdownDefaultPropsTransformer.js
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+const ACTIONS = {
+	activate(itemData) {
+		submitForm(document.hrefFm, itemData.activateURL);
+	},
+
+	deactivate(itemData) {
+		if (
+			confirm(
+				Liferay.Language.get('are-you-sure-you-want-to-deactivate-this')
+			)
+		) {
+			submitForm(document.hrefFm, itemData.deactivateURL);
+		}
+	},
+
+	delete(itemData) {
+		if (
+			confirm(
+				Liferay.Language.get('are-you-sure-you-want-to-delete-this')
+			)
+		) {
+			submitForm(document.hrefFm, itemData.deleteURL);
+		}
+	},
+
+	disablePropagation(itemData) {
+		submitForm(document.hrefFm, itemData.disablePropagationURL);
+	},
+
+	permissions(itemData) {
+		Liferay.Util.openWindow({
+			dialog: {
+				destroyOnHide: true,
+				modal: true,
+			},
+			dialogIframe: {
+				bodyCssClass: 'dialog-with-footer',
+			},
+			title: Liferay.Language.get('permissions'),
+			uri: itemData.permissionsURL,
+		});
+	},
+
+	readyForPropagation(itemData) {
+		submitForm(document.hrefFm, itemData.readyForPropagationURL);
+	},
+};
+
+export default function propsTransformer({actions, items, ...props}) {
+	const updateItem = (item) => {
+		const newItem = {
+			...item,
+			onClick(event) {
+				const action = item.data?.action;
+
+				if (action) {
+					event.preventDefault();
+
+					ACTIONS[action]?.(item.data);
+				}
+			},
+		};
+
+		if (Array.isArray(item.items)) {
+			newItem.items = item.items.map(updateItem);
+		}
+
+		return newItem;
+	};
+
+	return {
+		...props,
+		actions: actions?.map(updateItem),
+		items: items?.map(updateItem),
+	};
+}
