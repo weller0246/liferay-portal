2ff90515f84a0315636a13b2574b18506f1936fc
Luis Ortiz <luis.ortiz@liferay.com>
2022-07-13 18:07:07 +0200

    LPS-157280 Added unitary test for VersionTreeMap class

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/version/VersionTreeMapTest.java
@@ -0,0 +1,99 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.version;
+
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Luis Ortiz
+ */
+public class VersionTreeMapTest {
+
+	@Before
+	public void setUp() throws Exception {
+		_lastStepSuffix = ReflectionTestUtil.getFieldValue(
+			Version.class, "_LAST_STEP");
+	}
+
+	@Test
+	public void testInsertMultipleUpgradeProcesses() {
+		VersionTreeMap treeMap = new VersionTreeMap();
+
+		UpgradeProcess[] upgradeProcesses = {
+			new DummyUpgradeProcess(), new DummyUpgradeProcess(),
+			new DummyUpgradeProcess()
+		};
+
+		treeMap.put(new Version(1, 0, 0), upgradeProcesses);
+
+		_checkTreeMapValues(treeMap, upgradeProcesses);
+	}
+
+	@Test
+	public void testInsertSingleUpgradeProcess() {
+		VersionTreeMap treeMap = new VersionTreeMap();
+
+		UpgradeProcess upgradeProcess = new DummyUpgradeProcess();
+
+		treeMap.put(new Version(1, 0, 0), upgradeProcess);
+
+		UpgradeProcess[] upgradeProcesses = {upgradeProcess};
+
+		_checkTreeMapValues(treeMap, upgradeProcesses);
+	}
+
+	private void _checkTreeMapValues(
+		VersionTreeMap treeMap, UpgradeProcess[] upgradeProcesses) {
+
+		Assert.assertEquals(upgradeProcesses.length, treeMap.size());
+
+		Collection<Version> keys = treeMap.keySet();
+
+		Iterator<Version> iterator = keys.iterator();
+
+		int i = 0;
+
+		while (iterator.hasNext()) {
+			Version version = iterator.next();
+
+			UpgradeProcess upgradeProcess = treeMap.get(version);
+
+			Assert.assertEquals(upgradeProcesses[i], upgradeProcess);
+
+			String step = version.getStep();
+
+			if (iterator.hasNext()) {
+				Assert.assertTrue(step.equals("Step-" + (i + 1)));
+			}
+			else {
+				Assert.assertTrue(step.equals(_lastStepSuffix));
+			}
+
+			i++;
+		}
+	}
+
+	private static String _lastStepSuffix;
+
+}
