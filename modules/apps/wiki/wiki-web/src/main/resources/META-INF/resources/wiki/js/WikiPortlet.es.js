39bbf786482177b584ed785b5785de7d23bd5d45
Diego Nascimento <diego.nascimento@liferay.com>
2022-07-13 18:08:02 -0300

    LPS-148661 Refactor to not use _maybeRemoveTempImages
    * Since the single point of return variable of maybeRemoveTempImages was initialized with true, see https://cs.github.com/liferay/liferay-portal/blob/dd3405ce5fe38c8989049dbd4274971e0ffdb0f1/modules/apps/wiki/wiki-web/src/main/resources/META-INF/resources/wiki/js/WikiPortlet.es.js#L188, it was calling the submit if they have temp images or not

--- modules/apps/wiki/wiki-web/src/main/resources/META-INF/resources/wiki/js/WikiPortlet.es.js
+++ modules/apps/wiki/wiki-web/src/main/resources/META-INF/resources/wiki/js/WikiPortlet.es.js
@@ -180,43 +180,15 @@ class WikiPortlet {
 	}
 
 	/**
-	 * Checks if there are images that have not been uploaded yet.
-	 * In that case, it removes them after asking
-	 * confirmation to the user.
-	 *
-	 * @protected
-	 * @return {void} Call the callback if there aren't temporal
-	 * images, and the user does not confirm she wants to lose them.
-	 * Otherwise, the callback will not be called.
-	 */
-	_maybeRemoveTempImages(callback) {
-		const tempImages = this.rootNode.querySelector('img[data-random-id]');
-
-		if (tempImages && !!tempImages.length) {
-			openConfirmModal({
-				message: this._strings.confirmDiscardImages,
-				onConfirm: (isConfirmed) => {
-					if (isConfirmed) {
-						tempImages.forEach((node) => {
-							node.parentElement.remove();
-						});
-
-						callback();
-					}
-				},
-			});
-		}
-	}
-
-	/**
 	 * Submits the wiki page.
 	 *
 	 * @protected
 	 */
 	_save() {
 		const namespace = this._namespace;
+		const tempImages = this.rootNode.querySelector('img[data-random-id]');
 
-		this._maybeRemoveTempImages(() => {
+		const submit = () => {
 			document.getElementById(
 				namespace + this._constants.CMD
 			).value = this._currentAction;
@@ -230,7 +202,25 @@ class WikiPortlet {
 			}
 
 			submitForm(document[`${namespace}fm`]);
-		});
+		};
+
+		if (tempImages && !!tempImages.length) {
+			openConfirmModal({
+				message: this._strings.confirmDiscardImages,
+				onConfirm: (isConfirmed) => {
+					if (isConfirmed) {
+						tempImages.forEach((node) => {
+							node.parentElement.remove();
+						});
+					}
+
+					submit();
+				},
+			});
+		}
+		else {
+			submit();
+		}
 	}
 }
 
