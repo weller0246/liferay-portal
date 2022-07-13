0f480b7f0c2624ed5157cdf24789b9db2d8119b9
Kevin Lee <kevin.lee@liferay.com>
2022-07-12 18:23:40 -0700

    LPS-153072 Use BOM to fetch release-dxp-api dependencies

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GradleUpgradeReleaseDXPCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/GradleUpgradeReleaseDXPCheck.java
@@ -14,6 +14,30 @@
 
 package com.liferay.source.formatter.check;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.servlet.HttpMethods;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.check.util.SourceUtil;
+import com.liferay.source.formatter.util.SourceFormatterUtil;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
 /**
  * @author Kevin Lee
  */
@@ -24,7 +48,114 @@ public class GradleUpgradeReleaseDXPCheck extends BaseFileCheck {
 			String fileName, String absolutePath, String content)
 		throws Exception {
 
+		String upgradeToVersion = getAttributeValue(
+			SourceFormatterUtil.UPGRADE_TO_VERSION, absolutePath);
+
+		if (Validator.isNull(upgradeToVersion)) {
+			return content;
+		}
+
+		return _formatDependencies(content, upgradeToVersion);
+	}
+
+	private String _formatDependencies(
+		String content, String upgradeToVersion) {
+
+		Map<String, Set<String>> releaseDXPDependencies =
+			_getReleaseDXPDependencies(upgradeToVersion);
+
+		if (releaseDXPDependencies == null) {
+			return content;
+		}
+
 		return content;
 	}
+
+	private Map<String, Set<String>> _getReleaseDXPDependencies(
+		String upgradeToVersion) {
+
+		if (Objects.equals(upgradeToVersion, _upgradeToVersion)) {
+			return _releaseDXPDependencies;
+		}
+
+		try {
+			URL url = new URL(_getReleaseDXPPomURL(upgradeToVersion));
+
+			HttpURLConnection httpURLConnection =
+				(HttpURLConnection)url.openConnection();
+
+			httpURLConnection.setConnectTimeout(10000);
+			httpURLConnection.setReadTimeout(10000);
+			httpURLConnection.setRequestMethod(HttpMethods.GET);
+
+			String content = StringUtil.read(
+				httpURLConnection.getInputStream());
+
+			if (Objects.equals(content, StringPool.BLANK)) {
+				return null;
+			}
+
+			_upgradeToVersion = upgradeToVersion;
+			_releaseDXPDependencies = _readReleaseDXPDependencies(content);
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+
+			return null;
+		}
+
+		return _releaseDXPDependencies;
+	}
+
+	private String _getReleaseDXPPomURL(String upgradeToVersion) {
+		String baseURL =
+			"https://repository-cdn.liferay.com/nexus/content/repositories" +
+				"/liferay-public-releases/com/liferay/portal/release.dxp.bom/";
+
+		return StringBundler.concat(
+			baseURL, upgradeToVersion, "/release.dxp.bom-", upgradeToVersion,
+			".pom");
+	}
+
+	private Map<String, Set<String>> _readReleaseDXPDependencies(String content)
+		throws DocumentException {
+
+		Document document = SourceUtil.readXML(content);
+
+		Element rootElement = document.getRootElement();
+
+		Element dependencyManagementElement = rootElement.element(
+			"dependencyManagement");
+
+		Element dependenciesElement = dependencyManagementElement.element(
+			"dependencies");
+
+		List<Element> dependencyElements = dependenciesElement.elements(
+			"dependency");
+
+		Map<String, Set<String>> dependencies = new HashMap<>();
+
+		for (Element dependencyElement : dependencyElements) {
+			Element groupIdElement = dependencyElement.element("groupId");
+			Element artifactIdElement = dependencyElement.element("artifactId");
+
+			String groupId = groupIdElement.getText();
+			String artifactId = artifactIdElement.getText();
+
+			dependencies.putIfAbsent(groupId, new HashSet<>());
+
+			Set<String> artifactIds = dependencies.get(groupId);
+
+			artifactIds.add(artifactId);
+		}
+
+		return dependencies;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GradleUpgradeReleaseDXPCheck.class);
+
+	private Map<String, Set<String>> _releaseDXPDependencies;
+	private String _upgradeToVersion;
 
 }
