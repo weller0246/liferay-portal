f307c5cf5b7c7ec97a653f111da101a0f384fea6
Jose Abelenda <jose.abelenda@liferay.com>
2022-07-13 19:05:36 -0300

    LPS-158121 save interface configuration

--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/BundleSiteInitializer.java
@@ -75,7 +75,6 @@ import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalServ
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.util.LayoutCopyHelper;
 import com.liferay.layout.util.structure.LayoutStructure;
-import com.liferay.login.web.internal.configuration.AuthLoginConfiguration;
 import com.liferay.object.admin.rest.dto.v1_0.ObjectDefinition;
 import com.liferay.object.admin.rest.dto.v1_0.ObjectRelationship;
 import com.liferay.object.admin.rest.dto.v1_0.util.ObjectActionUtil;
@@ -110,7 +109,6 @@ import com.liferay.portal.kernel.model.Theme;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.model.UserGroup;
 import com.liferay.portal.kernel.model.role.RoleConstants;
-import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
 import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
@@ -134,7 +132,6 @@ import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
-import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
@@ -191,7 +188,6 @@ import javax.servlet.ServletContext;
 
 import org.osgi.framework.Bundle;
 import org.osgi.framework.wiring.BundleWiring;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Brian Wing Shun Chan
@@ -2704,20 +2700,32 @@ public class BundleSiteInitializer implements SiteInitializer {
 		Group group = _groupLocalService.getGroup(
 			serviceContext.getScopeGroupId());
 
-		JSONObject jsonObject = JSONFactoryUtil.createJSONObject(json);
+		JSONObject jsonObject1 = JSONFactoryUtil.createJSONObject(json);
 
-		_configurationProvider.saveGroupConfiguration(
-			AuthLoginConfiguration.class, serviceContext.getScopeGroupId(),
-			HashMapDictionaryBuilder.<String, Object>put(
-				"promptEnabled", jsonObject.getBoolean("promptEnabled", false)
-			).build());
-
-		group.setType(jsonObject.getInt("typeSite"));
-		group.setManualMembership(jsonObject.getBoolean("manualMembership"));
+		group.setType(jsonObject1.getInt("typeSite"));
+		group.setManualMembership(jsonObject1.getBoolean("manualMembership"));
 		group.setMembershipRestriction(
-			jsonObject.getInt("membershipRestriction"));
+			jsonObject1.getInt("membershipRestriction"));
 
 		_groupLocalService.updateGroup(group);
+
+		JSONArray jsonArray = jsonObject1.getJSONArray(
+			"interfaceConfigurations");
+
+		if (JSONUtil.isEmpty(jsonArray)) {
+			return;
+		}
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject2 = jsonArray.get(i);
+
+			_configurationProvider.saveGroupConfiguration(
+				Class.forName(jsonObject2.getString("className")),
+				serviceContext.getScopeGroupId(),
+				jsonObject2.get(
+					"values"
+				).toMap());
+		}
 	}
 
 	private void _addSiteNavigationMenu(
@@ -3884,6 +3892,4 @@ public class BundleSiteInitializer implements SiteInitializer {
 
 	}
 
-	@Reference
-	private ConfigurationProvider _configurationProvider;
 }
