6b0183a89354a834b96518d9651df423a96771fb
Mirna Gama <mirnabeatrizgamav@gmail.com>
2022-07-18 10:06:39 -0300

    LPS-157997 Replace Glyphicon with ClayIcon

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Geolocation/Geolocation.es.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/Geolocation/Geolocation.es.js
@@ -14,6 +14,7 @@
 
 import 'leaflet/dist/leaflet.css';
 import React from 'react';
+import ClayIcon from '@clayui/icon';
 
 import {FieldBase} from '../FieldBase/ReactFieldBase.es';
 import {MAP_PROVIDER, useGeolocation} from './useGeolocation.es';
@@ -55,11 +56,12 @@ const Geolocation = ({
 
 	return (
 		<div {...otherProps} className="ddm-geolocation field-labels-inline">
+			<div>
+				<ClayIcon symbol="geolocation" />
 
-			<div className="glyphicon glyphicon-map-marker">
-			<span id={`address_label_${instanceId}`}></span>
+				<span id={`address_label_${instanceId}`}></span>
 			</div>
-			
+
 			{!disabled || viewMode ? (
 				<dl>
 					<dt className="text-capitalize"></dt>
