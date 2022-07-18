a78e5db9bcfea20ba90da97264a13180bd76dafe
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-07-18 12:51:18 +0100

    LPS-150725 Implement it

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/internal/asset/entry/provider/AssetListAssetEntryProviderImpl.java
@@ -65,6 +65,9 @@ import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.search.hits.SearchHits;
+import com.liferay.portal.search.legacy.searcher.SearchRequestBuilderFactory;
+import com.liferay.portal.search.searcher.Searcher;
 import com.liferay.segments.constants.SegmentsEntryConstants;
 
 import java.io.Serializable;
@@ -416,16 +419,33 @@ public class AssetListAssetEntryProviderImpl
 
 	private List<AssetEntry> _dynamicSearch(
 		long companyId, long[][] assetCategoryIds,
-		AssetEntryQuery assetEntryQuery, String keywords) {
+		List<AssetEntryQuery> assetEntryQueries, String keywords) {
 
 		try {
-			Hits hits = _assetHelper.search(
+			if (ListUtil.isEmpty(assetEntryQueries)) {
+				return Collections.emptyList();
+			}
+
+			AssetEntryQuery assetEntryQuery = assetEntryQueries.get(0);
+
+			if (assetEntryQueries.size() == 1) {
+				Hits hits = _assetHelper.search(
+					_getDynamicSearchContext(
+						companyId, assetCategoryIds, assetEntryQuery, keywords),
+					assetEntryQuery, assetEntryQuery.getStart(),
+					assetEntryQuery.getEnd());
+
+				return _assetHelper.getAssetEntries(hits);
+			}
+
+			SearchHits searchHits = _assetHelper.search(
 				_getDynamicSearchContext(
-					companyId, assetCategoryIds, assetEntryQuery, keywords),
-				assetEntryQuery, assetEntryQuery.getStart(),
+					companyId, assetCategoryIds, assetEntryQueries.get(0),
+					keywords),
+				assetEntryQueries, assetEntryQuery.getStart(),
 				assetEntryQuery.getEnd());
 
-			return _assetHelper.getAssetEntries(hits);
+			return _assetHelper.getAssetEntries(searchHits);
 		}
 		catch (Exception exception) {
 			_log.error("Unable to get asset entries", exception);
@@ -436,13 +456,29 @@ public class AssetListAssetEntryProviderImpl
 
 	private int _dynamicSearchCount(
 		long companyId, long[][] assetCategoryIds,
-		AssetEntryQuery assetEntryQuery, String keywords) {
+		List<AssetEntryQuery> assetEntryQueries, String keywords) {
 
 		try {
+			if (ListUtil.isEmpty(assetEntryQueries)) {
+				return 0;
+			}
+
+			AssetEntryQuery assetEntryQuery = assetEntryQueries.get(0);
+
+			if (assetEntryQueries.size() == 1) {
+				Long count = _assetHelper.searchCount(
+					_getDynamicSearchContext(
+						companyId, assetCategoryIds, assetEntryQuery, keywords),
+					assetEntryQuery);
+
+				return count.intValue();
+			}
+
 			Long count = _assetHelper.searchCount(
 				_getDynamicSearchContext(
 					companyId, assetCategoryIds, assetEntryQuery, keywords),
-				assetEntryQuery);
+				assetEntryQueries, assetEntryQuery.getStart(),
+				assetEntryQuery.getEnd());
 
 			return count.intValue();
 		}
@@ -749,16 +785,20 @@ public class AssetListAssetEntryProviderImpl
 
 			return _dynamicSearch(
 				assetListEntry.getCompanyId(), assetCategoryIds,
-				assetEntryQuery, keywords);
+				Collections.singletonList(assetEntryQuery), keywords);
 		}
 
-		AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
-			assetListEntry,
-			_getCombinedSegmentsEntryIds(assetListEntry, segmentsEntryIds),
-			userId, end, start);
+		LongStream longStream = Arrays.stream(
+			_getCombinedSegmentsEntryIds(assetListEntry, segmentsEntryIds));
 
 		return _dynamicSearch(
-			assetListEntry.getCompanyId(), assetCategoryIds, assetEntryQuery,
+			assetListEntry.getCompanyId(), assetCategoryIds,
+			longStream.mapToObj(
+				segmentsEntryId -> getAssetEntryQuery(
+					assetListEntry, segmentsEntryId, userId)
+			).collect(
+				Collectors.toList()
+			),
 			keywords);
 	}
 
@@ -766,30 +806,28 @@ public class AssetListAssetEntryProviderImpl
 		AssetListEntry assetListEntry, long[] segmentsEntryIds,
 		long[][] assetCategoryIds, String keywords, String userId) {
 
-		if (_assetListConfiguration.combineAssetsFromAllSegmentsDynamic()) {
-			int totalCount = 0;
+		if (!_assetListConfiguration.combineAssetsFromAllSegmentsDynamic()) {
+			AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
+				assetListEntry,
+				_getFirstSegmentsEntryId(assetListEntry, segmentsEntryIds),
+				userId);
 
-			for (long segmentsEntryId :
-					_getCombinedSegmentsEntryIds(
-						assetListEntry, segmentsEntryIds)) {
-
-				AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
-					assetListEntry, segmentsEntryId, userId);
-
-				totalCount += _dynamicSearchCount(
-					assetListEntry.getCompanyId(), assetCategoryIds,
-					assetEntryQuery, keywords);
-			}
-
-			return totalCount;
+			return _dynamicSearchCount(
+				assetListEntry.getCompanyId(), assetCategoryIds,
+				Collections.singletonList(assetEntryQuery), keywords);
 		}
 
-		AssetEntryQuery assetEntryQuery = getAssetEntryQuery(
-			assetListEntry,
-			_getFirstSegmentsEntryId(assetListEntry, segmentsEntryIds), userId);
+		LongStream longStream = Arrays.stream(
+			_getCombinedSegmentsEntryIds(assetListEntry, segmentsEntryIds));
 
 		return _dynamicSearchCount(
-			assetListEntry.getCompanyId(), assetCategoryIds, assetEntryQuery,
+			assetListEntry.getCompanyId(), assetCategoryIds,
+			longStream.mapToObj(
+				segmentsEntryId -> getAssetEntryQuery(
+					assetListEntry, segmentsEntryId, userId)
+			).collect(
+				Collectors.toList()
+			),
 			keywords);
 	}
 
@@ -815,7 +853,6 @@ public class AssetListAssetEntryProviderImpl
 
 		searchContext.setBooleanClauses(
 			_getAssetCategoryIdsBooleanClauses(assetCategoryIds));
-		searchContext.setClassTypeIds(assetEntryQuery.getClassTypeIds());
 		searchContext.setCompanyId(companyId);
 		searchContext.setEnd(assetEntryQuery.getEnd());
 		searchContext.setKeywords(keywords);
@@ -1222,5 +1259,11 @@ public class AssetListAssetEntryProviderImpl
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private Searcher _searcher;
+
+	@Reference
+	private SearchRequestBuilderFactory _searchRequestBuilderFactory;
 
 }
