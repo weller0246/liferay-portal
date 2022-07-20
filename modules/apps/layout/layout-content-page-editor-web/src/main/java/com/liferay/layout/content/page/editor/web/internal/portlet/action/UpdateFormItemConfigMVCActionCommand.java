2863ea6db20b32c3bf99b503c6cca8464ccd8ab1
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-07-20 13:25:43 +0200

    LPS-158326 Add add the submit-button fragment

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
@@ -14,25 +14,39 @@
 
 package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
+import com.liferay.fragment.contributor.FragmentCollectionContributorTracker;
+import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.processor.FragmentEntryProcessorRegistry;
+import com.liferay.fragment.service.FragmentEntryLinkService;
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
+import com.liferay.layout.content.page.editor.web.internal.util.FragmentEntryLinkManager;
 import com.liferay.layout.page.template.model.LayoutPageTemplateStructure;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
 import com.liferay.layout.page.template.service.LayoutPageTemplateStructureService;
 import com.liferay.layout.util.structure.LayoutStructure;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -57,10 +71,12 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 
 		JSONPortletResponseUtil.writeJSON(
 			actionRequest, actionResponse,
-			_updateFormItemConfig(actionRequest));
+			_updateFormItemConfig(actionRequest, actionResponse));
 	}
 
-	private JSONObject _updateFormItemConfig(ActionRequest actionRequest) {
+	private JSONObject _updateFormItemConfig(
+		ActionRequest actionRequest, ActionResponse actionResponse) {
+
 		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
@@ -83,16 +99,51 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 			layoutStructure.updateItemConfig(
 				JSONFactoryUtil.createJSONObject(itemConfig), formItemId);
 
+			HttpServletRequest httpServletRequest =
+				_portal.getHttpServletRequest(actionRequest);
+
+			FragmentEntry fragmentEntry =
+				_fragmentCollectionContributorTracker.getFragmentEntry(
+					"INPUTS-submit-button");
+
+			ServiceContext serviceContext = ServiceContextFactory.getInstance(
+				httpServletRequest);
+
+			FragmentEntryLink fragmentEntryLink =
+				_fragmentEntryLinkService.addFragmentEntryLink(
+					themeDisplay.getScopeGroupId(), 0,
+					fragmentEntry.getFragmentEntryId(), segmentsExperienceId,
+					themeDisplay.getPlid(), fragmentEntry.getCss(),
+					fragmentEntry.getHtml(), fragmentEntry.getJs(),
+					fragmentEntry.getConfiguration(), null, StringPool.BLANK, 0,
+					fragmentEntry.getFragmentEntryKey(),
+					fragmentEntry.getType(), serviceContext);
+
+			layoutStructure.addFragmentStyledLayoutStructureItem(
+				fragmentEntryLink.getFragmentEntryLinkId(), formItemId, 0);
+
 			layoutPageTemplateStructure =
 				_layoutPageTemplateStructureService.
 					updateLayoutPageTemplateStructureData(
 						themeDisplay.getScopeGroupId(), themeDisplay.getPlid(),
 						segmentsExperienceId, layoutStructure.toString());
 
+			HttpServletResponse httpServletResponse =
+				_portal.getHttpServletResponse(actionResponse);
+
 			LayoutStructure updatedLayoutStructure = LayoutStructure.of(
 				layoutPageTemplateStructure.getData(segmentsExperienceId));
 
-			jsonObject.put("layoutData", updatedLayoutStructure.toJSONObject());
+			jsonObject.put(
+				"addedFragmentEntryLinks",
+				JSONUtil.put(
+					String.valueOf(fragmentEntryLink.getFragmentEntryLinkId()),
+					_fragmentEntryLinkManager.getFragmentEntryLinkJSONObject(
+						fragmentEntryLink, httpServletRequest,
+						httpServletResponse, updatedLayoutStructure))
+			).put(
+				"layoutData", updatedLayoutStructure.toJSONObject()
+			);
 		}
 		catch (Exception exception) {
 			_log.error(exception);
@@ -112,11 +163,27 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 		UpdateFormItemConfigMVCActionCommand.class);
 
 	@Reference
+	private FragmentCollectionContributorTracker
+		_fragmentCollectionContributorTracker;
+
+	@Reference
+	private FragmentEntryLinkManager _fragmentEntryLinkManager;
+
+	@Reference
+	private FragmentEntryLinkService _fragmentEntryLinkService;
+
+	@Reference
+	private FragmentEntryProcessorRegistry _fragmentEntryProcessorRegistry;
+
+	@Reference
 	private LayoutPageTemplateStructureLocalService
 		_layoutPageTemplateStructureLocalService;
 
 	@Reference
 	private LayoutPageTemplateStructureService
 		_layoutPageTemplateStructureService;
+
+	@Reference
+	private Portal _portal;
 
 }
