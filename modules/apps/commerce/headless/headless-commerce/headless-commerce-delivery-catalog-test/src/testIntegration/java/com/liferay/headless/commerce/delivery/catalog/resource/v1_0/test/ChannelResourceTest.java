94d8b997dc59abfc6f1ec4a570c70123df52f52b
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-07-20 09:22:41 +0200

    COMMERCE-9583 Add integration tests

---
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/catalog/resource/v1_0/test/ChannelResourceTest.java
@@ -0,0 +1,104 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.delivery.catalog.resource.v1_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.headless.commerce.delivery.catalog.client.dto.v1_0.Channel;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.test.rule.Inject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Crescenzo Rega
+ */
+@RunWith(Arquillian.class)
+public class ChannelResourceTest extends BaseChannelResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addUser(testCompany);
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			testCompany.getCompanyId(), testGroup.getGroupId(),
+			_user.getUserId());
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"currencyCode", "name", "type"};
+	}
+
+	protected Channel testGetChannelsPage_addChannel(Channel channel)
+		throws Exception {
+
+		return _addChannel(channel);
+	}
+
+	protected Channel testGraphQLChannel_addChannel() throws Exception {
+		return _addChannel(randomChannel());
+	}
+
+	private Channel _addChannel(Channel channel) throws Exception {
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.addCommerceChannel(
+				channel.getExternalReferenceCode(), channel.getSiteGroupId(),
+				channel.getName(), channel.getType(), null,
+				channel.getCurrencyCode(), _serviceContext);
+
+		_commerceChannels.add(commerceChannel);
+
+		return _toChannel(commerceChannel);
+	}
+
+	private Channel _toChannel(CommerceChannel commerceChannel) {
+		return new Channel() {
+			{
+				currencyCode = commerceChannel.getCommerceCurrencyCode();
+				externalReferenceCode =
+					commerceChannel.getExternalReferenceCode();
+				id = commerceChannel.getCommerceChannelId();
+				name = commerceChannel.getName();
+				siteGroupId = commerceChannel.getSiteGroupId();
+				type = commerceChannel.getType();
+			}
+		};
+	}
+
+	@Inject
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@DeleteAfterTestRun
+	private final List<CommerceChannel> _commerceChannels = new ArrayList<>();
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private User _user;
+
+}
