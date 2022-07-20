2ea0b57bea9ec69f3425cc808e64f104523314a3
hai.yu <hai.yu@liferay.com>
2022-07-20 14:22:27 +0800

    LPS-158702 Expose the issue.

---
+++ portal-kernel/test/unit/com/liferay/portal/kernel/servlet/ServletResponseUtilContentLengthTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.kernel.servlet;
+
+import com.liferay.portal.kernel.util.Props;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.ProxyFactory;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import java.io.ByteArrayInputStream;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import org.springframework.mock.web.MockHttpServletResponse;
+
+/**
+ * @author Hai Yu
+ */
+public class ServletResponseUtilContentLengthTest {
+
+	@Test
+	public void testContentLengthLowerThanInputStreamLength()
+		throws Exception {
+
+		PropsUtil.setProps(ProxyFactory.newDummyInstance(Props.class));
+
+		String content = StringUtil.randomString(10);
+
+		int contentLength = content.length() - 1;
+
+		MockHttpServletResponse mockHttpServletResponse =
+			new MockHttpServletResponse();
+
+		ServletResponseUtil.write(
+			mockHttpServletResponse,
+			new ByteArrayInputStream(content.getBytes()), contentLength);
+
+		Assert.assertEquals(
+			String.valueOf(contentLength),
+			mockHttpServletResponse.getHeader(HttpHeaders.CONTENT_LENGTH));
+		Assert.assertEquals(
+			content.substring(0, contentLength),
+			mockHttpServletResponse.getContentAsString());
+	}
+
+}
