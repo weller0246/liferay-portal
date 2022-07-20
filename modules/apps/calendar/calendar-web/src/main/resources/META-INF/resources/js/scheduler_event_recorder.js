0470e59b80cf48d4f71f5e20fee83648af8ccd3b
Lianne Louie <lianne.louie@liferay.com>
2022-07-19 17:54:05 -0700

    LPS-158192 - add aria-label to button to stop screen reader from identifying x button as times button

--- modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler_event_recorder.js
+++ modules/apps/calendar/calendar-web/src/main/resources/META-INF/resources/js/scheduler_event_recorder.js
@@ -746,7 +746,8 @@ AUI.add(
 						[
 							{
 								cssClass: 'close',
-								label: '\u00D7',
+								labelHTML:
+									'<span aria-label="close">&times;</span>',
 								on: {
 									click: A.bind(
 										instance._handleCancelEvent,
