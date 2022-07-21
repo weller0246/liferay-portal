17fcf2c2761bc48db95b00112c05f3a8e244388f
marcelabc <marcela.cunha@liferay.com>
2022-07-21 16:14:30 -0300

    LPS-158180 Validate the settings for the aggregation field type

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/AggregationObjectFieldBusinessType.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/field/business/type/AggregationObjectFieldBusinessType.java
@@ -16,15 +16,25 @@ package com.liferay.object.internal.field.business.type;
 
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
 import com.liferay.object.constants.ObjectFieldConstants;
+import com.liferay.object.exception.NoSuchObjectFieldException;
+import com.liferay.object.exception.NoSuchObjectRelationshipException;
 import com.liferay.object.exception.ObjectFieldSettingNameException;
 import com.liferay.object.exception.ObjectFieldSettingValueException;
 import com.liferay.object.field.business.type.ObjectFieldBusinessType;
 import com.liferay.object.field.render.ObjectFieldRenderingContext;
+import com.liferay.object.model.ObjectDefinition;
 import com.liferay.object.model.ObjectField;
 import com.liferay.object.model.ObjectFieldSetting;
+import com.liferay.object.model.ObjectFilter;
+import com.liferay.object.model.ObjectRelationship;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
 import com.liferay.object.service.ObjectFieldSettingLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -123,24 +133,36 @@ public class AggregationObjectFieldBusinessType
 			List<ObjectFieldSetting> objectFieldSettings)
 		throws PortalException {
 
-		Set<String> missingRequiredObjectFieldSettingsNames = new HashSet<>();
-
 		Stream<ObjectFieldSetting> stream = objectFieldSettings.stream();
 
-		Map<String, String> objectFieldSettingsValuesMap = stream.collect(
+		Map<String, Object> objectFieldSettingsValuesMap = stream.collect(
 			Collectors.toMap(
-				ObjectFieldSetting::getName, ObjectFieldSetting::getValue));
+				ObjectFieldSetting::getName,
+				objectFieldSetting -> {
+					if (Objects.equals(
+							objectFieldSetting.getName(), "filters")) {
+
+						return objectFieldSetting.getObjectFilters();
+					}
+
+					return objectFieldSetting.getValue();
+				}));
+
+		String function = GetterUtil.getString(
+			objectFieldSettingsValuesMap.get("function"));
 
 		Set<String> requiredObjectFieldSettingsNames =
 			getRequiredObjectFieldSettingsNames();
 
-		if (Objects.equals(
-				GetterUtil.getString(
-					objectFieldSettingsValuesMap.get("function")),
-				"COUNT")) {
-
+		if (!ArrayUtil.contains(_FUNCTION, function)) {
+			throw new ObjectFieldSettingValueException.InvalidValue(
+				objectFieldName, "function", function);
+		}
+		else if (Objects.equals(function, "COUNT")) {
 			requiredObjectFieldSettingsNames.remove("summarizeField");
 		}
+
+		Set<String> missingRequiredObjectFieldSettingsNames = new HashSet<>();
 
 		for (String requiredObjectFieldSettingName :
 				requiredObjectFieldSettingsNames) {
@@ -171,12 +193,136 @@ public class AggregationObjectFieldBusinessType
 			throw new ObjectFieldSettingNameException.NotAllowedNames(
 				objectFieldName, notAllowedObjectFieldSettingsNames);
 		}
+
+		try {
+			ObjectRelationship objectRelationship =
+				_objectRelationshipLocalService.getObjectRelationship(
+					objectDefinitionId,
+					GetterUtil.getString(
+						objectFieldSettingsValuesMap.get("relationship")));
+
+			ObjectDefinition objectDefinition =
+				_objectDefinitionLocalService.fetchObjectDefinition(
+					objectRelationship.getObjectDefinitionId2());
+
+			ObjectField objectField = _objectFieldLocalService.getObjectField(
+				objectDefinition.getObjectDefinitionId(),
+				GetterUtil.getString(
+					objectFieldSettingsValuesMap.get("summarizeField")));
+
+			if (!ArrayUtil.contains(
+					_NUMERIC_BUSINESS_TYPES, objectField.getBusinessType())) {
+
+				throw new ObjectFieldSettingValueException.InvalidValue(
+					objectFieldName, "summarizeField",
+					GetterUtil.getString(
+						objectFieldSettingsValuesMap.get("summarizeField")));
+			}
+
+			_validateObjectFilters(
+				objectDefinition, objectFieldName,
+				(List<ObjectFilter>)objectFieldSettingsValuesMap.get(
+					"filters"));
+		}
+		catch (NoSuchObjectRelationshipException
+					noSuchObjectRelationshipException) {
+
+			throw new ObjectFieldSettingValueException.InvalidValue(
+				objectFieldName, "relationship",
+				GetterUtil.getString(
+					objectFieldSettingsValuesMap.get("relationship")),
+				noSuchObjectRelationshipException);
+		}
+		catch (NoSuchObjectFieldException noSuchObjectFieldException) {
+			throw new ObjectFieldSettingValueException.InvalidValue(
+				objectFieldName, "summarizeField",
+				GetterUtil.getString(
+					objectFieldSettingsValuesMap.get("summarizeField")),
+				noSuchObjectFieldException);
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
 	}
+
+	private void _validateObjectFilters(
+			ObjectDefinition objectDefinition, String objectFieldName,
+			List<ObjectFilter> objectFilters)
+		throws PortalException {
+
+		if (ListUtil.isEmpty(objectFilters)) {
+			return;
+		}
+
+		for (ObjectFilter objectFilter : objectFilters) {
+			Set<String> missingObjectFilterValues = new HashSet<>();
+
+			_validateObjectFilterValue(
+				missingObjectFilterValues, "filterBy",
+				objectFilter.getFilterBy());
+
+			_validateObjectFilterValue(
+				missingObjectFilterValues, "filterType",
+				objectFilter.getFilterType());
+
+			_validateObjectFilterValue(
+				missingObjectFilterValues, "json", objectFilter.getJSON());
+
+			if (!missingObjectFilterValues.isEmpty()) {
+				throw new ObjectFieldSettingValueException.
+					MissingRequiredValues(
+						objectFieldName, missingObjectFilterValues);
+			}
+
+			ObjectField objectField = _objectFieldLocalService.fetchObjectField(
+				objectDefinition.getObjectDefinitionId(),
+				GetterUtil.getString(objectFilter.getFilterBy()));
+
+			if ((objectField == null) ||
+				Objects.equals(
+					objectField.getBusinessType(),
+					ObjectFieldConstants.BUSINESS_TYPE_AGGREGATION)) {
+
+				throw new ObjectFieldSettingValueException.InvalidValue(
+					objectFieldName, "filterBy",
+					GetterUtil.getString(objectFilter.getFilterBy()));
+			}
+		}
+	}
+
+	private void _validateObjectFilterValue(
+		Set<String> missingObjectFilterValues, String objectFilterName,
+		String objectFilterValue) {
+
+		if (Validator.isNull(objectFilterValue)) {
+			missingObjectFilterValues.add(objectFilterName);
+		}
+	}
+
+	private static final String[] _FUNCTION = {
+		"AVERAGE", "COUNT", "MAX", "MIN", "SUM"
+	};
+
+	private static final String[] _NUMERIC_BUSINESS_TYPES = {
+		ObjectFieldConstants.BUSINESS_TYPE_DECIMAL,
+		ObjectFieldConstants.BUSINESS_TYPE_INTEGER,
+		ObjectFieldConstants.BUSINESS_TYPE_LONG_INTEGER,
+		ObjectFieldConstants.BUSINESS_TYPE_PRECISION_DECIMAL
+	};
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectFieldLocalService _objectFieldLocalService;
 
 	@Reference
 	private Language _language;
 
 	@Reference
 	private ObjectFieldSettingLocalService _objectFieldSettingLocalService;
+
+	@Reference
+	private ObjectRelationshipLocalService _objectRelationshipLocalService;
 
 }
