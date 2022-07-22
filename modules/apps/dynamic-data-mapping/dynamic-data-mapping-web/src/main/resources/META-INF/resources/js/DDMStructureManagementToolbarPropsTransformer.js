99d2c7362a45378c9304c5ec23e383d870b14903
Brian Chan <brian.chan@liferay.com>
2022-07-22 09:38:18 -0300

    Revert "LPS-148661 Use conditionally `openConfirmModal` on `dynamic-data-mapping-web`"
    This reverts commit 57328bf3e37ca4ccbd1dd14e44af232baa86a966.

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/DDMStructureManagementToolbarPropsTransformer.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/DDMStructureManagementToolbarPropsTransformer.js
@@ -14,8 +14,6 @@
 
 import {getCheckedCheckboxes, postForm} from 'frontend-js-web';
 
-import openConfirm from './openConfirm';
-
 export default function propsTransformer({
 	additionalProps: {deleteStructuresURL},
 	portletNamespace,
@@ -25,34 +23,33 @@ export default function propsTransformer({
 		...otherProps,
 		onActionButtonClick(event, {item}) {
 			if (item?.data?.action === 'deleteStructures') {
-				openConfirm({
-					message: Liferay.Language.get(
-						'are-you-sure-you-want-to-delete-this'
-					),
-					onConfirm: (isConfirmed) => {
-						if (isConfirmed) {
-							const form = document.getElementById(
-								`${portletNamespace}fm`
-							);
+				if (
+					confirm(
+						Liferay.Language.get(
+							'are-you-sure-you-want-to-delete-this'
+						)
+					)
+				) {
+					const form = document.getElementById(
+						`${portletNamespace}fm`
+					);
 
-							const searchContainer = document.getElementById(
-								`${portletNamespace}entriesContainer`
-							);
+					const searchContainer = document.getElementById(
+						`${portletNamespace}entriesContainer`
+					);
 
-							if (form && searchContainer) {
-								postForm(form, {
-									data: {
-										deleteStructureIds: getCheckedCheckboxes(
-											searchContainer,
-											`${portletNamespace}allRowIds`
-										),
-									},
-									url: deleteStructuresURL,
-								});
-							}
-						}
-					},
-				});
+					if (form && searchContainer) {
+						postForm(form, {
+							data: {
+								deleteStructureIds: getCheckedCheckboxes(
+									searchContainer,
+									`${portletNamespace}allRowIds`
+								),
+							},
+							url: deleteStructuresURL,
+						});
+					}
+				}
 			}
 		},
 	};
