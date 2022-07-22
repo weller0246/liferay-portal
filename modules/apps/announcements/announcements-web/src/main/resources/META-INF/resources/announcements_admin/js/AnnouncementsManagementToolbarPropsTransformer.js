37234acd45e75f7bad22b2c482df0da3720088b8
Brian Chan <brian.chan@liferay.com>
2022-07-22 09:38:19 -0300

    Revert " LPS-148661 Use conditionally `openConfirmModal` on `announcements-web`"
    This reverts commit 82b7c7dee09ac23fdce57ee25420de925e6bdf86.

--- modules/apps/announcements/announcements-web/src/main/resources/META-INF/resources/announcements_admin/js/AnnouncementsManagementToolbarPropsTransformer.js
+++ modules/apps/announcements/announcements-web/src/main/resources/META-INF/resources/announcements_admin/js/AnnouncementsManagementToolbarPropsTransformer.js
@@ -12,17 +12,6 @@
  * details.
  */
 
-import {openConfirmModal} from 'frontend-js-web';
-
-function openConfirm({message, onConfirm}) {
-	if (Liferay.FeatureFlags.enableCustomDialogs) {
-		openConfirmModal({message, onConfirm});
-	}
-	else if (confirm(message)) {
-		onConfirm(true);
-	}
-}
-
 export default function propsTransformer({
 	additionalProps: {deleteEntriesURL, inputId, inputValue},
 	portletNamespace,
@@ -32,34 +21,33 @@ export default function propsTransformer({
 		...props,
 		onActionButtonClick(event, {item}) {
 			if (item?.data?.action === 'deleteEntries') {
-				openConfirm({
-					message: Liferay.Language.get(
-						'are-you-sure-you-want-to-delete-the-selected-entries'
-					),
-					onConfirm: (isConfirmed) => {
-						if (isConfirmed) {
-							const form = document.getElementById(
-								`${portletNamespace}fm`
-							);
+				if (
+					confirm(
+						Liferay.Language.get(
+							'are-you-sure-you-want-to-delete-the-selected-entries'
+						)
+					)
+				) {
+					const form = document.getElementById(
+						`${portletNamespace}fm`
+					);
 
-							if (!form) {
-								return;
-							}
+					if (!form) {
+						return;
+					}
 
-							form.setAttribute('method', 'post');
+					form.setAttribute('method', 'post');
 
-							const inputElement = document.getElementById(
-								`${portletNamespace}${inputId}`
-							);
+					const inputElement = document.getElementById(
+						`${portletNamespace}${inputId}`
+					);
 
-							if (inputElement) {
-								inputElement.setAttribute('value', inputValue);
-							}
+					if (inputElement) {
+						inputElement.setAttribute('value', inputValue);
+					}
 
-							submitForm(form, deleteEntriesURL);
-						}
-					},
-				});
+					submitForm(form, deleteEntriesURL);
+				}
 			}
 		},
 	};
