6569b3c9129dc7f076f52b2c0dd6319cd0f87a3d
Joao Silva <joao.silva@liferay.com>
2022-07-26 16:54:01 -0300

    LPS-159021 Components Date Picker

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/services/liferay.ts
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+interface IThemeDisplay {
+	getBCP47LanguageId(): () => string;
+	getCompanyGroupId: () => number;
+	getDefaultLanguageId: () => string;
+	getLanguageId: () => string;
+	getPathThemeImages: () => string;
+	getScopeGroupId: () => number;
+	getSiteGroupId: () => number;
+	getUserId: () => string;
+	getUserName: () => string;
+}
+interface LiferayUtil {
+	openToast: (options?: any) => void;
+}
+interface ILiferay {
+	ThemeDisplay: IThemeDisplay;
+	Util: LiferayUtil;
+	authToken: string;
+}
+declare global {
+	interface Window {
+		Liferay: ILiferay;
+	}
+}
+export const Liferay = window.Liferay || {
+	ThemeDisplay: {
+		getBCP47LanguageId: () => 'en-US',
+		getCompanyGroupId: () => 0,
+		getDefaultLanguageId: () => 'en_US',
+		getLanguageId: () => 'en_US',
+		getPathThemeImages: () => '',
+		getScopeGroupId: () => 0,
+		getSiteGroupId: () => 0,
+		getUserId: () => '0',
+		getUserName: () => 'Test Test',
+	},
+	authToken: '',
+};
---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/utils/getIconSpriteMap.ts
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import iconSVG from '@clayui/css/lib/images/icons/icons.svg';
+
+import {Liferay} from '../services/liferay';
+
+export default function getIconSpriteMap() {
+	const pathThemeImages = Liferay.ThemeDisplay.getPathThemeImages();
+	if (pathThemeImages) {
+		return `${pathThemeImages}/clay/icons.svg`;
+	}
+
+	return iconSVG;
+}
