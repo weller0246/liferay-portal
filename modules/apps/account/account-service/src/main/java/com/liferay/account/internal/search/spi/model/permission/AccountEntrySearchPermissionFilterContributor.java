2e70600d75c9871a0e571299119479c03964f82a
drewbrokke <drew.brokke@liferay.com>
2022-07-26 10:49:54 -0500

    LPS-156573 account-service: checks for the MANAGE_AVAILABLE_ACCOUNTS permission during search

--- modules/apps/account/account-service/src/main/java/com/liferay/account/internal/search/spi/model/permission/AccountEntrySearchPermissionFilterContributor.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/internal/search/spi/model/permission/AccountEntrySearchPermissionFilterContributor.java
@@ -22,18 +22,20 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.OrganizationConstants;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.search.BaseModelSearchResult;
 import com.liferay.portal.kernel.search.BooleanClauseOccur;
 import com.liferay.portal.kernel.search.filter.BooleanFilter;
 import com.liferay.portal.kernel.search.filter.TermsFilter;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.OrganizationLocalService;
-import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.permission.OrganizationPermission;
 import com.liferay.portal.kernel.util.LinkedHashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.search.spi.model.permission.SearchPermissionFilterContributor;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -78,20 +80,51 @@ public class AccountEntrySearchPermissionFilterContributor
 			"organizationIds");
 
 		try {
+			Set<Organization> organizationsSet = new HashSet<>();
+
+			for (Organization organization :
+					_organizationLocalService.getUserOrganizations(userId)) {
+
+				boolean hasManageAvailableAccountsPermission =
+					_organizationPermission.contains(
+						permissionChecker, organization.getOrganizationId(),
+						AccountActionKeys.MANAGE_AVAILABLE_ACCOUNTS);
+
+				if (hasManageAvailableAccountsPermission ||
+					_organizationPermission.contains(
+						permissionChecker, organization,
+						AccountActionKeys.MANAGE_ACCOUNTS)) {
+
+					organizationsSet.add(organization);
+				}
+
+				if (hasManageAvailableAccountsPermission ||
+					_organizationPermission.contains(
+						permissionChecker, organization,
+						AccountActionKeys.MANAGE_SUBORGANIZATIONS_ACCOUNTS)) {
+
+					List<Organization> suborganizations =
+						_organizationLocalService.getSuborganizations(
+							organization.getCompanyId(),
+							organization.getOrganizationId());
+
+					while (suborganizations.size() > 0) {
+						organizationsSet.addAll(suborganizations);
+
+						suborganizations =
+							_organizationLocalService.getSuborganizations(
+								suborganizations);
+					}
+				}
+			}
+
 			BaseModelSearchResult<Organization> baseModelSearchResult =
 				_organizationLocalService.searchOrganizations(
 					companyId, OrganizationConstants.ANY_PARENT_ORGANIZATION_ID,
 					null,
 					LinkedHashMapBuilder.<String, Object>put(
 						"accountsOrgsTree",
-						() -> {
-							User user = _userLocalService.getUser(userId);
-
-							return ListUtil.filter(
-								user.getOrganizations(true),
-								organization -> _hasManageAccountsPermission(
-									permissionChecker, organization));
-						}
+						ListUtil.fromCollection(organizationsSet)
 					).build(),
 					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
 
@@ -112,25 +145,6 @@ public class AccountEntrySearchPermissionFilterContributor
 		}
 	}
 
-	private boolean _hasManageAccountsPermission(
-		PermissionChecker permissionChecker, Organization organization) {
-
-		try {
-			_organizationPermission.check(
-				permissionChecker, organization,
-				AccountActionKeys.MANAGE_ACCOUNTS);
-		}
-		catch (PortalException portalException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(portalException);
-			}
-
-			return false;
-		}
-
-		return true;
-	}
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		AccountEntrySearchPermissionFilterContributor.class);
 
@@ -139,8 +153,5 @@ public class AccountEntrySearchPermissionFilterContributor
 
 	@Reference
 	private OrganizationPermission _organizationPermission;
-
-	@Reference
-	private UserLocalService _userLocalService;
 
 }
