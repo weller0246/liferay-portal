7fef21edd2ddd0dfec2f35258ece69cf39563ce2
Alan Huang <alan.huang@liferay.com>
2022-07-27 08:17:02 +0800

    Revert "LPS-147681 Add javax.portlet.version=3.0"
    This reverts commit d4ee3fe4ea536d82b8809766b3ef41ee2ac81366.

--- modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryLinkInstanceableTestPortlet.java
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable-test/src/testIntegration/java/com/liferay/fragment/entry/processor/editable/test/FragmentEntryLinkInstanceableTestPortlet.java
@@ -33,8 +33,7 @@ import org.osgi.service.component.annotations.Component;
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.scopeable=true", "javax.portlet.display-name=Test",
 		"javax.portlet.expiration-cache=0",
-		"javax.portlet.name=" + FragmentEntryLinkPortletKeys.FRAGMENT_ENTRY_LINK_INSTANCEABLE_TEST_PORTLET,
-		"javax.portlet.version=3.0"
+		"javax.portlet.name=" + FragmentEntryLinkPortletKeys.FRAGMENT_ENTRY_LINK_INSTANCEABLE_TEST_PORTLET
 	},
 	service = Portlet.class
 )
--- modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/FragmentPortlet.java
+++ modules/apps/fragment/fragment-web/src/main/java/com/liferay/fragment/web/internal/portlet/FragmentPortlet.java
@@ -73,8 +73,7 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + FragmentPortletKeys.FRAGMENT,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator",
-		"javax.portlet.version=3.0"
+		"javax.portlet.security-role-ref=administrator"
 	},
 	service = Portlet.class
 )
