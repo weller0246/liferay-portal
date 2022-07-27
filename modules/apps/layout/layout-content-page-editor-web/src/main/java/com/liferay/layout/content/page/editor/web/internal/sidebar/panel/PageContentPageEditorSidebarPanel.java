1958062a9ca4d209474f1612a4537725160e773c
Victor Galan <victor.galan@liferay.com>
2022-07-27 13:44:23 +0200

    LPS-153356 Add new panel

---
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/sidebar/panel/PageContentPageEditorSidebarPanel.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.content.page.editor.web.internal.sidebar.panel;
+
+import com.liferay.layout.content.page.editor.sidebar.panel.ContentPageEditorSidebarPanel;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.permission.LayoutPermission;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	configurationPid = "com.liferay.layout.content.page.editor.web.internal.configuration.FFLayoutContentPageEditorConfiguration",
+	immediate = true, property = "service.ranking:Integer=600",
+	service = ContentPageEditorSidebarPanel.class
+)
+public class PageContentPageEditorSidebarPanel
+	implements ContentPageEditorSidebarPanel {
+
+	@Override
+	public String getIcon() {
+		return "paperclip";
+	}
+
+	@Override
+	public String getId() {
+		return "page-content";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(resourceBundle, "page-content");
+	}
+
+	@Override
+	public boolean isVisible(
+		PermissionChecker permissionChecker, long plid, int layoutType) {
+
+		if (!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-153452"))) {
+			return false;
+		}
+
+		try {
+			if (_layoutPermission.containsLayoutUpdatePermission(
+					permissionChecker, plid)) {
+
+				return true;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PageContentPageEditorSidebarPanel.class);
+
+	@Reference
+	private LayoutPermission _layoutPermission;
+
+}
