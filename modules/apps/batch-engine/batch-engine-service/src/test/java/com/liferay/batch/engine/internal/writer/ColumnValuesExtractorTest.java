c9996fb66568ff89b23a7740b2e9a96e32752f49
Matija Petanjek <matija.petanjek@liferay.com>
2022-07-28 16:53:29 +0200

    COMMERCE-9736 batch-engine-service - csvParser parses string representation of array so that every element is a new column value

--- modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/writer/ColumnValuesExtractorTest.java
+++ modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/writer/ColumnValuesExtractorTest.java
@@ -53,13 +53,11 @@ public class ColumnValuesExtractorTest {
 		List<Object> objects = columnValuesExtractor.extractValues(
 			arrayContainer);
 
+		Assert.assertEquals(objects.toString(), 3, objects.size());
+
 		Assert.assertEquals(Integer.valueOf(5), objects.get(1));
 
 		CSVRecord csvRecord = _parseCSV((String)objects.get(0));
-
-		Assert.assertEquals(1, csvRecord.size());
-
-		csvRecord = _parseCSV(csvRecord.get(0));
 
 		Assert.assertEquals(5, csvRecord.size());
 
@@ -70,10 +68,6 @@ public class ColumnValuesExtractorTest {
 
 		csvRecord = _parseCSV((String)objects.get(2));
 
-		Assert.assertEquals(1, csvRecord.size());
-
-		csvRecord = _parseCSV(csvRecord.get(0));
-
 		Assert.assertEquals(5, csvRecord.size());
 
 		for (int i = 0; i < arrayContainer.length; i++) {
@@ -82,10 +76,10 @@ public class ColumnValuesExtractorTest {
 	}
 
 	private CSVRecord _parseCSV(String stringToParse) throws Exception {
-		CSVParser csvRecords = new CSVParser(
+		CSVParser csvParser = new CSVParser(
 			new StringReader(stringToParse), CSVFormat.DEFAULT);
 
-		List<CSVRecord> records = csvRecords.getRecords();
+		List<CSVRecord> records = csvParser.getRecords();
 
 		if (records.isEmpty()) {
 			throw new IllegalArgumentException(
