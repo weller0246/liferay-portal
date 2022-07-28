c9dea017184da7702288c8852aa620770c4d5d07
Brian Chan <brian.chan@liferay.com>
2022-07-28 10:07:23 -0300

    Revert "LPS-157670 LPP-41834 Use memory"
    This reverts commit 4176edca7ea9574e63805addede8d128ba6abdf2.

--- portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeLayout.java
+++ portal-impl/src/com/liferay/portal/upgrade/v7_0_0/UpgradeLayout.java
@@ -14,18 +14,9 @@
 
 package com.liferay.portal.upgrade.v7_0_0;
 
-import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.LoggingTimer;
-import com.liferay.portal.kernel.util.StringBundler;
-
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * @author Jonathan McCann
@@ -44,52 +35,10 @@ public class UpgradeLayout extends UpgradeProcess {
 	}
 
 	protected void deleteOrphanedFriendlyURL() throws Exception {
-		Set<Long> plids = new HashSet<>();
-
-		try (LoggingTimer loggingTimer = new LoggingTimer("step1");
-			PreparedStatement preparedStatement = connection.prepareStatement(
-				"select plid from Layout");
-			ResultSet resultSet = preparedStatement.executeQuery()) {
-
-			while (resultSet.next()) {
-				long plid = resultSet.getLong("plid");
-
-				plids.add(plid);
-			}
-		}
-
-		int count = 0;
-
-		try (LoggingTimer loggingTimer = new LoggingTimer("step2");
-			PreparedStatement preparedStatement1 = connection.prepareStatement(
-				"select plid from LayoutFriendlyURL");
-			ResultSet resultSet = preparedStatement1.executeQuery()) {
-
-			try (PreparedStatement preparedStatement2 =
-					AutoBatchPreparedStatementUtil.autoBatch(
-						connection,
-						"delete from LayoutFriendlyURL where plid = ?")) {
-
-				while (resultSet.next()) {
-					long plid = resultSet.getLong("plid");
-
-					if (plids.contains(plid)) {
-						continue;
-					}
-
-					preparedStatement2.setLong(1, plid);
-
-					preparedStatement2.addBatch();
-
-					count++;
-				}
-
-				preparedStatement2.executeBatch();
-			}
-		}
-
-		if (_log.isInfoEnabled()) {
-			_log.info("Deleted " + count + " layout friendly URLs");
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+			runSQL(
+				"delete from LayoutFriendlyURL where plid not in (select " +
+					"plid from Layout)");
 		}
 	}
 
@@ -120,7 +69,5 @@ public class UpgradeLayout extends UpgradeProcess {
 					"layoutPrototypeLinkEnabled = [$FALSE$]"));
 		}
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(UpgradeLayout.class);
 
 }
