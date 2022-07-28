e8063ce77338196bb068b93578882da3bcb375f2
Brian Chan <brian.chan@liferay.com>
2022-07-28 20:18:45 -0300

    LPS-147889 This exposed a bug in LPS-68555 at 9ed5e7950b9911c63c244e27129580b621924677 where there was a typo

--- modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/v2_0_0/KBCommentUpgradeProcess.java
+++ modules/apps/knowledge-base/knowledge-base-service/src/main/java/com/liferay/knowledge/base/internal/upgrade/v2_0_0/KBCommentUpgradeProcess.java
@@ -24,7 +24,7 @@ public class KBCommentUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		alterTableAddColumn("KBComment", "status", "INT null");
+		alterTableAddColumn("KBComment", "status", "INTEGER");
 
 		runSQL(
 			"update KBComment set status = " +
