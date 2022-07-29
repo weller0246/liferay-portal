78cd2a6c7d654f3c32d0838052d66f2305dff492
drewbrokke <drew.brokke@liferay.com>
2022-07-29 09:12:50 -0500

    LPS-159181 portal-impl: if the http method is not GET, allow the user to use the parameter if signed in

--- portal-impl/src/com/liferay/portal/events/ServicePreAction.java
+++ portal-impl/src/com/liferay/portal/events/ServicePreAction.java
@@ -992,7 +992,12 @@ public class ServicePreAction extends Action {
 							httpServletRequest),
 						"p_l_id");
 
-					if (originalPlid == plid) {
+					String method = httpServletRequest.getMethod();
+
+					if ((Objects.equals(method, "GET") &&
+						 (originalPlid == plid)) ||
+						(!Objects.equals(method, "GET") && !signedIn)) {
+
 						String message =
 							"User layouts cannot be accessed via p_l_id";
 
