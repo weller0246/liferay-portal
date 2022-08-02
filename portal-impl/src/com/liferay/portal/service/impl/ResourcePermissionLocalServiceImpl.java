8dea19b9cb929abed32ead99447d7b72368c29de
Brian Chan <brian.chan@liferay.com>
2022-08-02 19:11:15 -0300

    LPS-157759 not needed

--- portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portal/service/impl/ResourcePermissionLocalServiceImpl.java
@@ -337,8 +337,6 @@ public class ResourcePermissionLocalServiceImpl
 	 *        optionally an empty string if no instance exists
 	 * @param portletActions whether to associate portlet actions with the
 	 *        resource
-	 * @param serviceContext the service context to be applied. Can set group
-	 *        and guest permissions.
 	 */
 	@Override
 	public void addResourcePermissions(
