6cf77b90cb4274ab23d570de9ace4f7c27e668bf
Julius Lee <meng-huan.lee@liferay.com>
2022-08-02 16:17:24 -0700

    LPS-158489 Remove unused method in JavaMailUtil

--- modules/apps/petra/petra-mail/src/main/java/com/liferay/petra/mail/JavaMailUtil.java
+++ modules/apps/petra/petra-mail/src/main/java/com/liferay/petra/mail/JavaMailUtil.java
@@ -14,17 +14,12 @@
 
 package com.liferay.petra.mail;
 
-import com.liferay.petra.string.StringBundler;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 
 import java.io.IOException;
 
-import javax.mail.Address;
 import javax.mail.MessagingException;
 import javax.mail.Part;
-import javax.mail.internet.InternetAddress;
 
 /**
  * @author Brian Wing Shun Chan
@@ -36,30 +31,6 @@ public class JavaMailUtil {
 		throws IOException, MessagingException {
 
 		return FileUtil.getBytes(part.getInputStream());
-	}
-
-	public static String toUnicodeString(Address[] addresses) {
-		return toUnicodeString((InternetAddress[])addresses);
-	}
-
-	public static String toUnicodeString(InternetAddress[] addresses) {
-		if (ArrayUtil.isEmpty(addresses)) {
-			return StringPool.BLANK;
-		}
-
-		StringBundler sb = new StringBundler((addresses.length * 2) - 1);
-
-		for (int i = 0; i < addresses.length; i++) {
-			if (addresses[i] != null) {
-				sb.append(addresses[i].toUnicodeString());
-			}
-
-			if ((i + 1) != addresses.length) {
-				sb.append(", ");
-			}
-		}
-
-		return sb.toString();
 	}
 
 }
