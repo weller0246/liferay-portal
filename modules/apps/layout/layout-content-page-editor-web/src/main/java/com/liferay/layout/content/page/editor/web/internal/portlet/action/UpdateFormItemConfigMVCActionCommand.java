17c098aae7909702074159b592d3fcd0948b8620
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-08-02 12:10:27 +0200

    LPS-159515 Fix typo

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFormItemConfigMVCActionCommand.java
@@ -428,7 +428,7 @@ public class UpdateFormItemConfigMVCActionCommand extends BaseMVCActionCommand {
 					previousFormStyledLayoutStructureItem.getClassNameId(),
 					formStyledLayoutStructureItem.getClassNameId()) ||
 				 !Objects.equals(
-					 previousFormStyledLayoutStructureItem.getClassNameId(),
+					 previousFormStyledLayoutStructureItem.getClassTypeId(),
 					 formStyledLayoutStructureItem.getClassTypeId()))) {
 
 				removedLayoutStructureItemsJSONArray =
