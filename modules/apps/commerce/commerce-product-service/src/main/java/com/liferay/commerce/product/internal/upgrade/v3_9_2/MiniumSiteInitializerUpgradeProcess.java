dad30577ae84b93e55c6dfb32fcd4c50d4f86770
Brian Chan <brian.chan@liferay.com>
2022-08-03 13:05:18 -0300

    COMMERCE-9224 Refactor of the upgrade process to make it easier to read

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v3_9_2/MiniumSiteInitializerUpgradeProcess.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v3_9_2/MiniumSiteInitializerUpgradeProcess.java
@@ -39,88 +39,35 @@ public class MiniumSiteInitializerUpgradeProcess extends UpgradeProcess {
 
 	@Override
 	public void doUpgrade() throws Exception {
-		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
-			"select siteGroupId from CommerceChannel where siteGroupId " +
-			"in (select groupId from LayoutSet where privateLayout = ? and " +
-			"themeId = 'minium_WAR_miniumtheme')");
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"select siteGroupId from CommerceChannel where siteGroupId " +
+					"in (select groupId from LayoutSet where privateLayout = " +
+						"? and themeId = 'minium_WAR_miniumtheme')")) {
 
-			 PreparedStatement preparedStatement2 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "select plid from Layout where groupId = ? " +
-					 "and privateLayout = ? ORDER by " +
-					 "layoutId");
+			preparedStatement.setBoolean(1, true);
 
-			 PreparedStatement preparedStatement3 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "update Layout set layoutId = ?, privateLayout = ? " +
-					 "where groupId = ? and plid = ? and privateLayout = ?");
-
-			 PreparedStatement preparedStatement4 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "update LayoutFriendlyURL set privateLayout = ? " +
-					 "where groupId = ? and plid = ? and privateLayout = ?");
-
-			 PreparedStatement preparedStatement5 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "select max(layoutId) from Layout where groupId = ? " +
-					 "and privateLayout = ?");
-
-			 PreparedStatement preparedStatement6 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "select layoutId, parentLayoutId from Layout where " +
-					 "groupId = ? and layoutId > ? and parentLayoutId > 0");
-
-			 PreparedStatement preparedStatement7 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "update Layout set parentLayoutId = ? where " +
-					 "groupId = ? and layoutId = ? and parentLayoutId > 0");
-
-			 PreparedStatement preparedStatement8 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "select plid from Layout where friendlyURL != '/login' " +
-					 "and groupId = ? and parentPlid = 0 and type_ = " +
-					 "'portlet' order by priority");
-
-			 PreparedStatement preparedStatement9 =
-				 AutoBatchPreparedStatementUtil.autoBatch(
-					 connection,
-					 "update Layout set priority = ? where groupId = ? and " +
-					 "plid = ?")) {
-
-			preparedStatement1.setBoolean(1, true);
-
-			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
 				while (resultSet.next()) {
 					long siteGroupId = resultSet.getLong("siteGroupId");
 
-					long maxLayoutId = _getMaxLayoutId(
-						preparedStatement5, siteGroupId);
+					long maxLayoutId = _getMaxLayoutId(siteGroupId);
 
-					_updateLayoutIds(
-						preparedStatement2, preparedStatement3,
-						preparedStatement4, siteGroupId);
+					_updateLayoutIds(siteGroupId);
 
-					_updateParentLayoutIds(
-						maxLayoutId, preparedStatement6, preparedStatement7,
-						siteGroupId);
+					_updateParentLayoutIds(maxLayoutId, siteGroupId);
 
-					_updateLayoutPriorities(
-						preparedStatement8, preparedStatement9, siteGroupId);
+					_updateLayoutPriorities(siteGroupId);
 				}
 			}
 		}
 	}
 
-	private long _getMaxLayoutId(
-			PreparedStatement preparedStatement, long siteGroupId)
-		throws SQLException {
+	private long _getMaxLayoutId(long siteGroupId) throws SQLException {
+		PreparedStatement preparedStatement =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"select max(layoutId) from Layout where groupId = ? and " +
+					"privateLayout = ?");
 
 		preparedStatement.setLong(1, siteGroupId);
 		preparedStatement.setBoolean(2, false);
@@ -134,11 +81,22 @@ public class MiniumSiteInitializerUpgradeProcess extends UpgradeProcess {
 		return 0;
 	}
 
-	private void _updateLayoutIds(
-			PreparedStatement preparedStatement1,
-			PreparedStatement preparedStatement2,
-			PreparedStatement preparedStatement3, long siteGroupId)
-		throws SQLException {
+	private void _updateLayoutIds(long siteGroupId) throws SQLException {
+		PreparedStatement preparedStatement1 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"select plid from Layout where groupId = ? and privateLayout " +
+					"= ? ORDER by layoutId");
+		PreparedStatement preparedStatement2 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"update Layout set layoutId = ?, privateLayout = ? where " +
+					"groupId = ? and plid = ? and privateLayout = ?");
+		PreparedStatement preparedStatement3 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"update LayoutFriendlyURL set privateLayout = ? where " +
+					"groupId = ? and plid = ? and privateLayout = ?");
 
 		preparedStatement1.setLong(1, siteGroupId);
 
@@ -182,10 +140,18 @@ public class MiniumSiteInitializerUpgradeProcess extends UpgradeProcess {
 		preparedStatement3.executeBatch();
 	}
 
-	private void _updateLayoutPriorities(
-			PreparedStatement preparedStatement1,
-			PreparedStatement preparedStatement2, long siteGroupId)
-		throws SQLException {
+	private void _updateLayoutPriorities(long siteGroupId) throws SQLException {
+		PreparedStatement preparedStatement1 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"select plid from Layout where friendlyURL != '/login' and " +
+					"groupId = ? and parentPlid = 0 and type_ = 'portlet' " +
+						"order by priority");
+		PreparedStatement preparedStatement2 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"update Layout set priority = ? where groupId = ? and plid = " +
+					"?");
 
 		preparedStatement1.setLong(1, siteGroupId);
 
@@ -210,10 +176,19 @@ public class MiniumSiteInitializerUpgradeProcess extends UpgradeProcess {
 		}
 	}
 
-	private void _updateParentLayoutIds(
-			long maxLayoutId, PreparedStatement preparedStatement1,
-			PreparedStatement preparedStatement2, long siteGroupId)
+	private void _updateParentLayoutIds(long maxLayoutId, long siteGroupId)
 		throws SQLException {
+
+		PreparedStatement preparedStatement1 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"select layoutId, parentLayoutId from Layout where groupId = " +
+					"? and layoutId > ? and parentLayoutId > 0");
+		PreparedStatement preparedStatement2 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"update Layout set parentLayoutId = ? where groupId = ? and " +
+					"layoutId = ? and parentLayoutId > 0");
 
 		preparedStatement1.setLong(1, siteGroupId);
 		preparedStatement1.setLong(2, maxLayoutId);
