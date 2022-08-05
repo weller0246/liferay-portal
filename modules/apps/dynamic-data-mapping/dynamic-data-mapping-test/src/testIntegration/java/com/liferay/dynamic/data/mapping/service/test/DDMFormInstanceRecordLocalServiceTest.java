a815486d8cfdfebaeadfb7ec91767a3b43f3ed90
Rodrigo Paulino <rodrigo.paulino@liferay.com>
2022-08-05 15:52:22 -0300

    LPS-157363 Add integration test

---
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/service/test/DDMFormInstanceRecordLocalServiceTest.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.dynamic.data.mapping.service.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstance;
+import com.liferay.dynamic.data.mapping.model.DDMFormInstanceRecord;
+import com.liferay.dynamic.data.mapping.model.DDMStructure;
+import com.liferay.dynamic.data.mapping.model.Value;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMFormInstanceRecordLocalService;
+import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
+import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormInstanceTestUtil;
+import com.liferay.dynamic.data.mapping.test.util.DDMFormValuesTestUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Rodrigo Paulino
+ */
+@RunWith(Arquillian.class)
+public class DDMFormInstanceRecordLocalServiceTest
+	extends BaseDDMServiceTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Test
+	public void testUpdateFormInstanceRecord() throws Exception {
+		User user = TestPropsValues.getUser();
+
+		DDMFormInstance ddmFormInstance =
+			DDMFormInstanceTestUtil.addDDMFormInstance(
+				user.getGroup(), user.getUserId());
+
+		DDMFormValues ddmFormValues = DDMFormValuesTestUtil.createDDMFormValues(
+			ddmFormInstance.getDDMForm());
+
+		DDMFormFieldValue ddmFormFieldValue =
+			DDMFormValuesTestUtil.createLocalizedDDMFormFieldValue(
+				"text", "first text");
+
+		ddmFormValues.addDDMFormFieldValue(ddmFormFieldValue);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext();
+
+		DDMFormInstanceRecord ddmFormInstanceRecord =
+			_ddmFormInstanceRecordLocalService.addFormInstanceRecord(
+				user.getUserId(), user.getGroupId(),
+				ddmFormInstance.getFormInstanceId(), ddmFormValues,
+				serviceContext);
+
+		_assertDDMFormInstanceRecord(
+			ddmFormInstanceRecord, "1.0", "first text");
+
+		Value value = ddmFormFieldValue.getValue();
+
+		value.addString(value.getDefaultLocale(), "second text");
+
+		_assertDDMFormInstanceRecord(
+			_ddmFormInstanceRecordLocalService.updateFormInstanceRecord(
+				user.getUserId(),
+				ddmFormInstanceRecord.getFormInstanceRecordId(), false,
+				ddmFormValues, serviceContext),
+			"1.0", "second text");
+
+		DDMStructure ddmStructure = ddmFormInstance.getStructure();
+
+		_ddmFormInstanceLocalService.updateFormInstance(
+			ddmFormInstance.getFormInstanceId(), ddmStructure.getStructureId(),
+			ddmFormInstance.getNameMap(), ddmFormInstance.getDescriptionMap(),
+			ddmFormInstance.getSettingsDDMFormValues(), serviceContext);
+
+		value.addString(value.getDefaultLocale(), "third text");
+
+		_assertDDMFormInstanceRecord(
+			_ddmFormInstanceRecordLocalService.updateFormInstanceRecord(
+				user.getUserId(),
+				ddmFormInstanceRecord.getFormInstanceRecordId(), false,
+				ddmFormValues, serviceContext),
+			"1.1", "third text");
+	}
+
+	private void _assertDDMFormInstanceRecord(
+			DDMFormInstanceRecord ddmFormInstanceRecord,
+			String expectedFormInstanceVersion, String expectedValue)
+		throws Exception {
+
+		Assert.assertEquals(
+			expectedFormInstanceVersion,
+			ddmFormInstanceRecord.getFormInstanceVersion());
+
+		DDMFormValues ddmFormValues = ddmFormInstanceRecord.getDDMFormValues();
+
+		Map<String, List<DDMFormFieldValue>> ddmFormFieldValuesMap =
+			ddmFormValues.getDDMFormFieldValuesMap(true);
+
+		List<DDMFormFieldValue> ddmFormFieldValues = ddmFormFieldValuesMap.get(
+			"text");
+
+		DDMFormFieldValue ddmFormFieldValue = ddmFormFieldValues.get(0);
+
+		Value value = ddmFormFieldValue.getValue();
+
+		Assert.assertEquals(
+			expectedValue, value.getString(value.getDefaultLocale()));
+	}
+
+	@Inject
+	private DDMFormInstanceLocalService _ddmFormInstanceLocalService;
+
+	@Inject
+	private DDMFormInstanceRecordLocalService
+		_ddmFormInstanceRecordLocalService;
+
+}
