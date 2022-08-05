17d592597cb4ba1f197e890662bc3426510c17cd
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-08-05 11:02:46 -0300

    LPS-159861 Create GroovyScriptingValidator and its implementation

---
+++ modules/apps/object/object-runtime-scripting-impl/src/main/java/com/liferay/object/runtime/scripting/internal/validator/GroovyScriptingValidatorImpl.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.runtime.scripting.internal.validator;
+
+import com.liferay.object.runtime.scripting.exception.GroovyScriptingException;
+import com.liferay.object.runtime.scripting.validator.GroovyScriptingValidator;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+
+import groovy.lang.GroovyShell;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(immediate = true, service = GroovyScriptingValidator.class)
+public class GroovyScriptingValidatorImpl implements GroovyScriptingValidator {
+
+	@Override
+	public void validate(String script) throws GroovyScriptingException {
+		if (StringUtil.count(script, StringPool.NEW_LINE) >
+				_MAXIMUM_NUMBER_OF_LINES) {
+
+			throw new GroovyScriptingException(
+				"the-maximum-number-of-lines-available-is-2987");
+		}
+
+		try {
+			Class<?> clazz = getClass();
+
+			GroovyShell groovyShell = new GroovyShell(clazz.getClassLoader());
+
+			groovyShell.parse(script);
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			throw new GroovyScriptingException("syntax-error");
+		}
+	}
+
+	private static final int _MAXIMUM_NUMBER_OF_LINES = 2987;
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		GroovyScriptingValidatorImpl.class);
+
+}
