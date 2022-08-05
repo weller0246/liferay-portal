c5e659e96ccdc355781d7237404f14aa3d0569af
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-08-05 11:03:54 +0200

    LPS-160325 Execute on cluster to synchronize tokens as it is done in NonceUtil.

--- modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/src/main/java/com/liferay/document/library/opener/onedrive/web/internal/oauth/AccessTokenStoreUtil.java
+++ modules/dxp/apps/document-library-opener/document-library-opener-onedrive-web/src/main/java/com/liferay/document/library/opener/onedrive/web/internal/oauth/AccessTokenStoreUtil.java
@@ -14,6 +14,11 @@
 
 package com.liferay.document.library.opener.onedrive.web.internal.oauth;
 
+import com.liferay.portal.kernel.cluster.ClusterExecutorUtil;
+import com.liferay.portal.kernel.cluster.ClusterRequest;
+import com.liferay.portal.kernel.util.MethodHandler;
+import com.liferay.portal.kernel.util.MethodKey;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -27,19 +32,22 @@ public class AccessTokenStoreUtil {
 	public static void add(
 		long companyId, long userId, AccessToken accessToken) {
 
-		Map<Long, AccessToken> companyAccessTokens =
-			_accessTokenMap.computeIfAbsent(
-				companyId, key -> new ConcurrentHashMap<>());
+		_add(companyId, userId, accessToken);
 
-		companyAccessTokens.put(userId, accessToken);
+		if (ClusterExecutorUtil.isEnabled()) {
+			_executeOnCluster(
+				new MethodHandler(
+					_addMethodKey, companyId, userId, accessToken));
+		}
 	}
 
 	public static void delete(long companyId, long userId) {
-		Map<Long, AccessToken> companyAccessTokens =
-			_accessTokenMap.computeIfAbsent(
-				companyId, key -> new ConcurrentHashMap<>());
+		_delete(companyId, userId);
 
-		companyAccessTokens.remove(userId);
+		if (ClusterExecutorUtil.isEnabled()) {
+			_executeOnCluster(
+				new MethodHandler(_deleteMethodKey, companyId, userId));
+		}
 	}
 
 	public static Optional<AccessToken> getAccessTokenOptional(
@@ -51,7 +59,39 @@ public class AccessTokenStoreUtil {
 		return Optional.ofNullable(companyAccessTokens.get(userId));
 	}
 
+	private static void _add(
+		long companyId, long userId, AccessToken accessToken) {
+
+		Map<Long, AccessToken> companyAccessTokens =
+			_accessTokenMap.computeIfAbsent(
+				companyId, key -> new ConcurrentHashMap<>());
+
+		companyAccessTokens.put(userId, accessToken);
+	}
+
+	private static void _delete(long companyId, long userId) {
+		Map<Long, AccessToken> companyAccessTokens =
+			_accessTokenMap.computeIfAbsent(
+				companyId, key -> new ConcurrentHashMap<>());
+
+		companyAccessTokens.remove(userId);
+	}
+
+	private static void _executeOnCluster(MethodHandler methodHandler) {
+		ClusterRequest clusterRequest = ClusterRequest.createMulticastRequest(
+			methodHandler, true);
+
+		clusterRequest.setFireAndForget(true);
+
+		ClusterExecutorUtil.execute(clusterRequest);
+	}
+
 	private static final Map<Long, Map<Long, AccessToken>> _accessTokenMap =
 		new ConcurrentHashMap<>();
+	private static final MethodKey _addMethodKey = new MethodKey(
+		AccessTokenStoreUtil.class, "_add", long.class, long.class,
+		AccessToken.class);
+	private static final MethodKey _deleteMethodKey = new MethodKey(
+		AccessTokenStoreUtil.class, "_delete", long.class, long.class);
 
 }
