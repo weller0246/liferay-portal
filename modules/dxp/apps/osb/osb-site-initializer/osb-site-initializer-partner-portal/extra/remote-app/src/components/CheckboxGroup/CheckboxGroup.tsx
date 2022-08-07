0cdf262fe2eb71479e1d216f796c7cdf2c16e9bd
Jair Medeiros <jair.medeiros@liferay.com>
2022-08-07 13:58:02 -0300

    LPS-159028 Add CheckboxGroup

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/components/CheckboxGroup/CheckboxGroup.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable no-console */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {ClayCheckbox} from '@clayui/form';
+import {FieldProps} from 'formik';
+
+import BasicInput, {BasicInputProps} from '../BasicInput/BasicInput';
+
+interface IProps {
+	items: React.OptionHTMLAttributes<HTMLOptionElement>[];
+}
+
+const CheckboxGroup = ({
+	field,
+	form,
+	items = [],
+	label,
+	required,
+}: IProps & BasicInputProps & FieldProps<string[]>) => (
+	<BasicInput
+		{...form.getFieldMeta(field.name)}
+		label={label}
+		required={required}
+	>
+		{items.map((item, index) => (
+			<ClayCheckbox
+				{...field}
+				checked={field.value.includes(item.label as string)}
+				key={`${item.value}-${index}`}
+				label={item.label}
+				value={item.label}
+			/>
+		))}
+	</BasicInput>
+);
+export default CheckboxGroup;
--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/components/CheckboxGroup/index.tsx
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/components/CheckboxGroup/index.tsx
@@ -9,4 +9,4 @@
  * distribution rights of the Software.
  */
 
-export {default} from './CheckBoxList';
+export {default} from './CheckboxGroup';
