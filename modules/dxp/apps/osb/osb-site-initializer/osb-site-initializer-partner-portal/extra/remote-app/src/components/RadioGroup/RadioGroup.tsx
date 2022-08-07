f58a5e8a78f5f26219d47274dc71909b9a570d41
Jair Medeiros <jair.medeiros@liferay.com>
2022-08-07 13:58:15 -0300

    LPS-159028 Add RadioGroup

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/components/RadioGroup/RadioGroup.tsx
@@ -0,0 +1,56 @@
+/* eslint-disable no-console */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {ClayRadio} from '@clayui/form';
+import {FieldProps} from 'formik';
+
+import BasicInput, {BasicInputProps} from '../BasicInput/BasicInput';
+
+interface IProps {
+	items: (React.OptionHTMLAttributes<HTMLOptionElement> & {
+		description?: string;
+	})[];
+}
+
+const RadioGroup = ({
+	field,
+	form,
+	items = [],
+	label,
+	required,
+}: IProps & BasicInputProps & FieldProps<string>) => {
+	return (
+		<BasicInput
+			{...form.getFieldMeta(field.name)}
+			label={label}
+			required={required}
+		>
+			{items.map((item, index) => (
+				<div
+					className="border border-neutral-3 mb-4 p-3 rounded-lg"
+					key={index}
+				>
+					<ClayRadio
+						{...field}
+						checked={field.value === item.value}
+						key={`${item.value}-${index}`}
+						label={item.label}
+						value={item.value as string}
+					/>
+
+					{item.description && <span>{item.description}</span>}
+				</div>
+			))}
+		</BasicInput>
+	);
+};
+export default RadioGroup;
