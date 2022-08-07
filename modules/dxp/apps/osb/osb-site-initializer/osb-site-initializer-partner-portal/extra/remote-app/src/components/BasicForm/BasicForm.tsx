cdbcc9cc71e6936efc18fd6cea8450834862c08c
Jair Medeiros <jair.medeiros@liferay.com>
2022-08-07 13:55:22 -0300

    LPS-159028 Create BasicForm with sheet form structure

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/components/BasicForm/BasicForm.tsx
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import Footer from './components/Footer';
+import Group from './components/Group';
+import Section from './components/Section';
+
+type IProps = {
+	children?: React.ReactNode;
+	description?: string;
+	name: string;
+	title: string;
+};
+
+const BasicForm = ({children, description, name, title}: IProps) => {
+	return (
+		<div className="pt-5 px-6 sheet sheet-lg">
+			<div>
+				<div className="font-weight-bold mb-1 text-primary">
+					{name.toUpperCase()}
+				</div>
+
+				{description && <div className="sheet-text">{description}</div>}
+
+				<h2 className="mb-4">{title}</h2>
+			</div>
+
+			{children}
+		</div>
+	);
+};
+
+BasicForm.Section = Section;
+BasicForm.Group = Group;
+BasicForm.Footer = Footer;
+
+export default BasicForm;
---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/components/BasicInput/BasicInput.tsx
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import ClayForm from '@clayui/form';
+import classNames from 'classnames';
+import {FieldMetaProps} from 'formik';
+
+export interface BasicInputProps {
+	label?: string;
+	required?: boolean;
+}
+
+interface IProps {
+	children?: React.ReactNode;
+}
+
+const BasicInput = <T extends unknown>({
+	children,
+	error,
+	label,
+	required,
+	touched,
+}: BasicInputProps & IProps & FieldMetaProps<T>) => (
+	<div
+		className={classNames('mb-4', {
+			'has-error': touched && error,
+			'has-success': touched && !error,
+		})}
+	>
+		{label && (
+			<label>
+				{label}
+
+				{required && <span className="text-danger">*</span>}
+			</label>
+		)}
+
+		{children}
+
+		{error && touched && (
+			<ClayForm.FeedbackItem>{error}</ClayForm.FeedbackItem>
+		)}
+	</div>
+);
+
+export default BasicInput;
