ac0f96d5442029f6ad8b2134111c0a5726462721
Brian Chan <brian.chan@liferay.com>
2022-08-08 10:55:53 -0300

    LCD-14496 not needed

--- modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationLocalServiceImpl.java
+++ modules/apps/oauth2-provider/oauth2-provider-service/src/main/java/com/liferay/oauth2/provider/service/impl/OAuth2ApplicationLocalServiceImpl.java
@@ -156,14 +156,6 @@ public class OAuth2ApplicationLocalServiceImpl
 		OAuth2Application oAuth2Application =
 			oAuth2ApplicationPersistence.create(oAuth2ApplicationId);
 
-		if (serviceContext != null) {
-			String uuid = serviceContext.getUuid();
-
-			if (Validator.isNotNull(uuid)) {
-				oAuth2Application.setUuid(uuid);
-			}
-		}
-
 		oAuth2Application.setCompanyId(companyId);
 		oAuth2Application.setUserId(userId);
 		oAuth2Application.setUserName(userName);
@@ -261,14 +253,6 @@ public class OAuth2ApplicationLocalServiceImpl
 
 		OAuth2Application oAuth2Application =
 			oAuth2ApplicationPersistence.create(oAuth2ApplicationId);
-
-		if (serviceContext != null) {
-			String uuid = serviceContext.getUuid();
-
-			if (Validator.isNotNull(uuid)) {
-				oAuth2Application.setUuid(uuid);
-			}
-		}
 
 		oAuth2Application.setCompanyId(companyId);
 		oAuth2Application.setUserId(userId);
