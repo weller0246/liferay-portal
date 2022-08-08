59c99af20a27e8587bf5a4b128d2b235af9c79a4
Brian Chan <brian.chan@liferay.com>
2022-08-08 11:09:32 -0300

    LPS-154730 auto SF

--- portal-kernel/src/com/liferay/document/library/kernel/exception/DuplicateFolderExternalReferenceCodeException.java
+++ portal-kernel/src/com/liferay/document/library/kernel/exception/DuplicateFolderExternalReferenceCodeException.java
@@ -11,6 +11,7 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 package com.liferay.document.library.kernel.exception;
 
 import com.liferay.portal.kernel.exception.PortalException;
@@ -18,7 +19,8 @@ import com.liferay.portal.kernel.exception.PortalException;
 /**
  * @author Brian Wing Shun Chan
  */
-public class DuplicateFolderExternalReferenceCodeException extends PortalException {
+public class DuplicateFolderExternalReferenceCodeException
+	extends PortalException {
 
 	public DuplicateFolderExternalReferenceCodeException() {
 	}
@@ -27,7 +29,9 @@ public class DuplicateFolderExternalReferenceCodeException extends PortalExcepti
 		super(msg);
 	}
 
-	public DuplicateFolderExternalReferenceCodeException(String msg, Throwable throwable) {
+	public DuplicateFolderExternalReferenceCodeException(
+		String msg, Throwable throwable) {
+
 		super(msg, throwable);
 	}
 
