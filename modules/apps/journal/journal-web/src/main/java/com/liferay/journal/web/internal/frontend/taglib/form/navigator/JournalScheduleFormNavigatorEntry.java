2246623a41ac290dd3cea1e2b0492f22a1069903
Diego Hu <diego.hu@liferay.com>
2022-08-09 14:20:43 +0200

    LPS-156914 Remove FF LPS-156908

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/frontend/taglib/form/navigator/JournalScheduleFormNavigatorEntry.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/frontend/taglib/form/navigator/JournalScheduleFormNavigatorEntry.java
@@ -17,8 +17,6 @@ package com.liferay.journal.web.internal.frontend.taglib.form.navigator;
 import com.liferay.frontend.taglib.form.navigator.FormNavigatorEntry;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.PropsUtil;
 import com.liferay.portal.util.PropsValues;
 
 import javax.servlet.ServletContext;
@@ -43,11 +41,7 @@ public class JournalScheduleFormNavigatorEntry
 
 	@Override
 	public boolean isVisible(User user, JournalArticle article) {
-		if (!PropsValues.SCHEDULER_ENABLED ||
-			(isEditDefaultValues(article) &&
-			 !GetterUtil.getBoolean(
-				 PropsUtil.get("feature.flag.LPS-156908")))) {
-
+		if (!PropsValues.SCHEDULER_ENABLED) {
 			return false;
 		}
 
