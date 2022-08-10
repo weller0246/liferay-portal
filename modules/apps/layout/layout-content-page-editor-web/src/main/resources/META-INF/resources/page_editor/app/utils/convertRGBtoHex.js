6e3b77d8d423e9da00321e6eecdbf5be40ca21a2
Pablo Molina <contact@pablomolina.me>
2022-08-10 09:33:56 +0200

    LPS-159216 Make convertRGBtoHex more robust

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/convertRGBtoHex.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/convertRGBtoHex.js
@@ -12,20 +12,34 @@
  * details.
  */
 
+const RGB_REGEXP = /^rgb\((?<red>\d{1,3}),\s*(?<green>\d{1,3}),\s*(?<blue>\d{1,3})\)/;
+const RGBA_REGEXP = /^rgba\((?<red>\d{1,3}),\s*(?<green>\d{1,3}),\s*(?<blue>\d{1,3}),\s*(?<alpha>(1|0(\.\d+)?))\)/;
+
 export function convertRGBtoHex(rgbColor) {
-	const [, red, green, blue] = rgbColor.match(
-		/^rgba?\((\d{1,3}), (\d{1,3}), (\d{1,3})/
-	);
+	const groups =
+		rgbColor.match(RGB_REGEXP)?.groups ||
+		rgbColor.match(RGBA_REGEXP)?.groups ||
+		{};
 
-	return [red, green, blue]
-		.map((number) => {
-			let hex = parseInt(number, 10).toString(16);
+	const alpha = parseFloat(groups.alpha);
+	const blue = parseInt(groups.blue, 10);
+	const green = parseInt(groups.green, 10);
+	const red = parseInt(groups.red, 10);
 
-			if (hex.length === 1) {
-				hex = hex.padStart(2, '0');
-			}
+	if (isNaN(blue) || isNaN(green) || isNaN(red)) {
+		return rgbColor;
+	}
 
-			return hex;
-		})
+	const hexColor = [red, green, blue]
+		.map((number) => number.toString(16).padStart(2, '0').toUpperCase())
 		.join('');
+
+	if (isNaN(alpha)) {
+		return hexColor;
+	}
+
+	return `${hexColor}${parseInt(`${alpha * 255}`, 10)
+		.toString(16)
+		.padStart(2, '0')
+		.toUpperCase()}`;
 }
---
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/app/utils/convertRGBtoHex.test.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {convertRGBtoHex} from '../../../../src/main/resources/META-INF/resources/page_editor/app/utils/convertRGBtoHex';
+
+describe('convertRGBtoHex', () => {
+	it('converts rgb colors', () => {
+		expect(convertRGBtoHex('rgb(100, 100, 100)')).toBe('646464');
+		expect(convertRGBtoHex('rgb(100,100,   100)')).toBe('646464');
+	});
+
+	it('converts rgba colors', () => {
+		expect(convertRGBtoHex('rgba(100, 100, 100,   0)')).toBe('64646400');
+		expect(convertRGBtoHex('rgba(100,100,   100,1)')).toBe('646464FF');
+		expect(convertRGBtoHex('rgba(100, 100, 100, 0.8276)')).toBe('646464D3');
+	});
+
+	it('ignores invalid colors', () => {
+		expect(convertRGBtoHex('rgb(0, 0, 0, 1)')).toBe('rgb(0, 0, 0, 1)');
+		expect(convertRGBtoHex('rgba(0, 0, 0)')).toBe('rgba(0, 0, 0)');
+		expect(convertRGBtoHex('rgb(1, 2, -2)')).toBe('rgb(1, 2, -2)');
+		expect(convertRGBtoHex('rgba(0,0,0,-0.1)')).toBe('rgba(0,0,0,-0.1)');
+	});
+
+	it('ignores unknown colors', () => {
+		expect(convertRGBtoHex('#646464')).toBe('#646464');
+		expect(convertRGBtoHex('#646464AA')).toBe('#646464AA');
+		expect(convertRGBtoHex('hsl(0, 0%, 50%)')).toBe('hsl(0, 0%, 50%)');
+		expect(convertRGBtoHex('var(--my-red)')).toBe('var(--my-red)');
+	});
+});
