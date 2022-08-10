b6aad4911b33fabd0f99a92166cc46fae6d5a4b8
Clarice Brasil <clarice.brasil@liferay.com>
2022-08-10 19:59:01 -0300

    LPS-158363 Create LXC Environment Query

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/liferay-experience-cloud-environments/queries/useGetLiferayExperienceEnvironments.js
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {gql, useQuery} from '@apollo/client';
+
+export const GET_LIFERAY_EXPERIENCE_CLOUD_ENVIRONMENTS = gql`
+	query getLiferayExperienceCloudEnvironments(
+		$scopeKey: String
+		$filter: String
+		$page: Int = 1
+		$pageSize: Int = 20
+	) {
+		c {
+			liferayExperienceCloudEnvironments(
+				filter: $filter
+				page: $page
+				pageSize: $pageSize
+				scopeKey: $scopeKey
+			) {
+				items {
+					liferayExperienceCloudEnvironmentId
+					projectId
+				}
+			}
+		}
+	}
+`;
+
+export function useGetLiferayExperienceCloudEnvironments(
+	scopeKey,
+	options = {
+		filter: '',
+		notifyOnNetworkStatusChange: false,
+		page: 1,
+		pageSize: 20,
+		skip: false,
+	}
+) {
+	return useQuery(GET_LIFERAY_EXPERIENCE_CLOUD_ENVIRONMENTS, {
+		fetchPolicy: 'network-only',
+		notifyOnNetworkStatusChange: options.notifyOnNetworkStatusChange,
+		skip: options.skip,
+		variables: {
+			filter: options.filter || '',
+			page: options.page || 1,
+			pageSize: options.pageSize || 20,
+			scopeKey,
+		},
+	});
+}
