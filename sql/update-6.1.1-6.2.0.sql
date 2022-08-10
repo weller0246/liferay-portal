78844dce44c75fb1d9a7ffb9d80e010fe893b6a5
Brian Chan <brian.chan@liferay.com>
2022-08-10 10:30:36 -0300

    Revert "LPS-159788 This index exists in 6.1GA1 and GA2 but not in GA3. I have removed the index drop because we always remove old indexes automatically at the end of the upgrade"
    This reverts commit 8e79df3b2449522bcc9d5649a19a360337f9008b.

--- sql/update-6.1.1-6.2.0.sql
+++ sql/update-6.1.1-6.2.0.sql
@@ -325,6 +325,8 @@ alter table Image drop column text_;
 alter table Layout add userId LONG;
 alter table Layout add userName VARCHAR(75) null;
 
+drop index IX_CED31606 on Layout;
+
 create table LayoutFriendlyURL (
 	uuid_ VARCHAR(75) null,
 	layoutFriendlyURLId LONG not null primary key,
