de635b0d8b6622f0521b4732dd3afc1153ffd9ae
Sandro Chinea <sandrochinea@gmail.com>
2022-08-10 15:57:26 +0100

    LPS-160264 Remove custom fragments addition as they will now come from server

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/fragmentsReducer.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/reducers/fragmentsReducer.js
@@ -14,20 +14,8 @@
 
 import {
 	ADD_FRAGMENT_COMPOSITION,
-	INIT,
 	TOGGLE_FRAGMENT_HIGHLIGHTED,
 } from '../actions/types';
-import {LAYOUT_DATA_ITEM_TYPE_LABELS} from '../config/constants/layoutDataItemTypeLabels';
-import {LAYOUT_DATA_ITEM_TYPES} from '../config/constants/layoutDataItemTypes';
-import {config} from '../config/index';
-
-const CONTENT_DISPLAY_COLLECTION_ID = 'content-display';
-
-const DEFAULT_CONTENT_DISPLAY_COLLECTION = {
-	fragmentCollectionId: 'collection-display',
-	fragmentEntries: [],
-	name: Liferay.Language.get('collection-display'),
-};
 
 export default function fragmentsReducer(fragments = [], action) {
 	switch (action.type) {
@@ -62,98 +50,6 @@ export default function fragmentsReducer(fragments = [], action) {
 
 				newCollection,
 			];
-		}
-
-		case INIT: {
-			const contentDisplayCollection = fragments.find(
-				(fragment) =>
-					fragment.fragmentCollectionId ===
-					CONTENT_DISPLAY_COLLECTION_ID
-			);
-
-			const newFragments = fragments.filter(
-				(fragment) =>
-					fragment.fragmentCollectionId !==
-					CONTENT_DISPLAY_COLLECTION_ID
-			);
-
-			newFragments.unshift({
-				fragmentCollectionId: 'layout-elements',
-				fragmentEntries: [
-					{
-						fragmentEntryKey: 'container',
-						icon: 'container',
-						itemType: LAYOUT_DATA_ITEM_TYPES.container,
-						name:
-							LAYOUT_DATA_ITEM_TYPE_LABELS[
-								LAYOUT_DATA_ITEM_TYPES.container
-							],
-					},
-					{
-						fragmentEntryKey: 'row',
-						icon: 'table',
-						itemType: LAYOUT_DATA_ITEM_TYPES.row,
-						name:
-							LAYOUT_DATA_ITEM_TYPE_LABELS[
-								LAYOUT_DATA_ITEM_TYPES.row
-							],
-					},
-				],
-				name: Liferay.Language.get('layout-elements'),
-			});
-
-			let formComponentsCollection = {fragmentEntries: []};
-
-			const formComponentsCollectionIndex = newFragments.findIndex(
-				(collection) => collection.fragmentCollectionId === 'INPUTS'
-			);
-
-			if (formComponentsCollectionIndex !== -1) {
-				[formComponentsCollection] = newFragments.splice(
-					formComponentsCollectionIndex,
-					1
-				);
-			}
-
-			if (config.featureFlagLps150277) {
-				newFragments.splice(2, 0, {
-					fragmentCollectionId: 'form-components',
-					fragmentEntries: [
-						{
-							fragmentEntryKey: 'form',
-							icon: 'container',
-							itemType: LAYOUT_DATA_ITEM_TYPES.form,
-							name:
-								LAYOUT_DATA_ITEM_TYPE_LABELS[
-									LAYOUT_DATA_ITEM_TYPES.form
-								],
-						},
-						...formComponentsCollection.fragmentEntries,
-					],
-					name: Liferay.Language.get('form-components'),
-				});
-			}
-
-			newFragments.splice(3, 0, {
-				...(contentDisplayCollection ||
-					DEFAULT_CONTENT_DISPLAY_COLLECTION),
-
-				fragmentEntries: [
-					...(
-						contentDisplayCollection ||
-						DEFAULT_CONTENT_DISPLAY_COLLECTION
-					).fragmentEntries,
-
-					{
-						fragmentEntryKey: 'collection-display',
-						icon: 'list',
-						itemType: LAYOUT_DATA_ITEM_TYPES.collection,
-						name: Liferay.Language.get('collection-display'),
-					},
-				],
-			});
-
-			return newFragments;
 		}
 
 		case TOGGLE_FRAGMENT_HIGHLIGHTED: {
