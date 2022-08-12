e7f51bd2e04fc3ebe54013e8e4123a16c9bde330
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-08-12 11:35:00 +0100

    LPS-160355 Users: Update user vertical card to accept actions

--- modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/frontend/taglib/clay/servlet/taglib/UserVerticalCard.java
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/frontend/taglib/clay/servlet/taglib/UserVerticalCard.java
@@ -15,11 +15,16 @@
 package com.liferay.users.admin.web.internal.frontend.taglib.clay.servlet.taglib;
 
 import com.liferay.frontend.taglib.clay.servlet.taglib.BaseUserCard;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.portal.kernel.dao.search.RowChecker;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.taglib.util.LexiconUtil;
+import com.liferay.users.admin.web.internal.servlet.taglib.util.UserActionDropdownItems;
+
+import java.util.List;
 
 import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
 /**
  * @author Eudaldo Alonso
@@ -27,14 +32,42 @@ import javax.portlet.RenderRequest;
 public class UserVerticalCard extends BaseUserCard {
 
 	public UserVerticalCard(
+		RenderRequest renderRequest, RenderResponse renderResponse,
+		RowChecker rowChecker, boolean showActions, User user) {
+
+		super(user, renderRequest, rowChecker);
+
+		_renderResponse = renderResponse;
+		_showActions = showActions;
+	}
+
+	public UserVerticalCard(
 		RenderRequest renderRequest, RowChecker rowChecker, User user) {
 
 		super(user, renderRequest, rowChecker);
+
+		_renderResponse = null;
+		_showActions = false;
+	}
+
+	@Override
+	public List<DropdownItem> getActionDropdownItems() {
+		if (!_showActions) {
+			return null;
+		}
+
+		UserActionDropdownItems userActionDropdownItems =
+			new UserActionDropdownItems(renderRequest, _renderResponse, user);
+
+		return userActionDropdownItems.getActionDropdownItems();
 	}
 
 	@Override
 	public String getUserColorClass() {
 		return "sticker-user-icon " + LexiconUtil.getUserColorCssClass(user);
 	}
+
+	private final RenderResponse _renderResponse;
+	private final boolean _showActions;
 
 }
