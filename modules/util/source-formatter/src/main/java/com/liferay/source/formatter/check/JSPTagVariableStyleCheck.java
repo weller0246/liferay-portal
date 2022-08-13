464bde34682aab719857f572b982f9799528712a
Alan Huang <alan.huang@liferay.com>
2022-08-13 16:48:30 +0800

    LPS-160236 SF, simplify

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPTagVariableStyleCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPTagVariableStyleCheck.java
@@ -14,7 +14,6 @@
 
 package com.liferay.source.formatter.check;
 
-import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
@@ -22,7 +21,6 @@ import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.tools.ToolsUtil;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -36,270 +34,113 @@ public class JSPTagVariableStyleCheck extends BaseFileCheck {
 	protected String doProcess(
 		String fileName, String absolutePath, String content) {
 
-		return _fixIncorrectVariableStyle(content);
-	}
+		StringBuffer sb = new StringBuffer();
 
-	private String _fixIncorrectVariableStyle(String content) {
-		Matcher matcher = _javaSourceParameterPattern.matcher(content);
+		Matcher matcher = _jspExpressionTagPattern.matcher(content);
 
-		outLoop:
 		while (matcher.find()) {
-			if (!_insideQuotes(content, matcher.start())) {
+			String jspExpressionTag = matcher.group();
+
+			if (getLevel(jspExpressionTag, "(", ")") != 0) {
 				continue;
 			}
 
-			String parameter = matcher.group(1);
+			String newJspExpressionTag = _formatJspExpressionTag(
+				matcher.group(1));
 
-			String[] parameters = _splitParameters(parameter);
+			matcher.appendReplacement(sb, newJspExpressionTag);
+		}
 
-			if ((parameters == null) || (parameters.length == 1)) {
-				continue;
-			}
+		if (sb.length() > 0) {
+			matcher.appendTail(sb);
 
-			StringBundler sb = new StringBundler(parameters.length);
-			boolean changeFlg = false;
-			boolean preJavaParameterFlg = false;
-
-			for (String curParameter : parameters) {
-				if (Validator.isNull(curParameter)) {
-					continue;
-				}
-
-				curParameter = StringUtil.trim(curParameter);
-
-				int level = _getLevel(curParameter);
-
-				if (level != 0) {
-					continue outLoop;
-				}
-
-				if (curParameter.startsWith(StringPool.QUOTE) &&
-					curParameter.endsWith(StringPool.QUOTE)) {
-
-					sb.append(
-						curParameter.substring(1, curParameter.length() - 1));
-
-					changeFlg = true;
-					preJavaParameterFlg = false;
-				}
-				else {
-					if (preJavaParameterFlg) {
-						sb.setIndex(sb.index() - 1);
-						sb.append(" + ");
-					}
-					else {
-						sb.append("<%= ");
-					}
-
-					if (curParameter.startsWith(StringPool.OPEN_PARENTHESIS) &&
-						curParameter.endsWith(StringPool.CLOSE_PARENTHESIS)) {
-
-						curParameter = curParameter.substring(
-							1, curParameter.length() - 1);
-					}
-
-					sb.append(curParameter);
-					sb.append(" %>");
-
-					preJavaParameterFlg = true;
-				}
-			}
-
-			if (!StringUtil.equals(matcher.group(), sb.toString()) &&
-				changeFlg) {
-
-				return StringUtil.replaceFirst(
-					content, matcher.group(), sb.toString(), matcher.start());
-			}
+			return sb.toString();
 		}
 
 		return content;
 	}
 
-	private int _getLevel(String expression) {
-		int level = 0;
+	private String _formatJspExpressionTag(String expression) {
+		List<String> operandList = new ArrayList<>();
 
-		for (char e : expression.toCharArray()) {
-			if (e == CharPool.OPEN_PARENTHESIS) {
-				level += 1;
+		int startPosition = 0;
+
+		int x = startPosition;
+
+		while (true) {
+			x = expression.indexOf("+", x);
+
+			if (x == -1) {
+				break;
 			}
-			else if (e == CharPool.CLOSE_PARENTHESIS) {
-				level -= 1;
+
+			if (ToolsUtil.isInsideQuotes(expression, x)) {
+				x = x + 1;
+
+				continue;
 			}
+
+			String operand = expression.substring(startPosition, x);
+
+			if (getLevel(operand, "(", ")") != 0) {
+				x = x + 1;
+
+				continue;
+			}
+
+			operandList.add(operand.trim());
+
+			x = x + 1;
+
+			startPosition = x;
 		}
 
-		return level;
-	}
-
-	private boolean _insideQuotes(String s, int pos) {
-		int start = s.lastIndexOf(10, pos);
-
-		if (start == -1) {
-			start = 0;
+		if (x == -1) {
+			operandList.add(
+				StringUtil.trim(expression.substring(startPosition)));
 		}
 
-		int end = s.indexOf(10, pos);
+		StringBundler sb = new StringBundler();
 
-		if (end == -1) {
-			end = s.length();
-		}
+		String previousOperand = null;
 
-		pos -= start;
+		for (String operand : operandList) {
+			if (operand.startsWith(StringPool.QUOTE)) {
+				if (Validator.isNotNull(previousOperand) &&
+					!previousOperand.startsWith(StringPool.QUOTE)) {
 
-		String line = s.substring(start, end);
-		char delimeter = ' ';
-		boolean insideQuotes = false;
-
-		for (int i = 0; i < line.length(); ++i) {
-			char c = line.charAt(i);
-
-			if (!insideQuotes) {
-				if (c == '"') {
-					delimeter = c;
-					insideQuotes = true;
+					sb.append(" %>");
 				}
+
+				sb.append(StringUtil.unquote(operand));
 			}
-			else if (c == delimeter) {
-				int precedingBackSlashCount = 0;
-
-				for (int j = i - 1; (j >= 0) && (line.charAt(j) == '\\'); --j) {
-					++precedingBackSlashCount;
-				}
-
-				if ((precedingBackSlashCount % 2) == 0) {
-					insideQuotes = false;
-				}
-			}
-
-			if (pos == i) {
-				return insideQuotes;
-			}
-		}
-
-		return false;
-	}
-
-	private String[] _splitParameters(String parameter) {
-		int questionIndex = parameter.indexOf(StringPool.QUESTION);
-		int colonIndex = parameter.indexOf(StringPool.COLON);
-
-		String expression = null;
-		int x = questionIndex;
-		int y = colonIndex;
-		boolean conditionalOperatorFlg = false;
-
-		if ((questionIndex != -1) && (colonIndex != -1) &&
-			(questionIndex < colonIndex) &&
-			!ToolsUtil.isInsideQuotes(parameter, questionIndex) &&
-			!ToolsUtil.isInsideQuotes(parameter, colonIndex)) {
-
-			if ((StringUtil.count(parameter, StringPool.QUESTION) > 1) ||
-				(StringUtil.count(parameter, StringPool.COLON) > 1)) {
-
-				return null;
-			}
-
-			conditionalOperatorFlg = true;
-
-			int level = 0;
-
-			while (true) {
-				if (level == 0) {
-					x = parameter.lastIndexOf(
-						StringPool.OPEN_PARENTHESIS, x - 1);
-					y = parameter.indexOf(StringPool.CLOSE_PARENTHESIS, y + 1);
-				}
-				else if (level > 0) {
-					y = parameter.indexOf(StringPool.CLOSE_PARENTHESIS, y + 1);
-				}
-				else {
-					x = parameter.lastIndexOf(
-						StringPool.OPEN_PARENTHESIS, x - 1);
-				}
-
-				if ((x == -1) || (y == -1)) {
-					break;
-				}
-
-				expression = parameter.substring(x, y + 1);
-
-				level = _getLevel(expression);
-
-				if (level == 0) {
-					int frontPlusIndex = parameter.lastIndexOf(
-						StringPool.PLUS, x);
-					int behindPlusIndex = parameter.indexOf(StringPool.PLUS, y);
-
-					if ((frontPlusIndex == -1) && (behindPlusIndex == -1)) {
-						expression = null;
-
-						break;
-					}
-					else if (frontPlusIndex == -1) {
-						x = 0;
-						y = behindPlusIndex;
-					}
-					else if (behindPlusIndex == -1) {
-						x = frontPlusIndex + 1;
-						y = parameter.length();
+			else {
+				if (Validator.isNotNull(previousOperand)) {
+					if (!previousOperand.startsWith(StringPool.QUOTE)) {
+						sb.append(" + ");
 					}
 					else {
-						x = frontPlusIndex + 1;
-						y = behindPlusIndex;
+						sb.append("<%= ");
 					}
-
-					expression = parameter.substring(x, y);
-					expression = StringUtil.trim(expression);
-
-					if (expression.startsWith(StringPool.OPEN_PARENTHESIS) &&
-						expression.endsWith(StringPool.CLOSE_PARENTHESIS)) {
-
-						break;
-					}
-
-					x--;
+				}
+				else {
+					sb.append("<%= ");
 				}
 
-				expression = null;
-			}
-		}
-
-		if (Validator.isNull(expression)) {
-			if (conditionalOperatorFlg) {
-				String[] result = new String[1];
-
-				result[0] = parameter;
-
-				return result;
+				sb.append(operand);
 			}
 
-			return parameter.split("\\+");
+			previousOperand = operand;
 		}
 
-		List<String> parameters = new ArrayList<>();
-
-		String tmpExpression = parameter.substring(0, x);
-
-		if (Validator.isNotNull(tmpExpression)) {
-			Collections.addAll(parameters, tmpExpression.split("\\+"));
+		if (!previousOperand.startsWith(StringPool.QUOTE)) {
+			sb.append(" %>");
 		}
 
-		parameters.add(expression);
-
-		if (y == parameter.length()) {
-			return parameters.toArray(new String[0]);
-		}
-
-		tmpExpression = parameter.substring(y + 1);
-
-		if (Validator.isNotNull(tmpExpression)) {
-			Collections.addAll(parameters, tmpExpression.split("\\+"));
-		}
-
-		return parameters.toArray(new String[0]);
+		return sb.toString();
 	}
 
-	private static final Pattern _javaSourceParameterPattern = Pattern.compile(
-		"<%=(.+?)%>");
+	private static final Pattern _jspExpressionTagPattern = Pattern.compile(
+		"(?<=\")<%=(.+?)%>(?=\")");
 
 }
