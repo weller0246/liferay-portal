0c08b4a6064c0b11a84dd337d35e317e4ccd9270
Clarice Brasil <clarice.brasil@liferay.com>
2022-08-14 18:29:28 -0300

    LPS-158368 Create hook For GetAccountSubscriptionGroups

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/services/liferay/graphql/account-subscription-groups/queries/useGetAccountSubscriptionGroups.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {gql, useQuery} from '@apollo/client';
+
+const GET_ACCOUNT_SUBSCRIPTION_GROUPS = gql`
+	query getAccountSubscriptionGroups(
+		$aggregation: [String]
+		$filter: String
+		$page: Int = 1
+		$pageSize: Int = 20
+		$search: String
+		$sort: String
+	) {
+		c {
+			accountSubscriptionGroups(
+				aggregation: $aggregation
+				filter: $filter
+				page: $page
+				pageSize: $pageSize
+				search: $search
+				sort: $sort
+			) {
+				items {
+					accountSubscriptionGroupId
+					accountKey
+					activationStatus
+					name
+					tabOrder
+					menuOrder
+				}
+			}
+		}
+	}
+`;
+
+export function useGetAccountSubscriptionGroups(
+	options = {
+		filter: '',
+		notifyOnNetworkStatusChange: false,
+		page: 1,
+		pageSize: 20,
+		skip: false,
+	}
+) {
+	return useQuery(GET_ACCOUNT_SUBSCRIPTION_GROUPS, {
+		fetchPolicy: 'cache-and-network',
+		nextFetchPolicy: 'cache-first',
+		notifyOnNetworkStatusChange: options.notifyOnNetworkStatusChange,
+		skip: options.skip,
+		variables: {
+			filter: options.filter || '',
+			page: options.page || 1,
+			pageSize: options.pageSize || 20,
+		},
+	});
+}
