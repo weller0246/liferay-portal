d60775075ae9f256bbd455dfe6b997895fd7487d
marcelabc <marcela.cunha@liferay.com>
2022-08-15 18:44:00 -0300

    LPS-160455 Allow the creation of self many to many relationship

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -546,14 +546,6 @@ public class ObjectRelationshipLocalServiceImpl
 			throw new ObjectRelationshipTypeException("Invalid type " + type);
 		}
 
-		if (Objects.equals(
-				type, ObjectRelationshipConstants.TYPE_MANY_TO_MANY) &&
-			(objectDefinitionId1 == objectDefinitionId2)) {
-
-			throw new ObjectRelationshipTypeException(
-				"Many to many self relationships are not allowed");
-		}
-
 		ObjectDefinition objectDefinition1 =
 			_objectDefinitionPersistence.fetchByPrimaryKey(objectDefinitionId1);
 		ObjectDefinition objectDefinition2 =
