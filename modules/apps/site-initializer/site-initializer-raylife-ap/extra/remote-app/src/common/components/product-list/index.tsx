2cec129ca6f6c2abadf8f555e9a36e4b7cf54c79
Joao Marcelo Alves <joao.alves@liferay.com>
2022-08-16 18:48:48 -0300

    LPS-159791 Creating PRODUCT PERFORMANCE Product Filters Layout

---
+++ modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/common/components/product-list/index.tsx
@@ -0,0 +1,81 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import classNames from 'classnames';
+import React from 'react';
+
+import {getWebDavUrl} from '../../utils/webdav';
+
+type ProductListProps = {
+	onSelect: (productName: string) => void;
+	productList: ProductCell[];
+};
+
+export type ProductCell = {
+	active: boolean;
+	productName: string;
+	salesGoal: number;
+	totalSales: number;
+};
+
+const ProductList: React.FC<ProductListProps> = ({onSelect, productList}) => {
+	const currencyConversion = (value: number) =>
+		value.toLocaleString('en-US', {
+			currency: 'USD',
+			style: 'currency',
+		});
+
+	return (
+		<div className="d-flex d-md-block ray-product-list">
+			{productList.map((productCell: ProductCell, index: number) => (
+				<div
+					className={classNames(
+						'd-flex justify-content-center justify-content-md-between p-md-5 position-relative product-item py-3',
+						{
+							'product-selected': productCell.active,
+						}
+					)}
+					key={index}
+					onClick={() => onSelect(productCell.productName)}
+				>
+					<div className="align-items-center col-md-6 d-flex product-container px-0">
+						<img
+							className="mr-2 product-icon"
+							src={`${getWebDavUrl()}/${productCell.productName
+								.toLowerCase()
+								.replaceAll(' ', '_')}_icon.svg`}
+						/>
+
+						<p className="grey-title-color mb-0 product-title text-paragraph">
+							{productCell.productName}
+						</p>
+					</div>
+
+					<div className="col-md-6 d-md-block d-none goals-container px-0">
+						<p className="font-weight-bolder grey-title-color mb-0 text-paragraph text-right">
+							{currencyConversion(productCell.totalSales)}
+						</p>
+
+						<p className="mb-0 text-paragraph-sm text-right yearly-goal">
+							Yearly Goal:&nbsp;
+							{currencyConversion(productCell.salesGoal)}
+						</p>
+					</div>
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default ProductList;
--- modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/routes/dashboard/pages/ProductPerformance.tsx
+++ modules/apps/site-initializer/site-initializer-raylife-ap/extra/remote-app/src/routes/dashboard/pages/ProductPerformance.tsx
@@ -16,57 +16,113 @@ import ClayButton from '@clayui/button';
 import {ClaySelect} from '@clayui/form';
 import ClayIcon from '@clayui/icon';
 import classNames from 'classnames';
-import React, {useState} from 'react';
+import {useEffect, useState} from 'react';
 
 import Header from '../../../common/components/header';
+import ProductList, {
+	ProductCell,
+} from '../../../common/components/product-list';
 
-const PRODUCT_LIST = [
-	'All',
-	'Auto',
-	'General Liability',
-	'Professional Liability',
-	'Workers Compensation',
-	'Business Owners Policy',
+const PRODUCT_SALES_GOAL = [
+	{
+		active: false,
+		productName: 'Auto',
+		salesGoal: 102000,
+		totalSales: 120000,
+	},
+	{
+		active: false,
+		productName: 'Business Owner Policy',
+		salesGoal: 102000,
+		totalSales: 90000,
+	},
+	{
+		active: false,
+		productName: 'General Liability',
+		salesGoal: 102000,
+		totalSales: 10000,
+	},
+	{
+		active: false,
+		productName: 'Professional Liability',
+		salesGoal: 102000,
+		totalSales: 150000,
+	},
+	{
+		active: false,
+		productName: 'Workers Compensation',
+		salesGoal: 102000,
+		totalSales: 60000,
+	},
 ];
 
 const TIME_PERIODS = ['YTD', '3 MO', '6 MO'];
 
 const ProductPerformance = () => {
-	const [choosenProduct, setChoosenProduct] = useState(PRODUCT_LIST[0]);
+	const [products, setProducts] = useState<ProductCell[]>([]);
 	const [timePeriod, setTimePeriod] = useState(TIME_PERIODS[0]);
+
+	useEffect(() => {
+		setProducts(PRODUCT_SALES_GOAL);
+	}, []);
+
+	const isFilterAllActive = (product: ProductCell) => !product.active;
+	const findActiveProduct = products.find((product) => product.active)
+		?.productName;
+
+	const handleProductFilterToggle = (productName: string) => {
+		const newProducts = products.map((product) => {
+			product.productName === productName
+				? (product.active = true)
+				: (product.active = false);
+
+			return product;
+		});
+
+		setProducts(newProducts);
+	};
 
 	return (
 		<div className="d-flex flex-wrap ray-dashboard-product-performance">
-			<div className="col-5 left-container px-0">
+			<div className="col-md-5 left-container px-0">
 				<Header
 					className="header-row px-4 py-3"
 					title="Product Performance"
 				/>
 
-				<div className="p-5"></div>
+				<ProductList
+					onSelect={handleProductFilterToggle}
+					productList={products}
+				/>
 			</div>
 
-			<div className="col-7 px-0 right-container">
+			<div className="col-md-7 px-0 right-container">
 				<div className="align-items-center d-flex header-row justify-content-between px-4 py-3">
 					<p className="m-0 text-paragraph">
 						<ClayButton
 							className={classNames('general-filter mr-1', {
-								'font-weight-bolder': choosenProduct === 'All',
+								'disabled font-weight-bolder': products.every(
+									isFilterAllActive
+								),
 							})}
 							displayType="unstyled"
-							onClick={() => setChoosenProduct('All')}
+							onClick={() => {
+								if (!products.every(isFilterAllActive)) {
+									handleProductFilterToggle('All');
+								}
+							}}
 						>
 							All
 						</ClayButton>
 
-						{choosenProduct !== 'All' && (
+						{!products.every(isFilterAllActive) && (
 							<>
 								<ClayIcon
-									className="font-weight-bolder mr-1"
+									className="mr-1"
 									symbol="angle-right-small"
 								/>
 
-								<span className="font-weight-bolder">{`${choosenProduct}`}</span>
+								<span className="font-weight-bolder">{`${findActiveProduct}`}</span>
 							</>
 						)}
 					</p>
