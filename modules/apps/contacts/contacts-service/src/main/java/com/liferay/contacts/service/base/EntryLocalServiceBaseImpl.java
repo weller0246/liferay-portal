85bb32dc72b9dab466f4083261217ee3d7de70dd
Janis Zhang <janis.zhang@liferay.com>
2022-08-16 15:06:52 +0800

    LPS-160776 Auto generated

--- modules/apps/contacts/contacts-service/src/main/java/com/liferay/contacts/service/base/EntryLocalServiceBaseImpl.java
+++ modules/apps/contacts/contacts-service/src/main/java/com/liferay/contacts/service/base/EntryLocalServiceBaseImpl.java
@@ -460,16 +460,4 @@ public abstract class EntryLocalServiceBaseImpl
 	protected com.liferay.counter.kernel.service.CounterLocalService
 		counterLocalService;
 
-	@Reference
-	protected com.liferay.portal.kernel.service.ClassNameLocalService
-		classNameLocalService;
-
-	@Reference
-	protected com.liferay.portal.kernel.service.ResourceLocalService
-		resourceLocalService;
-
-	@Reference
-	protected com.liferay.portal.kernel.service.UserLocalService
-		userLocalService;
-
 }
