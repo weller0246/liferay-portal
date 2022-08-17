4edd7ce9c647276cc86a144a099f159ca1c1760c
Joao Silva <joao.silva@liferay.com>
2022-08-17 15:16:31 -0300

    LPS-160835 Create function to new fields objects mdfRequest minDate and maxDate

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/services/liferay/object/mdf-requests/createMDFRequest.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/services/liferay/object/mdf-requests/createMDFRequest.ts
@@ -11,8 +11,51 @@
 
 import {Liferay} from '../..';
 import MDFRequest from '../../../../interfaces/mdfRequest';
+import MDFRequestActivity from '../../../../interfaces/mdfRequestActivity';
 import {LiferayAPIs} from '../../common/enums/apis';
 import liferayFetcher from '../../common/utils/fetcher';
+
+interface DateActivities {
+	endDates: Date[];
+	startDates: Date[];
+}
+
+const getDateActivities = (mdfRequest: MDFRequest) => {
+	if (mdfRequest.activities.length) {
+		const datesActivities = mdfRequest.activities.reduce(
+			(dateAccumulator: DateActivities, activity: MDFRequestActivity) => {
+				return {
+					endDates: [...dateAccumulator.endDates, activity.endDate],
+					startDates: [
+						...dateAccumulator.startDates,
+						activity.startDate,
+					],
+				};
+			},
+			{endDates: [], startDates: []}
+		);
+
+		const maxDateActivity = new Date(
+			Math.min(
+				Number(
+					...datesActivities.startDates.map((date) => new Date(date))
+				)
+			)
+		);
+		const minDateActivity = new Date(
+			Math.max(
+				Number(
+					...datesActivities.endDates.map((date) => new Date(date))
+				)
+			)
+		);
+
+		return {
+			maxDateActivity,
+			minDateActivity,
+		};
+	}
+};
 
 export default async function createMDFRequest(mdfRequest: MDFRequest) {
 	const dtoMDFRequest = {
@@ -20,11 +63,12 @@ export default async function createMDFRequest(mdfRequest: MDFRequest) {
 		liferayBusinessSalesGoals: mdfRequest.liferayBusinessSalesGoals.join(
 			', '
 		),
+		...getDateActivities(mdfRequest),
 		targetAudienceRoles: mdfRequest.targetAudienceRoles.join(', '),
 		targetMarkets: mdfRequest.targetMarkets.join(', '),
 	};
 
-	return await liferayFetcher.post<typeof dtoMDFRequest>(
+	return await liferayFetcher.post(
 		`/o/${LiferayAPIs.OBJECT}/mdfrequests`,
 		Liferay.authToken,
 		dtoMDFRequest
