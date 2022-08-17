cb70689709905c9f708b52e0ba83c374d92b7212
Clarice Brasil <clarice.brasil@liferay.com>
2022-08-17 17:48:05 -0300

    LPS-160678 Fix Redirecting Page After Access Control Panel

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/common/hooks/useRouterPath.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import {useMemo} from 'react';
+import {ROUTE_TYPES} from '../utils/constants';
+
+export default function useRouterPath() {
+	const liferayURL = useMemo(() => {
+		const siteURL = Liferay.ThemeDisplay.getLayoutRelativeURL();
+
+		return `${Liferay.ThemeDisplay.getPortalURL()}${siteURL.substring(
+			0,
+			siteURL.lastIndexOf('/')
+		)}`;
+	}, []);
+
+	return {
+		onboarding: (externalReferenceCode) =>
+			`${liferayURL}/${ROUTE_TYPES.onboarding}/#/${externalReferenceCode}`,
+		project: (externalReferenceCode) =>
+			`${liferayURL}/${ROUTE_TYPES.project}/#/${externalReferenceCode}`,
+	};
+}
--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/home/components/ProjectsList/components/ProjectCard/ProjectCard.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/home/components/ProjectsList/components/ProjectCard/ProjectCard.js
@@ -14,78 +14,88 @@ import classNames from 'classnames';
 import {memo} from 'react';
 import i18n from '../../../../../../common/I18n';
 import {Skeleton, StatusTag} from '../../../../../../common/components';
-import {PAGE_ROUTER_TYPES} from '../../../../../../common/utils/constants';
+import useRouterPath from '../../../../../../common/hooks/useRouterPath';
 import getKebabCase from '../../../../../../common/utils/getKebabCase';
 import CardTitleDescription from './components/CardTitleDescription/CardTitleDescription';
 import StatusDescription from './components/StatusDescription';
 import redirect from './utils/redirect';
 
-const ProjectCard = ({compressed, loading, ...koroneikiAccount}) => (
-	<ClayCard
-		className={classNames('m-0', {
-			'cp-project-card': !compressed,
-			'cp-project-card-sm': compressed,
-		})}
-		onClick={() =>
-			redirect(PAGE_ROUTER_TYPES.project(koroneikiAccount.accountKey))
-		}
-	>
-		<ClayCard.Body
-			className={classNames('d-flex h-100 justify-content-between', {
-				'flex-column': !compressed,
-				'flex-row': compressed,
-			})}
-		>
-			{loading ? (
-				<Skeleton height={32} width={460.5} />
-			) : (
-				<CardTitleDescription
-					compressed={compressed}
-					{...koroneikiAccount}
-				/>
-			)}
+const ProjectCard = ({compressed, loading, ...koroneikiAccount}) => {
+	const pageRoutes = useRouterPath();
 
-			<div
-				className={classNames('d-flex justify-content-between', {
-					'align-items-end': compressed,
+	return (
+		<ClayCard
+			className={classNames('m-0', {
+				'cp-project-card': !compressed,
+				'cp-project-card-sm': compressed,
+			})}
+			onClick={() =>
+				redirect(pageRoutes.project(koroneikiAccount.accountKey))
+			}
+		>
+			<ClayCard.Body
+				className={classNames('d-flex h-100 justify-content-between', {
+					'flex-column': !compressed,
+					'flex-row': compressed,
 				})}
 			>
-				<ClayCard.Description
-					displayType="text"
-					tag="div"
-					title={null}
-					truncate={false}
+				{loading ? (
+					<Skeleton height={32} width={460.5} />
+				) : (
+					<CardTitleDescription
+						compressed={compressed}
+						{...koroneikiAccount}
+					/>
+				)}
+
+				<div
+					className={classNames('d-flex justify-content-between', {
+						'align-items-end': compressed,
+					})}
 				>
-					{loading ? (
-						<Skeleton height={20} width={54} />
-					) : (
-						<StatusTag currentStatus={koroneikiAccount.status} />
-					)}
+					<ClayCard.Description
+						displayType="text"
+						tag="div"
+						title={null}
+						truncate={false}
+					>
+						{loading ? (
+							<Skeleton height={20} width={54} />
+						) : (
+							<StatusTag
+								currentStatus={koroneikiAccount.status}
+							/>
+						)}
 
-					{loading ? (
-						<Skeleton className="mt-1" height={24} width={137} />
-					) : (
-						<StatusDescription
-							compressed={compressed}
-							{...koroneikiAccount}
-						/>
-					)}
+						{loading ? (
+							<Skeleton
+								className="mt-1"
+								height={24}
+								width={137}
+							/>
+						) : (
+							<StatusDescription
+								compressed={compressed}
+								{...koroneikiAccount}
+							/>
+						)}
 
-					{compressed && (
-						<div className="text-align-end text-neutral-5 text-paragraph-sm">
-							{i18n.translate('support-region')}
+						{compressed && (
+							<div className="text-align-end text-neutral-5 text-paragraph-sm">
+								{i18n.translate('support-region')}
 
-							<span className="font-weight-bold ml-1">
-								{i18n.translate(
-									getKebabCase(koroneikiAccount.region)
-								)}
-							</span>
-						</div>
-					)}
-				</ClayCard.Description>
-			</div>
-		</ClayCard.Body>
-	</ClayCard>
-);
+								<span className="font-weight-bold ml-1">
+									{i18n.translate(
+										getKebabCase(koroneikiAccount.region)
+									)}
+								</span>
+							</div>
+						)}
+					</ClayCard.Description>
+				</div>
+			</ClayCard.Body>
+		</ClayCard>
+	);
+};
 
 export default memo(ProjectCard);
