6715e7c3bbdcff9f1d852445165e939edd23b925
Jorge Díaz <jorge.diaz@liferay.com>
2022-08-19 14:26:22 +0200

    LPS-161064 Skip temporarily unavailable results in the same way it is done in the SimilarResultsPortlet, this will avoid breaking the customer freemarker templates that don't check the isTemporarilyUnavailable value for every result

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortlet.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/search/results/portlet/SearchResultsPortlet.java
@@ -469,7 +469,9 @@ public class SearchResultsPortlet extends MVCPortlet {
 					portletURLFactory, searchResultsPortletPreferences,
 					searchResultPreferences);
 
-			if (searchResultSummaryDisplayContext != null) {
+			if ((searchResultSummaryDisplayContext != null) &&
+				!searchResultSummaryDisplayContext.isTemporarilyUnavailable()) {
+
 				searchResultsSummariesHolder.put(
 					document, searchResultSummaryDisplayContext);
 			}
