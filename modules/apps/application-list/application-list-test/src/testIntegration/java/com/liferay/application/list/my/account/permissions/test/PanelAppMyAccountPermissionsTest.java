ee52b7452b663d0237bf96578625f67e05018c0d
Dante Wang <dante.wang@liferay.com>
2022-08-19 17:50:44 +0800

    LPS-160434 SF, no need to pass as parameter

--- modules/apps/application-list/application-list-test/src/testIntegration/java/com/liferay/application/list/my/account/permissions/test/PanelAppMyAccountPermissionsTest.java
+++ modules/apps/application-list/application-list-test/src/testIntegration/java/com/liferay/application/list/my/account/permissions/test/PanelAppMyAccountPermissionsTest.java
@@ -98,7 +98,7 @@ public class PanelAppMyAccountPermissionsTest {
 
 		_testCompany = addCompany();
 
-		_registerTestPortlet(_testPortletId);
+		_registerTestPortlet();
 
 		long defaultCompanyId = TestPropsValues.getCompanyId();
 
@@ -122,7 +122,7 @@ public class PanelAppMyAccountPermissionsTest {
 	public void testPermissionsAddedForPanelAppFromNewCompany()
 		throws Exception {
 
-		_registerTestPortlet(_testPortletId);
+		_registerTestPortlet();
 
 		_registerTestPanelApp();
 
@@ -166,12 +166,12 @@ public class PanelAppMyAccountPermissionsTest {
 				).build()));
 	}
 
-	private void _registerTestPortlet(String portletId) throws Exception {
+	private void _registerTestPortlet() {
 		_serviceRegistrations.add(
 			_bundleContext.registerService(
 				Portlet.class, new TestPortlet(),
 				HashMapDictionaryBuilder.put(
-					"javax.portlet.name", portletId
+					"javax.portlet.name", _testPortletId
 				).build()));
 	}
 
