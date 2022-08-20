b70b2620857e446514923a71a006da5ebcbd8766
Brian Chan <brian.chan@liferay.com>
2022-08-19 23:15:43 -0300

    LPS-158806 #sfme

--- portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
+++ portal-impl/test/unit/com/liferay/portal/util/PortalImplUnitTest.java
@@ -118,14 +118,15 @@ public class PortalImplUnitTest {
 
 		ActionRequest actionRequest = _createActionRequest(params);
 
-		MockedStatic<PortalUtil> portalUtilMock = Mockito.mockStatic(
+		MockedStatic<PortalUtil> portalUtilMockStatic = Mockito.mockStatic(
 			PortalUtil.class);
 
-		ActionResponse actionResponse = _createActionResponse(portalUtilMock);
+		ActionResponse actionResponse = _createActionResponse(
+			portalUtilMockStatic);
 
 		_portalImpl.copyRequestParameters(actionRequest, actionResponse);
 
-		portalUtilMock.close();
+		portalUtilMockStatic.close();
 
 		_assertActionResponse(actionResponse, params);
 	}
