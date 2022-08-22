47c4c0ee071e46fbe7c9ba9a7f0930f5591c922f
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-08-22 10:46:29 +0800

    LPS-160434 Use method getPortlet to get portlet ,remove variable _portlet and just implement method getPortlet in subclasses

--- modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/BasePanelApp.java
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/BasePanelApp.java
@@ -99,15 +99,12 @@ public abstract class BasePanelApp implements PanelApp {
 			return 0;
 		}
 
+		Portlet portlet = getPortlet();
+
 		return _userNotificationEventLocalService.
 			getUserNotificationEventsCount(
-				user.getUserId(), _portlet.getPortletId(),
+				user.getUserId(), portlet.getPortletId(),
 				UserNotificationDeliveryConstants.TYPE_WEBSITE, true, false);
-	}
-
-	@Override
-	public Portlet getPortlet() {
-		return _portlet;
 	}
 
 	@Override
@@ -226,7 +223,6 @@ public abstract class BasePanelApp implements PanelApp {
 
 	private static final Log _log = LogFactoryUtil.getLog(BasePanelApp.class);
 
-	private Portlet _portlet;
 	private PortletLocalService _portletLocalService;
 	private UserNotificationEventLocalService
 		_userNotificationEventLocalService;
