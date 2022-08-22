807514a20a5670899f91b7c35dd458efaa3e0c82
Janis Zhang <janis.zhang@liferay.com>
2022-08-22 16:40:39 +0800

    LPS-161153 Covert method reference to field references in BlogsEntryFullContentInfoItemRenderer

--- modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/info/item/renderer/BlogsEntryFullContentInfoItemRenderer.java
+++ modules/apps/blogs/blogs-web/src/main/java/com/liferay/blogs/web/internal/info/item/renderer/BlogsEntryFullContentInfoItemRenderer.java
@@ -62,16 +62,10 @@ public class BlogsEntryFullContentInfoItemRenderer
 		}
 	}
 
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.blogs.web)", unbind = "-"
-	)
-	public void setServletContext(ServletContext servletContext) {
-		_servletContext = servletContext;
-	}
-
 	@Reference
 	private Language _language;
 
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.blogs.web)")
 	private ServletContext _servletContext;
 
 }
