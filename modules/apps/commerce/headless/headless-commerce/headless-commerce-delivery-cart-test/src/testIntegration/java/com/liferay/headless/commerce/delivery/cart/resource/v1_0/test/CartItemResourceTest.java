157c5b0c67142afae63a48cb07e67d56938c3d08
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-08-22 15:15:38 +0200

    COMMERCE-9301 Implementation of missing integration tests

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/cart/resource/v1_0/test/CartItemResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-cart-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/cart/resource/v1_0/test/CartItemResourceTest.java
@@ -15,14 +15,246 @@
 package com.liferay.headless.commerce.delivery.cart.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.account.model.CommerceAccount;
+import com.liferay.commerce.account.service.CommerceAccountLocalService;
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.test.util.CommerceCurrencyTestUtil;
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouse;
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CPInstanceLocalServiceUtil;
+import com.liferay.commerce.product.test.util.CPTestUtil;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.commerce.test.util.CommerceInventoryTestUtil;
+import com.liferay.commerce.test.util.CommerceTestUtil;
+import com.liferay.headless.commerce.delivery.cart.client.dto.v1_0.CartItem;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.test.rule.Inject;
 
+import java.math.BigDecimal;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Ignore;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Andrea Sbarra
+ * @author Crescenzo Rega
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class CartItemResourceTest extends BaseCartItemResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addUser(testCompany);
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			testCompany.getCompanyId(), testGroup.getGroupId(),
+			_user.getUserId());
+
+		_commerceAccount =
+			_commerceAccountLocalService.addBusinessCommerceAccount(
+				"Test Business Account", 0, null, null, true, null, null, null,
+				_serviceContext);
+
+		_commerceCurrency = CommerceCurrencyTestUtil.addCommerceCurrency(
+			testGroup.getCompanyId());
+
+		_commerceChannel = CommerceTestUtil.addCommerceChannel(
+			testGroup.getGroupId(), _commerceCurrency.getCode());
+
+		_commerceInventoryWarehouse =
+			CommerceInventoryTestUtil.addCommerceInventoryWarehouse(
+				_serviceContext);
+	}
+
+	@After
+	@Override
+	public void tearDown() throws Exception {
+		super.tearDown();
+
+		List<CommerceOrder> commerceOrders =
+			_commerceOrderLocalService.getCommerceOrders(
+				_commerceChannel.getGroupId(),
+				_commerceAccount.getCommerceAccountId(), -1, -1, null);
+
+		for (CommerceOrder commerceOrder : commerceOrders) {
+			_commerceOrderLocalService.deleteCommerceOrder(
+				commerceOrder.getCommerceOrderId());
+		}
+
+		if (_commerceAccount != null) {
+			_commerceAccountLocalService.deleteCommerceAccount(
+				_commerceAccount);
+		}
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testDeleteCartItem() throws Exception {
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGetCartItemsPageWithPagination() throws Exception {
+	}
+
+	@Ignore
+	@Override
+	@Test
+	public void testGraphQLDeleteCartItem() throws Exception {
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"quantity"};
+	}
+
+	@Override
+	protected CartItem randomCartItem() throws Exception {
+		CPInstance cpInstance = _addCPInstance();
+
+		return new CartItem() {
+			{
+				quantity = 1;
+				sku = cpInstance.getSku();
+				skuId = cpInstance.getCPInstanceId();
+			}
+		};
+	}
+
+	@Override
+	protected CartItem testDeleteCartItem_addCartItem() throws Exception {
+		return _addCartItem();
+	}
+
+	@Override
+	protected CartItem testGetCartItem_addCartItem() throws Exception {
+		return _addCartItem();
+	}
+
+	@Override
+	protected CartItem testGetCartItemsPage_addCartItem(
+			Long cartId, CartItem cartItem)
+		throws Exception {
+
+		return cartItemResource.postCartItem(cartId, cartItem);
+	}
+
+	@Override
+	protected Long testGetCartItemsPage_getCartId() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrder();
+
+		return commerceOrder.getCommerceOrderId();
+	}
+
+	@Override
+	protected CartItem testGraphQLCartItem_addCartItem() throws Exception {
+		return _addCartItem();
+	}
+
+	@Override
+	protected CartItem testPatchCartItem_addCartItem() throws Exception {
+		return _addCartItem();
+	}
+
+	@Override
+	protected CartItem testPostCartItem_addCartItem(CartItem cartItem)
+		throws Exception {
+
+		return _addCartItem(cartItem);
+	}
+
+	@Override
+	protected CartItem testPutCartItem_addCartItem() throws Exception {
+		return _addCartItem();
+	}
+
+	private CartItem _addCartItem() throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrder();
+
+		return cartItemResource.postCartItem(
+			commerceOrder.getCommerceOrderId(), randomCartItem());
+	}
+
+	private CartItem _addCartItem(CartItem cartItem) throws Exception {
+		CommerceOrder commerceOrder = _addCommerceOrder();
+
+		return cartItemResource.postCartItem(
+			commerceOrder.getCommerceOrderId(), cartItem);
+	}
+
+	private CommerceOrder _addCommerceOrder() throws Exception {
+		if (_commerceOrder != null) {
+			return _commerceOrder;
+		}
+
+		_commerceOrder = _commerceOrderLocalService.addCommerceOrder(
+			_user.getUserId(), _commerceChannel.getGroupId(),
+			_commerceAccount.getCommerceAccountId(),
+			_commerceCurrency.getCommerceCurrencyId(), 0);
+
+		return _commerceOrder;
+	}
+
+	private CPInstance _addCPInstance() throws Exception {
+		CPInstance cpInstance = CPTestUtil.addCPInstanceWithRandomSku(
+			testGroup.getGroupId());
+
+		cpInstance.setPrice(BigDecimal.valueOf(RandomTestUtil.randomDouble()));
+
+		CPInstanceLocalServiceUtil.updateCPInstance(cpInstance);
+
+		CommerceInventoryTestUtil.addCommerceInventoryWarehouseItem(
+			_user.getUserId(), _commerceInventoryWarehouse, cpInstance.getSku(),
+			10);
+
+		_cpInstances.add(cpInstance);
+
+		return cpInstance;
+	}
+
+	private CommerceAccount _commerceAccount;
+
+	@Inject
+	private CommerceAccountLocalService _commerceAccountLocalService;
+
+	@DeleteAfterTestRun
+	private CommerceChannel _commerceChannel;
+
+	@DeleteAfterTestRun
+	private CommerceCurrency _commerceCurrency;
+
+	@DeleteAfterTestRun
+	private CommerceInventoryWarehouse _commerceInventoryWarehouse;
+
+	private CommerceOrder _commerceOrder;
+
+	@Inject
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+	@DeleteAfterTestRun
+	private List<CPInstance> _cpInstances = new ArrayList<>();
+
+	private ServiceContext _serviceContext;
+
+	@DeleteAfterTestRun
+	private User _user;
+
 }
