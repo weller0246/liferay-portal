affb853595b1cdb932ade98456ed21ca9721da05
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-08-22 12:59:03 +0100

    LPS-161164 Allow ContentDashboardItemSubtypeFactory API to support entity class PK.

--- modules/apps/content-dashboard/content-dashboard-api/src/main/java/com/liferay/content/dashboard/item/type/ContentDashboardItemSubtypeFactory.java
+++ modules/apps/content-dashboard/content-dashboard-api/src/main/java/com/liferay/content/dashboard/item/type/ContentDashboardItemSubtypeFactory.java
@@ -21,7 +21,14 @@ import com.liferay.portal.kernel.exception.PortalException;
  */
 public interface ContentDashboardItemSubtypeFactory<T> {
 
-	public ContentDashboardItemSubtype<T> create(long classPK)
+	public default ContentDashboardItemSubtype<T> create(long classPK)
+		throws PortalException {
+
+		return create(classPK, 0);
+	}
+
+	public ContentDashboardItemSubtype<T> create(
+			long classPK, long entityClassPK)
 		throws PortalException;
 
 }
