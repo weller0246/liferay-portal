632b2350641ca7b480b7776bf7044f227b6c4d80
Vy-CES <vy.bui@gs.liferay.com>
2022-08-24 14:20:18 +0700

    LPS-161248 Apply workflow for hidden pages
    Signed-off-by: Vy-CES <vy.bui@gs.liferay.com>

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/workflow/LayoutWorkflowHandler.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/workflow/LayoutWorkflowHandler.java
@@ -67,7 +67,7 @@ public class LayoutWorkflowHandler extends BaseWorkflowHandler<Layout> {
 
 		Layout layout = _layoutLocalService.getLayout(classPK);
 
-		if (layout.isHidden() || layout.isSystem() || !layout.isTypeContent()) {
+		if (layout.isSystem() || !layout.isTypeContent()) {
 			return null;
 		}
 
