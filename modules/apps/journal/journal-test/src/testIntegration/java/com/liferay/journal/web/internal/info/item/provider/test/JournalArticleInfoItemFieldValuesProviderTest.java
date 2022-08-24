b9081eb1f7f82480a36ac18c2b44aba48e6185a6
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-08-24 12:06:11 +0100

    LPS-159752 Add tests for the preview Image.

--- modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/web/internal/info/item/provider/test/JournalArticleInfoItemFieldValuesProviderTest.java
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/web/internal/info/item/provider/test/JournalArticleInfoItemFieldValuesProviderTest.java
@@ -22,22 +22,30 @@ import com.liferay.dynamic.data.mapping.test.util.DDMTemplateTestUtil;
 import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.item.InfoItemFieldValues;
 import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.info.type.WebImage;
 import com.liferay.journal.constants.JournalArticleConstants;
 import com.liferay.journal.constants.JournalFolderConstants;
 import com.liferay.journal.model.JournalArticle;
-import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.io.File;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -49,6 +57,8 @@ import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
 
 /**
  * @author Cristina González
@@ -83,23 +93,22 @@ public class JournalArticleInfoItemFieldValuesProviderTest {
 
 		displayDateCalendar.setTime(new Date());
 
-		JournalArticle journalArticle =
-			JournalArticleLocalServiceUtil.addArticle(
-				null, TestPropsValues.getUserId(), _group.getGroupId(),
-				JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
-				JournalArticleConstants.CLASS_NAME_ID_DEFAULT, 0,
-				StringPool.BLANK, true, JournalArticleConstants.VERSION_DEFAULT,
-				RandomTestUtil.randomLocaleStringMap(),
-				RandomTestUtil.randomLocaleStringMap(),
-				RandomTestUtil.randomLocaleStringMap(), content,
-				ddmStructure.getStructureKey(), ddmTemplate.getTemplateKey(),
-				null, displayDateCalendar.get(Calendar.MONTH),
-				displayDateCalendar.get(Calendar.DAY_OF_MONTH),
-				displayDateCalendar.get(Calendar.YEAR),
-				displayDateCalendar.get(Calendar.HOUR_OF_DAY),
-				displayDateCalendar.get(Calendar.MINUTE), 0, 0, 0, 0, 0, true,
-				0, 0, 0, 0, 0, true, true, false, null, null, null, null,
-				ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
+		JournalArticle journalArticle = _journalArticleLocalService.addArticle(
+			null, TestPropsValues.getUserId(), _group.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+			JournalArticleConstants.CLASS_NAME_ID_DEFAULT, 0, StringPool.BLANK,
+			true, JournalArticleConstants.VERSION_DEFAULT,
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(),
+			RandomTestUtil.randomLocaleStringMap(), content,
+			ddmStructure.getStructureKey(), ddmTemplate.getTemplateKey(), null,
+			displayDateCalendar.get(Calendar.MONTH),
+			displayDateCalendar.get(Calendar.DAY_OF_MONTH),
+			displayDateCalendar.get(Calendar.YEAR),
+			displayDateCalendar.get(Calendar.HOUR_OF_DAY),
+			displayDateCalendar.get(Calendar.MINUTE), 0, 0, 0, 0, 0, true, 0, 0,
+			0, 0, 0, true, true, false, null, null, null, null,
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
 
 		InfoItemFieldValues infoItemFieldValues =
 			_infoItemFieldValuesProvider.getInfoItemFieldValues(journalArticle);
@@ -118,12 +127,126 @@ public class JournalArticleInfoItemFieldValuesProviderTest {
 			journalArticle.getModifiedDate(),
 			modifiedDateInfoFieldValue.getValue());
 
+		Assert.assertNull(
+			infoItemFieldValues.getInfoFieldValue("previewImage"));
+
 		InfoFieldValue<Object> publishDateInfoFieldValue =
 			infoItemFieldValues.getInfoFieldValue("publishDate");
 
 		Assert.assertEquals(
 			journalArticle.getDisplayDate(),
 			publishDateInfoFieldValue.getValue());
+	}
+
+	@Test
+	public void testGetInfoItemFieldValuesWithSmallImage() throws Exception {
+		ServiceContext originalServiceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = _getThemeDisplay();
+
+		ServiceContextThreadLocal.pushServiceContext(
+			_getServiceContext(themeDisplay));
+
+		try {
+			String content = DDMStructureTestUtil.getSampleStructuredContent();
+
+			DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
+				_group.getGroupId(), JournalArticle.class.getName());
+
+			DDMTemplate ddmTemplate = DDMTemplateTestUtil.addTemplate(
+				_group.getGroupId(), ddmStructure.getStructureId(),
+				PortalUtil.getClassNameId(JournalArticle.class));
+
+			Calendar displayDateCalendar = new GregorianCalendar();
+
+			displayDateCalendar.setTime(new Date());
+
+			JournalArticle journalArticle =
+				_journalArticleLocalService.addArticle(
+					null, TestPropsValues.getUserId(), _group.getGroupId(),
+					JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
+					JournalArticleConstants.CLASS_NAME_ID_DEFAULT, 0,
+					StringPool.BLANK, true,
+					JournalArticleConstants.VERSION_DEFAULT,
+					RandomTestUtil.randomLocaleStringMap(),
+					RandomTestUtil.randomLocaleStringMap(),
+					RandomTestUtil.randomLocaleStringMap(), content,
+					ddmStructure.getStructureKey(),
+					ddmTemplate.getTemplateKey(), null,
+					displayDateCalendar.get(Calendar.MONTH),
+					displayDateCalendar.get(Calendar.DAY_OF_MONTH),
+					displayDateCalendar.get(Calendar.YEAR),
+					displayDateCalendar.get(Calendar.HOUR_OF_DAY),
+					displayDateCalendar.get(Calendar.MINUTE), 0, 0, 0, 0, 0,
+					true, 0, 0, 0, 0, 0, true, true, true, null, _getFile(),
+					null, null,
+					ServiceContextTestUtil.getServiceContext(
+						_group.getGroupId()));
+
+			InfoItemFieldValues infoItemFieldValues =
+				_infoItemFieldValuesProvider.getInfoItemFieldValues(
+					journalArticle);
+
+			InfoFieldValue<Object> previewImageInfoFieldValue =
+				infoItemFieldValues.getInfoFieldValue("previewImage");
+
+			WebImage webImage = (WebImage)previewImageInfoFieldValue.getValue();
+
+			Assert.assertEquals(
+				journalArticle.getArticleImageURL(themeDisplay),
+				webImage.getUrl());
+		}
+		finally {
+			ServiceContextThreadLocal.pushServiceContext(
+				originalServiceContext);
+		}
+	}
+
+	private File _getFile() throws Exception {
+		Class<?> clazz = getClass();
+
+		ClassLoader classLoader = clazz.getClassLoader();
+
+		File tempFile = FileUtil.createTempFile("jpg");
+
+		FileUtil.write(
+			tempFile,
+			FileUtil.getBytes(
+				classLoader.getResourceAsStream(
+					"com/liferay/journal/info/item/provider/test/dependencies" +
+						"/test.jpg")),
+			false);
+
+		return tempFile;
+	}
+
+	private ServiceContext _getServiceContext(ThemeDisplay themeDisplay)
+		throws Exception {
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(
+				_group.getGroupId(), TestPropsValues.getUserId());
+
+		serviceContext.setRequest(themeDisplay.getRequest());
+
+		return serviceContext;
+	}
+
+	private ThemeDisplay _getThemeDisplay() {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		MockHttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, themeDisplay);
+
+		themeDisplay.setRequest(mockHttpServletRequest);
+
+		themeDisplay.setURLCurrent("http://localhost:8080/currentURL");
+
+		return themeDisplay;
 	}
 
 	@DeleteAfterTestRun
@@ -133,5 +256,8 @@ public class JournalArticleInfoItemFieldValuesProviderTest {
 		filter = "component.name=*.JournalArticleInfoItemFieldValuesProvider"
 	)
 	private InfoItemFieldValuesProvider _infoItemFieldValuesProvider;
+
+	@Inject
+	private JournalArticleLocalService _journalArticleLocalService;
 
 }
