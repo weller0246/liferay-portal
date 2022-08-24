8082eba2e89dc88160e4175f02cffd436918bee3
thaynam <lazaro@thaynam.com>
2022-08-24 15:48:59 -0300

    LPS-154909 Create column object

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/common/enums/mdfColumnKey.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+export enum MDFColumnKey {
+	id = 'PRMMDFC-001',
+	status = 'PRMMDFC-002',
+	activityPeriod = 'PRMMDFC-003',
+	partner = 'PRMMDFC-004',
+	totalCost = 'PRMMDFC-005',
+	type = 'PRMMDFC-006',
+	requested = 'PRMMDFC-007',
+	amountClaimed = 'PRMMDFC-008',
+	approved = 'PRMMDFC-009',
+	paid = 'PRMMDFC-010',
+	reimbursementClaims = 'PRMMDFC-11',
+	dateSubmitted = 'PRMMDFC-12',
+}
