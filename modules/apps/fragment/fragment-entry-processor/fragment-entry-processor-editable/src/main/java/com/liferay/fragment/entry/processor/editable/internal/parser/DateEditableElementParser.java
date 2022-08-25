1ec0be1269ac12e7f31fe8f0e429938ce6c3321e
Diego Hu <diego.hu@liferay.com>
2022-08-25 18:14:45 +0200

    LPS-161398 Create editable of type date-time

---
+++ modules/apps/fragment/fragment-entry-processor/fragment-entry-processor-editable/src/main/java/com/liferay/fragment/entry/processor/editable/internal/parser/DateEditableElementParser.java
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.entry.processor.editable.internal.parser;
+
+import com.liferay.fragment.entry.processor.editable.parser.EditableElementParser;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.ResourceBundle;
+
+import org.jsoup.nodes.Element;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Diego Hu
+ */
+@Component(
+	enabled = false, immediate = true, property = "type=date-time",
+	service = EditableElementParser.class
+)
+public class DateEditableElementParser extends TextEditableElementParser {
+
+	@Override
+	public String getValue(Element element) {
+		String html = element.html();
+
+		if (Validator.isNull(html.trim())) {
+			ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+				"content.Language", getClass());
+
+			return _language.get(resourceBundle, "example-date-time");
+		}
+
+		return html;
+	}
+
+	@Override
+	protected String getEditableElementType() {
+		return "date-time";
+	}
+
+	@Reference
+	private Language _language;
+
+}
