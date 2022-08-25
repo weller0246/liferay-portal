a7a8dbfa200442b89a9bf047f9afa77bf606a2e0
Alicia García <alicia.garcia@liferay.com>
2022-08-25 11:59:41 +0200

    LPS-161368 Create UploadKBArticleAttachmentsMVCActionCommand

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/portlet/action/UploadKBArticleAttachmentsMVCActionCommand.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.internal.portlet.action;
+
+import com.liferay.item.selector.ItemSelectorUploadResponseHandler;
+import com.liferay.knowledge.base.constants.KBPortletKeys;
+import com.liferay.knowledge.base.web.internal.upload.KBArticleAttachmentKBUploadFileEntryHandler;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.upload.UploadHandler;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alicia García
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + KBPortletKeys.KNOWLEDGE_BASE_ADMIN,
+		"javax.portlet.name=" + KBPortletKeys.KNOWLEDGE_BASE_ARTICLE,
+		"mvc.command.name=uploadKBArticleAttachments"
+	},
+	service = MVCActionCommand.class
+)
+public class UploadKBArticleAttachmentsMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		_uploadHandler.upload(
+			_kbArticleAttachmentKBUploadFileEntryHandler,
+			_itemSelectorUploadResponseHandler, actionRequest, actionResponse);
+	}
+
+	@Reference
+	private ItemSelectorUploadResponseHandler
+		_itemSelectorUploadResponseHandler;
+
+	@Reference
+	private KBArticleAttachmentKBUploadFileEntryHandler
+		_kbArticleAttachmentKBUploadFileEntryHandler;
+
+	@Reference
+	private UploadHandler _uploadHandler;
+
+}
