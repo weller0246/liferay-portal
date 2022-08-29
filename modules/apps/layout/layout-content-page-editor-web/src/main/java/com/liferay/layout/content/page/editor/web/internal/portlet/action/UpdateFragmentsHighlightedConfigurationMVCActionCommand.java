4ce312d352f4b7630f2a3e55730db70bdf82f089
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-08-29 09:32:07 +0200

    LPS-159979 Implement mark and unmark a fragment as highlighted

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFragmentsHighlightedConfigurationMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdateFragmentsHighlightedConfigurationMVCActionCommand.java
@@ -16,15 +16,28 @@ package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactory;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.SetUtil;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Set;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Lourdes Fernández Besada
@@ -47,8 +60,62 @@ public class UpdateFragmentsHighlightedConfigurationMVCActionCommand
 
 		JSONPortletResponseUtil.writeJSON(
 			actionRequest, actionResponse,
-			JSONUtil.put(
-				"highlightedFragments", JSONFactoryUtil.createJSONArray()));
+			_updateFragmentsHighlightedConfiguration(actionRequest));
 	}
+
+	private JSONObject _updateFragmentsHighlightedConfiguration(
+			ActionRequest actionRequest)
+		throws Exception {
+
+		String fragmentEntryKey = ParamUtil.getString(
+			actionRequest, "fragmentEntryKey");
+
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			actionRequest);
+
+		if (Validator.isNull(fragmentEntryKey)) {
+			hideDefaultSuccessMessage(actionRequest);
+
+			return JSONUtil.put(
+				"error",
+				_language.get(
+					httpServletRequest, "an-unexpected-error-occurred"));
+		}
+
+		boolean highlighted = ParamUtil.getBoolean(
+			actionRequest, "highlighted");
+
+		PortalPreferences portalPreferences =
+			_portletPreferencesFactory.getPortalPreferences(httpServletRequest);
+
+		Set<String> highlightedFragmentEntryKeys = SetUtil.fromArray(
+			portalPreferences.getValues(
+				ContentPageEditorPortletKeys.CONTENT_PAGE_EDITOR_PORTLET,
+				"highlightedFragmentEntryKeys", new String[0]));
+
+		if (highlighted) {
+			highlightedFragmentEntryKeys.add(fragmentEntryKey);
+		}
+		else {
+			highlightedFragmentEntryKeys.remove(fragmentEntryKey);
+		}
+
+		portalPreferences.setValues(
+			ContentPageEditorPortletKeys.CONTENT_PAGE_EDITOR_PORTLET,
+			"highlightedFragmentEntryKeys",
+			highlightedFragmentEntryKeys.toArray(new String[0]));
+
+		return JSONUtil.put(
+			"highlightedFragments", JSONFactoryUtil.createJSONArray());
+	}
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private PortletPreferencesFactory _portletPreferencesFactory;
 
 }
