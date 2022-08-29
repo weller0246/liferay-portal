10402536eb897f0c03d7e726729abd1000533228
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-08-29 09:32:12 +0200

    LPS-159979 Return the updated highlighted portlets JSONArray.
    The logic to get the portlets and generate its JSONs is copied from GetPortletsMVCResourceCommand.

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdatePortletsHighlightedConfigurationMVCActionCommand.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/UpdatePortletsHighlightedConfigurationMVCActionCommand.java
@@ -15,10 +15,17 @@
 package com.liferay.layout.content.page.editor.web.internal.portlet.action;
 
 import com.liferay.layout.content.page.editor.constants.ContentPageEditorPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
 import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.model.PortletItem;
+import com.liferay.portal.kernel.model.PortletPreferences;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.portlet.PortletIdCodec;
@@ -26,19 +33,29 @@ import com.liferay.portal.kernel.portlet.PortletPreferencesFactory;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.PortletItemLocalService;
+import com.liferay.portal.kernel.service.PortletLocalService;
 import com.liferay.portal.kernel.service.permission.PortletPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.SetUtil;
 import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.kernel.util.comparator.PortletTitleComparator;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 import javax.portlet.ActionRequest;
 import javax.portlet.ActionResponse;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -65,6 +82,110 @@ public class UpdatePortletsHighlightedConfigurationMVCActionCommand
 		JSONPortletResponseUtil.writeJSON(
 			actionRequest, actionResponse,
 			_updatePortletsHighlightedConfiguration(actionRequest));
+	}
+
+	private JSONArray _getPortletItemsJSONArray(
+		Portlet portlet, ThemeDisplay themeDisplay) {
+
+		List<PortletItem> portletItems =
+			_portletItemLocalService.getPortletItems(
+				themeDisplay.getScopeGroupId(), portlet.getPortletId(),
+				PortletPreferences.class.getName());
+
+		if (ListUtil.isEmpty(portletItems)) {
+			return JSONFactoryUtil.createJSONArray();
+		}
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		for (PortletItem portletItem : portletItems) {
+			jsonArray.put(
+				JSONUtil.put(
+					"instanceable", portlet.isInstanceable()
+				).put(
+					"portletId", portlet.getPortletId()
+				).put(
+					"portletItemId", portletItem.getPortletItemId()
+				).put(
+					"title", HtmlUtil.escape(portletItem.getName())
+				));
+		}
+
+		return jsonArray;
+	}
+
+	private List<Portlet> _getPortlets(
+		Set<String> portletIds, ThemeDisplay themeDisplay) {
+
+		List<Portlet> portlets = new ArrayList<>();
+
+		for (String portletId : portletIds) {
+			Portlet portlet = _portletLocalService.getPortletById(
+				themeDisplay.getCompanyId(), portletId);
+
+			if ((portlet == null) ||
+				ArrayUtil.contains(
+					_UNSUPPORTED_PORTLETS_NAMES, portlet.getPortletName())) {
+
+				continue;
+			}
+
+			try {
+				if (PortletPermissionUtil.contains(
+						themeDisplay.getPermissionChecker(),
+						themeDisplay.getLayout(), portlet,
+						ActionKeys.ADD_TO_PAGE)) {
+
+					portlets.add(portlet);
+				}
+			}
+			catch (PortalException portalException) {
+				_log.error(
+					"Unable to check portlet permissions for " +
+						portlet.getPortletId(),
+					portalException);
+			}
+		}
+
+		return portlets;
+	}
+
+	private JSONArray _getPortletsJSONArray(
+		HttpServletRequest httpServletRequest, Set<String> portletIds,
+		ThemeDisplay themeDisplay) {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		HttpSession httpSession = httpServletRequest.getSession();
+
+		ServletContext servletContext = httpSession.getServletContext();
+
+		List<Portlet> portlets = _getPortlets(portletIds, themeDisplay);
+
+		portlets = ListUtil.sort(
+			portlets,
+			new PortletTitleComparator(
+				servletContext, themeDisplay.getLocale()));
+
+		for (Portlet portlet : portlets) {
+			jsonArray.put(
+				JSONUtil.put(
+					"highlighted", true
+				).put(
+					"instanceable", portlet.isInstanceable()
+				).put(
+					"portletId", portlet.getPortletId()
+				).put(
+					"portletItems",
+					_getPortletItemsJSONArray(portlet, themeDisplay)
+				).put(
+					"title",
+					_portal.getPortletTitle(
+						portlet, servletContext, themeDisplay.getLocale())
+				));
+		}
+
+		return jsonArray;
 	}
 
 	private JSONObject _updatePortletsHighlightedConfiguration(
@@ -119,14 +240,29 @@ public class UpdatePortletsHighlightedConfigurationMVCActionCommand
 			highlightedPortletIds.toArray(new String[0]));
 
 		return JSONUtil.put(
-			"highlightedPortlets", JSONFactoryUtil.createJSONArray());
+			"highlightedPortlets",
+			_getPortletsJSONArray(
+				httpServletRequest, highlightedPortletIds, themeDisplay));
 	}
+
+	private static final String[] _UNSUPPORTED_PORTLETS_NAMES = {
+		"com_liferay_nested_portlets_web_portlet_NestedPortletsPortlet"
+	};
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpdatePortletsHighlightedConfigurationMVCActionCommand.class);
 
 	@Reference
 	private Language _language;
 
 	@Reference
 	private Portal _portal;
+
+	@Reference
+	private PortletItemLocalService _portletItemLocalService;
+
+	@Reference
+	private PortletLocalService _portletLocalService;
 
 	@Reference
 	private PortletPreferencesFactory _portletPreferencesFactory;
