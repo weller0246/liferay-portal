6941772418fcc31de65678ebb382f98f20e4fada
shuyangzhou <shuyang.zhou@liferay.com>
2022-08-30 15:50:00 -0700

    LPS-163981 Remove Stream usages

--- modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorTrackerImpl.java
+++ modules/apps/fragment/fragment-impl/src/main/java/com/liferay/fragment/internal/contributor/FragmentCollectionContributorTrackerImpl.java
@@ -36,15 +36,12 @@ import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -107,40 +104,23 @@ public class FragmentCollectionContributorTrackerImpl
 
 	@Override
 	public Map<String, FragmentEntry> getFragmentEntries(Locale locale) {
-		Collection<FragmentCollectionContributor>
-			fragmentCollectionContributors = _serviceTrackerMap.values();
+		Map<String, FragmentEntry> fragmentEntries = new HashMap<>();
 
-		Stream<FragmentCollectionContributor> stream =
-			fragmentCollectionContributors.stream();
+		for (FragmentCollectionContributor fragmentCollectionContributor :
+				_serviceTrackerMap.values()) {
 
-		return stream.map(
-			fragmentCollectionContributor -> {
-				Map<String, FragmentEntry> fragmentEntries = new HashMap<>();
+			for (int type : _SUPPORTED_FRAGMENT_TYPES) {
+				for (FragmentEntry fragmentEntry :
+						fragmentCollectionContributor.getFragmentEntries(
+							type, locale)) {
 
-				for (int type : _SUPPORTED_FRAGMENT_TYPES) {
-					for (FragmentEntry fragmentEntry :
-							fragmentCollectionContributor.getFragmentEntries(
-								type, locale)) {
-
-						fragmentEntries.put(
-							fragmentEntry.getFragmentEntryKey(), fragmentEntry);
-					}
+					fragmentEntries.put(
+						fragmentEntry.getFragmentEntryKey(), fragmentEntry);
 				}
-
-				return fragmentEntries;
 			}
-		).flatMap(
-			fragmentEntriesMap -> {
-				Collection<FragmentEntry> fragmentEntries =
-					fragmentEntriesMap.values();
+		}
 
-				return fragmentEntries.stream();
-			}
-		).collect(
-			Collectors.toMap(
-				FragmentEntry::getFragmentEntryKey,
-				fragmentEntry -> fragmentEntry)
-		);
+		return fragmentEntries;
 	}
 
 	@Override
@@ -152,20 +132,21 @@ public class FragmentCollectionContributorTrackerImpl
 
 	@Override
 	public ResourceBundleLoader getResourceBundleLoader() {
-		Collection<FragmentCollectionContributor>
-			fragmentCollectionContributors = _serviceTrackerMap.values();
+		List<ResourceBundleLoader> resourceBundleLoaders = new ArrayList<>();
 
-		Stream<FragmentCollectionContributor> stream =
-			fragmentCollectionContributors.stream();
+		for (FragmentCollectionContributor fragmentCollectionContributor :
+				_serviceTrackerMap.values()) {
+
+			ResourceBundleLoader resourceBundleLoader =
+				fragmentCollectionContributor.getResourceBundleLoader();
+
+			if (resourceBundleLoader != null) {
+				resourceBundleLoaders.add(resourceBundleLoader);
+			}
+		}
 
 		return new AggregateResourceBundleLoader(
-			stream.map(
-				FragmentCollectionContributor::getResourceBundleLoader
-			).filter(
-				Objects::nonNull
-			).toArray(
-				ResourceBundleLoader[]::new
-			));
+			resourceBundleLoaders.toArray(new ResourceBundleLoader[0]));
 	}
 
 	@Activate
