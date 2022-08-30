71661433cd54672e65d2e51a023c439e38d37c4d
Michael Bowerman <michael.bowerman@liferay.com>
2022-08-30 11:53:49 -0700

    LPS-161841 We need to flush the session after every update; otherwise, the batch (simultaneous) updates will still cause the IX_D3ACAD4A even if the updates were applied in the correct order

--- modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/change/tracking/spi/resolver/BaseJournalArticleVersionConstraintResolver.java
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/change/tracking/spi/resolver/BaseJournalArticleVersionConstraintResolver.java
@@ -21,6 +21,7 @@ import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.journal.util.comparator.ArticleVersionComparator;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.persistence.change.tracking.CTPersistence;
 import com.liferay.portal.kernel.util.MathUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.language.LanguageResources;
@@ -90,10 +91,15 @@ public abstract class BaseJournalArticleVersionConstraintResolver
 		double currentVersion = MathUtil.format(
 			latestVersion + (0.1 * sourceArticles.size()), 1, 1);
 
+		CTPersistence ctPersistence =
+			journalArticleLocalService.getCTPersistence();
+
 		for (JournalArticle article : sourceArticles) {
 			article.setVersion(currentVersion);
 
 			journalArticleLocalService.updateJournalArticle(article);
+
+			ctPersistence.flush();
 
 			currentVersion = MathUtil.format(currentVersion - 0.1, 1, 1);
 		}
