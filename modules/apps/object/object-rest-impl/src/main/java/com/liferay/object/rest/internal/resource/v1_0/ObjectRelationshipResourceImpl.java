8d76fd1896711d28907f6ed3576c7a25b9256bab
Alejandro Tardín <alejandro.tardin@liferay.com>
2022-08-30 10:44:50 +0200

    LPS-161705 No need for the helper yet

--- modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectRelationshipResourceImpl.java
+++ modules/apps/object/object-rest-impl/src/main/java/com/liferay/object/rest/internal/resource/v1_0/ObjectRelationshipResourceImpl.java
@@ -14,9 +14,26 @@
 
 package com.liferay.object.rest.internal.resource.v1_0;
 
-import com.liferay.object.rest.internal.helper.ObjectRelationshipResourceHelper;
+import com.liferay.object.exception.NoSuchObjectRelationshipException;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectEntry;
+import com.liferay.object.model.ObjectRelationship;
+import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
+import com.liferay.object.rest.manager.v1_0.ObjectEntryManagerTracker;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
+
+import java.net.URI;
+
+import java.util.List;
 
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -41,12 +58,131 @@ public class ObjectRelationshipResourceImpl
 			String objectRelationshipName, Pagination pagination)
 		throws Exception {
 
-		return _objectRelationshipResourceHelper.getObjectRelatedObjectsPage(
-			objectEntryId, objectRelationshipName, pagination, _uriInfo);
+		ObjectDefinition currentObjectDefinition = _getCurrentObjectDefinition(
+			objectEntryId, objectRelationshipName, _uriInfo);
+
+		ObjectDefinition relatedObjectDefinition = _getRelatedObjectDefinition(
+			_getObjectRelationshipByName(objectRelationshipName),
+			currentObjectDefinition);
+
+		ObjectEntryManager objectEntryManager =
+			_objectEntryManagerTracker.getObjectEntryManager(
+				currentObjectDefinition.getStorageType());
+
+		if (relatedObjectDefinition.isSystem()) {
+			return objectEntryManager.getRelatedSystemObjectEntries(
+				currentObjectDefinition, objectEntryId, objectRelationshipName,
+				pagination);
+		}
+
+		return (Page)objectEntryManager.getObjectEntryRelatedObjectEntries(
+			_getDefaultDTOConverterContext(
+				currentObjectDefinition, objectEntryId, _uriInfo),
+			currentObjectDefinition, objectEntryId, objectRelationshipName,
+			pagination);
+	}
+
+	private ObjectDefinition _getCurrentObjectDefinition(
+			long objectEntryId, String objectRelationshipName, UriInfo uriInfo)
+		throws Exception {
+
+		ObjectEntry objectEntry = _objectEntryLocalService.fetchObjectEntry(
+			objectEntryId);
+
+		if (objectEntry != null) {
+			return _objectDefinitionLocalService.getObjectDefinition(
+				objectEntry.getObjectDefinitionId());
+		}
+
+		ObjectRelationship objectRelationship = _getObjectRelationshipByName(
+			objectRelationshipName);
+
+		ObjectDefinition objectDefinition =
+			_objectDefinitionLocalService.getObjectDefinition(
+				objectRelationship.getObjectDefinitionId1());
+
+		String objectDefinitionRESTContextPath =
+			objectDefinition.getRESTContextPath();
+
+		URI baseURI = uriInfo.getBaseUri();
+
+		String path = baseURI.getPath();
+
+		String endpointBasePath = path.split("/")[2];
+
+		if (!objectDefinitionRESTContextPath.equals(endpointBasePath)) {
+			objectDefinition =
+				_objectDefinitionLocalService.getObjectDefinition(
+					objectRelationship.getObjectDefinitionId2());
+		}
+
+		return objectDefinition;
+	}
+
+	private DefaultDTOConverterContext _getDefaultDTOConverterContext(
+		ObjectDefinition objectDefinition, Long objectEntryId,
+		UriInfo uriInfo) {
+
+		return new DefaultDTOConverterContext(
+			_dtoConverterRegistry, objectEntryId,
+			LocaleUtil.fromLanguageId(
+				objectDefinition.getDefaultLanguageId(), true, false),
+			uriInfo, null);
+	}
+
+	private ObjectRelationship _getObjectRelationshipByName(
+			String objectRelationshipName)
+		throws Exception {
+
+		List<ObjectRelationship> objectRelationships = ListUtil.filter(
+			_objectRelationshipLocalService.getObjectRelationships(
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS),
+			objectRelationship ->
+				!objectRelationship.isReverse() &&
+				objectRelationship.getName(
+				).equals(
+					objectRelationshipName
+				));
+
+		if (objectRelationships.isEmpty()) {
+			throw new NoSuchObjectRelationshipException(
+				"No ObjectRelationship exists with the name " +
+					objectRelationshipName);
+		}
+
+		return objectRelationships.get(0);
+	}
+
+	private ObjectDefinition _getRelatedObjectDefinition(
+			ObjectRelationship objectRelationship,
+			ObjectDefinition objectDefinition)
+		throws Exception {
+
+		long objectDefinitionId1 = objectRelationship.getObjectDefinitionId1();
+
+		if (objectDefinitionId1 != objectDefinition.getObjectDefinitionId()) {
+			return _objectDefinitionLocalService.getObjectDefinition(
+				objectRelationship.getObjectDefinitionId1());
+		}
+
+		return _objectDefinitionLocalService.getObjectDefinition(
+			objectRelationship.getObjectDefinitionId2());
 	}
 
 	@Reference
-	private ObjectRelationshipResourceHelper _objectRelationshipResourceHelper;
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Reference
+	private ObjectEntryManagerTracker _objectEntryManagerTracker;
+
+	@Reference
+	private ObjectRelationshipLocalService _objectRelationshipLocalService;
 
 	@Context
 	private UriInfo _uriInfo;
