9668acb470a8b5e750207f4e4c4177ec41456ad5
Rafael Praxedes <rafael.praxedes@liferay.com>
2022-08-30 10:23:11 -0300

    LPS-161793 Increase default interval of WorkflowMetricsSLAProcessMessageListener to 10 minutes

--- modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/configuration/WorkflowMetricsConfiguration.java
+++ modules/dxp/apps/portal-workflow/portal-workflow-metrics-service/src/main/java/com/liferay/portal/workflow/metrics/internal/configuration/WorkflowMetricsConfiguration.java
@@ -30,7 +30,7 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 public interface WorkflowMetricsConfiguration {
 
 	@Meta.AD(
-		deflt = "1", description = "check-sla-job-interval-description",
+		deflt = "10", description = "check-sla-job-interval-description",
 		name = "check-sla-job-interval", required = false
 	)
 	public int checkSLAJobInterval();
