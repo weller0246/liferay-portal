5aa491f53d50e3624ea2c0df0ff9f0f66622b004
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-08-30 12:41:19 +0100

    LPS-155386 Add Filter by Pending.

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/display/context/ContentDashboardAdminManagementToolbarDisplayContext.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/display/context/ContentDashboardAdminManagementToolbarDisplayContext.java
@@ -930,9 +930,9 @@ public class ContentDashboardAdminManagementToolbarDisplayContext
 
 	private List<Integer> _getStatuses() {
 		return Arrays.asList(
-			WorkflowConstants.STATUS_ANY, WorkflowConstants.STATUS_DRAFT,
-			WorkflowConstants.STATUS_SCHEDULED,
-			WorkflowConstants.STATUS_APPROVED);
+			WorkflowConstants.STATUS_ANY, WorkflowConstants.STATUS_APPROVED,
+			WorkflowConstants.STATUS_DRAFT, WorkflowConstants.STATUS_PENDING,
+			WorkflowConstants.STATUS_SCHEDULED);
 	}
 
 	private String _getStatusLabel(int status) {
