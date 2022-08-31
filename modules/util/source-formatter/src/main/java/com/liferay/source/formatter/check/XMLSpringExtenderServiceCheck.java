15d1d96167acf3f7c70a2792bdc82ea8f19b7dda
Alan Huang <alan.huang@liferay.com>
2022-08-31 16:07:02 +0800

    LPS-160078 Add XMLSpringExtenderServiceCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/XMLSpringExtenderServiceCheck.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.check.util.SourceUtil;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
+/**
+ * @author Alan Huang
+ */
+public class XMLSpringExtenderServiceCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isLiferaySourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws DocumentException {
+
+		if (!absolutePath.contains("/modules/apps/") &&
+			!absolutePath.contains("/modules/dxp/apps/")) {
+
+			return content;
+		}
+
+		int x = absolutePath.indexOf("/modules/apps/archived/");
+
+		if (x != -1) {
+			return content;
+		}
+
+		if (fileName.endsWith("/module-spring.xml")) {
+			addMessage(
+				fileName,
+				"Do not use Spring extender service as a dependency " +
+					"injection, use DS instead");
+
+			return content;
+		}
+
+		if (fileName.endsWith("/service.xml")) {
+			Document document = SourceUtil.readXML(content);
+
+			Element rootElement = document.getRootElement();
+
+			String dependencyInjectorName = rootElement.attributeValue(
+				"dependency-injector");
+
+			if (Validator.isNull(dependencyInjectorName)) {
+				return content;
+			}
+
+			if (dependencyInjectorName.equals("spring")) {
+				addMessage(
+					fileName,
+					"Do not use Spring extender service as a dependency " +
+						"injection, use DS instead");
+
+				return content;
+			}
+		}
+
+		return content;
+	}
+
+}
