9b3c863a964ffe863bcbe8ed652c71a9479c6ba6
Bryan Engler <bryan.engler@liferay.com>
2022-08-31 12:42:32 -0700

    LPS-156862 LPS-164847 portal-search: Set include regex
    https://issues.liferay.com/browse/LPS-164847

--- modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/category/CategoryFacetSearchContributorImpl.java
+++ modules/apps/portal-search/portal-search/src/main/java/com/liferay/portal/search/internal/facet/category/CategoryFacetSearchContributorImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.portal.search.internal.facet.category;
 
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.facet.config.FacetConfiguration;
@@ -106,6 +108,13 @@ public class CategoryFacetSearchContributorImpl
 			return this;
 		}
 
+		@Override
+		public CategoryFacetBuilder vocabularyIds(String[] vocabularyIds) {
+			_vocabularyIds = vocabularyIds;
+
+			return this;
+		}
+
 		protected FacetConfiguration buildFacetConfiguration(Facet facet) {
 			FacetConfiguration facetConfiguration = new FacetConfiguration();
 
@@ -120,10 +129,38 @@ public class CategoryFacetSearchContributorImpl
 			jsonObject.put(
 				"frequencyThreshold", _frequencyThreshold
 			).put(
+				"include", _getIncludeRegexString(facet.getFieldName())
+			).put(
 				"maxTerms", _maxTerms
 			);
 
 			return facetConfiguration;
+		}
+
+		private String _getIncludeRegexString(String fieldName) {
+			if (ArrayUtil.isEmpty(_vocabularyIds) ||
+				fieldName.equals("assetCategoryIds")) {
+
+				return null;
+			}
+
+			StringBundler sb = new StringBundler(_vocabularyIds.length * 5);
+
+			for (String vocabularyId : _vocabularyIds) {
+				sb.append(vocabularyId);
+				sb.append(StringPool.DASH);
+				sb.append(StringPool.PERIOD);
+				sb.append(StringPool.STAR);
+				sb.append(StringPool.PIPE);
+			}
+
+			if (sb.index() > 0) {
+				sb.setIndex(sb.index() - 1);
+
+				return sb.toString();
+			}
+
+			return null;
 		}
 
 		private String _aggregationName;
@@ -131,6 +168,7 @@ public class CategoryFacetSearchContributorImpl
 		private int _maxTerms;
 		private final SearchContext _searchContext;
 		private long[] _selectedCategoryIds;
+		private String[] _vocabularyIds;
 
 	}
 
