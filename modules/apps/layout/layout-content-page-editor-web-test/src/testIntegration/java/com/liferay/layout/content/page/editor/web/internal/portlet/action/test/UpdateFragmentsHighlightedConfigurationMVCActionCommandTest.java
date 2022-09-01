ca6ddee938a90e60bf3a6022fd99b368ad87ed50
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-09-01 20:34:54 +0200

    LPS-159981 Add a basic test based in a user that changes his favorite fragments preferences.
    1. The user mark as favorite the heading fragment
    2. The user mark as favorite the HTML fragment
    3. The user unmark as favorite the HTML fragment
    3. The user unmark as favorite the heading fragment

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/UpdateFragmentsHighlightedConfigurationMVCActionCommandTest.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/portlet/action/test/UpdateFragmentsHighlightedConfigurationMVCActionCommandTest.java
@@ -16,21 +16,43 @@ package com.liferay.layout.content.page.editor.web.internal.portlet.action.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionRequest;
+import com.liferay.portal.kernel.test.portlet.MockLiferayPortletActionResponse;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 
+import javax.portlet.ActionRequest;
+
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -55,6 +77,115 @@ public class UpdateFragmentsHighlightedConfigurationMVCActionCommandTest {
 		_layout = LayoutTestUtil.addTypeContentLayout(_group);
 	}
 
+	@Test
+	public void testUpdateFragmentsHighlightedConfiguration() throws Exception {
+		try {
+			User user = UserTestUtil.addGroupAdminUser(_group);
+
+			_assertUpdateHighlightedFragment(
+				new String[] {"BASIC_COMPONENT-heading"},
+				"BASIC_COMPONENT-heading", true, user);
+
+			_assertUpdateHighlightedFragment(
+				new String[] {
+					"BASIC_COMPONENT-heading", "BASIC_COMPONENT-html"
+				},
+				"BASIC_COMPONENT-html", true, user);
+
+			_assertUpdateHighlightedFragment(
+				new String[] {"BASIC_COMPONENT-heading"},
+				"BASIC_COMPONENT-html", false, user);
+
+			_assertUpdateHighlightedFragment(
+				new String[0], "BASIC_COMPONENT-heading", false, user);
+		}
+		finally {
+			ServiceContextThreadLocal.popServiceContext();
+		}
+	}
+
+	private void _assertUpdateHighlightedFragment(
+			String[] expectedHighlightedFragmentEntryKeys,
+			String fragmentEntryKey, boolean highlighted, User user)
+		throws Exception {
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			_getMockLiferayPortletActionRequest(user);
+
+		mockLiferayPortletActionRequest.addParameter(
+			"fragmentEntryKey", fragmentEntryKey);
+		mockLiferayPortletActionRequest.addParameter(
+			"highlighted", String.valueOf(highlighted));
+
+		JSONObject jsonObject = ReflectionTestUtil.invoke(
+			_mvcActionCommand, "_updateFragmentsHighlightedConfiguration",
+			new Class<?>[] {ActionRequest.class},
+			mockLiferayPortletActionRequest);
+
+		Assert.assertTrue(jsonObject.has("highlightedFragments"));
+		Assert.assertFalse(jsonObject.has("error"));
+
+		JSONArray highlightedFragmentsJSONArray = jsonObject.getJSONArray(
+			"highlightedFragments");
+
+		Assert.assertEquals(
+			expectedHighlightedFragmentEntryKeys.length,
+			highlightedFragmentsJSONArray.length());
+
+		for (int i = 0; i < highlightedFragmentsJSONArray.length(); i++) {
+			JSONObject fragmentEntryJSONObject =
+				highlightedFragmentsJSONArray.getJSONObject(i);
+
+			Assert.assertTrue(
+				ArrayUtil.contains(
+					expectedHighlightedFragmentEntryKeys,
+					fragmentEntryJSONObject.getString("fragmentEntryKey")));
+		}
+	}
+
+	private MockLiferayPortletActionRequest _getMockLiferayPortletActionRequest(
+			User user)
+		throws Exception {
+
+		MockLiferayPortletActionRequest mockLiferayPortletActionRequest =
+			new MockLiferayPortletActionRequest();
+
+		mockLiferayPortletActionRequest.setAttribute(
+			JavaConstants.JAVAX_PORTLET_RESPONSE,
+			new MockLiferayPortletActionResponse());
+		mockLiferayPortletActionRequest.setAttribute(WebKeys.LAYOUT, _layout);
+		mockLiferayPortletActionRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(user));
+
+		return mockLiferayPortletActionRequest;
+	}
+
+	private ThemeDisplay _getThemeDisplay(User user) throws Exception {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setCompany(_company);
+		themeDisplay.setLayout(_layout);
+		themeDisplay.setLayoutSet(_layout.getLayoutSet());
+		themeDisplay.setLayoutTypePortlet(
+			(LayoutTypePortlet)_layout.getLayoutType());
+		themeDisplay.setLocale(PortalUtil.getSiteDefaultLocale(_group));
+
+		LayoutSet layoutSet = _group.getPublicLayoutSet();
+
+		themeDisplay.setLookAndFeel(layoutSet.getTheme(), null);
+
+		themeDisplay.setPermissionChecker(
+			PermissionCheckerFactoryUtil.create(user));
+		themeDisplay.setPlid(_layout.getPlid());
+		themeDisplay.setRealUser(user);
+		themeDisplay.setScopeGroupId(_group.getGroupId());
+		themeDisplay.setSignedIn(true);
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+		themeDisplay.setUser(user);
+
+		return themeDisplay;
+	}
+
 	private Company _company;
 
 	@Inject
@@ -69,5 +200,8 @@ public class UpdateFragmentsHighlightedConfigurationMVCActionCommandTest {
 		filter = "mvc.command.name=/layout_content_page_editor/update_fragments_highlighted_configuration"
 	)
 	private MVCActionCommand _mvcActionCommand;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
 
 }
