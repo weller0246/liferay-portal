a2bfd96bdc2e0b57c8e9de55acd4fd15fa18c185
Clarice Brasil <clarice.brasil@liferay.com>
2022-09-01 11:37:21 -0300

    LPS-161975 Fix message in dxp download keys

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/utils/constants/alertAggregateKeysDownloadText.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/utils/constants/alertAggregateKeysDownloadText.js
@@ -14,7 +14,7 @@ import {ALERT_DOWNLOAD_TYPE} from '../../../../utils/constants/alertDownloadType
 
 export const ALERT_ACTIVATION_AGGREGATED_KEYS_DOWNLOAD_TEXT = {
 	[ALERT_DOWNLOAD_TYPE.danger]: i18n.translate(
-		'unable-to-download-key-please-try-again'
+		'unable-to-export-keys-please-try-again'
 	),
 	[ALERT_DOWNLOAD_TYPE.success]: i18n.translate(
 		'activation-key-was-downloaded-successfully'
