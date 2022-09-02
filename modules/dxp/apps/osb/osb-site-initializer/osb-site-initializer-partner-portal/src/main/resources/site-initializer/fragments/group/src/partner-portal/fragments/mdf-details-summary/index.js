9c0db6898c5eb49ac4703dc655776bdfae72248b
Bruno Santana <bruno.santana@liferay.com>
2022-09-02 18:51:21 -0300

    LPS-161295 Changed the date fragment to a Summary Fragment

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/partner-portal/fragments/mdf-details-summary/index.js
@@ -0,0 +1,65 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const currentPath = Liferay.currentURL.split('/');
+const mdfRequestId = +currentPath[currentPath.length - 1];
+
+const getMDFRequest = async () => {
+	// eslint-disable-next-line @liferay/portal/no-global-fetch
+	const response = await fetch(`/o/c/mdfrequests/${mdfRequestId}`, {
+		headers: {
+			'accept': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+	});
+
+	if (response.ok) {
+		const data = await response.json();
+
+		const startDate = new Intl.DateTimeFormat(
+			Liferay.ThemeDisplay.getBCP47LanguageId(),
+			{day: 'numeric', month: 'short'}
+		).format(new Date(data.minDateActivity));
+
+		const endDate = new Intl.DateTimeFormat(
+			Liferay.ThemeDisplay.getBCP47LanguageId(),
+			{day: 'numeric', month: 'short', year: 'numeric'}
+		).format(new Date(data.maxDateActivity));
+
+		const totalCost = new Intl.NumberFormat(
+			Liferay.ThemeDisplay.getBCP47LanguageId(),
+			{
+				currency: 'USD',
+				style: 'currency',
+			}
+		).format(data.totalCostOfExpense);
+
+		const requestedCost = new Intl.NumberFormat(
+			Liferay.ThemeDisplay.getBCP47LanguageId(),
+			{
+				currency: 'USD',
+				style: 'currency',
+			}
+		).format(data.totalMDFRequestAmount);
+
+		fragmentElement.querySelector(
+			'#dateField'
+		).innerHTML = `${startDate} - ${endDate}`;
+
+		fragmentElement.querySelector('#totalCost').innerHTML = `${totalCost}`;
+
+		fragmentElement.querySelector(
+			'#requestedCost'
+		).innerHTML = `${requestedCost}`;
+	}
+};
+
+getMDFRequest();
