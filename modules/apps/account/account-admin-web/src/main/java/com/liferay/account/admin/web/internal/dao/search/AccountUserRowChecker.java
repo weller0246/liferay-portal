fcd07cf1cb37aa1cdca86f54e2310d32568a08a5
drewbrokke <drew.brokke@liferay.com>
2022-09-02 11:46:02 -0500

    LPS-161019 account-admin-web: uses validator in the row checker

--- modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountUserRowChecker.java
+++ modules/apps/account/account-admin-web/src/main/java/com/liferay/account/admin/web/internal/dao/search/AccountUserRowChecker.java
@@ -15,20 +15,12 @@
 package com.liferay.account.admin.web.internal.dao.search;
 
 import com.liferay.account.admin.web.internal.display.AccountUserDisplay;
-import com.liferay.account.configuration.AccountEntryEmailDomainsConfiguration;
+import com.liferay.account.admin.web.internal.util.AccountEntryEmailValidatorFactoryUtil;
 import com.liferay.account.model.AccountEntry;
 import com.liferay.account.service.AccountEntryLocalServiceUtil;
 import com.liferay.account.service.AccountEntryUserRelLocalServiceUtil;
-import com.liferay.petra.string.CharPool;
+import com.liferay.account.validator.AccountEntryEmailValidator;
 import com.liferay.portal.kernel.dao.search.EmptyOnClickRowChecker;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.module.configuration.ConfigurationException;
-import com.liferay.portal.kernel.module.configuration.ConfigurationProviderUtil;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
-import com.liferay.portal.kernel.util.StringUtil;
 
 import javax.portlet.PortletResponse;
 
@@ -59,56 +51,26 @@ public class AccountUserRowChecker extends EmptyOnClickRowChecker {
 			return true;
 		}
 
+		AccountEntry accountEntry =
+			AccountEntryLocalServiceUtil.fetchAccountEntry(_accountEntryId);
+
+		if (accountEntry == null) {
+			return false;
+		}
+
+		AccountEntryEmailValidator accountEntryEmailValidator =
+			AccountEntryEmailValidatorFactoryUtil.create(
+				accountEntry.getCompanyId(), accountEntry.getDomainsArray());
 		AccountUserDisplay accountUserDisplay = (AccountUserDisplay)object;
 
-		User user = UserLocalServiceUtil.fetchUser(
-			accountUserDisplay.getUserId());
+		if (accountEntryEmailValidator.isValidDomain(
+				accountUserDisplay.getEmailAddress())) {
 
-		try {
-			AccountEntryEmailDomainsConfiguration
-				accountEntryEmailDomainsConfiguration =
-					ConfigurationProviderUtil.getCompanyConfiguration(
-						AccountEntryEmailDomainsConfiguration.class,
-						user.getCompanyId());
-
-			if (!accountEntryEmailDomainsConfiguration.
-					enableEmailDomainValidation()) {
-
-				return false;
-			}
-
-			AccountEntry accountEntry =
-				AccountEntryLocalServiceUtil.fetchAccountEntry(_accountEntryId);
-
-			if ((accountEntry == null) ||
-				ArrayUtil.isEmpty(accountEntry.getDomainsArray())) {
-
-				return false;
-			}
-
-			String emailAddress = accountUserDisplay.getEmailAddress();
-
-			emailAddress = StringUtil.toLowerCase(emailAddress);
-
-			int index = emailAddress.indexOf(CharPool.AT);
-
-			String domain = emailAddress.substring(index + 1);
-
-			if (!ArrayUtil.contains(accountEntry.getDomainsArray(), domain)) {
-				return true;
-			}
-		}
-		catch (ConfigurationException configurationException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(configurationException);
-			}
+			return false;
 		}
 
-		return false;
+		return true;
 	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		AccountUserRowChecker.class);
 
 	private final long _accountEntryId;
 
