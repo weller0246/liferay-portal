13737db40121454defb8f54720ebb638e889bfce
Janis Zhang <janis.zhang@liferay.com>
2022-09-02 13:55:55 +0800

    LPS-162150 Convert method references to field references in DDMFormAssetRendererFactory

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/asset/model/DDMFormAssetRendererFactory.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/asset/model/DDMFormAssetRendererFactory.java
@@ -126,14 +126,6 @@ public class DDMFormAssetRendererFactory
 			permissionChecker, ddmFormInstance, actionId);
 	}
 
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.dynamic.data.mapping.form.web)",
-		unbind = "-"
-	)
-	public void setServletContext(ServletContext servletContext) {
-		_servletContext = servletContext;
-	}
-
 	private AssetRenderer<DDMFormInstanceRecord> _createAssetRenderer(
 		DDMFormInstanceRecord formInstanceRecord,
 		DDMFormInstanceRecordVersion formInstanceRecordVersion, int type) {
@@ -187,6 +179,9 @@ public class DDMFormAssetRendererFactory
 	@Reference
 	private Portal _portal;
 
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.dynamic.data.mapping.form.web)"
+	)
 	private ServletContext _servletContext;
 
 }
