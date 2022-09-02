fd1f4180d99b212d9911130276c9c7e150f6b448
Alan Huang <alan.huang@liferay.com>
2022-09-03 02:29:42 +0800

    LPS-162237 Always log

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/UnprocessedExceptionCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/checkstyle/check/UnprocessedExceptionCheck.java
@@ -14,30 +14,12 @@
 
 package com.liferay.source.formatter.checkstyle.check;
 
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.io.unsync.UnsyncStringReader;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.source.formatter.check.util.JavaSourceUtil;
-import com.liferay.source.formatter.util.ThreadSafeSortedClassLibraryBuilder;
-
 import com.puppycrawl.tools.checkstyle.api.DetailAST;
-import com.puppycrawl.tools.checkstyle.api.FileContents;
-import com.puppycrawl.tools.checkstyle.api.FileText;
 import com.puppycrawl.tools.checkstyle.api.FullIdent;
 import com.puppycrawl.tools.checkstyle.api.TokenTypes;
 
-import com.thoughtworks.qdox.JavaProjectBuilder;
-import com.thoughtworks.qdox.model.JavaClass;
-import com.thoughtworks.qdox.model.JavaSource;
-import com.thoughtworks.qdox.parser.ParseException;
-
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
 
 /**
  * @author Hugo Huijser
@@ -60,30 +42,14 @@ public class UnprocessedExceptionCheck extends BaseCheck {
 		}
 
 		if (detailAST.getType() == TokenTypes.LITERAL_CATCH) {
-			FileContents fileContents = getFileContents();
-
-			FileText fileText = fileContents.getText();
-
-			_checkUnprocessedException(
-				detailAST, (String)fileText.getFullText());
+			_checkUnprocessedException(detailAST);
 		}
 		else {
 			_checkUnprocessedThrownException(detailAST);
 		}
 	}
 
-	private void _checkJSONException(DetailAST detailAST) {
-		List<DetailAST> methodCallDetailASTList = getMethodCalls(
-			detailAST, "_log", _LOG_METHOD_NAMES);
-
-		if (methodCallDetailASTList.isEmpty()) {
-			log(detailAST, _MSG_MISSING_DEBUG_LOG_JSON_EXCEPTION);
-		}
-	}
-
-	private void _checkUnprocessedException(
-		DetailAST detailAST, String content) {
-
+	private void _checkUnprocessedException(DetailAST detailAST) {
 		DetailAST parameterDefinitionDetailAST = detailAST.findFirstToken(
 			TokenTypes.PARAMETER_DEF);
 
@@ -99,77 +65,9 @@ public class UnprocessedExceptionCheck extends BaseCheck {
 		String exceptionClassName = _getExceptionClassName(
 			parameterDefinitionDetailAST);
 
-		if (exceptionClassName == null) {
-			return;
-		}
-		else if (exceptionClassName.equals("JSONException")) {
-			_checkJSONException(detailAST);
-
-			return;
-		}
-
-		JavaProjectBuilder javaProjectBuilder = _getJavaProjectBuilder(content);
-
-		if (javaProjectBuilder == null) {
-			return;
-		}
-
-		String originalExceptionClassName = exceptionClassName;
-
-		if (!exceptionClassName.contains(StringPool.PERIOD)) {
-			for (String importedExceptionClassName :
-					_getImportedExceptionClassNames(javaProjectBuilder)) {
-
-				if (importedExceptionClassName.endsWith(
-						StringPool.PERIOD + exceptionClassName)) {
-
-					exceptionClassName = importedExceptionClassName;
-
-					break;
-				}
-			}
-		}
-
-		if (!exceptionClassName.contains(StringPool.PERIOD)) {
-			exceptionClassName =
-				JavaSourceUtil.getPackageName(content) + StringPool.PERIOD +
-					exceptionClassName;
-		}
-
-		JavaClass exceptionClass = javaProjectBuilder.getClassByName(
+		log(
+			parameterDefinitionDetailAST, _MSG_UNPROCESSED_EXCEPTION,
 			exceptionClassName);
-
-		if (exceptionClass == null) {
-			return;
-		}
-
-		while (true) {
-			String packageName = exceptionClass.getPackageName();
-
-			if (!packageName.contains("com.liferay")) {
-				break;
-			}
-
-			exceptionClassName = exceptionClass.getName();
-
-			if (exceptionClassName.equals("PortalException") ||
-				exceptionClassName.equals("SystemException")) {
-
-				log(
-					parameterDefinitionDetailAST, _MSG_UNPROCESSED_EXCEPTION,
-					originalExceptionClassName);
-
-				break;
-			}
-
-			JavaClass exceptionSuperClass = exceptionClass.getSuperJavaClass();
-
-			if (exceptionSuperClass == null) {
-				break;
-			}
-
-			exceptionClass = exceptionSuperClass;
-		}
 	}
 
 	private void _checkUnprocessedThrownException(DetailAST detailAST) {
@@ -246,44 +144,6 @@ public class UnprocessedExceptionCheck extends BaseCheck {
 		return typeFullIdent.getText();
 	}
 
-	private Set<String> _getImportedExceptionClassNames(
-		JavaProjectBuilder javaProjectBuilder) {
-
-		Set<String> exceptionClassNames = new HashSet<>();
-
-		Collection<JavaSource> sources = javaProjectBuilder.getSources();
-
-		Iterator<JavaSource> iterator = sources.iterator();
-
-		JavaSource javaSource = iterator.next();
-
-		for (String importClassName : javaSource.getImports()) {
-			if (importClassName.endsWith("Exception")) {
-				exceptionClassNames.add(importClassName);
-			}
-		}
-
-		return exceptionClassNames;
-	}
-
-	private JavaProjectBuilder _getJavaProjectBuilder(String content) {
-		JavaProjectBuilder javaProjectBuilder = new JavaProjectBuilder(
-			new ThreadSafeSortedClassLibraryBuilder());
-
-		try {
-			javaProjectBuilder.addSource(new UnsyncStringReader(content));
-		}
-		catch (ParseException parseException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(parseException);
-			}
-
-			return null;
-		}
-
-		return javaProjectBuilder;
-	}
-
 	private String _getName(DetailAST detailAST) {
 		String name = getName(detailAST);
 
@@ -300,17 +160,7 @@ public class UnprocessedExceptionCheck extends BaseCheck {
 		return null;
 	}
 
-	private static final String[] _LOG_METHOD_NAMES = {
-		"debug", "error", "info", "trace", "warn"
-	};
-
-	private static final String _MSG_MISSING_DEBUG_LOG_JSON_EXCEPTION =
-		"missing.debug.log.json.exception";
-
 	private static final String _MSG_UNPROCESSED_EXCEPTION =
 		"exception.unprocessed";
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		UnprocessedExceptionCheck.class);
 
 }
