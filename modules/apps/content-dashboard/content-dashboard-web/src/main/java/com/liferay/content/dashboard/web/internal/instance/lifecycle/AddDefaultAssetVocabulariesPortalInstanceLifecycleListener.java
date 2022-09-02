885f1d369ccca7d9d3c1d0334b6e6cc435953ef7
Victor Ware <victor.ware@liferay.com>
2022-09-02 09:00:36 -0700

    Revert "LPS-161429 Remove stream usage in AddDefaultAssetVocabulariesPortalInstanceLifecycleListener"
    This reverts commit 442883e0c590cb4a078cea829f805f9be85630f1.

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/instance/lifecycle/AddDefaultAssetVocabulariesPortalInstanceLifecycleListener.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/instance/lifecycle/AddDefaultAssetVocabulariesPortalInstanceLifecycleListener.java
@@ -37,10 +37,11 @@ import com.liferay.portlet.asset.util.AssetVocabularySettingsHelper;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -58,14 +59,18 @@ public class AddDefaultAssetVocabulariesPortalInstanceLifecycleListener
 			company, PropsValues.ASSET_VOCABULARY_DEFAULT,
 			AssetVocabularyConstants.VISIBILITY_TYPE_PUBLIC);
 
-		Collection<String> classNames =
-			_contentDashboardItemFactoryTracker.getClassNames();
+		Collection<Long> classNameIds =
+			_contentDashboardItemFactoryTracker.getClassNameIds();
 
-		Set<Long> searchClassNameIds = new HashSet<>();
+		Stream<Long> stream = classNameIds.stream();
 
-		for (String className : classNames) {
-			searchClassNameIds.add(_portal.getClassNameId(className));
-		}
+		Set<Long> searchClassNameIds = stream.map(
+			classNameId -> _portal.getClassNameId(
+				_infoSearchClassMapperTracker.getSearchClassName(
+					_portal.getClassName(classNameId)))
+		).collect(
+			Collectors.toSet()
+		);
 
 		for (ContentDashboardConstants.DefaultInternalAssetVocabularyName
 				defaultInternalAssetVocabularyName :
