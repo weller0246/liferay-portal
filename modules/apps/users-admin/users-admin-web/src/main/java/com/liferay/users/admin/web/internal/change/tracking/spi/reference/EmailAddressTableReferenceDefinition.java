597cb0bd85166c5513796b34d8db220e3c31279f
tamas.molnar <tamas.molnar@liferay.com>
2022-09-02 13:13:06 +0200

    LPS-161618 Create EmailAddressTableReferenceDefinition

---
+++ modules/apps/users-admin/users-admin-web/src/main/java/com/liferay/users/admin/web/internal/change/tracking/spi/reference/EmailAddressTableReferenceDefinition.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.users.admin.web.internal.change.tracking.spi.reference;
+
+import com.liferay.change.tracking.spi.reference.TableReferenceDefinition;
+import com.liferay.change.tracking.spi.reference.builder.ChildTableReferenceInfoBuilder;
+import com.liferay.change.tracking.spi.reference.builder.ParentTableReferenceInfoBuilder;
+import com.liferay.portal.kernel.model.EmailAddressTable;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationTable;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.model.UserTable;
+import com.liferay.portal.kernel.service.persistence.BasePersistence;
+import com.liferay.portal.kernel.service.persistence.EmailAddressPersistence;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Tamas Molnar
+ */
+@Component(service = TableReferenceDefinition.class)
+public class EmailAddressTableReferenceDefinition
+	implements TableReferenceDefinition<EmailAddressTable> {
+
+	@Override
+	public void defineChildTableReferences(
+		ChildTableReferenceInfoBuilder<EmailAddressTable>
+			childTableReferenceInfoBuilder) {
+	}
+
+	@Override
+	public void defineParentTableReferences(
+		ParentTableReferenceInfoBuilder<EmailAddressTable>
+			parentTableReferenceInfoBuilder) {
+
+		parentTableReferenceInfoBuilder.classNameReference(
+			EmailAddressTable.INSTANCE.classPK,
+			OrganizationTable.INSTANCE.organizationId, Organization.class
+		).classNameReference(
+			EmailAddressTable.INSTANCE.classPK, UserTable.INSTANCE.userId,
+			User.class
+		);
+	}
+
+	@Override
+	public BasePersistence<?> getBasePersistence() {
+		return _emailAddressPersistence;
+	}
+
+	@Override
+	public EmailAddressTable getTable() {
+		return EmailAddressTable.INSTANCE;
+	}
+
+	@Reference
+	private EmailAddressPersistence _emailAddressPersistence;
+
+}
