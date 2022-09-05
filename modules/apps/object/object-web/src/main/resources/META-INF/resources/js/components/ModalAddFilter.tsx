a63132214b67dc0b9149d1aac5b2e40399536e72
Igor Franca <igor.franca@liferay.com>
2022-09-05 15:24:28 -0300

    LPS-152650 Removing unnecessary filter from ModalAddFilter

--- modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModalAddFilter.tsx
+++ modules/apps/object/object-web/src/main/resources/META-INF/resources/js/components/ModalAddFilter.tsx
@@ -69,21 +69,7 @@ export function ModalAddFilter({
 	const [filterEndDate, setFilterEndDate] = useState('');
 
 	const filteredAvailableFields = useMemo(() => {
-		const availableFields = objectFields.filter(
-			(objectField: ObjectFieldView) => {
-				if (
-					(objectField.businessType === 'Picklist' &&
-						!objectField.hasFilter) ||
-					objectField.name === 'createDate' ||
-					objectField.name === 'modifiedDate' ||
-					(objectField.name === 'status' && !objectField.hasFilter)
-				) {
-					return objectField;
-				}
-			}
-		);
-
-		return availableFields.filter(({label}: ObjectField) =>
+		return objectFields.filter(({label}: ObjectField) =>
 			stringIncludesQuery(label[defaultLanguageId] as string, query)
 		);
 	}, [objectFields, query]);
