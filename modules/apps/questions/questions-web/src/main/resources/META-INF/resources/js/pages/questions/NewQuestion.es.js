597215faf82745340c8e04b672f3c006b8f47dc9
Claudionilton <claudionilton.junior@liferay.com>
2022-09-05 11:53:48 -0300

    LPS-161837 SF

--- modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/NewQuestion.es.js
+++ modules/apps/questions/questions-web/src/main/resources/META-INF/resources/js/pages/questions/NewQuestion.es.js
@@ -100,7 +100,8 @@ export default withRouter(
 							.messageBoardSections.items,
 						...section.messageBoardSections.items,
 					]);
-				} else {
+				}
+				else {
 					setSections([
 						{
 							id: section.id,
@@ -154,7 +155,8 @@ export default withRouter(
 				})
 					.then(({error}) => processResponse(error))
 					.catch(processError);
-			} else {
+			}
+			else {
 				createQuestionInASection({
 					fetchOptionsOverrides: getContextLink(sectionTitle),
 					variables: {
