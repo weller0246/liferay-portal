014ec070b837f6fb96dcae3d9710dd23df17d5f9
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-09-06 16:07:42 +0200

    LPS-162222 Add the company to the request, it's needed for runtime tag

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/search/spi/model/index/contributor/LayoutModelDocumentContributor.java
@@ -420,6 +420,8 @@ public class LayoutModelDocumentContributor
 			httpServletRequest.setAttribute(
 				WebKeys.THEME_DISPLAY, themeDisplay);
 
+			httpServletRequest.setAttribute(
+				WebKeys.COMPANY_ID, Long.valueOf(_layout.getCompanyId()));
 			httpServletRequest.setAttribute(WebKeys.LAYOUT, _layout);
 			httpServletRequest.setAttribute(WebKeys.USER, _user);
 			httpServletRequest.setAttribute(WebKeys.USER_ID, _user.getUserId());
