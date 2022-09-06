e9274056b37d1e30fc57c6ac20766a88c1eba00f
Alan Huang <alan.huang@liferay.com>
2022-09-06 23:02:30 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/dispatch/dispatch-web/src/main/java/com/liferay/dispatch/web/internal/portlet/DispatchPortlet.java
+++ modules/apps/dispatch/dispatch-web/src/main/java/com/liferay/dispatch/web/internal/portlet/DispatchPortlet.java
@@ -57,7 +57,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view_dispatch_trigger.jsp",
 		"javax.portlet.name=" + DispatchPortletKeys.DISPATCH,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
