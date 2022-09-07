64c808f62edd259fae315ffb7e00e4ec46b02f3a
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2022-09-07 20:17:35 +0200

    LPS-162529 Take into account the custom value when the style sheet is generated

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/generateStyleSheet.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/utils/generateStyleSheet.js
@@ -115,7 +115,9 @@ function getValue(itemId, styleName, styleValue) {
 	// the CSS variable --spacer-x which is used by the mx-x and px-x clay classes
 
 	if (SPACING_OPTIONS.includes(styleName)) {
-		return `var(--spacer-${styleValue}, ${DEFAULT_SPACING_VALUES[styleValue]}rem)`;
+		return isNaN(styleValue)
+			? styleValue
+			: `var(--spacer-${styleValue}, ${DEFAULT_SPACING_VALUES[styleValue]}rem)`;
 	}
 
 	// Instead of trying to calculate the backgroundImage here, we rely on the item
