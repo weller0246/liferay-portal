26fff9bad85d67e27c86383e5c705a70c276fc25
Rubén Pulido <ruben.pulido@liferay.com>
2022-09-08 10:34:55 +0200

    LPS-161742 Prefix value with its position before storing and take into account the position saved when returning the sorted keys
    This is necessary because PortalPreferencesImpl converts the arrays of old and new values to sets, and returns if both sets are equals.
    
    Set<String> valuesSet = SetUtil.fromArray(values);
    Set<String> oldValuesSet = SetUtil.fromArray(oldValues);
    
    if (valuesSet.equals(oldValuesSet)) {
    	return;
    }
    
    Therefore we cannot persist the order implicitly and have to do it explicitly.

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/PortalPreferencesUtil.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/util/PortalPreferencesUtil.java
@@ -14,10 +14,13 @@
 
 package com.liferay.layout.content.page.editor.web.internal.util;
 
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.portlet.PortalPreferences;
 import com.liferay.portal.kernel.util.ListUtil;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author Rubén Pulido
@@ -28,14 +31,47 @@ public class PortalPreferencesUtil {
 		PortalPreferences portalPreferences, String namespace, String key) {
 
 		return ListUtil.fromArray(
-			portalPreferences.getValues(namespace, key, new String[0]));
+			_removeNumberPrefix(
+				portalPreferences.getValues(namespace, key, new String[0])));
 	}
 
 	public static void updateSortedPortalPreferencesValues(
 		PortalPreferences portalPreferences, String namespace, String key,
-		String[] values) {
+		String[] sortedValues) {
 
-		portalPreferences.setValues(namespace, key, values);
+		portalPreferences.setValues(
+			namespace, key, _addNumberPrefix(sortedValues));
+	}
+
+	private static String[] _addNumberPrefix(String[] sortedValues) {
+		String[] numberedSortedValues = new String[sortedValues.length];
+
+		for (int i = 0; i < sortedValues.length; i++) {
+			numberedSortedValues[i] =
+				i + StringPool.DOUBLE_DOLLAR + sortedValues[i];
+		}
+
+		return numberedSortedValues;
+	}
+
+	private static String[] _removeNumberPrefix(String[] numberedValues) {
+		String[] sortedValues = new String[numberedValues.length];
+
+		Map<String, String> numberValueMap = new HashMap<>();
+
+		for (String numberedValue : numberedValues) {
+			numberValueMap.put(
+				numberedValue.substring(
+					0, numberedValue.indexOf(StringPool.DOUBLE_DOLLAR)),
+				numberedValue.substring(
+					numberedValue.indexOf(StringPool.DOUBLE_DOLLAR) + 2));
+		}
+
+		for (int i = 0; i < numberedValues.length; i++) {
+			sortedValues[i] = numberValueMap.get(String.valueOf(i));
+		}
+
+		return sortedValues;
 	}
 
 }
