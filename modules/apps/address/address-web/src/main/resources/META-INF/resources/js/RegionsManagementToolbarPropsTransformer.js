0c99957091939ed1325725211da8144433659494
Rafael Cardoso <rafael.cardoso@liferay.com>
2022-09-09 14:49:01 -0300

    LPS-160386 (address-web): Regions actions

--- modules/apps/address/address-web/src/main/resources/META-INF/resources/js/RegionsManagementToolbarPropsTransformer.js
+++ modules/apps/address/address-web/src/main/resources/META-INF/resources/js/RegionsManagementToolbarPropsTransformer.js
@@ -15,6 +15,46 @@
 import {getCheckedCheckboxes, postForm} from 'frontend-js-web';
 
 export default function propsTransformer({portletNamespace, ...otherProps}) {
+	const activateRegions = (itemData) => {
+		const form = document.getElementById(`${portletNamespace}fm`);
+
+		if (form) {
+			postForm(form, {
+				data: {
+					regionIds: getCheckedCheckboxes(
+						form,
+						`${portletNamespace}allRowIds`
+					),
+				},
+				url: itemData?.activateRegionsURL,
+			});
+		}
+	};
+
+	const deactivateRegions = (itemData) => {
+		if (
+			confirm(
+				Liferay.Language.get(
+					'are-you-sure-you-want-to-deactivate-the-selected-regions'
+				)
+			)
+		) {
+			const form = document.getElementById(`${portletNamespace}fm`);
+
+			if (form) {
+				postForm(form, {
+					data: {
+						regionIds: getCheckedCheckboxes(
+							form,
+							`${portletNamespace}allRowIds`
+						),
+					},
+					url: itemData?.deactivateRegionsURL,
+				});
+			}
+		}
+	};
+
 	const deleteRegions = (itemData) => {
 		if (
 			confirm(
@@ -46,7 +86,13 @@ export default function propsTransformer({portletNamespace, ...otherProps}) {
 
 			const action = data?.action;
 
-			if (action === 'deleteRegions') {
+			if (action === 'activateRegions') {
+				activateRegions(data);
+			}
+			else if (action === 'deactivateRegions') {
+				deactivateRegions(data);
+			}
+			else if (action === 'deleteRegions') {
 				deleteRegions(data);
 			}
 		},
