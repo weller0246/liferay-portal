dd7265c23e603fc93e3d8ce8663c2e4d888e121e
Brian Chan <brian.chan@liferay.com>
2022-09-09 08:49:52 -0300

    LPS-161944 Sort

--- modules/apps/portal/portal-events-test/src/testIntegration/java/com/liferay/portal/events/test/ServicePreActionTest.java
+++ modules/apps/portal/portal-events-test/src/testIntegration/java/com/liferay/portal/events/test/ServicePreActionTest.java
@@ -103,13 +103,11 @@ public class ServicePreActionTest {
 		LayoutTestUtil.addTypePortletLayout(
 			_group.getGroupId(), "Page not visible", false, null, false, true);
 
-		_mockHttpServletRequest.setRequestURI(
-			_portal.getPathMain() + "/portal/login");
-
 		_mockHttpServletRequest.setAttribute(WebKeys.COMPANY, _company);
-
 		_mockHttpServletRequest.setAttribute(
 			WebKeys.VIRTUAL_HOST_LAYOUT_SET, _group.getPublicLayoutSet());
+		_mockHttpServletRequest.setRequestURI(
+			_portal.getPathMain() + "/portal/login");
 	}
 
 	@Test
