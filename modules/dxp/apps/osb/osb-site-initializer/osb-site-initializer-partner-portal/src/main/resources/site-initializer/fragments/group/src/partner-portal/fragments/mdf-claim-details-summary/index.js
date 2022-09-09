6633b2f4e1a72858cbf67f14c6bae78e08e95552
Bruno Santana <bruno.santana@liferay.com>
2022-09-09 17:37:58 -0300

    LPS-162344 Create the mdf claim summary

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/partner-portal/fragments/mdf-claim-details-summary/index.js
@@ -0,0 +1,62 @@
+/* eslint-disable no-undef */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const currentPath = Liferay.currentURL.split('/');
+const mdfClaimId = +currentPath[currentPath.length - 1];
+
+const updateMDFClaimSummary = async () => {
+	// eslint-disable-next-line @liferay/portal/no-global-fetch
+	const response = await fetch(`/o/c/mdfclaims/${mdfClaimId}`, {
+		headers: {
+			'accept': 'application/json',
+			'x-csrf-token': Liferay.authToken,
+		},
+	});
+
+	if (response.ok) {
+	const data = await response.json();
+
+		const type = data.type;
+
+	const amountClaimed = formatCurrency(data.amountClaimed);
+
+		const paymentReceived = formatCurrency(data.paymentReceived);
+
+		const check = formatCurrency(data.check);
+
+		fragmentElement.querySelector('#ClaimType').innerHTML = `${type}`;
+
+		fragmentElement.querySelector(
+			'#ClaimAmountClaimed'
+		).innerHTML = `${amountClaimed}`;
+
+		fragmentElement.querySelector(
+			'#ClaimPaymentReceived'
+		).innerHTML = `${paymentReceived}`;
+		fragmentElement.querySelector('#ClaimCheck').innerHTML = `${check}`;
+
+		return;
+	}
+
+	Liferay.Util.openToast({
+		message: 'An unexpected error occured.',
+		type: 'danger',
+	});
+};
+
+const formatCurrency = (value) =>
+	new Intl.NumberFormat(Liferay.ThemeDisplay.getBCP47LanguageId(), {
+		currency: 'USD',
+		style: 'currency',
+	}).format(value);
+
+updateMDFClaimSummary();
