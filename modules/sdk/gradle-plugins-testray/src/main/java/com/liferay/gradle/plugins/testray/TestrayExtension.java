b3021fa8824106178b290273bc02f862c02dfe70
Michael Hashimoto <michael.hashimoto@liferay.com>
2022-09-10 08:11:03 -0700

    LRCI-3230 Update com.liferay.jenkins.results.parser to 1.0.1118 in gradle-plugins-testray

--- modules/sdk/gradle-plugins-testray/src/main/java/com/liferay/gradle/plugins/testray/TestrayExtension.java
+++ modules/sdk/gradle-plugins-testray/src/main/java/com/liferay/gradle/plugins/testray/TestrayExtension.java
@@ -47,7 +47,7 @@ public class TestrayExtension {
 		_testrayPropertiesFile = testrayPropertiesFile;
 	}
 
-	private Object _jenkinsResultsParserVersion = "1.0.1105";
+	private Object _jenkinsResultsParserVersion = "1.0.1118";
 	private final Project _project;
 	private Object _testrayPropertiesFile = "testray.properties";
 
