712f2a095f7fd76889f183df5127315c2887ce61
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-09-12 16:21:25 +0200

    LPS-162527 Removes deprecated logic

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/persistence/impl/FragmentEntryLinkFinderImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/persistence/impl/FragmentEntryLinkFinderImpl.java
@@ -42,14 +42,8 @@ public class FragmentEntryLinkFinderImpl
 	public static final String COUNT_BY_G_F =
 		FragmentEntryLinkFinder.class.getName() + ".countByG_F";
 
-	public static final String COUNT_BY_G_F_C =
-		FragmentEntryLinkFinder.class.getName() + ".countByG_F_C";
-
 	public static final String COUNT_BY_G_F_P =
 		FragmentEntryLinkFinder.class.getName() + ".countByG_F_P";
-
-	public static final String COUNT_BY_G_F_C_L =
-		FragmentEntryLinkFinder.class.getName() + ".countByG_F_C_L";
 
 	public static final String COUNT_BY_G_F_P_L =
 		FragmentEntryLinkFinder.class.getName() + ".countByG_F_P_L";
@@ -60,14 +54,8 @@ public class FragmentEntryLinkFinderImpl
 	public static final String FIND_BY_G_F =
 		FragmentEntryLinkFinder.class.getName() + ".findByG_F";
 
-	public static final String FIND_BY_G_F_C =
-		FragmentEntryLinkFinder.class.getName() + ".findByG_F_C";
-
 	public static final String FIND_BY_G_F_P =
 		FragmentEntryLinkFinder.class.getName() + ".findByG_F_P";
-
-	public static final String FIND_BY_G_F_C_L =
-		FragmentEntryLinkFinder.class.getName() + ".findByG_F_C_L";
 
 	public static final String FIND_BY_G_F_P_L =
 		FragmentEntryLinkFinder.class.getName() + ".findByG_F_P_L";
@@ -108,32 +96,6 @@ public class FragmentEntryLinkFinderImpl
 		finally {
 			closeSession(session);
 		}
-	}
-
-	/**
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 *             #countByG_F_P_L(long, long, int)}
-	 */
-	@Deprecated
-	@Override
-	public int countByG_F_C(
-		long groupId, long fragmentEntryId, long classNameId) {
-
-		return countByG_F_P_L(groupId, fragmentEntryId, -1);
-	}
-
-	/**
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 *             #countByG_F_P_L(long, long, int)}
-	 */
-	@Deprecated
-	@Override
-	public int countByG_F_C_L(
-		long groupId, long fragmentEntryId, long classNameId,
-		int layoutPageTemplateEntryType) {
-
-		return countByG_F_P_L(
-			groupId, fragmentEntryId, layoutPageTemplateEntryType);
 	}
 
 	@Override
@@ -254,36 +216,6 @@ public class FragmentEntryLinkFinderImpl
 		finally {
 			closeSession(session);
 		}
-	}
-
-	/**
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 *             #findByG_F_P_L(long, long, int, int, int, OrderByComparator)}
-	 */
-	@Deprecated
-	@Override
-	public List<FragmentEntryLink> findByG_F_C(
-		long groupId, long fragmentEntryId, long classNameId, int start,
-		int end, OrderByComparator<FragmentEntryLink> orderByComparator) {
-
-		return findByG_F_P_L(
-			groupId, fragmentEntryId, -1, start, end, orderByComparator);
-	}
-
-	/**
-	 * @deprecated As of Athanasius (7.3.x), replaced by {@link
-	 *             #findByG_F_P_L(long, long, int, int, int, OrderByComparator)}
-	 */
-	@Deprecated
-	@Override
-	public List<FragmentEntryLink> findByG_F_C_L(
-		long groupId, long fragmentEntryId, long classNameId,
-		int layoutPageTemplateEntryType, int start, int end,
-		OrderByComparator<FragmentEntryLink> orderByComparator) {
-
-		return findByG_F_P_L(
-			groupId, fragmentEntryId, layoutPageTemplateEntryType, start, end,
-			orderByComparator);
 	}
 
 	@Override
