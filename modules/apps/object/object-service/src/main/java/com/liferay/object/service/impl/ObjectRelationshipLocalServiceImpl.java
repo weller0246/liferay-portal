9d1d76e0c70c4ec7d235f037b87e9bb7e33f090c
Murilo Stodolni <murilo.stodolni@liferay.com>
2022-09-13 16:45:02 -0300

    LPS-161819 Disable mandatory ObjectField when changed relationship to disassociate

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectRelationshipLocalServiceImpl.java
@@ -59,6 +59,7 @@ import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
 import java.io.Serializable;
@@ -463,6 +464,15 @@ public class ObjectRelationshipLocalServiceImpl
 					ObjectRelationship.class);
 
 			indexer.reindex(reverseObjectRelationship);
+		}
+
+		if ((objectRelationship.getObjectFieldId2() != 0) &&
+			StringUtil.equals(
+				deletionType,
+				ObjectRelationshipConstants.DELETION_TYPE_DISASSOCIATE)) {
+
+			_objectFieldLocalService.updateRequired(
+				objectRelationship.getObjectFieldId2(), false);
 		}
 
 		return _updateObjectRelationship(
