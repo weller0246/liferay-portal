12c8673921b8843a1ce2a1b28df0324ec369c1ac
Lily.Chi <lily.chi@liferay.com>
2022-09-13 16:05:49 +0800

    LPS-162756 Convert method reference to field reference in HelloVelocityPortlet

--- modules/apps/hello-velocity/hello-velocity-web/src/main/java/com/liferay/hello/velocity/web/internal/portlet/HelloVelocityPortlet.java
+++ modules/apps/hello-velocity/hello-velocity-web/src/main/java/com/liferay/hello/velocity/web/internal/portlet/HelloVelocityPortlet.java
@@ -83,13 +83,6 @@ public class HelloVelocityPortlet extends VelocityPortlet {
 		mergeTemplate(templateId, template, portletRequest, portletResponse);
 	}
 
-	@Reference(
-		target = "(&(release.bundle.symbolic.name=com.liferay.hello.velocity.web)(&(release.schema.version>=1.0.0)(!(release.schema.version>=2.0.0))))",
-		unbind = "-"
-	)
-	protected void setRelease(Release release) {
-	}
-
 	private TemplateResource _getTemplateResource(String templateId) {
 		if (templateId.indexOf(StringPool.SLASH) != 0) {
 			templateId = StringPool.SLASH.concat(templateId);
@@ -114,5 +107,10 @@ public class HelloVelocityPortlet extends VelocityPortlet {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		HelloVelocityPortlet.class);
+
+	@Reference(
+		target = "(&(release.bundle.symbolic.name=com.liferay.hello.velocity.web)(&(release.schema.version>=1.0.0)(!(release.schema.version>=2.0.0))))"
+	)
+	private Release _release;
 
 }
