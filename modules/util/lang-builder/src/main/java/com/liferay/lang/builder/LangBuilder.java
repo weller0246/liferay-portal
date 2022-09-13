3e6a2b22f79872a4acbc0a8dcd4fbd0dc0a6a724
Brian Wulbern <brian.wulbern@liferay.com>
2022-09-13 22:00:20 +0800

    LPS-158690 Add support for three varieties of Spanish

--- modules/util/lang-builder/src/main/java/com/liferay/lang/builder/LangBuilder.java
+++ modules/util/lang-builder/src/main/java/com/liferay/lang/builder/LangBuilder.java
@@ -191,7 +191,10 @@ public class LangBuilder {
 		_createProperties(content, "sr_RS_latin"); // Serbian (Latin)
 		_createProperties(content, "sk"); // Slovak
 		_createProperties(content, "sl"); // Slovene
+		_createProperties(content, "es_AR"); // Spanish (Argentina)
+		_createProperties(content, "es_CO"); // Spanish (Columbia)
 		_createProperties(content, "es"); // Spanish
+		_createProperties(content, "es_MX"); // Spanish (Mexico)
 		_createProperties(content, "sv"); // Swedish
 		_createProperties(content, "ta_IN"); // Tamil
 		_createProperties(content, "th"); // Thai
