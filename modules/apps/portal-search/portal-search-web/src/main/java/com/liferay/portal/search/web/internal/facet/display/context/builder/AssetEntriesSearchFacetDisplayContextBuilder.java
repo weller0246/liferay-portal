512167c35ad7ce2688cea07c9f1d347e68eb9b6a
Brian Chan <brian.chan@liferay.com>
2022-09-13 21:39:51 -0300

    LPS-161791 SF

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/builder/AssetEntriesSearchFacetDisplayContextBuilder.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/builder/AssetEntriesSearchFacetDisplayContextBuilder.java
@@ -267,33 +267,34 @@ public class AssetEntriesSearchFacetDisplayContextBuilder
 	protected Map<String, String> getTypeNames() {
 		Map<String, String> assetTypesTypeNames = new HashMap<>();
 
-		if (_classNames != null) {
-			for (String className : _classNames) {
-				AssetRendererFactory<?> assetRendererFactory =
-					AssetRendererFactoryRegistryUtil.
-						getAssetRendererFactoryByClassName(className);
+		if (_classNames == null) {
+			return assetTypesTypeNames;
+		}
 
-				String typeName = className;
+		for (String className : _classNames) {
+			String typeName = className;
 
-				if (assetRendererFactory != null) {
-					typeName = assetRendererFactory.getTypeName(
-						_themeDisplay.getLocale());
-				}
-				else if (className.startsWith(
-							ObjectDefinition.class.getName() + "#")) {
+			AssetRendererFactory<?> assetRendererFactory =
+				AssetRendererFactoryRegistryUtil.
+					getAssetRendererFactoryByClassName(className);
 
-					String[] parts = StringUtil.split(className, "#");
-
-					ObjectDefinition objectDefinition =
-						ObjectDefinitionLocalServiceUtil.fetchObjectDefinition(
-							Long.valueOf(parts[1]));
-
-					typeName = objectDefinition.getLabel(
-						_themeDisplay.getLocale());
-				}
-
-				assetTypesTypeNames.put(className, typeName);
+			if (assetRendererFactory != null) {
+				typeName = assetRendererFactory.getTypeName(
+					_themeDisplay.getLocale());
 			}
+			else if (className.startsWith(
+						ObjectDefinition.class.getName() + "#")) {
+
+				String[] parts = StringUtil.split(className, "#");
+
+				ObjectDefinition objectDefinition =
+					ObjectDefinitionLocalServiceUtil.fetchObjectDefinition(
+						Long.valueOf(parts[1]));
+
+				typeName = objectDefinition.getLabel(_themeDisplay.getLocale());
+			}
+
+			assetTypesTypeNames.put(className, typeName);
 		}
 
 		return assetTypesTypeNames;
