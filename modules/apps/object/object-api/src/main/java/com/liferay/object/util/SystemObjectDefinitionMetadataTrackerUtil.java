fbdee0a021d5f7416e351645e82874afbdd1e762
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2022-09-14 11:53:22 -0300

    LPS-162853 Create SystemObjectDefinitionMetadataTrackerUtil

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/util/SystemObjectDefinitionMetadataTrackerUtil.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.util;
+
+import com.liferay.object.system.SystemObjectDefinitionMetadata;
+import com.liferay.object.system.SystemObjectDefinitionMetadataTracker;
+import com.liferay.osgi.util.ServiceTrackerFactory;
+
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Paulo Albuquerque
+ */
+public class SystemObjectDefinitionMetadataTrackerUtil {
+
+	public static SystemObjectDefinitionMetadata
+		getSystemObjectDefinitionMetadata(String name) {
+
+		SystemObjectDefinitionMetadataTracker
+			systemObjectDefinitionMetadataTracker =
+				_serviceTracker.getService();
+
+		return systemObjectDefinitionMetadataTracker.
+			getSystemObjectDefinitionMetadata(name);
+	}
+
+	private static final ServiceTracker
+		<SystemObjectDefinitionMetadataTracker,
+		 SystemObjectDefinitionMetadataTracker> _serviceTracker =
+			ServiceTrackerFactory.open(
+				FrameworkUtil.getBundle(
+					SystemObjectDefinitionMetadataTracker.class),
+				SystemObjectDefinitionMetadataTracker.class);
+
+}
