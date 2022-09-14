ed63611efee71b3d33a840cc4a32bb1f5d913d51
Adolfo Pérez Álvarez <adolfo.perez@liferay.com>
2022-09-14 14:25:41 +0200

    LPS-165648 Add skeleton repository browser tag

---
+++ modules/apps/document-library/document-library-taglib/src/main/java/com/liferay/document/library/taglib/servlet/taglib/RepositoryBrowserTag.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.taglib.servlet.taglib;
+
+import com.liferay.taglib.util.IncludeTag;
+
+/**
+ * @author Adolfo Pérez
+ */
+public class RepositoryBrowserTag extends IncludeTag {
+
+	@Override
+	public int doStartTag() {
+		return EVAL_BODY_INCLUDE;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	private static final String _PAGE = "/repository_browser/page.jsp";
+
+}
