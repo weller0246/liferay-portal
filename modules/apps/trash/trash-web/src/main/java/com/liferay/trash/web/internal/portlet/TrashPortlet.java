d7e558f151b4ed3ec5974e3a6c33d309a5ed0ed8
Alan Huang <alan.huang@liferay.com>
2022-09-15 19:46:38 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/trash/trash-web/src/main/java/com/liferay/trash/web/internal/portlet/TrashPortlet.java
+++ modules/apps/trash/trash-web/src/main/java/com/liferay/trash/web/internal/portlet/TrashPortlet.java
@@ -80,7 +80,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + TrashPortletKeys.TRASH,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = {Portlet.class, TrashPortlet.class}
 )
