9f79c766b94f3fe2f0cb54440fa1f0a3cfed6f41
Alan Huang <alan.huang@liferay.com>
2022-09-16 00:06:10 +0800

    LPS-163052 Catch more cases

--- modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPLanguageUtilCheck.java
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/JSPLanguageUtilCheck.java
@@ -14,11 +14,9 @@
 
 package com.liferay.source.formatter.check;
 
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.source.formatter.check.util.JavaSourceUtil;
+import com.liferay.portal.tools.ToolsUtil;
 
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -41,58 +39,22 @@ public class JSPLanguageUtilCheck extends BaseFileCheck {
 			}
 		}
 
-		return _replaceLiferayTag(content);
-	}
-
-	private String _replaceLiferayTag(String content) {
-		Matcher matcher = _javaSourcePattern.matcher(content);
-
-		StringBuffer sb = new StringBuffer();
+		matcher = _languageUtilGetCallPattern.matcher(content);
 
 		while (matcher.find()) {
-			String expression = matcher.group();
-
-			expression = expression.replaceAll("\n\t*", StringPool.BLANK);
-
-			Matcher expMatcher = _languageUtilGetPattern.matcher(expression);
-
-			if (expMatcher.find()) {
-				String javaSource = expMatcher.group(1);
-
-				List<String> parameters = JavaSourceUtil.getParameterList(
-					javaSource);
-
-				if (parameters.size() != 2) {
-					continue;
-				}
-
-				String secondParameter = parameters.get(1);
-
-				if (!secondParameter.startsWith("\"") ||
-					!secondParameter.endsWith("\"") ||
-					(StringUtil.count(secondParameter, "\"") > 2)) {
-
-					continue;
-				}
-
-				matcher.appendReplacement(
-					sb, "<liferay-ui:message key=" + secondParameter + " />");
+			if (!ToolsUtil.isInsideQuotes(content, matcher.start())) {
+				addMessage(
+					fileName,
+					"Use <liferay-ui:message> tag instead of LanguageUtil.get'",
+					getLineNumber(content, matcher.start()));
 			}
-		}
-
-		if (sb.length() > 0) {
-			matcher.appendTail(sb);
-
-			return sb.toString();
 		}
 
 		return content;
 	}
 
-	private static final Pattern _javaSourcePattern = Pattern.compile(
-		"(?<!['\"])<%=((?!%>)[\\s\\S])*%>");
-	private static final Pattern _languageUtilGetPattern = Pattern.compile(
-		"<%= ?(LanguageUtil\\.get\\(request, ((?!\\)).)+\\)) ?%>");
+	private static final Pattern _languageUtilGetCallPattern = Pattern.compile(
+		"<%= LanguageUtil\\.get\\(");
 	private static final Pattern _languageUtilPattern = Pattern.compile(
 		"LanguageUtil\\.get\\(locale,");
 
