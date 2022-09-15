73d5181a55688a0481f5be2ef285d00eb8e4c314
Alejandro Tardín <alejandro.tardin@liferay.com>
2022-09-15 18:14:39 +0200

    LPS-160310 Using the right className
    The ContentDashboardFactory uses FileEntry but the vocabulary must be
    linked to DLFileEntry. This was causing FileEntry to be added to the
    list of classNameIds for the vocabulary which was causing the failure in
    the ticket, because it should not have been there.

--- modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/ContentDashboardItemFactoryTracker.java
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/java/com/liferay/content/dashboard/web/internal/item/ContentDashboardItemFactoryTracker.java
@@ -18,6 +18,7 @@ import com.liferay.asset.kernel.model.AssetVocabularyConstants;
 import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
 import com.liferay.content.dashboard.web.internal.constants.ContentDashboardConstants;
 import com.liferay.content.dashboard.web.internal.util.AssetVocabularyUtil;
+import com.liferay.info.search.InfoSearchClassMapperTracker;
 import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
@@ -85,6 +86,9 @@ public class ContentDashboardItemFactoryTracker {
 	@Reference
 	private CompanyLocalService _companyLocalService;
 
+	@Reference
+	private InfoSearchClassMapperTracker _infoSearchClassMapperTracker;
+
 	private volatile ServiceTrackerMap<String, ContentDashboardItemFactory<?>>
 		_serviceTrackerMap;
 
@@ -106,7 +110,9 @@ public class ContentDashboardItemFactoryTracker {
 				_bundleContext.getService(serviceReference);
 
 			long classNameId = _classNameLocalService.getClassNameId(
-				GenericUtil.getGenericClassName(contentDashboardItemFactory));
+				_infoSearchClassMapperTracker.getSearchClassName(
+					GenericUtil.getGenericClassName(
+						contentDashboardItemFactory)));
 
 			for (ContentDashboardConstants.DefaultInternalAssetVocabularyName
 					defaultInternalAssetVocabularyName :
