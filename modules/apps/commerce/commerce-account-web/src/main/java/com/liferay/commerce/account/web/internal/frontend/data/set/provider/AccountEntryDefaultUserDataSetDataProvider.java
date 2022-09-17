75912e01fbb703fc287c44621670b3a369a5230e
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-09-17 09:21:10 +0200

    COMMERCE-9707 Add new dataset

---
+++ modules/apps/commerce/commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/frontend/data/set/provider/AccountEntryDefaultUserDataSetDataProvider.java
@@ -0,0 +1,150 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.web.internal.frontend.data.set.provider;
+
+import com.liferay.commerce.account.web.internal.constants.CommerceAccountFDSNames;
+import com.liferay.commerce.account.web.internal.model.User;
+import com.liferay.commerce.product.constants.CommerceChannelAccountEntryRelConstants;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.model.CommerceChannelAccountEntryRel;
+import com.liferay.commerce.product.service.CommerceChannelAccountEntryRelService;
+import com.liferay.commerce.product.service.CommerceChannelService;
+import com.liferay.frontend.data.set.provider.FDSDataProvider;
+import com.liferay.frontend.data.set.provider.search.FDSKeywords;
+import com.liferay.frontend.data.set.provider.search.FDSPagination;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.UserService;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.vulcan.util.TransformUtil;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "fds.data.provider.key=" + CommerceAccountFDSNames.ACCOUNT_ENTRY_DEFAULT_USERS,
+	service = FDSDataProvider.class
+)
+public class AccountEntryDefaultUserDataSetDataProvider
+	implements FDSDataProvider<User> {
+
+	@Override
+	public List<User> getItems(
+			FDSKeywords fdsKeywords, FDSPagination fdsPagination,
+			HttpServletRequest httpServletRequest, Sort sort)
+		throws PortalException {
+
+		long accountEntryId = ParamUtil.getLong(
+			httpServletRequest, "accountEntryId");
+		int type = ParamUtil.getInteger(httpServletRequest, "type");
+
+		return TransformUtil.transform(
+			_commerceChannelAccountEntryRelService.
+				getCommerceChannelAccountEntryRels(
+					accountEntryId,
+					CommerceChannelAccountEntryRelConstants.TYPE_USER,
+					QueryUtil.ALL_POS, QueryUtil.ALL_POS, null),
+			commerceChannelAccountEntryRel -> {
+				com.liferay.portal.kernel.model.User user =
+					_userService.getUserById(
+						commerceChannelAccountEntryRel.getClassPK());
+
+				return new User(
+					commerceChannelAccountEntryRel.getAccountEntryId(),
+					_getChannelName(
+						accountEntryId,
+						commerceChannelAccountEntryRel.getCommerceChannelId(),
+						httpServletRequest, type),
+					commerceChannelAccountEntryRel.
+						getCommerceChannelAccountEntryRelId(),
+					user.getFullName(),
+					commerceChannelAccountEntryRel.getType());
+			});
+	}
+
+	@Override
+	public int getItemsCount(
+			FDSKeywords fdsKeywords, HttpServletRequest httpServletRequest)
+		throws PortalException {
+
+		long accountEntryId = ParamUtil.getLong(
+			httpServletRequest, "accountEntryId");
+
+		return _commerceChannelAccountEntryRelService.
+			getCommerceChannelAccountEntryRelsCount(
+				accountEntryId,
+				CommerceChannelAccountEntryRelConstants.TYPE_USER);
+	}
+
+	private String _getChannelName(
+			long accountEntryId, long commerceChannelId,
+			HttpServletRequest httpServletRequest, int type)
+		throws PortalException {
+
+		CommerceChannel commerceChannel =
+			_commerceChannelService.fetchCommerceChannel(commerceChannelId);
+
+		if (commerceChannel == null) {
+			List<CommerceChannelAccountEntryRel>
+				commerceChannelAccountEntryRels =
+					_commerceChannelAccountEntryRelService.
+						getCommerceChannelAccountEntryRels(
+							accountEntryId, type, QueryUtil.ALL_POS,
+							QueryUtil.ALL_POS, null);
+
+			if (commerceChannelAccountEntryRels.size() == 1) {
+				CommerceChannelAccountEntryRel commerceChannelAccountEntryRel =
+					commerceChannelAccountEntryRels.get(0);
+
+				if (commerceChannelAccountEntryRel.getCommerceChannelId() ==
+						0) {
+
+					return _language.get(httpServletRequest, "all-channels");
+				}
+			}
+			else if (!commerceChannelAccountEntryRels.isEmpty()) {
+				return _language.get(httpServletRequest, "all-other-channels");
+			}
+
+			return _language.get(httpServletRequest, "all-channels");
+		}
+
+		return commerceChannel.getName();
+	}
+
+	@Reference
+	private CommerceChannelAccountEntryRelService
+		_commerceChannelAccountEntryRelService;
+
+	@Reference
+	private CommerceChannelService _commerceChannelService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private UserService _userService;
+
+}
---
+++ modules/apps/commerce/commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/frontend/data/set/provider/AccountEntryDefaultUserFDSActionProvider.java
@@ -0,0 +1,151 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.web.internal.frontend.data.set.provider;
+
+import com.liferay.account.constants.AccountPortletKeys;
+import com.liferay.account.model.AccountEntry;
+import com.liferay.commerce.account.web.internal.constants.CommerceAccountFDSNames;
+import com.liferay.commerce.account.web.internal.model.User;
+import com.liferay.frontend.data.set.provider.FDSActionProvider;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+
+import java.util.List;
+
+import javax.portlet.PortletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferencePolicy;
+import org.osgi.service.component.annotations.ReferencePolicyOption;
+
+/**
+ * @author Crescenzo Rega
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = "fds.data.provider.key=" + CommerceAccountFDSNames.ACCOUNT_ENTRY_DEFAULT_USERS,
+	service = FDSActionProvider.class
+)
+public class AccountEntryDefaultUserFDSActionProvider
+	implements FDSActionProvider {
+
+	@Override
+	public List<DropdownItem> getDropdownItems(
+			long groupId, HttpServletRequest httpServletRequest, Object model)
+		throws PortalException {
+
+		User user = (User)model;
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		return DropdownItemListBuilder.add(
+			() -> _accountEntryModelResourcePermission.contains(
+				permissionChecker, user.getAccountEntryId(), ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.setHref(
+					_getCommerceChannelAccountEntryRelEditURL(
+						user.getAccountEntryId(),
+						user.getCommerceChannelAccountEntryRelId(),
+						httpServletRequest, user.getType()));
+				dropdownItem.setLabel(
+					_language.get(httpServletRequest, Constants.EDIT));
+				dropdownItem.setTarget("modal-lg");
+			}
+		).add(
+			() -> _accountEntryModelResourcePermission.contains(
+				permissionChecker, user.getAccountEntryId(), ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.setHref(
+					_getCommerceChannelAccountEntryRelDeleteURL(
+						user.getCommerceChannelAccountEntryRelId(),
+						httpServletRequest));
+				dropdownItem.setLabel(
+					_language.get(httpServletRequest, Constants.DELETE));
+			}
+		).build();
+	}
+
+	private String _getCommerceChannelAccountEntryRelDeleteURL(
+		long commerceChannelAccountEntryRelId,
+		HttpServletRequest httpServletRequest) {
+
+		return PortletURLBuilder.create(
+			_portal.getControlPanelPortletURL(
+				httpServletRequest, AccountPortletKeys.ACCOUNT_ENTRIES_ADMIN,
+				PortletRequest.ACTION_PHASE)
+		).setActionName(
+			"/account_entries_admin/edit_account_entry_user"
+		).setCMD(
+			Constants.DELETE
+		).setRedirect(
+			ParamUtil.getString(
+				httpServletRequest, "currentUrl",
+				_portal.getCurrentURL(httpServletRequest))
+		).setParameter(
+			"commerceChannelAccountEntryRelId", commerceChannelAccountEntryRelId
+		).buildString();
+	}
+
+	private String _getCommerceChannelAccountEntryRelEditURL(
+		long accountEntryId, long commerceChannelAccountEntryRelId,
+		HttpServletRequest httpServletRequest, int type) {
+
+		return PortletURLBuilder.create(
+			_portal.getControlPanelPortletURL(
+				httpServletRequest, AccountPortletKeys.ACCOUNT_ENTRIES_ADMIN,
+				PortletRequest.RENDER_PHASE)
+		).setMVCRenderCommandName(
+			"/account_entries_admin/edit_account_entry_user"
+		).setParameter(
+			"accountEntryId", accountEntryId
+		).setParameter(
+			"commerceChannelAccountEntryRelId", commerceChannelAccountEntryRelId
+		).setParameter(
+			"type", type
+		).setWindowState(
+			LiferayWindowState.POP_UP
+		).buildString();
+	}
+
+	@Reference(
+		policy = ReferencePolicy.DYNAMIC,
+		policyOption = ReferencePolicyOption.GREEDY,
+		target = "(model.class.name=com.liferay.account.model.AccountEntry)"
+	)
+	private volatile ModelResourcePermission<AccountEntry>
+		_accountEntryModelResourcePermission;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+}
