b7fd6762defe475a74e4e81322498d368df5038f
Lily.Chi <lily.chi@liferay.com>
2022-09-19 17:38:23 +0800

    LPS-163055 Convert method reference to field reference in AssetCategoryTitleInfoItemRenderer

--- modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/item/renderer/AssetCategoryTitleInfoItemRenderer.java
+++ modules/apps/asset/asset-categories-admin-web/src/main/java/com/liferay/asset/categories/admin/web/internal/info/item/renderer/AssetCategoryTitleInfoItemRenderer.java
@@ -63,17 +63,12 @@ public class AssetCategoryTitleInfoItemRenderer
 		}
 	}
 
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.asset.categories.admin.web)",
-		unbind = "-"
-	)
-	public void setServletContext(ServletContext servletContext) {
-		_servletContext = servletContext;
-	}
-
 	@Reference
 	private Language _language;
 
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.asset.categories.admin.web)"
+	)
 	private ServletContext _servletContext;
 
 }
