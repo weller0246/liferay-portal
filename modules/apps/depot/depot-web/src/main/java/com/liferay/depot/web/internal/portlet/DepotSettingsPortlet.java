21633ff9752d6be5c9fa7b30540c1b4be4b01f9f
Alan Huang <alan.huang@liferay.com>
2022-09-20 01:44:50 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/DepotSettingsPortlet.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/portlet/DepotSettingsPortlet.java
@@ -57,7 +57,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/edit_depot_entry.jsp",
 		"javax.portlet.name=" + DepotPortletKeys.DEPOT_SETTINGS,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
