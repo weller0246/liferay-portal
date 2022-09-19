4016b4cd4ecb8782ba63c50c8270bf97e8910ce4
Joao Marcelo Alves <joao.alves@liferay.com>
2022-09-19 17:43:33 -0300

    LPS-163132 Create EVP Process Section

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/evp-process-steps-slider/index.js
@@ -0,0 +1,144 @@
+/* eslint-disable no-undef */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const defaultColor = configuration.textColor;
+const stepsNumber = fragmentElement.querySelectorAll('h1');
+const stepsTitle = fragmentElement.querySelectorAll('h5');
+
+stepsNumber.forEach((stepNumber) => {
+	stepNumber.style.color = defaultColor;
+});
+
+stepsTitle.forEach((stepTitle) => {
+	stepTitle.style.color = defaultColor;
+});
+
+const MOVE_LEFT = 'move-left';
+const MOVE_RIGHT = 'move-right';
+const INTERVAL = 5000;
+
+const editMode = layoutMode === 'edit';
+const indicators = [].slice.call(
+	fragmentElement.querySelectorAll('.carousel-navigation button')
+);
+const items = [].slice.call(fragmentElement.querySelectorAll('.carousel-item'));
+
+let moving = false;
+
+function getActiveIndicator() {
+	return fragmentElement.querySelector('.carousel-navigation .active');
+}
+
+function activateIndicator(activeItem, nextItem, movement) {
+	if (movement) {
+		activeItem.classList.add(movement);
+		nextItem.classList.add(movement);
+	}
+
+	getActiveIndicator().classList.remove('active');
+	indicators[this.nextItemIndex].classList.add('active');
+}
+
+function activateItem(activeItem, nextItem, movement) {
+	activeItem.classList.remove('active');
+	nextItem.classList.add('active');
+
+	if (movement) {
+		activeItem.classList.remove(movement);
+		nextItem.classList.remove(movement);
+	}
+}
+
+function move(movement, index = null) {
+	if (moving) {
+		return;
+	}
+
+	moving = true;
+
+	const activeItem = fragmentElement.querySelector('.carousel-item.active');
+	const indexActiveItem = items.indexOf(activeItem);
+
+	this.nextItemIndex =
+		indexActiveItem < 1 ? items.length - 1 : indexActiveItem - 1;
+
+	if (index !== null) {
+		this.nextItemIndex = index;
+	}
+	else if (movement === MOVE_RIGHT) {
+		this.nextItemIndex =
+			indexActiveItem >= items.length - 1 ? 0 : indexActiveItem + 1;
+	}
+
+	const nextItem = items[this.nextItemIndex];
+
+	activateIndicator(activeItem, nextItem, movement);
+
+	setTimeout(() => {
+		activateItem(activeItem, nextItem, movement);
+
+		moving = false;
+	}, 600);
+}
+
+function createInterval() {
+	let intervalId = null;
+
+	if (!editMode) {
+		intervalId = setInterval(() => {
+			if (document.contains(items[0])) {
+				move(MOVE_RIGHT);
+			}
+			else {
+				clearInterval(intervalId);
+			}
+		}, INTERVAL);
+	}
+
+	return intervalId;
+}
+
+function main() {
+	let intervalId = createInterval();
+
+	if (this.nextItemIndex && this.nextItemIndex < items.length) {
+		const activeItem = fragmentElement.querySelector(
+			'.carousel-item.active'
+		);
+		const nextItem = items[this.nextItemIndex];
+
+		activateIndicator(activeItem, nextItem);
+		activateItem(activeItem, nextItem);
+	}
+
+	indicators.forEach((indicator, index) => {
+		indicator.addEventListener('click', () => {
+			const indexActiveIndicator = indicators.indexOf(
+				getActiveIndicator()
+			);
+
+			if (index !== indexActiveIndicator) {
+				if (index < indexActiveIndicator) {
+					move(MOVE_LEFT, index);
+				}
+				else {
+					move(MOVE_RIGHT, index);
+				}
+			}
+
+			clearInterval(intervalId);
+			intervalId = createInterval();
+		});
+	});
+}
+
+main();
