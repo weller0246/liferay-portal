c2d51e6972174f662d9e306a756d6abe840a17bd
Crescenzo Rega <97021604+CrescenzoRegaLF@users.noreply.github.com>
2022-09-19 14:57:19 +0200

    COMMERCE-9707 Add util class to retrive commerceChannelId

---
+++ modules/apps/commerce/commerce-account-service/src/main/java/com/liferay/commerce/account/internal/util/AccountEntryUtil.java
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.internal.util;
+
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CommerceChannelLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+
+/**
+ * @author Crescenzo Rega
+ */
+public class AccountEntryUtil {
+
+	public static long getCommerceChannelId(
+			CommerceContext commerceContext, Group group)
+		throws PortalException {
+
+		if (commerceContext != null) {
+			return commerceContext.getCommerceChannelId();
+		}
+
+		if (group != null) {
+			CommerceChannel commerceChannel =
+				CommerceChannelLocalServiceUtil.fetchCommerceChannelByGroupId(
+					group.getGroupId());
+
+			if (commerceChannel != null) {
+				return commerceChannel.getCommerceChannelId();
+			}
+
+			commerceChannel =
+				CommerceChannelLocalServiceUtil.
+					fetchCommerceChannelBySiteGroupId(group.getGroupId());
+
+			if (commerceChannel != null) {
+				return commerceChannel.getCommerceChannelId();
+			}
+		}
+
+		return 0;
+	}
+
+}
