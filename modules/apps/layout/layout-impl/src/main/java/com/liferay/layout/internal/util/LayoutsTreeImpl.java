3c190f5004a21774e7a71474a4c964b5b6295830
Victor Galan <victor.galan@liferay.com>
2022-09-20 15:08:51 +0200

    LPS-163492 Set a default target in case we don't have any target configured

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutsTreeImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/util/LayoutsTreeImpl.java
@@ -46,6 +46,7 @@ import com.liferay.portal.kernel.service.permission.LayoutPermissionUtil;
 import com.liferay.portal.kernel.servlet.BrowserSniffer;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HtmlUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
@@ -664,7 +665,9 @@ public class LayoutsTreeImpl implements LayoutsTree {
 				_sites.isLayoutSortable(layout)
 			).put(
 				"target",
-				HtmlUtil.escape(layout.getTypeSettingsProperty("target"))
+				GetterUtil.getString(
+					HtmlUtil.escape(layout.getTypeSettingsProperty("target")),
+					"_self")
 			).put(
 				"type", layout.getType()
 			).put(
