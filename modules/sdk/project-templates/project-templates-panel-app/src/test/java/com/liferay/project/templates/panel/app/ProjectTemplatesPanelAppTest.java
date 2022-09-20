24af340ea81a4f91c2176b94fc16152a628f6cb5
Seiphon Wang <seiphon.wang@liferay.com>
2022-09-20 18:08:01 +0800

    LPS-160104 Fix test issues for project-templates-panel-app

--- modules/sdk/project-templates/project-templates-panel-app/src/test/java/com/liferay/project/templates/panel/app/ProjectTemplatesPanelAppTest.java
+++ modules/sdk/project-templates/project-templates-panel-app/src/test/java/com/liferay/project/templates/panel/app/ProjectTemplatesPanelAppTest.java
@@ -50,7 +50,8 @@ public class ProjectTemplatesPanelAppTest
 	public static Iterable<Object[]> data() {
 		return Arrays.asList(
 			new Object[][] {
-				{"7.0.6-2"}, {"7.1.3-1"}, {"7.2.1-1"}, {"7.3.7"}, {"7.4.3.36"}
+				{"7.0.6-2", "dxp"}, {"7.1.3-1", "dxp"}, {"7.2.1-1", "dxp"},
+				{"7.3.7", "portal"}, {"7.4.3.36", "portal"}
 			});
 	}
 
@@ -70,8 +71,9 @@ public class ProjectTemplatesPanelAppTest
 		_gradleDistribution = URI.create(gradleDistribution);
 	}
 
-	public ProjectTemplatesPanelAppTest(String liferayVersion) {
+	public ProjectTemplatesPanelAppTest(String liferayVersion, String product) {
 		_liferayVersion = liferayVersion;
+		_product = product;
 	}
 
 	@Test
@@ -88,7 +90,7 @@ public class ProjectTemplatesPanelAppTest
 
 		File gradleProjectDir = buildTemplateWithGradle(
 			gradleWorkspaceModulesDir, template, name, "--liferay-version",
-			_liferayVersion, "--class-name", "Foo");
+			_liferayVersion, "--class-name", "Foo", "--product", _product);
 
 		testExists(gradleProjectDir, "bnd.bnd");
 
@@ -100,9 +102,15 @@ public class ProjectTemplatesPanelAppTest
 			gradleProjectDir, "bnd.bnd",
 			"Export-Package: gradle.test.constants");
 
-		testContains(
-			gradleProjectDir, "build.gradle",
-			DEPENDENCY_RELEASE_PORTAL_API);
+		if (VersionUtil.getMinorVersion(_liferayVersion) < 3) {
+			testContains(
+				gradleProjectDir, "build.gradle", DEPENDENCY_RELEASE_DXP_API);
+		}
+		else {
+			testContains(
+				gradleProjectDir, "build.gradle",
+				DEPENDENCY_RELEASE_PORTAL_API);
+		}
 
 		testContains(
 			gradleProjectDir,
@@ -134,7 +142,7 @@ public class ProjectTemplatesPanelAppTest
 		File mavenProjectDir = buildTemplateWithMaven(
 			mavenModulesDir, mavenModulesDir, template, name, "com.test",
 			mavenExecutor, "-DclassName=Foo", "-Dpackage=gradle.test",
-			"-DliferayVersion=" + _liferayVersion);
+			"-DliferayVersion=" + _liferayVersion, "-Dproduct=" + _product);
 
 		if (!_liferayVersion.startsWith("7.0")) {
 			testContains(
@@ -159,5 +167,6 @@ public class ProjectTemplatesPanelAppTest
 	private static URI _gradleDistribution;
 
 	private final String _liferayVersion;
+	private final String _product;
 
 }
