6f3ef90f18e10e67a6f86b88a7aa854e22a5be06
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-09-21 10:57:37 +0200

    LPS-162525 Move to internal package

--- modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/util/SegmentsExperienceTestUtil.java
+++ modules/apps/layout/layout-content-page-editor-web-test/src/testIntegration/java/com/liferay/layout/content/page/editor/web/internal/util/SegmentsExperienceTestUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.layout.content.page.editor.web.util;
+package com.liferay.layout.content.page.editor.web.internal.util;
 
 import com.liferay.fragment.model.FragmentEntryLink;
 import com.liferay.fragment.service.FragmentEntryLinkLocalServiceUtil;
