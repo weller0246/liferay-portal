9faec7aafdb84f49a40711c532e3e2ea7c3b3fea
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-09-21 12:22:09 -0300

    LRAC-11909 - Add side menu on connected page

--- modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/pages/default/DefaultPage.tsx
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/pages/default/DefaultPage.tsx
@@ -12,12 +12,70 @@
  * details.
  */
 
-import React from 'react';
+import {ClayVerticalNav} from '@clayui/nav';
+import React, {useState} from 'react';
 
 import WorkspaceConnection from './WorkspaceConnection';
 
+const NAV_KEYS = {
+	ATTRIBUTES: 'attributes',
+	PEOPLE: 'people',
+	PROPERTIES: 'properties',
+	WORKSPACE_CONNECTION: 'workspaceConnection',
+};
+
 const DefaultPage: React.FC<React.HTMLAttributes<HTMLElement>> = () => {
-	return <WorkspaceConnection />;
+	const [activeNavKey, setActiveNavKey] = useState(
+		NAV_KEYS.WORKSPACE_CONNECTION
+	);
+
+	return (
+		<div className="d-flex">
+			<div>
+				<p className="text-uppercase">
+					{Liferay.Language.get('instance-scope')}
+				</p>
+
+				<ClayVerticalNav
+					items={[
+						{
+							active:
+								activeNavKey === NAV_KEYS.WORKSPACE_CONNECTION,
+							label: Liferay.Language.get('workspace-connection'),
+							onClick: () =>
+								setActiveNavKey(NAV_KEYS.WORKSPACE_CONNECTION),
+						},
+						{
+							active: activeNavKey === NAV_KEYS.PROPERTIES,
+							label: Liferay.Language.get('properties'),
+							onClick: () => setActiveNavKey(NAV_KEYS.PROPERTIES),
+						},
+						{
+							active: activeNavKey === NAV_KEYS.PEOPLE,
+							label: Liferay.Language.get('people'),
+							onClick: () => setActiveNavKey(NAV_KEYS.PEOPLE),
+						},
+						{
+							active: activeNavKey === NAV_KEYS.ATTRIBUTES,
+							label: Liferay.Language.get('attributes'),
+							onClick: () => setActiveNavKey(NAV_KEYS.ATTRIBUTES),
+						},
+					]}
+					large={false}
+				/>
+			</div>
+
+			{activeNavKey === NAV_KEYS.WORKSPACE_CONNECTION && (
+				<WorkspaceConnection />
+			)}
+
+			{activeNavKey === NAV_KEYS.PROPERTIES && <p>properties</p>}
+
+			{activeNavKey === NAV_KEYS.PEOPLE && <p>people</p>}
+
+			{activeNavKey === NAV_KEYS.ATTRIBUTES && <p>attributes</p>}
+		</div>
+	);
 };
 
 export default DefaultPage;
