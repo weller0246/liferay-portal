35b978d5022ac1540535777305b36d420cb43fec
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-09-22 18:19:40 +0200

    LPS-163798 Adds a new enum to represent the different utility pages types

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/constants/LayoutUtilityPageEntryConstants.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.constants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class LayoutUtilityPageEntryConstants {
+
+	public enum Type {
+
+		ERROR_404("404", 1), TERMS_OF_USE("terms-of-use", 2);
+
+		public String getLabel() {
+			return _label;
+		}
+
+		public int getType() {
+			return _type;
+		}
+
+		public void setLabel(String label) {
+			_label = label;
+		}
+
+		public void setType(int type) {
+			_type = type;
+		}
+
+		private Type(String label, int type) {
+			_label = label;
+			_type = type;
+		}
+
+		private String _label;
+		private int _type;
+
+	}
+
+}
