7cc5ab5d3e6ff9b698e7c8d0e7524c54c90d94cc
Felipe Veloso <felipe.veloso@liferay.com>
2022-09-22 11:20:53 -0300

    LPS-163805 config ServiceTrackerMap

--- modules/apps/view-count/view-count-service/src/main/java/com/liferay/view/count/service/impl/ViewCountEntryLocalServiceImpl.java
+++ modules/apps/view-count/view-count-service/src/main/java/com/liferay/view/count/service/impl/ViewCountEntryLocalServiceImpl.java
@@ -14,12 +14,17 @@
 
 package com.liferay.view.count.service.impl;
 
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.petra.sql.dsl.Table;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.change.tracking.CTAware;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.increment.BufferedIncrement;
 import com.liferay.portal.kernel.increment.NumberIncrement;
+import com.liferay.portal.kernel.model.ClassName;
 import com.liferay.portal.kernel.module.framework.service.IdentifiableOSGiService;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.PersistedModelLocalService;
@@ -31,6 +36,7 @@ import com.liferay.portal.kernel.transaction.Transactional;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.view.count.ViewCountManager;
 import com.liferay.view.count.configuration.ViewCountConfiguration;
+import com.liferay.view.count.listener.ViewCountEntryModelListener;
 import com.liferay.view.count.model.ViewCountEntry;
 import com.liferay.view.count.model.ViewCountEntryTable;
 import com.liferay.view.count.service.ViewCountEntryLocalService;
@@ -41,8 +47,10 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+import org.osgi.framework.BundleContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
@@ -121,7 +129,29 @@ public class ViewCountEntryLocalServiceImpl
 			viewCountEntryFinder.incrementViewCount(
 				companyId, classNameId, classPK, increment);
 		}
+
+		try {
+			ClassName className = _classNameLocalService.getClassName(classNameId);
+			ViewCountEntryModelListener viewCountIncrementListener = _serviceTrackerMap.getService(className.getValue());
+
+			if(viewCountIncrementListener == null){
+				return;
+			}
+
+			viewCountIncrementListener.afterIncrement(fetchViewCountEntry(new ViewCountEntryPK(
+				companyId, classNameId,
+				classPK)));
+		}
+		catch (PortalException portalException) {
+			throw new RuntimeException(portalException);
+		}
 	}
+
+	@Deactivate
+	protected void deactivate() {_serviceTrackerMap.close();}
+
+	private ServiceTrackerMap<String, ViewCountEntryModelListener>
+		_serviceTrackerMap;
 
 	@Override
 	@Transactional(enabled = false)
@@ -140,8 +170,17 @@ public class ViewCountEntryLocalServiceImpl
 	}
 
 	@Activate
+	protected void activate(BundleContext bundleContext, Map<String, Object> properties) {
+		modified(properties);
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, ViewCountEntryModelListener.class, null,
+			ServiceReferenceMapperFactory.createFromFunction(
+				bundleContext,
+				ViewCountEntryModelListener::getModelClassName));
+	}
 	@Modified
-	protected void activate(Map<String, Object> properties) {
+	protected void modified(Map<String, Object> properties) {
 		ViewCountConfiguration viewCountConfiguration =
 			ConfigurableUtil.createConfigurable(
 				ViewCountConfiguration.class, properties);
