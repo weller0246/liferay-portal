19b22ad4b240865cde068d5300dd8a2d2a0553dd
Brian Chan <brian.chan@liferay.com>
2022-09-22 08:36:05 -0300

    LPS-163687 Apply

--- modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/portlet/category/PortletCategoryManagerImpl.java
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/portlet/category/PortletCategoryManagerImpl.java
@@ -214,8 +214,7 @@ public class PortletCategoryManagerImpl implements PortletCategoryManager {
 									currentPortletCategory, themeDisplay);
 
 						return JSONUtil.toJSONArray(
-							new ArrayList<>(
-								childPortletCategoryJSONObjectsMap.values()),
+							childPortletCategoryJSONObjectsMap.values(),
 							portletCategoryJSONObject ->
 								portletCategoryJSONObject);
 					}
