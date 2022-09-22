87163dbbd464d9bf2a401de5b75a77089c41a5a9
Felipe Veloso <felipe.veloso@liferay.com>
2022-09-22 14:49:24 -0300

    LPS-163805 SF

--- modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/model/listener/MBThreadViewCountEntryModelListener.java
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/model/listener/MBThreadViewCountEntryModelListener.java
@@ -1,51 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.message.boards.internal.model.listener;
 
-import com.liferay.message.boards.model.MBMailingList;
 import com.liferay.message.boards.model.MBMessage;
 import com.liferay.message.boards.model.MBThread;
 import com.liferay.message.boards.service.MBMessageLocalService;
 import com.liferay.message.boards.service.MBThreadLocalService;
 import com.liferay.portal.kernel.search.Indexer;
 import com.liferay.portal.kernel.search.IndexerRegistryUtil;
-import com.liferay.portal.kernel.search.SearchException;
 import com.liferay.view.count.listener.ViewCountEntryModelListener;
 import com.liferay.view.count.model.ViewCountEntry;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
-
-@Component(
-	immediate = true,
-	service = ViewCountEntryModelListener.class
-)
-public class MBThreadViewCountEntryModelListener implements
-	ViewCountEntryModelListener {
+/**
+ * @author Felipe Veloso
+ */
+@Component(immediate = true, service = ViewCountEntryModelListener.class)
+public class MBThreadViewCountEntryModelListener
+	implements ViewCountEntryModelListener {
 
 	@Override
 	public void afterIncrement(ViewCountEntry viewCountEntry) {
+		MBThread mbThread = _mbThreadLocalService.fetchMBThread(
+			viewCountEntry.getClassPK());
 
-		 _mbMessageLocalService.fetchMBMessage(_mbThreadLocalService.fetchMBThread(viewCountEntry.getClassPK()).getRootMessageId());
+		MBMessage mbMessage = _mbMessageLocalService.fetchMBMessage(
+			mbThread.getRootMessageId());
 
 		Indexer<MBMessage> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
 			MBMessage.class);
 
 		try {
-			indexer.reindex(_mbMessageLocalService.fetchMBMessage(_mbThreadLocalService.fetchMBThread(viewCountEntry.getClassPK()).getRootMessageId()));
+			indexer.reindex(mbMessage);
 		}
-		catch (SearchException e) {
-			throw new RuntimeException(e);
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
 		}
-
 	}
 
 	@Override
-	public String getModelClassName(){
+	public String getModelClassName() {
 		return MBThread.class.getName();
 	}
 
 	@Reference
-	private MBThreadLocalService _mbThreadLocalService;
+	private MBMessageLocalService _mbMessageLocalService;
 
 	@Reference
-	private MBMessageLocalService _mbMessageLocalService;
+	private MBThreadLocalService _mbThreadLocalService;
+
 }
