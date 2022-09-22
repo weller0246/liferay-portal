a0741fc955dee4190ee391ca93fdd388823b6053
Gleice Lisbino <gleice.lisbino@liferay.com>
2022-09-22 16:32:16 -0300

    LPS-163842 Index scopeGroupId

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/CommerceChannelIndexer.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/CommerceChannelIndexer.java
@@ -123,6 +123,9 @@ public class CommerceChannelIndexer extends BaseIndexer<CommerceChannel> {
 		document.addKeyword(
 			CPField.COMMERCE_CHANNEL_GROUP_ID, group.getGroupId());
 
+		document.addKeyword(
+			Field.SCOPE_GROUP_ID, commerceChannel.getSiteGroupId());
+
 		if (_log.isDebugEnabled()) {
 			_log.debug("Document " + commerceChannel + " indexed successfully");
 		}
