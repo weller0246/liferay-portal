8b6306ce5ca9aa8ca896ba2223864897c5488b55
Brian Chan <brian.chan@liferay.com>
2022-09-22 13:37:12 -0300

    LRAC-11869 Remove for now

--- modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/resource/v1_0/DataSourceResourceImpl.java
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/resource/v1_0/DataSourceResourceImpl.java
@@ -14,16 +14,9 @@
 
 package com.liferay.analytics.settings.rest.internal.resource.v1_0;
 
-import com.liferay.analytics.settings.rest.dto.v1_0.DataSourceToken;
-import com.liferay.analytics.settings.rest.internal.helper.v1_0.AnalyticsCloudClientHelper;
-import com.liferay.analytics.settings.rest.internal.helper.v1_0.AnalyticsSettingsHelper;
 import com.liferay.analytics.settings.rest.resource.v1_0.DataSourceResource;
-import com.liferay.portal.kernel.json.JSONObject;
-
-import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -34,34 +27,4 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = DataSourceResource.class
 )
 public class DataSourceResourceImpl extends BaseDataSourceResourceImpl {
-
-	@Override
-	public void deleteDataSource() throws Exception {
-		long companyId = contextCompany.getCompanyId();
-
-		_analyticsSettingsHelper.deleteCompanyConfiguration(companyId);
-
-		_analyticsCloudClientHelper.disconnectDataSource(companyId);
-	}
-
-	@Override
-	public void postDataSource(DataSourceToken dataSource) throws Exception {
-		JSONObject dataSourceJSONObject =
-			_analyticsCloudClientHelper.connectDataSource(
-				contextCompany.getCompanyId(), dataSource.getToken());
-
-		Map<String, Object> connectionProperties = dataSourceJSONObject.toMap();
-
-		connectionProperties.put("token", dataSource.getToken());
-
-		_analyticsSettingsHelper.updateCompanyConfiguration(
-			contextCompany.getCompanyId(), connectionProperties);
-	}
-
-	@Reference
-	private AnalyticsCloudClientHelper _analyticsCloudClientHelper;
-
-	@Reference
-	private AnalyticsSettingsHelper _analyticsSettingsHelper;
-
 }
