f57db39cf14db631d69dd23061e0332de56e9704
Victor Galan <victor.galan@liferay.com>
2022-09-22 18:58:43 +0200

    LPS-163828 Adapt tests

--- modules/apps/layout/layout-content-page-editor-web/test/page_editor/common/components/SpacingBox.test.js
+++ modules/apps/layout/layout-content-page-editor-web/test/page_editor/common/components/SpacingBox.test.js
@@ -19,6 +19,7 @@ import React from 'react';
 
 import SpacingBox from '../../../../src/main/resources/META-INF/resources/page_editor/common/components/SpacingBox';
 import {StyleBookContextProvider} from '../../../../src/main/resources/META-INF/resources/page_editor/plugins/page-design-options/hooks/useStyleBook';
+import StoreMother from '../../../../src/main/resources/META-INF/resources/page_editor/test-utils/StoreMother';
 
 jest.mock(
 	'../../../../src/main/resources/META-INF/resources/page_editor/app/config',
@@ -45,103 +46,105 @@ const SpacingBoxTest = ({
 	onChange = () => {},
 	value = {},
 }) => (
-	<StyleBookContextProvider>
-		<SpacingBox
-			canSetCustomValue={canSetCustomValue}
-			fields={{
-				marginBottom: {
-					defaultValue: '0',
-					label: 'margin-bottom',
-					name: 'marginTop',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+	<StoreMother.Component>
+		<StyleBookContextProvider>
+			<SpacingBox
+				canSetCustomValue={canSetCustomValue}
+				fields={{
+					marginBottom: {
+						defaultValue: '0',
+						label: 'margin-bottom',
+						name: 'marginTop',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-				marginLeft: {
-					defaultValue: '0',
-					label: 'margin-left',
-					name: 'marginLeft',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+					marginLeft: {
+						defaultValue: '0',
+						label: 'margin-left',
+						name: 'marginLeft',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-				marginRight: {
-					defaultValue: '0',
-					label: 'margin-right',
-					name: 'marginRight',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+					marginRight: {
+						defaultValue: '0',
+						label: 'margin-right',
+						name: 'marginRight',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-				marginTop: {
-					defaultValue: '0',
-					label: 'margin-top',
-					name: 'marginTop',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+					marginTop: {
+						defaultValue: '0',
+						label: 'margin-top',
+						name: 'marginTop',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-				paddingBottom: {
-					defaultValue: '0',
-					label: 'padding-bottom',
-					name: 'paddingBottom',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+					paddingBottom: {
+						defaultValue: '0',
+						label: 'padding-bottom',
+						name: 'paddingBottom',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-				paddingLeft: {
-					defaultValue: '0',
-					label: 'padding-left',
-					name: 'paddingLeft',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+					paddingLeft: {
+						defaultValue: '0',
+						label: 'padding-left',
+						name: 'paddingLeft',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-				paddingRight: {
-					defaultValue: '0',
-					label: 'padding-right',
-					name: 'paddingRight',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '5', value: '5'},
-						],
+					paddingRight: {
+						defaultValue: '0',
+						label: 'padding-right',
+						name: 'paddingRight',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '5', value: '5'},
+							],
+						},
 					},
-				},
-				paddingTop: {
-					defaultValue: '0',
-					label: 'padding-top',
-					name: 'paddingTop',
-					typeOptions: {
-						validValues: [
-							{label: '0', value: '0'},
-							{label: '10', value: '10'},
-						],
+					paddingTop: {
+						defaultValue: '0',
+						label: 'padding-top',
+						name: 'paddingTop',
+						typeOptions: {
+							validValues: [
+								{label: '0', value: '0'},
+								{label: '10', value: '10'},
+							],
+						},
 					},
-				},
-			}}
-			onChange={onChange}
-			value={value}
-		/>
-	</StyleBookContextProvider>
+				}}
+				onChange={onChange}
+				value={value}
+			/>
+		</StyleBookContextProvider>
+	</StoreMother.Component>
 );
 
 describe('SpacingBox', () => {
