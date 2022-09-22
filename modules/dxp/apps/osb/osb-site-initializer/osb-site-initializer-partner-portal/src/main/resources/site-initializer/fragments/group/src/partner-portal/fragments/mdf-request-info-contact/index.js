48cf7dbbe30c5e340f845a02b41d2c8b14455aa5
Eunice de Souza <eunice.souza@liferay.com>
2022-09-22 11:33:36 -0300

    LPS-163462 Adding the contact info fragment on tge display page template

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/partner-portal/fragments/mdf-request-info-contact/index.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+/* eslint-disable no-undef */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+const currentPath = Liferay.currentURL.split('/');
+const mdfRequestId = +currentPath[currentPath.length - 1];
+
+const getContactInfo = async () => {
+	// eslint-disable-next-line @liferay/portal/no-global-fetch
+	const response = await fetch(
+		`/o/c/mdfrequests/${mdfRequestId}?nestedFields=user`,
+		{
+			headers: {
+				'accept': 'application/json',
+				'x-csrf-token': Liferay.authToken,
+			},
+		}
+	);
+	if (response.ok) {
+		const data = await response.json();
+		const firstName = data.r_userToMDFRequests_user.givenName;
+		const infoEmail = data.r_userToMDFRequests_user.emailAddress;
+
+		fragmentElement.querySelector('#firstName').innerHTML = htmlUtil.escape(
+			firstName
+		);
+		fragmentElement.querySelector('#infoEmail').innerHTML = htmlUtil.escape(
+			infoEmail
+		);
+
+		return;
+	}
+
+	Liferay.Util.openToast({
+		message: 'An unexpected error occured.',
+		type: 'danger',
+	});
+};
+
+getContactInfo();
