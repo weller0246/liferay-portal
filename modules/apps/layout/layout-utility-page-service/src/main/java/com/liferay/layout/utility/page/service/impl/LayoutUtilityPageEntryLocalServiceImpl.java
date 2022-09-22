205a4c1d89efeff17fb578a332d1848571288b21
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-09-22 18:31:01 +0200

    LPS-163798 Adds the related layout when we are adding a new utility page

--- modules/apps/layout/layout-utility-page-service/src/main/java/com/liferay/layout/utility/page/service/impl/LayoutUtilityPageEntryLocalServiceImpl.java
+++ modules/apps/layout/layout-utility-page-service/src/main/java/com/liferay/layout/utility/page/service/impl/LayoutUtilityPageEntryLocalServiceImpl.java
@@ -19,18 +19,33 @@ import com.liferay.layout.utility.page.exception.LayoutUtilityPageEntryNameExcep
 import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
 import com.liferay.layout.utility.page.service.base.LayoutUtilityPageEntryLocalServiceBaseImpl;
 import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.NoSuchLayoutException;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.ColorScheme;
 import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutSet;
 import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.LayoutSetLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.service.ThemeLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -68,6 +83,15 @@ public class LayoutUtilityPageEntryLocalServiceImpl
 		layoutUtilityPageEntry.setUserName(user.getFullName());
 
 		layoutUtilityPageEntry.setExternalReferenceCode(externalReferenceCode);
+
+		Layout layout = _addLayout(
+			userId, groupId, name, type, 0, WorkflowConstants.STATUS_APPROVED,
+			ServiceContextThreadLocal.getServiceContext());
+
+		if (layout != null) {
+			plid = layout.getPlid();
+		}
+
 		layoutUtilityPageEntry.setPlid(plid);
 		layoutUtilityPageEntry.setName(name);
 		layoutUtilityPageEntry.setType(type);
@@ -161,6 +185,83 @@ public class LayoutUtilityPageEntryLocalServiceImpl
 		return layoutUtilityPageEntryPersistence.update(layoutUtilityPageEntry);
 	}
 
+	private Layout _addLayout(
+			long userId, long groupId, String name, int type,
+			long masterLayoutPlid, int status, ServiceContext serviceContext)
+		throws PortalException {
+
+		Map<Locale, String> titleMap = Collections.singletonMap(
+			LocaleUtil.getSiteDefault(), name);
+
+		UnicodeProperties typeSettingsUnicodeProperties =
+			new UnicodeProperties();
+
+		if (status == WorkflowConstants.STATUS_APPROVED) {
+			typeSettingsUnicodeProperties.put("published", "true");
+		}
+
+		if (masterLayoutPlid > 0) {
+			typeSettingsUnicodeProperties.setProperty(
+				"lfr-theme:regular:show-footer", Boolean.FALSE.toString());
+			typeSettingsUnicodeProperties.setProperty(
+				"lfr-theme:regular:show-header", Boolean.FALSE.toString());
+			typeSettingsUnicodeProperties.setProperty(
+				"lfr-theme:regular:show-header-search",
+				Boolean.FALSE.toString());
+			typeSettingsUnicodeProperties.setProperty(
+				"lfr-theme:regular:wrap-widget-page-content",
+				Boolean.FALSE.toString());
+		}
+
+		String typeSettings = typeSettingsUnicodeProperties.toString();
+
+		serviceContext.setAttribute(
+			"layout.instanceable.allowed", Boolean.TRUE);
+		serviceContext.setAttribute("layout.page.template.entry.type", type);
+
+		Layout layout = _layoutLocalService.addLayout(
+			userId, groupId, true, 0, 0, 0, titleMap, titleMap, null, null,
+			null, LayoutConstants.TYPE_CONTENT, typeSettings, true, true,
+			new HashMap<>(), masterLayoutPlid, serviceContext);
+
+		Layout draftLayout = layout.fetchDraftLayout();
+
+		if (masterLayoutPlid > 0) {
+			LayoutSet layoutSet = _layoutSetLocalService.getLayoutSet(
+				groupId, false);
+
+			String themeId = layoutSet.getThemeId();
+
+			String colorSchemeId = _getColorSchemeId(
+				layout.getCompanyId(), themeId);
+
+			draftLayout = _layoutLocalService.updateLookAndFeel(
+				groupId, true, draftLayout.getLayoutId(), themeId,
+				colorSchemeId, StringPool.BLANK);
+
+			layout = _layoutLocalService.updateLookAndFeel(
+				groupId, true, layout.getLayoutId(), themeId, colorSchemeId,
+				StringPool.BLANK);
+		}
+
+		if (status == WorkflowConstants.STATUS_DRAFT) {
+			_layoutLocalService.updateStatus(
+				userId, draftLayout.getPlid(), status, serviceContext);
+
+			layout = _layoutLocalService.updateStatus(
+				userId, layout.getPlid(), status, serviceContext);
+		}
+
+		return layout;
+	}
+
+	private String _getColorSchemeId(long companyId, String themeId) {
+		ColorScheme colorScheme = _themeLocalService.getColorScheme(
+			companyId, themeId, StringPool.BLANK);
+
+		return colorScheme.getColorSchemeId();
+	}
+
 	private void _validateExternalReferenceCode(
 			String externalReferenceCode, long groupId)
 		throws PortalException {
@@ -215,6 +316,12 @@ public class LayoutUtilityPageEntryLocalServiceImpl
 
 	@Reference
 	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutSetLocalService _layoutSetLocalService;
+
+	@Reference
+	private ThemeLocalService _themeLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
