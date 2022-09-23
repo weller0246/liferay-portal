49e076f15c6d4486ec38f52d37cae621a5f9d620
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-09-23 16:47:25 +0200

    LRAC-11869 analytics-settings-rest-impl Add Configuration managment component

---
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/manager/AnalyticsSettingsManager.java
@@ -0,0 +1,177 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.rest.internal.manager;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.analytics.settings.configuration.AnalyticsConfiguration;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.settings.SettingsDescriptor;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.io.IOException;
+import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import org.osgi.framework.InvalidSyntaxException;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@Component(immediate = true, service = AnalyticsSettingsManager.class)
+public class AnalyticsSettingsManager {
+
+	public void deleteCompanyConfiguration(long companyId)
+		throws ConfigurationException {
+
+		_configurationProvider.deleteCompanyConfiguration(
+			AnalyticsConfiguration.class, companyId);
+	}
+
+	public boolean isAnalyticsEnabled(long companyId) throws Exception {
+		AnalyticsConfiguration analyticsConfiguration =
+			_configurationProvider.getCompanyConfiguration(
+				AnalyticsConfiguration.class, companyId);
+
+		if (Validator.isNull(
+				analyticsConfiguration.liferayAnalyticsDataSourceId()) ||
+			Validator.isNull(
+				analyticsConfiguration.
+					liferayAnalyticsFaroBackendSecuritySignature()) ||
+			Validator.isNull(
+				analyticsConfiguration.liferayAnalyticsFaroBackendURL())) {
+
+			return false;
+		}
+
+		return true;
+	}
+
+	public void updateCompanyConfiguration(
+			long companyId, Map<String, Object> properties)
+		throws Exception {
+
+		SettingsDescriptor settingsDescriptor =
+			_settingsFactory.getSettingsDescriptor(_getConfigurationPid());
+
+		Set<String> allKeys = settingsDescriptor.getAllKeys();
+
+		Configuration configuration = _getFactoryConfiguration(
+			_getConfigurationPid(), ExtendedObjectClassDefinition.Scope.COMPANY,
+			companyId);
+
+		Map<String, Object> configurationProperties = new HashMap<>();
+
+		if (configuration != null) {
+			configurationProperties = _toMap(configuration.getProperties());
+		}
+
+		for (Map.Entry<String, Object> entry : properties.entrySet()) {
+			if (allKeys.contains(entry.getKey())) {
+				configurationProperties.put(entry.getKey(), entry.getValue());
+			}
+		}
+
+		_configurationProvider.saveCompanyConfiguration(
+			AnalyticsConfiguration.class, companyId,
+			_toDictionary(configurationProperties));
+	}
+
+	private String _getConfigurationPid() {
+		Class<?> clazz = AnalyticsConfiguration.class;
+
+		Meta.OCD ocd = clazz.getAnnotation(Meta.OCD.class);
+
+		return ocd.id();
+	}
+
+	private Configuration _getFactoryConfiguration(
+			String factoryPid, ExtendedObjectClassDefinition.Scope scope,
+			Serializable scopePK)
+		throws Exception {
+
+		try {
+			String filterString = StringBundler.concat(
+				"(&(service.factoryPid=", factoryPid, ".scoped)(",
+				scope.getPropertyKey(), "=", scopePK, "))");
+
+			Configuration[] configurations =
+				_configurationAdmin.listConfigurations(filterString);
+
+			if (configurations != null) {
+				return configurations[0];
+			}
+
+			return null;
+		}
+		catch (InvalidSyntaxException | IOException exception) {
+			_log.error(exception);
+
+			throw new ConfigurationException(
+				"Unable to retrieve factory configuration " + factoryPid,
+				exception);
+		}
+	}
+
+	private Dictionary<String, Object> _toDictionary(Map<String, Object> map) {
+		return new HashMapDictionary<>(map);
+	}
+
+	private Map<String, Object> _toMap(Dictionary<String, Object> dictionary) {
+		if (dictionary == null) {
+			return Collections.emptyMap();
+		}
+
+		List<String> keys = Collections.list(dictionary.keys());
+
+		Stream<String> stream = keys.stream();
+
+		return stream.collect(
+			Collectors.toMap(Function.identity(), dictionary::get));
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AnalyticsSettingsManager.class);
+
+	@Reference
+	private ConfigurationAdmin _configurationAdmin;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private SettingsFactory _settingsFactory;
+
+}
