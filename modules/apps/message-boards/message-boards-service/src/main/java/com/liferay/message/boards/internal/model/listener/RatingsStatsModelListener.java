8fb6d4fd6ce79a0d4f21c0e8f5a772f0a81988e9
Felipe Veloso <felipe.veloso@liferay.com>
2022-09-23 14:52:49 -0300

    LPS-163838 create RatingStatsModelListener to reindex threads

---
+++ modules/apps/message-boards/message-boards-service/src/main/java/com/liferay/message/boards/internal/model/listener/RatingsStatsModelListener.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.message.boards.internal.model.listener;
+
+import com.liferay.message.boards.model.MBCategory;
+import com.liferay.message.boards.model.MBMessage;
+import com.liferay.message.boards.model.MBThread;
+import com.liferay.message.boards.service.MBThreadLocalService;
+import com.liferay.message.boards.service.MBMessageLocalService;
+import com.liferay.portal.kernel.exception.ModelListenerException;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.ratings.kernel.model.RatingsStats;
+import com.liferay.staging.model.listener.StagingModelListener;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+
+/**
+ * @author Felipe Veloso
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class RatingsStatsModelListener  extends BaseModelListener<RatingsStats> {
+
+
+	@Override
+	public void onAfterCreate(RatingsStats ratingsStats)
+		throws ModelListenerException {
+
+		MBMessage mbMessage = _mbMessageLocalService.fetchMBMessage(
+			ratingsStats.getClassPK());
+
+		Indexer<MBMessage> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+			MBMessage.class);
+
+		try {
+			indexer.reindex(mbMessage);
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+
+		_stagingModelListener.onAfterCreate(ratingsStats);
+	}
+
+	@Override
+	public void onAfterUpdate(RatingsStats originalRatingsStats, RatingsStats ratingsStats)
+		throws ModelListenerException {
+
+		MBMessage mbMessage = _mbMessageLocalService.fetchMBMessage(
+			ratingsStats.getClassPK());
+
+		Indexer<MBMessage> indexer = IndexerRegistryUtil.nullSafeGetIndexer(
+			MBMessage.class);
+
+		try {
+			indexer.reindex(mbMessage);
+		}
+		catch (Exception exception) {
+			throw new RuntimeException(exception);
+		}
+
+	}
+
+	@Reference
+	private StagingModelListener<RatingsStats> _stagingModelListener;
+
+	@Reference
+	private MBMessageLocalService _mbMessageLocalService;
+
+	@Reference
+	private MBThreadLocalService _mbThreadLocalService;
+}
