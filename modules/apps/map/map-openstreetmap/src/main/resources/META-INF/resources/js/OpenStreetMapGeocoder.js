daa4fd8787c3a1ab457e6d28363c6d248eaca4da
Krešimir Čoko <kresimir.coko@liferay.com>
2022-09-23 10:47:21 +0200

    LPS-159765 Replace usage of AUI.jsonp with our fetch

--- modules/apps/map/map-openstreetmap/src/main/resources/META-INF/resources/js/OpenStreetMapGeocoder.js
+++ modules/apps/map/map-openstreetmap/src/main/resources/META-INF/resources/js/OpenStreetMapGeocoder.js
@@ -12,7 +12,9 @@
  * details.
  */
 
-const OPEN_STREET_MAP_URL = '//nominatim.openstreetmap.org/';
+import {fetch} from 'frontend-js-web';
+
+const OPEN_STREET_MAP_URL = 'https://nominatim.openstreetmap.org/';
 
 /**
  * OpenStreetMapGeocoder
@@ -26,7 +28,7 @@ export default class OpenStreetMapGeocoder {
 	 * @protected
 	 * @review
 	 */
-	_handleForwardJSONP(response, callback) {
+	_handleForward(response, callback) {
 		callback(response);
 	}
 
@@ -37,13 +39,15 @@ export default class OpenStreetMapGeocoder {
 	 * @protected
 	 * @review
 	 */
-	_handleReverseJSONP({display_name, error, lat, lon}, callback) {
+	_handleReverse(response, callback) {
+		const {display_name, error, lat, lon} = response;
+
 		const result = {
 			data: {},
 			error,
 		};
 
-		if (!result.err) {
+		if (!result.error) {
 			result.data = {
 				address: display_name,
 				location: {
@@ -63,19 +67,14 @@ export default class OpenStreetMapGeocoder {
 	 * @review
 	 */
 	forward(query, callback) {
-		AUI().use('jsonp', (A) => {
-			const forwardUrl = OpenStreetMapGeocoder.TPL_FORWARD_GEOCODING_URL.replace(
-				'{query}',
-				query
-			);
+		const forwardURL = OpenStreetMapGeocoder.TPL_FORWARD_GEOCODING_URL.replace(
+			'{query}',
+			query
+		);
 
-			A.jsonp(forwardUrl, {
-				context: this,
-				on: {
-					success: A.rbind('_handleForwardJSONP', this, callback),
-				},
-			});
-		});
+		fetch(forwardURL)
+			.then((response) => response.json())
+			.then((response) => this._handleForward(response, callback));
 	}
 
 	/**
@@ -85,19 +84,14 @@ export default class OpenStreetMapGeocoder {
 	 * @review
 	 */
 	reverse(location, callback) {
-		AUI().use('jsonp', (A) => {
-			const reverseUrl = OpenStreetMapGeocoder.TPL_REVERSE_GEOCODING_URL.replace(
-				'{lat}',
-				location.lat
-			).replace('{lng}', location.lng);
+		const reverseURL = OpenStreetMapGeocoder.TPL_REVERSE_GEOCODING_URL.replace(
+			'{lat}',
+			location.lat
+		).replace('{lng}', location.lng);
 
-			A.jsonp(reverseUrl, {
-				context: this,
-				on: {
-					success: A.rbind('_handleReverseJSONP', this, callback),
-				},
-			});
-		});
+		fetch(reverseURL)
+			.then((response) => response.json())
+			.then((response) => this._handleReverse(response, callback));
 	}
 }
 
@@ -107,7 +101,7 @@ export default class OpenStreetMapGeocoder {
  * @see OpenStreetMapGeocoder.forward()
  * @type {string}
  */
-OpenStreetMapGeocoder.TPL_FORWARD_GEOCODING_URL = `${OPEN_STREET_MAP_URL}search?format=json&json_callback={callback}&q={query}`;
+OpenStreetMapGeocoder.TPL_FORWARD_GEOCODING_URL = `${OPEN_STREET_MAP_URL}search?q={query}&format=jsonv2`;
 
 /**
  * Url template used for OpenStreetMapGeocoder.reverse() method
@@ -115,4 +109,4 @@ OpenStreetMapGeocoder.TPL_FORWARD_GEOCODING_URL = `${OPEN_STREET_MAP_URL}search?
  * @see OpenStreetMapGeocoder.reverse()
  * @type {string}
  */
-OpenStreetMapGeocoder.TPL_REVERSE_GEOCODING_URL = `${OPEN_STREET_MAP_URL}reverse?format=json&json_callback={callback}&lat={lat}&lon={lng}`;
+OpenStreetMapGeocoder.TPL_REVERSE_GEOCODING_URL = `${OPEN_STREET_MAP_URL}reverse?lat={lat}&lon={lng}&format=jsonv2`;
