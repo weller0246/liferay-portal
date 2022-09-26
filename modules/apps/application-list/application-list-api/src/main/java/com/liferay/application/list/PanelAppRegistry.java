659f603cd0a8cdd079639c3e75ed3de989f90b8d
Janis Zhang <janis.zhang@liferay.com>
2022-09-26 11:59:03 +0800

    LPS-164057 Remove unused reference fields,its usages has been deleted in 05b22e84f7b927f5c0e88fbe69e64559e7815661

--- modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/PanelAppRegistry.java
+++ modules/apps/application-list/application-list-api/src/main/java/com/liferay/application/list/PanelAppRegistry.java
@@ -27,14 +27,10 @@ import com.liferay.portal.kernel.model.CompanyConstants;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.portlet.PortletPreferencesFactory;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.service.PortletLocalService;
-import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
-import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.PrefsProps;
 
 import java.io.Serializable;
 
@@ -209,18 +205,6 @@ public class PanelAppRegistry {
 
 	@Reference
 	private PortletLocalService _portletLocalService;
-
-	@Reference
-	private PortletPreferencesFactory _portletPreferencesFactory;
-
-	@Reference
-	private PrefsProps _prefsProps;
-
-	@Reference
-	private ResourcePermissionLocalService _resourcePermissionLocalService;
-
-	@Reference
-	private RoleLocalService _roleLocalService;
 
 	private ServiceTrackerMap<String, List<PanelApp>> _serviceTrackerMap;
 
