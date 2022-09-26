bb4fb30f48af44fde1a159eeccd9d71ae2ba66dc
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-09-26 18:37:57 +0200

    LPS-162398 Add an upgrade process in order to complete the urlTitle in those global article versions that have an empty urlTitle

---
+++ modules/apps/journal/journal-service/src/main/java/com/liferay/journal/internal/upgrade/v4_4_0/GlobalJournalArticleUrlTitleUpgradeProcess.java
@@ -0,0 +1,128 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.internal.upgrade.v4_4_0;
+
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.LoggingTimer;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class GlobalJournalArticleUrlTitleUpgradeProcess extends UpgradeProcess {
+
+	public GlobalJournalArticleUrlTitleUpgradeProcess(
+		ClassNameLocalService classNameLocalService,
+		CompanyLocalService companyLocalService,
+		GroupLocalService groupLocalService) {
+
+		_classNameLocalService = classNameLocalService;
+		_companyLocalService = companyLocalService;
+		_groupLocalService = groupLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_updateGlobalJournalArticles();
+	}
+
+	private String _getUrlTitle(
+			long classNameId, long classPK, String languageId, long groupId)
+		throws Exception {
+
+		try (PreparedStatement preparedStatement = connection.prepareStatement(
+				"select urlTitle from FriendlyURLEntryLocalization where " +
+					"classNameId = ? and classPK = ? and groupId = ? and " +
+						"languageId = ?")) {
+
+			preparedStatement.setLong(1, classNameId);
+			preparedStatement.setLong(2, classPK);
+			preparedStatement.setLong(3, groupId);
+			preparedStatement.setString(4, languageId);
+
+			try (ResultSet resultSet = preparedStatement.executeQuery()) {
+				if (resultSet.next()) {
+					return resultSet.getString("urlTitle");
+				}
+			}
+		}
+
+		return StringPool.BLANK;
+	}
+
+	private void _updateGlobalJournalArticles() throws Exception {
+		try (LoggingTimer loggingTimer = new LoggingTimer()) {
+			long classNameId = _classNameLocalService.getClassNameId(
+				JournalArticle.class.getName());
+
+			_companyLocalService.forEachCompanyId(
+				companyId -> _updateGlobalJournalArticles(
+					classNameId, companyId));
+		}
+	}
+
+	private void _updateGlobalJournalArticles(long classNameId, long companyId)
+		throws Exception {
+
+		Group companyGroup = _groupLocalService.getCompanyGroup(companyId);
+
+		try (PreparedStatement preparedStatement1 = connection.prepareStatement(
+				"select defaultLanguageId, id_, resourcePrimKey from " +
+					"JournalArticle where groupId = ? and (urlTitle is null " +
+						"or urlTitle = '')");
+			PreparedStatement preparedStatement2 =
+				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+					connection,
+					"update JournalArticle set urlTitle = ? where id_ = ?")) {
+
+			preparedStatement1.setLong(1, companyGroup.getGroupId());
+
+			try (ResultSet resultSet = preparedStatement1.executeQuery()) {
+				while (resultSet.next()) {
+					String defaultLanguageId = resultSet.getString(
+						"defaultLanguageId");
+					long id = resultSet.getLong("id_");
+					long classPK = resultSet.getLong("resourcePrimKey");
+
+					preparedStatement2.setString(
+						1,
+						_getUrlTitle(
+							classNameId, classPK, defaultLanguageId,
+							companyGroup.getGroupId()));
+
+					preparedStatement2.setLong(2, id);
+
+					preparedStatement2.addBatch();
+				}
+
+				preparedStatement2.executeBatch();
+			}
+		}
+	}
+
+	private final ClassNameLocalService _classNameLocalService;
+	private final CompanyLocalService _companyLocalService;
+	private final GroupLocalService _groupLocalService;
+
+}
