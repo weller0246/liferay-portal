c8235395bdafca6aecce0a71c1fd77a75251cbbc
Alan Huang <alan.huang@liferay.com>
2022-09-27 14:23:01 +0800

    LPS-164044 Add XMLServiceAutoImportDefaultReferencesCheck

---
+++ modules/util/source-formatter/src/main/java/com/liferay/source/formatter/check/XMLServiceAutoImportDefaultReferencesCheck.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.check;
+
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.source.formatter.check.util.SourceUtil;
+
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+
+/**
+ * @author Alan Huang
+ */
+public class XMLServiceAutoImportDefaultReferencesCheck extends BaseFileCheck {
+
+	@Override
+	public boolean isModuleSourceCheck() {
+		return true;
+	}
+
+	@Override
+	protected String doProcess(
+			String fileName, String absolutePath, String content)
+		throws DocumentException {
+
+		if (!fileName.endsWith("/service.xml")) {
+			return content;
+		}
+
+		Document document = SourceUtil.readXML(content);
+
+		Element rootElement = document.getRootElement();
+
+		String autoImportDefaultReferences = rootElement.attributeValue(
+			"auto-import-default-references");
+
+		if (Validator.isNull(autoImportDefaultReferences) ||
+			!autoImportDefaultReferences.equals("false")) {
+
+			addMessage(
+				fileName,
+				"Attribute 'auto-import-default-references' should always be " +
+					"'false' in service.xml");
+		}
+
+		return content;
+	}
+
+}
