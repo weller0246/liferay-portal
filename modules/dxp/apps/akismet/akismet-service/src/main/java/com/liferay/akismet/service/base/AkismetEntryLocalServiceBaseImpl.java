4ccdc2ce34d967e4997367178f7f9e9f2ec8ce7f
Janis Zhang <janis.zhang@liferay.com>
2022-09-28 08:18:33 +0800

    LPS-164189 Auto generated

--- modules/dxp/apps/akismet/akismet-service/src/main/java/com/liferay/akismet/service/base/AkismetEntryLocalServiceBaseImpl.java
+++ modules/dxp/apps/akismet/akismet-service/src/main/java/com/liferay/akismet/service/base/AkismetEntryLocalServiceBaseImpl.java
@@ -473,18 +473,6 @@ public abstract class AkismetEntryLocalServiceBaseImpl
 	protected com.liferay.counter.kernel.service.CounterLocalService
 		counterLocalService;
 
-	@Reference
-	protected com.liferay.portal.kernel.service.ClassNameLocalService
-		classNameLocalService;
-
-	@Reference
-	protected com.liferay.portal.kernel.service.ResourceLocalService
-		resourceLocalService;
-
-	@Reference
-	protected com.liferay.portal.kernel.service.UserLocalService
-		userLocalService;
-
 	private static final Log _log = LogFactoryUtil.getLog(
 		AkismetEntryLocalServiceBaseImpl.class);
 
