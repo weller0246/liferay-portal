ec9a431c4c285a7d6b27888c92fe101d2a7fa813
Alessio Rendina <asilcomando@gmail.com>
2022-09-28 10:29:44 +0200

    COMMERCE-10013 Added product attachment file entry info item permission provider

---
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/info/item/provider/CPAttachmentFileEntryInfoItemPermissionProvider.java
@@ -0,0 +1,130 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.web.internal.info.item.provider;
+
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.context.CommerceContextThreadLocal;
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.permission.CommerceProductViewPermission;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryLocalService;
+import com.liferay.commerce.util.CommerceUtil;
+import com.liferay.info.exception.InfoItemPermissionException;
+import com.liferay.info.item.ClassPKInfoItemIdentifier;
+import com.liferay.info.item.InfoItemIdentifier;
+import com.liferay.info.item.InfoItemReference;
+import com.liferay.info.item.provider.InfoItemPermissionProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.util.Portal;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(enabled = false, service = InfoItemPermissionProvider.class)
+public class CPAttachmentFileEntryInfoItemPermissionProvider
+	implements InfoItemPermissionProvider<CPAttachmentFileEntry> {
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker,
+			CPAttachmentFileEntry cpAttachmentFileEntry, String actionId)
+		throws InfoItemPermissionException {
+
+		if (_portal.getClassNameId(CPDefinition.class.getName()) !=
+				cpAttachmentFileEntry.getClassNameId()) {
+
+			return false;
+		}
+
+		CommerceContext commerceContext = CommerceContextThreadLocal.get();
+
+		if (commerceContext == null) {
+			return false;
+		}
+
+		try {
+			return _commerceProductViewPermission.contains(
+				permissionChecker,
+				CommerceUtil.getCommerceAccountId(commerceContext),
+				commerceContext.getCommerceChannelGroupId(),
+				cpAttachmentFileEntry.getClassPK());
+		}
+		catch (PortalException portalException) {
+			throw new InfoItemPermissionException(
+				cpAttachmentFileEntry.getCPAttachmentFileEntryId(),
+				portalException);
+		}
+	}
+
+	@Override
+	public boolean hasPermission(
+			PermissionChecker permissionChecker,
+			InfoItemReference infoItemReference, String actionId)
+		throws InfoItemPermissionException {
+
+		CommerceContext commerceContext = CommerceContextThreadLocal.get();
+
+		if (commerceContext == null) {
+			return false;
+		}
+
+		InfoItemIdentifier infoItemIdentifier =
+			infoItemReference.getInfoItemIdentifier();
+
+		if (!(infoItemIdentifier instanceof ClassPKInfoItemIdentifier)) {
+			return false;
+		}
+
+		ClassPKInfoItemIdentifier classPKInfoItemIdentifier =
+			(ClassPKInfoItemIdentifier)infoItemIdentifier;
+
+		try {
+			CPAttachmentFileEntry cpAttachmentFileEntry =
+				_cpAttachmentFileEntryLocalService.getCPAttachmentFileEntry(
+					classPKInfoItemIdentifier.getClassPK());
+
+			if (_portal.getClassNameId(CPDefinition.class.getName()) !=
+					cpAttachmentFileEntry.getClassNameId()) {
+
+				return false;
+			}
+
+			return _commerceProductViewPermission.contains(
+				permissionChecker,
+				CommerceUtil.getCommerceAccountId(commerceContext),
+				commerceContext.getCommerceChannelGroupId(),
+				cpAttachmentFileEntry.getClassPK());
+		}
+		catch (PortalException portalException) {
+			throw new InfoItemPermissionException(
+				classPKInfoItemIdentifier.getClassPK(), portalException);
+		}
+	}
+
+	@Reference
+	private CommerceProductViewPermission _commerceProductViewPermission;
+
+	@Reference
+	private CPAttachmentFileEntryLocalService
+		_cpAttachmentFileEntryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
