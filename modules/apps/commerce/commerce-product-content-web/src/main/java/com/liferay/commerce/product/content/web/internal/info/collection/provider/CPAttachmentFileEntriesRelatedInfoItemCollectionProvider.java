1dd036dd179d23846ccb878073e6c06feb727647
Alessio Rendina <asilcomando@gmail.com>
2022-09-28 09:13:57 +0200

    COMMERCE-10013 Added product attachments related info item collection

---
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/info/collection/provider/CPAttachmentFileEntriesRelatedInfoItemCollectionProvider.java
@@ -0,0 +1,115 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.content.web.internal.info.collection.provider;
+
+import com.liferay.commerce.product.constants.CPAttachmentFileEntryConstants;
+import com.liferay.commerce.product.model.CPAttachmentFileEntry;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPAttachmentFileEntryLocalService;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.info.collection.provider.CollectionQuery;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
+import com.liferay.info.pagination.InfoPage;
+import com.liferay.info.pagination.Pagination;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	enabled = false, immediate = true,
+	service = RelatedInfoItemCollectionProvider.class
+)
+public class CPAttachmentFileEntriesRelatedInfoItemCollectionProvider
+	implements RelatedInfoItemCollectionProvider
+		<CPDefinition, CPAttachmentFileEntry> {
+
+	@Override
+	public InfoPage<CPAttachmentFileEntry> getCollectionInfoPage(
+		CollectionQuery collectionQuery) {
+
+		Optional<Object> relatedItemOptional =
+			collectionQuery.getRelatedItemObjectOptional();
+
+		Object relatedItem = relatedItemOptional.orElse(null);
+
+		Pagination pagination = collectionQuery.getPagination();
+
+		if (!(relatedItem instanceof CPDefinition)) {
+			return InfoPage.of(Collections.emptyList(), pagination, 0);
+		}
+
+		CPDefinition cpDefinition = (CPDefinition)relatedItem;
+
+		try {
+			List<CPAttachmentFileEntry> cpAttachmentFileEntries =
+				_cpAttachmentFileEntryLocalService.getCPAttachmentFileEntries(
+					_portal.getClassNameId(CPDefinition.class.getName()),
+					cpDefinition.getCPDefinitionId(),
+					CPAttachmentFileEntryConstants.TYPE_OTHER,
+					WorkflowConstants.STATUS_APPROVED, QueryUtil.ALL_POS,
+					QueryUtil.ALL_POS);
+
+			if (!cpAttachmentFileEntries.isEmpty()) {
+				return InfoPage.of(
+					ListUtil.subList(
+						cpAttachmentFileEntries, pagination.getStart(),
+						pagination.getEnd()),
+					pagination, cpAttachmentFileEntries.size());
+			}
+		}
+		catch (Exception exception) {
+			_log.error(exception);
+		}
+
+		return InfoPage.of(Collections.emptyList(), pagination, 0);
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "product-attachments");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPAttachmentFileEntriesRelatedInfoItemCollectionProvider.class);
+
+	@Reference
+	private CPAttachmentFileEntryLocalService
+		_cpAttachmentFileEntryLocalService;
+
+	@Reference
+	private CPDefinitionService _cpDefinitionService;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+}
