128153479c3d6a2cac53640ec64b1b6aae8c1a80
Lourdes Fernandez Besada <lourdes.fernandez@gmail.com>
2022-09-28 13:10:38 +0200

    LPS-164263 Add a new interface to check the info permission

---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/exception/InfoPermissionException.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public class InfoPermissionException extends PortalException {
+
+	public InfoPermissionException(String className, Throwable throwable) {
+		super("Unable to check permission for " + className, throwable);
+
+		_className = className;
+	}
+
+	public String getClassName() {
+		return _className;
+	}
+
+	private final String _className;
+
+}
---
+++ modules/apps/info/info-api/src/main/java/com/liferay/info/permission/provider/InfoPermissionProvider.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.permission.provider;
+
+import com.liferay.info.exception.InfoPermissionException;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+public interface InfoPermissionProvider<T> {
+
+	public boolean hasViewPermission(PermissionChecker permissionChecker)
+		throws InfoPermissionException;
+
+}
