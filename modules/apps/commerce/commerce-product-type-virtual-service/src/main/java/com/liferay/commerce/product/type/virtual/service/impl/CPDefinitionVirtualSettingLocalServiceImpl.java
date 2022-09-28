5d495b39da99064e0620a7c6a26b241e686b833a
Lily.Chi <lily.chi@liferay.com>
2022-09-28 15:39:00 +0800

    LPS-164215 Fix missing references manually

--- modules/apps/commerce/commerce-product-type-virtual-service/src/main/java/com/liferay/commerce/product/type/virtual/service/impl/CPDefinitionVirtualSettingLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-type-virtual-service/src/main/java/com/liferay/commerce/product/type/virtual/service/impl/CPDefinitionVirtualSettingLocalServiceImpl.java
@@ -37,7 +37,9 @@ import com.liferay.journal.service.JournalArticleLocalService;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.uuid.PortalUUID;
@@ -72,7 +74,7 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 			ServiceContext serviceContext)
 		throws PortalException {
 
-		User user = userLocalService.getUser(serviceContext.getUserId());
+		User user = _userLocalService.getUser(serviceContext.getUserId());
 		long groupId = serviceContext.getScopeGroupId();
 
 		if (Validator.isNotNull(url)) {
@@ -221,7 +223,7 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 			String className, long classPK)
 		throws PortalException {
 
-		long classNameId = classNameLocalService.getClassNameId(className);
+		long classNameId = _classNameLocalService.getClassNameId(className);
 
 		CPDefinitionVirtualSetting cpDefinitionVirtualSetting =
 			cpDefinitionVirtualSettingPersistence.fetchByC_C(
@@ -272,7 +274,7 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 		String className, long classPK) {
 
 		return cpDefinitionVirtualSettingPersistence.fetchByC_C(
-			classNameLocalService.getClassNameId(className), classPK);
+			_classNameLocalService.getClassNameId(className), classPK);
 	}
 
 	@Override
@@ -281,7 +283,7 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 		throws PortalException {
 
 		return cpDefinitionVirtualSettingPersistence.fetchByC_C(
-			classNameLocalService.getClassNameId(className), classPK);
+			_classNameLocalService.getClassNameId(className), classPK);
 	}
 
 	@Override
@@ -337,9 +339,9 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 			termsOfUseRequired, termsOfUseContentMap,
 			termsOfUseJournalArticleResourcePrimKey);
 
-		long cpDefinitionClassNameId = classNameLocalService.getClassNameId(
+		long cpDefinitionClassNameId = _classNameLocalService.getClassNameId(
 			CPDefinition.class);
-		long cpInstanceClassNameId = classNameLocalService.getClassNameId(
+		long cpInstanceClassNameId = _classNameLocalService.getClassNameId(
 			CPInstance.class);
 
 		if ((cpDefinitionVirtualSetting.getClassNameId() ==
@@ -497,6 +499,9 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 	}
 
 	@Reference
+	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
 	private CPDefinitionLocalService _cpDefinitionLocalService;
 
 	@Reference
@@ -513,5 +518,8 @@ public class CPDefinitionVirtualSettingLocalServiceImpl
 
 	@Reference
 	private PortalUUID _portalUUID;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
