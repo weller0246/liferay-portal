19225e87fb7a12e5e7a72f1c6d042377e534582f
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-09-30 16:44:18 -0300

    LPS-162137 Add StatusSystemObjectFieldFilterStrategy

---
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/field/filter/parser/StatusSystemObjectFieldFilterStrategy.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.field.filter.parser;
+
+import com.liferay.frontend.data.set.filter.FDSFilter;
+import com.liferay.object.exception.ObjectViewFilterColumnException;
+import com.liferay.object.field.frontend.data.set.filter.ObjectEntryStatusSelectionFDSFilter;
+import com.liferay.object.model.ObjectViewFilterColumn;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+/**
+ * @author Feliphe Marinho
+ */
+public class StatusSystemObjectFieldFilterStrategy
+	extends BaseObjectFieldFilterStrategy {
+
+	public StatusSystemObjectFieldFilterStrategy(
+		Language language, Locale locale,
+		ObjectViewFilterColumn objectViewFilterColumn) {
+
+		super(locale, objectViewFilterColumn);
+
+		_language = language;
+	}
+
+	@Override
+	public FDSFilter getFDSFilter() throws PortalException {
+		return new ObjectEntryStatusSelectionFDSFilter(parse());
+	}
+
+	@Override
+	public List<Integer> getItemsValues() throws JSONException {
+		JSONArray jsonArray = getJSONArray();
+
+		List<Integer> statuses = new ArrayList<>();
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			statuses.add((Integer)jsonArray.get(i));
+		}
+
+		return statuses;
+	}
+
+	@Override
+	public String toValueSummary() throws PortalException {
+		return StringUtil.merge(
+			ListUtil.toList(
+				getItemsValues(),
+				itemValue -> _language.get(
+					locale, WorkflowConstants.getStatusLabel(itemValue))),
+			StringPool.COMMA_AND_SPACE);
+	}
+
+	@Override
+	public void validate() throws PortalException {
+		super.validate();
+
+		try {
+			getItemsValues();
+		}
+		catch (Exception exception) {
+			throw new ObjectViewFilterColumnException(
+				"JSON array is invalid for filter type " +
+					objectViewFilterColumn.getFilterType(),
+				exception);
+		}
+	}
+
+	private final Language _language;
+
+}
