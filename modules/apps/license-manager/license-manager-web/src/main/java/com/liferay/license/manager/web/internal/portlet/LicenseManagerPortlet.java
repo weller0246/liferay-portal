aa6b0aa616dbdb1126ffcf048f0e7061f03075d8
Alan Huang <alan.huang@liferay.com>
2022-09-30 14:22:10 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/license-manager/license-manager-web/src/main/java/com/liferay/license/manager/web/internal/portlet/LicenseManagerPortlet.java
+++ modules/apps/license-manager/license-manager-web/src/main/java/com/liferay/license/manager/web/internal/portlet/LicenseManagerPortlet.java
@@ -42,7 +42,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + LicenseManagerPortletKeys.LICENSE_MANAGER,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=administrator"
+		"javax.portlet.security-role-ref=administrator",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
