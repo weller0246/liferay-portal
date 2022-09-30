363d38f577d2c8d384b1315b8244bc4cf7df4194
Alan Huang <alan.huang@liferay.com>
2022-09-30 14:22:19 +0800

    LPS-147681 Add javax.portlet.version=3.0

--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/FastLoginPortlet.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/FastLoginPortlet.java
@@ -49,7 +49,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.name=" + LoginPortletKeys.FAST_LOGIN,
 		"javax.portlet.portlet-mode=text/html;config",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=guest,power-user,user"
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
--- modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/LoginPortlet.java
+++ modules/apps/login/login-web/src/main/java/com/liferay/login/web/internal/portlet/LoginPortlet.java
@@ -48,7 +48,8 @@ import org.osgi.service.component.annotations.Reference;
 		"javax.portlet.name=" + LoginPortletKeys.LOGIN,
 		"javax.portlet.portlet-mode=text/html;config",
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=guest,power-user,user"
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
