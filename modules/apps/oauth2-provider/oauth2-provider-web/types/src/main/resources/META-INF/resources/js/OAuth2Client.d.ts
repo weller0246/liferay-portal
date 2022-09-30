3745d1208be75c9e2fd92412bece833d424f5395
Krešimir Čoko <kresimir.coko@liferay.com>
2022-09-30 11:03:10 +0200

    LPS-162074 SF

--- modules/apps/oauth2-provider/oauth2-provider-web/types/src/main/resources/META-INF/resources/js/OAuth2Client.d.ts
+++ modules/apps/oauth2-provider/oauth2-provider-web/types/src/main/resources/META-INF/resources/js/OAuth2Client.d.ts
@@ -11,36 +11,41 @@
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
  * details.
  */
+
 interface IOAuth2ClientFromParametersOptions {
-    authorizeURL?: string;
-    clientId: string;
-    homePageURL: string;
-    redirectURIs?: Array<string>;
-    tokenURL?: string;
+	authorizeURL?: string;
+	clientId: string;
+	homePageURL: string;
+	redirectURIs?: Array<string>;
+	tokenURL?: string;
 }
 interface IOAuth2ClientOptions {
-    authorizeURL: string;
-    clientId: string;
-    encodedRedirectURL: string;
-    homePageURL: string;
-    redirectURIs: Array<string>;
-    tokenURL: string;
+	authorizeURL: string;
+	clientId: string;
+	encodedRedirectURL: string;
+	homePageURL: string;
+	redirectURIs: Array<string>;
+	tokenURL: string;
 }
 declare class OAuth2Client {
-    private authorizeURL;
-    private clientId;
-    private encodedRedirectURL;
-    private homePageURL;
-    private redirectURIs;
-    private tokenURL;
-    constructor(options: IOAuth2ClientOptions);
-    fetch(url: RequestInfo, options?: any): Promise<any>;
-    private _createIframe;
-    private _fetch;
-    private _getOrRequestToken;
-    private _requestTokenSilently;
-    private _requestToken;
+	private authorizeURL;
+	private clientId;
+	private encodedRedirectURL;
+	private homePageURL;
+	private redirectURIs;
+	private tokenURL;
+	constructor(options: IOAuth2ClientOptions);
+	fetch(url: RequestInfo, options?: any): Promise<any>;
+	private _createIframe;
+	private _fetch;
+	private _getOrRequestToken;
+	private _requestTokenSilently;
+	private _requestToken;
 }
-export declare function FromParameters(options: IOAuth2ClientFromParametersOptions): OAuth2Client;
-export declare function FromUserAgentApplication(userAgentApplicationName: string): OAuth2Client;
+export declare function FromParameters(
+	options: IOAuth2ClientFromParametersOptions
+): OAuth2Client;
+export declare function FromUserAgentApplication(
+	userAgentApplicationName: string
+): OAuth2Client;
 export {};
