74084f34c36ac910b8b424fe1367d7e8caf831da
Wellington Barbosa <welitonpe@gmail.com>
2022-09-30 18:18:05 -0300

    LPS-156547 Common and Utils

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/dashboard-collection/common/components/container/index.js
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+import React from 'react';
+
+function Container({children, className, footer, title}) {
+	return (
+		<div
+			className={`bg-neutral-0 d-flex flex-column justify-content-between p-4 rounded shadow-sm ${className}`}
+		>
+			<div>
+				<div className="font-weight-semi-bold h5 m-0">{title}</div>
+
+				<div>
+					<hr className="mb-4 mt-1" />
+				</div>
+			</div>
+
+			<div className="align-items-stretch d-flex flex-column">
+				{children}
+
+				{footer && (
+					<div>
+						<hr className="mb-3 mt-1" />
+
+						<div className="d-flex">{footer}</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+}
+
+export default Container;
---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/dashboard-collection/common/mock/mock.js
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+export const level = {
+	progressNewMoc: Object.entries({
+		achieved: {qtd: 350, total: 'USD $80.000,29'},
+		remaining: {qtd: 100, total: 'USD $5.500,00'},
+	}),
+	progressTotalMoc: Object.entries({
+		achieved: {qtd: 350, total: 'USD $6.500,50'},
+		supplemental: {qtd: 100, total: 'USD $5.500,00'},
+	}),
+	totalLevel: {
+		business: 600,
+		newBusiness: 450,
+	},
+};
+
+export const mdf = {
+	ProgressClain: {
+		approved: {qtd: 120, total: 'USD $6.500,50'},
+		pending: {qtd: 100, total: 'USD $5.500,00'},
+	},
+	ProgressMdf: {
+		approved: {qtd: 300, total: 'USD $80.000,29'},
+		pending: {qtd: 92, total: 'USD $12.993,00'},
+	},
+};
