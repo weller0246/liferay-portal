c219b7bb794f68715846603366d22464ebe2704c
Victor Galan <victor.galan@liferay.com>
2022-10-03 11:45:37 +0200

    LPS-164384 Show display preview when viewing the content in global or in an asset library

---
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/frontend/taglib/form/navigator/JournalDisplayPagePreviewFormNavigatorEntry.java
@@ -0,0 +1,98 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.web.internal.frontend.taglib.form.navigator;
+
+import com.liferay.frontend.taglib.form.navigator.FormNavigatorEntry;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextThreadLocal;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.util.PropsUtil;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Víctor Galán
+ */
+@Component(
+	property = "form.navigator.entry.order:Integer=80",
+	service = FormNavigatorEntry.class
+)
+public class JournalDisplayPagePreviewFormNavigatorEntry
+	extends BaseJournalFormNavigatorEntry {
+
+	@Override
+	public String getKey() {
+		return "display-page-preview";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public boolean isVisible(User user, JournalArticle article) {
+		if (GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-163074")) &&
+			(_isDepotArticle(article) || isGlobalScopeArticle(article))) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Override
+	protected String getJspPath() {
+		return "/article/display_page_preview.jsp";
+	}
+
+	private Group _getGroup(JournalArticle article) {
+		if ((article != null) && (article.getId() > 0)) {
+			return _groupLocalService.fetchGroup(article.getGroupId());
+		}
+
+		ServiceContext serviceContext =
+			ServiceContextThreadLocal.getServiceContext();
+
+		ThemeDisplay themeDisplay = serviceContext.getThemeDisplay();
+
+		return themeDisplay.getScopeGroup();
+	}
+
+	private boolean _isDepotArticle(JournalArticle article) {
+		Group group = _getGroup(article);
+
+		if ((group != null) && group.isDepot()) {
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference(target = "(osgi.web.symbolicname=com.liferay.journal.web)")
+	private ServletContext _servletContext;
+
+}
