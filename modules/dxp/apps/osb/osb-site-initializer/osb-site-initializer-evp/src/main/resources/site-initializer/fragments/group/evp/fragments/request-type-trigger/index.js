d11a4923b7dee3cf89ba8a2a962f5d74dcfca5f9
Bruno Santana <bruno.santana@liferay.com>
2022-10-03 18:08:16 -0300

    LPS-164592 remade the request object

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/request-type-trigger/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/request-type-trigger/index.js
@@ -1,37 +1,53 @@
-let requestType = document.querySelector('[name="requestType"]');
-const serviceForm = document.getElementsByClassName(".service-form");
-const grantForm = document.getElementsByClassName(".grant-form");
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const requestType = document.querySelector('[name="requestType"]');
+const serviceForm = document.getElementsByClassName('.service-form');
+const grantForm = document.getElementsByClassName('.grant-form');
 document.addEventListener('click', handleDocumentClick);
 
+function updateValue(requestType) {
+	if (
+		requestType.value === 'grant' &&
+		grantForm[0].classList.contains('d-none')
+	) {
+		grantForm[0].classList.toggle('d-none');
+		toggleGrantRequired(grantForm[0]);
 
-
-function updateValue(requestType){
-			if (requestType.value == "grant" && grantForm[0].classList.contains("d-none")){
-				grantForm[0].classList.toggle('d-none');
-				toggleGrantRequired(grantForm[0])
-				
-				if(!serviceForm[0].classList.contains("d-none")){
-					serviceForm[0].classList.toggle('d-none');
-					toggleServiceRequired(serviceForm[0])
-				}
-	}
-		if (requestType.value == "service" && serviceForm[0].classList.contains("d-none")) {
+		if (!serviceForm[0].classList.contains('d-none')) {
 			serviceForm[0].classList.toggle('d-none');
-			toggleServiceRequired(serviceForm[0])
-			
-				if(!grantForm[0].classList.contains("d-none")){
-					grantForm[0].classList.toggle('d-none');
-					toggleGrantRequired(grantForm[0])
-				}
+			toggleServiceRequired(serviceForm[0]);
 		}
+	}
+	if (
+		requestType.value === 'service' &&
+		serviceForm[0].classList.contains('d-none')
+	) {
+		serviceForm[0].classList.toggle('d-none');
+		toggleServiceRequired(serviceForm[0]);
+
+		if (!grantForm[0].classList.contains('d-none')) {
+			grantForm[0].classList.toggle('d-none');
+			toggleGrantRequired(grantForm[0]);
+		}
+	}
 }
-function toggleServiceRequired(service){
-	if(service.querySelector('[name="managerEmailAddress"]').required){
+function toggleServiceRequired(service) {
+	if (service.querySelector('[name="managerEmailAddress"]').required) {
 		service.querySelector('[name="managerEmailAddress"]').required = false;
 		service.querySelector('[name="totalHoursRequested"]').required = false;
 		service.querySelector('[name="startDate"]').required = false;
 		service.querySelector('[name="endDate"]').required = false;
-	}else{
+	}
+	else {
 		service.querySelector('[name="managerEmailAddress"]').required = true;
 		service.querySelector('[name="totalHoursRequested"]').required = true;
 		service.querySelector('[name="startDate"]').required = true;
@@ -39,16 +55,15 @@ function toggleServiceRequired(service){
 	}
 }
 
-function toggleGrantRequired(grant){
-	if(grant.querySelector('[name="grantAmount"]').required){
-    grant.querySelector('[name="grantAmount"]').required = false;
-}else{
-    grant.querySelector('[name="grantAmount"]').required = true;
+function toggleGrantRequired(grant) {
+	if (grant.querySelector('[name="grantAmount"]').required) {
+		grant.querySelector('[name="grantAmount"]').required = false;
+	}
+	else {
+		grant.querySelector('[name="grantAmount"]').required = true;
+	}
 }
-
-}
-
 
 function handleDocumentClick() {
-	updateValue(requestType)
+	updateValue(requestType);
 }
