42f572f2a94e8801f54babf35ca0102b5af89c0f
Richard Jeremias <ricmartinsrocha@gmail.com>
2022-10-04 17:57:43 -0300

    LPS-163762 Focusing properly in the current Form

--- modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/core/components/AutoFocus.es.js
+++ modules/apps/data-engine/data-engine-js-components-web/src/main/resources/META-INF/resources/js/core/components/AutoFocus.es.js
@@ -40,7 +40,7 @@ export function AutoFocus({children}) {
 						document.activeElement.querySelector('input') &&
 						document.activeElement.querySelector('input').type ===
 							'hidden') ||
-					containerElement.current.parentNode.className.includes(
+					containerElement?.current.parentNode.className.includes(
 						'ddm-form-builder-app'
 					)
 				) {
@@ -111,15 +111,17 @@ export function AutoFocus({children}) {
 }
 
 function scrollComponentToTop(currentTitle) {
-	const containerPosition = currentTitle.getBoundingClientRect();
+	if (currentTitle) {
+		const containerPosition = currentTitle.getBoundingClientRect();
 
-	const menuSize = document.querySelector('.control-menu-container')
-		?.clientHeight;
+		const menuSize = document.querySelector('.control-menu-container')
+			?.clientHeight;
 
-	window.scroll(
-		containerPosition.x - menuSize,
-		containerPosition.y - menuSize
-	);
+		window.scroll(
+			containerPosition.x - menuSize,
+			containerPosition.y - menuSize
+		);
+	}
 }
 
 function removeTabs() {
