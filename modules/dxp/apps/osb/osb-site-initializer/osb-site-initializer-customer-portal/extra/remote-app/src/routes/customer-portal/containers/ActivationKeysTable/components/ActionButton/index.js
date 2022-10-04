754314437f01dca0419f732b8cde325914d8c378
luisacmn <luisanovaes.arq@gmail.com>
2022-10-04 14:56:03 -0300

    LPS-162239 Fixed download button text

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/components/ActionButton/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-customer-portal/extra/remote-app/src/routes/customer-portal/containers/ActivationKeysTable/components/ActionButton/index.js
@@ -68,7 +68,7 @@ const ActionButton = ({
 		return (
 			<ButtonDropDown
 				items={activationKeysDownloadItems}
-				label={i18n.translate('Download')}
+				label={i18n.translate('download')}
 				menuElementAttrs={{
 					className: 'p-0 cp-drop-down-action-button',
 				}}
@@ -90,7 +90,7 @@ const ActionButton = ({
 					)
 				}
 			>
-				{i18n.translate('Download')}
+				{i18n.translate('download')}
 			</Button>
 		);
 	}
