c7d5ed1bf177bc52d3dba685e6bb0cbcb956b644
Alicia García <alicia.garcia@liferay.com>
2022-10-04 12:28:10 +0200

    LPS-164161 add new exceptions to check the validity of the expiration date and the review date

---
+++ modules/apps/knowledge-base/knowledge-base-api/src/main/java/com/liferay/knowledge/base/exception/KBArticleReviewDateException.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.exception;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Alicia García
+ */
+public class KBArticleReviewDateException extends PortalException {
+
+	public KBArticleReviewDateException() {
+	}
+
+	public KBArticleReviewDateException(String msg) {
+		super(msg);
+	}
+
+	public KBArticleReviewDateException(String msg, Throwable throwable) {
+		super(msg, throwable);
+	}
+
+	public KBArticleReviewDateException(Throwable throwable) {
+		super(throwable);
+	}
+
+}
