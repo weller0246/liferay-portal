e4c702b206f2b04dc8404f8eb2d40619322edebe
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-10-04 17:48:18 +0200

    LPS-164756 Adds staging classes for LayoutLocalization

---
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/data/handler/LayoutLocalizationStagedModelDataHandler.java
@@ -0,0 +1,178 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.internal.exportimport.data.handler;
+
+import com.liferay.exportimport.data.handler.base.BaseStagedModelDataHandler;
+import com.liferay.exportimport.kernel.lar.ExportImportPathUtil;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.kernel.lar.StagedModelDataHandler;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.layout.model.LayoutLocalization;
+import com.liferay.layout.service.LayoutLocalizationLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.util.MapUtil;
+import com.liferay.portal.kernel.xml.Element;
+
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	service = {
+		LayoutLocalizationStagedModelDataHandler.class,
+		StagedModelDataHandler.class
+	}
+)
+public class LayoutLocalizationStagedModelDataHandler
+	extends BaseStagedModelDataHandler<LayoutLocalization> {
+
+	public static final String[] CLASS_NAMES = {
+		LayoutLocalization.class.getName()
+	};
+
+	@Override
+	public void deleteStagedModel(LayoutLocalization layoutLocalization)
+		throws PortalException {
+
+		_stagedModelRepository.deleteStagedModel(layoutLocalization);
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		_stagedModelRepository.deleteStagedModel(
+			uuid, groupId, className, extraData);
+	}
+
+	@Override
+	public String[] getClassNames() {
+		return CLASS_NAMES;
+	}
+
+	@Override
+	protected void doExportStagedModel(
+			PortletDataContext portletDataContext,
+			LayoutLocalization layoutLocalization)
+		throws Exception {
+
+		Element element = portletDataContext.getExportDataElement(
+			layoutLocalization);
+
+		portletDataContext.addClassedModel(
+			element, ExportImportPathUtil.getModelPath(layoutLocalization),
+			layoutLocalization);
+	}
+
+	@Override
+	protected void doImportMissingReference(
+			PortletDataContext portletDataContext, String uuid, long groupId,
+			long layoutLocalizationId)
+		throws Exception {
+
+		LayoutLocalization existingLayoutLocalization = fetchMissingReference(
+			uuid, groupId);
+
+		if (existingLayoutLocalization == null) {
+			return;
+		}
+
+		Map<Long, Long> layoutLocalizationIds =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				LayoutLocalization.class);
+
+		layoutLocalizationIds.put(
+			layoutLocalizationId,
+			existingLayoutLocalization.getLayoutLocalizationId());
+	}
+
+	@Override
+	protected void doImportStagedModel(
+			PortletDataContext portletDataContext,
+			LayoutLocalization layoutLocalization)
+		throws Exception {
+
+		LayoutLocalization importedLayoutLocalization =
+			(LayoutLocalization)layoutLocalization.clone();
+
+		importedLayoutLocalization.setCompanyId(
+			portletDataContext.getCompanyId());
+
+		Map<Long, Long> plids =
+			(Map<Long, Long>)portletDataContext.getNewPrimaryKeysMap(
+				Layout.class);
+
+		long plid = MapUtil.getLong(
+			plids, layoutLocalization.getPlid(), layoutLocalization.getPlid());
+
+		importedLayoutLocalization.setPlid(plid);
+
+		LayoutLocalization existingLayoutLocalization =
+			_layoutLocalizationLocalService.fetchLayoutLocalization(
+				portletDataContext.getCompanyId(),
+				layoutLocalization.getLanguageId(), plid);
+
+		if (existingLayoutLocalization == null) {
+			existingLayoutLocalization =
+				_stagedModelRepository.fetchStagedModelByUuidAndGroupId(
+					layoutLocalization.getUuid(),
+					portletDataContext.getScopeGroupId());
+		}
+
+		if ((existingLayoutLocalization == null) ||
+			!portletDataContext.isDataStrategyMirror()) {
+
+			importedLayoutLocalization = _stagedModelRepository.addStagedModel(
+				portletDataContext, importedLayoutLocalization);
+		}
+		else {
+			importedLayoutLocalization.setMvccVersion(
+				existingLayoutLocalization.getMvccVersion());
+			importedLayoutLocalization.setLayoutLocalizationId(
+				existingLayoutLocalization.getLayoutLocalizationId());
+
+			importedLayoutLocalization =
+				_stagedModelRepository.updateStagedModel(
+					portletDataContext, importedLayoutLocalization);
+		}
+
+		portletDataContext.importClassedModel(
+			layoutLocalization, importedLayoutLocalization);
+	}
+
+	@Override
+	protected StagedModelRepository<LayoutLocalization>
+		getStagedModelRepository() {
+
+		return _stagedModelRepository;
+	}
+
+	@Reference
+	private LayoutLocalizationLocalService _layoutLocalizationLocalService;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.layout.model.LayoutLocalization)",
+		unbind = "-"
+	)
+	private StagedModelRepository<LayoutLocalization> _stagedModelRepository;
+
+}
---
+++ modules/apps/layout/layout-impl/src/main/java/com/liferay/layout/internal/exportimport/staged/model/repository/LayoutLocalizationStagedModelRepository.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.internal.exportimport.staged.model.repository;
+
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.exportimport.staged.model.repository.StagedModelRepository;
+import com.liferay.layout.model.LayoutLocalization;
+import com.liferay.layout.service.LayoutLocalizationLocalService;
+import com.liferay.portal.kernel.dao.orm.ExportActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	immediate = true,
+	property = "model.class.name=com.liferay.layout.model.LayoutLocalization",
+	service = StagedModelRepository.class
+)
+public class LayoutLocalizationStagedModelRepository
+	implements StagedModelRepository<LayoutLocalization> {
+
+	@Override
+	public LayoutLocalization addStagedModel(
+			PortletDataContext portletDataContext,
+			LayoutLocalization layoutLocalization)
+		throws PortalException {
+
+		ServiceContext serviceContext = portletDataContext.createServiceContext(
+			layoutLocalization);
+
+		if (portletDataContext.isDataStrategyMirror()) {
+			serviceContext.setUuid(layoutLocalization.getUuid());
+		}
+
+		return _layoutLocalizationLocalService.addLayoutLocalization(
+			layoutLocalization.getGroupId(), layoutLocalization.getContent(),
+			layoutLocalization.getLanguageId(), layoutLocalization.getPlid(),
+			serviceContext);
+	}
+
+	@Override
+	public void deleteStagedModel(LayoutLocalization layoutLocalization)
+		throws PortalException {
+
+		_layoutLocalizationLocalService.deleteLayoutLocalization(
+			layoutLocalization);
+	}
+
+	@Override
+	public void deleteStagedModel(
+			String uuid, long groupId, String className, String extraData)
+		throws PortalException {
+
+		LayoutLocalization layoutLocalization =
+			fetchStagedModelByUuidAndGroupId(uuid, groupId);
+
+		if (layoutLocalization != null) {
+			deleteStagedModel(layoutLocalization);
+		}
+	}
+
+	@Override
+	public void deleteStagedModels(PortletDataContext portletDataContext)
+		throws PortalException {
+	}
+
+	@Override
+	public LayoutLocalization fetchStagedModelByUuidAndGroupId(
+		String uuid, long groupId) {
+
+		return _layoutLocalizationLocalService.
+			fetchLayoutLocalizationByUuidAndGroupId(uuid, groupId);
+	}
+
+	@Override
+	public List<LayoutLocalization> fetchStagedModelsByUuidAndCompanyId(
+		String uuid, long companyId) {
+
+		return _layoutLocalizationLocalService.
+			getLayoutLocalizationsByUuidAndCompanyId(uuid, companyId);
+	}
+
+	@Override
+	public ExportActionableDynamicQuery getExportActionableDynamicQuery(
+		PortletDataContext portletDataContext) {
+
+		return _layoutLocalizationLocalService.getExportActionableDynamicQuery(
+			portletDataContext);
+	}
+
+	@Override
+	public LayoutLocalization saveStagedModel(
+			LayoutLocalization layoutLocalization)
+		throws PortalException {
+
+		return _layoutLocalizationLocalService.updateLayoutLocalization(
+			layoutLocalization);
+	}
+
+	@Override
+	public LayoutLocalization updateStagedModel(
+			PortletDataContext portletDataContext,
+			LayoutLocalization layoutLocalization)
+		throws PortalException {
+
+		return _layoutLocalizationLocalService.updateLayoutLocalization(
+			layoutLocalization);
+	}
+
+	@Reference
+	private LayoutLocalizationLocalService _layoutLocalizationLocalService;
+
+}
