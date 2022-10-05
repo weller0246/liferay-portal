5f1d3810aca218cacc4c2a6cb8cc77dedce0e6c7
Diego Hu <diego.hu@liferay.com>
2022-10-05 14:30:59 +0200

    LPS-152834 creates new descriptors, copy logic from RelatedInfoItemCollectionProviderItemSelectorDisplayContext, RelatedInfoItemCollectionProviderItemSelectorManagementToolbarDisplayContext, and view_related_info_collection_providers

---
+++ modules/apps/info/info-collection-provider-item-selector-web/src/main/java/com/liferay/info/collection/provider/item/selector/web/internal/RelatedInfoItemCollectionProviderItemDescriptor.java
@@ -0,0 +1,119 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.collection.provider.item.selector.web.internal;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.VerticalCard;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
+import com.liferay.info.collection.provider.SingleFormVariationInfoCollectionProvider;
+import com.liferay.info.collection.provider.item.selector.web.internal.frontend.taglib.clay.servlet.taglib.RelatedInfoItemCollectionProviderVerticalCard;
+import com.liferay.item.selector.ItemSelectorViewDescriptor;
+import com.liferay.portal.kernel.dao.search.RowChecker;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.Locale;
+
+import javax.portlet.RenderRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Diego Hu
+ */
+public class RelatedInfoItemCollectionProviderItemDescriptor
+	implements ItemSelectorViewDescriptor.ItemDescriptor {
+
+	public RelatedInfoItemCollectionProviderItemDescriptor(
+		HttpServletRequest httpServletRequest,
+		RelatedInfoItemCollectionProvider relatedInfoItemCollectionProvider) {
+
+		_httpServletRequest = httpServletRequest;
+		_relatedInfoItemCollectionProvider = relatedInfoItemCollectionProvider;
+	}
+
+	@Override
+	public String getIcon() {
+		return null;
+	}
+
+	@Override
+	public String getImageURL() {
+		return null;
+	}
+
+	@Override
+	public String getPayload() {
+		return JSONUtil.put(
+			"itemSubtype",
+			() -> {
+				if (_relatedInfoItemCollectionProvider instanceof
+						SingleFormVariationInfoCollectionProvider) {
+
+					SingleFormVariationInfoCollectionProvider<?>
+						singleFormVariationInfoCollectionProvider =
+							(SingleFormVariationInfoCollectionProvider<?>)
+								_relatedInfoItemCollectionProvider;
+
+					return singleFormVariationInfoCollectionProvider.
+						getFormVariationKey();
+				}
+
+				return null;
+			}
+		).put(
+			"itemType",
+			_relatedInfoItemCollectionProvider.getCollectionItemClassName()
+		).put(
+			"key", _relatedInfoItemCollectionProvider.getKey()
+		).put(
+			"sourceItemType",
+			_relatedInfoItemCollectionProvider.getSourceItemClassName()
+		).put(
+			"title",
+			() -> {
+				ThemeDisplay themeDisplay =
+					(ThemeDisplay)_httpServletRequest.getAttribute(
+						WebKeys.THEME_DISPLAY);
+
+				return _relatedInfoItemCollectionProvider.getLabel(
+					themeDisplay.getLocale());
+			}
+		).toString();
+	}
+
+	@Override
+	public String getSubtitle(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return null;
+	}
+
+	@Override
+	public VerticalCard getVerticalCard(
+		RenderRequest renderRequest, RowChecker rowChecker) {
+
+		return new RelatedInfoItemCollectionProviderVerticalCard(
+			renderRequest, _relatedInfoItemCollectionProvider, rowChecker);
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final RelatedInfoItemCollectionProvider
+		_relatedInfoItemCollectionProvider;
+
+}
---
+++ modules/apps/info/info-collection-provider-item-selector-web/src/main/java/com/liferay/info/collection/provider/item/selector/web/internal/RelatedInfoItemCollectionProviderItemSelectorViewDescriptor.java
@@ -0,0 +1,213 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.info.collection.provider.item.selector.web.internal;
+
+import com.liferay.info.collection.provider.FilteredInfoCollectionProvider;
+import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
+import com.liferay.info.collection.provider.item.selector.criterion.RelatedInfoItemCollectionProviderItemSelectorCriterion;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorViewDescriptor;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.StringComparator;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Diego Hu
+ */
+public class RelatedInfoItemCollectionProviderItemSelectorViewDescriptor
+	implements ItemSelectorViewDescriptor
+		<RelatedInfoItemCollectionProvider<?, ?>> {
+
+	public static boolean supportsFilters(
+		RelatedInfoItemCollectionProvider<?, ?>
+			relatedInfoItemCollectionProvider) {
+
+		if (relatedInfoItemCollectionProvider instanceof
+				FilteredInfoCollectionProvider) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	public RelatedInfoItemCollectionProviderItemSelectorViewDescriptor(
+		RelatedInfoItemCollectionProviderItemSelectorCriterion
+			relatedInfoItemCollectionProviderItemSelectorCriterion,
+		Language language, HttpServletRequest httpServletRequest,
+		PortletURL portletURL,
+		List<RelatedInfoItemCollectionProvider<?, ?>>
+			relatedInfoItemCollectionProviders) {
+
+		_relatedInfoItemCollectionProviderItemSelectorCriterion =
+			relatedInfoItemCollectionProviderItemSelectorCriterion;
+		_language = language;
+		_httpServletRequest = httpServletRequest;
+		_portletURL = portletURL;
+		_relatedInfoItemCollectionProviders =
+			relatedInfoItemCollectionProviders;
+
+		_themeDisplay = (ThemeDisplay)_httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	@Override
+	public String getDefaultDisplayStyle() {
+		return "icon";
+	}
+
+	@Override
+	public String[] getDisplayViews() {
+		return new String[] {"icon"};
+	}
+
+	@Override
+	public ItemDescriptor getItemDescriptor(
+		RelatedInfoItemCollectionProvider<?, ?>
+			relatedInfoItemCollectionProvider) {
+
+		return new RelatedInfoItemCollectionProviderItemDescriptor(
+			_httpServletRequest, relatedInfoItemCollectionProvider);
+	}
+
+	@Override
+	public ItemSelectorReturnType getItemSelectorReturnType() {
+		return new RelatedInfoItemCollectionProviderItemSelectorReturnType();
+	}
+
+	@Override
+	public String[] getOrderByKeys() {
+		Set<String> keysSet = new HashSet<>();
+
+		for (RelatedInfoItemCollectionProvider
+				relatedInfoItemCollectionProvider :
+					_relatedInfoItemCollectionProviders) {
+
+			keysSet.add(
+				relatedInfoItemCollectionProvider.getCollectionItemClassName());
+		}
+
+		Stream<String> keysSetSorted = keysSet.stream(
+		).sorted(
+			new StringComparator(false, true)
+		);
+
+		return keysSetSorted.toArray(String[]::new);
+	}
+
+	public SearchContainer<RelatedInfoItemCollectionProvider<?, ?>>
+			getSearchContainer()
+		throws PortalException {
+
+		PortletRequest portletRequest =
+			(PortletRequest)_httpServletRequest.getAttribute(
+				JavaConstants.JAVAX_PORTLET_REQUEST);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", themeDisplay.getLocale(), getClass());
+
+		SearchContainer<RelatedInfoItemCollectionProvider<?, ?>>
+			searchContainer = new SearchContainer<>(
+				portletRequest, _portletURL, null,
+				_language.get(
+					resourceBundle,
+					"there-are-no-related-items-collection-providers"));
+
+		List<RelatedInfoItemCollectionProvider<?, ?>>
+			relatedInfoItemCollectionProviders = new ArrayList<>(
+				_relatedInfoItemCollectionProviders);
+
+		String orderByCol = ParamUtil.getString(
+			_httpServletRequest, "orderByCol");
+
+		if (Validator.isNotNull(orderByCol)) {
+			relatedInfoItemCollectionProviders = ListUtil.filter(
+				relatedInfoItemCollectionProviders,
+				relatedInfoItemCollectionProvider -> {
+					if (Objects.equals(
+							relatedInfoItemCollectionProvider.
+								getCollectionItemClassName(),
+							orderByCol)) {
+
+						return true;
+					}
+
+					return false;
+				});
+		}
+
+		String keywords = ParamUtil.getString(_httpServletRequest, "keywords");
+
+		if (Validator.isNotNull(keywords)) {
+			relatedInfoItemCollectionProviders = ListUtil.filter(
+				relatedInfoItemCollectionProviders,
+				relatedInfoItemCollectionProvider -> {
+					String label = StringUtil.toLowerCase(
+						relatedInfoItemCollectionProvider.getLabel(
+							themeDisplay.getLocale()));
+
+					if (label.contains(StringUtil.toLowerCase(keywords))) {
+						return true;
+					}
+
+					return false;
+				});
+		}
+
+		searchContainer.setResultsAndTotal(relatedInfoItemCollectionProviders);
+
+		return searchContainer;
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return true;
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final Language _language;
+	private final PortletURL _portletURL;
+	private final RelatedInfoItemCollectionProviderItemSelectorCriterion
+		_relatedInfoItemCollectionProviderItemSelectorCriterion;
+	private final List<RelatedInfoItemCollectionProvider<?, ?>>
+		_relatedInfoItemCollectionProviders;
+	private final ThemeDisplay _themeDisplay;
+
+}
