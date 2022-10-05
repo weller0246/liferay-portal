40c12449b74e56559aab2d9cff93e5cc1d137621
DiogoNo <diogo.nobrega@liferay.com>
2022-10-05 17:15:57 -0300

    LPS-162412 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/mdf-request-detail-collection/new-claim-button/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/mdf-request-detail-collection/new-claim-button/index.js
@@ -1,12 +1,26 @@
-const currentPath = Liferay.currentURL.split('/')
-const mdfRequestedId = +currentPath[currentPath.length -1];
+/* eslint-disable no-undef */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const currentPath = Liferay.currentURL.split('/');
+const mdfRequestedId = +currentPath[currentPath.length - 1];
 const SITE_URL = Liferay.ThemeDisplay.getLayoutRelativeURL()
-				.split('/')
-				.slice(0, 3)
-				.join('/')
+	.split('/')
+	.slice(0, 3)
+	.join('/');
 
-const claimButton = fragmentElement.querySelector('.newClaim')
+const claimButton = fragmentElement.querySelector('.newClaim');
 
-
-claimButton.onclick = () => {Liferay.Util.navigate(`${SITE_URL}/marketing/mdf-claim/new/#/mdfrequest/${mdfRequestedId}`)
-}
+claimButton.onclick = () => {
+	Liferay.Util.navigate(
+		`${SITE_URL}/marketing/mdf-claim/new/#/mdfrequest/${mdfRequestedId}`
+	);
+};
