80fa098560207faf3ef4c63af4c20cc8cefa27e9
Brian Chan <brian.chan@liferay.com>
2022-10-05 09:28:15 -0300

    LPS-161982 SF

--- modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest.java
+++ modules/apps/object/object-admin-rest-test/src/testIntegration/java/com/liferay/object/admin/rest/resource/v1_0/test/ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest.java
@@ -188,7 +188,6 @@ public class ObjectDefinitionVulcanBatchEngineTaskItemDelegateTest {
 						}
 					}
 				};
-
 				panelAppOrder = StringUtil.toLowerCase(
 					RandomTestUtil.randomString());
 				panelCategoryKey = StringUtil.toLowerCase(
