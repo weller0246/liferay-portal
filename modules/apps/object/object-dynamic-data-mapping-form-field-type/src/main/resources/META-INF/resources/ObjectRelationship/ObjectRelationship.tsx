17c9271ff18f248a5b1be42ffa9e28c40a1b1118
Daniel Bonasser <danielbonasser@gmail.com>
2022-10-05 14:11:35 -0300

    LPS-164817 Fix Typescript erros in ObjectRelationship.tsx

--- modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ObjectRelationship/ObjectRelationship.tsx
+++ modules/apps/object/object-dynamic-data-mapping-form-field-type/src/main/resources/META-INF/resources/ObjectRelationship/ObjectRelationship.tsx
@@ -24,6 +24,8 @@ import {ReactFieldBase as FieldBase} from 'dynamic-data-mapping-form-field-type'
 import {fetch} from 'frontend-js-web';
 import React, {useEffect, useRef, useState} from 'react';
 
+type LocalizedValue<T> = Liferay.Language.LocalizedValue<T>;
+
 const defaultLanguageId = Liferay.ThemeDisplay.getDefaultLanguageId();
 
 async function fetchOptions<T>(url: string) {
@@ -45,7 +47,9 @@ function getLabel<T extends ObjectMap<any>>(item: T, key: keyof T) {
 		return value ? String(value) : '';
 	}
 
-	const label = value[defaultLanguageId] ?? value['name'];
+	const label =
+		(value as LocalizedValue<string>)[defaultLanguageId] ??
+		(value as {[key: string]: string})['name'];
 
 	return label ? String(label) : '';
 }
