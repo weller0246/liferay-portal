ecfa6e1c454c0d0ddff8db2a2f618cae2914293d
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-10-07 00:24:31 -0300

    LPS-164094 Create NotificationTypeServiceTracker and its impl

---
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/type/NotificationTypeServiceTrackerImpl.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notification.internal.type;
+
+import com.liferay.notification.type.NotificationType;
+import com.liferay.notification.type.NotificationTypeServiceTracker;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = NotificationTypeServiceTracker.class)
+public class NotificationTypeServiceTrackerImpl
+	implements NotificationTypeServiceTracker {
+
+	@Override
+	public NotificationType getNotificationType(String key) {
+		return _serviceTrackerMap.getService(key);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, NotificationType.class, "notification.type.key");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, NotificationType> _serviceTrackerMap;
+
+}
