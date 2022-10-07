9a566a27a3791e8049b35a8fc630588b2a1f2f73
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2022-10-07 14:25:19 -0300

    LPS-164522 (login-authentication-facebook-connect-web) Renamed variables

--- modules/apps/login/login-authentication-facebook-connect-web/src/main/java/com/liferay/login/authentication/facebook/connect/web/internal/struts/FacebookConnectStrutsAction.java
+++ modules/apps/login/login-authentication-facebook-connect-web/src/main/java/com/liferay/login/authentication/facebook/connect/web/internal/struts/FacebookConnectStrutsAction.java
@@ -231,8 +231,8 @@ public class FacebookConnectStrutsAction implements StrutsAction {
 		String firstName = jsonObject.getString("first_name");
 		String middleName = StringPool.BLANK;
 		String lastName = jsonObject.getString("last_name");
-		long prefixId = 0;
-		long suffixId = 0;
+		long prefixListTypeId = 0;
+		long suffixListTypeId = 0;
 		boolean male = Objects.equals(jsonObject.getString("gender"), "male");
 		int birthdayMonth = Calendar.JANUARY;
 		int birthdayDay = 1;
@@ -247,9 +247,9 @@ public class FacebookConnectStrutsAction implements StrutsAction {
 		User user = _userLocalService.addUser(
 			creatorUserId, companyId, autoPassword, password1, password2,
 			autoScreenName, screenName, emailAddress, locale, firstName,
-			middleName, lastName, prefixId, suffixId, male, birthdayMonth,
-			birthdayDay, birthdayYear, jobTitle, groupIds, organizationIds,
-			roleIds, userGroupIds, sendEmail, serviceContext);
+			middleName, lastName, prefixListTypeId, suffixListTypeId, male,
+			birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+			organizationIds, roleIds, userGroupIds, sendEmail, serviceContext);
 
 		user = _userLocalService.updateLastLogin(
 			user.getUserId(), user.getLoginIP());
