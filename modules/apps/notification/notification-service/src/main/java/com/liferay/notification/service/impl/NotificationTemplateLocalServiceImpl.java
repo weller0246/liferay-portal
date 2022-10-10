958a4c6347eb0b7eece2b96cd33ec7995be1f564
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-10-10 00:35:33 -0300

    LPS-164094 Not need

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationTemplateLocalServiceImpl.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/service/impl/NotificationTemplateLocalServiceImpl.java
@@ -14,70 +14,37 @@
 
 package com.liferay.notification.service.impl;
 
-import com.liferay.document.library.kernel.model.DLFileEntry;
-import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.notification.constants.NotificationConstants;
-import com.liferay.notification.constants.NotificationPortletKeys;
-import com.liferay.notification.constants.NotificationTermContributorConstants;
 import com.liferay.notification.model.NotificationQueueEntry;
 import com.liferay.notification.model.NotificationTemplate;
 import com.liferay.notification.model.NotificationTemplateAttachment;
-import com.liferay.notification.service.NotificationQueueEntryLocalService;
 import com.liferay.notification.service.NotificationTemplateAttachmentLocalService;
 import com.liferay.notification.service.base.NotificationTemplateLocalServiceBaseImpl;
 import com.liferay.notification.service.persistence.NotificationQueueEntryPersistence;
 import com.liferay.notification.service.persistence.NotificationTemplateAttachmentPersistence;
-import com.liferay.notification.term.contributor.NotificationTermContributor;
-import com.liferay.notification.term.contributor.NotificationTermContributorRegistry;
-import com.liferay.notification.type.LegacyNotificationType;
 import com.liferay.notification.type.NotificationContext;
 import com.liferay.notification.type.NotificationType;
 import com.liferay.notification.type.NotificationTypeServiceTracker;
-import com.liferay.notification.util.LegacyNotificationTypeRegistry;
-import com.liferay.object.model.ObjectField;
-import com.liferay.object.service.ObjectDefinitionLocalService;
-import com.liferay.object.service.ObjectFieldLocalService;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.portletfilerepository.PortletFileRepository;
-import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
-import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
-import com.liferay.portal.kernel.security.auth.EmailAddressValidator;
-import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.ResourceLocalService;
-import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.MapUtil;
-import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.StringUtil;
-import com.liferay.portal.kernel.util.Validator;
-import com.liferay.portal.security.auth.EmailAddressValidatorFactory;
 
 import java.io.Serializable;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -196,154 +163,6 @@ public class NotificationTemplateLocalServiceImpl
 		return notificationTemplate;
 	}
 
-	@Override
-	public void sendNotificationTemplate(
-			long userId, long notificationTemplateId,
-			String notificationTypeKey, Object object)
-		throws PortalException {
-
-		if (Validator.isBlank(notificationTypeKey)) {
-			return;
-		}
-
-		LegacyNotificationType notificationType =
-			_notificationTypeRegistry.getNotificationType(notificationTypeKey);
-
-		if (notificationType == null) {
-			return;
-		}
-
-		NotificationTemplate notificationTemplate =
-			notificationTemplatePersistence.findByPrimaryKey(
-				notificationTemplateId);
-
-		User user = _userLocalService.getUser(userId);
-
-		String bcc = _formatContent(
-			notificationTemplate.getBcc(), user.getLocale(), null,
-			notificationType, object);
-
-		Locale siteDefaultLocale = _portal.getSiteDefaultLocale(
-			user.getGroupId());
-
-		if (Validator.isNull(bcc)) {
-			bcc = _formatContent(
-				notificationTemplate.getBcc(), siteDefaultLocale, null,
-				notificationType, object);
-		}
-
-		String body = _formatContent(
-			notificationTemplate.getBody(user.getLocale()), user.getLocale(),
-			null, notificationType, object);
-
-		if (Validator.isNull(body)) {
-			body = _formatContent(
-				notificationTemplate.getBody(siteDefaultLocale),
-				siteDefaultLocale, null, notificationType, object);
-		}
-
-		String cc = _formatContent(
-			notificationTemplate.getCc(), user.getLocale(), null,
-			notificationType, object);
-
-		if (Validator.isNull(cc)) {
-			cc = _formatContent(
-				notificationTemplate.getCc(), siteDefaultLocale, null,
-				notificationType, object);
-		}
-
-		String from = _formatContent(
-			notificationTemplate.getFrom(), user.getLocale(), null,
-			notificationType, object);
-
-		if (Validator.isNull(from)) {
-			from = _formatContent(
-				notificationTemplate.getFrom(), siteDefaultLocale, null,
-				notificationType, object);
-		}
-
-		String fromName = _formatContent(
-			notificationTemplate.getFromName(user.getLocale()),
-			user.getLocale(), null, notificationType, object);
-
-		if (Validator.isNull(fromName)) {
-			fromName = _formatContent(
-				notificationTemplate.getFromName(siteDefaultLocale),
-				siteDefaultLocale, null, notificationType, object);
-		}
-
-		String subject = _formatContent(
-			notificationTemplate.getSubject(user.getLocale()), user.getLocale(),
-			null, notificationType, object);
-
-		if (Validator.isNull(subject)) {
-			subject = _formatContent(
-				notificationTemplate.getSubject(siteDefaultLocale),
-				siteDefaultLocale, null, notificationType, object);
-		}
-
-		String to = _formatTo(
-			notificationTemplate.getTo(user.getLocale()), user.getLocale(),
-			notificationType, object);
-
-		if (Validator.isNull(to)) {
-			to = _formatContent(
-				notificationTemplate.getTo(siteDefaultLocale),
-				siteDefaultLocale,
-				NotificationTermContributorConstants.RECIPIENT,
-				notificationType, object);
-		}
-
-		EmailAddressValidator emailAddressValidator =
-			EmailAddressValidatorFactory.getInstance();
-		List<Long> fileEntryIds = _getFileEntryIds(
-			user.getCompanyId(), notificationTemplateId, object);
-
-		for (String emailAddressOrUserId : StringUtil.split(to)) {
-			User toUser = _userLocalService.fetchUser(
-				GetterUtil.getLong(emailAddressOrUserId));
-
-			if ((toUser == null) &&
-				emailAddressValidator.validate(
-					user.getCompanyId(), emailAddressOrUserId)) {
-
-				toUser = _userLocalService.fetchUserByEmailAddress(
-					user.getCompanyId(), emailAddressOrUserId);
-
-				if (toUser == null) {
-					if (_log.isInfoEnabled()) {
-						_log.info(
-							"No user exists with email address " +
-								emailAddressOrUserId);
-					}
-
-					User defaultUser = _userLocalService.getDefaultUser(
-						CompanyThreadLocal.getCompanyId());
-
-					_notificationQueueEntryLocalService.
-						addNotificationQueueEntry(
-							defaultUser.getUserId(),
-							notificationTemplate.getNotificationTemplateId(),
-							bcc, body, cc,
-							notificationType.getClassName(object),
-							notificationType.getClassPK(object), from, fromName,
-							0, subject, emailAddressOrUserId,
-							emailAddressOrUserId,
-							NotificationConstants.TYPE_EMAIL, fileEntryIds);
-
-					continue;
-				}
-			}
-
-			_notificationQueueEntryLocalService.addNotificationQueueEntry(
-				userId, notificationTemplate.getNotificationTemplateId(), bcc,
-				body, cc, notificationType.getClassName(object),
-				notificationType.getClassPK(object), from, fromName, 0, subject,
-				toUser.getEmailAddress(), toUser.getFullName(),
-				NotificationConstants.TYPE_EMAIL, fileEntryIds);
-		}
-	}
-
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public NotificationTemplate updateNotificationTemplate(
@@ -414,156 +233,6 @@ public class NotificationTemplateLocalServiceImpl
 		return notificationTemplate;
 	}
 
-	private String _formatContent(
-			String content, Locale locale,
-			List<NotificationTermContributor> notificationTermContributors,
-			Object object, List<String> termNames)
-		throws PortalException {
-
-		for (NotificationTermContributor notificationTermContributor :
-				notificationTermContributors) {
-
-			for (String termName : termNames) {
-				content = StringUtil.replace(
-					content, termName,
-					notificationTermContributor.getTermValue(
-						locale, object, termName));
-			}
-		}
-
-		return content;
-	}
-
-	private String _formatContent(
-			String content, Locale locale,
-			String notificationTermContributorKey,
-			LegacyNotificationType notificationType, Object object)
-		throws PortalException {
-
-		if (Validator.isNull(content)) {
-			return StringPool.BLANK;
-		}
-
-		List<String> termNames = new ArrayList<>();
-
-		Matcher matcher = _pattern.matcher(content);
-
-		while (matcher.find()) {
-			termNames.add(matcher.group());
-		}
-
-		if (Validator.isNotNull(notificationTermContributorKey)) {
-			content = _formatContent(
-				content, locale,
-				_notificationTermContributorRegistry.
-					getNotificationTermContributorsByNotificationTermContributorKey(
-						notificationTermContributorKey),
-				object, termNames);
-		}
-
-		return _formatContent(
-			content, locale,
-			_notificationTermContributorRegistry.
-				getNotificationTermContributorsByNotificationTypeKey(
-					notificationType.getKey()),
-			object, termNames);
-	}
-
-	private String _formatTo(
-			String to, Locale locale, LegacyNotificationType notificationType,
-			Object object)
-		throws PortalException {
-
-		if (Validator.isNull(to)) {
-			return StringPool.BLANK;
-		}
-
-		Set<String> emailAddresses = new HashSet<>();
-
-		Matcher matcher = _emailAddressPattern.matcher(to);
-
-		while (matcher.find()) {
-			emailAddresses.add(matcher.group());
-		}
-
-		return _formatContent(
-			StringUtil.merge(emailAddresses), locale,
-			NotificationTermContributorConstants.RECIPIENT, notificationType,
-			object);
-	}
-
-	private List<Long> _getFileEntryIds(
-			long companyId, long notificationTemplateId, Object object)
-		throws PortalException {
-
-		if (!(object instanceof Map)) {
-			return new ArrayList<>();
-		}
-
-		Group group = _groupLocalService.getCompanyGroup(companyId);
-
-		Repository repository = _getRepository(group.getGroupId());
-
-		if (repository == null) {
-			return new ArrayList<>();
-		}
-
-		List<Long> fileEntryIds = new ArrayList<>();
-
-		for (NotificationTemplateAttachment notificationTemplateAttachment :
-				_notificationTemplateAttachmentPersistence.
-					findByNotificationTemplateId(notificationTemplateId)) {
-
-			ObjectField objectField = _objectFieldLocalService.fetchObjectField(
-				notificationTemplateAttachment.getObjectFieldId());
-
-			DLFileEntry dlFileEntry = _dlFileEntryLocalService.fetchDLFileEntry(
-				MapUtil.getLong(
-					(Map<String, Object>)object, objectField.getName()));
-
-			if (dlFileEntry == null) {
-				continue;
-			}
-
-			FileEntry fileEntry = _portletFileRepository.addPortletFileEntry(
-				null, repository.getGroupId(),
-				_userLocalService.getDefaultUserId(companyId),
-				NotificationTemplate.class.getName(), 0,
-				NotificationPortletKeys.NOTIFICATION_TEMPLATES,
-				repository.getDlFolderId(), dlFileEntry.getContentStream(),
-				_portletFileRepository.getUniqueFileName(
-					group.getGroupId(), repository.getDlFolderId(),
-					dlFileEntry.getFileName()),
-				dlFileEntry.getMimeType(), false);
-
-			fileEntryIds.add(fileEntry.getFileEntryId());
-		}
-
-		return fileEntryIds;
-	}
-
-	private Repository _getRepository(long groupId) {
-		Repository repository = _portletFileRepository.fetchPortletRepository(
-			groupId, NotificationPortletKeys.NOTIFICATION_TEMPLATES);
-
-		if (repository != null) {
-			return repository;
-		}
-
-		try {
-			return _portletFileRepository.addPortletRepository(
-				groupId, NotificationPortletKeys.NOTIFICATION_TEMPLATES,
-				new ServiceContext());
-		}
-		catch (PortalException portalException) {
-			if (_log.isDebugEnabled()) {
-				_log.debug(portalException);
-			}
-
-			return null;
-		}
-	}
-
 	private void _validate(
 			long objectDefinitionId, String from, String name, String type,
 			List<Long> attachmentObjectFieldIds)
@@ -592,25 +261,6 @@ public class NotificationTemplateLocalServiceImpl
 		}
 	}
 
-	private static final Log _log = LogFactoryUtil.getLog(
-		NotificationTemplateLocalServiceImpl.class);
-
-	private static final Pattern _emailAddressPattern = Pattern.compile(
-		"[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@" +
-			"(?:\\w(?:[\\w-]*\\w)?\\.)+(\\w(?:[\\w-]*\\w))");
-	private static final Pattern _pattern = Pattern.compile(
-		"\\[%[^\\[%]+%\\]", Pattern.CASE_INSENSITIVE);
-
-	@Reference
-	private DLFileEntryLocalService _dlFileEntryLocalService;
-
-	@Reference
-	private GroupLocalService _groupLocalService;
-
-	@Reference
-	private NotificationQueueEntryLocalService
-		_notificationQueueEntryLocalService;
-
 	@Reference
 	private NotificationQueueEntryPersistence
 		_notificationQueueEntryPersistence;
@@ -624,26 +274,7 @@ public class NotificationTemplateLocalServiceImpl
 		_notificationTemplateAttachmentPersistence;
 
 	@Reference
-	private NotificationTermContributorRegistry
-		_notificationTermContributorRegistry;
-
-	@Reference
-	private LegacyNotificationTypeRegistry _notificationTypeRegistry;
-
-	@Reference
 	private NotificationTypeServiceTracker _notificationTypeServiceTracker;
-
-	@Reference
-	private ObjectDefinitionLocalService _objectDefinitionLocalService;
-
-	@Reference
-	private ObjectFieldLocalService _objectFieldLocalService;
-
-	@Reference
-	private Portal _portal;
-
-	@Reference
-	private PortletFileRepository _portletFileRepository;
 
 	@Reference
 	private ResourceLocalService _resourceLocalService;
