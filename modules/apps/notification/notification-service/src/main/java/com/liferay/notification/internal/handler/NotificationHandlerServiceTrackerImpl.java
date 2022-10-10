76c7d8e69957ca0e2d595b4b4157e1b83253ba80
Feliphe Marinho <feliphe.marinho@liferay.com>
2022-10-10 00:18:31 -0300

    LPS-164094 Create NotificationHandler, NotificationHandlerServiceTracker and its impl

---
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/handler/NotificationHandlerServiceTrackerImpl.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notification.internal.handler;
+
+import com.liferay.notification.handler.NotificationHandler;
+import com.liferay.notification.handler.NotificationHandlerServiceTracker;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Feliphe Marinho
+ */
+@Component(service = NotificationHandlerServiceTracker.class)
+public class NotificationHandlerServiceTrackerImpl
+	implements NotificationHandlerServiceTracker {
+
+	@Override
+	public NotificationHandler getNotificationHandlerByClassName(
+		String className) {
+
+		return _serviceTrackerMap.getService(className);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, NotificationHandler.class, "class.name");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, NotificationHandler> _serviceTrackerMap;
+
+}
