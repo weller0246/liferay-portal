38c24107ffcf5ecc01ff8f42939ba66e183567cc
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-10-10 15:50:20 +0800

    LPS-165032 Remove unused reference variable _portal(usge was removed in task LPS-128746(53d76c82306e649505ffbc8ed661493061189993))

--- modules/dxp/apps/analytics/analytics-reports-journal-impl/src/main/java/com/liferay/analytics/reports/journal/internal/info/item/JournalArticleAnalyticsReportsInfoItem.java
+++ modules/dxp/apps/analytics/analytics-reports-journal-impl/src/main/java/com/liferay/analytics/reports/journal/internal/info/item/JournalArticleAnalyticsReportsInfoItem.java
@@ -30,7 +30,6 @@ import com.liferay.layout.display.page.LayoutDisplayPageProviderTracker;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.UserLocalService;
-import com.liferay.portal.kernel.util.Portal;
 
 import java.util.Arrays;
 import java.util.Date;
@@ -177,9 +176,6 @@ public class JournalArticleAnalyticsReportsInfoItem
 
 	@Reference
 	private LayoutDisplayPageProviderTracker _layoutDisplayPageProviderTracker;
-
-	@Reference
-	private Portal _portal;
 
 	@Reference
 	private UserLocalService _userLocalService;
