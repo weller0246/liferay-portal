67ba3fe8984ce5c157d2196bbad47dbb195825d7
Janis Zhang <janis.zhang@liferay.com>
2022-10-11 20:33:42 +0800

    LPS-165329 Remove unused Reference field _groupLocalService,as it already been referenced in ExportImportHelperImpl.

--- modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/configuration/ExportImportConfigurationParameterMapFactoryImpl.java
+++ modules/apps/export-import/export-import-service/src/main/java/com/liferay/exportimport/internal/configuration/ExportImportConfigurationParameterMapFactoryImpl.java
@@ -29,7 +29,6 @@ import com.liferay.exportimport.kernel.lar.UserIdStrategy;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
-import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ListUtil;
@@ -664,8 +663,5 @@ public class ExportImportConfigurationParameterMapFactoryImpl
 
 	@Reference
 	private ExportImportHelper _exportImportHelper;
-
-	@Reference
-	private GroupLocalService _groupLocalService;
 
 }
