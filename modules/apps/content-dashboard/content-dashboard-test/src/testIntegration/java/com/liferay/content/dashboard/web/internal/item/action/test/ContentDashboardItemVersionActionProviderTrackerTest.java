8f492ae42af168df8a9c33d61e0e5aed8214fae8
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-10-11 10:08:05 +0200

    LPS-164363 Adds integration tests for ContentDashboardItemVersionActionProviderTracker

---
+++ modules/apps/content-dashboard/content-dashboard-test/src/testIntegration/java/com/liferay/content/dashboard/web/internal/item/action/test/ContentDashboardItemVersionActionProviderTrackerTest.java
@@ -0,0 +1,180 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.web.internal.item.action.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemVersionAction;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemVersionActionProviderTracker;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemVersionActionProvider;
+import com.liferay.petra.function.UnsafeRunnable;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+import org.osgi.framework.ServiceRegistration;
+
+/**
+ * @author Jürgen Kappler
+ */
+@RunWith(Arquillian.class)
+public class ContentDashboardItemVersionActionProviderTrackerTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Test
+	public void testInvalidContentDashboardItemVersionActions()
+		throws Exception {
+
+		_withTestItemContentDashboardItemVersionActionProviderRegistered(
+			() -> {
+				List<ContentDashboardItemVersionActionProvider>
+					contentDashboardItemVersionActionProviders =
+						_contentDashboardItemVersionActionProviderTracker.
+							getContentDashboardItemVersionActionProviders(
+								RandomTestUtil.randomString());
+
+				Assert.assertNotNull(
+					contentDashboardItemVersionActionProviders);
+
+				Assert.assertEquals(
+					contentDashboardItemVersionActionProviders.toString(), 0,
+					contentDashboardItemVersionActionProviders.size());
+			});
+	}
+
+	@Test
+	public void testTestItemContentDashboardItemVersionActions()
+		throws Exception {
+
+		_withTestItemContentDashboardItemVersionActionProviderRegistered(
+			() -> {
+				List<ContentDashboardItemVersionActionProvider>
+					contentDashboardItemVersionActionProviders =
+						_contentDashboardItemVersionActionProviderTracker.
+							getContentDashboardItemVersionActionProviders(
+								TestItem.class.getName());
+
+				Assert.assertNotNull(
+					contentDashboardItemVersionActionProviders);
+
+				Assert.assertEquals(
+					contentDashboardItemVersionActionProviders.toString(), 1,
+					contentDashboardItemVersionActionProviders.size());
+
+				ContentDashboardItemVersionActionProvider
+					contentDashboardItemVersionActionProvider =
+						contentDashboardItemVersionActionProviders.get(0);
+
+				Assert.assertNotNull(contentDashboardItemVersionActionProvider);
+			});
+	}
+
+	private void
+			_withTestItemContentDashboardItemVersionActionProviderRegistered(
+				UnsafeRunnable<Exception> unsafeRunnable)
+		throws Exception {
+
+		Bundle bundle = FrameworkUtil.getBundle(
+			ContentDashboardItemVersionActionProviderTrackerTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		ServiceRegistration<ContentDashboardItemVersionActionProvider>
+			serviceRegistration = bundleContext.registerService(
+				ContentDashboardItemVersionActionProvider.class,
+				new TestItemContentDashboardItemVersionActionProvider(), null);
+
+		try {
+			unsafeRunnable.run();
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
+
+	@Inject
+	private ContentDashboardItemVersionActionProviderTracker
+		_contentDashboardItemVersionActionProviderTracker;
+
+	private static class TestContentDashboardItemVersionAction
+		implements ContentDashboardItemVersionAction {
+
+		@Override
+		public String getIcon() {
+			return null;
+		}
+
+		@Override
+		public String getLabel(Locale locale) {
+			return null;
+		}
+
+		@Override
+		public String getName() {
+			return null;
+		}
+
+		@Override
+		public String getURL() {
+			return null;
+		}
+
+	}
+
+	private static class TestItem {
+	}
+
+	private static class TestItemContentDashboardItemVersionActionProvider
+		implements ContentDashboardItemVersionActionProvider<TestItem> {
+
+		@Override
+		public ContentDashboardItemVersionAction
+			getContentDashboardItemVersionAction(
+				TestItem testItem, HttpServletRequest httpServletRequest) {
+
+			return new TestContentDashboardItemVersionAction();
+		}
+
+		@Override
+		public boolean isShow(
+			TestItem testItem, HttpServletRequest httpServletRequest) {
+
+			return true;
+		}
+
+	}
+
+}
