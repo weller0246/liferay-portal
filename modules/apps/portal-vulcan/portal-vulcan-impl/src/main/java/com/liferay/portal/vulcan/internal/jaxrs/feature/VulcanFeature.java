fa095472f6e7a8bec9093246397e03e6c5e031f3
Matija Petanjek <matija.petanjek@liferay.com>
2022-10-11 23:00:28 +0200

    COMMERCE-10129 portal-vulcan-impl - inject nested fields before wrapping entity into entityExtension

--- modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/feature/VulcanFeature.java
+++ modules/apps/portal-vulcan/portal-vulcan-impl/src/main/java/com/liferay/portal/vulcan/internal/jaxrs/feature/VulcanFeature.java
@@ -74,6 +74,7 @@ import com.liferay.portal.vulcan.internal.jaxrs.writer.interceptor.NestedFieldsW
 import com.liferay.portal.vulcan.internal.jaxrs.writer.interceptor.PageEntityExtensionWriterInterceptor;
 import com.liferay.portal.vulcan.internal.param.converter.provider.DateParamConverterProvider;
 
+import javax.ws.rs.Priorities;
 import javax.ws.rs.core.Feature;
 import javax.ws.rs.core.FeatureContext;
 
@@ -165,7 +166,8 @@ public class VulcanFeature implements Feature {
 		_nestedFieldsWriterInterceptor = new NestedFieldsWriterInterceptor(
 			_bundleContext);
 
-		featureContext.register(_nestedFieldsWriterInterceptor);
+		featureContext.register(
+			_nestedFieldsWriterInterceptor, Priorities.USER - 10);
 
 		featureContext.register(
 			new SiteParamConverterProvider(
