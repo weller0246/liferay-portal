39bda4c1def958bb333fb14207d66c41f33a0dc4
Brian I. Kim <il.kim@liferay.com>
2022-10-10 19:09:28 -0700

    COMMERCE-10132 delete dlFileEntry if used only by catalog and not used by other cpAttachmentFileEntries

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPAttachmentFileEntryLocalServiceImpl.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPAttachmentFileEntryLocalServiceImpl.java
@@ -75,6 +75,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
@@ -368,6 +369,23 @@ public class CPAttachmentFileEntryLocalServiceImpl
 		// Commerce product attachment file entry
 
 		cpAttachmentFileEntryPersistence.remove(cpAttachmentFileEntry);
+
+		// DLFileEntry
+
+		FileEntry fileEntry = _dlAppLocalService.getFileEntry(
+			cpAttachmentFileEntry.getFileEntryId());
+
+		if (fileEntry.getGroupId() == cpAttachmentFileEntry.getGroupId()) {
+			List<CPAttachmentFileEntry> cpAttachmentFileEntries =
+				cpAttachmentFileEntryPersistence.findByG_C_F(
+					cpAttachmentFileEntry.getGroupId(), cpDefinitionClassNameId,
+					cpAttachmentFileEntry.getFileEntryId());
+
+			if (ListUtil.isEmpty(cpAttachmentFileEntries)) {
+				_dlAppLocalService.deleteFileEntry(
+					cpAttachmentFileEntry.getFileEntryId());
+			}
+		}
 
 		// Expando
 
