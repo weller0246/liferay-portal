d33e799e675e22a9ef32540bcafd349156e90f5d
Raymond Augé <raymond.auge@liferay.com>
2022-10-11 14:23:20 -0400

    LPS-164458 test

---
+++ modules/apps/portal-configuration/portal-configuration-cluster/src/test/java/com/liferay/portal/configuration/cluster/internal/messaging/ConfigurationMessageListenerTest.java
@@ -0,0 +1,227 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.configuration.cluster.internal.messaging;
+
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.portal.configuration.persistence.ReloadablePersistenceManager;
+import com.liferay.portal.kernel.messaging.Message;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.util.HashMapDictionary;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import java.util.Collections;
+import java.util.Dictionary;
+import java.util.EnumSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import org.osgi.framework.Constants;
+import org.osgi.service.cm.Configuration;
+import org.osgi.service.cm.ConfigurationAdmin;
+import org.osgi.service.cm.ConfigurationEvent;
+
+/**
+ * @author Raymond Augé
+ */
+public class ConfigurationMessageListenerTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.openMocks(this);
+
+		_configurationMessageListener = new ConfigurationMessageListener();
+
+		ReflectionTestUtil.setFieldValue(
+			_configurationMessageListener, "_configurationAdmin",
+			_configurationAdmin);
+		ReflectionTestUtil.setFieldValue(
+			_configurationMessageListener, "_reloadablePersistenceManager",
+			_reloadablePersistenceManager);
+	}
+
+	@Test
+	public void testReadOnlyConfigurationDelete() throws Exception {
+		_invokeConfigurationListener(
+			EnumSet.of(Configuration.ConfigurationAttribute.READ_ONLY),
+			ConfigurationEvent.CM_DELETED, new HashMapDictionary<>(),
+			configuration -> {
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).delete();
+
+				Mockito.verify(
+					configuration, Mockito.times(0)
+				).addAttributes(
+					Configuration.ConfigurationAttribute.READ_ONLY
+				);
+			});
+	}
+
+	@Test
+	public void testReadOnlyConfigurationUpdate() throws Exception {
+		_invokeConfigurationListener(
+			EnumSet.of(Configuration.ConfigurationAttribute.READ_ONLY),
+			ConfigurationEvent.CM_UPDATED,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"foo", "bar"
+			).build(),
+			configuration -> {
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).update(
+					Mockito.any()
+				);
+
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).addAttributes(
+					Configuration.ConfigurationAttribute.READ_ONLY
+				);
+			});
+	}
+
+	@Test
+	public void testReadOnlyConfigurationUpdateEmpty() throws Exception {
+		_invokeConfigurationListener(
+			EnumSet.of(Configuration.ConfigurationAttribute.READ_ONLY),
+			ConfigurationEvent.CM_UPDATED, null,
+			configuration -> {
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).update();
+
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).addAttributes(
+					Configuration.ConfigurationAttribute.READ_ONLY
+				);
+			});
+	}
+
+	@Test
+	public void testReadWriteConfigurationDelete() throws Exception {
+		_invokeConfigurationListener(
+			Collections.emptySet(), ConfigurationEvent.CM_DELETED, null,
+			configuration -> {
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).delete();
+
+				Mockito.verify(
+					configuration, Mockito.times(0)
+				).addAttributes(
+					Mockito.any()
+				);
+			});
+	}
+
+	@Test
+	public void testReadWriteConfigurationUpdate() throws Exception {
+		_invokeConfigurationListener(
+			Collections.emptySet(), ConfigurationEvent.CM_UPDATED,
+			HashMapDictionaryBuilder.<String, Object>put(
+				"foo", "bar"
+			).build(),
+			configuration -> {
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).update(
+					Mockito.any()
+				);
+
+				Mockito.verify(
+					configuration, Mockito.times(0)
+				).addAttributes(
+					Mockito.any()
+				);
+			});
+	}
+
+	@Test
+	public void testReadWriteConfigurationUpdateEmpty() throws Exception {
+		_invokeConfigurationListener(
+			Collections.emptySet(), ConfigurationEvent.CM_UPDATED, null,
+			configuration -> {
+				Mockito.verify(
+					configuration, Mockito.times(1)
+				).update();
+
+				Mockito.verify(
+					configuration, Mockito.times(0)
+				).addAttributes(
+					Mockito.any()
+				);
+			});
+	}
+
+	private void _invokeConfigurationListener(
+			Set<Configuration.ConfigurationAttribute> attributes,
+			int configuratonEventType, Dictionary<String, Object> properties,
+			UnsafeConsumer<Configuration, Exception> testChecks)
+		throws Exception {
+
+		Mockito.when(
+			_reloadablePersistenceManager.load(Mockito.anyString())
+		).thenReturn(
+			properties
+		);
+
+		Configuration configuration = Mockito.mock(Configuration.class);
+
+		Mockito.when(
+			configuration.getAttributes()
+		).thenReturn(
+			attributes
+		);
+
+		Mockito.when(
+			_configurationAdmin.listConfigurations(Mockito.anyString())
+		).thenReturn(
+			new Configuration[] {configuration}
+		);
+
+		Message message = new Message();
+
+		message.put(Constants.SERVICE_PID, "test");
+		message.put("configuration.event.type", configuratonEventType);
+
+		_configurationMessageListener.doReceive(message);
+
+		testChecks.accept(configuration);
+	}
+
+	@Mock
+	private ConfigurationAdmin _configurationAdmin;
+
+	private ConfigurationMessageListener _configurationMessageListener;
+
+	@Mock
+	private ReloadablePersistenceManager _reloadablePersistenceManager;
+
+}
