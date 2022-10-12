059ff302ef8d84be76c33d68bbc69f42d9d28185
Qi Zhang <qi.zhang@liferay.com>
2022-10-12 09:28:02 +0800

    LPS-147890 Add testcase

---
+++ modules/util/source-formatter/src/test/java/com/liferay/source/formatter/processor/JavaUpgradeProcessTest.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.source.formatter.processor;
+
+import org.junit.Test;
+
+/**
+ * @author Qi Zhang
+ */
+public class JavaUpgradeProcessTest extends BaseSourceProcessorTestCase {
+
+	@Test
+	public void testCheckExtraJudgement() throws Exception {
+		test("CheckExtraJudgementUpgradeProcess.testjava");
+	}
+
+	@Test
+	public void testCheckIfDelete() throws Exception {
+		test(
+			"CheckIfDeleteUpgradeProcess.testjava",
+			"Delete this class and replace it by inline calls to the " +
+				"UpgradeProcessFactory class in the registry class.",
+			22);
+	}
+
+	@Test
+	public void testMoveLogicToPreOrPostMethod() throws Exception {
+		test(
+			"MoveLogicToPreOrPostMethodUpgradeProcess.testjava",
+			new String[] {
+				"Drop 26 to 26 line and make that operation from the " +
+					"'getPreUpgradeSteps' method by using the " +
+						"UpgradeProcessFactory class.",
+				"Drop 30 to 30 line and make that operation from the " +
+					"'getPostUpgradeSteps' method by using the " +
+						"UpgradeProcessFactory class."
+			},
+			new Integer[] {26, 30});
+	}
+
+}
