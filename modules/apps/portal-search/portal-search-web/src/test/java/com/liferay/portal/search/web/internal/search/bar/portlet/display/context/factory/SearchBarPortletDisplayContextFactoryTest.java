d90e2de642bd2a3eaae102c891518390f1ebb1b6
Brian Chan <brian.chan@liferay.com>
2022-10-11 21:15:08 -0300

    LPS-161173 Sort

--- modules/apps/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactoryTest.java
+++ modules/apps/portal-search/portal-search-web/src/test/java/com/liferay/portal/search/web/internal/search/bar/portlet/display/context/factory/SearchBarPortletDisplayContextFactoryTest.java
@@ -457,12 +457,6 @@ public class SearchBarPortletDisplayContextFactoryTest {
 		portletPreferences.setValue(
 			SearchBarPortletPreferences.PREFERENCE_KEY_SEARCH_SCOPE, scope);
 
-		Mockito.when(
-			_searchBarPortletInstanceConfiguration.destination()
-		).thenReturn(
-			destination
-		);
-
 		if (scopeParameterName != null) {
 			portletPreferences.setValue(
 				SearchBarPortletPreferences.PREFERENCE_KEY_SCOPE_PARAMETER_NAME,
@@ -490,6 +484,12 @@ public class SearchBarPortletDisplayContextFactoryTest {
 			_portletSharedSearchRequest.search(renderRequest)
 		).thenReturn(
 			portletSharedSearchResponse
+		);
+
+		Mockito.when(
+			_searchBarPortletInstanceConfiguration.destination()
+		).thenReturn(
+			destination
 		);
 
 		Mockito.when(
