bf9c05c7bcf8bbb28695f4407cd561be63319e95
Jose Abelenda <jose.abelenda@liferay.com>
2022-10-13 09:55:33 -0300

    LPS-165486 Persist siteInitializerKey on TypeSettingsProperties on AddGroupMVCActionCommand

--- modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/action/AddGroupMVCActionCommand.java
+++ modules/apps/site/site-admin-web/src/main/java/com/liferay/site/admin/web/internal/portlet/action/AddGroupMVCActionCommand.java
@@ -305,6 +305,10 @@ public class AddGroupMVCActionCommand extends BaseMVCActionCommand {
 			}
 		}
 
+		typeSettingsUnicodeProperties.setProperty(
+			"siteInitializerKey",
+			ParamUtil.getString(actionRequest, "siteInitializerKey"));
+
 		boolean trashEnabled = ParamUtil.getBoolean(
 			actionRequest, "trashEnabled",
 			GetterUtil.getBoolean(
