44d3274a4f08ecd25753724ecf36de5a45f09fd8
Bryan Engler <bryan.engler@liferay.com>
2022-10-12 17:01:56 -0700

    LPS-162046 LPS-165510 portal-search-web: Category - Extend BucketDisplayContext and delete overriding fields
    https://issues.liferay.com/browse/LPS-165510

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/AssetCategoriesSearchFacetTermDisplayContext.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/AssetCategoriesSearchFacetTermDisplayContext.java
@@ -14,13 +14,11 @@
 
 package com.liferay.portal.search.web.internal.facet.display.context;
 
-import java.io.Serializable;
-
 /**
  * @author Lino Alves
  */
 public class AssetCategoriesSearchFacetTermDisplayContext
-	implements Serializable {
+	extends BucketDisplayContext {
 
 	public long getAssetCategoryId() {
 		return _assetCategoryId;
@@ -28,22 +26,6 @@ public class AssetCategoriesSearchFacetTermDisplayContext
 
 	public String getDisplayName() {
 		return _displayName;
-	}
-
-	public int getFrequency() {
-		return _frequency;
-	}
-
-	public int getPopularity() {
-		return _popularity;
-	}
-
-	public boolean isFrequencyVisible() {
-		return _frequencyVisible;
-	}
-
-	public boolean isSelected() {
-		return _selected;
 	}
 
 	public void setAssetCategoryId(long assetCategoryId) {
@@ -54,27 +36,7 @@ public class AssetCategoriesSearchFacetTermDisplayContext
 		_displayName = title;
 	}
 
-	public void setFrequency(int frequency) {
-		_frequency = frequency;
-	}
-
-	public void setFrequencyVisible(boolean frequencyVisible) {
-		_frequencyVisible = frequencyVisible;
-	}
-
-	public void setPopularity(int popularity) {
-		_popularity = popularity;
-	}
-
-	public void setSelected(boolean selected) {
-		_selected = selected;
-	}
-
 	private long _assetCategoryId;
 	private String _displayName;
-	private int _frequency;
-	private boolean _frequencyVisible;
-	private int _popularity;
-	private boolean _selected;
 
 }
