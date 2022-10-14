7873d54ebaeb6b969d4eaad6b9eadb9e9e915414
Julio Cezar <julio.cezar@liferay.com>
2022-10-14 14:24:22 -0300

    LPS-165687 Adding login button functionality and Okta Configuration

---
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-evp/src/main/resources/site-initializer/fragments/group/evp/fragments/login-button/index.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ */
+
+const loginButton = fragmentElement.querySelector('.btn-login');
+const url = themeDisplay.getPortalURL();
+
+if (themeDisplay.getUserEmailAddress() !== '') {
+	loginButton.innerHTML = 'Sign out';
+	loginButton.href = `${url}/c/portal/logout`;
+}
