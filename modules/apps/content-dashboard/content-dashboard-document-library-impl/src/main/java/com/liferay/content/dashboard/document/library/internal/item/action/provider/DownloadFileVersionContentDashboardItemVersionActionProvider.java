57e38dea565df35c3da45cf7cd814209be64243c
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-10-14 08:20:40 +0200

    LPS-165154 Add download action provider for versions in the Content Dashboard

---
+++ modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/action/provider/DownloadFileVersionContentDashboardItemVersionActionProvider.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.document.library.internal.item.action.provider;
+
+import com.liferay.content.dashboard.document.library.internal.item.action.DownloadFileVersionContentDashboardItemVersionAction;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemVersionAction;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemVersionActionProvider;
+import com.liferay.info.item.InfoItemServiceTracker;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.repository.model.FileVersion;
+import com.liferay.portal.kernel.util.Validator;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(service = ContentDashboardItemVersionActionProvider.class)
+public class DownloadFileVersionContentDashboardItemVersionActionProvider
+	implements ContentDashboardItemVersionActionProvider<FileVersion> {
+
+	@Override
+	public ContentDashboardItemVersionAction
+		getContentDashboardItemVersionAction(
+			FileVersion fileVersion, HttpServletRequest httpServletRequest) {
+
+		if (!isShow(fileVersion, httpServletRequest)) {
+			return null;
+		}
+
+		return _getContentDashboardItemAction(fileVersion);
+	}
+
+	@Override
+	public boolean isShow(
+		FileVersion fileVersion, HttpServletRequest httpServletRequest) {
+
+		ContentDashboardItemVersionAction contentDashboardItemVersionAction =
+			_getContentDashboardItemAction(fileVersion);
+
+		if (Validator.isNull(contentDashboardItemVersionAction.getURL())) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private ContentDashboardItemVersionAction _getContentDashboardItemAction(
+		FileVersion fileVersion) {
+
+		FileEntry fileEntry = null;
+
+		try {
+			fileEntry = fileVersion.getFileEntry();
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+		}
+
+		InfoItemFieldValuesProvider<FileEntry> infoItemFieldValuesProvider =
+			_infoItemServiceTracker.getFirstInfoItemService(
+				InfoItemFieldValuesProvider.class, FileEntry.class.getName());
+
+		return new DownloadFileVersionContentDashboardItemVersionAction(
+			fileEntry, _language, infoItemFieldValuesProvider);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		DownloadFileVersionContentDashboardItemVersionActionProvider.class);
+
+	@Reference
+	private InfoItemServiceTracker _infoItemServiceTracker;
+
+	@Reference
+	private Language _language;
+
+}
