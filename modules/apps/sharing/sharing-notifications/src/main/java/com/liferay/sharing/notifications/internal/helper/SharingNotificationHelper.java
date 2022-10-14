e2175be2f678efa1d8ddff9fa9f208e4c047117a
Janis Zhang <janis.zhang@liferay.com>
2022-10-14 20:19:25 +0800

    LPS-165628 Remove reference field _sharingEntryLocalService, its usage was removed in 08af2543507e734034d756965fb0c64f2db1b3bb

--- modules/apps/sharing/sharing-notifications/src/main/java/com/liferay/sharing/notifications/internal/helper/SharingNotificationHelper.java
+++ modules/apps/sharing/sharing-notifications/src/main/java/com/liferay/sharing/notifications/internal/helper/SharingNotificationHelper.java
@@ -34,7 +34,6 @@ import com.liferay.sharing.interpreter.SharingEntryInterpreter;
 import com.liferay.sharing.interpreter.SharingEntryInterpreterProvider;
 import com.liferay.sharing.model.SharingEntry;
 import com.liferay.sharing.security.permission.SharingEntryAction;
-import com.liferay.sharing.service.SharingEntryLocalService;
 
 import java.text.Format;
 
@@ -252,9 +251,6 @@ public class SharingNotificationHelper {
 
 	@Reference
 	private SharingEntryInterpreterProvider _sharingEntryInterpreterProvider;
-
-	@Reference
-	private SharingEntryLocalService _sharingEntryLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
