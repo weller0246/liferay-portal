b5b42fa393af6881f1bafbaa125f9e36b34fc50a
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2022-10-14 08:20:26 +0200

    LPS-165154 Add download action for versions in the Content Dashboard

---
+++ modules/apps/content-dashboard/content-dashboard-document-library-impl/src/main/java/com/liferay/content/dashboard/document/library/internal/item/action/DownloadFileVersionContentDashboardItemVersionAction.java
@@ -0,0 +1,87 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.document.library.internal.item.action;
+
+import com.liferay.content.dashboard.item.action.ContentDashboardItemVersionAction;
+import com.liferay.info.field.InfoFieldValue;
+import com.liferay.info.item.InfoItemFieldValues;
+import com.liferay.info.item.provider.InfoItemFieldValuesProvider;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.repository.model.FileEntry;
+import com.liferay.portal.kernel.util.ContentTypes;
+
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Optional;
+
+/**
+ * @author Jürgen Kappler
+ */
+public class DownloadFileVersionContentDashboardItemVersionAction
+	implements ContentDashboardItemVersionAction {
+
+	public DownloadFileVersionContentDashboardItemVersionAction(
+		FileEntry fileEntry, Language language,
+		InfoItemFieldValuesProvider<FileEntry> infoItemFieldValuesProvider) {
+
+		_fileEntry = fileEntry;
+		_language = language;
+		_infoItemFieldValuesProvider = infoItemFieldValuesProvider;
+	}
+
+	@Override
+	public String getIcon() {
+		return "download";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "download");
+	}
+
+	@Override
+	public String getName() {
+		return "download";
+	}
+
+	@Override
+	public String getURL() {
+		if (Objects.equals(
+				_fileEntry.getMimeType(),
+				ContentTypes.
+					APPLICATION_VND_LIFERAY_VIDEO_EXTERNAL_SHORTCUT_HTML)) {
+
+			return StringPool.BLANK;
+		}
+
+		InfoItemFieldValues infoItemFieldValues =
+			_infoItemFieldValuesProvider.getInfoItemFieldValues(_fileEntry);
+
+		return Optional.ofNullable(
+			infoItemFieldValues.getInfoFieldValue("downloadURL")
+		).map(
+			InfoFieldValue::getValue
+		).orElse(
+			StringPool.BLANK
+		).toString();
+	}
+
+	private final FileEntry _fileEntry;
+	private final InfoItemFieldValuesProvider<FileEntry>
+		_infoItemFieldValuesProvider;
+	private final Language _language;
+
+}
