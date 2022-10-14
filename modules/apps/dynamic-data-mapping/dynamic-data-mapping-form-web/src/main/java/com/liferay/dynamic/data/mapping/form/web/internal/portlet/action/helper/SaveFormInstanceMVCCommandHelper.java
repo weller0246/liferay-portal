04e040c9ed2220bebb3cec73817ee80eddbc08b9
Richard Jeremias <ricmartinsrocha@gmail.com>
2022-10-14 19:21:12 -0300

    LPS-165731 Prevent saving form instance if CSRF token is missing or invalid

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/helper/SaveFormInstanceMVCCommandHelper.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-form-web/src/main/java/com/liferay/dynamic/data/mapping/form/web/internal/portlet/action/helper/SaveFormInstanceMVCCommandHelper.java
@@ -42,6 +42,7 @@ import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.redirect.RedirectURLSettings;
+import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
 import com.liferay.portal.kernel.util.GetterUtil;
@@ -113,16 +114,30 @@ public class SaveFormInstanceMVCCommandHelper {
 			boolean validateDDMFormFieldSettings)
 		throws Exception {
 
-		long formInstanceId = ParamUtil.getLong(
-			portletRequest, "formInstanceId");
+		HttpServletRequest httpServletRequest = _portal.getHttpServletRequest(
+			portletRequest);
 
-		if (formInstanceId == 0) {
-			return _addFormInstance(
-				portletRequest, validateDDMFormFieldSettings);
+		try {
+			AuthTokenUtil.checkCSRFToken(
+				httpServletRequest,
+				SaveFormInstanceMVCCommandHelper.class.getName());
+
+			long formInstanceId = ParamUtil.getLong(
+				portletRequest, "formInstanceId");
+
+			if (formInstanceId == 0) {
+				return _addFormInstance(
+					portletRequest, validateDDMFormFieldSettings);
+			}
+
+			return _updateFormInstance(
+				portletRequest, formInstanceId, validateDDMFormFieldSettings);
+		}
+		catch (PortalException portalException) {
+			_log.error("The CSRF token is missing or invalid", portalException);
 		}
 
-		return _updateFormInstance(
-			portletRequest, formInstanceId, validateDDMFormFieldSettings);
+		return null;
 	}
 
 	protected DDMFormLayout getDDMFormLayout(PortletRequest portletRequest)
