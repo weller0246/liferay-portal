6eedf995d9c34578a2d9093a9c9abe3b3eb7bc30
shuyangzhou <shuyang.zhou@liferay.com>
2022-10-14 13:54:36 -0700

    LPS-165696 Remove unused service

--- modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/messaging/CheckNotificationQueueEntryMessageListener.java
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/messaging/CheckNotificationQueueEntryMessageListener.java
@@ -38,9 +38,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Gustavo Lima
  */
-@Component(
-	immediate = true, service = CheckNotificationQueueEntryMessageListener.class
-)
+@Component(service = {})
 public class CheckNotificationQueueEntryMessageListener
 	extends BaseMessageListener {
 
