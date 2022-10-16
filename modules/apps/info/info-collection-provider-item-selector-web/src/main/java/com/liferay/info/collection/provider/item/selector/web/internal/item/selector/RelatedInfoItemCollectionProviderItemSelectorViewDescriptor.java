b0f12be069ee75e61468423c2de313b2947921e9
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-10-16 09:30:11 +0100

    LPS-152834 Implements management toolbar filters since it should not be order

--- modules/apps/info/info-collection-provider-item-selector-web/src/main/java/com/liferay/info/collection/provider/item/selector/web/internal/item/selector/RelatedInfoItemCollectionProviderItemSelectorViewDescriptor.java
+++ modules/apps/info/info-collection-provider-item-selector-web/src/main/java/com/liferay/info/collection/provider/item/selector/web/internal/item/selector/RelatedInfoItemCollectionProviderItemSelectorViewDescriptor.java
@@ -14,25 +14,33 @@
 
 package com.liferay.info.collection.provider.item.selector.web.internal.item.selector;
 
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemList;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.LabelItemListBuilder;
 import com.liferay.info.collection.provider.RelatedInfoItemCollectionProvider;
 import com.liferay.info.list.provider.item.selector.criterion.InfoListProviderItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorReturnType;
 import com.liferay.item.selector.ItemSelectorViewDescriptor;
 import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.security.permission.ResourceActionsUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.KeyValuePair;
+import com.liferay.portal.kernel.util.KeyValuePairComparator;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
-import com.liferay.portal.kernel.util.StringComparator;
 import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
-import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import javax.portlet.PortletRequest;
@@ -56,11 +64,51 @@ public class RelatedInfoItemCollectionProviderItemSelectorViewDescriptor
 		_portletURL = portletURL;
 		_relatedInfoItemCollectionProviders =
 			relatedInfoItemCollectionProviders;
+
+		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
 	}
 
 	@Override
 	public String[] getDisplayViews() {
 		return new String[] {"icon"};
+	}
+
+	@Override
+	public List<DropdownItem> getFilterDropdownItems() {
+		return DropdownItemListBuilder.addGroup(
+			dropdownGroupItem -> {
+				dropdownGroupItem.setDropdownItems(
+					_getFilterTypeDropdownItems());
+				dropdownGroupItem.setLabel(
+					LanguageUtil.get(
+						_themeDisplay.getLocale(), "filter-by-item-type"));
+			}
+		).build();
+	}
+
+	@Override
+	public List<LabelItem> getFilterLabelItems() {
+		return LabelItemListBuilder.add(
+			() -> Validator.isNotNull(_getSelectedItemType()),
+			labelItem -> {
+				labelItem.putData(
+					"removeLabelURL",
+					PortletURLBuilder.create(
+						_portletURL
+					).setParameter(
+						"itemType", (String)null
+					).buildString());
+				labelItem.setDismissible(true);
+
+				String modelResource = ResourceActionsUtil.getModelResource(
+					_themeDisplay.getLocale(), _getSelectedItemType());
+
+				labelItem.setLabel(
+					LanguageUtil.get(_themeDisplay.getLocale(), "item-type") +
+						": " + modelResource);
+			}
+		).build();
 	}
 
 	@Override
@@ -75,26 +123,6 @@ public class RelatedInfoItemCollectionProviderItemSelectorViewDescriptor
 	@Override
 	public ItemSelectorReturnType getItemSelectorReturnType() {
 		return new InfoListProviderItemSelectorReturnType();
-	}
-
-	@Override
-	public String[] getOrderByKeys() {
-		Set<String> keysSet = new HashSet<>();
-
-		for (RelatedInfoItemCollectionProvider<?, ?>
-				relatedInfoItemCollectionProvider :
-					_relatedInfoItemCollectionProviders) {
-
-			keysSet.add(
-				relatedInfoItemCollectionProvider.getCollectionItemClassName());
-		}
-
-		Stream<String> keysSetSorted = keysSet.stream(
-		).sorted(
-			new StringComparator(false, true)
-		);
-
-		return keysSetSorted.toArray(String[]::new);
 	}
 
 	public SearchContainer<RelatedInfoItemCollectionProvider<?, ?>>
@@ -113,17 +141,14 @@ public class RelatedInfoItemCollectionProviderItemSelectorViewDescriptor
 			relatedInfoItemCollectionProviders = new ArrayList<>(
 				_relatedInfoItemCollectionProviders);
 
-		String orderByCol = ParamUtil.getString(
-			_httpServletRequest, "orderByCol");
-
-		if (Validator.isNotNull(orderByCol)) {
+		if (Validator.isNotNull(_getSelectedItemType())) {
 			relatedInfoItemCollectionProviders = ListUtil.filter(
 				relatedInfoItemCollectionProviders,
 				relatedInfoItemCollectionProvider -> {
 					if (Objects.equals(
 							relatedInfoItemCollectionProvider.
 								getCollectionItemClassName(),
-							orderByCol)) {
+							_getSelectedItemType())) {
 
 						return true;
 					}
@@ -135,16 +160,12 @@ public class RelatedInfoItemCollectionProviderItemSelectorViewDescriptor
 		String keywords = ParamUtil.getString(_httpServletRequest, "keywords");
 
 		if (Validator.isNotNull(keywords)) {
-			ThemeDisplay themeDisplay =
-				(ThemeDisplay)portletRequest.getAttribute(
-					WebKeys.THEME_DISPLAY);
-
 			relatedInfoItemCollectionProviders = ListUtil.filter(
 				relatedInfoItemCollectionProviders,
 				relatedInfoItemCollectionProvider -> {
 					String label = StringUtil.toLowerCase(
 						relatedInfoItemCollectionProvider.getLabel(
-							themeDisplay.getLocale()));
+							_themeDisplay.getLocale()));
 
 					if (label.contains(StringUtil.toLowerCase(keywords))) {
 						return true;
@@ -164,9 +185,65 @@ public class RelatedInfoItemCollectionProviderItemSelectorViewDescriptor
 		return true;
 	}
 
+	private List<DropdownItem> _getFilterTypeDropdownItems() {
+		Stream<RelatedInfoItemCollectionProvider<?, ?>> stream =
+			_relatedInfoItemCollectionProviders.stream();
+
+		List<KeyValuePair> keyValuePairs = stream.map(
+			relatedInfoItemCollectionProvider -> {
+				String collectionItemClassName =
+					relatedInfoItemCollectionProvider.
+						getCollectionItemClassName();
+
+				return new KeyValuePair(
+					collectionItemClassName,
+					ResourceActionsUtil.getModelResource(
+						_themeDisplay.getLocale(), collectionItemClassName));
+			}
+		).distinct(
+		).sorted(
+			new KeyValuePairComparator(false, true)
+		).collect(
+			Collectors.toList()
+		);
+
+		return new DropdownItemList() {
+			{
+				for (KeyValuePair keyValuePair : keyValuePairs) {
+					add(
+						dropdownItem -> {
+							if (Objects.equals(
+									keyValuePair.getKey(),
+									_getSelectedItemType())) {
+
+								dropdownItem.setActive(true);
+							}
+
+							dropdownItem.setHref(
+								_portletURL, "itemType", keyValuePair.getKey());
+							dropdownItem.setLabel(keyValuePair.getValue());
+						});
+				}
+			}
+		};
+	}
+
+	private String _getSelectedItemType() {
+		if (_selectedItemType != null) {
+			return _selectedItemType;
+		}
+
+		_selectedItemType = ParamUtil.getString(
+			_httpServletRequest, "itemType");
+
+		return _selectedItemType;
+	}
+
 	private final HttpServletRequest _httpServletRequest;
 	private final PortletURL _portletURL;
 	private final List<RelatedInfoItemCollectionProvider<?, ?>>
 		_relatedInfoItemCollectionProviders;
+	private String _selectedItemType;
+	private final ThemeDisplay _themeDisplay;
 
 }
