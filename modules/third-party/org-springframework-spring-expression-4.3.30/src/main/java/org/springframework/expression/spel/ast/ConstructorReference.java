96825309ee14cd3f39758c216f1c885464336c9b
javalosjr96 <jorge.avalos@liferay.com>
2022-10-17 13:27:41 -0700

    LPS-158181 Create and apply patch Source:https://github.com/opennms-forge/spring-framework/commit/619eefa6ba0eef0a0aad7426f5d28c8ba562386f

--- modules/third-party/org-springframework-spring-expression-4.3.30/src/main/java/org/springframework/expression/spel/ast/ConstructorReference.java
+++ modules/third-party/org-springframework-spring-expression-4.3.30/src/main/java/org/springframework/expression/spel/ast/ConstructorReference.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2002-2018 the original author or authors.
+ * Copyright 2002-2022 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -51,11 +51,19 @@ import org.springframework.expression.spel.support.ReflectiveConstructorExecutor
  *
  * @author Andy Clement
  * @author Juergen Hoeller
+ * @author Sam Brannen
  * @since 3.0
  */
 public class ConstructorReference extends SpelNodeImpl {
 
-	private boolean isArrayConstructor = false;
+	/**
+	 * Maximum number of elements permitted in an array declaration, applying
+	 * to one-dimensional as well as multi-dimensional arrays.
+	 * @since 5.2.20
+	 */
+	private static final int MAX_ARRAY_ELEMENTS = 256 * 1024; // 256K
+
+	private final boolean isArrayConstructor;
 
 	private SpelNodeImpl[] dimensions;
 
@@ -256,14 +264,19 @@ public class ConstructorReference extends SpelNodeImpl {
 			if (this.dimensions.length == 1) {
 				TypedValue o = this.dimensions[0].getTypedValue(state);
 				int arraySize = ExpressionUtils.toInt(typeConverter, o);
+				checkNumElements(arraySize);
 				newArray = Array.newInstance(componentType, arraySize);
 			}
 			else {
 				// Multi-dimensional - hold onto your hat!
 				int[] dims = new int[this.dimensions.length];
+				long numElements = 1;
 				for (int d = 0; d < this.dimensions.length; d++) {
 					TypedValue o = this.dimensions[d].getTypedValue(state);
-					dims[d] = ExpressionUtils.toInt(typeConverter, o);
+					int arraySize = ExpressionUtils.toInt(typeConverter, o);
+					dims[d] = arraySize;
+					numElements *= arraySize;
+					checkNumElements(numElements);
 				}
 				newArray = Array.newInstance(componentType, dims);
 			}
@@ -322,6 +335,14 @@ public class ConstructorReference extends SpelNodeImpl {
 		}
 		return new TypedValue(newArray);
 	}
+
+	private void checkNumElements(long numElements) {
+		if (numElements >= MAX_ARRAY_ELEMENTS) {
+			throw new SpelEvaluationException(getStartPosition(),
+				SpelMessage.MAX_ARRAY_ELEMENTS_THRESHOLD_EXCEEDED, MAX_ARRAY_ELEMENTS);
+		}
+	}
+
 
 	private void populateReferenceTypeArray(ExpressionState state, Object newArray, TypeConverter typeConverter,
 			InlineList initializer, Class<?> componentType) {
@@ -457,3 +478,4 @@ public class ConstructorReference extends SpelNodeImpl {
 	}
 
 }
+/* @generated */
