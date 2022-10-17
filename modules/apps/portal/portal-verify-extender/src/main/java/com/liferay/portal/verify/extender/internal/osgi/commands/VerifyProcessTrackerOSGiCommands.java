ee6de5bc5d1ecb811161e537c93aad96e22a338c
shuyangzhou <shuyang.zhou@liferay.com>
2022-10-17 08:04:57 -0700

    LPS-164871 Remove leftover header

--- modules/apps/portal/portal-verify-extender/src/main/java/com/liferay/portal/verify/extender/internal/osgi/commands/VerifyProcessTrackerOSGiCommands.java
+++ modules/apps/portal/portal-verify-extender/src/main/java/com/liferay/portal/verify/extender/internal/osgi/commands/VerifyProcessTrackerOSGiCommands.java
@@ -61,8 +61,7 @@ import org.osgi.util.tracker.ServiceTrackerCustomizer;
 		"osgi.command.function=check", "osgi.command.function=checkAll",
 		"osgi.command.function=execute", "osgi.command.function=executeAll",
 		"osgi.command.function=help", "osgi.command.function=list",
-		"osgi.command.function=show", "osgi.command.function=showReports",
-		"osgi.command.scope=verify"
+		"osgi.command.function=show", "osgi.command.scope=verify"
 	},
 	service = VerifyProcessTrackerOSGiCommands.class
 )
