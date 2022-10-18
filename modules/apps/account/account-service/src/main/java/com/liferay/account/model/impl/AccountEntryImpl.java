b6671dd19e70bd3ead08c67860c4f154d5840b20
drewbrokke <drew.brokke@liferay.com>
2022-10-18 10:26:43 -0500

    LPS-163706 account-service: adds helper methods to AccountEntryImpl

--- modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryImpl.java
@@ -15,18 +15,70 @@
 package com.liferay.account.model.impl;
 
 import com.liferay.account.model.AccountEntry;
+import com.liferay.account.model.AccountEntryOrganizationRel;
+import com.liferay.account.model.AccountEntryUserRel;
+import com.liferay.account.service.AccountEntryOrganizationRelLocalServiceUtil;
+import com.liferay.account.service.AccountEntryUserRelLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Address;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.AddressLocalServiceUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalServiceUtil;
 import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
 import com.liferay.portal.kernel.util.StringUtil;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author Brian Wing Shun Chan
  */
 public class AccountEntryImpl extends AccountEntryBaseImpl {
+
+	@Override
+	public List<Organization> fetchOrganizations() {
+		List<Organization> organizations = new ArrayList<>();
+
+		for (AccountEntryOrganizationRel accountEntryOrganizationRel :
+				AccountEntryOrganizationRelLocalServiceUtil.
+					getAccountEntryOrganizationRels(getAccountEntryId())) {
+
+			try {
+				organizations.add(
+					accountEntryOrganizationRel.getOrganization());
+			}
+			catch (PortalException portalException) {
+				_log.error(portalException);
+			}
+		}
+
+		return organizations;
+	}
+
+	@Override
+	public List<User> fetchUsers() {
+		List<User> users = new ArrayList<>();
+
+		for (AccountEntryUserRel accountEntryUserRel :
+				AccountEntryUserRelLocalServiceUtil.
+					getAccountEntryUserRelsByAccountEntryId(
+						getAccountEntryId())) {
+
+			try {
+				users.add(accountEntryUserRel.getUser());
+			}
+			catch (PortalException portalException) {
+				_log.error(portalException);
+			}
+		}
+
+		return users;
+	}
 
 	@Override
 	public Group getAccountEntryGroup() {
@@ -67,5 +119,8 @@ public class AccountEntryImpl extends AccountEntryBaseImpl {
 			ClassNameLocalServiceUtil.getClassNameId(AccountEntry.class),
 			getAccountEntryId());
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		AccountEntryImpl.class);
 
 }
