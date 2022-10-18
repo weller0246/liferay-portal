7f2873670c8013d283191f1ecfd99ff67aff8eac
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-10-18 12:55:40 +0200

    LPS-165334 Move unit tests to integration tests

---
+++ modules/apps/content-dashboard/content-dashboard-blogs-test/src/testIntegration/java/com/liferay/content/dashboard/blogs/internal/item/test/BlogsEntryContentDashboardItemTest.java
@@ -0,0 +1,379 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.blogs.internal.item.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.asset.display.page.constants.AssetDisplayPageConstants;
+import com.liferay.asset.display.page.service.AssetDisplayPageEntryLocalService;
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.kernel.service.AssetTagLocalService;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalService;
+import com.liferay.blogs.model.BlogsEntry;
+import com.liferay.blogs.test.util.BlogsTestUtil;
+import com.liferay.content.dashboard.item.ContentDashboardItem;
+import com.liferay.content.dashboard.item.ContentDashboardItemFactory;
+import com.liferay.content.dashboard.item.type.ContentDashboardItemSubtype;
+import com.liferay.layout.page.template.constants.LayoutPageTemplateEntryTypeConstants;
+import com.liferay.layout.page.template.model.LayoutPageTemplateEntry;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.util.Collections;
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.mock.web.MockHttpServletRequest;
+
+/**
+ * @author Cristina González
+ * @author Jürgen Kappler
+ */
+@RunWith(Arquillian.class)
+public class BlogsEntryContentDashboardItemTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup(
+			TestPropsValues.getCompanyId(), TestPropsValues.getUserId(), 0);
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+	}
+
+	@Test
+	public void testGetAssetCategories() throws Exception {
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.addVocabulary(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), _serviceContext);
+
+		AssetCategory assetCategory = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), assetVocabulary.getVocabularyId(),
+			_serviceContext);
+
+		_serviceContext.setAssetCategoryIds(
+			new long[] {assetCategory.getCategoryId()});
+
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			Collections.singletonList(assetCategory),
+			contentDashboardItem.getAssetCategories());
+	}
+
+	@Test
+	public void testGetAssetCategoriesByAssetVocabulary() throws Exception {
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.addVocabulary(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), _serviceContext);
+
+		AssetCategory assetCategory = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), assetVocabulary.getVocabularyId(),
+			_serviceContext);
+
+		_serviceContext.setAssetCategoryIds(
+			new long[] {assetCategory.getCategoryId()});
+
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			Collections.singletonList(assetCategory),
+			contentDashboardItem.getAssetCategories(
+				assetCategory.getVocabularyId()));
+	}
+
+	@Test
+	public void testGetAssetCategoriesByAssetVocabularyWithEmptyAssetCategories()
+		throws Exception {
+
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			Collections.emptyList(),
+			contentDashboardItem.getAssetCategories(
+				RandomTestUtil.randomLong()));
+	}
+
+	@Test
+	public void testGetAssetCategoriesWithNoAssetCategoriesInAssetVocabulary()
+		throws Exception {
+
+		AssetVocabulary assetVocabulary =
+			_assetVocabularyLocalService.addVocabulary(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), _serviceContext);
+
+		AssetCategory assetCategory = _assetCategoryLocalService.addCategory(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), assetVocabulary.getVocabularyId(),
+			_serviceContext);
+
+		_serviceContext.setAssetCategoryIds(
+			new long[] {assetCategory.getCategoryId()});
+
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			Collections.emptyList(),
+			contentDashboardItem.getAssetCategories(
+				RandomTestUtil.randomLong()));
+	}
+
+	@Test
+	public void testGetAssetTags() throws Exception {
+		AssetTag assetTag = _assetTagLocalService.addTag(
+			TestPropsValues.getUserId(), _group.getGroupId(),
+			RandomTestUtil.randomString(), _serviceContext);
+
+		_serviceContext.setAssetTagNames(new String[] {assetTag.getName()});
+
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			Collections.singletonList(assetTag),
+			contentDashboardItem.getAssetTags());
+	}
+
+	@Test
+	public void testGetDescription() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			blogsEntry.getSubtitle(),
+			contentDashboardItem.getDescription(LocaleUtil.US));
+	}
+
+	@Test
+	public void testGetModifiedDate() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			blogsEntry.getModifiedDate(),
+			contentDashboardItem.getModifiedDate());
+	}
+
+	@Test
+	public void testGetScopeName() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			_group.getDescriptiveName(LocaleUtil.US),
+			contentDashboardItem.getScopeName(LocaleUtil.US));
+	}
+
+	@Test
+	public void testGetSubtype() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		ContentDashboardItemSubtype contentDashboardItemType =
+			contentDashboardItem.getContentDashboardItemSubtype();
+
+		Assert.assertNull(contentDashboardItemType);
+	}
+
+	@Test
+	public void testGetTitle() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			blogsEntry.getTitle(),
+			contentDashboardItem.getTitle(LocaleUtil.US));
+	}
+
+	@Test
+	public void testGetUserId() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			blogsEntry.getUserId(), contentDashboardItem.getUserId());
+	}
+
+	@Test
+	public void testGetUserName() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		Assert.assertEquals(
+			blogsEntry.getUserName(), contentDashboardItem.getUserName());
+	}
+
+	@Test
+	public void testIsViewable() throws Exception {
+		BlogsEntry blogsEntry = BlogsTestUtil.addEntryWithWorkflow(
+			TestPropsValues.getUserId(), RandomTestUtil.randomString(), true,
+			_serviceContext);
+
+		ContentDashboardItem contentDashboardItem =
+			_contentDashboardItemFactory.create(blogsEntry.getEntryId());
+
+		LayoutPageTemplateEntry layoutPageTemplateEntry =
+			_layoutPageTemplateEntryLocalService.addLayoutPageTemplateEntry(
+				_group.getCreatorUserId(), blogsEntry.getGroupId(), 0,
+				_portal.getClassNameId(BlogsEntry.class.getName()), 0,
+				RandomTestUtil.randomString(),
+				LayoutPageTemplateEntryTypeConstants.TYPE_DISPLAY_PAGE, 0, true,
+				0, 0, 0, 0, _serviceContext);
+
+		_assetDisplayPageEntryLocalService.addAssetDisplayPageEntry(
+			blogsEntry.getUserId(), blogsEntry.getGroupId(),
+			_portal.getClassNameId(BlogsEntry.class.getName()),
+			blogsEntry.getEntryId(),
+			layoutPageTemplateEntry.getLayoutPageTemplateEntryId(),
+			AssetDisplayPageConstants.TYPE_DEFAULT, _serviceContext);
+
+		HttpServletRequest mockHttpServletRequest =
+			new MockHttpServletRequest();
+
+		mockHttpServletRequest.setAttribute(
+			WebKeys.THEME_DISPLAY, _getThemeDisplay(LocaleUtil.US));
+
+		Assert.assertTrue(
+			contentDashboardItem.isViewable(mockHttpServletRequest));
+	}
+
+	private ThemeDisplay _getThemeDisplay(Locale locale) {
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		themeDisplay.setLocale(locale);
+		themeDisplay.setSiteGroupId(_group.getGroupId());
+
+		return themeDisplay;
+	}
+
+	@Inject
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Inject
+	private AssetDisplayPageEntryLocalService
+		_assetDisplayPageEntryLocalService;
+
+	@Inject
+	private AssetTagLocalService _assetTagLocalService;
+
+	@Inject
+	private AssetVocabularyLocalService _assetVocabularyLocalService;
+
+	@Inject(
+		filter = "component.name=com.liferay.content.dashboard.blogs.internal.item.BlogsEntryContentDashboardItemFactory"
+	)
+	private ContentDashboardItemFactory _contentDashboardItemFactory;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+	@Inject
+	private Portal _portal;
+
+	private ServiceContext _serviceContext;
+
+}
