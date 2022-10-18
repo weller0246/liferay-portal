dd2439279f04f11af8eb5b7e7e68aa2935c9996a
drewbrokke <drew.brokke@liferay.com>
2022-10-18 10:20:44 -0500

    LPS-163706 account-service: adds helper methods to AccountEntryOrganizationRelImpl

--- modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryOrganizationRelImpl.java
+++ modules/apps/account/account-service/src/main/java/com/liferay/account/model/impl/AccountEntryOrganizationRelImpl.java
@@ -14,9 +14,40 @@
 
 package com.liferay.account.model.impl;
 
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.service.AccountEntryLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+
 /**
  * @author Brian Wing Shun Chan
  */
 public class AccountEntryOrganizationRelImpl
 	extends AccountEntryOrganizationRelBaseImpl {
+
+	@Override
+	public AccountEntry fetchAccountEntry() {
+		return AccountEntryLocalServiceUtil.fetchAccountEntry(
+			getAccountEntryId());
+	}
+
+	@Override
+	public Organization fetchOrganization() {
+		return OrganizationLocalServiceUtil.fetchOrganization(
+			getOrganizationId());
+	}
+
+	@Override
+	public AccountEntry getAccountEntry() throws PortalException {
+		return AccountEntryLocalServiceUtil.getAccountEntry(
+			getAccountEntryId());
+	}
+
+	@Override
+	public Organization getOrganization() throws PortalException {
+		return OrganizationLocalServiceUtil.getOrganization(
+			getOrganizationId());
+	}
+
 }
