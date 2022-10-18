fe858c16cea92c650dc1d6a59bda652b30ee3bb7
Jose Abelenda <jose.abelenda@liferay.com>
2022-10-18 16:56:03 -0300

    LPS-165488 Create panel category

---
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-web/src/main/java/com/liferay/site/initializer/extender/web/internal/application/list/SiteInitializerExtenderUpdatePanelApp.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.initializer.extender.web.internal.application.list;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.site.initializer.extender.web.internal.constants.SiteInitializerExtenderUpdatePortletKeys;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author José Abelenda
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=600",
+		"panel.category.key=" + PanelCategoryKeys.SITE_ADMINISTRATION_PUBLISHING
+	},
+	service = PanelApp.class
+)
+public class SiteInitializerExtenderUpdatePanelApp extends BasePanelApp {
+
+	@Override
+	public String getPortletId() {
+		return SiteInitializerExtenderUpdatePortletKeys.
+			SITE_INITIALIZER_EXTENDER_UPDATE;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + SiteInitializerExtenderUpdatePortletKeys.SITE_INITIALIZER_EXTENDER_UPDATE + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
--- modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-web/src/main/java/com/liferay/site/initializer/extender/web/internal/portlet/SiteInitializerExtenderUpdatePortlet.java
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender-web/src/main/java/com/liferay/site/initializer/extender/web/internal/portlet/SiteInitializerExtenderUpdatePortlet.java
@@ -28,7 +28,7 @@ import org.osgi.service.component.annotations.Component;
 	immediate = true,
 	property = {
 		"com.liferay.portlet.add-default-resource=true",
-		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.display-category=category.hidden",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.private-request-attributes=false",
@@ -41,7 +41,8 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + SiteInitializerExtenderUpdatePortletKeys.SITE_INITIALIZER_EXTENDER_UPDATE,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user"
+		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
 )
