dccbfb55a6db308a9ac30f109641124ec003e23f
Attila Bakay <attila.bakay@liferay.com>
2022-10-18 14:55:55 +0200

    LPS-158085 Add integration tests for RedirectEntry SourceURL upgrade

---
+++ modules/apps/redirect/redirect-test/src/testIntegration/java/com/liferay/redirect/internal/upgrade/v3_0_2/test/UpgradeRedirectEntrySourceURLTest.java
@@ -0,0 +1,276 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.redirect.internal.upgrade.v3_0_2.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.counter.kernel.service.CounterLocalService;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.dao.db.DB;
+import com.liferay.portal.kernel.dao.db.DBManagerUtil;
+import com.liferay.portal.kernel.dao.db.DBType;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.upgrade.UpgradeStep;
+import com.liferay.portal.kernel.uuid.PortalUUIDUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+import com.liferay.redirect.model.RedirectEntry;
+import com.liferay.redirect.service.RedirectEntryLocalServiceUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Timestamp;
+
+import java.util.Objects;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Attila Bakay
+ */
+@RunWith(Arquillian.class)
+public class UpgradeRedirectEntrySourceURLTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_db = DBManagerUtil.getDB();
+
+		_timestamp = new Timestamp(System.currentTimeMillis());
+		_group1 = GroupTestUtil.addGroup();
+		_group2 = GroupTestUtil.addGroup();
+
+		_setUpUpgradeSourceURL();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		_db.runSQL(
+			"delete from RedirectEntry where groupId = " +
+				_group1.getGroupId());
+		_db.runSQL(
+			"delete from RedirectEntry where groupId = " +
+				_group2.getGroupId());
+	}
+
+	@Test
+	public void testUpgradeWithMultipleGroup() throws Exception {
+		long redirectEntryId1 = _counterLocalService.increment();
+		long redirectEntryId2 = _counterLocalService.increment();
+		long redirectEntryId3 = _counterLocalService.increment();
+		long redirectEntryId4 = _counterLocalService.increment();
+		long redirectEntryId5 = _counterLocalService.increment();
+		long redirectEntryId6 = _counterLocalService.increment();
+
+		addRedirectEntry(
+			redirectEntryId1, _group1.getGroupId(), _group1.getCompanyId(),
+			"destination1", "Page1");
+		addRedirectEntry(
+			redirectEntryId2, _group1.getGroupId(), _group1.getCompanyId(),
+			"destination2", "Page2");
+		addRedirectEntry(
+			redirectEntryId3, _group1.getGroupId(), _group1.getCompanyId(),
+			"destination3", "Page3");
+
+		addRedirectEntry(
+			redirectEntryId4, _group2.getGroupId(), _group2.getCompanyId(),
+			"destination1", "Page1");
+		addRedirectEntry(
+			redirectEntryId5, _group2.getGroupId(), _group2.getCompanyId(),
+			"destination2", "Page2");
+		addRedirectEntry(
+			redirectEntryId6, _group2.getGroupId(), _group2.getCompanyId(),
+			"destination3", "Page3");
+
+		_redirectEntrySourceURLUpgradeProcess.upgrade();
+
+		RedirectEntry redirectEntry1 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId1);
+		RedirectEntry redirectEntry2 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId2);
+		RedirectEntry redirectEntry3 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId3);
+		RedirectEntry redirectEntry4 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId4);
+		RedirectEntry redirectEntry5 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId5);
+		RedirectEntry redirectEntry6 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId6);
+
+		Assert.assertEquals("page1", redirectEntry1.getSourceURL());
+		Assert.assertEquals("page2", redirectEntry2.getSourceURL());
+		Assert.assertEquals("page3", redirectEntry3.getSourceURL());
+		Assert.assertEquals("page1", redirectEntry4.getSourceURL());
+		Assert.assertEquals("page2", redirectEntry5.getSourceURL());
+		Assert.assertEquals("page3", redirectEntry6.getSourceURL());
+	}
+
+	@Test
+	public void testUpgradeWithSingleGroup() throws Exception {
+		long redirectEntryId1 = _counterLocalService.increment();
+		long redirectEntryId2 = _counterLocalService.increment();
+		long redirectEntryId3 = _counterLocalService.increment();
+
+		addRedirectEntry(
+			redirectEntryId1, _group1.getGroupId(), _group1.getCompanyId(),
+			"destination1", "PageA");
+		addRedirectEntry(
+			redirectEntryId2, _group1.getGroupId(), _group1.getCompanyId(),
+			"destination2", "PageB");
+		addRedirectEntry(
+			redirectEntryId3, _group1.getGroupId(), _group1.getCompanyId(),
+			"destination3", "PageC");
+
+		_redirectEntrySourceURLUpgradeProcess.upgrade();
+
+		RedirectEntry redirectEntry1 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId1);
+		RedirectEntry redirectEntry2 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId2);
+		RedirectEntry redirectEntry3 =
+			RedirectEntryLocalServiceUtil.getRedirectEntry(redirectEntryId3);
+
+		Assert.assertEquals("pagea", redirectEntry1.getSourceURL());
+		Assert.assertEquals("pageb", redirectEntry2.getSourceURL());
+		Assert.assertEquals("pagec", redirectEntry3.getSourceURL());
+	}
+
+	@Test
+	public void testUpgradeWithSourceURLCollision() throws Exception {
+		if (_db.getDBType() == DBType.MYSQL) {
+			Assume.assumeTrue(
+				"MYSQL is case-insensitive so it is not possible to create " +
+					"this scenario, skip test.",
+				_db.getDBType() == DBType.MYSQL);
+		}
+		else {
+			long redirectEntryId1 = _counterLocalService.increment();
+			long redirectEntryId2 = _counterLocalService.increment();
+
+			addRedirectEntry(
+				redirectEntryId1, _group1.getGroupId(), _group1.getCompanyId(),
+				"destination1", "testpage");
+			addRedirectEntry(
+				redirectEntryId2, _group1.getGroupId(), _group1.getCompanyId(),
+				"destination2", "TestPage");
+
+			_redirectEntrySourceURLUpgradeProcess.upgrade();
+
+			RedirectEntry redirectEntry1 =
+				RedirectEntryLocalServiceUtil.getRedirectEntry(
+					redirectEntryId1);
+			RedirectEntry redirectEntry2 =
+				RedirectEntryLocalServiceUtil.getRedirectEntry(
+					redirectEntryId2);
+
+			Assert.assertEquals("testpage", redirectEntry1.getSourceURL());
+			Assert.assertEquals("TestPage", redirectEntry2.getSourceURL());
+		}
+	}
+
+	protected void addRedirectEntry(
+			long redirectEntryId, long groupId, long companyId,
+			String destinationURL, String sourceURL)
+		throws Exception {
+
+		try (Connection connection = DataAccess.getConnection();
+			PreparedStatement preparedStatement = connection.prepareStatement(
+				StringBundler.concat(
+					"insert into RedirectEntry (uuid_, redirectEntryId,",
+					"groupId, companyId, userId, userName, createDate, ",
+					"modifiedDate, destinationURL, sourceURL) values (?, ?, ",
+					"?, ?, ?, ?, ?, ?, ?, ?)"))) {
+
+			preparedStatement.setString(1, PortalUUIDUtil.generate());
+			preparedStatement.setLong(2, redirectEntryId);
+			preparedStatement.setLong(3, groupId);
+			preparedStatement.setLong(4, companyId);
+			preparedStatement.setLong(5, TestPropsValues.getUserId());
+			preparedStatement.setString(6, null);
+			preparedStatement.setTimestamp(7, _timestamp);
+			preparedStatement.setTimestamp(8, _timestamp);
+			preparedStatement.setString(9, destinationURL);
+			preparedStatement.setString(10, sourceURL);
+
+			preparedStatement.executeUpdate();
+		}
+	}
+
+	private void _setUpUpgradeSourceURL() {
+		_upgradeStepRegistrator.register(
+			new UpgradeStepRegistrator.Registry() {
+
+				@Override
+				public void register(
+					String fromSchemaVersionString,
+					String toSchemaVersionString, UpgradeStep... upgradeSteps) {
+
+					for (UpgradeStep upgradeStep : upgradeSteps) {
+						Class<?> clazz = upgradeStep.getClass();
+
+						if (Objects.equals(clazz.getName(), _CLASS_NAME)) {
+							_redirectEntrySourceURLUpgradeProcess =
+								(UpgradeProcess)upgradeStep;
+						}
+					}
+				}
+
+			});
+	}
+
+	private static final String _CLASS_NAME =
+		"com.liferay.redirect.internal.upgrade.v3_0_2." +
+			"RedirectEntrySourceURLUpgradeProcess";
+
+	@Inject(
+		filter = "(&(objectClass=com.liferay.redirect.internal.upgrade.registry.RedirectServiceUpgradeStepRegistrator))"
+	)
+	private static UpgradeStepRegistrator _upgradeStepRegistrator;
+
+	@Inject
+	private CounterLocalService _counterLocalService;
+
+	private DB _db;
+
+	@DeleteAfterTestRun
+	private Group _group1;
+
+	@DeleteAfterTestRun
+	private Group _group2;
+
+	private UpgradeProcess _redirectEntrySourceURLUpgradeProcess;
+	private Timestamp _timestamp;
+
+}
