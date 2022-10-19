d94af7a332358f66eec27559a89b3079f4dc0de9
Murilo Stodolni <murilo.stodolni@liferay.com>
2022-10-19 17:53:29 -0300

    LPS-165005 Create mapper to ObjectFieldListTypeDefinitionIdException

---
+++ modules/apps/object/object-admin-rest-impl/src/main/java/com/liferay/object/admin/rest/internal/jaxrs/exception/mapper/ObjectFieldListTypeDefinitionIdExceptionMapper.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.object.admin.rest.internal.jaxrs.exception.mapper;
+
+import com.liferay.object.exception.ObjectFieldListTypeDefinitionIdException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Murilo Stodolni
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Object.Admin.REST)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Object.Admin.REST.ObjectFieldListTypeIdDefinitionExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class ObjectFieldListTypeDefinitionIdExceptionMapper
+	extends BaseExceptionMapper<ObjectFieldListTypeDefinitionIdException> {
+
+	@Override
+	protected Problem getProblem(
+		ObjectFieldListTypeDefinitionIdException
+			objectFieldListTypeDefinitionIdException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			objectFieldListTypeDefinitionIdException.getMessage());
+	}
+
+	@Override
+	protected boolean isSanitize() {
+		return false;
+	}
+
+}
