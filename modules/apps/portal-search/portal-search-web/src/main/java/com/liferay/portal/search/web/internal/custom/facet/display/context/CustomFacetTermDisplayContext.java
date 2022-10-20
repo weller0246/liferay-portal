352451cf4e21be17797dca1f9d1ffbe4f4ff78dd
Amanda Costa <amanda.costa@liferay.com>
2022-10-20 18:50:01 -0300

    LPS-162054 LPS-165716 portal-search-web: Custom - Extend BucketDisplayContext and delete overriding fields
    https://issues.liferay.com/browse/LPS-165716

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/custom/facet/display/context/CustomFacetTermDisplayContext.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/custom/facet/display/context/CustomFacetTermDisplayContext.java
@@ -14,46 +14,21 @@
 
 package com.liferay.portal.search.web.internal.custom.facet.display.context;
 
+import com.liferay.portal.search.web.internal.facet.display.context.BucketDisplayContext;
+
 /**
  * @author Wade Cao
  */
-public class CustomFacetTermDisplayContext {
+public class CustomFacetTermDisplayContext extends BucketDisplayContext {
 
 	public String getFieldName() {
 		return _fieldName;
-	}
-
-	public int getFrequency() {
-		return _frequency;
-	}
-
-	public boolean isFrequencyVisible() {
-		return _frequencyVisible;
-	}
-
-	public boolean isSelected() {
-		return _selected;
 	}
 
 	public void setFieldName(String fieldName) {
 		_fieldName = fieldName;
 	}
 
-	public void setFrequency(int frequency) {
-		_frequency = frequency;
-	}
-
-	public void setFrequencyVisible(boolean showFrequency) {
-		_frequencyVisible = showFrequency;
-	}
-
-	public void setSelected(boolean selected) {
-		_selected = selected;
-	}
-
 	private String _fieldName;
-	private int _frequency;
-	private boolean _frequencyVisible;
-	private boolean _selected;
 
 }
