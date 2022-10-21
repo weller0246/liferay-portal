69e22b28247e7967193736ded0f0c3ee48462b0c
shuyangzhou <shuyang.zhou@liferay.com>
2022-10-21 12:34:19 -0700

    LPS-166679 Remove leftover MessagingProxy from QuerySuggester

--- portal-kernel/src/com/liferay/portal/kernel/search/suggest/QuerySuggester.java
+++ portal-kernel/src/com/liferay/portal/kernel/search/suggest/QuerySuggester.java
@@ -14,8 +14,6 @@
 
 package com.liferay.portal.kernel.search.suggest;
 
-import com.liferay.portal.kernel.messaging.proxy.MessagingProxy;
-import com.liferay.portal.kernel.messaging.proxy.ProxyMode;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.search.SearchException;
 
@@ -25,7 +23,6 @@ import java.util.Map;
 /**
  * @author Michael C. Han
  */
-@MessagingProxy(mode = ProxyMode.SYNC)
 public interface QuerySuggester {
 
 	public String spellCheckKeywords(SearchContext searchContext)
