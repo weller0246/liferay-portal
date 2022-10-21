e0ae5f3f753a4a1f54c26ce7397bcc1a0420184e
Alberto Chaparro <alberto.chaparro@liferay.com>
2022-10-21 15:08:37 +0200

    Revert "LPS-164440 SF"
    This reverts commit 2f831c492165309dc86ce41118da08ac57a4fbb7.

--- modules/apps/data-cleanup/data-cleanup/src/main/java/com/liferay/data/cleanup/internal/upgrade/registry/DataCleanupUpgradeStepRegistrator.java
+++ modules/apps/data-cleanup/data-cleanup/src/main/java/com/liferay/data/cleanup/internal/upgrade/registry/DataCleanupUpgradeStepRegistrator.java
@@ -135,7 +135,6 @@ public class DataCleanupUpgradeStepRegistrator
 				"com.liferay.change.tracking.store.service",
 				() -> new PublishedCTSContentDataUpgradeProcess(
 					_ctsContentLocalService, _portal));
-
 			_removeModuleData(
 				_dataCleanupConfiguration::removeExpiredJournalArticles,
 				"com.liferay.journal.service",
@@ -177,21 +176,18 @@ public class DataCleanupUpgradeStepRegistrator
 			Supplier<UpgradeProcess> upgradeProcessSupplier)
 		throws UpgradeException {
 
-		if (!booleanSupplier.get()) {
-			return;
+		if (booleanSupplier.get()) {
+			Release release = _releaseLocalService.fetchRelease(
+				servletContextName);
+
+			if (release != null) {
+				UpgradeProcess upgradeProcess = upgradeProcessSupplier.get();
+
+				upgradeProcess.upgrade();
+
+				CacheRegistryUtil.clear();
+			}
 		}
-
-		Release release = _releaseLocalService.fetchRelease(servletContextName);
-
-		if (release == null) {
-			return;
-		}
-
-		UpgradeProcess upgradeProcess = upgradeProcessSupplier.get();
-
-		upgradeProcess.upgrade();
-
-		CacheRegistryUtil.clear();
 	}
 
 	@Reference
