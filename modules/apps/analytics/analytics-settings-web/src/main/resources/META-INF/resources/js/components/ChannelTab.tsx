5f3b34bec3ab8ba8270d1cadb4b7b78c01355ecc
Ilza Medeiros <ilza.medeiros@liferay.com>
2022-10-21 12:57:58 -0300

    LRAC-11904 - Create channel tab

--- modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/ChannelTab.tsx
+++ modules/apps/analytics/analytics-settings-web/src/main/resources/META-INF/resources/js/components/ChannelTab.tsx
@@ -12,10 +12,80 @@
  * details.
  */
 
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
-const ChannelTab = () => {
-	return <div>ChannelTab</div>;
+import {fetchChannels} from '../utils/api';
+import {TProperty} from './PropertiesTable';
+import TabsTemplate, {TData} from './TabsTemplate';
+
+interface IChannelTabProps {
+	description?: string;
+	displayChannels?: boolean;
+	property: TProperty;
+}
+
+const ChannelTab: React.FC<IChannelTabProps> = ({
+	displayChannels,
+	property,
+}) => {
+	const [items, setItems] = useState<TData>([]);
+	const [selectedAll, setSelectedAll] = useState(false);
+
+	useEffect(() => {
+		const request = async () => {
+			const response = await fetchChannels();
+			setItems(response.items);
+		};
+		request();
+	}, []);
+
+	useEffect(() => {
+		setSelectedAll(items.every((item) => item.channelName));
+	}, [items]);
+
+	const handleCheckboxChange = (index: number) => {
+		const newItems = items;
+		newItems[index].channelName
+			? delete newItems[index].channelName
+			: (newItems[index].channelName = property.name);
+		setItems([...newItems]);
+	};
+
+	return (
+		<TabsTemplate
+			channelTab
+			checked={selectedAll}
+			displayChannels={displayChannels}
+			handleCheckboxChange={handleCheckboxChange}
+			handleSelectAll={(checked: boolean) => {
+				const newItems = items.map((item) => {
+					const disabled =
+						(item.channelName &&
+							item.channelName !== property.name) ||
+						!displayChannels;
+
+					if (disabled) {
+						return item;
+					} else {
+						if (
+							(checked && !item.channelName) ||
+							(checked && item.channelName)
+						) {
+							return {...item, channelName: property.name};
+						} else {
+							delete item.channelName;
+
+							return item;
+						}
+					}
+				});
+				setItems(newItems);
+			}}
+			items={items}
+			property={property}
+			selectedAllDisabled={!displayChannels}
+		/>
+	);
 };
 
 export default ChannelTab;
