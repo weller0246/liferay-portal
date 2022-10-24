48e1697ab5239315293f78245f4f1e9e5d7f48b3
Crescenzo Rega <crescenzo.rega@liferay.com>
2022-10-24 15:05:39 +0200

    COMMERCE-4270 Add dispatcher module
    Co-Authored-By: riccardo-alberti <56033516+riccardo-alberti@users.noreply.github.com>

---
+++ modules/dxp/apps/commerce-avalara/commerce-avalara-dispatch-web/src/main/java/com/liferay/commerce/avalara/dispatch/web/internal/executor/CommerceAvalaraDispatchTaskExecutor.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * The contents of this file are subject to the terms of the Liferay Enterprise
+ * Subscription License ("License"). You may not use this file except in
+ * compliance with the License. You can obtain a copy of the License by
+ * contacting Liferay, Inc. See the License for the specific language governing
+ * permissions and limitations under the License, including but not limited to
+ * distribution rights of the Software.
+ *
+ *
+ *
+ */
+
+package com.liferay.commerce.avalara.dispatch.web.internal.executor;
+
+import com.liferay.commerce.avalara.connector.engine.CommerceAvalaraConnectorEngine;
+import com.liferay.dispatch.executor.BaseDispatchTaskExecutor;
+import com.liferay.dispatch.executor.DispatchTaskExecutor;
+import com.liferay.dispatch.executor.DispatchTaskExecutorOutput;
+import com.liferay.dispatch.model.DispatchTrigger;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.UnicodeProperties;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Katie Nesterovich
+ */
+@Component(
+	enabled = false, immediate = true,
+	property = {
+		"dispatch.task.executor.name=" + CommerceAvalaraDispatchTaskExecutor.AVALARA,
+		"dispatch.task.executor.type=" + CommerceAvalaraDispatchTaskExecutor.AVALARA
+	},
+	service = DispatchTaskExecutor.class
+)
+public class CommerceAvalaraDispatchTaskExecutor
+	extends BaseDispatchTaskExecutor {
+
+	public static final String AVALARA = "avalara";
+
+	@Override
+	public void doExecute(
+			DispatchTrigger dispatchTrigger,
+			DispatchTaskExecutorOutput dispatchTaskExecutorOutput)
+		throws PortalException {
+
+		UnicodeProperties unicodeProperties =
+			dispatchTrigger.getDispatchTaskSettingsUnicodeProperties();
+
+		String groupId = GetterUtil.getString(
+			unicodeProperties.getProperty("groupId"));
+
+		try {
+			_commerceAvalaraConnectorEngine.updateByAddressEntries(
+				Long.parseLong(groupId));
+		}
+		catch (Exception exception) {
+			dispatchTaskExecutorOutput.setError(exception.getMessage());
+
+			throw new PortalException(exception);
+		}
+	}
+
+	@Override
+	public String getName() {
+		return null;
+	}
+
+	@Reference
+	private CommerceAvalaraConnectorEngine _commerceAvalaraConnectorEngine;
+
+}
