eadff63505ddbf425fc58f34d161da041ffa7b3d
Bárbara Cabrera <barbara.cabrera@liferay.com>
2022-10-24 14:01:28 +0200

    LPS-166857 Add Import option in the dropdown for Utility Pages

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/configuration/icon/ImportPortletConfigurationIcon.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.portlet.configuration.icon;
+
+import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
+import com.liferay.layout.utility.page.constants.LayoutUtilityPageActionKeys;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.configuration.icon.BasePortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.configuration.icon.PortletConfigurationIcon;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.permission.GroupPermission;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Bárbara Cabrera
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + LayoutAdminPortletKeys.GROUP_PAGES,
+	service = PortletConfigurationIcon.class
+)
+public class ImportPortletConfigurationIcon
+	extends BasePortletConfigurationIcon {
+
+	@Override
+	public String getIconCssClass() {
+		return "download";
+	}
+
+	@Override
+	public String getMessage(PortletRequest portletRequest) {
+		return _language.get(
+			getResourceBundle(getLocale(portletRequest)), "import");
+	}
+
+	@Override
+	public String getOnClick(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return StringBundler.concat(
+			"Liferay.Util.openModal({onClose: function(event){",
+			"window.location.reload();}, title: '", getMessage(portletRequest),
+			"', url: '",
+			PortletURLBuilder.create(
+				_portal.getControlPanelPortletURL(
+					portletRequest, LayoutAdminPortletKeys.GROUP_PAGES,
+					PortletRequest.RENDER_PHASE)
+			).setMVCPath(
+				"/view_import.jsp"
+			).setWindowState(
+				LiferayWindowState.POP_UP
+			).buildString(),
+			"'});");
+	}
+
+	@Override
+	public String getURL(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		return "javascript:void(0);";
+	}
+
+	@Override
+	public double getWeight() {
+		return 100;
+	}
+
+	@Override
+	public boolean isShow(PortletRequest portletRequest) {
+		ThemeDisplay themeDisplay = (ThemeDisplay)portletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		if (_groupPermission.contains(
+				themeDisplay.getPermissionChecker(),
+				LayoutUtilityPageActionKeys.ADD_LAYOUT_UTILITY_PAGE_ENTRY)) {
+
+			return true;
+		}
+
+		return false;
+	}
+
+	@Reference
+	private GroupPermission _groupPermission;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private Portal _portal;
+
+}
