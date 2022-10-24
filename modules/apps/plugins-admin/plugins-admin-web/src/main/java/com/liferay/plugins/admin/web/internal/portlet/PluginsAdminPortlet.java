5032c9c2b94aaa54e5ba2a941e087afad89f5a50
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2022-10-24 15:42:10 -0300

    LPS-166543 Remove immediate from Plugins Admin module

--- modules/apps/plugins-admin/plugins-admin-web/src/main/java/com/liferay/plugins/admin/web/internal/portlet/PluginsAdminPortlet.java
+++ modules/apps/plugins-admin/plugins-admin-web/src/main/java/com/liferay/plugins/admin/web/internal/portlet/PluginsAdminPortlet.java
@@ -27,7 +27,6 @@ import org.osgi.service.component.annotations.Reference;
  * @author Peter Fellwock
  */
 @Component(
-	immediate = true,
 	property = {
 		"com.liferay.portlet.css-class-wrapper=portlet-users-admin",
 		"com.liferay.portlet.display-category=category.hidden",
