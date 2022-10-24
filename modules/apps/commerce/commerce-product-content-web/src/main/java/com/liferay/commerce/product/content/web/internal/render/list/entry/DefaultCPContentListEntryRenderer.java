b230981ec77ff5e64c26ceae87273d3d03313a01
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-10-24 13:34:02 +0800

    LPS-166273 Remove unused reference variable _commerceOrderItemLocalService(usge was removed in task COMMERCE-5456(6c824a91df9b78cfcb4928a7ecf223eade8af553))

--- modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/render/list/entry/DefaultCPContentListEntryRenderer.java
+++ modules/apps/commerce/commerce-product-content-web/src/main/java/com/liferay/commerce/product/content/web/internal/render/list/entry/DefaultCPContentListEntryRenderer.java
@@ -16,7 +16,6 @@ package com.liferay.commerce.product.content.web.internal.render.list.entry;
 
 import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.product.content.render.list.entry.CPContentListEntryRenderer;
-import com.liferay.commerce.service.CommerceOrderItemLocalService;
 import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.Language;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
@@ -77,9 +76,6 @@ public class DefaultCPContentListEntryRenderer
 			_servletContext, httpServletRequest, httpServletResponse,
 			"/product_publisher/render/list/entry/view.jsp");
 	}
-
-	@Reference
-	private CommerceOrderItemLocalService _commerceOrderItemLocalService;
 
 	@Reference
 	private JSPRenderer _jspRenderer;
