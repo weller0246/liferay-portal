2914e3b02173ff7c5d8d08131c8d88c0aa9d3cb7
Igor Beslic <igor.beslic@liferay.com>
2022-10-24 19:45:05 +0200

    LPS-166317 salesforce-connector - locate correct archives for building Job Scheduler Triggers

--- modules/dxp/apps/salesforce-connector/salesforce-connector/src/main/java/com/liferay/salesforce/connector/internal/instance/lifecycle/AddSalesforceConnectorPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/salesforce-connector/salesforce-connector/src/main/java/com/liferay/salesforce/connector/internal/instance/lifecycle/AddSalesforceConnectorPortalInstanceLifecycleListener.java
@@ -46,13 +46,13 @@ public class AddSalesforceConnectorPortalInstanceLifecycleListener
 	@Override
 	public void portalInstanceRegistered(Company company) throws Exception {
 		_createDispatchTrigger(
-			company, "etl-salesforce-account-connector-0.3.zip");
+			company, "etl-salesforce-account-connector-0.4.zip");
 		_createDispatchTrigger(
-			company, "etl-salesforce-order-connector-0.6.zip");
+			company, "etl-salesforce-order-connector-0.7.zip");
 		_createDispatchTrigger(
-			company, "etl-salesforce-price-list-connector-0.6.zip");
+			company, "etl-salesforce-price-list-connector-0.7.zip");
 		_createDispatchTrigger(
-			company, "etl-salesforce-product-connector-0.3.zip");
+			company, "etl-salesforce-product-connector-0.4.zip");
 	}
 
 	private void _createDispatchTrigger(Company company, String name)
