0ce911516c30611ea0242d9c7e21364fcb1af72d
Vendel Toreki <vendel.toreki@liferay.com>
2022-10-25 15:44:26 +0200

    LPS-167777 Set the correct portletId for each item in PortletDataContext while publishing through ChangeSets (when "From Last Publish Date" option is selected in the form)

--- modules/apps/export-import/export-import-changeset-web/src/main/java/com/liferay/exportimport/changeset/web/internal/portlet/data/handler/ChangesetPortletDataHandler.java
+++ modules/apps/export-import/export-import-changeset-web/src/main/java/com/liferay/exportimport/changeset/web/internal/portlet/data/handler/ChangesetPortletDataHandler.java
@@ -26,7 +26,9 @@ import com.liferay.exportimport.changeset.ChangesetManager;
 import com.liferay.exportimport.changeset.constants.ChangesetPortletKeys;
 import com.liferay.exportimport.kernel.exception.ExportImportRuntimeException;
 import com.liferay.exportimport.kernel.lar.BasePortletDataHandler;
+import com.liferay.exportimport.kernel.lar.ExportImportClassedModelUtil;
 import com.liferay.exportimport.kernel.lar.ExportImportDateUtil;
+import com.liferay.exportimport.kernel.lar.ExportImportHelper;
 import com.liferay.exportimport.kernel.lar.PortletDataContext;
 import com.liferay.exportimport.kernel.lar.PortletDataException;
 import com.liferay.exportimport.kernel.lar.PortletDataHandler;
@@ -42,15 +44,18 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.ClassName;
+import com.liferay.portal.kernel.model.Portlet;
 import com.liferay.portal.kernel.model.StagedModel;
 import com.liferay.portal.kernel.model.TypedModel;
 import com.liferay.portal.kernel.model.adapter.ModelAdapterUtil;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.xml.Element;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
@@ -322,10 +327,54 @@ public class ChangesetPortletDataHandler extends BasePortletDataHandler {
 			}
 		}
 
-		StagedModelDataHandlerUtil.exportStagedModel(
-			portletDataContext, stagedModel);
+		String originalPortletId = portletDataContext.getPortletId();
+
+		try {
+			String portletId = _getPortletIdForClassName(
+				ExportImportClassedModelUtil.getClassName(stagedModel));
+
+			if (Validator.isNotNull(portletId)) {
+				portletDataContext.setPortletId(portletId);
+			}
+
+			StagedModelDataHandlerUtil.exportStagedModel(
+				portletDataContext, stagedModel);
+		}
+		finally {
+			portletDataContext.setPortletId(originalPortletId);
+		}
 
 		return true;
+	}
+
+	private String _getPortletIdForClassName(String className) {
+		List<Portlet> dataSiteLevelPortlets = Collections.emptyList();
+
+		try {
+			dataSiteLevelPortlets =
+				_exportImportHelper.getDataSiteLevelPortlets(
+					CompanyThreadLocal.getCompanyId());
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+
+			return null;
+		}
+
+		for (Portlet dataSiteLevelPortlet : dataSiteLevelPortlets) {
+			PortletDataHandler portletDataHandler =
+				dataSiteLevelPortlet.getPortletDataHandlerInstance();
+
+			if (ArrayUtil.contains(
+					portletDataHandler.getClassNames(), className)) {
+
+				return dataSiteLevelPortlet.getRootPortletId();
+			}
+		}
+
+		return null;
 	}
 
 	private String[] _getPortletResourceNames(
@@ -372,5 +421,8 @@ public class ChangesetPortletDataHandler extends BasePortletDataHandler {
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private ExportImportHelper _exportImportHelper;
 
 }
