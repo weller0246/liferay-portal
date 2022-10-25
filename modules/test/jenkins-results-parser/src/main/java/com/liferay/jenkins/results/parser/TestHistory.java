af8be1973523d770388752fb3e7a7722a043a01c
Michael Hashimoto <michael.hashimoto@liferay.com>
2022-10-25 15:30:31 -0700

    LRCI-3231 Add testray case result URL & the test count to the TestHistory objects

--- modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestHistory.java
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/TestHistory.java
@@ -41,12 +41,41 @@ public class TestHistory {
 		return _statusChanges;
 	}
 
+	public long getTestCount() {
+		return _testCount;
+	}
+
 	public String getTestName() {
 		return _testName;
 	}
 
 	public long getTestrayCaseResultID() {
 		return _testrayCaseResultID;
+	}
+
+	public String getTestrayCaseResultURL() {
+		long testrayCaseResultID = getTestrayCaseResultID();
+
+		if (testrayCaseResultID <= 0L) {
+			return null;
+		}
+
+		BatchHistory batchHistory = getBatchHistory();
+
+		if (batchHistory == null) {
+			return null;
+		}
+
+		JobHistory jobHistory = batchHistory.getJobHistory();
+
+		if (jobHistory == null) {
+			return null;
+		}
+
+		return JenkinsResultsParserUtil.combine(
+			String.valueOf(jobHistory.getTestrayURL()),
+			"/home/-/testray/case_results/",
+			String.valueOf(testrayCaseResultID), "/history");
 	}
 
 	protected TestHistory(BatchHistory batchHistory, JSONObject jsonObject) {
@@ -58,6 +87,7 @@ public class TestHistory {
 		_failureCount = jsonObject.optInt("failureCount");
 		_statusChanges = jsonObject.optInt("statusChanges");
 		_testName = jsonObject.getString("testName");
+		_testCount = jsonObject.optInt("testCount");
 		_testrayCaseResultID = jsonObject.optLong("testrayCaseResultID");
 	}
 
@@ -66,6 +96,7 @@ public class TestHistory {
 	private final BatchHistory _batchHistory;
 	private final int _failureCount;
 	private final int _statusChanges;
+	private final int _testCount;
 	private final String _testName;
 	private final long _testrayCaseResultID;
 
