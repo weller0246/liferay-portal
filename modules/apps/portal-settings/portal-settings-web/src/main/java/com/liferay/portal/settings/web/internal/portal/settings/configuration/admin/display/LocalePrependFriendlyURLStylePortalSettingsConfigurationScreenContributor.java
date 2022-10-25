bdd440405089550bbfb3e80a0109d1dc11543f29
Renato Rego <renato.rego@liferay.com>
2022-10-25 19:01:20 -0300

    LPS-163638 Add new configuration related to locale in friendly URLs

---
+++ modules/apps/portal-settings/portal-settings-web/src/main/java/com/liferay/portal/settings/web/internal/portal/settings/configuration/admin/display/LocalePrependFriendlyURLStylePortalSettingsConfigurationScreenContributor.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.portal.settings.web.internal.portal.settings.configuration.admin.display;
+
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PrefsPropsUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.settings.configuration.admin.display.PortalSettingsConfigurationScreenContributor;
+
+import javax.portlet.PortletPreferences;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Albert Gomes Cabral
+ * @author Renato Rego
+ */
+@Component(service = PortalSettingsConfigurationScreenContributor.class)
+public class
+	LocalePrependFriendlyURLStylePortalSettingsConfigurationScreenContributor
+		extends BaseEditCompanyPortalSettingsConfigurationScreenContributor {
+
+	@Override
+	public String getCategoryKey() {
+		return "localization";
+	}
+
+	@Override
+	public String getJspPath() {
+		return "/locale_prepend_friendly_url_style.jsp";
+	}
+
+	@Override
+	public String getKey() {
+		return "locale-prepend-friendly-url-style";
+	}
+
+	@Override
+	public void setAttributes(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		PortletPreferences companyPortletPreferences =
+			PrefsPropsUtil.getPreferences(themeDisplay.getCompanyId());
+
+		httpServletRequest.setAttribute(
+			PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE,
+			companyPortletPreferences.getValue(
+				PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE, null));
+	}
+
+}
---
+++ modules/apps/portal-settings/portal-settings-web/src/main/resources/META-INF/resources/locale_prepend_friendly_url_style.jsp
@@ -0,0 +1,32 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<aui:input name="<%= Constants.CMD %>" type="hidden" value="<%= Constants.UPDATE %>" />
+
+<div class="row">
+	<div class="col-md-12">
+		<br />
+
+		<aui:select label="locale-prepend-friendly-url-style" name='<%= "settings--" + PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE + "--" %>' type="text" value="<%= (String)request.getAttribute(PropsKeys.LOCALE_PREPEND_FRIENDLY_URL_STYLE) %>">
+			<aui:option label="locale-is-not-automatically-prepended-to-a-url" value="0" />
+			<aui:option label="locale-is-automatically-prepended-to-a-url-when-the-requested-locale-is-not-the-default-locale" value="1" />
+			<aui:option label="locale-is-automatically-prepended-to-every-url" value="2" />
+			<aui:option label="locale-is-automatically-prepended-to-a-url-when-the-requested-locale-is-not-the-default-user-locale" value="3" />
+		</aui:select>
+	</div>
+</div>
