a843f985d0f92fc35c4ae0eedab080dd71b5a2bd
Peter Yoo <peter.yoo@liferay.com>
2022-10-26 15:02:19 -0700

    LRCI-3313 Add TestClassBalancedListSplitter

---
+++ modules/test/jenkins-results-parser/src/main/java/com/liferay/jenkins/results/parser/test/clazz/TestClassBalancedListSplitter.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.jenkins.results.parser.test.clazz;
+
+import com.liferay.jenkins.results.parser.BalancedListSplitter;
+
+/**
+ * @author Peter Yoo
+ */
+public class TestClassBalancedListSplitter
+	extends BalancedListSplitter<TestClass> {
+
+	public TestClassBalancedListSplitter(long maxListWeight) {
+		super(maxListWeight);
+	}
+
+	@Override
+	public long getWeight(TestClass item) {
+		return item.getAverageDuration() + item.getAverageOverheadDuration();
+	}
+
+}
