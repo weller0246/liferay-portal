fbdf854453dbc5647b7c0d53322d099b52b8727d
Joshua Cords <joshua.cords@liferay.com>
2022-10-26 15:27:55 -0700

    LPS-162047 LPS-165550 portal-search-web: Tag - Extend BucketDisplayContext and delete overriding fields
    https://issues.liferay.com/browse/LPS-165550

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/AssetTagsSearchFacetTermDisplayContext.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/AssetTagsSearchFacetTermDisplayContext.java
@@ -17,56 +17,21 @@ package com.liferay.portal.search.web.internal.facet.display.context;
 /**
  * @author André de Oliveira
  */
-public class AssetTagsSearchFacetTermDisplayContext {
+public class AssetTagsSearchFacetTermDisplayContext
+	extends BucketDisplayContext {
 
 	public String getDisplayName() {
 		return _value;
-	}
-
-	public int getFrequency() {
-		return _frequency;
-	}
-
-	public int getPopularity() {
-		return _popularity;
 	}
 
 	public String getValue() {
 		return _value;
 	}
 
-	public boolean isFrequencyVisible() {
-		return _frequencyVisible;
-	}
-
-	public boolean isSelected() {
-		return _selected;
-	}
-
-	public void setFrequency(int frequency) {
-		_frequency = frequency;
-	}
-
-	public void setFrequencyVisible(boolean frequencyVisible) {
-		_frequencyVisible = frequencyVisible;
-	}
-
-	public void setPopularity(int popularity) {
-		_popularity = popularity;
-	}
-
-	public void setSelected(boolean selected) {
-		_selected = selected;
-	}
-
 	public void setValue(String value) {
 		_value = value;
 	}
 
-	private int _frequency;
-	private boolean _frequencyVisible;
-	private int _popularity;
-	private boolean _selected;
 	private String _value;
 
 }
