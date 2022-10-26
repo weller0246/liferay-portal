f0c5ca776c896d52f26f3997b5b7f0aa03bc96d3
Cristina González Castellano <cristina.gonzalez@liferay.com>
2022-10-26 12:32:38 +0100

    LPS-162881 Create Preview version for web contents.

---
+++ modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/PreviewJournalArticleContentDashboardItemVersionAction.java
@@ -0,0 +1,158 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.journal.internal.item.action;
+
+import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemVersionAction;
+import com.liferay.journal.constants.JournalPortletKeys;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.layout.display.page.LayoutDisplayPageProviderTracker;
+import com.liferay.layout.seo.kernel.LayoutSEOLinkManager;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactory;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.HttpComponentsUtil;
+import com.liferay.portal.kernel.util.JavaConstants;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Locale;
+
+import javax.portlet.PortletResponse;
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Cristina González
+ */
+public class PreviewJournalArticleContentDashboardItemVersionAction
+	implements ContentDashboardItemVersionAction {
+
+	public PreviewJournalArticleContentDashboardItemVersionAction(
+		AssetDisplayPageFriendlyURLProvider assetDisplayPageFriendlyURLProvider,
+		HttpServletRequest httpServletRequest, JournalArticle journalArticle,
+		Language language,
+		LayoutDisplayPageProviderTracker layoutDisplayPageProviderTracker,
+		LayoutLocalService layoutLocalService,
+		LayoutSEOLinkManager layoutSEOLinkManager, Portal portal,
+		RequestBackedPortletURLFactory requestBackedPortletURLFactory) {
+
+		_assetDisplayPageFriendlyURLProvider =
+			assetDisplayPageFriendlyURLProvider;
+		_httpServletRequest = httpServletRequest;
+		_journalArticle = journalArticle;
+		_language = language;
+		_layoutDisplayPageProviderTracker = layoutDisplayPageProviderTracker;
+		_layoutLocalService = layoutLocalService;
+		_layoutSEOLinkManager = layoutSEOLinkManager;
+		_portal = portal;
+		_requestBackedPortletURLFactory = requestBackedPortletURLFactory;
+	}
+
+	@Override
+	public String getIcon() {
+		return "view";
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		if (_journalArticle.isDraft()) {
+			return _language.get(locale, "preview-draft");
+		}
+
+		return _language.get(locale, "preview");
+	}
+
+	@Override
+	public String getName() {
+		return "preview";
+	}
+
+	public Type getType() {
+		return Type.BLANK;
+	}
+
+	@Override
+	public String getURL() {
+		ViewJournalArticleContentDashboardItemAction
+			viewJournalArticleContentDashboardItemAction =
+				new ViewJournalArticleContentDashboardItemAction(
+					_assetDisplayPageFriendlyURLProvider, _httpServletRequest,
+					_journalArticle, _language,
+					_layoutDisplayPageProviderTracker, _layoutLocalService,
+					_layoutSEOLinkManager, _portal);
+
+		String displayPageTemplateURL =
+			viewJournalArticleContentDashboardItemAction.getURL();
+
+		if (Validator.isNotNull(displayPageTemplateURL)) {
+			displayPageTemplateURL = HttpComponentsUtil.addParameter(
+				displayPageTemplateURL, "p_l_mode", Constants.PREVIEW);
+
+			return HttpComponentsUtil.addParameter(
+				displayPageTemplateURL, "version",
+				_journalArticle.getVersion());
+		}
+
+		if (Validator.isNull(_journalArticle.getDDMTemplateKey())) {
+			return null;
+		}
+
+		LiferayPortletResponse liferayPortletResponse =
+			_portal.getLiferayPortletResponse(
+				(PortletResponse)_httpServletRequest.getAttribute(
+					JavaConstants.JAVAX_PORTLET_RESPONSE));
+
+		PortletURL portletURL = liferayPortletResponse.createRenderURL();
+
+		return PortletURLBuilder.create(
+			_requestBackedPortletURLFactory.createRenderURL(
+				JournalPortletKeys.JOURNAL)
+		).setMVCPath(
+			"/preview_article_content.jsp"
+		).setRedirect(
+			portletURL
+		).setBackURL(
+			portletURL.toString()
+		).setParameter(
+			"articleId", _journalArticle.getArticleId()
+		).setParameter(
+			"groupId", _journalArticle.getGroupId()
+		).setParameter(
+			"version", _journalArticle.getVersion()
+		).setWindowState(
+			LiferayWindowState.POP_UP
+		).buildString();
+	}
+
+	private final AssetDisplayPageFriendlyURLProvider
+		_assetDisplayPageFriendlyURLProvider;
+	private final HttpServletRequest _httpServletRequest;
+	private final JournalArticle _journalArticle;
+	private final Language _language;
+	private final LayoutDisplayPageProviderTracker
+		_layoutDisplayPageProviderTracker;
+	private final LayoutLocalService _layoutLocalService;
+	private final LayoutSEOLinkManager _layoutSEOLinkManager;
+	private final Portal _portal;
+	private final RequestBackedPortletURLFactory
+		_requestBackedPortletURLFactory;
+
+}
---
+++ modules/apps/content-dashboard/content-dashboard-journal-impl/src/main/java/com/liferay/content/dashboard/journal/internal/item/action/provider/PreviewJournalArticleContentDashboardItemVersionActionProvider.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.content.dashboard.journal.internal.item.action.provider;
+
+import com.liferay.asset.display.page.portlet.AssetDisplayPageFriendlyURLProvider;
+import com.liferay.content.dashboard.item.action.ContentDashboardItemVersionAction;
+import com.liferay.content.dashboard.item.action.provider.ContentDashboardItemVersionActionProvider;
+import com.liferay.content.dashboard.journal.internal.item.action.PreviewJournalArticleContentDashboardItemVersionAction;
+import com.liferay.content.dashboard.journal.internal.item.action.ViewJournalArticleContentDashboardItemAction;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.layout.display.page.LayoutDisplayPageProviderTracker;
+import com.liferay.layout.seo.kernel.LayoutSEOLinkManager;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.portlet.RequestBackedPortletURLFactoryUtil;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Cristina González
+ */
+@Component(service = ContentDashboardItemVersionActionProvider.class)
+public class PreviewJournalArticleContentDashboardItemVersionActionProvider
+	implements ContentDashboardItemVersionActionProvider<JournalArticle> {
+
+	@Override
+	public ContentDashboardItemVersionAction
+		getContentDashboardItemVersionAction(
+			JournalArticle journalArticle,
+			HttpServletRequest httpServletRequest) {
+
+		if (!isShow(journalArticle, httpServletRequest)) {
+			return null;
+		}
+
+		return new PreviewJournalArticleContentDashboardItemVersionAction(
+			_assetDisplayPageFriendlyURLProvider, httpServletRequest,
+			journalArticle, _language, _layoutDisplayPageProviderTracker,
+			_layoutLocalService, _layoutSEOLinkManager, _portal,
+			RequestBackedPortletURLFactoryUtil.create(httpServletRequest));
+	}
+
+	@Override
+	public boolean isShow(
+		JournalArticle journalArticle, HttpServletRequest httpServletRequest) {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		try {
+			if (!_modelResourcePermission.contains(
+					themeDisplay.getPermissionChecker(), journalArticle,
+					ActionKeys.VIEW)) {
+
+				return false;
+			}
+
+			ViewJournalArticleContentDashboardItemAction
+				viewJournalArticleContentDashboardItemAction =
+					new ViewJournalArticleContentDashboardItemAction(
+						_assetDisplayPageFriendlyURLProvider,
+						httpServletRequest, journalArticle, _language,
+						_layoutDisplayPageProviderTracker, _layoutLocalService,
+						_layoutSEOLinkManager, _portal);
+
+			if (Validator.isNull(
+					viewJournalArticleContentDashboardItemAction.getURL()) &&
+				Validator.isNull(journalArticle.getDDMTemplateKey())) {
+
+				return false;
+			}
+
+			return true;
+		}
+		catch (PortalException portalException) {
+			_log.error(portalException);
+
+			return false;
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		PreviewJournalArticleContentDashboardItemVersionActionProvider.class);
+
+	@Reference
+	private AssetDisplayPageFriendlyURLProvider
+		_assetDisplayPageFriendlyURLProvider;
+
+	@Reference
+	private Language _language;
+
+	@Reference
+	private LayoutDisplayPageProviderTracker _layoutDisplayPageProviderTracker;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutSEOLinkManager _layoutSEOLinkManager;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.journal.model.JournalArticle)"
+	)
+	private ModelResourcePermission<JournalArticle> _modelResourcePermission;
+
+	@Reference
+	private Portal _portal;
+
+}
