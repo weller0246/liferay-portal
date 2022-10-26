408db856efa225954e59f3e24347dafbd2994391
Igor Beslic <igor.beslic@liferay.com>
2022-10-26 15:12:33 +0200

    LPS-161981 batch-engine - advanced json reader - support sequential read of json file segments

---
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/auto/deploy/AdvancedJsonReader.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.engine.internal.auto.deploy;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.petra.string.CharPool;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import java.util.Arrays;
+
+/**
+ * @author Igor Beslic
+ */
+public class AdvancedJsonReader<T> {
+
+	public AdvancedJsonReader(InputStream inputStream) {
+		_inputStream = inputStream;
+	}
+
+	public T getObject(String name, Class<T> clazz) throws IOException {
+		ByteArrayOutputStream byteArrayOutputStream =
+			new ByteArrayOutputStream();
+
+		transferJsonObject(name, byteArrayOutputStream);
+
+		ObjectMapper objectMapper = new ObjectMapper();
+
+		return objectMapper.readValue(
+			byteArrayOutputStream.toByteArray(), clazz);
+	}
+
+	public boolean hasKey(String name) throws IOException {
+		_readUntil("\"" + name + "\":");
+
+		if (_inputStream.available() <= 0) {
+			return false;
+		}
+
+		return true;
+	}
+
+	public void transferJsonArray(String name, OutputStream outputStream)
+		throws IOException {
+
+		_readUntil("\"" + name + "\"");
+
+		_transferBlock(
+			CharPool.OPEN_BRACKET, CharPool.CLOSE_BRACKET, outputStream);
+	}
+
+	public void transferJsonObject(String name, OutputStream outputStream)
+		throws IOException {
+
+		_readUntil("\"" + name + "\"");
+
+		_transferBlock(
+			CharPool.OPEN_CURLY_BRACE, CharPool.CLOSE_CURLY_BRACE,
+			outputStream);
+	}
+
+	private void _readUntil(String expression) throws IOException {
+		byte[] bufferTemplate = expression.getBytes();
+
+		byte[] buffer = new byte[bufferTemplate.length];
+
+		int read = _inputStream.read(buffer);
+
+		while (read > -1) {
+			if (Arrays.equals(bufferTemplate, buffer)) {
+				break;
+			}
+
+			for (int i = 1; i < buffer.length; i++) {
+				buffer[i - 1] = buffer[i];
+			}
+
+			read = _inputStream.read(buffer, buffer.length - 1, 1);
+		}
+	}
+
+	private void _transferBlock(
+			char opener, char closer, OutputStream outputStream)
+		throws IOException {
+
+		int read = -1;
+
+		do {
+			read = _inputStream.read();
+
+			if (read == -1) {
+				throw new IllegalStateException(
+					"Unable to find character " + opener);
+			}
+		}
+		while (read != (int)opener);
+
+		int opened = 0;
+
+		do {
+			outputStream.write(read);
+
+			if (read == (int)opener) {
+				opened++;
+			}
+
+			if (read == (int)closer) {
+				opened--;
+			}
+
+			read = _inputStream.read();
+		}
+		while ((opened != 0) && (read > -1));
+	}
+
+	private final InputStream _inputStream;
+
+}
---
+++ modules/apps/batch-engine/batch-engine-service/src/test/java/com/liferay/batch/engine/internal/auto/deploy/AdvancedJsonReaderTest.java
@@ -0,0 +1,114 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.batch.engine.internal.auto.deploy;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.util.FastDateFormatFactoryUtil;
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+import com.liferay.portal.util.FastDateFormatFactoryImpl;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import java.net.URL;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Igor Beslic
+ */
+public class AdvancedJsonReaderTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Before
+	public void setUp() {
+		FastDateFormatFactoryUtil fastDateFormatFactoryUtil =
+			new FastDateFormatFactoryUtil();
+
+		fastDateFormatFactoryUtil.setFastDateFormatFactory(
+			new FastDateFormatFactoryImpl());
+	}
+
+	@Test
+	public void testGetObject() throws Exception {
+		try (InputStream inputStream = new FileInputStream(
+				_getFile("advanced_sample.json"))) {
+
+			AdvancedJsonReader
+				<BatchEngineAutoDeployListener.BatchEngineImportConfiguration>
+					advancedJsonReader = new AdvancedJsonReader<>(inputStream);
+
+			BatchEngineAutoDeployListener.BatchEngineImportConfiguration
+				batchEngineImportConfiguration = advancedJsonReader.getObject(
+					"configuration",
+					BatchEngineAutoDeployListener.
+						BatchEngineImportConfiguration.class);
+
+			Assert.assertEquals(2410, batchEngineImportConfiguration.companyId);
+			Assert.assertEquals(245647, batchEngineImportConfiguration.userId);
+			Assert.assertEquals(
+				"v10.0", batchEngineImportConfiguration.version);
+		}
+	}
+
+	@Test
+	public void testWriteData() throws Exception {
+		try (InputStream inputStream = new FileInputStream(
+				_getFile("advanced_sample.json"))) {
+
+			AdvancedJsonReader
+				<BatchEngineAutoDeployListener.BatchEngineImportConfiguration>
+					advancedJsonReader = new AdvancedJsonReader<>(inputStream);
+
+			try (ByteArrayOutputStream byteArrayOutputStream =
+					new ByteArrayOutputStream()) {
+
+				advancedJsonReader.transferJsonArray(
+					"items", byteArrayOutputStream);
+
+				String content = byteArrayOutputStream.toString();
+
+				Assert.assertTrue(content.startsWith(StringPool.OPEN_BRACKET));
+				Assert.assertTrue(content.endsWith(StringPool.CLOSE_BRACKET));
+				Assert.assertTrue(content.contains("innerArray1"));
+				Assert.assertTrue(content.contains("Array Element 4"));
+			}
+		}
+	}
+
+	private File _getFile(String fileName) throws Exception {
+		URL url = BatchEngineAutoDeployListenerTest.class.getResource(fileName);
+
+		Assert.assertEquals("file", url.getProtocol());
+
+		Path path = Paths.get(url.toURI());
+
+		return path.toFile();
+	}
+
+}
