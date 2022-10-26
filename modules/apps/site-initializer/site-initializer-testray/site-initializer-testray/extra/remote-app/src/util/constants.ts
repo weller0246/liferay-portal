adc919528af1d8c65857196815edd3940f2a38cf
Keven Leone <keven.leone@liferay.com>
2022-10-26 17:04:11 -0300

    LPS-166828 Remove Unused Types

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/util/constants.ts
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/util/constants.ts
@@ -12,8 +12,6 @@
  * details.
  */
 
-import {StatusBarOptions} from '../components/StatusBadge/StatusBadge';
-
 export enum RoleTypes {
 	REGULAR = 1,
 	SITE = 2,
@@ -73,48 +71,7 @@ export const PAGINATION = {
 	ellipsisBuffer: 3,
 };
 
-export enum TEST_STATUS {
-	Blocked = 4,
-	'Did Not Run' = 6,
-	Failed = 3,
-	'In Progress' = 1,
-	'Passed' = 2,
-	'Test Fix' = 7,
-	'Untested' = 0,
-}
-
-export const TEST_STATUS_LABEL: any = {
-	0: 'Untested',
-	1: 'In Progress',
-	2: 'Passed',
-	3: 'Failed',
-	4: 'Blocked',
-	6: 'Did Not Run',
-	7: 'Test Fix',
-};
-
-const getStatusLabel = (status: number): StatusBarOptions =>
-	String(
-		(TEST_STATUS_LABEL as any)[status]
-	)?.toLowerCase() as StatusBarOptions;
-
-export enum SUB_TASK_STATUS {
-	'ABANDONED' = 2,
-	'COMPLETE' = 3,
-	'IN_ANALYSIS' = 1,
-	'OPEN' = 4,
-}
-
-export const SUBTASK_STATUS = {
-	1: {color: 'label-chart-in-analysis', label: 'IN ANALYSIS'},
-	2: {color: 'label-secondary', label: 'ABANDONED'},
-	3: {color: 'label-primary', label: 'COMPLETE'},
-	4: {color: 'label-secondary', label: 'OPEN'},
-};
-
 export const BUILD_STATUS = {
 	0: {color: 'label-chart-in-analysis', label: 'IN ANALYSIS'},
 	2: {color: 'label-secondary', label: 'OPEN'},
 };
-
-export {getStatusLabel};
