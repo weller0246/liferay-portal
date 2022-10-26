36cb50b7499b658ed6fdfdde3e5205446be7481d
Brian Chan <brian.chan@liferay.com>
2022-10-26 09:42:24 +0200

    LPS-163706 Regen

--- modules/apps/account/account-api/src/main/java/com/liferay/account/service/AccountEntryLocalServiceWrapper.java
+++ modules/apps/account/account-api/src/main/java/com/liferay/account/service/AccountEntryLocalServiceWrapper.java
@@ -77,29 +77,6 @@ public class AccountEntryLocalServiceWrapper
 		return _accountEntryLocalService.addAccountEntry(accountEntry);
 	}
 
-	/**
-	 * @param userId the creator's userId. Required.
-	 * @param parentAccountEntryId the parent account's ID. Use 0 if there
-	 is no parent account.
-	 * @param name the account's name. Required.
-	 * @param description the account's description. Optional.
-	 * @param domains an array of email domains associated with the account.
-	 This will define which users can be managed by the account
-	 administrator, as well as optionally restricting membership to
-	 users with matching email address domains. Optional.
-	 * @param emailAddress the email address associated with this account.
-	 Optional.
-	 * @param logoBytes the account's logo bytes. Optional.
-	 * @param taxIdNumber the account's tax ID number. Optional.
-	 * @param type the account's type. Must be one of: "business", "person"
-	 * @param status the account's workflow status. If workflow is enabled for
-	 the AccountEntry model, this parameter is ignored.
-	 * @param serviceContext the service context object. This object can contain
-	 information about the account's asset tags and categories, and
-	 custom fields.
-	 * @return the newly created AccountEntry instance
-	 * @throws PortalException
-	 */
 	@Override
 	public com.liferay.account.model.AccountEntry addAccountEntry(
 			long userId, long parentAccountEntryId, String name,
@@ -637,28 +614,6 @@ public class AccountEntryLocalServiceWrapper
 		return _accountEntryLocalService.updateAccountEntry(accountEntry);
 	}
 
-	/**
-	 * @param accountEntryId the account's ID. Required.
-	 * @param parentAccountEntryId the parent account's ID. Use 0 if there
-	 is no parent account.
-	 * @param name the account's name. Required.
-	 * @param description the account's description. Optional.
-	 * @param domains an array of email domains associated with the account.
-	 This will define which users can be managed by the account
-	 administrator, as well as optionally restricting membership to
-	 users with matching email address domains. Optional.
-	 * @param emailAddress the email address associated with this account.
-	 Optional.
-	 * @param logoBytes the account's logo bytes. Optional.
-	 * @param taxIdNumber the account's tax ID number. Optional.
-	 * @param status the account's workflow status. If workflow is enabled for
-	 the AccountEntry model, this parameter is ignored.
-	 * @param serviceContext the service context object. This object can contain
-	 information about the account's asset tags and categories, and
-	 custom fields.
-	 * @return the updated AccountEntry instance
-	 * @throws PortalException
-	 */
 	@Override
 	public com.liferay.account.model.AccountEntry updateAccountEntry(
 			long accountEntryId, long parentAccountEntryId, String name,
