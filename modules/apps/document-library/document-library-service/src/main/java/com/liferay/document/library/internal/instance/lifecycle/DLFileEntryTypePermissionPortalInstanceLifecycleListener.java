10dc456dd4b54d13551ddf94c89ef58bc08047a6
Alicia García <alicia.garcia@liferay.com>
2022-10-26 14:01:31 +0200

    LPS-165793 add the action view to the power user role for the company for the document types

---
+++ modules/apps/document-library/document-library-service/src/main/java/com/liferay/document/library/internal/instance/lifecycle/DLFileEntryTypePermissionPortalInstanceLifecycleListener.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.document.library.internal.instance.lifecycle;
+
+import com.liferay.document.library.kernel.model.DLFileEntryType;
+import com.liferay.portal.instance.lifecycle.BasePortalInstanceLifecycleListener;
+import com.liferay.portal.instance.lifecycle.PortalInstanceLifecycleListener;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.ResourceConstants;
+import com.liferay.portal.kernel.model.ResourcePermission;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.role.RoleConstants;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alicia García
+ */
+@Component(immediate = true, service = PortalInstanceLifecycleListener.class)
+public class DLFileEntryTypePermissionPortalInstanceLifecycleListener
+	extends BasePortalInstanceLifecycleListener {
+
+	@Override
+	public void portalInstanceRegistered(Company company) throws Exception {
+		Role role = _roleLocalService.fetchRole(
+			company.getCompanyId(), RoleConstants.POWER_USER);
+
+		if (role == null) {
+			return;
+		}
+
+		ResourcePermission resourcePermission =
+			_resourcePermissionLocalService.fetchResourcePermission(
+				company.getCompanyId(), DLFileEntryType.class.getName(),
+				ResourceConstants.SCOPE_COMPANY,
+				String.valueOf(company.getCompanyId()), role.getRoleId());
+
+		if (resourcePermission != null) {
+			return;
+		}
+
+		_resourcePermissionLocalService.setResourcePermissions(
+			company.getCompanyId(), DLFileEntryType.class.getName(),
+			ResourceConstants.SCOPE_COMPANY,
+			String.valueOf(company.getCompanyId()), role.getRoleId(),
+			new String[] {ActionKeys.VIEW});
+	}
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private ResourceActionLocalService _resourceActionLocalService;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+}
