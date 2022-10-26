0ed661947ff94027d239abc0dc58cfe7eb69bb88
Keven Leone <keven.leone@liferay.com>
2022-10-26 18:43:06 -0300

    LPS-166826 Simplify

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/services/rest/TestraySubtask.ts
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/services/rest/TestraySubtask.ts
@@ -13,10 +13,10 @@
  */
 
 import yupSchema from '../../schema/yup';
-import {SUB_TASK_STATUS} from '../../util/constants';
+import {SubTaskStatuses} from '../../util/statuses';
 import {Liferay} from '../liferay';
 import Rest from './Rest';
-import {testrayCaseResultRest} from './TestrayCaseResult';
+import {testrayCaseResultImpl} from './TestrayCaseResult';
 import {TestraySubTask} from './types';
 
 type SubtaskForm = typeof yupSchema.subtask.__outputType & {
@@ -24,6 +24,8 @@ type SubtaskForm = typeof yupSchema.subtask.__outputType & {
 };
 
 class TestraySubtaskImpl extends Rest<SubtaskForm, TestraySubTask> {
+	public UNASSIGNED_USER_ID = 0;
+
 	constructor() {
 		super({
 			adapter: ({
@@ -40,31 +42,27 @@ class TestraySubtaskImpl extends Rest<SubtaskForm, TestraySubTask> {
 				score,
 			}),
 			nestedFields: 'tasks,users',
-			transformData: (subtask) => ({
-				...subtask,
-				task: subtask.r_taskToSubtasks_c_task,
-				user: subtask.r_userToSubtasks_user,
+			transformData: (subTask) => ({
+				...subTask,
+				task: subTask.r_taskToSubtasks_c_task,
+				user: subTask.r_userToSubtasks_user,
 			}),
 			uri: 'subtasks',
 		});
 	}
 
-	public assignTo(subtask: TestraySubTask, userId: number) {
-		const data = {
-			dueStatus: SUB_TASK_STATUS['IN_ANALYSIS'],
-			r_userToSubtasks_userId: userId,
-		};
-
-		return this.fetcher.put(`/subtasks/${subtask.id}`, data);
+	public assignTo(subTask: TestraySubTask, userId: number) {
+		return this.update(subTask.id, {
+			dueStatus: SubTaskStatuses.IN_ANALYSIS,
+			userId,
+		});
 	}
 
-	public assignToMe(subtask: TestraySubTask) {
-		const data = {
-			dueStatus: SUB_TASK_STATUS['IN_ANALYSIS'],
-			r_userToSubtasks_userId: Liferay.ThemeDisplay.getUserId(),
-		};
-
-		return this.fetcher.put(`/subtasks/${subtask.id}`, data);
+	public assignToMe(subTask: TestraySubTask) {
+		return this.update(subTask.id, {
+			dueStatus: SubTaskStatuses.IN_ANALYSIS,
+			userId: Number(Liferay.ThemeDisplay.getUserId()),
+		});
 	}
 
 	public async complete(
@@ -73,26 +71,24 @@ class TestraySubtaskImpl extends Rest<SubtaskForm, TestraySubTask> {
 		dueStatus: string
 	) {
 		await this.update(subtaskId, {
-			dueStatus: SUB_TASK_STATUS['COMPLETE'],
+			dueStatus: SubTaskStatuses.COMPLETE,
 			userId: Number(Liferay.ThemeDisplay.getUserId()),
 		});
 
-		await testrayCaseResultRest.updateBatch(
+		await testrayCaseResultImpl.updateBatch(
 			caseResultIds,
 			caseResultIds.map(() => ({
-				dueStatus: (dueStatus as unknown) as string,
+				dueStatus,
 			}))
 		);
 	}
 
-	public returnToOpen(subtask: TestraySubTask) {
-		const data = {
-			dueStatus: SUB_TASK_STATUS['OPEN'],
-			r_userToSubtasks_userId: 0,
-		};
-
-		return this.fetcher.put(`/subtasks/${subtask.id}`, data);
+	public returnToOpen(subTask: TestraySubTask) {
+		return this.update(subTask.id, {
+			dueStatus: SubTaskStatuses.OPEN,
+			userId: this.UNASSIGNED_USER_ID,
+		});
 	}
 }
 
-export const testraySubtaskImpl = new TestraySubtaskImpl();
+export const testraySubTaskImpl = new TestraySubtaskImpl();
