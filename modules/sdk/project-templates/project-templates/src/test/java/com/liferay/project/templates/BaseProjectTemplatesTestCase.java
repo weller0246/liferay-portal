14b009838040d1a1d7f3fe7d9f2982fccd18fb75
Simon.Jiang <simon.jiang@liferay.com>
2022-10-26 10:28:51 +0800

    LPS-159640 fix test error

--- modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/BaseProjectTemplatesTestCase.java
+++ modules/sdk/project-templates/project-templates/src/test/java/com/liferay/project/templates/BaseProjectTemplatesTestCase.java
@@ -155,7 +155,7 @@ public interface BaseProjectTemplatesTestCase {
 		"gradle/wrapper/gradle-wrapper.properties"
 	};
 
-	public static final String GRADLE_WRAPPER_VERSION = "6.6.1";
+	public static final String GRADLE_WRAPPER_VERSION = "6.9.2";
 
 	public static final String MAVEN_GOAL_BUILD_REST = "rest-builder:build";
 
