88f9547e66b8d7e617ee6453afcae314e5d4f274
drewbrokke <drew.brokke@liferay.com>
2022-10-27 17:11:55 -0500

    LPS-163348 account-test: compare lists ignoring order

--- modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountGroupLocalServiceTest.java
+++ modules/apps/account/account-test/src/testIntegration/java/com/liferay/account/service/test/AccountGroupLocalServiceTest.java
@@ -35,7 +35,6 @@ import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.OrderByComparatorFactoryUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
@@ -174,13 +173,10 @@ public class AccountGroupLocalServiceTest {
 		_addAccountGroup();
 		_addAccountGroup();
 
-		OrderByComparator<AccountGroup> orderByComparator =
-			OrderByComparatorFactoryUtil.create("AccountGroup", "name", true);
-
 		List<AccountGroup> expectedAccountGroups =
 			_accountGroupLocalService.getAccountGroups(
 				TestPropsValues.getCompanyId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS, orderByComparator);
+				QueryUtil.ALL_POS, null);
 
 		expectedAccountGroups = ListUtil.filter(
 			expectedAccountGroups,
@@ -189,12 +185,13 @@ public class AccountGroupLocalServiceTest {
 		BaseModelSearchResult<AccountGroup> baseModelSearchResult =
 			_accountGroupLocalService.searchAccountGroups(
 				TestPropsValues.getCompanyId(), null, QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS, orderByComparator);
+				QueryUtil.ALL_POS, null);
 
 		Assert.assertEquals(
 			expectedAccountGroups.size(), baseModelSearchResult.getLength());
-		Assert.assertEquals(
-			expectedAccountGroups, baseModelSearchResult.getBaseModels());
+		Assert.assertTrue(
+			expectedAccountGroups.containsAll(
+				baseModelSearchResult.getBaseModels()));
 	}
 
 	@Test
