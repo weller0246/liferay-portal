978fd26991004d2c1649090471b4ca675eaebb3d
thaynam <lazaro@thaynam.com>
2022-10-27 11:44:16 -0300

    LPS-165355 Add submit documents to submitForm

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFClaimForm/utils/submitForm.ts
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/extra/remote-app/src/routes/MDFClaimForm/utils/submitForm.ts
@@ -12,6 +12,8 @@
 import {FormikHelpers} from 'formik';
 
 import MDFClaim from '../../../common/interfaces/mdfClaim';
+import createDocumentFolder from '../../../common/services/liferay/headless-delivery/createDocumentFolder';
+import createDocumentFolderDocument from '../../../common/services/liferay/headless-delivery/createDocumentFolderDocument';
 import createMDFClaimActivities from '../../../common/services/liferay/object/claim-activity/createMDFClaimActivities';
 import createMDFClaimActivityBudgets from '../../../common/services/liferay/object/claim-budgets/createMDFClaimActivityBudgets';
 import createMDFClaimDocuments from '../../../common/services/liferay/object/mdf-claim-documents/createMDFClaimDocuments';
@@ -19,61 +21,132 @@ import createMDFClaim from '../../../common/services/liferay/object/mdf-claim/cr
 
 export default async function submitForm(
 	values: MDFClaim,
-	formikHelpers: Omit<FormikHelpers<MDFClaim>, 'setFieldValue'>
+	formikHelpers: Omit<FormikHelpers<MDFClaim>, 'setFieldValue'>,
+	claimParentFolderId: number
 ) {
 	formikHelpers.setSubmitting(true);
 
 	const dtoMDFClaim = await createMDFClaim(values);
-	if (values.reimbursementInvoice) {
-		await createMDFClaimDocuments(
-			values.reimbursementInvoice,
-			dtoMDFClaim?.id
-		);
-	}
-
-	if (values.activities?.length && dtoMDFClaim?.id) {
-		const dtoMDFClaimActivities = await createMDFClaimActivities(
-			dtoMDFClaim.id,
-			values.activities?.filter((activity) => {
-				return activity.selected;
-			})
+	if (dtoMDFClaim.id && claimParentFolderId) {
+		const claimFolder = await createDocumentFolder(
+			claimParentFolderId,
+			`claim#${dtoMDFClaim.id}`
 		);
 
-		if (dtoMDFClaimActivities?.length) {
-			await Promise.all(
+		if (values.reimbursementInvoice && claimFolder.id) {
+			const claimDocument = await createDocumentFolderDocument(
+				claimFolder.id,
+				values.reimbursementInvoice
+			);
+
+			if (claimDocument.contentUrl) {
+				await createMDFClaimDocuments(
+					values.reimbursementInvoice,
+					claimDocument.contentUrl,
+					dtoMDFClaim?.id
+				);
+			}
+		}
+
+		if (values.activities?.length) {
+			const dtoMDFClaimActivities = await createMDFClaimActivities(
+				dtoMDFClaim.id,
+				values.activities?.filter((activity) => {
+					return activity.selected;
+				})
+			);
+
+			if (dtoMDFClaimActivities.length) {
 				values.activities.map(async (activity, index) => {
 					const dtoActivity = dtoMDFClaimActivities[index];
 
-					if (
-						activity.budgets?.length &&
-						dtoActivity?.id &&
-						activity.listQualifiedLeads
-					) {
-						await createMDFClaimDocuments(
-							activity?.listQualifiedLeads,
-							dtoActivity?.id
+					if (dtoActivity.id) {
+						const activityFolder = await createDocumentFolder(
+							claimFolder.id,
+							`activity#${dtoActivity.id}`
 						);
 
-						const dtoMDFClaimBudgets = await createMDFClaimActivityBudgets(
-							dtoActivity.id,
-							activity.budgets?.filter((budget) => {
-								return budget.invoice;
-							})
-						);
-
-						activity.budgets.map(async (budgets, index) => {
-							const dtoBudget = dtoMDFClaimBudgets[index];
-
-							if (dtoBudget?.id && budgets.invoice) {
-								await createMDFClaimDocuments(
-									budgets?.invoice,
-									dtoBudget?.id
+						if (activityFolder.id) {
+							if (activity.listQualifiedLeads) {
+								const activityListQualifiedLeads = await createDocumentFolderDocument(
+									activityFolder.id,
+									activity.listQualifiedLeads
 								);
+
+								if (activityListQualifiedLeads.contentUrl) {
+									createMDFClaimDocuments(
+										activity.listQualifiedLeads,
+										activityListQualifiedLeads.contentUrl,
+										dtoActivity?.id
+									);
+								}
 							}
-						});
+
+							if (activity.documents) {
+								activity.documents.map(async (document) => {
+									const activityDocument = await createDocumentFolderDocument(
+										activityFolder.id,
+										document
+									);
+
+									if (activityDocument.contentUrl) {
+										createMDFClaimDocuments(
+											document,
+											activityDocument.contentUrl,
+											dtoActivity?.id
+										);
+									}
+								});
+							}
+
+							if (activity.budgets?.length) {
+								const dtoMDFClaimBudgets = await createMDFClaimActivityBudgets(
+									dtoActivity.id,
+									activity.budgets?.filter((budget) => {
+										return budget.invoice;
+									})
+								);
+
+								if (dtoMDFClaimBudgets.length) {
+									activity.budgets.map(
+										async (budgets, index) => {
+											const dtoBudget =
+												dtoMDFClaimBudgets[index];
+
+											if (dtoBudget?.id) {
+												const budgetFolder = await createDocumentFolder(
+													activityFolder.id,
+													`budget#${dtoBudget.id}`
+												);
+
+												if (
+													budgets.invoice &&
+													budgetFolder.id
+												) {
+													const budgetDocument = await createDocumentFolderDocument(
+														budgetFolder.id,
+														budgets.invoice
+													);
+
+													if (
+														budgetDocument.contentUrl
+													) {
+														await createMDFClaimDocuments(
+															budgets.invoice,
+															budgetDocument.contentUrl,
+															dtoBudget?.id
+														);
+													}
+												}
+											}
+										}
+									);
+								}
+							}
+						}
 					}
-				})
-			);
+				});
+			}
 		}
 	}
 }
