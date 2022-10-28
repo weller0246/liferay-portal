efcfd8e757ea321653bb6e94efa40fedcdd1d2d6
Bryan Engler <bryan.engler@liferay.com>
2022-10-28 15:33:43 -0700

    LPS-166987 LPS-167421 portal-search-solr8-impl: BaseSearchEngine no longer exists
    https://issues.liferay.com/browse/LPS-167421

--- modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/SolrSearchEngine.java
+++ modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/SolrSearchEngine.java
@@ -15,15 +15,10 @@
 package com.liferay.portal.search.solr8.internal;
 
 import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.search.BaseSearchEngine;
 import com.liferay.portal.kernel.search.IndexSearcher;
 import com.liferay.portal.kernel.search.IndexWriter;
 import com.liferay.portal.kernel.search.SearchEngine;
-import com.liferay.portal.kernel.util.MapUtil;
 
-import java.util.Map;
-
-import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
 
@@ -35,7 +30,7 @@ import org.osgi.service.component.annotations.Reference;
 	property = {"search.engine.id=SYSTEM_ENGINE", "search.engine.impl=Solr"},
 	service = {SearchEngine.class, SolrSearchEngine.class}
 )
-public class SolrSearchEngine extends BaseSearchEngine {
+public class SolrSearchEngine implements SearchEngine {
 
 	@Override
 	public synchronized String backup(long companyId, String backupName) {
@@ -43,8 +38,22 @@ public class SolrSearchEngine extends BaseSearchEngine {
 	}
 
 	@Override
+	public IndexSearcher getIndexSearcher() {
+		return _indexSearcher;
+	}
+
+	@Override
+	public IndexWriter getIndexWriter() {
+		return _indexWriter;
+	}
+
+	@Override
+	public String getVendor() {
+		return "Solr";
+	}
+
+	@Override
 	public void initialize(long companyId) {
-		super.initialize(companyId);
 	}
 
 	@Override
@@ -53,28 +62,16 @@ public class SolrSearchEngine extends BaseSearchEngine {
 
 	@Override
 	public void removeCompany(long companyId) {
-		super.removeCompany(companyId);
 	}
 
 	@Override
 	public synchronized void restore(long companyId, String backupName) {
 	}
 
-	@Override
-	@Reference(target = "(search.engine.impl=Solr)", unbind = "-")
-	public void setIndexSearcher(IndexSearcher indexSearcher) {
-		super.setIndexSearcher(indexSearcher);
-	}
+	@Reference(target = "(search.engine.impl=Solr)")
+	private IndexSearcher _indexSearcher;
 
-	@Override
-	@Reference(target = "(search.engine.impl=Solr)", unbind = "-")
-	public void setIndexWriter(IndexWriter indexWriter) {
-		super.setIndexWriter(indexWriter);
-	}
-
-	@Activate
-	protected void activate(Map<String, Object> properties) {
-		setVendor(MapUtil.getString(properties, "search.engine.impl"));
-	}
+	@Reference(target = "(search.engine.impl=Solr)")
+	private IndexWriter _indexWriter;
 
 }
