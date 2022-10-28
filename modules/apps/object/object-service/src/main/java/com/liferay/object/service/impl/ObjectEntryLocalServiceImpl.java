0b0cb07ef60978c868bfd3ad476c126bd24f8e2e
Brian Chan <brian.chan@liferay.com>
2022-10-28 12:12:09 -0300

    LPS-166239 my bad

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectEntryLocalServiceImpl.java
@@ -2591,7 +2591,7 @@ public class ObjectEntryLocalServiceImpl
 			// Remove the first [ and the last ] in
 			// "[pickListEntryKey1, pickListEntryKey2, pickListEntryKey3]"
 
-			valueString = valueString.substring(1, stringValue.length() - 1);
+			valueString = valueString.substring(1, valueString.length() - 1);
 
 			_setColumn(
 				preparedStatement, index, column.getSQLType(), valueString);
