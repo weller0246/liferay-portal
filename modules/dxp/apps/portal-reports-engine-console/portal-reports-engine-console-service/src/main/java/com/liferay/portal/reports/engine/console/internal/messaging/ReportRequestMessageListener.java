44fd57d7dd21a2c570f905f96c2300e82671ff87
shuyangzhou <shuyang.zhou@liferay.com>
2022-10-29 19:56:11 -0700

    LPS-167144 Merge MessagingConfigurators into their MessageListeners.

--- modules/dxp/apps/portal-reports-engine-console/portal-reports-engine-console-service/src/main/java/com/liferay/portal/reports/engine/console/internal/messaging/ReportRequestMessageListener.java
+++ modules/dxp/apps/portal-reports-engine-console/portal-reports-engine-console-service/src/main/java/com/liferay/portal/reports/engine/console/internal/messaging/ReportRequestMessageListener.java
@@ -17,9 +17,13 @@ package com.liferay.portal.reports.engine.console.internal.messaging;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
 import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
 import com.liferay.portal.reports.engine.ByteArrayReportResultContainer;
 import com.liferay.portal.reports.engine.ReportDesignRetriever;
 import com.liferay.portal.reports.engine.ReportEngine;
@@ -30,7 +34,15 @@ import com.liferay.portal.reports.engine.console.internal.constants.ReportsEngin
 import com.liferay.portal.reports.engine.console.service.EntryLocalService;
 import com.liferay.portal.reports.engine.console.status.ReportStatus;
 
+import java.util.Dictionary;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -41,6 +53,54 @@ import org.osgi.service.component.annotations.Reference;
 	service = MessageListener.class
 )
 public class ReportRequestMessageListener extends BaseMessageListener {
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_PARALLEL,
+				ReportsEngineDestinationNames.REPORT_REQUEST);
+
+		destinationConfiguration.setMaximumQueueSize(_MAXIMUM_QUEUE_SIZE);
+
+		RejectedExecutionHandler rejectedExecutionHandler =
+			new ThreadPoolExecutor.CallerRunsPolicy() {
+
+				@Override
+				public void rejectedExecution(
+					Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"The current thread will handle the request " +
+								"because the graph walker's task queue is at " +
+									"its maximum capacity");
+					}
+
+					super.rejectedExecution(runnable, threadPoolExecutor);
+				}
+
+			};
+
+		destinationConfiguration.setRejectedExecutionHandler(
+			rejectedExecutionHandler);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		Dictionary<String, Object> destinationProperties =
+			HashMapDictionaryBuilder.<String, Object>put(
+				"destination.name", destination.getName()
+			).build();
+
+		_serviceRegistration = bundleContext.registerService(
+			Destination.class, destination, destinationProperties);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
@@ -81,13 +141,20 @@ public class ReportRequestMessageListener extends BaseMessageListener {
 		}
 	}
 
+	private static final int _MAXIMUM_QUEUE_SIZE = 200;
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		ReportRequestMessageListener.class);
+
+	@Reference
+	private DestinationFactory _destinationFactory;
 
 	@Reference
 	private EntryLocalService _entryLocalService;
 
 	@Reference
 	private ReportEngine _reportEngine;
+
+	private ServiceRegistration<Destination> _serviceRegistration;
 
 }
--- modules/dxp/apps/portal-reports-engine-console/portal-reports-engine-console-service/src/main/java/com/liferay/portal/reports/engine/console/internal/messaging/SchedulerEventMessageListener.java
+++ modules/dxp/apps/portal-reports-engine-console/portal-reports-engine-console-service/src/main/java/com/liferay/portal/reports/engine/console/internal/messaging/SchedulerEventMessageListener.java
@@ -14,23 +14,97 @@
 
 package com.liferay.portal.reports.engine.console.internal.messaging;
 
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.messaging.BaseMessageListener;
+import com.liferay.portal.kernel.messaging.Destination;
+import com.liferay.portal.kernel.messaging.DestinationConfiguration;
+import com.liferay.portal.kernel.messaging.DestinationFactory;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
+import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
+import com.liferay.portal.reports.engine.console.configuration.ReportsPortletMessagingConfiguration;
 import com.liferay.portal.reports.engine.console.internal.constants.ReportsEngineConsoleDestinationNames;
 import com.liferay.portal.reports.engine.console.service.EntryLocalService;
 
+import java.util.Dictionary;
+import java.util.Map;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Gavin Wan
  */
 @Component(
+	configurationPid = "com.liferay.portal.reports.engine.console.configuration.ReportsPortletMessagingConfiguration",
 	property = "destination.name=" + ReportsEngineConsoleDestinationNames.REPORTS_SCHEDULER_EVENT,
 	service = MessageListener.class
 )
 public class SchedulerEventMessageListener extends BaseMessageListener {
+
+	@Activate
+	protected void activate(
+		BundleContext bundleContext, Map<String, Object> properties) {
+
+		ReportsPortletMessagingConfiguration
+			reportsPortletMessagingConfiguration =
+				ConfigurableUtil.createConfigurable(
+					ReportsPortletMessagingConfiguration.class, properties);
+
+		DestinationConfiguration destinationConfiguration =
+			new DestinationConfiguration(
+				DestinationConfiguration.DESTINATION_TYPE_PARALLEL,
+				ReportsEngineConsoleDestinationNames.REPORTS_SCHEDULER_EVENT);
+
+		destinationConfiguration.setMaximumQueueSize(
+			reportsPortletMessagingConfiguration.reportMessageQueueSize());
+
+		RejectedExecutionHandler rejectedExecutionHandler =
+			new ThreadPoolExecutor.CallerRunsPolicy() {
+
+				@Override
+				public void rejectedExecution(
+					Runnable runnable, ThreadPoolExecutor threadPoolExecutor) {
+
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"The current thread will handle the request " +
+								"because the report console's task queue is " +
+									"at its maximum capacity");
+					}
+
+					super.rejectedExecution(runnable, threadPoolExecutor);
+				}
+
+			};
+
+		destinationConfiguration.setRejectedExecutionHandler(
+			rejectedExecutionHandler);
+
+		Destination destination = _destinationFactory.createDestination(
+			destinationConfiguration);
+
+		Dictionary<String, Object> destinationProperties =
+			HashMapDictionaryBuilder.<String, Object>put(
+				"destination.name", destination.getName()
+			).build();
+
+		_serviceRegistration = bundleContext.registerService(
+			Destination.class, destination, destinationProperties);
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceRegistration.unregister();
+	}
 
 	@Override
 	protected void doReceive(Message message) throws Exception {
@@ -40,7 +114,15 @@ public class SchedulerEventMessageListener extends BaseMessageListener {
 		_entryLocalService.generateReport(entryId, reportName);
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		SchedulerEventMessageListener.class);
+
+	@Reference
+	private DestinationFactory _destinationFactory;
+
 	@Reference
 	private EntryLocalService _entryLocalService;
+
+	private ServiceRegistration<Destination> _serviceRegistration;
 
 }
