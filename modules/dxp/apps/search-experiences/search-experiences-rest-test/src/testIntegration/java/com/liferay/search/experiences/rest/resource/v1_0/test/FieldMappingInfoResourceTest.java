46554462fb6bbb8b167d2cdda9c63c1d1d8389d7
Wade Cao <wade.cao@liferay.com>
2022-11-01 13:30:12 -0700

    LPS-153813 LPS-167190 search-experiences-rest-test:add an integration test
    https://issues.liferay.com/browse/LPS-167190

--- modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/FieldMappingInfoResourceTest.java
+++ modules/dxp/apps/search-experiences/search-experiences-rest-test/src/testIntegration/java/com/liferay/search/experiences/rest/resource/v1_0/test/FieldMappingInfoResourceTest.java
@@ -15,15 +15,58 @@
 package com.liferay.search.experiences.rest.resource.v1_0.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.search.engine.adapter.SearchEngineAdapter;
+import com.liferay.portal.search.engine.adapter.index.GetIndexIndexRequest;
+import com.liferay.portal.search.engine.adapter.index.GetIndexIndexResponse;
+import com.liferay.portal.search.index.IndexNameBuilder;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.search.experiences.rest.client.dto.v1_0.FieldMappingInfo;
+import com.liferay.search.experiences.rest.client.pagination.Page;
 
-import org.junit.Ignore;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Brian Wing Shun Chan
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class FieldMappingInfoResourceTest
 	extends BaseFieldMappingInfoResourceTestCase {
+
+	@Override
+	@Test
+	public void testGetFieldMappingInfosPage() throws Exception {
+		String prefix =
+			_indexNameBuilder.getIndexName(testCompany.getCompanyId()) +
+				StringPool.DASH;
+
+		GetIndexIndexResponse getIndexIndexResponse =
+			_searchEngineAdapter.execute(
+				new GetIndexIndexRequest(prefix + StringPool.STAR));
+
+		for (String indexName : getIndexIndexResponse.getIndexNames()) {
+			Page<FieldMappingInfo> page =
+				fieldMappingInfoResource.getFieldMappingInfosPage(
+					false, indexName, null);
+
+			Assert.assertNotNull(page.getItems());
+
+			assertValid(page);
+		}
+	}
+
+	@Override
+	@Test
+	public void testGraphQLGetFieldMappingInfosPage() throws Exception {
+		Assert.assertTrue(true);
+	}
+
+	@Inject
+	private IndexNameBuilder _indexNameBuilder;
+
+	@Inject
+	private SearchEngineAdapter _searchEngineAdapter;
+
 }
