e593b8b70e3ac290061ed6f1c4dc3a39b52b62b6
Julius Lee <meng-huan.lee@liferay.com>
2022-11-02 16:34:53 -0700

    LPS-165363 Remove UtilLocator usage, use SAXReaderUtil to provide SAXReader in templates instead of utilLocator.findUtil()

--- portal-impl/src/com/liferay/portal/template/TemplateContextHelper.java
+++ portal-impl/src/com/liferay/portal/template/TemplateContextHelper.java
@@ -90,7 +90,7 @@ import com.liferay.portal.kernel.util.UnicodeFormatter_IW;
 import com.liferay.portal.kernel.util.Validator_IW;
 import com.liferay.portal.kernel.util.WebKeys;
 import com.liferay.portal.kernel.webserver.WebServerServletTokenUtil;
-import com.liferay.portal.kernel.xml.SAXReader;
+import com.liferay.portal.kernel.xml.SAXReaderUtil;
 import com.liferay.portal.struts.Definition;
 import com.liferay.portal.struts.TilesUtil;
 
@@ -627,14 +627,7 @@ public class TemplateContextHelper {
 
 			// SAX reader util
 
-			try {
-				variables.put(
-					"saxReaderUtil",
-					utilLocator.findUtil(SAXReader.class.getName()));
-			}
-			catch (SecurityException securityException) {
-				_log.error(securityException);
-			}
+			variables.put("saxReaderUtil", SAXReaderUtil.getSAXReader());
 		}
 		catch (SecurityException securityException) {
 			_log.error(securityException);
