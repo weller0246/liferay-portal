33cdb385d33ceba4b9619875daf97d92105a0952
Beltran Rengifo <beltran.rengifo@liferay.com>
2022-11-03 15:32:36 +0100

    LPS-167477 Prevent unwanted refetching and memoize functions

--- modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/SidebarPanel.js
+++ modules/apps/content-dashboard/content-dashboard-web/src/main/resources/META-INF/resources/js/components/SidebarPanel.js
@@ -17,6 +17,7 @@ import ClayLoadingIndicator from '@clayui/loading-indicator';
 import {useIsMounted} from '@liferay/frontend-js-react-web';
 import {fetch} from 'frontend-js-web';
 import React, {
+	useCallback,
 	useEffect,
 	useImperativeHandle,
 	useReducer,
@@ -124,13 +125,16 @@ const SidebarPanel = React.forwardRef(
 
 		const [state, dispatch] = useReducer(dataReducer, initialState);
 
-		const safeDispatch = (action) => {
-			if (isMounted()) {
-				dispatch(action);
-			}
-		};
+		const safeDispatch = useCallback(
+			(action) => {
+				if (isMounted()) {
+					dispatch(action);
+				}
+			},
+			[isMounted]
+		);
 
-		const getData = (fetchURL) => {
+		const getData = useCallback(() => {
 			safeDispatch({type: 'LOAD_DATA'});
 
 			fetch(fetchURL, {
@@ -157,15 +161,14 @@ const SidebarPanel = React.forwardRef(
 						type: 'SET_ERROR',
 					});
 				});
-		};
+		}, [fetchURL, safeDispatch]);
 
 		const onCloseHandle = () =>
 			onClose ? onClose() : safeDispatch({type: 'CLOSE_SIDEBAR'});
 
 		useEffect(() => {
-			getData(fetchURL);
-			// eslint-disable-next-line react-hooks/exhaustive-deps
-		}, [fetchURL]);
+			getData();
+		}, [getData]);
 
 		useEffect(() => {
 			CurrentViewRef.current = View;
@@ -199,8 +202,6 @@ const SidebarPanel = React.forwardRef(
 			open: (fetchURL, View) => {
 				CurrentViewRef.current = View;
 
-				getData(fetchURL);
-
 				safeDispatch({type: 'OPEN_SIDEBAR'});
 
 				setFetchURL(fetchURL);
@@ -209,7 +210,7 @@ const SidebarPanel = React.forwardRef(
 
 		return (
 			<Sidebar
-				fetchData={() => getData(fetchURL)}
+				fetchData={getData}
 				onClose={onCloseHandle}
 				open={state.isOpen}
 			>
