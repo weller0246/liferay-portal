2c26d3bb8b60fab045ca7b9d4e47466e9e40abc8
JMachado98 <johnny.machado@liferay.com>
2022-11-03 14:55:37 -0300

    LPS-166501 fixed management bar tooltip message

--- modules/apps/digital-signature/digital-signature-web/src/main/resources/META-INF/resources/js/components/management-toolbar/ManagementToolbarFilterAndOrder.js
+++ modules/apps/digital-signature/digital-signature-web/src/main/resources/META-INF/resources/js/components/management-toolbar/ManagementToolbarFilterAndOrder.js
@@ -235,7 +235,7 @@ export default function ManagementToolbarFilterAndOrder({
 							onClick={() => onSortButtonClick(!asc, column)}
 							symbol="order-arrow"
 							title={Liferay.Language.get(
-								'reverse-sort-direction'
+								'reverse-order-direction'
 							)}
 						/>
 					</ManagementToolbar.Item>
