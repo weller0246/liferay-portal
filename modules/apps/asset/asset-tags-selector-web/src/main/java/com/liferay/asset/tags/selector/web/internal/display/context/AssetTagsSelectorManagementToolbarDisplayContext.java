8f68adb073ace6bb2e32139ff3987f7fde1f32f0
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2022-11-04 13:38:37 -0300

    LPS-167665 (asset-tags-selector-web) Remove method getSearchActionURL

--- modules/apps/asset/asset-tags-selector-web/src/main/java/com/liferay/asset/tags/selector/web/internal/display/context/AssetTagsSelectorManagementToolbarDisplayContext.java
+++ modules/apps/asset/asset-tags-selector-web/src/main/java/com/liferay/asset/tags/selector/web/internal/display/context/AssetTagsSelectorManagementToolbarDisplayContext.java
@@ -20,8 +20,6 @@ import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 
-import javax.portlet.PortletURL;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -53,13 +51,6 @@ public class AssetTagsSelectorManagementToolbarDisplayContext
 	@Override
 	public String getComponentId() {
 		return "assetTagsSelectorManagementToolbar";
-	}
-
-	@Override
-	public String getSearchActionURL() {
-		PortletURL searchActionURL = getPortletURL();
-
-		return searchActionURL.toString();
 	}
 
 	@Override
