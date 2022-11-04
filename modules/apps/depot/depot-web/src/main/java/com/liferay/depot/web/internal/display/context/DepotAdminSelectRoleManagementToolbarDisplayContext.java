b1c33d0ff213231528fd85c0807a82fd3ea9c575
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2022-11-04 15:26:23 -0300

    LPS-167693 (depot-web) Remove method getSearchActionURL

--- modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/display/context/DepotAdminSelectRoleManagementToolbarDisplayContext.java
+++ modules/apps/depot/depot-web/src/main/java/com/liferay/depot/web/internal/display/context/DepotAdminSelectRoleManagementToolbarDisplayContext.java
@@ -21,8 +21,6 @@ import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
 
-import javax.portlet.PortletURL;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -55,13 +53,6 @@ public class DepotAdminSelectRoleManagementToolbarDisplayContext
 	@Override
 	public String getComponentId() {
 		return "depotAdminSelectRoleManagementToolbar";
-	}
-
-	@Override
-	public String getSearchActionURL() {
-		PortletURL searchActionURL = getPortletURL();
-
-		return searchActionURL.toString();
 	}
 
 	@Override
