f2790f0ef76d0636d1a62efc96437a73c0aabf62
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-11-07 14:20:08 +0100

    LPS-167759 Add and adapt methods. We add one method to retrieve a resource by path, the adapt the existing one to retrieve all resources for a collection. This is done recursively since we now have folders

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/model/impl/FragmentCollectionImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/model/impl/FragmentCollectionImpl.java
@@ -14,6 +14,7 @@
 
 package com.liferay.fragment.model.impl;
 
+import com.liferay.document.library.kernel.service.DLAppServiceUtil;
 import com.liferay.fragment.constants.FragmentExportImportConstants;
 import com.liferay.fragment.constants.FragmentPortletKeys;
 import com.liferay.fragment.model.FragmentComposition;
@@ -27,17 +28,18 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Repository;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.portletfilerepository.PortletFileRepositoryUtil;
 import com.liferay.portal.kernel.repository.model.FileEntry;
 import com.liferay.portal.kernel.repository.model.Folder;
-import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalServiceUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.kernel.zip.ZipWriter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -46,18 +48,30 @@ import java.util.List;
 public class FragmentCollectionImpl extends FragmentCollectionBaseImpl {
 
 	@Override
-	public List<FileEntry> getResources() throws PortalException {
-		long groupId = getGroupId();
+	public FileEntry getResource(String path) {
+		try {
+			Repository repository = _getRepository();
 
-		if (groupId == 0) {
-			Company company = CompanyLocalServiceUtil.getCompany(
-				getCompanyId());
-
-			groupId = company.getGroupId();
+			return PortletFileRepositoryUtil.fetchPortletFileEntry(
+				getGroupId(),
+				_getResourcesFolderId(
+					getResourcesFolderId(true), path,
+					repository.getRepositoryId()),
+				_getFileName(path));
+		}
+		catch (PortalException portalException) {
+			if (_log.isDebugEnabled()) {
+				_log.debug("Unable to retrieve file entry ", portalException);
+			}
 		}
 
-		return PortletFileRepositoryUtil.getPortletFileEntries(
-			groupId, getResourcesFolderId());
+		return null;
+	}
+
+	@Override
+	public List<FileEntry> getResources() throws PortalException {
+		return _getResources(
+			PortletFileRepositoryUtil.getPortletFolder(getResourcesFolderId()));
 	}
 
 	@Override
@@ -73,27 +87,7 @@ public class FragmentCollectionImpl extends FragmentCollectionBaseImpl {
 			return _resourcesFolderId;
 		}
 
-		long groupId = getGroupId();
-
-		if (groupId == 0) {
-			User user = UserLocalServiceUtil.getUser(getUserId());
-
-			groupId = user.getGroupId();
-		}
-
-		Repository repository =
-			PortletFileRepositoryUtil.fetchPortletRepository(
-				groupId, FragmentPortletKeys.FRAGMENT);
-
-		if (repository == null) {
-			ServiceContext serviceContext = new ServiceContext();
-
-			serviceContext.setAddGroupPermissions(true);
-			serviceContext.setAddGuestPermissions(true);
-
-			repository = PortletFileRepositoryUtil.addPortletRepository(
-				groupId, FragmentPortletKeys.FRAGMENT, serviceContext);
-		}
+		Repository repository = _getRepository();
 
 		Folder folder = null;
 
@@ -195,9 +189,123 @@ public class FragmentCollectionImpl extends FragmentCollectionBaseImpl {
 		}
 	}
 
+	private String _getFileName(String path) {
+		if (Validator.isNull(path) || path.endsWith(StringPool.SLASH)) {
+			return StringPool.BLANK;
+		}
+
+		if (path.startsWith(StringPool.SLASH)) {
+			path = path.substring(1);
+		}
+
+		int index = path.lastIndexOf(StringPool.SLASH);
+
+		if (index == -1) {
+			return path;
+		}
+
+		return path.substring(index + 1);
+	}
+
+	private Repository _getRepository() throws PortalException {
+		if (_repository != null) {
+			return _repository;
+		}
+
+		long groupId = getGroupId();
+
+		if (groupId == 0) {
+			User user = UserLocalServiceUtil.getUser(getUserId());
+
+			groupId = user.getGroupId();
+		}
+
+		Repository repository =
+			PortletFileRepositoryUtil.fetchPortletRepository(
+				groupId, FragmentPortletKeys.FRAGMENT);
+
+		if (repository == null) {
+			ServiceContext serviceContext = new ServiceContext();
+
+			serviceContext.setAddGroupPermissions(true);
+			serviceContext.setAddGuestPermissions(true);
+
+			repository = PortletFileRepositoryUtil.addPortletRepository(
+				groupId, FragmentPortletKeys.FRAGMENT, serviceContext);
+		}
+
+		_repository = repository;
+
+		return _repository;
+	}
+
+	private List<FileEntry> _getResources(Folder folder)
+		throws PortalException {
+
+		List<FileEntry> resources = new ArrayList<>();
+
+		Repository repository = _getRepository();
+
+		List<Object> foldersAndFileEntriesAndFileShortcuts =
+			DLAppServiceUtil.getFoldersAndFileEntriesAndFileShortcuts(
+				repository.getRepositoryId(), folder.getFolderId(),
+				WorkflowConstants.STATUS_APPROVED, false, QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS);
+
+		for (Object object : foldersAndFileEntriesAndFileShortcuts) {
+			if (object instanceof Folder) {
+				Folder childFolder = (Folder)object;
+
+				resources.addAll(_getResources(childFolder));
+			}
+			else if (object instanceof FileEntry) {
+				resources.add((FileEntry)object);
+			}
+		}
+
+		return resources;
+	}
+
+	private long _getResourcesFolderId(
+		long folderId, String path, long repositoryId) {
+
+		if (Validator.isNull(path) || path.endsWith(StringPool.SLASH)) {
+			return folderId;
+		}
+
+		if (path.startsWith(StringPool.SLASH)) {
+			path = path.substring(1);
+		}
+
+		String[] pathArray = path.split(StringPool.SLASH);
+
+		if (pathArray.length == 1) {
+			return folderId;
+		}
+
+		try {
+			Folder folder = DLAppServiceUtil.getFolder(
+				repositoryId, folderId, pathArray[0]);
+
+			return _getResourcesFolderId(
+				folder.getFolderId(),
+				path.substring(path.indexOf(StringPool.SLASH)), repositoryId);
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"No resource folder found with name " + pathArray[0],
+					exception);
+			}
+
+			return 0;
+		}
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		FragmentCollectionImpl.class);
 
+	private Repository _repository;
 	private long _resourcesFolderId;
 
 }
