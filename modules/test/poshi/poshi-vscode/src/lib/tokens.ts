67234c6ccc39526e295d34d728068f3007b715ff
drewbrokke <drew.brokke@liferay.com>
2022-11-07 17:50:07 -0600

    POSHI-406 poshi-vscode: updates tokens api to allow retrieving all tokens for an entire document

--- modules/test/poshi/poshi-vscode/src/lib/tokens.ts
+++ modules/test/poshi/poshi-vscode/src/lib/tokens.ts
@@ -14,8 +14,14 @@
  */
 
 import {inRange as _inRange} from 'lodash';
+import * as vscode from 'vscode';
 
 const tokenPatternMap = {
+	/**
+	 * matches "test TestCaseName {"
+	 */
+	testCaseName: /test ([A-Z][A-Za-z]+) \{/g,
+
 	/**
 	 * matches "${vari|able}"
 	 */
@@ -66,50 +72,102 @@ const tokenPatternMap = {
 type TokenType = keyof typeof tokenPatternMap;
 
 interface Token {
-	matches: string[];
+	lineNumber?: number;
+	match: Match;
 	type: TokenType;
 }
 
-function getTextMatchesUnderCursor(
-	lineText: string,
-	columnNumber: number,
-	regex: RegExp
-): string[] | undefined {
-	for (const match of lineText.matchAll(regex)) {
-		const {index} = match;
-
-		if (index === undefined) {
-			continue;
-		}
-
-		if (index === -1) {
-			continue;
-		}
-
-		if (_inRange(columnNumber, index, index + match[0].length)) {
-			return Array.from(match);
+function getTokenAtColumn(
+	tokens: Token[],
+	columnNumber: number
+): Token | undefined {
+	for (const token of tokens) {
+		if (token.match.isInRange(columnNumber)) {
+			return token;
 		}
 	}
+}
+function getTokensOfType(tokens: Token[], type: TokenType): Token[] {
+	return tokens.filter((token) => token.type === type);
+}
+
+interface Match {
+	captures: string[];
+	originalText: string;
+	start: number;
+	end: number;
+	length: number;
+	isInRange(columnNumber: number): boolean;
+}
+
+function toMatch(regExpMatchArray: RegExpMatchArray): Match | void {
+	if (regExpMatchArray.index === undefined) return;
+	if (regExpMatchArray.index === -1) return;
+	if (!regExpMatchArray.input) return;
+
+	return {
+		captures: Array.from(regExpMatchArray),
+		originalText: regExpMatchArray.input,
+		start: regExpMatchArray.index,
+		end: regExpMatchArray.index + regExpMatchArray[0].length,
+		length: regExpMatchArray[0].length,
+		isInRange(columnNumber: number) {
+			return _inRange(columnNumber, this.start, this.end);
+		},
+	};
+}
+
+function getTextMatches(lineText: string, regex: RegExp): Match[] {
+	const matches = [];
+
+	for (const regExpMatchArray of lineText.matchAll(regex)) {
+		const match = toMatch(regExpMatchArray);
+
+		if (match) {
+			matches.push(match);
+		}
+	}
+
+	return matches;
 }
 
 export function getToken(
 	text: string,
 	currentIndex: number
 ): Token | undefined {
+	return getTokenAtColumn(getTokens(text), currentIndex);
+}
+
+export function getTokens(text: string): Token[] {
+	const tokens = [];
 	for (const key of Object.keys(tokenPatternMap)) {
 		const type = key as TokenType;
 
-		const matches = getTextMatchesUnderCursor(
+		for (const match of getTextMatches(
 			text,
-			currentIndex,
 			new RegExp(tokenPatternMap[type])
-		);
-
-		if (matches !== undefined) {
-			return {
-				matches,
+		)) {
+			tokens.push({
+				match: match,
 				type,
-			};
+			});
 		}
 	}
+
+	return tokens;
+}
+
+export function getDocumentTokens(document: vscode.TextDocument): Token[] {
+	const tokens: Token[] = [];
+
+	for (let lineNumber = 0; lineNumber < document.lineCount; lineNumber++) {
+		const line = document.lineAt(lineNumber);
+
+		for (const token of getTokens(line.text)) {
+			token.lineNumber = lineNumber + 1;
+			tokens.push(token);
+		}
+	}
+
+	return tokens;
 }
