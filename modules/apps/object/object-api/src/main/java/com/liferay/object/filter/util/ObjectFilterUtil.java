f6f6e5b3d7b8b8c618b3fe0242a8916893e17588
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-11-08 11:39:32 -0300

    LPS-167967 Move ObjectFilterUtil to package com.liferay.object.filter.util

--- modules/apps/object/object-api/src/main/java/com/liferay/object/filter/util/ObjectFilterUtil.java
+++ modules/apps/object/object-api/src/main/java/com/liferay/object/filter/util/ObjectFilterUtil.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.object.util;
+package com.liferay.object.filter.util;
 
 import com.liferay.object.model.ObjectFilter;
 import com.liferay.portal.kernel.json.JSONArray;
