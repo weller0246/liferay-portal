5f132d6c2f08f4718ead600f3fab3efde58f2a65
Gislayne Vitorino <gislayne.vitorino@liferay.com>
2022-11-08 18:11:34 -0300

    LPS-168027 (site-navigation-admin-web) Remove method getSearchActionURL

--- modules/apps/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/display/context/SiteNavigationAdminManagementToolbarDisplayContext.java
+++ modules/apps/site-navigation/site-navigation-admin-web/src/main/java/com/liferay/site/navigation/admin/web/internal/display/context/SiteNavigationAdminManagementToolbarDisplayContext.java
@@ -35,8 +35,6 @@ import com.liferay.site.navigation.model.SiteNavigationMenu;
 
 import java.util.List;
 
-import javax.portlet.PortletURL;
-
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -122,13 +120,6 @@ public class SiteNavigationAdminManagementToolbarDisplayContext
 					LanguageUtil.get(httpServletRequest, "add"));
 			}
 		).build();
-	}
-
-	@Override
-	public String getSearchActionURL() {
-		PortletURL searchActionURL = getPortletURL();
-
-		return searchActionURL.toString();
 	}
 
 	@Override
