2f232f1600342824fdba1d25b75a08c52a286817
Sandro Chinea <sandro.chinea@liferay.com>
2022-11-08 11:49:50 +0100

    LPS-167518 Adapt code for vertical card

--- modules/apps/asset/asset-list-web/src/main/resources/META-INF/resources/js/AssetEntryListDropdownDefaultPropsTransformer.js
+++ modules/apps/asset/asset-list-web/src/main/resources/META-INF/resources/js/AssetEntryListDropdownDefaultPropsTransformer.js
@@ -51,25 +51,33 @@ const ACTIONS = {
 	},
 };
 
-export default function propsTransformer({items, portletNamespace, ...props}) {
+export default function propsTransformer({
+	actions,
+	items,
+	portletNamespace,
+	...props
+}) {
+	const updateItem = (item) => {
+		return {
+			...item,
+			items: item.items.map((child) => ({
+				...child,
+				onClick(event) {
+					const action = child.data?.action;
+
+					if (action) {
+						event.preventDefault();
+
+						ACTIONS[action](child.data, portletNamespace);
+					}
+				},
+			})),
+		};
+	};
+
 	return {
 		...props,
-		items: items.map((item) => {
-			return {
-				...item,
-				items: item.items.map((child) => ({
-					...child,
-					onClick(event) {
-						const action = child.data?.action;
-
-						if (action) {
-							event.preventDefault();
-
-							ACTIONS[action](child.data, portletNamespace);
-						}
-					},
-				})),
-			};
-		}),
+		actions: actions?.map(updateItem),
+		items: items?.map(updateItem),
 	};
 }
