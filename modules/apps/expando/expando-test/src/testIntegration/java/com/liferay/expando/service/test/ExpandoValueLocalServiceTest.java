90e4a674334c7e61b729e58534e77dc3504f5497
Mariano Alvaro Saiz <mariano.alvaro@liferay.com>
2022-11-09 16:16:01 +0100

    LPS-134898 Expose how concurrency/flush can easily affect ExpandoValueLocalServiceImpl.addValue(). Eventhough addValues should be used, consecutive calls to addValue are licit.

--- modules/apps/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
+++ modules/apps/expando/expando-test/src/testIntegration/java/com/liferay/expando/service/test/ExpandoValueLocalServiceTest.java
@@ -252,6 +252,61 @@ public class ExpandoValueLocalServiceTest {
 	}
 
 	@Test
+	public void testAddValueWithFlushInBetween() throws Throwable {
+		ExpandoColumn column1 = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column1", ExpandoColumnConstants.STRING);
+
+		ExpandoColumn column2 = ExpandoTestUtil.addColumn(
+			_expandoTable, "Test Column2", ExpandoColumnConstants.STRING);
+
+		long classPK = CounterLocalServiceUtil.increment();
+
+		// Create initial ExpandoValue for each column
+
+		ExpandoTestUtil.addValues(
+			_expandoTable, classPK,
+			new HashMapBuilder<>().<String, Serializable>put(
+				"Test Column1", "column1-one"
+			).<String, Serializable>put(
+				"Test Column2", "column2-one"
+			).build());
+
+		// Remove cache to force  ExpandoValuePeristence find methods be
+		// executed against the database to force flush in between.
+
+		_entityCache.clearCache(ExpandoValueImpl.class);
+		_finderCache.clearCache(ExpandoValueImpl.class);
+
+		TransactionInvokerUtil.invoke(
+			TransactionConfig.Factory.create(
+				Propagation.REQUIRED, new Class<?>[] {Exception.class}),
+			() -> {
+
+				// Add different value so that ExpandoValue gets modified and
+				// ExpandoRow gets updated. ExpandoRow isn't persisted on the
+				// database yet because transaction hasn't finish.
+
+				ExpandoTestUtil.addValue(
+					_expandoTable, column1, classPK, "column1-two");
+
+				// Add a new different value so that both ExpandoValue and
+				// ExpandoRow are updated. Since ExpandoRow was updated in
+				// the previous addValue we must ensure that ExpandoRow
+				// isn't stale before updating it again. Right now there's
+				// a expandoValuePersistence.fetchByC_R between the moment
+				// that row is retrieved and the moment it gets updated. That
+				// fetch can cause a flush if the value isn't cached so we
+				// must ensure that row has the latest mvcc version before it
+				// gets persisted.
+
+				ExpandoTestUtil.addValue(
+					_expandoTable, column2, classPK, "column2-two");
+
+				return null;
+			});
+	}
+
+	@Test
 	public void testAddWrongValue() throws Exception {
 		ExpandoColumn column = ExpandoTestUtil.addColumn(
 			_expandoTable, "Test Column", ExpandoColumnConstants.STRING);
