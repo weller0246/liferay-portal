dda4e544ea42b33d9eba282410e384a7c73eaa2a
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-11-10 14:26:17 +0100

    LPS-168147 Use the util method instead of the private one, no logic changes

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/info/item/provider/test/DDMFormValuesInfoFieldValuesProviderTest.java
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-test/src/testIntegration/java/com/liferay/dynamic/data/mapping/info/item/provider/test/DDMFormValuesInfoFieldValuesProviderTest.java
@@ -17,51 +17,33 @@ package com.liferay.dynamic.data.mapping.info.item.provider.test;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.dynamic.data.mapping.form.field.type.constants.DDMFormFieldTypeConstants;
 import com.liferay.dynamic.data.mapping.info.item.provider.DDMFormValuesInfoFieldValuesProvider;
-import com.liferay.dynamic.data.mapping.model.DDMForm;
 import com.liferay.dynamic.data.mapping.model.DDMFormField;
 import com.liferay.dynamic.data.mapping.model.DDMFormFieldOptions;
-import com.liferay.dynamic.data.mapping.model.DDMStructure;
-import com.liferay.dynamic.data.mapping.model.DDMTemplate;
 import com.liferay.dynamic.data.mapping.model.LocalizedValue;
-import com.liferay.dynamic.data.mapping.model.Value;
-import com.liferay.dynamic.data.mapping.storage.DDMFormFieldValue;
-import com.liferay.dynamic.data.mapping.storage.DDMFormValues;
-import com.liferay.dynamic.data.mapping.storage.Fields;
-import com.liferay.dynamic.data.mapping.test.util.DDMStructureTestUtil;
-import com.liferay.dynamic.data.mapping.test.util.DDMTemplateTestUtil;
 import com.liferay.dynamic.data.mapping.util.DDMBeanTranslator;
 import com.liferay.dynamic.data.mapping.util.DDMFormValuesToFieldsConverter;
 import com.liferay.info.field.InfoField;
 import com.liferay.info.field.InfoFieldValue;
 import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.info.type.KeyLocalizedLabelPair;
-import com.liferay.journal.constants.JournalArticleConstants;
-import com.liferay.journal.constants.JournalFolderConstants;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.journal.util.JournalConverter;
-import com.liferay.layout.test.util.LayoutFriendlyURLRandomizerBumper;
 import com.liferay.petra.function.UnsafeConsumer;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.json.JSONUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
-import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.util.CalendarFactoryUtil;
-import com.liferay.portal.kernel.util.FriendlyURLNormalizerUtil;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.ListUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
-import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
-import java.util.Calendar;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -113,11 +95,12 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 			).build(),
 			DDMFormFieldTypeConstants.CHECKBOX_MULTIPLE);
 
-		JournalArticle journalArticle = _addJournalArticle(
-			ddmFormField,
+		JournalArticle journalArticle = JournalTestUtil.addJournalArticle(
+			ddmFormField, _ddmFormValuesToFieldsConverter,
 			JSONUtil.putAll(
 				expectedKey1, expectedKey2
-			).toString());
+			).toString(),
+			_group.getGroupId(), _journalConverter);
 
 		_assertGetInfoFieldValues(
 			ddmFormField.getName(), journalArticle,
@@ -144,8 +127,9 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 			).build(),
 			DDMFormFieldTypeConstants.RADIO);
 
-		JournalArticle journalArticle = _addJournalArticle(
-			ddmFormField, expectedKey);
+		JournalArticle journalArticle = JournalTestUtil.addJournalArticle(
+			ddmFormField, _ddmFormValuesToFieldsConverter, expectedKey,
+			_group.getGroupId(), _journalConverter);
 
 		_assertGetInfoFieldValues(
 			ddmFormField.getName(), journalArticle,
@@ -169,8 +153,9 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 			).build(),
 			DDMFormFieldTypeConstants.RADIO);
 
-		JournalArticle journalArticle = _addJournalArticle(
-			ddmFormField, StringPool.BLANK);
+		JournalArticle journalArticle = JournalTestUtil.addJournalArticle(
+			ddmFormField, _ddmFormValuesToFieldsConverter, StringPool.BLANK,
+			_group.getGroupId(), _journalConverter);
 
 		_assertGetInfoFieldValues(
 			ddmFormField.getName(), journalArticle,
@@ -193,11 +178,12 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 			).build(),
 			DDMFormFieldTypeConstants.SELECT);
 
-		JournalArticle journalArticle = _addJournalArticle(
-			ddmFormField,
+		JournalArticle journalArticle = JournalTestUtil.addJournalArticle(
+			ddmFormField, _ddmFormValuesToFieldsConverter,
 			JSONUtil.put(
 				expectedKey
-			).toString());
+			).toString(),
+			_group.getGroupId(), _journalConverter);
 
 		_assertGetInfoFieldValues(
 			ddmFormField.getName(), journalArticle,
@@ -221,67 +207,14 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 			).build(),
 			DDMFormFieldTypeConstants.SELECT);
 
-		JournalArticle journalArticle = _addJournalArticle(
-			ddmFormField, StringPool.BLANK);
+		JournalArticle journalArticle = JournalTestUtil.addJournalArticle(
+			ddmFormField, _ddmFormValuesToFieldsConverter, StringPool.BLANK,
+			_group.getGroupId(), _journalConverter);
 
 		_assertGetInfoFieldValues(
 			ddmFormField.getName(), journalArticle,
 			value -> _assertExpectedKeyLocalizedLabelPairs(
 				value, Collections.emptyMap()));
-	}
-
-	private JournalArticle _addJournalArticle(
-			DDMFormField ddmFormField, String stringValue)
-		throws Exception {
-
-		DDMForm ddmForm = _createDDMForm(ddmFormField);
-
-		DDMStructure ddmStructure = DDMStructureTestUtil.addStructure(
-			_group.getGroupId(), JournalArticle.class.getName(), ddmForm,
-			LocaleUtil.US);
-
-		Fields fields = _ddmFormValuesToFieldsConverter.convert(
-			ddmStructure,
-			_createDDMFormValues(
-				ddmForm, _getDDMFormFieldValue(ddmFormField, stringValue)));
-
-		String content = _journalConverter.getContent(
-			ddmStructure, fields, _group.getGroupId());
-
-		DDMTemplate ddmTemplate = DDMTemplateTestUtil.addTemplate(
-			_group.getGroupId(), ddmStructure.getStructureId(),
-			PortalUtil.getClassNameId(JournalArticle.class));
-
-		User user = TestPropsValues.getUser();
-
-		Calendar displayCal = CalendarFactoryUtil.getCalendar(
-			user.getTimeZone());
-
-		int displayDateDay = displayCal.get(Calendar.DATE);
-		int displayDateMonth = displayCal.get(Calendar.MONTH);
-		int displayDateYear = displayCal.get(Calendar.YEAR);
-		int displayDateHour = displayCal.get(Calendar.HOUR_OF_DAY);
-		int displayDateMinute = displayCal.get(Calendar.MINUTE);
-
-		return _journalArticleLocalService.addArticle(
-			null, TestPropsValues.getUserId(), _group.getGroupId(),
-			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID,
-			JournalArticleConstants.CLASS_NAME_ID_DEFAULT, 0, StringPool.BLANK,
-			true, JournalArticleConstants.VERSION_DEFAULT,
-			RandomTestUtil.randomLocaleStringMap(LocaleUtil.US),
-			RandomTestUtil.randomLocaleStringMap(LocaleUtil.US),
-			HashMapBuilder.put(
-				LocaleUtil.US,
-				FriendlyURLNormalizerUtil.normalize(
-					RandomTestUtil.randomString(
-						LayoutFriendlyURLRandomizerBumper.INSTANCE))
-			).build(),
-			content, ddmStructure.getStructureKey(),
-			ddmTemplate.getTemplateKey(), null, displayDateMonth,
-			displayDateDay, displayDateYear, displayDateHour, displayDateMinute,
-			0, 0, 0, 0, 0, true, 0, 0, 0, 0, 0, true, true, false, null, null,
-			null, null,
-			ServiceContextTestUtil.getServiceContext(_group.getGroupId()));
 	}
 
 	private void _assertExpectedKeyLocalizedLabelPairs(
@@ -333,17 +266,6 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 			infoFieldValue.getValue(LocaleUtil.US));
 	}
 
-	private DDMForm _createDDMForm(DDMFormField ddmFormField) {
-		DDMForm ddmForm = new DDMForm();
-
-		ddmForm.setDefaultLocale(LocaleUtil.US);
-		ddmForm.addAvailableLocale(LocaleUtil.US);
-
-		ddmForm.addDDMFormField(ddmFormField);
-
-		return ddmForm;
-	}
-
 	private DDMFormField _createDDMFormField(
 		boolean multiple, Map<String, String> optionsMap, String type) {
 
@@ -373,33 +295,6 @@ public class DDMFormValuesInfoFieldValuesProviderTest {
 		}
 
 		return ddmFormField;
-	}
-
-	private DDMFormValues _createDDMFormValues(
-		DDMForm ddmForm, DDMFormFieldValue ddmFormFieldValue) {
-
-		DDMFormValues ddmFormValues = new DDMFormValues(ddmForm);
-
-		ddmFormValues.addAvailableLocale(LocaleUtil.US);
-		ddmFormValues.addDDMFormFieldValue(ddmFormFieldValue);
-
-		return ddmFormValues;
-	}
-
-	private DDMFormFieldValue _getDDMFormFieldValue(
-		DDMFormField ddmFormField, String stringValue) {
-
-		DDMFormFieldValue ddmFormFieldValue = new DDMFormFieldValue();
-
-		ddmFormFieldValue.setName(ddmFormField.getName());
-
-		Value value = new LocalizedValue(LocaleUtil.US);
-
-		value.addString(LocaleUtil.US, stringValue);
-
-		ddmFormFieldValue.setValue(value);
-
-		return ddmFormFieldValue;
 	}
 
 	@Inject
