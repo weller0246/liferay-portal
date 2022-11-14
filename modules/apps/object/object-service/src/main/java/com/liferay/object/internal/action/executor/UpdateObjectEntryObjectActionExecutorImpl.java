a02db0fba1ec74efbb2b0831a1b2c61d7d8fa641
Paulo Albuquerque <paulo.albuquerque@liferay.com>
2022-11-14 13:11:10 -0300

    LPS-168471 Fix logic of UpdateObjectEntryObjectActionExecutor

--- modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/UpdateObjectEntryObjectActionExecutorImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/internal/action/executor/UpdateObjectEntryObjectActionExecutorImpl.java
@@ -14,16 +14,37 @@
 
 package com.liferay.object.internal.action.executor;
 
+import com.liferay.dynamic.data.mapping.expression.CreateExpressionRequest;
+import com.liferay.dynamic.data.mapping.expression.DDMExpression;
+import com.liferay.dynamic.data.mapping.expression.DDMExpressionFactory;
 import com.liferay.object.action.executor.ObjectActionExecutor;
 import com.liferay.object.constants.ObjectActionExecutorConstants;
-import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.internal.action.util.ObjectEntryVariablesUtil;
+import com.liferay.object.model.ObjectDefinition;
+import com.liferay.object.model.ObjectField;
+import com.liferay.object.rest.dto.v1_0.ObjectEntry;
+import com.liferay.object.rest.manager.v1_0.ObjectEntryManager;
+import com.liferay.object.rest.manager.v1_0.ObjectEntryManagerRegistry;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectFieldLocalService;
+import com.liferay.object.system.SystemObjectDefinitionMetadataRegistry;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
-import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.GetterUtil;
-import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.UnicodeProperties;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.vulcan.dto.converter.DTOConverterRegistry;
+import com.liferay.portal.vulcan.dto.converter.DefaultDTOConverterContext;
 
 import java.io.Serializable;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -41,16 +62,26 @@ public class UpdateObjectEntryObjectActionExecutorImpl
 			JSONObject payloadJSONObject, long userId)
 		throws Exception {
 
-		_objectEntryLocalService.updateObjectEntry(
-			userId,
-			GetterUtil.getLong(parametersUnicodeProperties.get("classPK")),
-			HashMapBuilder.<String, Serializable>put(
-				parametersUnicodeProperties.get("objectFieldName"),
-				parametersUnicodeProperties.get("objectFieldValue")
-			).build(),
-			new ServiceContext() {
+		ObjectDefinition sourceObjectDefinition =
+			_objectDefinitionLocalService.fetchObjectDefinition(
+				payloadJSONObject.getLong("objectDefinitionId"));
+		User user = _userLocalService.getUser(userId);
+
+		ObjectEntryManager objectEntryManager =
+			_objectEntryManagerRegistry.getObjectEntryManager(
+				sourceObjectDefinition.getStorageType());
+
+		objectEntryManager.updateObjectEntry(
+			new DefaultDTOConverterContext(
+				false, Collections.emptyMap(), _dtoConverterRegistry, null,
+				user.getLocale(), null, user),
+			sourceObjectDefinition,
+			GetterUtil.getLong(payloadJSONObject.getLong("classPK")),
+			new ObjectEntry() {
 				{
-					setUserId(userId);
+					properties = _getValues(
+						sourceObjectDefinition, parametersUnicodeProperties,
+						payloadJSONObject);
 				}
 			});
 	}
@@ -60,7 +91,92 @@ public class UpdateObjectEntryObjectActionExecutorImpl
 		return ObjectActionExecutorConstants.KEY_UPDATE_OBJECT_ENTRY;
 	}
 
+	private Serializable _evaluateExpression(
+			String expression, Map<String, Object> variables)
+		throws Exception {
+
+		DDMExpression<Serializable> ddmExpression =
+			_ddmExpressionFactory.createExpression(
+				CreateExpressionRequest.Builder.newBuilder(
+					expression
+				).build());
+
+		ddmExpression.setVariables(variables);
+
+		return ddmExpression.evaluate();
+	}
+
+	private Map<String, Object> _getValues(
+			ObjectDefinition objectDefinition,
+			UnicodeProperties parametersUnicodeProperties,
+			JSONObject payloadJSONObject)
+		throws Exception {
+
+		Map<String, Object> values = new HashMap<>();
+
+		Map<String, Object> variables =
+			ObjectEntryVariablesUtil.getActionVariables(
+				_dtoConverterRegistry, objectDefinition, payloadJSONObject,
+				_systemObjectDefinitionMetadataRegistry);
+
+		JSONArray jsonArray = _jsonFactory.createJSONArray(
+			parametersUnicodeProperties.get("predefinedValues"));
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			Object value = jsonObject.get("value");
+
+			if (Validator.isNull(value)) {
+				continue;
+			}
+
+			if (!jsonObject.getBoolean("inputAsValue")) {
+				value = _evaluateExpression(value.toString(), variables);
+			}
+
+			values.put(jsonObject.getString("name"), value);
+		}
+
+		List<ObjectField> objectFields =
+			_objectFieldLocalService.getObjectFields(
+				objectDefinition.getObjectDefinitionId(), false);
+
+		objectFields.forEach(
+			objectField -> {
+				if (!values.containsKey(objectField.getName())) {
+					values.put(
+						objectField.getName(),
+						variables.get(objectField.getName()));
+				}
+			});
+
+		return values;
+	}
+
 	@Reference
-	private ObjectEntryLocalService _objectEntryLocalService;
+	private DDMExpressionFactory _ddmExpressionFactory;
+
+	@Reference
+	private DTOConverterRegistry _dtoConverterRegistry;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectEntryManagerRegistry _objectEntryManagerRegistry;
+
+	@Reference
+	private ObjectFieldLocalService _objectFieldLocalService;
+
+	@Reference
+	private SystemObjectDefinitionMetadataRegistry
+		_systemObjectDefinitionMetadataRegistry;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
