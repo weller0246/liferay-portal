021634bcc6a6b42c477cf668d87664a1224039bf
Eva Budai <eva.budai@liferay.com>
2022-11-14 16:57:18 +0100

    LPS-164273 Test assetSearcher returns all entries with the filtering removed

--- modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetSearcherTest.java
+++ modules/apps/asset/asset-test/src/testIntegration/java/com/liferay/asset/search/test/AssetSearcherTest.java
@@ -16,15 +16,23 @@ package com.liferay.asset.search.test;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetEntry;
 import com.liferay.asset.kernel.model.AssetVocabulary;
+import com.liferay.asset.kernel.service.AssetCategoryLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetEntryLocalServiceUtil;
+import com.liferay.asset.kernel.service.AssetVocabularyLocalServiceUtil;
 import com.liferay.asset.kernel.service.persistence.AssetEntryQuery;
 import com.liferay.asset.test.util.AssetTestUtil;
 import com.liferay.blogs.service.BlogsEntryLocalServiceUtil;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalServiceUtil;
+import com.liferay.journal.test.util.JournalTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
@@ -32,8 +40,14 @@ import com.liferay.portal.kernel.test.util.RandomTestUtil;
 import com.liferay.portal.kernel.test.util.SearchContextTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portlet.asset.util.AssetSearcher;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -80,6 +94,58 @@ public class AssetSearcherTest {
 			_internalAssetCategory.getCategoryId(),
 			_publicAssetCategory1.getCategoryId(),
 			_publicAssetCategory2.getCategoryId());
+	}
+
+	@Test
+	public void testGetAssetEntriesFilteredByAllCategoryIds() throws Exception {
+		addAssetVocabulary();
+
+		setGuestUser();
+
+		long[] allAssetCategoryIds = {
+			_assetCategoryIds[0], _assetCategoryIds[1], _assetCategoryIds[2]
+		};
+
+		addAssetEntries(allAssetCategoryIds, new String[0], 3, true);
+
+		AssetSearcher assetSearcher = new AssetSearcher();
+
+		AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
+
+		assetEntryQuery.setAllCategoryIds(allAssetCategoryIds);
+
+		assetSearcher.setAssetEntryQuery(assetEntryQuery);
+
+		Hits hits = assetSearcher.search(
+			SearchContextTestUtil.getSearchContext(_group.getGroupId()));
+
+		Assert.assertEquals(hits.toString(), 3, hits.getLength());
+	}
+
+	@Test
+	public void testGetAssetEntriesFilteredByAnyCategoryIds() throws Exception {
+		addAssetVocabulary();
+
+		setGuestUser();
+
+		long[] anyAssetCategoryIds = {
+			_assetCategoryIds[0], _assetCategoryIds[1], _assetCategoryIds[2]
+		};
+
+		addAssetEntries(anyAssetCategoryIds, new String[0], 3, true);
+
+		AssetSearcher assetSearcher = new AssetSearcher();
+
+		AssetEntryQuery assetEntryQuery = new AssetEntryQuery();
+
+		assetEntryQuery.setAnyCategoryIds(anyAssetCategoryIds);
+
+		assetSearcher.setAssetEntryQuery(assetEntryQuery);
+
+		Hits hits = assetSearcher.search(
+			SearchContextTestUtil.getSearchContext(_group.getGroupId()));
+
+		Assert.assertEquals(hits.toString(), 3, hits.getLength());
 	}
 
 	@Test
@@ -294,6 +360,67 @@ public class AssetSearcherTest {
 		Assert.assertEquals(hits.toString(), 1, hits.getLength());
 	}
 
+	protected void addAssetCategories(long vocabularyId) throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext();
+
+		for (String assetCategoryName : _ASSET_CATEGORY_NAMES) {
+			AssetCategory assetCategory =
+				AssetCategoryLocalServiceUtil.addCategory(
+					TestPropsValues.getUserId(),
+					serviceContext.getScopeGroupId(), assetCategoryName,
+					vocabularyId, serviceContext);
+
+			_assetCategoryIds = ArrayUtil.append(
+				_assetCategoryIds, assetCategory.getCategoryId());
+		}
+	}
+
+	protected List<AssetEntry> addAssetEntries(
+			long[] assetCategoryIds, String[] assetTagNames, int count,
+			boolean manualMode)
+		throws Exception {
+
+		List<AssetEntry> assetEntries = new ArrayList<>();
+
+		for (int i = 0; i < count; i++) {
+			JournalArticle article = JournalTestUtil.addArticle(
+				_group.getGroupId(), RandomTestUtil.randomString(),
+				RandomTestUtil.randomString(100));
+
+			JournalArticleLocalServiceUtil.updateAsset(
+				TestPropsValues.getUserId(), article, assetCategoryIds,
+				assetTagNames, null, null);
+
+			AssetEntry assetEntry = AssetEntryLocalServiceUtil.getEntry(
+				JournalArticle.class.getName(), article.getResourcePrimKey());
+
+			assetEntries.add(assetEntry);
+		}
+
+		return assetEntries;
+	}
+
+	protected void addAssetVocabulary() throws Exception {
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
+
+		serviceContext.setAddGroupPermissions(false);
+		serviceContext.setAddGuestPermissions(false);
+
+		AssetVocabulary assetVocabulary =
+			AssetVocabularyLocalServiceUtil.addVocabulary(
+				TestPropsValues.getUserId(), _group.getGroupId(),
+				RandomTestUtil.randomString(), serviceContext);
+
+		addAssetCategories(assetVocabulary.getVocabularyId());
+	}
+
+	protected void setGuestUser() throws Exception {
+		UserTestUtil.setUser(
+			_userLocalService.getDefaultUser(_group.getCompanyId()));
+	}
+
 	private void _addBlogsEntry(long... assetCategoryIds) throws Exception {
 		ServiceContext serviceContext =
 			ServiceContextTestUtil.getServiceContext(_group.getGroupId());
@@ -306,6 +433,15 @@ public class AssetSearcherTest {
 			1, 1, 1965, 0, 0, true, true, null, StringPool.BLANK, null, null,
 			serviceContext);
 	}
+
+	private static final String[] _ASSET_CATEGORY_NAMES = {
+		"Athletic", "Olympia", "Sport"
+	};
+
+	@Inject
+	private static UserLocalService _userLocalService;
+
+	private long[] _assetCategoryIds = new long[0];
 
 	@DeleteAfterTestRun
 	private Group _group;
