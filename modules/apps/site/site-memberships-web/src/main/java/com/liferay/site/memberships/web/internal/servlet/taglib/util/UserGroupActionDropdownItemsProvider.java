ad6f0eb1c45b60fd4c26ccb69bf4e3c052cf0883
Diego Hu <diego.hu@liferay.com>
2022-11-15 12:02:12 +0100

    LPS-167959 Creates new component

---
+++ modules/apps/site/site-memberships-web/src/main/java/com/liferay/site/memberships/web/internal/servlet/taglib/util/UserGroupActionDropdownItemsProvider.java
@@ -0,0 +1,139 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.memberships.web.internal.servlet.taglib.util;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.model.UserGroup;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.site.memberships.web.internal.display.context.SiteMembershipsDisplayContext;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Diego Hu
+ */
+public class UserGroupActionDropdownItemsProvider {
+
+	public UserGroupActionDropdownItemsProvider(
+		LiferayPortletRequest liferayPortletRequest,
+		LiferayPortletResponse liferayPortletResponse,
+		SiteMembershipsDisplayContext siteMembershipsDisplayContext,
+		UserGroup userGroup) {
+
+		_liferayPortletResponse = liferayPortletResponse;
+		_siteMembershipsDisplayContext = siteMembershipsDisplayContext;
+		_userGroup = userGroup;
+
+		_httpServletRequest = PortalUtil.getHttpServletRequest(
+			liferayPortletRequest);
+		_themeDisplay = (ThemeDisplay)liferayPortletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getActionDropdownItems() throws Exception {
+		return DropdownItemListBuilder.add(
+			() -> GroupPermissionUtil.contains(
+				_themeDisplay.getPermissionChecker(),
+				_siteMembershipsDisplayContext.getGroup(),
+				ActionKeys.ASSIGN_USER_ROLES),
+			dropdownItem -> {
+				dropdownItem.putData("action", "assignUserGroupRole");
+				dropdownItem.putData(
+					"assignURL",
+					PortletURLBuilder.createActionURL(
+						_liferayPortletResponse
+					).setMVCPath(
+						"/user_groups_roles.jsp"
+					).setParameter(
+						"groupId", _siteMembershipsDisplayContext.getGroupId()
+					).setParameter(
+						"userGroupId", _userGroup.getUserGroupId()
+					).setWindowState(
+						LiferayWindowState.POP_UP
+					).buildString());
+				dropdownItem.putData(
+					"userGroupId", String.valueOf(_userGroup.getUserGroupId()));
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "assign-roles"));
+			}
+		).add(
+			dropdownItem -> {
+				dropdownItem.putData("action", "unassignUserGroupRole");
+				dropdownItem.putData(
+					"unassignURL",
+					PortletURLBuilder.createActionURL(
+						_liferayPortletResponse
+					).setMVCPath(
+						"/user_groups_roles.jsp"
+					).setParameter(
+						"assignRoles", Boolean.FALSE
+					).setParameter(
+						"groupId", _siteMembershipsDisplayContext.getGroupId()
+					).setParameter(
+						"userGroupId", _userGroup.getUserGroupId()
+					).setWindowState(
+						LiferayWindowState.POP_UP
+					).buildString());
+				dropdownItem.putData(
+					"userGroupId", String.valueOf(_userGroup.getUserGroupId()));
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "unassign-roles"));
+			}
+		).add(
+			() -> GroupPermissionUtil.contains(
+				_themeDisplay.getPermissionChecker(),
+				_siteMembershipsDisplayContext.getGroup(),
+				ActionKeys.ASSIGN_MEMBERS),
+			dropdownItem -> {
+				dropdownItem.putData("action", "deleteUserGroup");
+				dropdownItem.putData(
+					"deleteGroupUserGroupsURL",
+					PortletURLBuilder.createActionURL(
+						_liferayPortletResponse
+					).setActionName(
+						"deleteGroupUserGroups"
+					).setRedirect(
+						_themeDisplay.getURLCurrent()
+					).setParameter(
+						"groupId", _siteMembershipsDisplayContext.getGroupId()
+					).setParameter(
+						"removeUserGroupId", _userGroup.getUserGroupId()
+					).buildString());
+				dropdownItem.setIcon("trash");
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "delete"));
+			}
+		).build();
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final LiferayPortletResponse _liferayPortletResponse;
+	private final SiteMembershipsDisplayContext _siteMembershipsDisplayContext;
+	private final ThemeDisplay _themeDisplay;
+	private final UserGroup _userGroup;
+
+}
---
+++ modules/apps/site/site-memberships-web/src/main/resources/META-INF/resources/js/UserGroupsDefaultPropsTransformer.js
@@ -0,0 +1,142 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import {openConfirmModal} from 'frontend-js-web';
+
+const ACTIONS = {
+	assignUserGroupRole(itemData, portletNamespace) {
+		const addUserGroupGroupRoleFm =
+			document[`${portletNamespace}addUserGroupGroupRoleFm`];
+
+		if (!addUserGroupGroupRoleFm) {
+			return;
+		}
+
+		Liferay.Util.setFormValues(addUserGroupGroupRoleFm, {
+			userGroupId: itemData.userGroupId,
+		});
+
+		Liferay.Util.openSelectionModal({
+			buttonAddLabel: Liferay.Language.get('done'),
+			multiple: true,
+			onSelect(selectedItems) {
+				if (!selectedItems) {
+					return;
+				}
+
+				const input = document.createElement('input');
+
+				input.name = `${portletNamespace}rowIds`;
+
+				const selectedUserGroupIds = Array.prototype.map.call(
+					selectedItems,
+					(item) => item.value
+				);
+
+				input.value = selectedUserGroupIds.join();
+
+				addUserGroupGroupRoleFm.appendChild(input);
+
+				submitForm(addUserGroupGroupRoleFm);
+			},
+			title: Liferay.Language.get('assign-roles'),
+			url: itemData.assignURL,
+		});
+	},
+
+	deleteUserGroup(itemData) {
+		openConfirmModal({
+			message: Liferay.Language.get(
+				'are-you-sure-you-want-to-delete-this'
+			),
+			onConfirm: (isConfirmed) =>
+				isConfirmed &&
+				submitForm(document.hrefFm, itemData.deleteGroupUserGroupsURL),
+		});
+	},
+
+	unassignUserGroupRole(itemData, portletNamespace) {
+		const unassignUserGroupGroupRoleFm =
+			document[`${portletNamespace}unassignUserGroupGroupRoleFm`];
+
+		if (!unassignUserGroupGroupRoleFm) {
+			return;
+		}
+
+		Liferay.Util.setFormValues(unassignUserGroupGroupRoleFm, {
+			userGroupId: itemData.userGroupId,
+		});
+
+		Liferay.Util.openSelectionModal({
+			buttonAddLabel: Liferay.Language.get('done'),
+			multiple: true,
+			onSelect(selectedItems) {
+				if (!selectedItems) {
+					return;
+				}
+
+				const input = document.createElement('input');
+
+				input.name = `${portletNamespace}rowIds`;
+
+				const selectedUserGroupIds = Array.prototype.map.call(
+					selectedItems,
+					(item) => item.value
+				);
+
+				input.value = selectedUserGroupIds.join();
+
+				unassignUserGroupGroupRoleFm.appendChild(input);
+
+				submitForm(unassignUserGroupGroupRoleFm);
+			},
+			title: Liferay.Language.get('unassign-roles'),
+			url: itemData.unassignURL,
+		});
+	},
+};
+
+export default function propsTransformer({
+	actions,
+	items,
+	portletNamespace,
+	...props
+}) {
+	const updateItem = (item) => {
+		const newItem = {
+			...item,
+			onClick(event) {
+				const action = item.data?.action;
+
+				if (action) {
+					event.preventDefault();
+
+					ACTIONS[action]?.(item.data, portletNamespace);
+				}
+			},
+		};
+
+		if (Array.isArray(item.items)) {
+			newItem.items = item.items.map(updateItem);
+		}
+
+		return newItem;
+	};
+
+	return {
+		...props,
+		actions: actions?.map(updateItem),
+		items: items?.map(updateItem),
+	};
+}
