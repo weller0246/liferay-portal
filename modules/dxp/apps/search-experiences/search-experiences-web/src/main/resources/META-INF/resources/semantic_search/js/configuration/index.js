6cdd30b69f16843b769a5c88b344da75e3aef255
Olivia Yu <olivia.yu@liferay.com>
2022-11-15 12:14:11 -0800

    LPS-168167 LPS-168293 search-experiences-web: Rename some labels in semantic search configuration

--- modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/semantic_search/js/configuration/index.js
+++ modules/dxp/apps/search-experiences/search-experiences-web/src/main/resources/META-INF/resources/semantic_search/js/configuration/index.js
@@ -230,7 +230,7 @@ export default function ({
 		<div className="semantic-search-settings-root">
 			<div className="sheet-section">
 				<h3 className="sheet-subtitle">
-					{Liferay.Language.get('transform-provider-settings')}
+					{Liferay.Language.get('sentence-transformer-settings')}
 				</h3>
 
 				<ClayForm.Group>
@@ -255,7 +255,7 @@ export default function ({
 					items={transformToLabelValueArray(
 						availableSentenceTransformProviders
 					)}
-					label={Liferay.Language.get('sentence-transform-provider')}
+					label={Liferay.Language.get('sentence-transformer')}
 					name={`${namespace}sentenceTransformProvider`}
 					onBlur={_handleInputBlur('sentenceTransformProvider')}
 					onChange={_handleInputChange('sentenceTransformProvider')}
@@ -427,7 +427,7 @@ export default function ({
 
 			<div className="sheet-section">
 				<h3 className="sheet-subtitle">
-					{Liferay.Language.get('indexing-settings')}
+					{Liferay.Language.get('index-settings')}
 				</h3>
 
 				<Input
@@ -499,21 +499,27 @@ export default function ({
 				/>
 			</div>
 
-			<Input
-				error={formik.errors.cacheTimeout}
-				helpText={Liferay.Language.get(
-					'sentence-transformer-cache-timeout-help'
-				)}
-				label={Liferay.Language.get('cache-timeout')}
-				name={`${namespace}cacheTimeout`}
-				onBlur={_handleInputBlur('cacheTimeout')}
-				onChange={_handleInputChange('cacheTimeout')}
-				options={{min: 0}}
-				required
-				touched={formik.touched.cacheTimeout}
-				type="number"
-				value={formik.values.cacheTimeout}
-			/>
+			<div className="sheet-section">
+				<h3 className="sheet-subtitle">
+					{Liferay.Language.get('search-settings')}
+				</h3>
+
+				<Input
+					error={formik.errors.cacheTimeout}
+					helpText={Liferay.Language.get(
+						'sentence-transformer-cache-timeout-help'
+					)}
+					label={Liferay.Language.get('cache-timeout')}
+					name={`${namespace}cacheTimeout`}
+					onBlur={_handleInputBlur('cacheTimeout')}
+					onChange={_handleInputChange('cacheTimeout')}
+					options={{min: 0}}
+					required
+					touched={formik.touched.cacheTimeout}
+					type="number"
+					value={formik.values.cacheTimeout}
+				/>
+			</div>
 		</div>
 	);
 }
