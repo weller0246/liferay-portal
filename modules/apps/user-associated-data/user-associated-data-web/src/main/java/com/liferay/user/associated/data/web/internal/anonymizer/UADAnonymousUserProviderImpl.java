6340066143970bb8658c40a1ac7ab726df52c777
Roland Pákai <roland.pakai@liferay.com>
2022-11-16 18:41:13 +0100

    LPS-168719 Fix randomString length when creating anonymous user - take into account the policy

--- modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/anonymizer/UADAnonymousUserProviderImpl.java
+++ modules/apps/user-associated-data/user-associated-data-web/src/main/java/com/liferay/user/associated/data/web/internal/anonymizer/UADAnonymousUserProviderImpl.java
@@ -21,9 +21,11 @@ import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.PasswordPolicy;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.PasswordPolicyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.HashMapDictionaryBuilder;
@@ -78,7 +80,11 @@ public class UADAnonymousUserProviderImpl implements UADAnonymousUserProvider {
 	private User _createAnonymousUser(long companyId) throws Exception {
 		long creatorUserId = 0;
 
-		String randomString = StringUtil.randomString();
+		PasswordPolicy passwordPolicy =
+			_passwordPolicyLocalService.getDefaultPasswordPolicy(companyId);
+
+		String randomString = StringUtil.randomString(
+			passwordPolicy.getMinLength());
 
 		long counter = _counterLocalService.increment(
 			UADAnonymousUserProvider.class.getName());
@@ -181,6 +187,9 @@ public class UADAnonymousUserProviderImpl implements UADAnonymousUserProvider {
 
 	@Reference
 	private CounterLocalService _counterLocalService;
+
+	@Reference
+	private PasswordPolicyLocalService _passwordPolicyLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
