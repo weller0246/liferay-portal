59f80b33326810a305f8669aa0a2a8ac7644cc36
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-11-16 15:19:42 +0100

    LPS-168582 Adds new integration test for propagation of contributed fragments

--- modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/contributor/test/FragmentCollectionContributorTest.java
+++ modules/apps/fragment/fragment-test/src/testIntegration/java/com/liferay/fragment/contributor/test/FragmentCollectionContributorTest.java
@@ -19,14 +19,30 @@ import com.liferay.fragment.constants.FragmentConstants;
 import com.liferay.fragment.contributor.FragmentCollectionContributor;
 import com.liferay.fragment.contributor.FragmentCollectionContributorRegistry;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.service.FragmentEntryLinkLocalService;
 import com.liferay.fragment.service.FragmentEntryLocalServiceUtil;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
 import com.liferay.portal.kernel.util.HashMapBuilder;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.MapUtil;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -35,6 +51,7 @@ import java.util.Locale;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -54,7 +71,102 @@ public class FragmentCollectionContributorTest {
 	@ClassRule
 	@Rule
 	public static final AggregateTestRule aggregateTestRule =
-		new LiferayIntegrationTestRule();
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_serviceContext = ServiceContextTestUtil.getServiceContext(
+			_group.getGroupId());
+	}
+
+	@Test
+	public void testPropagateContributedFragmentEntry() throws Exception {
+		String fragmentCollectionContributorKey = RandomTestUtil.randomString();
+
+		String fragmentEntryKey = StringBundler.concat(
+			fragmentCollectionContributorKey, StringPool.DASH,
+			RandomTestUtil.randomString());
+
+		Layout layout = LayoutTestUtil.addTypeContentLayout(_group);
+
+		long segmentsExperienceId =
+			_segmentsExperienceLocalService.fetchDefaultSegmentsExperienceId(
+				layout.getPlid());
+
+		FragmentEntryLink fragmentEntryLink =
+			_fragmentEntryLinkLocalService.addFragmentEntryLink(
+				TestPropsValues.getUserId(), _group.getGroupId(), 0, 0,
+				segmentsExperienceId, layout.getPlid(), StringPool.BLANK,
+				"<div data-lfr-editable-id=\"editable-1\" " +
+					"data-lfr-editable-type=\"rich-text\">EDITABLE 1</div>",
+				StringPool.BLANK, StringPool.BLANK, null, StringPool.BLANK, 0,
+				fragmentEntryKey, FragmentConstants.TYPE_COMPONENT,
+				_serviceContext);
+
+		String modifiedHtml = StringBundler.concat(
+			"<div data-lfr-editable-id=\"editable-1\" ",
+			"data-lfr-editable-type=\"rich-text\">EDITABLE 1</div>",
+			"<div data-lfr-editable-id=\"editable-2\" ",
+			"data-lfr-editable-type=\"rich-text\">EDITABLE 2</div>");
+
+		ServiceRegistration<?> serviceRegistration = _getServiceRegistration(
+			new TestFragmentCollectionContributor(
+				fragmentCollectionContributorKey,
+				HashMapBuilder.put(
+					FragmentConstants.TYPE_COMPONENT,
+					_getFragmentEntry(
+						fragmentEntryKey, modifiedHtml,
+						FragmentConstants.TYPE_COMPONENT)
+				).build()));
+
+		try {
+			FragmentEntryLink persistedFragmentEntryLink =
+				_fragmentEntryLinkLocalService.getFragmentEntryLink(
+					fragmentEntryLink.getFragmentEntryLinkId());
+
+			Assert.assertEquals(
+				modifiedHtml, persistedFragmentEntryLink.getHtml());
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				persistedFragmentEntryLink.getEditableValues());
+
+			JSONObject editableFragmentEntryProcessorJSONObject =
+				jsonObject.getJSONObject(
+					"com.liferay.fragment.entry.processor.editable." +
+						"EditableFragmentEntryProcessor");
+
+			Assert.assertEquals(
+				editableFragmentEntryProcessorJSONObject.toString(), 2,
+				editableFragmentEntryProcessorJSONObject.length());
+
+			Assert.assertTrue(
+				editableFragmentEntryProcessorJSONObject.has("editable-1"));
+
+			JSONObject editable1JSONObject =
+				editableFragmentEntryProcessorJSONObject.getJSONObject(
+					"editable-1");
+
+			Assert.assertEquals(
+				"EDITABLE 1", editable1JSONObject.get("defaultValue"));
+
+			Assert.assertTrue(
+				editableFragmentEntryProcessorJSONObject.has("editable-2"));
+
+			JSONObject editable2JSONObject =
+				editableFragmentEntryProcessorJSONObject.getJSONObject(
+					"editable-2");
+
+			Assert.assertEquals(
+				"EDITABLE 2", editable2JSONObject.get("defaultValue"));
+		}
+		finally {
+			serviceRegistration.unregister();
+		}
+	}
 
 	@Test
 	public void testRegisterContributedFragmentEntries() {
@@ -162,6 +274,17 @@ public class FragmentCollectionContributorTest {
 	@Inject
 	private FragmentCollectionContributorRegistry
 		_fragmentCollectionContributorRegistry;
+
+	@Inject
+	private FragmentEntryLinkLocalService _fragmentEntryLinkLocalService;
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	private ServiceContext _serviceContext;
 
 	private static class TestFragmentCollectionContributor
 		implements FragmentCollectionContributor {
