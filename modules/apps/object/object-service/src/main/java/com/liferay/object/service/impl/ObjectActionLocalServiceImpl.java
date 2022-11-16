79edafb0e08b15c0e59bbe1ef7d8442a19c4cdb2
Carolina Barbosa <carolina.barbosa@liferay.com>
2022-11-16 15:53:02 -0300

    LPS-168447 Update property "ObjectActionTriggerKey" if the object definition has not been published

--- modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
+++ modules/apps/object/object-service/src/main/java/com/liferay/object/service/impl/ObjectActionLocalServiceImpl.java
@@ -194,8 +194,6 @@ public class ObjectActionLocalServiceImpl
 		_validateErrorMessage(errorMessageMap, objectActionTriggerKey);
 		_validateLabel(labelMap);
 		_validateObjectActionExecutorKey(objectActionExecutorKey);
-		_validateObjectActionTriggerKey(
-			conditionExpression, objectActionTriggerKey);
 		_validateParameters(
 			conditionExpression, objectActionExecutorKey,
 			objectActionTriggerKey, parametersUnicodeProperties);
@@ -210,7 +208,6 @@ public class ObjectActionLocalServiceImpl
 			errorMessageMap, LocaleUtil.getSiteDefault());
 		objectAction.setLabelMap(labelMap, LocaleUtil.getSiteDefault());
 		objectAction.setObjectActionExecutorKey(objectActionExecutorKey);
-		objectAction.setObjectActionTriggerKey(objectActionTriggerKey);
 		objectAction.setParameters(parametersUnicodeProperties.toString());
 		objectAction.setStatus(ObjectActionConstants.STATUS_NEVER_RAN);
 
@@ -224,8 +221,11 @@ public class ObjectActionLocalServiceImpl
 
 		_validateName(
 			objectActionId, objectDefinition.getObjectDefinitionId(), name);
+		_validateObjectActionTriggerKey(
+			conditionExpression, objectActionTriggerKey);
 
 		objectAction.setName(name);
+		objectAction.setObjectActionTriggerKey(objectActionTriggerKey);
 
 		return objectActionPersistence.update(objectAction);
 	}
