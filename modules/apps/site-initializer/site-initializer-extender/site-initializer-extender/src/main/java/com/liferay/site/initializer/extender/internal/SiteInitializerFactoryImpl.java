5225d91c1e5171befe14133d2febd7bfbe5fe16d
shuyangzhou <shuyang.zhou@liferay.com>
2022-11-17 12:00:46 -0800

    LPS-151811 Create SiteInitializerFactory to support creating transient SiteInitializer from folder.

---
+++ modules/apps/site-initializer/site-initializer-extender/site-initializer-extender/src/main/java/com/liferay/site/initializer/extender/internal/SiteInitializerFactoryImpl.java
@@ -0,0 +1,358 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.site.initializer.extender.internal;
+
+import com.liferay.account.service.AccountRoleLocalService;
+import com.liferay.asset.kernel.service.AssetCategoryLocalService;
+import com.liferay.asset.list.service.AssetListEntryLocalService;
+import com.liferay.client.extension.service.ClientExtensionEntryLocalService;
+import com.liferay.document.library.util.DLURLHelper;
+import com.liferay.dynamic.data.mapping.service.DDMStructureLocalService;
+import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.dynamic.data.mapping.util.DefaultDDMStructureHelper;
+import com.liferay.fragment.importer.FragmentsImporter;
+import com.liferay.headless.admin.list.type.resource.v1_0.ListTypeDefinitionResource;
+import com.liferay.headless.admin.list.type.resource.v1_0.ListTypeEntryResource;
+import com.liferay.headless.admin.taxonomy.resource.v1_0.TaxonomyCategoryResource;
+import com.liferay.headless.admin.taxonomy.resource.v1_0.TaxonomyVocabularyResource;
+import com.liferay.headless.admin.user.resource.v1_0.AccountResource;
+import com.liferay.headless.admin.user.resource.v1_0.AccountRoleResource;
+import com.liferay.headless.admin.user.resource.v1_0.OrganizationResource;
+import com.liferay.headless.admin.user.resource.v1_0.UserAccountResource;
+import com.liferay.headless.admin.workflow.resource.v1_0.WorkflowDefinitionResource;
+import com.liferay.headless.delivery.resource.v1_0.DocumentFolderResource;
+import com.liferay.headless.delivery.resource.v1_0.DocumentResource;
+import com.liferay.headless.delivery.resource.v1_0.KnowledgeBaseArticleResource;
+import com.liferay.headless.delivery.resource.v1_0.KnowledgeBaseFolderResource;
+import com.liferay.headless.delivery.resource.v1_0.StructuredContentFolderResource;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.layout.importer.LayoutsImporter;
+import com.liferay.layout.page.template.service.LayoutPageTemplateEntryLocalService;
+import com.liferay.layout.page.template.service.LayoutPageTemplateStructureLocalService;
+import com.liferay.layout.util.LayoutCopyHelper;
+import com.liferay.notification.rest.resource.v1_0.NotificationTemplateResource;
+import com.liferay.object.admin.rest.resource.v1_0.ObjectDefinitionResource;
+import com.liferay.object.admin.rest.resource.v1_0.ObjectRelationshipResource;
+import com.liferay.object.service.ObjectActionLocalService;
+import com.liferay.object.service.ObjectDefinitionLocalService;
+import com.liferay.object.service.ObjectEntryLocalService;
+import com.liferay.object.service.ObjectRelationshipLocalService;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.service.LayoutSetLocalService;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
+import com.liferay.portal.kernel.service.ResourceActionLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.ThemeLocalService;
+import com.liferay.portal.kernel.service.UserGroupLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.service.WorkflowDefinitionLinkLocalService;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ProxyUtil;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.security.service.access.policy.service.SAPEntryLocalService;
+import com.liferay.segments.service.SegmentsEntryLocalService;
+import com.liferay.segments.service.SegmentsExperienceLocalService;
+import com.liferay.site.initializer.SiteInitializer;
+import com.liferay.site.initializer.SiteInitializerFactory;
+import com.liferay.site.initializer.extender.internal.file.backed.osgi.FileBackedBundleDelegate;
+import com.liferay.site.initializer.extender.internal.file.backed.servlet.FileBackedServletContextDelegate;
+import com.liferay.site.navigation.service.SiteNavigationMenuItemLocalService;
+import com.liferay.site.navigation.service.SiteNavigationMenuLocalService;
+import com.liferay.site.navigation.type.SiteNavigationMenuItemTypeRegistry;
+import com.liferay.style.book.zip.processor.StyleBookEntryZipProcessor;
+
+import java.io.File;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Shuyang Zhou
+ */
+@Component(service = SiteInitializerFactory.class)
+public class SiteInitializerFactoryImpl implements SiteInitializerFactory {
+
+	@Override
+	public SiteInitializer create(File folder) throws Exception {
+		String fileKey = StringUtil.randomString(16);
+
+		String symbolicName = "Liferay Site Initializer - File - " + fileKey;
+
+		Bundle bundle = ProxyUtil.newDelegateProxyInstance(
+			Bundle.class.getClassLoader(), Bundle.class,
+			new FileBackedBundleDelegate(
+				_bundleContext, folder, _jsonFactory, symbolicName),
+			null);
+
+		BundleSiteInitializer bundleSiteInitializer = new BundleSiteInitializer(
+			_accountResourceFactory, _accountRoleLocalService,
+			_accountRoleResourceFactory, _assetCategoryLocalService,
+			_assetListEntryLocalService, bundle,
+			_clientExtensionEntryLocalService, _configurationProvider,
+			_ddmStructureLocalService, _ddmTemplateLocalService,
+			_defaultDDMStructureHelper, _dlURLHelper,
+			_documentFolderResourceFactory, _documentResourceFactory,
+			_fragmentsImporter, _groupLocalService, _journalArticleLocalService,
+			_jsonFactory, _knowledgeBaseArticleResourceFactory,
+			_knowledgeBaseFolderResourceFactory, _layoutCopyHelper,
+			_layoutLocalService, _layoutPageTemplateEntryLocalService,
+			_layoutsImporter, _layoutPageTemplateStructureLocalService,
+			_layoutSetLocalService, _listTypeDefinitionResource,
+			_listTypeDefinitionResourceFactory, _listTypeEntryResource,
+			_listTypeEntryResourceFactory, _notificationTemplateResourceFactory,
+			_objectActionLocalService, _objectDefinitionLocalService,
+			_objectDefinitionResourceFactory, _objectRelationshipLocalService,
+			_objectRelationshipResourceFactory, _objectEntryLocalService,
+			_organizationLocalService, _organizationResourceFactory, _portal,
+			_resourceActionLocalService, _resourcePermissionLocalService,
+			_roleLocalService, _sapEntryLocalService,
+			_segmentsEntryLocalService, _segmentsExperienceLocalService,
+			_settingsFactory, _siteNavigationMenuItemLocalService,
+			_siteNavigationMenuItemTypeRegistry,
+			_siteNavigationMenuLocalService,
+			_structuredContentFolderResourceFactory,
+			_styleBookEntryZipProcessor, _taxonomyCategoryResourceFactory,
+			_taxonomyVocabularyResourceFactory, _themeLocalService,
+			_userAccountResourceFactory, _userGroupLocalService,
+			_userLocalService, _workflowDefinitionLinkLocalService,
+			_workflowDefinitionResourceFactory);
+
+		ServiceReference<CommerceSiteInitializer> serviceReference =
+			_bundleContext.getServiceReference(CommerceSiteInitializer.class);
+
+		if (serviceReference != null) {
+			bundleSiteInitializer.setCommerceSiteInitializer(
+				_bundleContext.getService(serviceReference));
+		}
+
+		bundleSiteInitializer.setServletContext(
+			ProxyUtil.newDelegateProxyInstance(
+				ServletContext.class.getClassLoader(), ServletContext.class,
+				new FileBackedServletContextDelegate(
+					folder, fileKey, symbolicName),
+				null));
+
+		return bundleSiteInitializer;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
+	}
+
+	@Reference
+	private AccountResource.Factory _accountResourceFactory;
+
+	@Reference
+	private AccountRoleLocalService _accountRoleLocalService;
+
+	@Reference
+	private AccountRoleResource.Factory _accountRoleResourceFactory;
+
+	@Reference
+	private AssetCategoryLocalService _assetCategoryLocalService;
+
+	@Reference
+	private AssetListEntryLocalService _assetListEntryLocalService;
+
+	private BundleContext _bundleContext;
+
+	@Reference
+	private ClientExtensionEntryLocalService _clientExtensionEntryLocalService;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private DDMStructureLocalService _ddmStructureLocalService;
+
+	@Reference
+	private DDMTemplateLocalService _ddmTemplateLocalService;
+
+	@Reference
+	private DefaultDDMStructureHelper _defaultDDMStructureHelper;
+
+	@Reference
+	private DLURLHelper _dlURLHelper;
+
+	@Reference
+	private DocumentFolderResource.Factory _documentFolderResourceFactory;
+
+	@Reference
+	private DocumentResource.Factory _documentResourceFactory;
+
+	@Reference
+	private FragmentsImporter _fragmentsImporter;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private JournalArticleLocalService _journalArticleLocalService;
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+	@Reference
+	private KnowledgeBaseArticleResource.Factory
+		_knowledgeBaseArticleResourceFactory;
+
+	@Reference
+	private KnowledgeBaseFolderResource.Factory
+		_knowledgeBaseFolderResourceFactory;
+
+	@Reference
+	private LayoutCopyHelper _layoutCopyHelper;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private LayoutPageTemplateEntryLocalService
+		_layoutPageTemplateEntryLocalService;
+
+	@Reference
+	private LayoutPageTemplateStructureLocalService
+		_layoutPageTemplateStructureLocalService;
+
+	@Reference
+	private LayoutSetLocalService _layoutSetLocalService;
+
+	@Reference
+	private LayoutsImporter _layoutsImporter;
+
+	@Reference
+	private ListTypeDefinitionResource _listTypeDefinitionResource;
+
+	@Reference
+	private ListTypeDefinitionResource.Factory
+		_listTypeDefinitionResourceFactory;
+
+	@Reference
+	private ListTypeEntryResource _listTypeEntryResource;
+
+	@Reference
+	private ListTypeEntryResource.Factory _listTypeEntryResourceFactory;
+
+	@Reference
+	private NotificationTemplateResource.Factory
+		_notificationTemplateResourceFactory;
+
+	@Reference
+	private ObjectActionLocalService _objectActionLocalService;
+
+	@Reference
+	private ObjectDefinitionLocalService _objectDefinitionLocalService;
+
+	@Reference
+	private ObjectDefinitionResource.Factory _objectDefinitionResourceFactory;
+
+	@Reference
+	private ObjectEntryLocalService _objectEntryLocalService;
+
+	@Reference
+	private ObjectRelationshipLocalService _objectRelationshipLocalService;
+
+	@Reference
+	private ObjectRelationshipResource.Factory
+		_objectRelationshipResourceFactory;
+
+	@Reference
+	private OrganizationLocalService _organizationLocalService;
+
+	@Reference
+	private OrganizationResource.Factory _organizationResourceFactory;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private ResourceActionLocalService _resourceActionLocalService;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private SAPEntryLocalService _sapEntryLocalService;
+
+	@Reference
+	private SegmentsEntryLocalService _segmentsEntryLocalService;
+
+	@Reference
+	private SegmentsExperienceLocalService _segmentsExperienceLocalService;
+
+	@Reference
+	private SettingsFactory _settingsFactory;
+
+	@Reference
+	private SiteNavigationMenuItemLocalService
+		_siteNavigationMenuItemLocalService;
+
+	@Reference
+	private SiteNavigationMenuItemTypeRegistry
+		_siteNavigationMenuItemTypeRegistry;
+
+	@Reference
+	private SiteNavigationMenuLocalService _siteNavigationMenuLocalService;
+
+	@Reference
+	private StructuredContentFolderResource.Factory
+		_structuredContentFolderResourceFactory;
+
+	@Reference
+	private StyleBookEntryZipProcessor _styleBookEntryZipProcessor;
+
+	@Reference
+	private TaxonomyCategoryResource.Factory _taxonomyCategoryResourceFactory;
+
+	@Reference
+	private TaxonomyVocabularyResource.Factory
+		_taxonomyVocabularyResourceFactory;
+
+	@Reference
+	private ThemeLocalService _themeLocalService;
+
+	@Reference
+	private UserAccountResource.Factory _userAccountResourceFactory;
+
+	@Reference
+	private UserGroupLocalService _userGroupLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+	@Reference
+	private WorkflowDefinitionLinkLocalService
+		_workflowDefinitionLinkLocalService;
+
+	@Reference
+	private WorkflowDefinitionResource.Factory
+		_workflowDefinitionResourceFactory;
+
+}
