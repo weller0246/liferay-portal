e0981dc198fc7d2d670ce5e5c65f9f906b12450b
Sandro Chinea <sandrochinea@gmail.com>
2022-11-17 11:49:56 +0000

    LPS-168811 Remove old navigation menu tree CSS, now we want it to have ClayTreeView default styles

--- modules/apps/product-navigation/product-navigation-product-menu-web/src/main/resources/META-INF/resources/js/NavigationMenuItemsTree.js
+++ modules/apps/product-navigation/product-navigation-product-menu-web/src/main/resources/META-INF/resources/js/NavigationMenuItemsTree.js
@@ -14,7 +14,6 @@
 
 import {TreeView as ClayTreeView} from '@clayui/core';
 import ClayIcon from '@clayui/icon';
-import classNames from 'classnames';
 import React from 'react';
 
 export default function NavigationMenuItemsTree({
@@ -24,71 +23,58 @@ export default function NavigationMenuItemsTree({
 	const selectedKeys = new Set([selectedSiteNavigationMenuItemId]);
 
 	return (
-		<div className="navigation-menu-items-tree">
-			<ClayTreeView
-				defaultExpandedKeys={selectedKeys}
-				defaultItems={siteNavigationMenuItems}
-				displayType="dark"
-				expanderIcons={{
-					close: <ClayIcon symbol="hr" />,
-					open: <ClayIcon symbol="plus" />,
-				}}
-				nestedKey="children"
-				showExpanderOnHover={false}
-			>
-				{(item) => {
-					const hasUrl = item.url && item.url !== '#';
+		<ClayTreeView
+			defaultExpandedKeys={selectedKeys}
+			defaultItems={siteNavigationMenuItems}
+			displayType="dark"
+			nestedKey="children"
+			showExpanderOnHover={false}
+		>
+			{(item) => {
+				const hasUrl = item.url && item.url !== '#';
 
-					return (
-						<ClayTreeView.Item>
-							<ClayTreeView.ItemStack
-								className={classNames(
-									'navigation-menu-items-tree-node',
-									{selected: selectedKeys.has(item.id)}
-								)}
-							>
-								<ClayIcon
-									symbol={item.url ? 'page' : 'folder'}
-								/>
+				return (
+					<ClayTreeView.Item>
+						<ClayTreeView.ItemStack
+							active={selectedKeys.has(item.id)}
+						>
+							<ClayIcon symbol={item.url ? 'page' : 'folder'} />
 
-								{hasUrl ? (
-									<a
-										className="d-block h-100 w-100"
-										href={item.url}
-									>
-										{item.name}
-									</a>
-								) : (
-									<p className="m-0">{item.name}</p>
-								)}
-							</ClayTreeView.ItemStack>
+							{hasUrl ? (
+								<a
+									className="d-block h-100 w-100"
+									href={item.url}
+								>
+									{item.name}
+								</a>
+							) : (
+								<p className="m-0">{item.name}</p>
+							)}
+						</ClayTreeView.ItemStack>
 
-							<ClayTreeView.Group items={item.children}>
-								{(item) => (
-									<ClayTreeView.Item>
-										<ClayIcon
-											symbol={
-												item.url ? 'page' : 'folder'
-											}
-										/>
+						<ClayTreeView.Group items={item.children}>
+							{(item) => (
+								<ClayTreeView.Item>
+									<ClayIcon
+										symbol={item.url ? 'page' : 'folder'}
+									/>
 
-										{hasUrl ? (
-											<a
-												className="d-block h-100 w-100"
-												href={item.url}
-											>
-												{item.name}
-											</a>
-										) : (
-											<p className="m-0">{item.name}</p>
-										)}
-									</ClayTreeView.Item>
-								)}
-							</ClayTreeView.Group>
-						</ClayTreeView.Item>
-					);
-				}}
-			</ClayTreeView>
-		</div>
+									{hasUrl ? (
+										<a
+											className="d-block h-100 w-100"
+											href={item.url}
+										>
+											{item.name}
+										</a>
+									) : (
+										<p className="m-0">{item.name}</p>
+									)}
+								</ClayTreeView.Item>
+							)}
+						</ClayTreeView.Group>
+					</ClayTreeView.Item>
+				);
+			}}
+		</ClayTreeView>
 	);
 }
