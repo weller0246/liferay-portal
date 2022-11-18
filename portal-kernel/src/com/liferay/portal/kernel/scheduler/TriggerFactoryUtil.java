84a8a9dedc84fbe2009ec0e2ae5fea28f83ca0e7
Tina Tian <tina.tian@liferay.com>
2022-11-18 08:25:51 -0800

    LPS-169028 Remove unused API methods

--- portal-kernel/src/com/liferay/portal/kernel/scheduler/TriggerFactoryUtil.java
+++ portal-kernel/src/com/liferay/portal/kernel/scheduler/TriggerFactoryUtil.java
@@ -26,42 +26,10 @@ public class TriggerFactoryUtil {
 
 	public static Trigger createTrigger(
 		String jobName, String groupName, Date startDate, Date endDate,
-		int interval, TimeUnit timeUnit) {
-
-		return _triggerFactory.createTrigger(
-			jobName, groupName, startDate, endDate, interval, timeUnit);
-	}
-
-	public static Trigger createTrigger(
-		String jobName, String groupName, Date startDate, Date endDate,
-		String cronExpression) {
-
-		return _triggerFactory.createTrigger(
-			jobName, groupName, startDate, endDate, cronExpression);
-	}
-
-	public static Trigger createTrigger(
-		String jobName, String groupName, Date startDate, Date endDate,
 		String cronExpression, TimeZone timeZone) {
 
 		return _triggerFactory.createTrigger(
 			jobName, groupName, startDate, endDate, cronExpression, timeZone);
-	}
-
-	public static Trigger createTrigger(
-		String jobName, String groupName, Date startDate, int interval,
-		TimeUnit timeUnit) {
-
-		return _triggerFactory.createTrigger(
-			jobName, groupName, startDate, null, interval, timeUnit);
-	}
-
-	public static Trigger createTrigger(
-		String jobName, String groupName, Date startDate,
-		String cronExpression) {
-
-		return _triggerFactory.createTrigger(
-			jobName, groupName, startDate, null, cronExpression);
 	}
 
 	public static Trigger createTrigger(
