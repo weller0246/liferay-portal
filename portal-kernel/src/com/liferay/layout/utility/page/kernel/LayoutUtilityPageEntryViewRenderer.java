0afaa8984f571e7afd1ea4b79a1c1536d4ffee46
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-11-20 09:28:49 +0000

    LPS-169066 Adds new interface to allow us overwrite system pages

---
+++ portal-kernel/src/com/liferay/layout/utility/page/kernel/LayoutUtilityPageEntryViewRenderer.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.utility.page.kernel;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public interface LayoutUtilityPageEntryViewRenderer {
+
+	public String getLabel(Locale locale);
+
+	public String getType();
+
+	public void renderHTML(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException, ServletException;
+
+}
