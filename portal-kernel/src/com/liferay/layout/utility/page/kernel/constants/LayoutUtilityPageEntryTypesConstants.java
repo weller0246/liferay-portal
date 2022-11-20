fafe1feebee837c16a3197c2a38a257c9731765d
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-11-20 09:30:55 +0000

    LPS-169066 Adds constant class

---
+++ portal-kernel/src/com/liferay/layout/utility/page/kernel/constants/LayoutUtilityPageEntryTypesConstants.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.utility.page.kernel.constants;
+
+/**
+ * @author Eudaldo Alonso
+ */
+public class LayoutUtilityPageEntryTypesConstants {
+
+	public static final String LAYOUT = "LAYOUT";
+
+	public static final String STATUS = "STATUS";
+
+	public static final String TERMS_OF_USE = "TERMS_OF_USE";
+
+}
