909bf6ea0d334214cbe1b85d36ef9e197247e55b
drewbrokke <drew.brokke@liferay.com>
2022-11-21 15:35:55 -0600

    LPS-167706 account-admin-web: refactors js components using typescript

---
+++ modules/apps/account/account-admin-web/src/main/resources/META-INF/resources/account_entries_admin/js/InviteUsersForm.tsx
@@ -0,0 +1,227 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayButton from '@clayui/button';
+import ClayForm from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import ClayLayout from '@clayui/layout';
+import {sub} from 'frontend-js-web';
+import React, {MouseEventHandler, useState} from 'react';
+
+import InviteUserFormGroup from './InviteUsersFormGroup';
+import {InputGroup, MultiSelectItem, ValidatableMultiSelectItem} from './types';
+
+interface IProps {
+	accountEntryId: number;
+	availableAccountRoles: MultiSelectItem[];
+	inviteAccountUsersURL: string;
+	portletNamespace: string;
+	redirectURL: string;
+}
+
+function InviteUsersForm({
+	accountEntryId,
+	availableAccountRoles,
+	inviteAccountUsersURL,
+	portletNamespace,
+	redirectURL,
+}: IProps) {
+	const [inputGroups, setInputGroups] = useState<InputGroup[]>([
+		{
+			accountRoles: [],
+			emailAddresses: [],
+			id: 'inputGroup-0',
+		},
+	]);
+
+	const formId = `${portletNamespace}inviteUserForm`;
+
+	function closeModal(modalConfig = {}) {
+		const openerWindow = Liferay.Util.getOpener();
+
+		openerWindow.Liferay.fire('closeModal', modalConfig);
+	}
+
+	function setAccountRoles(
+		inputGroupId: string,
+		accountRoles: MultiSelectItem[]
+	) {
+		const inputGroup = inputGroups.find(
+			(inputGroup) => inputGroup.id === inputGroupId
+		);
+
+		if (inputGroup) {
+			inputGroup.accountRoles = accountRoles.map((accountRole) => {
+				const validatedAccountRole: ValidatableMultiSelectItem = {
+					...accountRole,
+				};
+
+				if (
+					!availableAccountRoles.some(
+						(availableAccountRole) =>
+							availableAccountRole.label === accountRole.label
+					)
+				) {
+					validatedAccountRole.errorMessage = sub(
+						Liferay.Language.get('x-is-not-a-valid-role'),
+						accountRole.label
+					);
+				}
+
+				return validatedAccountRole;
+			});
+
+			setInputGroups([...inputGroups]);
+		}
+	}
+
+	async function setEmailAddresses(
+		inputGroupId: string,
+		emailAddresses: MultiSelectItem[]
+	) {
+		const inputGroup = inputGroups.find(
+			(inputGroup) => inputGroup.id === inputGroupId
+		);
+
+		if (inputGroup) {
+			const promises = emailAddresses.map(
+				(emailAddress) =>
+					new Promise<ValidatableMultiSelectItem>((resolve) => {
+						const validatedEmailAddress: ValidatableMultiSelectItem = {
+							...emailAddress,
+						};
+
+						Liferay.Util.fetch(
+							`/o/account-admin/validate-email-address/`,
+							{
+								body: Liferay.Util.objectToFormData({
+									accountEntryId,
+									emailAddress: emailAddress.label,
+								}),
+								method: 'POST',
+							}
+						)
+							.then((response) => response.json())
+							.then(({errorMessage}) => {
+								if (errorMessage) {
+									validatedEmailAddress.errorMessage = errorMessage;
+								}
+
+								resolve(validatedEmailAddress);
+							});
+					})
+			);
+
+			inputGroup.emailAddresses = await Promise.all(promises);
+
+			setInputGroups([...inputGroups]);
+		}
+	}
+
+	const submitForm: MouseEventHandler<HTMLButtonElement> = async (event) => {
+		event.preventDefault();
+
+		const form = document.querySelector(`#${formId}`) as HTMLFormElement;
+
+		const error = form?.querySelector('.has-error');
+
+		if (!error && form) {
+			const formData = new FormData(form);
+
+			formData.append(
+				`${portletNamespace}count`,
+				String(inputGroups.length)
+			);
+
+			const response = await Liferay.Util.fetch(inviteAccountUsersURL, {
+				body: formData,
+				method: 'POST',
+			});
+
+			const {success} = await response.json();
+
+			if (success) {
+				closeModal({
+					id: `${portletNamespace}inviteUsersDialog`,
+					redirect: redirectURL,
+				});
+			}
+			else {
+				Liferay.Util.openToast({
+					message: Liferay.Language.get(
+						'your-request-failed-to-complete'
+					),
+					title: Liferay.Language.get('error'),
+					type: 'danger',
+				});
+			}
+		}
+	};
+
+	return (
+		<ClayForm className="lfr-form-content" id={formId}>
+			{inputGroups.map((inputGroup, index) => (
+				<InviteUserFormGroup
+					accountRoles={inputGroup.accountRoles}
+					availableAccountRoles={availableAccountRoles}
+					emailAddresses={inputGroup.emailAddresses}
+					id={inputGroup.id}
+					index={index}
+					key={inputGroup.id}
+					onAccountRoleItemsChange={(items) =>
+						setAccountRoles(inputGroup.id, items)
+					}
+					onEmailAddressItemsChange={(items) =>
+						setEmailAddresses(inputGroup.id, items)
+					}
+					portletNamespace={portletNamespace}
+				/>
+			))}
+
+			<ClayLayout.SheetFooter>
+				<ClayButton
+					displayType="secondary"
+					onClick={() => {
+						setInputGroups([
+							...inputGroups,
+							{
+								accountRoles: [],
+								emailAddresses: [],
+								id: `inputGroup-${inputGroups.length}`,
+							},
+						]);
+					}}
+				>
+					<span className="inline-item inline-item-before">
+						<ClayIcon symbol="plus" />
+					</span>
+
+					{Liferay.Language.get('add-entry')}
+				</ClayButton>
+			</ClayLayout.SheetFooter>
+
+			<ClayLayout.SheetFooter className="dialog-footer">
+				<ClayButton displayType="primary" onClick={submitForm}>
+					{Liferay.Language.get('invite')}
+				</ClayButton>
+
+				<ClayButton displayType="secondary" onClick={closeModal}>
+					{Liferay.Language.get('cancel')}
+				</ClayButton>
+			</ClayLayout.SheetFooter>
+		</ClayForm>
+	);
+}
+
+export default InviteUsersForm;
---
+++ modules/apps/account/account-admin-web/src/main/resources/META-INF/resources/account_entries_admin/js/InviteUsersFormGroup.tsx
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+import ClayForm, {ClayInput} from '@clayui/form';
+import ClayIcon from '@clayui/icon';
+import ClayLayout from '@clayui/layout';
+import ClayMultiSelect from '@clayui/multi-select';
+import React, {useState} from 'react';
+
+import {InputGroup, MultiSelectItem, ValidatableMultiSelectItem} from './types';
+
+type OnItemsChangeFn = (items: MultiSelectItem[]) => void;
+
+interface IProps extends InputGroup {
+	availableAccountRoles: MultiSelectItem[];
+	index: number;
+	onAccountRoleItemsChange: OnItemsChangeFn;
+	onEmailAddressItemsChange: OnItemsChangeFn;
+	portletNamespace: string;
+}
+
+const MultiSelect = ({
+	autoFocus = false,
+	errorMessages,
+	helpText,
+	inputName,
+	items,
+	label,
+	onBlurFn = () => {},
+	onItemsChangeFn,
+	required = false,
+	sourceItems = [],
+}: {
+	autoFocus?: boolean;
+	errorMessages: string[];
+	helpText?: string;
+	inputName: string;
+	items: MultiSelectItem[];
+	label: string;
+	onBlurFn?: () => void;
+	onItemsChangeFn: OnItemsChangeFn;
+	required?: boolean;
+	sourceItems?: MultiSelectItem[];
+}) => (
+	<ClayForm.Group className={errorMessages.length ? 'has-error' : ''}>
+		<label htmlFor={inputName}>
+			{label}
+
+			{required && (
+				<ClayIcon className="ml-1 reference-mark" symbol="asterisk" />
+			)}
+		</label>
+
+		<ClayInput.Group>
+			<ClayInput.GroupItem>
+				<ClayMultiSelect
+					autoFocus={autoFocus}
+					id={`${inputName}MultiSelect`}
+					inputName={inputName}
+					items={items}
+					onBlur={onBlurFn}
+
+					// @ts-ignore
+
+					onItemsChange={onItemsChangeFn}
+					sourceItems={sourceItems}
+				/>
+
+				<ClayForm.FeedbackGroup>
+					{Boolean(helpText) && (
+						<ClayForm.FeedbackItem>
+							{helpText}
+						</ClayForm.FeedbackItem>
+					)}
+
+					{errorMessages.map((errorMessage) => (
+						<ClayForm.FeedbackItem key={errorMessage}>
+							{errorMessage}
+						</ClayForm.FeedbackItem>
+					))}
+				</ClayForm.FeedbackGroup>
+			</ClayInput.GroupItem>
+		</ClayInput.Group>
+	</ClayForm.Group>
+);
+
+function getErrorMessages(items: ValidatableMultiSelectItem[]) {
+	return items.map((item) => item.errorMessage).filter(Boolean) as string[];
+}
+
+const InviteUserFormGroup = ({
+	accountRoles,
+	availableAccountRoles,
+	emailAddresses,
+	index,
+	onAccountRoleItemsChange,
+	onEmailAddressItemsChange,
+	portletNamespace,
+}: IProps) => {
+	const [showRequiredMessage, setShowRequiredMessage] = useState<boolean>(
+		false
+	);
+
+	const emailAddressErrorMessages = [];
+
+	if (showRequiredMessage && !emailAddresses.length) {
+		emailAddressErrorMessages.push(
+			Liferay.Language.get('this-field-is-required')
+		);
+	}
+
+	for (const errorMessage of getErrorMessages(emailAddresses)) {
+		emailAddressErrorMessages.push(errorMessage);
+	}
+
+	return (
+		<ClayLayout.Sheet size="lg">
+			<MultiSelect
+				autoFocus={true}
+				errorMessages={emailAddressErrorMessages}
+				inputName={`${portletNamespace}emailAddresses${index}`}
+				items={emailAddresses}
+				label={Liferay.Language.get('emails')}
+				onBlurFn={() => setShowRequiredMessage(true)}
+				onItemsChangeFn={onEmailAddressItemsChange}
+				required={true}
+			/>
+
+			<MultiSelect
+				errorMessages={getErrorMessages(accountRoles)}
+				helpText={Liferay.Language.get(
+					'roles-will-be-applied-to-all-users-above'
+				)}
+				inputName={`${portletNamespace}accountRoleIds${index}`}
+				items={accountRoles}
+				label={Liferay.Language.get('roles')}
+				onItemsChangeFn={onAccountRoleItemsChange}
+				sourceItems={availableAccountRoles}
+			/>
+		</ClayLayout.Sheet>
+	);
+};
+
+export default InviteUserFormGroup;
