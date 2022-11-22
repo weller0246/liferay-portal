6fc41fc5eee4696392b85be60a1395918d48106a
Brian I. Kim <il.kim@liferay.com>
2022-11-22 03:49:47 -0800

    COMMERCE-10247 Grab closest here and also make Window.getWindow work.

--- modules/apps/commerce/commerce-cart-content-web/src/main/resources/META-INF/resources/js/cart_mini/transition.js
+++ modules/apps/commerce/commerce-cart-content-web/src/main/resources/META-INF/resources/js/cart_mini/transition.js
@@ -13,87 +13,93 @@
  */
 
 export default function ({namespace}) {
-	Liferay.provide(window, `${namespace}transition`, (event) => {
-		const link = event.delegateTarget;
+	Liferay.provide(
+		window,
+		`${namespace}transition`,
+		(event) => {
+			const link = event.target.closest('.transition-link');
 
-		const workflowTaskId = parseInt(link.dataset.workflowtaskid, 10);
+			const workflowTaskId = parseInt(link.dataset.workflowtaskid, 10);
 
-		const form = document.getElementById(`${namespace}transitionFm`);
+			const form = document.getElementById(`${namespace}transitionFm`);
 
-		document.getElementById(`${namespace}transitionCommerceOrderId`).value =
-			link.dataset.commerceorderid;
+			document.getElementById(
+				`${namespace}transitionCommerceOrderId`
+			).value = link.dataset.commerceorderid;
 
-		document.getElementById(
-			`${namespace}workflowTaskId`
-		).value = workflowTaskId;
-		document.getElementById(`${namespace}transitionName`).value =
-			link.dataset.transitionname;
+			document.getElementById(
+				`${namespace}workflowTaskId`
+			).value = workflowTaskId;
+			document.getElementById(`${namespace}transitionName`).value =
+				link.dataset.transitionname;
 
-		if (workflowTaskId <= 0) {
-			submitForm(form);
+			if (workflowTaskId <= 0) {
+				submitForm(form);
 
-			return;
-		}
+				return;
+			}
 
-		const transitionComments = document.getElementById(
-			`${namespace}transitionComments`
-		);
+			const transitionComments = document.getElementById(
+				`${namespace}transitionComments`
+			);
 
-		transitionComments.classList.remove('hide');
+			transitionComments.classList.remove('hide');
 
-		transitionComments.style.display = 'block';
+			transitionComments.style.display = 'block';
 
-		const dialog = Liferay.Util.Window.getWindow({
-			dialog: {
-				bodyContent: form,
-				height: 400,
-				resizable: false,
-				toolbars: {
-					footer: [
-						{
-							cssClass: 'btn-primary mr-2',
-							label: Liferay.Language.get('done'),
-							on: {
-								click() {
-									submitForm(form);
+			const dialog = Liferay.Util.Window.getWindow({
+				dialog: {
+					bodyContent: form,
+					height: 400,
+					resizable: false,
+					toolbars: {
+						footer: [
+							{
+								cssClass: 'btn-primary mr-2',
+								label: Liferay.Language.get('done'),
+								on: {
+									click() {
+										submitForm(form);
+									},
 								},
 							},
-						},
-						{
-							cssClass: 'btn-cancel',
-							label: Liferay.Language.get('cancel'),
-							on: {
-								click() {
-									if (form) {
-										form.reset();
-									}
+							{
+								cssClass: 'btn-cancel',
+								label: Liferay.Language.get('cancel'),
+								on: {
+									click() {
+										if (form) {
+											form.reset();
+										}
 
-									dialog.hide();
+										dialog.hide();
+									},
 								},
 							},
-						},
-					],
-					header: [
-						{
-							cssClass: 'close',
-							discardDefaultButtonCssClasses: true,
-							labelHTML:
-								'<span aria-hidden="true">&times;</span>',
-							on: {
-								click() {
-									if (form) {
-										form.reset();
-									}
+						],
+						header: [
+							{
+								cssClass: 'close',
+								discardDefaultButtonCssClasses: true,
+								labelHTML:
+									'<span aria-hidden="true">&times;</span>',
+								on: {
+									click() {
+										if (form) {
+											form.reset();
+										}
 
-									dialog.hide();
+										dialog.hide();
+									},
 								},
 							},
-						},
-					],
+						],
+					},
+					width: 720,
 				},
-				width: 720,
-			},
-			title: link.innerText,
-		});
-	});
+				title: link.innerText,
+			});
+		},
+		['liferay-util-window']
+	);
 }
