8899341010bf0c3c4852015ac5cdd77156e01959
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-11-23 16:00:25 +0000

    LPS-169437 Replace layout status with utility page

---
+++ modules/apps/layout/layout-utility-page/layout-utility-page-layout/src/main/java/com/liferay/layout/utility/page/layout/internal/LayoutStatusLayoutUtilityPageEntryViewRenderer.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.utility.page.layout.internal;
+
+import com.liferay.layout.utility.page.kernel.LayoutUtilityPageEntryViewRenderer;
+import com.liferay.layout.utility.page.kernel.constants.LayoutUtilityPageEntryTypesConstants;
+import com.liferay.portal.kernel.language.Language;
+
+import java.io.IOException;
+
+import java.util.Locale;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Eudaldo Alonso
+ */
+@Component(
+	property = "utility.page.type=" + LayoutUtilityPageEntryTypesConstants.STATUS,
+	service = LayoutUtilityPageEntryViewRenderer.class
+)
+public class LayoutStatusLayoutUtilityPageEntryViewRenderer
+	implements LayoutUtilityPageEntryViewRenderer {
+
+	@Override
+	public String getLabel(Locale locale) {
+		return _language.get(locale, "status");
+	}
+
+	@Override
+	public String getType() {
+		return LayoutUtilityPageEntryTypesConstants.STATUS;
+	}
+
+	@Override
+	public void renderHTML(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException, ServletException {
+
+		RequestDispatcher requestDispatcher =
+			_servletContext.getRequestDispatcher("/layout.jsp");
+
+		requestDispatcher.include(httpServletRequest, httpServletResponse);
+	}
+
+	@Reference
+	private Language _language;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.layout.utility.page.layout)"
+	)
+	private ServletContext _servletContext;
+
+}
---
+++ modules/apps/layout/layout-utility-page/layout-utility-page-layout/src/main/resources/META-INF/resources/layout.jsp
@@ -0,0 +1,43 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<liferay-layout:render-layout-utility-page-entry
+	type="<%= LayoutUtilityPageEntryConstants.Type.ERROR_404.getType() %>"
+>
+	<div class="container pb-3 pt-3">
+		<h3 class="alert alert-danger">
+			<liferay-ui:message key="not-found" />
+		</h3>
+
+		<liferay-ui:message key="the-requested-resource-could-not-be-found" />
+
+		<br /><br />
+
+		<%
+		String url = ParamUtil.getString(request, "previousURL");
+
+		if (Validator.isNull(url)) {
+			url = PortalUtil.getCurrentURL(request);
+		}
+
+		url = HttpComponentsUtil.decodeURL(themeDisplay.getPortalURL() + url);
+		%>
+
+		<code class="lfr-url-error"><%= HtmlUtil.escape(url) %></code>
+	</div>
+</liferay-layout:render-layout-utility-page-entry>
