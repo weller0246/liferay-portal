9a0d4986c4e8ee5918abe1334deb7bf14ced975b
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-11-23 11:11:33 +0100

    LPS-168249 Allow edit the WC friendly URL for Global WCs

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/frontend/taglib/form/navigator/JournalFriendlyURLFormNavigatorEntry.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/frontend/taglib/form/navigator/JournalFriendlyURLFormNavigatorEntry.java
@@ -45,7 +45,7 @@ public class JournalFriendlyURLFormNavigatorEntry
 
 	@Override
 	public boolean isVisible(User user, JournalArticle article) {
-		if (isEditDefaultValues(article) || isGlobalScopeArticle(article)) {
+		if (isEditDefaultValues(article)) {
 			return false;
 		}
 
