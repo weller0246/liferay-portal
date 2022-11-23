a3814dc9945f06a0673f4af2e20e1016c17330db
Jiaxu Wei <jiaxu.wei@liferay.com>
2022-11-23 15:00:44 +0800

    LPS-167592 NodeValidator: Convert method reference to ServiceTrackerMap in class NodeValidatorRegistry(remove IllegalArgumentException)

--- modules/apps/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/parser/NodeValidatorRegistry.java
+++ modules/apps/portal-workflow/portal-workflow-kaleo-definition-impl/src/main/java/com/liferay/portal/workflow/kaleo/definition/internal/parser/NodeValidatorRegistry.java
@@ -14,19 +14,18 @@
 
 package com.liferay.portal.workflow.kaleo.definition.internal.parser;
 
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapperFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.workflow.kaleo.definition.Node;
 import com.liferay.portal.workflow.kaleo.definition.NodeType;
-import com.liferay.portal.workflow.kaleo.definition.NodeTypeDependentObjectRegistry;
 import com.liferay.portal.workflow.kaleo.definition.exception.KaleoDefinitionValidationException;
 import com.liferay.portal.workflow.kaleo.definition.parser.NodeValidator;
 
-import java.util.Map;
-
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
-import org.osgi.service.component.annotations.ReferenceCardinality;
-import org.osgi.service.component.annotations.ReferencePolicy;
-import org.osgi.service.component.annotations.ReferencePolicyOption;
+import org.osgi.service.component.annotations.Deactivate;
 
 /**
  * @author Michael C. Han
@@ -37,41 +36,25 @@ public class NodeValidatorRegistry {
 	public NodeValidator<Node> getNodeValidator(NodeType nodeType)
 		throws KaleoDefinitionValidationException {
 
-		return _nodeValidators.getNodeTypeDependentObjects(nodeType);
+		return _serviceTrackerMap.getService(nodeType);
 	}
 
-	@Reference(
-		cardinality = ReferenceCardinality.MULTIPLE,
-		policy = ReferencePolicy.DYNAMIC,
-		policyOption = ReferencePolicyOption.GREEDY
-	)
-	protected void addNodeValidator(
-		NodeValidator<Node> nodeValidator, Map<String, Object> properties) {
-
-		String nodeType = (String)properties.get("node.type");
-
-		if (nodeType == null) {
-			throw new IllegalArgumentException(
-				"The property \"node.type\" is null");
-		}
-
-		_nodeValidators.addNodeTypeDependentObject(nodeType, nodeValidator);
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext,
+			(Class<NodeValidator<Node>>)(Class<?>)NodeValidator.class, null,
+			ServiceReferenceMapperFactory.create(
+				bundleContext,
+				(nodeValidator, emitter) -> emitter.emit(
+					nodeValidator.getNodeType())));
 	}
 
-	protected void removeNodeValidator(
-		NodeValidator<Node> nodeValidator, Map<String, Object> properties) {
-
-		String nodeType = (String)properties.get("node.type");
-
-		if (nodeType == null) {
-			throw new IllegalArgumentException(
-				"The property \"node.type\" is null");
-		}
-
-		_nodeValidators.removeNodeTypeDependentObjects(nodeType);
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
 	}
 
-	private final NodeTypeDependentObjectRegistry<NodeValidator<Node>>
-		_nodeValidators = new NodeTypeDependentObjectRegistry<>();
+	private ServiceTrackerMap<NodeType, NodeValidator<Node>> _serviceTrackerMap;
 
 }
