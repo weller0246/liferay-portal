c6c12cefc8a1ad1f195b53663493511a98d429b6
Rafael Praxedes <rafael.praxedes@liferay.com>
2022-11-26 10:45:04 -0300

    LPS-169623 Create a helper class to hold a tracker of ModelResourcePermission and verify if a user has permission to view a given entity

---
+++ modules/apps/notification/notification-service/src/main/java/com/liferay/notification/internal/type/users/provider/UsersProviderHelper.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.notification.internal.type.users.provider;
+
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Rafael Praxedes
+ */
+@Component(service = UsersProviderHelper.class)
+public class UsersProviderHelper {
+
+	public boolean hasViewPermission(
+		String className, long classPK, User user) {
+
+		ModelResourcePermission<?> modelResourcePermission =
+			_serviceTrackerMap.getService(className);
+
+		if (modelResourcePermission != null) {
+			try {
+				return modelResourcePermission.contains(
+					_permissionCheckerFactory.create(user), classPK,
+					ActionKeys.VIEW);
+			}
+			catch (PortalException portalException) {
+				throw new RuntimeException(portalException);
+			}
+		}
+
+		return false;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext,
+			(Class<ModelResourcePermission<?>>)
+				(Class<?>)ModelResourcePermission.class,
+			"model.class.name");
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	@Reference
+	private PermissionCheckerFactory _permissionCheckerFactory;
+
+	private ServiceTrackerMap<String, ModelResourcePermission<?>>
+		_serviceTrackerMap;
+
+}
