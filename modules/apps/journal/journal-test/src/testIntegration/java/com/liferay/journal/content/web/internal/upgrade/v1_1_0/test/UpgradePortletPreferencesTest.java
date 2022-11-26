4c8db93778695ac88e1d6540adce9019f7eb511f
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-11-26 13:45:22 +0100

    LPS-169664 Add an integration test to ensure that when a Global scoped Journal Article is selected, its scoped-related configuration is adapted if apply

---
+++ modules/apps/journal/journal-test/src/testIntegration/java/com/liferay/journal/content/web/internal/upgrade/v1_1_0/test/UpgradePortletPreferencesTest.java
@@ -0,0 +1,281 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.web.internal.upgrade.v1_1_0.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.journal.constants.JournalContentPortletKeys;
+import com.liferay.journal.constants.JournalFolderConstants;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.journal.service.JournalArticleLocalService;
+import com.liferay.journal.test.util.JournalTestUtil;
+import com.liferay.layout.test.util.LayoutTestUtil;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.Language;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutTemplate;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.test.ReflectionTestUtil;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.TestPropsValues;
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PortletKeys;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.test.rule.Inject;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+
+import java.lang.reflect.Constructor;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+
+import javax.portlet.PortletPreferences;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * @author Lourdes Fernández Besada
+ */
+@RunWith(Arquillian.class)
+public class UpgradePortletPreferencesTest {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		_setUpUpgradePortletPreferences();
+	}
+
+	@Test
+	public void testUpgradePortletPreferencesGlobalScopedJournalArticle()
+		throws Exception {
+
+		Locale locale = _portal.getSiteDefaultLocale(_group);
+
+		HashMap<String, String> expectedScopedPreferenceMap =
+			HashMapBuilder.put(
+				"portletSetupTitle",
+				StringUtil.appendParentheticalSuffix(
+					"Web Content Display", _language.get(locale, "global"))
+			).put(
+				"scopeType", "company"
+			).build();
+
+		Group companyGroup = _groupLocalService.getCompanyGroup(
+			_group.getCompanyId());
+
+		JournalArticle journalArticle = JournalTestUtil.addArticle(
+			companyGroup.getGroupId(),
+			JournalFolderConstants.DEFAULT_PARENT_FOLDER_ID);
+
+		Layout layout = LayoutTestUtil.addTypePortletLayout(_group);
+
+		_assertUpgradePortletPreferences(
+			expectedScopedPreferenceMap, journalArticle, layout, locale,
+			HashMapBuilder.put(
+				"portletSetupTitle", "Web Content Display"
+			).build());
+
+		_assertUpgradePortletPreferences(
+			expectedScopedPreferenceMap, journalArticle, layout, locale,
+			HashMapBuilder.put(
+				"portletSetupTitle",
+				StringUtil.appendParentheticalSuffix(
+					"Web Content Display", layout.getName(locale))
+			).put(
+				"scopeLayoutUuid", layout.getUuid()
+			).put(
+				"scopeType", "layout"
+			).build());
+	}
+
+	private String _addJournalContentPortletToLayout(
+			Layout layout, Map<String, String[]> preferencesMap)
+		throws Exception {
+
+		LayoutTypePortlet layoutTypePortlet =
+			(LayoutTypePortlet)layout.getLayoutType();
+
+		LayoutTemplate layoutTemplate = layoutTypePortlet.getLayoutTemplate();
+
+		List<String> columns = layoutTemplate.getColumns();
+
+		String columnId = columns.get(0);
+
+		return LayoutTestUtil.addPortletToLayout(
+			TestPropsValues.getUserId(), layout,
+			JournalContentPortletKeys.JOURNAL_CONTENT, columnId,
+			preferencesMap);
+	}
+
+	private void _assertUpgradePortletPreferences(
+			Map<String, String> expectedScopedPreferencesMap,
+			JournalArticle journalArticle, Layout layout, Locale locale,
+			Map<String, String> originalScopedPreferencesMap)
+		throws Exception {
+
+		String portletId = _addJournalContentPortletToLayout(
+			layout,
+			HashMapBuilder.put(
+				"articleId", new String[] {journalArticle.getArticleId()}
+			).put(
+				"groupId",
+				new String[] {String.valueOf(journalArticle.getGroupId())}
+			).put(
+				"lfrScopeLayoutUuid",
+				new String[] {
+					originalScopedPreferencesMap.getOrDefault(
+						"scopeLayoutUuid", StringPool.BLANK)
+				}
+			).put(
+				"lfrScopeType",
+				new String[] {
+					originalScopedPreferencesMap.getOrDefault(
+						"scopeType", StringPool.BLANK)
+				}
+			).put(
+				"portletSetupTitle_" + LocaleUtil.toLanguageId(locale),
+				new String[] {
+					originalScopedPreferencesMap.getOrDefault(
+						"portletSetupTitle", StringPool.BLANK)
+				}
+			).build());
+
+		PortletPreferences portletPreferences =
+			PortletPreferencesFactoryUtil.fromXML(
+				layout.getCompanyId(), 0, PortletKeys.PREFS_OWNER_TYPE_LAYOUT,
+				layout.getPlid(), portletId,
+				ReflectionTestUtil.invoke(
+					_upgradePortletPreferences, "upgradePreferences",
+					new Class<?>[] {
+						long.class, long.class, int.class, long.class,
+						String.class, String.class
+					},
+					layout.getCompanyId(), 0,
+					PortletKeys.PREFS_OWNER_TYPE_LAYOUT, layout.getPlid(),
+					portletId,
+					PortletPreferencesFactoryUtil.toXML(
+						LayoutTestUtil.getPortletPreferences(
+							layout, portletId))));
+
+		Assert.assertEquals(
+			journalArticle.getArticleId(),
+			portletPreferences.getValue("articleId", null));
+
+		Assert.assertEquals(
+			String.valueOf(journalArticle.getGroupId()),
+			portletPreferences.getValue("groupId", null));
+
+		Assert.assertEquals(
+			expectedScopedPreferencesMap.getOrDefault(
+				"scopeLayoutUuid", StringPool.BLANK),
+			portletPreferences.getValue("lfrScopeLayoutUuid", null));
+
+		Assert.assertEquals(
+			expectedScopedPreferencesMap.getOrDefault(
+				"scopeType", StringPool.BLANK),
+			portletPreferences.getValue("lfrScopeType", null));
+
+		Assert.assertEquals(
+			expectedScopedPreferencesMap.getOrDefault(
+				"portletSetupTitle", StringPool.BLANK),
+			portletPreferences.getValue(
+				"portletSetupTitle_" + LocaleUtil.toLanguageId(locale), null));
+	}
+
+	private void _setUpUpgradePortletPreferences() throws Exception {
+		Bundle bundle = FrameworkUtil.getBundle(
+			UpgradePortletPreferencesTest.class);
+
+		BundleContext bundleContext = bundle.getBundleContext();
+
+		Bundle journalContentWebBundle = null;
+
+		for (Bundle curBundle : bundleContext.getBundles()) {
+			if (Objects.equals(
+					curBundle.getSymbolicName(),
+					"com.liferay.journal.content.web")) {
+
+				journalContentWebBundle = curBundle;
+
+				break;
+			}
+		}
+
+		Assert.assertNotNull(
+			"Unable to find journal-content-web bundle",
+			journalContentWebBundle);
+
+		Class<?> clazz = journalContentWebBundle.loadClass(
+			"com.liferay.journal.content.web.internal.upgrade.v1_1_0." +
+				"UpgradePortletPreferences");
+
+		Constructor<?> constructor = clazz.getConstructor(
+			GroupLocalService.class, JournalArticleLocalService.class,
+			Language.class, LayoutLocalService.class, Portal.class);
+
+		_upgradePortletPreferences = constructor.newInstance(
+			_groupLocalService, _journalArticleLocalService, _language,
+			_layoutLocalService, _portal);
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+	@Inject
+	private GroupLocalService _groupLocalService;
+
+	@Inject
+	private JournalArticleLocalService _journalArticleLocalService;
+
+	@Inject
+	private Language _language;
+
+	@Inject
+	private LayoutLocalService _layoutLocalService;
+
+	@Inject
+	private Portal _portal;
+
+	private Object _upgradePortletPreferences;
+
+}
