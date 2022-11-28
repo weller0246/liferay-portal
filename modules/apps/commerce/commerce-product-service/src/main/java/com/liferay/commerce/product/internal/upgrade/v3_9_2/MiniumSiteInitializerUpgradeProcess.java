55b2b19e746a4aa3636e42429448ad6f8a224ae8
Crescenzo Rega <crescenzo.rega@liferay.com>
2022-11-28 15:57:16 +0100

    COMMERCE-10342 Fix UpgradeProcess breaks the pages hierarchy when organized in multiple levels

--- modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v3_9_2/MiniumSiteInitializerUpgradeProcess.java
+++ modules/apps/commerce/commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v3_9_2/MiniumSiteInitializerUpgradeProcess.java
@@ -50,89 +50,12 @@ public class MiniumSiteInitializerUpgradeProcess extends UpgradeProcess {
 				while (resultSet.next()) {
 					long siteGroupId = resultSet.getLong("siteGroupId");
 
-					long maxLayoutId = _getMaxLayoutId(siteGroupId);
-
-					_updateLayoutIds(siteGroupId);
-
-					_updateParentLayoutIds(maxLayoutId, siteGroupId);
+					_updateLayouts(siteGroupId);
 
 					_updateLayoutPriorities(siteGroupId);
 				}
 			}
 		}
-	}
-
-	private long _getMaxLayoutId(long siteGroupId) throws SQLException {
-		PreparedStatement preparedStatement =
-			AutoBatchPreparedStatementUtil.autoBatch(
-				connection,
-				"select max(layoutId) from Layout where groupId = ? and " +
-					"privateLayout = ?");
-
-		preparedStatement.setLong(1, siteGroupId);
-		preparedStatement.setBoolean(2, false);
-
-		try (ResultSet resultSet = preparedStatement.executeQuery()) {
-			if (resultSet.next()) {
-				return resultSet.getLong(1);
-			}
-		}
-
-		return 0;
-	}
-
-	private void _updateLayoutIds(long siteGroupId) throws SQLException {
-		PreparedStatement preparedStatement1 =
-			AutoBatchPreparedStatementUtil.autoBatch(
-				connection,
-				"select plid from Layout where groupId = ? and privateLayout " +
-					"= ? ORDER by layoutId");
-		PreparedStatement preparedStatement2 =
-			AutoBatchPreparedStatementUtil.autoBatch(
-				connection,
-				"update Layout set layoutId = ?, privateLayout = ? where " +
-					"groupId = ? and plid = ? and privateLayout = ?");
-		PreparedStatement preparedStatement3 =
-			AutoBatchPreparedStatementUtil.autoBatch(
-				connection,
-				"update LayoutFriendlyURL set privateLayout = ? where " +
-					"groupId = ? and plid = ? and privateLayout = ?");
-
-		preparedStatement1.setLong(1, siteGroupId);
-		preparedStatement1.setBoolean(2, true);
-
-		try (ResultSet resultSet = preparedStatement1.executeQuery()) {
-			while (resultSet.next()) {
-				long newLayoutId = _counterLocalService.increment(
-					StringBundler.concat(
-						Layout.class.getName(), StringPool.POUND, siteGroupId,
-						StringPool.POUND, false));
-
-				preparedStatement2.setLong(1, newLayoutId);
-
-				preparedStatement2.setBoolean(2, false);
-				preparedStatement2.setLong(3, siteGroupId);
-
-				long plid = resultSet.getLong("plid");
-
-				preparedStatement2.setLong(4, plid);
-
-				preparedStatement2.setBoolean(5, true);
-
-				preparedStatement2.addBatch();
-
-				preparedStatement3.setBoolean(1, false);
-				preparedStatement3.setLong(2, siteGroupId);
-				preparedStatement3.setLong(3, plid);
-				preparedStatement3.setBoolean(4, true);
-
-				preparedStatement3.addBatch();
-			}
-		}
-
-		preparedStatement2.executeBatch();
-
-		preparedStatement3.executeBatch();
 	}
 
 	private void _updateLayoutPriorities(long siteGroupId) throws SQLException {
@@ -167,35 +90,67 @@ public class MiniumSiteInitializerUpgradeProcess extends UpgradeProcess {
 		}
 	}
 
-	private void _updateParentLayoutIds(long maxLayoutId, long siteGroupId)
-		throws SQLException {
-
+	private void _updateLayouts(long siteGroupId) throws SQLException {
 		PreparedStatement preparedStatement1 =
 			AutoBatchPreparedStatementUtil.autoBatch(
 				connection,
-				"select layoutId, parentLayoutId from Layout where groupId = " +
-					"? and layoutId > ? and parentLayoutId > 0");
+				"select plid from Layout where groupId = ? and privateLayout " +
+					"= ? ORDER by layoutId");
 		PreparedStatement preparedStatement2 =
 			AutoBatchPreparedStatementUtil.autoBatch(
 				connection,
+				"update Layout set layoutId = ?, privateLayout = ? where " +
+					"groupId = ? and plid = ? and privateLayout = ?");
+		PreparedStatement preparedStatement3 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
 				"update Layout set parentLayoutId = ? where groupId = ? and " +
-					"layoutId = ? and parentLayoutId > 0");
+					"parentLayoutId > 0 and parentPlid = ?");
+		PreparedStatement preparedStatement4 =
+			AutoBatchPreparedStatementUtil.autoBatch(
+				connection,
+				"update LayoutFriendlyURL set privateLayout = ? where " +
+					"groupId = ? and plid = ? and privateLayout = ?");
 
 		preparedStatement1.setLong(1, siteGroupId);
-		preparedStatement1.setLong(2, maxLayoutId);
+		preparedStatement1.setBoolean(2, true);
 
 		try (ResultSet resultSet = preparedStatement1.executeQuery()) {
 			while (resultSet.next()) {
-				preparedStatement2.setLong(
-					1, resultSet.getLong("parentLayoutId") + maxLayoutId);
-				preparedStatement2.setLong(2, siteGroupId);
-				preparedStatement2.setLong(3, resultSet.getLong("layoutId"));
+				long newLayoutId = _counterLocalService.increment(
+					StringBundler.concat(
+						Layout.class.getName(), StringPool.POUND, siteGroupId,
+						StringPool.POUND, false));
+
+				long plid = resultSet.getLong("plid");
+
+				preparedStatement2.setLong(1, newLayoutId);
+				preparedStatement2.setBoolean(2, false);
+				preparedStatement2.setLong(3, siteGroupId);
+				preparedStatement2.setLong(4, plid);
+				preparedStatement2.setBoolean(5, true);
 
 				preparedStatement2.addBatch();
+
+				preparedStatement3.setLong(1, newLayoutId);
+				preparedStatement3.setLong(2, siteGroupId);
+				preparedStatement3.setLong(3, plid);
+				preparedStatement3.addBatch();
+
+				preparedStatement4.setBoolean(1, false);
+				preparedStatement4.setLong(2, siteGroupId);
+				preparedStatement4.setLong(3, plid);
+				preparedStatement4.setBoolean(4, true);
+
+				preparedStatement4.addBatch();
 			}
 		}
 
 		preparedStatement2.executeBatch();
+
+		preparedStatement3.executeBatch();
+
+		preparedStatement4.executeBatch();
 	}
 
 	private final CounterLocalService _counterLocalService;
