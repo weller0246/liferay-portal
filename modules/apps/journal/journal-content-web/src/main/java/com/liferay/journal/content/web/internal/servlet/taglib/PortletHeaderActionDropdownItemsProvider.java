1f207ff534abb6c02065a05a02af64686621ce8f
Diego Hu <diego.hu@liferay.com>
2022-11-28 17:33:26 +0100

    LPS-169842 Creates new clay:dropdown-menu component

---
+++ modules/apps/journal/journal-content-web/src/main/java/com/liferay/journal/content/web/internal/servlet/taglib/PortletHeaderActionDropdownItemsProvider.java
@@ -0,0 +1,122 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.journal.content.web.internal.servlet.taglib;
+
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
+import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.journal.content.web.internal.display.context.JournalContentDisplayContext;
+import com.liferay.journal.content.web.internal.security.permission.resource.JournalArticlePermission;
+import com.liferay.journal.model.JournalArticle;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayWindowState;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.HtmlUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.security.PermissionsURLTag;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Diego Hu
+ */
+public class PortletHeaderActionDropdownItemsProvider {
+
+	public PortletHeaderActionDropdownItemsProvider(
+		JournalContentDisplayContext journalContentDisplayContext,
+		HttpServletRequest httpServletRequest, PortletDisplay portletDisplay) {
+
+		_journalContentDisplayContext = journalContentDisplayContext;
+		_httpServletRequest = httpServletRequest;
+		_portletDisplay = portletDisplay;
+
+		_themeDisplay = (ThemeDisplay)httpServletRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+	}
+
+	public List<DropdownItem> getActionDropdownItems() throws Exception {
+		JournalArticle article = _journalContentDisplayContext.getArticle();
+		JournalArticle latestArticle =
+			_journalContentDisplayContext.getLatestArticle();
+
+		return DropdownItemListBuilder.add(
+			_journalContentDisplayContext::isShowEditArticleIcon,
+			dropdownItem -> {
+				dropdownItem.putData("destroyOnHide", "true");
+				dropdownItem.putData(
+					"id", _portletDisplay.getPortletName() + "editAsset");
+				dropdownItem.putData(
+					"title",
+					latestArticle.getTitle(_httpServletRequest.getLocale()));
+				dropdownItem.setHref(
+					_journalContentDisplayContext.getURLEdit());
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "edit-web-content"));
+			}
+		).add(
+			_journalContentDisplayContext::isShowEditTemplateIcon,
+			dropdownItem -> {
+				dropdownItem.setHref(
+					_journalContentDisplayContext.getURLEditTemplate());
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "edit-template"));
+			}
+		).add(
+			() -> JournalArticlePermission.contains(
+				_themeDisplay.getPermissionChecker(), article,
+				ActionKeys.PERMISSIONS),
+			dropdownItem -> {
+				dropdownItem.putData("action", "permissionsPortletHeader");
+				dropdownItem.putData(
+					"permissionsPortletHeaderURL",
+					PermissionsURLTag.doTag(
+						StringPool.BLANK, JournalArticle.class.getName(),
+						HtmlUtil.escape(
+							article.getTitle(_themeDisplay.getLocale())),
+						null, String.valueOf(article.getResourcePrimKey()),
+						LiferayWindowState.POP_UP.toString(), null,
+						_httpServletRequest));
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "permissions"));
+			}
+		).add(
+			() -> JournalArticlePermission.contains(
+				_themeDisplay.getPermissionChecker(), article,
+				ActionKeys.UPDATE),
+			dropdownItem -> {
+				dropdownItem.putData("destroyOnHide", "true");
+				dropdownItem.putData(
+					"id", _portletDisplay.getPortletName() + "editAsset");
+				dropdownItem.putData(
+					"title",
+					latestArticle.getTitle(_httpServletRequest.getLocale()));
+				dropdownItem.setHref(
+					_journalContentDisplayContext.getURLViewHistory());
+				dropdownItem.setLabel(
+					LanguageUtil.get(_httpServletRequest, "view-history"));
+			}
+		).build();
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final JournalContentDisplayContext _journalContentDisplayContext;
+	private final PortletDisplay _portletDisplay;
+	private final ThemeDisplay _themeDisplay;
+
+}
