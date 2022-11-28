72d30f8c90fbf7039bb536522f53230bd4d7f79b
Lily.Chi <lily.chi@liferay.com>
2022-11-28 20:07:10 +0800

    LPS-168433 Remove immediate=true module portal-settings

--- modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/portlet/util/ConfigurationProviderUtil.java
+++ modules/apps/portal-settings/portal-settings-authentication-ldap-web/src/main/java/com/liferay/portal/settings/authentication/ldap/web/internal/portlet/util/ConfigurationProviderUtil.java
@@ -26,7 +26,7 @@ import org.osgi.service.component.annotations.Reference;
 /**
  * @author Michael C. Han
  */
-@Component(immediate = true, service = {})
+@Component(service = {})
 public class ConfigurationProviderUtil {
 
 	public static ConfigurationProvider<LDAPAuthConfiguration>
