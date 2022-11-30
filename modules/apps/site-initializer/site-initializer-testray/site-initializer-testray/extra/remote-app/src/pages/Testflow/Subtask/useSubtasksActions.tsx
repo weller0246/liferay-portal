d405af32a909111848b78d57fe8d7d84608928c6
rafaelfs98 <rafaelfs98@outlook.com>
2022-11-30 12:39:12 -0300

    LPS-170045 Create "Assign Begin analysis" actions according to the status "OPEN"

--- modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/pages/Testflow/Subtask/useSubtasksActions.tsx
+++ modules/apps/site-initializer/site-initializer-testray/site-initializer-testray/extra/remote-app/src/pages/Testflow/Subtask/useSubtasksActions.tsx
@@ -19,9 +19,11 @@ import useFormModal from '../../../hooks/useFormModal';
 import useModalContext from '../../../hooks/useModalContext';
 import useMutate from '../../../hooks/useMutate';
 import i18n from '../../../i18n';
+import {Liferay} from '../../../services/liferay';
 import {TestraySubTask} from '../../../services/rest';
 import {testraySubTaskImpl} from '../../../services/rest/TestraySubtask';
 import {Action} from '../../../types';
+import {SubTaskStatuses} from '../../../util/statuses';
 import {UserListView} from '../../Manage/User';
 
 const useSubtasksActions = () => {
@@ -65,10 +67,21 @@ const useSubtasksActions = () => {
 					title: i18n.translate('users'),
 				}),
 			icon: 'user',
-			name: i18n.translate('assign'),
+			name: ({dueStatus}) => {
+				if (dueStatus.key === SubTaskStatuses.IN_ANALYSIS) {
+					return i18n.translate('assign');
+				}
+
+				if (dueStatus.key === SubTaskStatuses.OPEN) {
+					return i18n.translate('assign-and-begin-analysis');
+				}
+			},
 		},
 		{
 			action: (subtask) => completeModal.open(subtask),
+			hidden: ({dueStatus, user}) =>
+				user?.id !== Number(Liferay.ThemeDisplay.getUserId()) ||
+				dueStatus.key !== SubTaskStatuses.IN_ANALYSIS,
 			icon: 'polls',
 			name: i18n.sub('complete-x', ''),
 		},
