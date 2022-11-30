73c4917b92f7840b6beba981b4d21d015725dfd5
Jürgen Kappler <jurgen.kappler@liferay.com>
2022-11-30 12:33:27 +0100

    LPS-170032 Adds new export import content processor for handling mapped values in structure items

---
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/exportimport/content/processor/DataValuesMappingExportImportContentProcessor.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.page.template.internal.exportimport.content.processor;
+
+import com.liferay.exportimport.content.processor.ExportImportContentProcessor;
+import com.liferay.exportimport.kernel.lar.PortletDataContext;
+import com.liferay.portal.kernel.json.JSONFactory;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.model.StagedModel;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(
+	property = "content.processor.type=LayoutPageTemplateStructureRelReferences",
+	service = ExportImportContentProcessor.class
+)
+public class DataValuesMappingExportImportContentProcessor
+	implements ExportImportContentProcessor<String> {
+
+	@Override
+	public String replaceExportContentReferences(
+			PortletDataContext portletDataContext, StagedModel stagedModel,
+			String data, boolean exportReferencedContent, boolean escapeContent)
+		throws Exception {
+
+		if (!JSONUtil.isValid(data)) {
+			return data;
+		}
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject(data);
+
+		return jsonObject.toString();
+	}
+
+	@Override
+	public String replaceImportContentReferences(
+			PortletDataContext portletDataContext, StagedModel stagedModel,
+			String data)
+		throws Exception {
+
+		if (!JSONUtil.isValid(data)) {
+			return data;
+		}
+
+		JSONObject jsonObject = _jsonFactory.createJSONObject(data);
+
+		return jsonObject.toString();
+	}
+
+	@Override
+	public void validateContentReferences(long groupId, String data) {
+	}
+
+	@Reference
+	private JSONFactory _jsonFactory;
+
+}
