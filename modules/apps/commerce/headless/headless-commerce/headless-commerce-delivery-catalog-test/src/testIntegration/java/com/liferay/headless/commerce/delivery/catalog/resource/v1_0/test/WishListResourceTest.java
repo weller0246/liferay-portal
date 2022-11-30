d12d11a8755c0594229c972f4244bb8beb461a72
Stefano Motta <stefano.motta@liferay.com>
2022-11-30 16:15:12 +0100

    COMMERCE-10366 Integration tests for Wishlist API

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/catalog/resource/v1_0/test/WishListResourceTest.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-test/src/testIntegration/java/com/liferay/headless/commerce/delivery/catalog/resource/v1_0/test/WishListResourceTest.java
@@ -14,15 +14,145 @@
 
 package com.liferay.headless.commerce.delivery.catalog.resource.v1_0.test;
 
+import com.liferay.account.constants.AccountConstants;
+import com.liferay.account.model.AccountEntry;
+import com.liferay.account.service.AccountEntryLocalServiceUtil;
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.test.util.CommerceTestUtil;
+import com.liferay.headless.commerce.delivery.catalog.client.dto.v1_0.WishList;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+import com.liferay.portal.kernel.test.util.UserTestUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 
-import org.junit.Ignore;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
  * @author Andrea Sbarra
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class WishListResourceTest extends BaseWishListResourceTestCase {
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_user = UserTestUtil.addUser(testCompany);
+
+		_accountEntry = AccountEntryLocalServiceUtil.addAccountEntry(
+			_user.getUserId(), AccountConstants.PARENT_ACCOUNT_ENTRY_ID_DEFAULT,
+			RandomTestUtil.randomString(), RandomTestUtil.randomString(), null,
+			RandomTestUtil.randomString() + "@liferay.com", null,
+			RandomTestUtil.randomString(),
+			AccountConstants.ACCOUNT_ENTRY_TYPE_BUSINESS, 1,
+			ServiceContextTestUtil.getServiceContext(
+				testCompany.getCompanyId(), testGroup.getGroupId(),
+				_user.getUserId()));
+
+		_commerceChannel = CommerceTestUtil.addCommerceChannel(
+			testGroup.getGroupId(), RandomTestUtil.randomString());
+	}
+
+	@Override
+	@Test
+	public void testPatchChannelWishList() throws Exception {
+		WishList postWishList = testPatchChannelWishList_addWishList();
+
+		WishList randomPatchWishList = randomPatchWishList();
+
+		wishListResource.patchChannelWishList(
+			postWishList.getId(), randomPatchWishList);
+
+		WishList expectedPatchWishList = postWishList.clone();
+
+		BeanTestUtil.copyProperties(randomPatchWishList, expectedPatchWishList);
+
+		WishList getWishList = wishListResource.getWishList(
+			postWishList.getId());
+
+		assertEquals(expectedPatchWishList, getWishList);
+		assertValid(getWishList);
+	}
+
+	@Override
+	protected String[] getAdditionalAssertFieldNames() {
+		return new String[] {"defaultWishList", "name"};
+	}
+
+	@Override
+	protected WishList randomWishList() throws Exception {
+		return new WishList() {
+			{
+				defaultWishList = false;
+				id = RandomTestUtil.randomLong();
+				name = StringUtil.toLowerCase(RandomTestUtil.randomString());
+			}
+		};
+	}
+
+	@Override
+	protected WishList testDeleteWishList_addWishList() throws Exception {
+		return wishListResource.postChannelWishList(
+			_commerceChannel.getCommerceChannelId(),
+			_accountEntry.getAccountEntryId(), randomWishList());
+	}
+
+	@Override
+	protected WishList testGetChannelWishListsPage_addWishList(
+			Long channelId, WishList wishList)
+		throws Exception {
+
+		return wishListResource.postChannelWishList(
+			channelId, _accountEntry.getAccountEntryId(), wishList);
+	}
+
+	@Override
+	protected Long testGetChannelWishListsPage_getChannelId() throws Exception {
+		return _commerceChannel.getCommerceChannelId();
+	}
+
+	@Override
+	protected WishList testGetWishList_addWishList() throws Exception {
+		return wishListResource.postChannelWishList(
+			_commerceChannel.getCommerceChannelId(),
+			_accountEntry.getAccountEntryId(), randomWishList());
+	}
+
+	@Override
+	protected WishList testGraphQLWishList_addWishList() throws Exception {
+		return wishListResource.postChannelWishList(
+			_commerceChannel.getCommerceChannelId(),
+			_accountEntry.getAccountEntryId(), randomWishList());
+	}
+
+	protected WishList testPatchChannelWishList_addWishList() throws Exception {
+		return wishListResource.postChannelWishList(
+			_commerceChannel.getCommerceChannelId(),
+			_accountEntry.getAccountEntryId(), randomWishList());
+	}
+
+	@Override
+	protected WishList testPostChannelWishList_addWishList(WishList wishList)
+		throws Exception {
+
+		return wishListResource.postChannelWishList(
+			_commerceChannel.getCommerceChannelId(),
+			_accountEntry.getAccountEntryId(), wishList);
+	}
+
+	@DeleteAfterTestRun
+	private AccountEntry _accountEntry;
+
+	@DeleteAfterTestRun
+	private CommerceChannel _commerceChannel;
+
+	@DeleteAfterTestRun
+	private User _user;
+
 }
