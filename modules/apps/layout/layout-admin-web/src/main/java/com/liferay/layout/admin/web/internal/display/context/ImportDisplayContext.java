bdde9eacbd30e54592e18b2caf1f4b07ac33d303
Bárbara Cabrera <barbara.cabrera@liferay.com>
2022-12-01 18:22:43 +0000

    LPS-169953 Validate Utility pages imported to warn that they already exist

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/ImportDisplayContext.java
@@ -0,0 +1,282 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.display.context;
+
+import com.liferay.layout.importer.LayoutsImporterResultEntry;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.servlet.SessionMessages;
+import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.MapUtil;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import javax.portlet.RenderRequest;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Bárbara Cabrera
+ */
+public class ImportDisplayContext {
+
+	public ImportDisplayContext(
+		HttpServletRequest httpServletRequest, RenderRequest renderRequest) {
+
+		_httpServletRequest = httpServletRequest;
+		_renderRequest = renderRequest;
+	}
+
+	public String getDialogMessage() {
+		String dialogMessage =
+			"some-utility-pages-could-not-be-imported-but-other-utility-" +
+				"pages-were-imported-correctly-or-with-warnings";
+
+		List<LayoutsImporterResultEntry> importedLayoutsImporterResultEntries =
+			getImportedLayoutsImporterResultEntries();
+
+		List<LayoutsImporterResultEntry>
+			layoutsImporterResultEntriesWithWarnings =
+				getLayoutsImporterResultEntriesWithWarnings();
+
+		List<LayoutsImporterResultEntry>
+			notImportedLayoutsImporterResultEntries =
+				getNotImportedLayoutsImporterResultEntries();
+
+		if (ListUtil.isNotEmpty(importedLayoutsImporterResultEntries) &&
+			ListUtil.isNotEmpty(layoutsImporterResultEntriesWithWarnings) &&
+			ListUtil.isEmpty(notImportedLayoutsImporterResultEntries)) {
+
+			dialogMessage =
+				"some-page-templates-were-imported-correctly-and-other-page-" +
+					"templates-were-imported-with-warnings";
+		}
+		else if (ListUtil.isEmpty(layoutsImporterResultEntriesWithWarnings) &&
+				 ListUtil.isEmpty(notImportedLayoutsImporterResultEntries)) {
+
+			dialogMessage = "all-utility-pages-were-imported-correctly";
+		}
+		else if (ListUtil.isEmpty(importedLayoutsImporterResultEntries) &&
+				 ListUtil.isEmpty(layoutsImporterResultEntriesWithWarnings)) {
+
+			dialogMessage = "no-utility-pages-could-be-imported";
+		}
+		else if (ListUtil.isEmpty(importedLayoutsImporterResultEntries)) {
+			dialogMessage = "some-utility-pages-were-imported-with-warnings";
+		}
+
+		return LanguageUtil.get(_httpServletRequest, dialogMessage);
+	}
+
+	public String getDialogType() {
+		if (ListUtil.isEmpty(getImportedLayoutsImporterResultEntries()) &&
+			ListUtil.isEmpty(getLayoutsImporterResultEntriesWithWarnings())) {
+
+			return "danger";
+		}
+
+		if (ListUtil.isEmpty(getNotImportedLayoutsImporterResultEntries()) &&
+			ListUtil.isEmpty(getLayoutsImporterResultEntriesWithWarnings())) {
+
+			return "success";
+		}
+
+		return "warning";
+	}
+
+	public List<LayoutsImporterResultEntry>
+		getImportedLayoutsImporterResultEntries() {
+
+		if (_importedLayoutsImporterResultEntries != null) {
+			return _importedLayoutsImporterResultEntries;
+		}
+
+		Map<LayoutsImporterResultEntry.Status, List<LayoutsImporterResultEntry>>
+			layoutsImporterResultEntryMap = getLayoutsImporterResultEntryMap();
+
+		if (MapUtil.isEmpty(layoutsImporterResultEntryMap)) {
+			return null;
+		}
+
+		List<LayoutsImporterResultEntry> layoutsImporterResultEntries =
+			layoutsImporterResultEntryMap.get(
+				LayoutsImporterResultEntry.Status.IMPORTED);
+
+		if (layoutsImporterResultEntries == null) {
+			return null;
+		}
+
+		List<LayoutsImporterResultEntry> importedLayoutsImporterResultEntries =
+			new ArrayList<>();
+
+		for (LayoutsImporterResultEntry layoutsImporterResultEntry :
+				layoutsImporterResultEntries) {
+
+			if (ArrayUtil.isEmpty(
+					layoutsImporterResultEntry.getWarningMessages())) {
+
+				importedLayoutsImporterResultEntries.add(
+					layoutsImporterResultEntry);
+			}
+		}
+
+		return importedLayoutsImporterResultEntries;
+	}
+
+	public List<LayoutsImporterResultEntry>
+		getLayoutsImporterResultEntriesWithWarnings() {
+
+		if (_layoutsImporterResultEntriesWithWarnings != null) {
+			return _layoutsImporterResultEntriesWithWarnings;
+		}
+
+		Map<LayoutsImporterResultEntry.Status, List<LayoutsImporterResultEntry>>
+			layoutsImporterResultEntryMap = getLayoutsImporterResultEntryMap();
+
+		if (MapUtil.isEmpty(layoutsImporterResultEntryMap)) {
+			return null;
+		}
+
+		List<LayoutsImporterResultEntry> layoutsImporterResultEntries =
+			layoutsImporterResultEntryMap.get(
+				LayoutsImporterResultEntry.Status.IMPORTED);
+
+		if (layoutsImporterResultEntries == null) {
+			return null;
+		}
+
+		Stream<LayoutsImporterResultEntry> stream =
+			layoutsImporterResultEntries.stream();
+
+		_layoutsImporterResultEntriesWithWarnings = stream.filter(
+			layoutsImporterResultEntry -> ArrayUtil.isNotEmpty(
+				layoutsImporterResultEntry.getWarningMessages())
+		).collect(
+			Collectors.toList()
+		);
+
+		return _layoutsImporterResultEntriesWithWarnings;
+	}
+
+	public Map
+		<LayoutsImporterResultEntry.Status, List<LayoutsImporterResultEntry>>
+			getLayoutsImporterResultEntryMap() {
+
+		if (MapUtil.isNotEmpty(_layoutsImporterResultEntryMap)) {
+			return _layoutsImporterResultEntryMap;
+		}
+
+		List<LayoutsImporterResultEntry> layoutsImporterResultEntries =
+			(List<LayoutsImporterResultEntry>)SessionMessages.get(
+				_renderRequest, "layoutUtilityPageImporterResultEntries");
+
+		if (layoutsImporterResultEntries == null) {
+			return null;
+		}
+
+		_layoutsImporterResultEntryMap = new HashMap<>();
+
+		for (LayoutsImporterResultEntry layoutsImporterResultEntry :
+				layoutsImporterResultEntries) {
+
+			List<LayoutsImporterResultEntry>
+				statusLayoutsImporterResultEntries = new ArrayList<>();
+
+			LayoutsImporterResultEntry.Status status =
+				layoutsImporterResultEntry.getStatus();
+
+			if (_layoutsImporterResultEntryMap.get(status) != null) {
+				statusLayoutsImporterResultEntries =
+					_layoutsImporterResultEntryMap.get(status);
+			}
+
+			statusLayoutsImporterResultEntries.add(layoutsImporterResultEntry);
+
+			_layoutsImporterResultEntryMap.put(
+				status, statusLayoutsImporterResultEntries);
+		}
+
+		return _layoutsImporterResultEntryMap;
+	}
+
+	public List<LayoutsImporterResultEntry>
+		getNotImportedLayoutsImporterResultEntries() {
+
+		if (_notImportedLayoutsImporterResultEntries != null) {
+			return _notImportedLayoutsImporterResultEntries;
+		}
+
+		Map<LayoutsImporterResultEntry.Status, List<LayoutsImporterResultEntry>>
+			layoutsImporterResultEntryMap = getLayoutsImporterResultEntryMap();
+
+		if (MapUtil.isEmpty(layoutsImporterResultEntryMap)) {
+			return null;
+		}
+
+		List<LayoutsImporterResultEntry>
+			notImportedLayoutsImporterResultEntries = new ArrayList<>();
+
+		for (Map.Entry
+				<LayoutsImporterResultEntry.Status,
+				 List<LayoutsImporterResultEntry>> entrySet :
+					layoutsImporterResultEntryMap.entrySet()) {
+
+			if (entrySet.getKey() !=
+					LayoutsImporterResultEntry.Status.IMPORTED) {
+
+				notImportedLayoutsImporterResultEntries.addAll(
+					entrySet.getValue());
+			}
+		}
+
+		return notImportedLayoutsImporterResultEntries;
+	}
+
+	public String getSuccessMessage(
+		List<LayoutsImporterResultEntry> layoutsImporterResultEntries) {
+
+		return LanguageUtil.format(
+			_httpServletRequest, "x-x-s-imported-correctly",
+			new String[] {
+				"utility-page",
+				String.valueOf(layoutsImporterResultEntries.size())
+			},
+			true);
+	}
+
+	public String getWarningMessage(String layoutsImporterResultEntryName) {
+		return LanguageUtil.format(
+			_httpServletRequest, "x-was-imported-with-warnings",
+			new Object[] {layoutsImporterResultEntryName}, true);
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private List<LayoutsImporterResultEntry>
+		_importedLayoutsImporterResultEntries;
+	private List<LayoutsImporterResultEntry>
+		_layoutsImporterResultEntriesWithWarnings;
+	private Map
+		<LayoutsImporterResultEntry.Status, List<LayoutsImporterResultEntry>>
+			_layoutsImporterResultEntryMap;
+	private List<LayoutsImporterResultEntry>
+		_notImportedLayoutsImporterResultEntries;
+	private final RenderRequest _renderRequest;
+
+}
--- modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/view_import.jsp
+++ modules/apps/layout/layout-admin-web/src/main/resources/META-INF/resources/view_import.jsp
@@ -16,6 +16,10 @@
 
 <%@ include file="/init.jsp" %>
 
+<%
+ImportDisplayContext importDisplayContext = new ImportDisplayContext(request, renderRequest);
+%>
+
 <portlet:actionURL name="/layout_admin/import_layout_utility_page_entries" var="importURL">
 	<portlet:param name="redirect" value="<%= currentURL %>" />
 	<portlet:param name="portletResource" value="<%= portletDisplay.getId() %>" />
@@ -46,6 +50,103 @@
 
 			<aui:input checked="<%= true %>" label="overwrite-existing-utility-pages" name="overwrite" type="checkbox" />
 		</liferay-frontend:fieldset>
+
+		<%
+		Map<LayoutsImporterResultEntry.Status, List<LayoutsImporterResultEntry>> layoutsImporterResultEntryMap = importDisplayContext.getLayoutsImporterResultEntryMap();
+		%>
+
+		<c:if test="<%= MapUtil.isNotEmpty(layoutsImporterResultEntryMap) %>">
+
+			<%
+			String dialogType = importDisplayContext.getDialogType();
+			%>
+
+			<div class="alert alert-<%= dialogType %> <%= dialogType %>-dialog">
+				<span class="<%= dialogType %>-message"><%= importDisplayContext.getDialogMessage() %></span>
+
+				<ul class="<%= dialogType %>-list-items">
+
+					<%
+					List<LayoutsImporterResultEntry> importedLayoutsImporterResultEntriesMap = importDisplayContext.getImportedLayoutsImporterResultEntries();
+					%>
+
+					<c:if test="<%= ListUtil.isNotEmpty(importedLayoutsImporterResultEntriesMap) %>">
+						<li>
+							<span class="<%= dialogType %>-info"><%= HtmlUtil.escape(importDisplayContext.getSuccessMessage(importedLayoutsImporterResultEntriesMap)) %></span>
+						</li>
+					</c:if>
+
+					<%
+					List<LayoutsImporterResultEntry> layoutsImporterResultEntriesWithWarnings = importDisplayContext.getLayoutsImporterResultEntriesWithWarnings();
+					%>
+
+					<c:if test="<%= ListUtil.isNotEmpty(layoutsImporterResultEntriesWithWarnings) %>">
+
+						<%
+						for (int i = 0; i < layoutsImporterResultEntriesWithWarnings.size(); i++) {
+							LayoutsImporterResultEntry layoutsImporterResultEntry = layoutsImporterResultEntriesWithWarnings.get(i);
+
+							String[] warningMessages = layoutsImporterResultEntry.getWarningMessages();
+						%>
+
+							<li>
+								<span class="<%= dialogType %>-info"><%= HtmlUtil.escape(importDisplayContext.getWarningMessage(layoutsImporterResultEntry.getName())) %></span>
+
+								<ul>
+
+									<%
+									for (String warningMessage : warningMessages) {
+									%>
+
+										<li><span class="<%= dialogType %>-info"><%= HtmlUtil.escape(warningMessage) %></span></li>
+
+									<%
+									}
+									%>
+
+								</ul>
+							</li>
+
+						<%
+						}
+						%>
+
+					</c:if>
+
+					<%
+					int total = 0;
+					int viewTotal = 0;
+
+					List<LayoutsImporterResultEntry> notImportedLayoutsImporterResultEntries = importDisplayContext.getNotImportedLayoutsImporterResultEntries();
+					%>
+
+					<c:if test="<%= ListUtil.isNotEmpty(notImportedLayoutsImporterResultEntries) %>">
+
+						<%
+						total = notImportedLayoutsImporterResultEntries.size();
+
+						viewTotal = (total > 10) ? 10 : total;
+
+						for (int i = 0; i < viewTotal; i++) {
+							LayoutsImporterResultEntry layoutsImporterResultEntry = notImportedLayoutsImporterResultEntries.get(i);
+						%>
+
+							<li>
+								<span class="<%= dialogType %>-info"><%= HtmlUtil.escape(layoutsImporterResultEntry.getErrorMessage()) %></span>
+							</li>
+
+						<%
+						}
+						%>
+
+					</c:if>
+				</ul>
+
+				<c:if test="<%= total > 10 %>">
+					<span class="<%= dialogType %>-info"><%= LanguageUtil.format(request, "x-more-entries-could-also-not-be-imported", "<strong>" + (total - viewTotal) + "</strong>", false) %></span>
+				</c:if>
+			</div>
+		</c:if>
 	</liferay-frontend:edit-form-body>
 
 	<liferay-frontend:edit-form-footer>
