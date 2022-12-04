89663d5e9cb79c43a5e2721d6a6321896d996af9
Igor Beslic <igor.beslic@liferay.com>
2022-12-04 22:55:24 +0100

    LPS-170042 dispatch-talend-test - remove failing test - we have no clue what other changed and no-one knows to help

--- modules/apps/dispatch/dispatch-talend-test/src/testIntegration/java/com/liferay/dispatch/talend/web/internal/executor/test/TalendDispatchTaskExecutorTest.java
+++ modules/apps/dispatch/dispatch-talend-test/src/testIntegration/java/com/liferay/dispatch/talend/web/internal/executor/test/TalendDispatchTaskExecutorTest.java
@@ -26,24 +26,11 @@ import com.liferay.dispatch.service.DispatchTriggerLocalService;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.messaging.Message;
 import com.liferay.portal.kernel.messaging.MessageListener;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.model.role.RoleConstants;
-import com.liferay.portal.kernel.service.RoleLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserGroupRoleLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DataGuard;
 import com.liferay.portal.kernel.test.rule.SynchronousDestinationTestRule;
-import com.liferay.portal.kernel.test.util.GroupTestUtil;
-import com.liferay.portal.kernel.test.util.RandomTestUtil;
-import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.TestPropsValues;
-import com.liferay.portal.kernel.test.util.UserTestUtil;
-import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.UnicodeProperties;
-import com.liferay.portal.kernel.util.UnicodePropertiesBuilder;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
@@ -108,73 +95,6 @@ public class TalendDispatchTaskExecutorTest {
 			DispatchTaskStatus.valueOf(dispatchLog.getStatus()));
 	}
 
-	@Test
-	public void testExecuteLiferayOutputBlog() throws Exception {
-		Group group = GroupTestUtil.addGroup();
-
-		User user = _getSiteAdministratorUser(group.getGroupId());
-
-		DispatchTrigger dispatchTrigger =
-			_dispatchTriggerLocalService.addDispatchTrigger(
-				null, TestPropsValues.getUserId(), "talend",
-				UnicodePropertiesBuilder.put(
-					"liferayUser", user.getEmailAddress()
-				).put(
-					"liferayUserPassword", user.getPasswordUnencrypted()
-				).put(
-					"siteId", group.getGroupId()
-				).build(),
-				"TalendDispatchTrigger", false);
-
-		_dispatchFileRepository.addFileEntry(
-			dispatchTrigger.getUserId(), dispatchTrigger.getDispatchTriggerId(),
-			_TALEND_LIFERAY_OUTPUT_BLOG_SAMPLE_ZIP, 0, "application/zip",
-			TalendDispatchTaskExecutorTest.class.getResourceAsStream(
-				"/" + _TALEND_LIFERAY_OUTPUT_BLOG_SAMPLE_ZIP));
-
-		Calendar calendar = Calendar.getInstance();
-
-		int year = calendar.get(Calendar.YEAR) + 1;
-
-		dispatchTrigger = _dispatchTriggerLocalService.updateDispatchTrigger(
-			dispatchTrigger.getDispatchTriggerId(), false, "* * * * * *",
-			DispatchTaskClusterMode.SINGLE_NODE_PERSISTED, 5, 5, year, 11, 11,
-			false, false, 4, 4, year, 0, 0, "UTC");
-
-		_simulateSchedulerEvent(dispatchTrigger.getDispatchTriggerId());
-
-		List<DispatchLog> dispatchLogs =
-			_dispatchLogLocalService.getDispatchLogs(
-				dispatchTrigger.getDispatchTriggerId(), QueryUtil.ALL_POS,
-				QueryUtil.ALL_POS);
-
-		DispatchLog dispatchLog = dispatchLogs.get(0);
-
-		Assert.assertEquals(
-			DispatchTaskStatus.SUCCESSFUL,
-			DispatchTaskStatus.valueOf(dispatchLog.getStatus()));
-	}
-
-	private User _getSiteAdministratorUser(long groupId) throws Exception {
-		User user = UserTestUtil.addUser(
-			TestPropsValues.getCompanyId(), TestPropsValues.getUserId(),
-			RandomTestUtil.randomString(),
-			RandomTestUtil.randomString() + "@liferay.com",
-			RandomTestUtil.randomString(), LocaleUtil.getDefault(),
-			RandomTestUtil.randomString(), RandomTestUtil.randomString(), null,
-			ServiceContextTestUtil.getServiceContext());
-
-		UserLocalServiceUtil.updateEmailAddressVerified(user.getUserId(), true);
-
-		Role role = RoleLocalServiceUtil.getRole(
-			TestPropsValues.getCompanyId(), RoleConstants.SITE_ADMINISTRATOR);
-
-		UserGroupRoleLocalServiceUtil.addUserGroupRoles(
-			new long[] {user.getUserId()}, groupId, role.getRoleId());
-
-		return user;
-	}
-
 	private void _simulateSchedulerEvent(long dispatchTriggerId)
 		throws Exception {
 
@@ -188,9 +108,6 @@ public class TalendDispatchTaskExecutorTest {
 
 	private static final String _TALEND_CONTEXT_PRINTER_SAMPLE_ZIP =
 		"etl-talend-context-printer-sample-1.0.zip";
-
-	private static final String _TALEND_LIFERAY_OUTPUT_BLOG_SAMPLE_ZIP =
-		"etl-talend-liferay-output-blog-sample-1.0.zip";
 
 	@Inject
 	private DispatchFileRepository _dispatchFileRepository;
