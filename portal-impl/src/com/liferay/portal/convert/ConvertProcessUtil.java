ebec0ec1d20cf07329482dac5f5b045325e08ac3
shuyangzhou <shuyang.zhou@liferay.com>
2022-12-05 07:51:42 -0800

    LPS-170540 Track ConvertProcess by ServiceTrackerMap

--- portal-impl/src/com/liferay/portal/convert/ConvertProcessUtil.java
+++ portal-impl/src/com/liferay/portal/convert/ConvertProcessUtil.java
@@ -14,21 +14,27 @@
 
 package com.liferay.portal.convert;
 
-import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
-import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.module.util.SystemBundleUtil;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
+import org.osgi.framework.BundleContext;
+
 /**
  * @author Iván Zaera
  */
 public class ConvertProcessUtil {
 
+	public static ConvertProcess getConvertProcess(String className) {
+		return _convertProcesses.getService(className);
+	}
+
 	public static Collection<ConvertProcess> getConvertProcesses() {
-		return _convertProcesses.toList();
+		return _convertProcesses.values();
 	}
 
 	public static Collection<ConvertProcess> getEnabledConvertProcesses() {
@@ -48,8 +54,21 @@ public class ConvertProcessUtil {
 		return convertProcesses;
 	}
 
-	private static final ServiceTrackerList<ConvertProcess> _convertProcesses =
-		ServiceTrackerListFactory.open(
-			SystemBundleUtil.getBundleContext(), ConvertProcess.class);
+	private static final BundleContext _bundleContext =
+		SystemBundleUtil.getBundleContext();
+
+	private static final ServiceTrackerMap<String, ConvertProcess>
+		_convertProcesses = ServiceTrackerMapFactory.openSingleValueMap(
+			_bundleContext, ConvertProcess.class, null,
+			(serviceReference, emitter) -> {
+				ConvertProcess convertProcess = _bundleContext.getService(
+					serviceReference);
+
+				Class<?> clazz = convertProcess.getClass();
+
+				emitter.emit(clazz.getName());
+
+				_bundleContext.ungetService(serviceReference);
+			});
 
 }
