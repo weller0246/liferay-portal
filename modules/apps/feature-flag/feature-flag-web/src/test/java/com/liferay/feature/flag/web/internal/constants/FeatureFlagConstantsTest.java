79f44207b62be97a4e140ad2117cecb6a05a3953
drewbrokke <drew.brokke@liferay.com>
2022-12-05 11:23:47 -0600

    LPS-168928 feature-flag-web: adds FeatureFlagsConstants and unit test

---
+++ modules/apps/feature-flag/feature-flag-web/src/test/java/com/liferay/feature/flag/web/internal/constants/FeatureFlagConstantsTest.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.feature.flag.web.internal.constants;
+
+import com.liferay.portal.test.rule.LiferayUnitTestRule;
+
+import org.junit.Assert;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+/**
+ * @author Drew Brokke
+ */
+public class FeatureFlagConstantsTest {
+
+	@ClassRule
+	@Rule
+	public static final LiferayUnitTestRule liferayUnitTestRule =
+		LiferayUnitTestRule.INSTANCE;
+
+	@Test
+	public void testGetKey() {
+		Assert.assertEquals("feature.flag", FeatureFlagConstants.getKey());
+		Assert.assertEquals(
+			"feature.flag.foo.bar", FeatureFlagConstants.getKey("foo", "bar"));
+	}
+
+}
