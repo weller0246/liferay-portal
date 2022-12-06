dfd6735b89ad54922103bf5f1025ce3cc426c811
Lourdes Fernández Besada <lourdes.fernandez@liferay.com>
2022-12-06 10:51:35 +0100

    LPS-169953 Reuse LayoutUtilityPageEntryPortalExceptionRequestHandler

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddLayoutUtilityPageEntryMVCActionCommand.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/portlet/action/AddLayoutUtilityPageEntryMVCActionCommand.java
@@ -15,17 +15,13 @@
 package com.liferay.layout.admin.web.internal.portlet.action;
 
 import com.liferay.layout.admin.constants.LayoutAdminPortletKeys;
-import com.liferay.layout.utility.page.exception.LayoutUtilityPageEntryNameException;
+import com.liferay.layout.admin.web.internal.handler.LayoutUtilityPageEntryPortalExceptionRequestHandler;
 import com.liferay.layout.utility.page.model.LayoutUtilityPageEntry;
 import com.liferay.layout.utility.page.service.LayoutUtilityPageEntryLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.json.JSONUtil;
-import com.liferay.portal.kernel.language.Language;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Layout;
-import com.liferay.portal.kernel.model.ModelHintsUtil;
 import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
 import com.liferay.portal.kernel.portlet.PortletURLFactoryUtil;
 import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
@@ -40,7 +36,6 @@ import com.liferay.portal.kernel.util.Constants;
 import com.liferay.portal.kernel.util.HttpComponentsUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import javax.portlet.ActionRequest;
@@ -96,71 +91,10 @@ public class AddLayoutUtilityPageEntryMVCActionCommand
 		catch (PortalException portalException) {
 			hideDefaultErrorMessage(actionRequest);
 
-			_handlePortalException(
-				actionRequest, actionResponse, portalException);
+			_layoutUtilityPageEntryPortalExceptionRequestHandler.
+				handlePortalException(
+					actionRequest, actionResponse, portalException);
 		}
-	}
-
-	private JSONObject _createErrorJSONObject(
-		ActionRequest actionRequest, PortalException portalException) {
-
-		if (_log.isDebugEnabled()) {
-			_log.debug(portalException);
-		}
-
-		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
-			WebKeys.THEME_DISPLAY);
-
-		String errorMessage = null;
-
-		if (portalException instanceof
-				LayoutUtilityPageEntryNameException.MustNotBeDuplicate) {
-
-			errorMessage = _language.get(
-				themeDisplay.getLocale(),
-				"a-utility-page-with-that-name-already-exists");
-		}
-		else if (portalException instanceof
-					LayoutUtilityPageEntryNameException.MustNotBeNull) {
-
-			errorMessage = _language.get(
-				themeDisplay.getLocale(), "name-must-not-be-empty");
-		}
-		else if (portalException instanceof
-					LayoutUtilityPageEntryNameException.
-						MustNotContainInvalidCharacters) {
-
-			LayoutUtilityPageEntryNameException.MustNotContainInvalidCharacters
-				lptene =
-					(LayoutUtilityPageEntryNameException.
-						MustNotContainInvalidCharacters)portalException;
-
-			errorMessage = _language.format(
-				themeDisplay.getLocale(),
-				"name-cannot-contain-the-following-invalid-character-x",
-				lptene.character);
-		}
-		else if (portalException instanceof
-					LayoutUtilityPageEntryNameException.
-						MustNotExceedMaximumSize) {
-
-			int nameMaxLength = ModelHintsUtil.getMaxLength(
-				LayoutUtilityPageEntry.class.getName(), "name");
-
-			errorMessage = _language.format(
-				themeDisplay.getLocale(),
-				"please-enter-a-name-with-fewer-than-x-characters",
-				nameMaxLength);
-		}
-
-		if (Validator.isNull(errorMessage)) {
-			errorMessage = _language.get(
-				themeDisplay.getLocale(), "an-unexpected-error-occurred");
-
-			_log.error(portalException);
-		}
-
-		return JSONUtil.put("error", errorMessage);
 	}
 
 	private String _getRedirectURL(
@@ -191,30 +125,16 @@ public class AddLayoutUtilityPageEntryMVCActionCommand
 			layoutFullURL, "p_l_mode", Constants.EDIT);
 	}
 
-	private void _handlePortalException(
-			ActionRequest actionRequest, ActionResponse actionResponse,
-			PortalException portalException)
-		throws Exception {
-
-		JSONObject errorJSONObject = _createErrorJSONObject(
-			actionRequest, portalException);
-
-		JSONPortletResponseUtil.writeJSON(
-			actionRequest, actionResponse, errorJSONObject);
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		AddLayoutUtilityPageEntryMVCActionCommand.class);
-
-	@Reference
-	private Language _language;
-
 	@Reference
 	private LayoutLocalService _layoutLocalService;
 
 	@Reference
 	private LayoutUtilityPageEntryLocalService
 		_layoutUtilityPageEntryLocalService;
+
+	@Reference
+	private LayoutUtilityPageEntryPortalExceptionRequestHandler
+		_layoutUtilityPageEntryPortalExceptionRequestHandler;
 
 	@Reference
 	private Portal _portal;
