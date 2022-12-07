8ae1648104a1ab2df9b04e74278ba6ec897abbd5
Brian Chan <brian.chan@liferay.com>
2022-12-07 11:37:40 -0300

    Revert "LPS-169164 Lazy digest calculation to avoid startup time impact."
    This reverts commit ee28af8366f6e4c6f0bfc212dc81c44c182efdda.

--- modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/servlet/JSResolveModulesServlet.java
+++ modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/servlet/JSResolveModulesServlet.java
@@ -37,7 +37,6 @@ import java.net.URLDecoder;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
-import java.util.Objects;
 
 import javax.servlet.Servlet;
 import javax.servlet.http.HttpServlet;
@@ -72,7 +71,7 @@ public class JSResolveModulesServlet
 	extends HttpServlet implements NPMRegistryUpdatesListener {
 
 	public String getURL() {
-		return _state.getUrl();
+		return _state.url;
 	}
 
 	@Override
@@ -141,10 +140,7 @@ public class JSResolveModulesServlet
 			return;
 		}
 
-		if (!Objects.equals(
-				state.getExpectedPathInfo(),
-				httpServletRequest.getPathInfo())) {
-
+		if (!state.expectedPathInfo.equals(httpServletRequest.getPathInfo())) {
 			AbsolutePortalURLBuilder absolutePortalURLBuilder =
 				_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
 					httpServletRequest);
@@ -218,7 +214,10 @@ public class JSResolveModulesServlet
 			return;
 		}
 
-		_state = new State(npmRegistry);
+		NPMRegistryResolutionStateDigest npmRegistryResolutionStateDigest =
+			new NPMRegistryResolutionStateDigest(npmRegistry);
+
+		_state = new State(npmRegistryResolutionStateDigest.getDigest());
 	}
 
 	@Reference
@@ -233,59 +232,13 @@ public class JSResolveModulesServlet
 
 	private static class State {
 
-		public String getExpectedPathInfo() {
-			String expectedPathInfo = _expectedPathInfo;
-
-			if (expectedPathInfo != null) {
-				return expectedPathInfo;
-			}
-
-			synchronized (this) {
-				if (_expectedPathInfo == null) {
-					_calculate();
-				}
-
-				expectedPathInfo = _expectedPathInfo;
-			}
-
-			return expectedPathInfo;
+		public State(String digest) {
+			expectedPathInfo = StringPool.SLASH + digest;
+			url = "/js_resolve_modules/" + digest;
 		}
 
-		public String getUrl() {
-			String url = _url;
-
-			if (url != null) {
-				return url;
-			}
-
-			synchronized (this) {
-				if (_url == null) {
-					_calculate();
-				}
-
-				url = _url;
-			}
-
-			return url;
-		}
-
-		private State(NPMRegistry npmRegistry) {
-			_npmRegistry = npmRegistry;
-		}
-
-		private void _calculate() {
-			NPMRegistryResolutionStateDigest npmRegistryResolutionStateDigest =
-				new NPMRegistryResolutionStateDigest(_npmRegistry);
-
-			String digest = npmRegistryResolutionStateDigest.getDigest();
-
-			_expectedPathInfo = StringPool.SLASH + digest;
-			_url = "/js_resolve_modules/" + digest;
-		}
-
-		private volatile String _expectedPathInfo;
-		private final NPMRegistry _npmRegistry;
-		private volatile String _url;
+		public final String expectedPathInfo;
+		public final String url;
 
 	}
 
