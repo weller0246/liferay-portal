f614cec9ba84b4ad715f93ba21a072f28ab6c1b5
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2022-12-07 20:17:37 +0100

    LPS-170682 Don't show plus button for utility pages on live group

--- modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutUtilityPageEntryManagementToolbarDisplayContext.java
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/display/context/LayoutUtilityPageEntryManagementToolbarDisplayContext.java
@@ -18,13 +18,18 @@ import com.liferay.frontend.taglib.clay.servlet.taglib.display.context.SearchCon
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.CreationMenu;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItem;
 import com.liferay.frontend.taglib.clay.servlet.taglib.util.DropdownItemListBuilder;
+import com.liferay.layout.utility.page.constants.LayoutUtilityPageActionKeys;
 import com.liferay.layout.utility.page.kernel.LayoutUtilityPageEntryViewRenderer;
 import com.liferay.layout.utility.page.kernel.LayoutUtilityPageEntryViewRendererRegistryUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
 import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
 import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.service.permission.GroupPermissionUtil;
 import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.WebKeys;
 
@@ -144,6 +149,33 @@ public class LayoutUtilityPageEntryManagementToolbarDisplayContext
 	}
 
 	@Override
+	public Boolean isShowCreationMenu() {
+		Group group = _themeDisplay.getScopeGroup();
+
+		if (group.hasLocalOrRemoteStagingGroup()) {
+			return false;
+		}
+
+		try {
+			if (GroupPermissionUtil.contains(
+					_themeDisplay.getPermissionChecker(),
+					_themeDisplay.getScopeGroup(),
+					LayoutUtilityPageActionKeys.
+						ADD_LAYOUT_UTILITY_PAGE_ENTRY)) {
+
+				return true;
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(exception);
+			}
+		}
+
+		return false;
+	}
+
+	@Override
 	protected String[] getOrderByKeys() {
 		return new String[] {"name", "create-date"};
 	}
@@ -169,6 +201,9 @@ public class LayoutUtilityPageEntryManagementToolbarDisplayContext
 			"type", type
 		).buildString();
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutUtilityPageEntryManagementToolbarDisplayContext.class);
 
 	private final ThemeDisplay _themeDisplay;
 
