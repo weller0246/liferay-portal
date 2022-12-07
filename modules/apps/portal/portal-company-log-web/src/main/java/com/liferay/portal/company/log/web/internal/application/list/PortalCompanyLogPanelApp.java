77ad815e15caebeb7ee6b8a985636eb365ccf302
Keven Leone <keven.leone@liferay.com>
2022-12-07 14:23:19 -0300

    LPS-170670 Add Feature Flag to PortalCompanyLog

--- modules/apps/portal/portal-company-log-web/src/main/java/com/liferay/portal/company/log/web/internal/application/list/PortalCompanyLogPanelApp.java
+++ modules/apps/portal/portal-company-log-web/src/main/java/com/liferay/portal/company/log/web/internal/application/list/PortalCompanyLogPanelApp.java
@@ -18,7 +18,12 @@ import com.liferay.application.list.BasePanelApp;
 import com.liferay.application.list.PanelApp;
 import com.liferay.application.list.constants.PanelCategoryKeys;
 import com.liferay.portal.company.log.web.internal.constants.PortalCompanyLogPortletKeys;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.Portlet;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.PropsUtil;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -39,6 +44,17 @@ public class PortalCompanyLogPanelApp extends BasePanelApp {
 	@Override
 	public String getPortletId() {
 		return PortalCompanyLogPortletKeys.PORTAL_COMPANY_LOG;
+	}
+
+	@Override
+	public boolean isShow(PermissionChecker permissionChecker, Group group)
+		throws PortalException {
+
+		if (!GetterUtil.getBoolean(PropsUtil.get("feature.flag.LPS-170670"))) {
+			return false;
+		}
+
+		return super.isShow(permissionChecker, group);
 	}
 
 	@Override
