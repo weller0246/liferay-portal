afb6dc801423cce6d54657ebcbe63a225b736a0e
Brian Chan <brian.chan@liferay.com>
2022-12-08 10:47:14 -0300

    LPS-170271 Default to admins only

--- modules/apps/portal/portal-company-log-web/src/main/java/com/liferay/portal/company/log/web/internal/portlet/PortalCompanyLogPortlet.java
+++ modules/apps/portal/portal-company-log-web/src/main/java/com/liferay/portal/company/log/web/internal/portlet/PortalCompanyLogPortlet.java
@@ -41,7 +41,7 @@ import org.osgi.service.component.annotations.Component;
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + PortalCompanyLogPortletKeys.PORTAL_COMPANY_LOG,
 		"javax.portlet.resource-bundle=content.Language",
-		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.security-role-ref=administrator",
 		"javax.portlet.version=3.0"
 	},
 	service = Portlet.class
