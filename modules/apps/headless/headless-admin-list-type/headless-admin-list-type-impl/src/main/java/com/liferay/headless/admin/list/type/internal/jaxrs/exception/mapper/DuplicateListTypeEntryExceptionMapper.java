09da5603facfc6117ab79033f5de8966fa93e476
Nathaly Gomes <nathaly.gomes@liferay.com>
2022-12-09 16:08:09 -0300

    LPS-170322 create mapper to DuplicateListTypeEntryException

---
+++ modules/apps/headless/headless-admin-list-type/headless-admin-list-type-impl/src/main/java/com/liferay/headless/admin/list/type/internal/jaxrs/exception/mapper/DuplicateListTypeEntryExceptionMapper.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.admin.list.type.internal.jaxrs.exception.mapper;
+
+import com.liferay.list.type.exception.DuplicateListTypeEntryException;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.BaseExceptionMapper;
+import com.liferay.portal.vulcan.jaxrs.exception.mapper.Problem;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Nathaly Gomes
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Admin.List.Type)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Admin.List.Type.DuplicateListTypeEntryExceptionMapper"
+	},
+	service = ExceptionMapper.class
+)
+public class DuplicateListTypeEntryExceptionMapper
+	extends BaseExceptionMapper<DuplicateListTypeEntryException> {
+
+	@Override
+	protected Problem getProblem(
+		DuplicateListTypeEntryException duplicateListTypeEntryException) {
+
+		return new Problem(
+			Response.Status.BAD_REQUEST,
+			duplicateListTypeEntryException.getMessage());
+	}
+
+	@Override
+	protected boolean isSanitize() {
+		return false;
+	}
+
+}
