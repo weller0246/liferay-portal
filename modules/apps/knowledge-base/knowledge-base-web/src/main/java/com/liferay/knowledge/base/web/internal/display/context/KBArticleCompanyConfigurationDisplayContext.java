c2a477554422bb4e99f21cab08f107a1100305c2
Alicia García <alicia.garcia@liferay.com>
2022-12-12 17:58:29 +0100

    LPS-170077 add context and jsp

---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/java/com/liferay/knowledge/base/web/internal/display/context/KBArticleCompanyConfigurationDisplayContext.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.knowledge.base.web.internal.display.context;
+
+import com.liferay.knowledge.base.configuration.KBServiceConfigurationProvider;
+import com.liferay.portal.kernel.module.configuration.ConfigurationException;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.url.builder.PortletURLBuilder;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alicia García
+ */
+public class KBArticleCompanyConfigurationDisplayContext {
+
+	public KBArticleCompanyConfigurationDisplayContext(
+		HttpServletRequest httpServletRequest,
+		KBServiceConfigurationProvider kbServiceConfigurationProvider,
+		LiferayPortletResponse liferayPortletResponse) {
+
+		_httpServletRequest = httpServletRequest;
+		_kbServiceConfigurationProvider = kbServiceConfigurationProvider;
+		_liferayPortletResponse = liferayPortletResponse;
+	}
+
+	public int getCheckInterval() throws ConfigurationException {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _kbServiceConfigurationProvider.getCheckInterval(
+			themeDisplay.getCompanyId());
+	}
+
+	public String getEditKBArticleConfigurationURL() {
+		return PortletURLBuilder.createActionURL(
+			_liferayPortletResponse
+		).setActionName(
+			"/instance_settings/edit_kb_article_expiration_date_configuration"
+		).setRedirect(
+			PortalUtil.getCurrentURL(_httpServletRequest)
+		).buildString();
+	}
+
+	public int getExpirationDateNotificationDateWeeks()
+		throws ConfigurationException {
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)_httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return _kbServiceConfigurationProvider.
+			getExpirationDateNotificationDateWeeks(themeDisplay.getCompanyId());
+	}
+
+	private final HttpServletRequest _httpServletRequest;
+	private final KBServiceConfigurationProvider
+		_kbServiceConfigurationProvider;
+	private final LiferayPortletResponse _liferayPortletResponse;
+
+}
---
+++ modules/apps/knowledge-base/knowledge-base-web/src/main/resources/META-INF/resources/admin/knowledge_base_settings/kb_article_expiration_date_configuration.jsp
@@ -0,0 +1,51 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/admin/knowledge_base_settings/init.jsp" %>
+
+<%
+KBArticleCompanyConfigurationDisplayContext kbArticleCompanyConfigurationDisplayContext = (KBArticleCompanyConfigurationDisplayContext)request.getAttribute(KBArticleCompanyConfigurationDisplayContext.class.getName());
+%>
+
+<aui:form action="<%= kbArticleCompanyConfigurationDisplayContext.getEditKBArticleConfigurationURL() %>" method="post" name="fm">
+	<clay:sheet>
+		<clay:sheet-header>
+			<h2>
+				<liferay-ui:message key="knowledge-base-service-configuration-name" />
+			</h2>
+		</clay:sheet-header>
+
+		<clay:sheet-section>
+			<aui:input label="check-interval" name="checkInterval" value="<%= kbArticleCompanyConfigurationDisplayContext.getCheckInterval() %>" />
+		</clay:sheet-section>
+
+		<clay:sheet-section>
+			<h3 class="sheet-subtitle"><liferay-ui:message key="article-expiration-date-notification" /></h3>
+
+			<p class="text-muted">
+				<liferay-ui:message key="expiration-date-notification-date-weeks-description" />
+			</p>
+
+			<div>
+				<aui:input helpMessage="expiration-date-notification-date-weeks-help" label="notification-date-weeks" name="expirationDateNotificationDateWeeks" type="number" value="<%= kbArticleCompanyConfigurationDisplayContext.getExpirationDateNotificationDateWeeks() %>" />
+			</div>
+		</clay:sheet-section>
+
+		<clay:sheet-footer>
+			<aui:button primary="<%= true %>" type="submit" />
+		</clay:sheet-footer>
+	</clay:sheet>
+</aui:form>
