85d3d9cffc9d333116b810ae412a25d3ec108afe
Amanda Costa <amanda.costa@liferay.com>
2022-12-12 17:36:00 -0300

    LPS-162050 LPS-165712 portal-search-web: User - Extend BucketDisplayContext and delete overriding fields
    https://issues.liferay.com/browse/LPS-165712

--- modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/UserSearchFacetTermDisplayContext.java
+++ modules/apps/portal-search/portal-search-web/src/main/java/com/liferay/portal/search/web/internal/facet/display/context/UserSearchFacetTermDisplayContext.java
@@ -19,43 +19,16 @@ import java.io.Serializable;
 /**
  * @author Lino Alves
  */
-public class UserSearchFacetTermDisplayContext implements Serializable {
-
-	public int getFrequency() {
-		return _frequency;
-	}
+public class UserSearchFacetTermDisplayContext extends BucketDisplayContext {
 
 	public String getUserName() {
 		return _userName;
 	}
 
-	public boolean isFrequencyVisible() {
-		return _frequencyVisible;
-	}
-
-	public boolean isSelected() {
-		return _selected;
-	}
-
-	public void setFrequency(int frequency) {
-		_frequency = frequency;
-	}
-
-	public void setFrequencyVisible(boolean showFrequency) {
-		_frequencyVisible = showFrequency;
-	}
-
-	public void setSelected(boolean selected) {
-		_selected = selected;
-	}
-
 	public void setUserName(String userName) {
 		_userName = userName;
 	}
-
-	private int _frequency;
-	private boolean _frequencyVisible;
-	private boolean _selected;
+	
 	private String _userName;
 
 }
