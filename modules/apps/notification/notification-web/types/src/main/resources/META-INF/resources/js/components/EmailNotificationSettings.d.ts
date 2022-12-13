72574ba4aa2ce664c51d62e53ff2902239fd00df
Daniel Bonasser <danielbonasser@gmail.com>
2022-12-13 12:30:59 -0300

    LPS-170680 Extract Email Notification Settings to its own component

---
+++ modules/apps/notification/notification-web/types/src/main/resources/META-INF/resources/js/components/EmailNotificationSettings.d.ts
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+/// <reference types="react" />
+
+import {FormError} from '@liferay/object-js-components-web';
+import './EditNotificationTemplate.scss';
+import {NotificationTemplateError} from './EditNotificationTemplate';
+interface EmailNotificationSettingsProps {
+	errors: FormError<NotificationTemplate & NotificationTemplateError>;
+	selectedLocale: Locale;
+	setValues: (values: Partial<NotificationTemplate>) => void;
+	values: NotificationTemplate;
+}
+export declare function EmailNotificationSettings({
+	errors,
+	selectedLocale,
+	setValues,
+	values,
+}: EmailNotificationSettingsProps): JSX.Element;
+export {};
