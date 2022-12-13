24315d9883225e27552bc0e79c026bd5a5fd3bdd
Brian Chan <brian.chan@liferay.com>
2022-12-13 06:46:37 -0800

    Revert "LPS-168945 Simplify, reuse the ServiceTrackerCustomizer for the ServiceTrackerList to populate a local map"
    This reverts commit daffe09d047be98c53bc0eec768c5ff368fcfef5.

--- modules/apps/social/social-bookmarks-taglib/src/main/java/com/liferay/social/bookmarks/taglib/internal/util/SocialBookmarksRegistryImpl.java
+++ modules/apps/social/social-bookmarks-taglib/src/main/java/com/liferay/social/bookmarks/taglib/internal/util/SocialBookmarksRegistryImpl.java
@@ -17,6 +17,8 @@ package com.liferay.social.bookmarks.taglib.internal.util;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerList;
 import com.liferay.osgi.service.tracker.collections.list.ServiceTrackerListFactory;
 import com.liferay.osgi.service.tracker.collections.map.PropertyServiceReferenceComparator;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.social.bookmarks.SocialBookmark;
@@ -25,9 +27,7 @@ import com.liferay.social.bookmarks.SocialBookmarksRegistry;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -44,7 +44,7 @@ public class SocialBookmarksRegistryImpl implements SocialBookmarksRegistry {
 
 	@Override
 	public SocialBookmark getSocialBookmark(String type) {
-		SocialBookmark socialBookmark = _socialBookmarks.get(type);
+		SocialBookmark socialBookmark = _serviceTrackerMap.getService(type);
 
 		if (socialBookmark == null) {
 			if (_log.isWarnEnabled()) {
@@ -81,44 +81,36 @@ public class SocialBookmarksRegistryImpl implements SocialBookmarksRegistry {
 	@Activate
 	protected void activate(BundleContext bundleContext) {
 		_serviceTrackerList = ServiceTrackerListFactory.open(
-			bundleContext, SocialBookmark.class, "(social.bookmarks.type=*)",
-			new SocialBookmarkTypeServiceTrackerCustomizer(bundleContext),
+			bundleContext, SocialBookmark.class, null,
+			new SocialBookmarkTypeServiceTrackerCustomizer(),
 			new PropertyServiceReferenceComparator<>(
 				"social.bookmarks.priority"));
+
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, SocialBookmark.class, "social.bookmarks.type");
 	}
 
 	@Deactivate
 	protected void deactivate() {
 		_serviceTrackerList.close();
+		_serviceTrackerMap.close();
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		SocialBookmarksRegistryImpl.class);
 
 	private ServiceTrackerList<String> _serviceTrackerList;
-	private final Map<String, SocialBookmark> _socialBookmarks =
-		new ConcurrentHashMap<>();
+	private ServiceTrackerMap<String, SocialBookmark> _serviceTrackerMap;
 
-	private class SocialBookmarkTypeServiceTrackerCustomizer
+	private static class SocialBookmarkTypeServiceTrackerCustomizer
 		implements ServiceTrackerCustomizer<SocialBookmark, String> {
-
-		public SocialBookmarkTypeServiceTrackerCustomizer(
-			BundleContext bundleContext) {
-
-			_bundleContext = bundleContext;
-		}
 
 		@Override
 		public String addingService(
 			ServiceReference<SocialBookmark> serviceReference) {
 
-			String type = (String)serviceReference.getProperty(
+			return (String)serviceReference.getProperty(
 				"social.bookmarks.type");
-
-			_socialBookmarks.put(
-				type, _bundleContext.getService(serviceReference));
-
-			return type;
 		}
 
 		@Override
@@ -129,14 +121,7 @@ public class SocialBookmarksRegistryImpl implements SocialBookmarksRegistry {
 		@Override
 		public void removedService(
 			ServiceReference<SocialBookmark> serviceReference, String service) {
-
-			_socialBookmarks.remove(
-				(String)serviceReference.getProperty("social.bookmarks.type"));
-
-			_bundleContext.ungetService(serviceReference);
 		}
-
-		private final BundleContext _bundleContext;
 
 	}
 
