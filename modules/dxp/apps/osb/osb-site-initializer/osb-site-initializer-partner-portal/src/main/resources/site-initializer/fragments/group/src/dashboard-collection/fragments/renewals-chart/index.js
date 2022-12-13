f6adc1412a68b98be95ca0e8ea1b4ba5be25c80b
luisacmn <luisanovaes.arq@gmail.com>
2022-12-13 18:26:13 -0300

    LPS-165935 Removed space between siblings

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/dashboard-collection/fragments/renewals-chart/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/dashboard-collection/fragments/renewals-chart/index.js
@@ -118,8 +118,6 @@ export default function () {
 											{expirationInDays} days.
 										</span>
 
-										{" "}
-
 										<span className="ml-2">
 											{item.closeDate}
 										</span>
