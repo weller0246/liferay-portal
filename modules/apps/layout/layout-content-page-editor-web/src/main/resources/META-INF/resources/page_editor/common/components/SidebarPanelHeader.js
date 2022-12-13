1e35f0127caf81971f379dda2cbbd3abaf368469
Sandro Chinea <sandrochinea@gmail.com>
2022-12-13 10:10:54 +0000

    LPS-170968 Make close button optional

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/SidebarPanelHeader.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/SidebarPanelHeader.js
@@ -23,6 +23,7 @@ export default function SidebarPanelHeader({
 	children,
 	iconLeft = null,
 	iconRight = null,
+	showCloseButton = true,
 }) {
 	const dispatch = useDispatch();
 
@@ -40,20 +41,24 @@ export default function SidebarPanelHeader({
 
 			{iconRight}
 
-			<ClayButtonWithIcon
-				aria-label={Liferay.Language.get('close')}
-				displayType="unstyled"
-				onClick={() => {
-					dispatch(switchSidebarPanel({sidebarOpen: false}));
+			{showCloseButton && (
+				<ClayButtonWithIcon
+					aria-label={Liferay.Language.get('close')}
+					displayType="unstyled"
+					onClick={() => {
+						dispatch(switchSidebarPanel({sidebarOpen: false}));
 
-					document
-						.querySelector(`[data-panel-id="${sidebarPanelId}"]`)
-						?.focus();
-				}}
-				small
-				symbol="times"
-				title={Liferay.Language.get('close')}
-			/>
+						document
+							.querySelector(
+								`[data-panel-id="${sidebarPanelId}"]`
+							)
+							?.focus();
+					}}
+					small
+					symbol="times"
+					title={Liferay.Language.get('close')}
+				/>
+			)}
 		</header>
 	);
 }
