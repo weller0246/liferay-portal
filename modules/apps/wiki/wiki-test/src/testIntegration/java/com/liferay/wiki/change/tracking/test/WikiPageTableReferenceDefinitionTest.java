88248f18390b9dd03408e61b56c13e84385bcf43
Noor Najjar <noor.najjar@liferay.com>
2022-12-12 16:22:28 -0800

    LPS-165088 Create TableReferenceDefinitionTest classes for wiki

---
+++ modules/apps/wiki/wiki-test/src/testIntegration/java/com/liferay/wiki/change/tracking/test/WikiPageTableReferenceDefinitionTest.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.wiki.change.tracking.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.change.tracking.test.util.BaseTableReferenceDefinitionTestCase;
+import com.liferay.portal.kernel.model.change.tracking.CTModel;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+import com.liferay.portal.test.rule.PermissionCheckerMethodTestRule;
+import com.liferay.wiki.model.WikiNode;
+import com.liferay.wiki.test.util.WikiTestUtil;
+
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Noor Najjar
+ */
+@RunWith(Arquillian.class)
+public class WikiPageTableReferenceDefinitionTest
+	extends BaseTableReferenceDefinitionTestCase {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new AggregateTestRule(
+			new LiferayIntegrationTestRule(),
+			PermissionCheckerMethodTestRule.INSTANCE);
+
+	@Before
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+
+		_wikiNode = WikiTestUtil.addNode(group.getGroupId());
+	}
+
+	@Override
+	protected CTModel<?> addCTModel() throws Exception {
+		return WikiTestUtil.addPage(
+			group.getGroupId(), _wikiNode.getNodeId(), true);
+	}
+
+	private WikiNode _wikiNode;
+
+}
