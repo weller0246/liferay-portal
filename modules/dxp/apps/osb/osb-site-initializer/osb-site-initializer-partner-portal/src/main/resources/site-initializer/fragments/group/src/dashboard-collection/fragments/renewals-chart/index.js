a98578e0babb12528b1afad33008c19dafe2448b
luisacmn <luisanovaes.arq@gmail.com>
2022-12-13 18:27:36 -0300

    LPS-165935 SF

--- modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/dashboard-collection/fragments/renewals-chart/index.js
+++ modules/dxp/apps/osb/osb-site-initializer/osb-site-initializer-partner-portal/src/main/resources/site-initializer/fragments/group/src/dashboard-collection/fragments/renewals-chart/index.js
@@ -117,7 +117,6 @@ export default function () {
 										<span className="font-weight-semi-bold">
 											{expirationInDays} days.
 										</span>
-
 										<span className="ml-2">
 											{item.closeDate}
 										</span>
