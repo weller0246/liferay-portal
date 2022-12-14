08a273242eaa94c3b44bfd0254ff80ad1caab108
Victor Galan <victor.galan@liferay.com>
2022-12-14 17:17:27 +0100

    LPS-169679 Fix default title function

--- modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/CopyFragmentModal.js
+++ modules/apps/fragment/fragment-web/src/main/resources/META-INF/resources/js/CopyFragmentModal.js
@@ -327,5 +327,11 @@ function getDefaultFragmentSetName(fragmentCollections) {
 		)
 	);
 
-	return `${Liferay.Language.get('untitled-set')} ${untitledSets.length + 1}`;
+	if (untitledSets.length) {
+		return `${Liferay.Language.get('untitled-set')} ${
+			untitledSets.length + 1
+		}`;
+	}
+
+	return Liferay.Language.get('untitled-set');
 }
