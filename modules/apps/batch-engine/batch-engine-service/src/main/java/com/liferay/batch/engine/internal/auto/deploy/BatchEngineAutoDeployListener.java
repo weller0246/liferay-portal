f06fd7908dee74dd896d54fb0ee0a35705f84b00
Igor Beslic <igor.beslic@liferay.com>
2022-12-15 18:45:41 +0100

    LPS-170462 batch-engine - this is better

--- modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/auto/deploy/BatchEngineAutoDeployListener.java
+++ modules/apps/batch-engine/batch-engine-service/src/main/java/com/liferay/batch/engine/internal/auto/deploy/BatchEngineAutoDeployListener.java
@@ -34,7 +34,6 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
-import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.PropsKeys;
@@ -201,11 +200,10 @@ public class BatchEngineAutoDeployListener implements AutoDeployListener {
 			}
 
 			try {
-				User user = _userLocalService.getUserByScreenName(
-					batchEngineConfiguration.companyId,
-					PropsUtil.get(PropsKeys.DEFAULT_ADMIN_SCREEN_NAME));
-
-				batchEngineConfiguration.userId = user.getUserId();
+				batchEngineConfiguration.userId =
+					_userLocalService.getUserIdByScreenName(
+						batchEngineConfiguration.companyId,
+						PropsUtil.get(PropsKeys.DEFAULT_ADMIN_SCREEN_NAME));
 			}
 			catch (PortalException portalException) {
 				_log.error("Unable to get default user ID", portalException);
