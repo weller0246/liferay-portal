6e409747b81bca412b0bf5ac20e85aadfb834e1a
Ethan Liao <marineethan@gmail.com>
2022-12-16 11:12:39 -0800

    ISSITES-5840 add search functionality and styling to public sites nav

--- modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
+++ modules/apps/site-initializer/site-initializer-liferay-learn/src/main/resources/site-initializer/fragments/group/public-sites-navigation/fragments/public-sites-navigation/index.js
@@ -79,4 +79,115 @@ window.addEventListener('load', () => {
 		'list-open',
 		true
 	);
+
+	new navigation.default.DropdownProvider(
+		'.search-icon, .close-search',
+		'.search-wrapper',
+		'search-open',
+		true
+	);
 });
+
+const searchSuggestionsInput = fragmentElement.querySelector(".search-suggestions-input");
+const searchSuggestionsResult = fragmentElement.querySelector(".search-suggestions");
+const suggestedText = fragmentElement.querySelector('.suggested-text');
+const noResultsMessage = fragmentElement.querySelector('.no-results-message');
+
+
+searchSuggestionsInput.oninput = function() {
+	searchSuggestionsResult.innerHTML = "";
+
+	const query = searchSuggestionsInput.value;
+	navSearch(query);
+};
+
+function navSearch(query) {
+	const postDataUrl = window.location.origin + `/o/portal-search-rest/v1.0/suggestions?currentURL=${window.location.origin}&destinationFriendlyURL=/search&groupId=${Liferay.ThemeDisplay.getScopeGroupId()}&plid=${Liferay.ThemeDisplay.getPlid()}&scope=this-site&search=${query}`;
+
+	postData(
+		postDataUrl,
+		[{
+			attributes: {
+				fields: ['content_en_US'],
+				includeAssetSearchSummary: true,
+				includeAssetURL: true,
+				sxpBlueprintId: configuration.searchBlueprintId
+			},
+			contributorName: 'sxpBlueprint',
+			displayGroupName: 'Public Nav Search Recommendations',
+			size: '3'
+		}]
+	).then((data) => {
+
+		const searchSuggestions = fragmentElement.querySelector(".search-suggestions");
+		const searchSuggestionsSeeAllResults = fragmentElement.querySelector(".search-suggestions-see-all-results")
+
+		if (data.items[0] !== void(0)) {
+			const myjson = JSON.parse(JSON.stringify(data.items[0]));
+			if (myjson) {
+				for (const suggestion of myjson.suggestions) {
+					const newSuggestion = document.createElement("div");
+					newSuggestion.classList.add("search-suggestion-item");
+					newSuggestion.href = suggestion.attributes.assetURL;
+
+					const suggestionTitle = document.createElement("div");
+					const suggestionTitleText = document.createTextNode(suggestion.text);
+					suggestionTitle.classList.add("search-suggestion-item-title");
+					suggestionTitle.appendChild(suggestionTitleText);
+
+					const suggestionContent = document.createElement("div");
+					let suggestionContentTextValue = suggestion.attributes.fields.content_en_US;
+
+					if (!suggestionContentTextValue) {
+						suggestionContentTextValue = "No content preview";
+					}
+
+					const suggestionContentText = document.createTextNode(suggestionContentTextValue);
+					suggestionContent.classList.add("search-suggestion-item-content");
+					suggestionContent.appendChild(suggestionContentText);
+
+					newSuggestion.appendChild(suggestionTitle);
+					newSuggestion.appendChild(suggestionContent);
+
+					searchSuggestions.appendChild(newSuggestion);
+
+					searchSuggestionsSeeAllResults.classList.remove('d-none');
+					suggestedText.classList.remove('d-none');
+					noResultsMessage.classList.add('d-none');
+				}
+
+				// search highlighting
+
+				const searchSuggestionItemContents = document.querySelectorAll('.search-suggestion-item-content');
+
+				const highlightedTerm = "<b>" + query + "</b>";
+
+				if (searchSuggestionItemContents) {
+					for (const searchSuggestionItemContent of searchSuggestionItemContents) {
+						searchSuggestionItemContent.innerHTML = searchSuggestionItemContent.innerHTML.replaceAll(query, highlightedTerm);
+					}
+				}
+			}
+		} else {
+			searchSuggestionsSeeAllResults.classList.add('d-none');
+			suggestedText.classList.add('d-none');
+			noResultsMessage.classList.remove('d-none');
+		}
+	});
+}
+
+async function postData(url = '', data = {}) {
+	const response = await Liferay.Util.fetch(url, {
+		body: JSON.stringify(data),
+		credentials: 'include',
+		headers: {
+			'Accept': 'application/json',
+			'Accept-Language': Liferay.ThemeDisplay.getBCP47LanguageId(),
+			'Content-Type': 'application/json',
+			'x-csrf-token': Liferay.authToken
+		},
+		method: 'POST'
+	});
+
+	return response.json();
+}
