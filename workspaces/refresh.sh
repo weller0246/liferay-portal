9332e552759db9f41babbbc94b9588a7d3eec188
Gregory Amerson <gregory.amerson@liferay.com>
2022-12-19 09:43:29 -0600

    LPS-166479 update spritemap file

--- workspaces/refresh.sh
+++ workspaces/refresh.sh
@@ -34,6 +34,10 @@ function check_blade {
 	#jpm install -f https://repository-cdn.liferay.com/nexus/service/local/repositories/liferay-public-releases/content/com/liferay/blade/com.liferay.blade.cli/4.1.1/com.liferay.blade.cli-4.1.1.jar
 }
 
+function cleanup {
+	find . -name '*.bak' -exec rm {} +
+}
+
 function copy_template {
 	cp -R ../modules/apps/client-extension/client-extension-type-api/src/main/resources/com/liferay/client/extension/type/dependencies/templates/${1} "${2}"
 
@@ -82,6 +86,8 @@ function refresh_sample_default_workspace {
 	{ head -n 5 gradle.properties ; tail -n +6 gradle.properties | sort | perl -e "chomp if eof" -p; } >gradle.properties.tmp
 
 	mv gradle.properties.tmp gradle.properties
+
+	sed -i'.bak' 's/name: "com.liferay.gradle.plugins.workspace", version: ".*"/name: "com.liferay.gradle.plugins.workspace", version: "4.1.2"/' settings.gradle
 
 	touch modules/.touch
 	touch themes/.touch
@@ -175,6 +181,8 @@ function main {
 	refresh_sample_minimal_workspace
 
 	refresh_liferay_learn_workspace
+
+	cleanup
 }
 
 main "${@}"
