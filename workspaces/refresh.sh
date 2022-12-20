ce7d79bd4844351887c81ece3731f9ecc4ab9506
Gregory Amerson <gregory.amerson@liferay.com>
2022-12-20 14:48:58 -0600

    LPS-168898 fix export and test script

--- workspaces/refresh.sh
+++ workspaces/refresh.sh
@@ -159,8 +159,10 @@ class DadJoke extends React.Component {
 	}
 }
 
-export default Joke;
+export default DadJoke;
 EOF
+
+	sed -i'.bak' 's/react-scripts test/react-scripts test --passWithNoTests --watchAll=false/' fox-remote-app/package.json
 
 	mv fox-remote-app sample-minimal-workspace/client-extensions
 
