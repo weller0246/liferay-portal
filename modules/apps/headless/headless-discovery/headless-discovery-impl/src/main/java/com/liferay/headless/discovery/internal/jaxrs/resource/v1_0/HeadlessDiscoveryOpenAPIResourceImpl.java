4f2f650e22648031af61dc6c32d804ff887e1e24
Carlos Correa <carlos.correa@liferay.com>
2022-12-20 18:00:05 +0100

    LPS-170494 Generate a unique OpenAPI with all the APIs of the portal

--- modules/apps/headless/headless-discovery/headless-discovery-impl/src/main/java/com/liferay/headless/discovery/internal/jaxrs/resource/v1_0/HeadlessDiscoveryOpenAPIResourceImpl.java
+++ modules/apps/headless/headless-discovery/headless-discovery-impl/src/main/java/com/liferay/headless/discovery/internal/jaxrs/resource/v1_0/HeadlessDiscoveryOpenAPIResourceImpl.java
@@ -17,26 +17,49 @@ package com.liferay.headless.discovery.internal.jaxrs.resource.v1_0;
 import com.liferay.osgi.service.tracker.collections.EagerServiceTrackerCustomizer;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
 import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.vulcan.openapi.OpenAPIContext;
+import com.liferay.portal.vulcan.resource.OpenAPIResource;
+import com.liferay.portal.vulcan.util.UriInfoUtil;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+import java.net.URI;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.http.HttpServletRequest;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceReference;
@@ -63,6 +86,67 @@ import org.osgi.service.jaxrs.runtime.dto.RuntimeDTO;
 public class HeadlessDiscoveryOpenAPIResourceImpl {
 
 	@GET
+	@Path("/openapi.{type:json|yaml}")
+	@Produces({"application/json", "application/xml"})
+	public Response getGlobalOpenAPI(@PathParam("type") String type)
+		throws Exception {
+
+		Map<OpenAPIContext, Response> responses = new HashMap<>();
+
+		Map<String, List<String>> openAPIMap = _getOpenAPIMap(null);
+
+		for (Map.Entry<String, List<String>> entry : openAPIMap.entrySet()) {
+			String path = entry.getKey();
+
+			List<String> openAPIPaths = entry.getValue();
+
+			for (String openAPIPath : openAPIPaths) {
+				String version = _getVersion(path, openAPIPath);
+
+				Object resource = _getOpenAPIResource(path, version);
+
+				if (resource == null) {
+					continue;
+				}
+
+				Class<?> resourceClass = resource.getClass();
+
+				for (Field field : resourceClass.getDeclaredFields()) {
+					Class<?> fieldClass = field.getType();
+
+					if (fieldClass.isAssignableFrom(OpenAPIResource.class)) {
+						field.setAccessible(true);
+
+						field.set(resource, _openAPIResource);
+					}
+				}
+
+				Method method = resourceClass.getDeclaredMethod(
+					"getOpenAPI", HttpServletRequest.class, String.class,
+					UriInfo.class);
+
+				Response response = (Response)method.invoke(
+					resource, _httpServletRequest, "json",
+					_getUriInfo(path, version));
+
+				OpenAPIContext openAPIContext = new OpenAPIContext();
+
+				openAPIContext.setPath(path);
+				openAPIContext.setVersion(version);
+
+				responses.put(openAPIContext, response);
+			}
+		}
+
+		return _openAPIResource.mergeOpenAPIs(
+			StringUtil.removeLast(
+				UriInfoUtil.getBasePath(_uriInfo),
+				HeadlessDiscoveryOpenAPIApplication.BASE_PATH + CharPool.SLASH),
+			"OpenAPI specification of all Liferay REST APIs", responses,
+			"Global REST API - OpenAPI", type);
+	}
+
+	@GET
 	@Produces({"application/json", "application/xml"})
 	public Map<String, List<String>> openAPI(
 		@HeaderParam("Accept") String accept) {
@@ -72,6 +156,7 @@ public class HeadlessDiscoveryOpenAPIResourceImpl {
 
 	@Activate
 	protected void activate(BundleContext bundleContext) {
+		_bundleContext = bundleContext;
 		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
 			bundleContext, Application.class, "companyId",
 			new EagerServiceTrackerCustomizer<Application, Application>() {
@@ -195,6 +280,168 @@ public class HeadlessDiscoveryOpenAPIResourceImpl {
 		return pathsMap;
 	}
 
+	private Object _getOpenAPIResource(String path, String version) {
+		try {
+			String versionFilter = "(api.version=" + version + ")";
+
+			if (version == null) {
+				versionFilter = "(!(api.version=*))";
+			}
+
+			ServiceReference<?>[] serviceReferences =
+				_bundleContext.getServiceReferences(
+					(String)null,
+					StringBundler.concat(
+						"(&(openapi.resource=true)(openapi.resource.path=",
+						path, ")", versionFilter, ")"));
+
+			for (ServiceReference<?> serviceReference2 : serviceReferences) {
+				long companyId = GetterUtil.get(
+					serviceReference2.getProperty("companyId"),
+					_company.getCompanyId());
+
+				if (companyId == _company.getCompanyId()) {
+					return _bundleContext.getService(serviceReference2);
+				}
+			}
+		}
+		catch (Exception exception) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(exception);
+			}
+		}
+
+		return null;
+	}
+
+	private UriInfo _getUriInfo(String basePath, String version) {
+		return new UriInfo() {
+
+			@Override
+			public URI getAbsolutePath() {
+				return null;
+			}
+
+			@Override
+			public UriBuilder getAbsolutePathBuilder() {
+				return null;
+			}
+
+			@Override
+			public URI getBaseUri() {
+				URI uri = _uriInfo.getBaseUri();
+
+				return URI.create(
+					StringUtil.replace(
+						uri.toString(),
+						HeadlessDiscoveryOpenAPIApplication.BASE_PATH,
+						basePath));
+			}
+
+			@Override
+			public UriBuilder getBaseUriBuilder() {
+				return UriBuilder.fromUri(getBaseUri());
+			}
+
+			@Override
+			public List<Object> getMatchedResources() {
+				return null;
+			}
+
+			@Override
+			public List<String> getMatchedURIs() {
+				return null;
+			}
+
+			@Override
+			public List<String> getMatchedURIs(boolean decode) {
+				return null;
+			}
+
+			@Override
+			public String getPath() {
+				return version + "/openapi.json";
+			}
+
+			@Override
+			public String getPath(boolean decode) {
+				return getPath();
+			}
+
+			@Override
+			public MultivaluedMap<String, String> getPathParameters() {
+				return null;
+			}
+
+			@Override
+			public MultivaluedMap<String, String> getPathParameters(
+				boolean decode) {
+
+				return null;
+			}
+
+			@Override
+			public List<PathSegment> getPathSegments() {
+				return null;
+			}
+
+			@Override
+			public List<PathSegment> getPathSegments(boolean decode) {
+				return null;
+			}
+
+			@Override
+			public MultivaluedMap<String, String> getQueryParameters() {
+				return null;
+			}
+
+			@Override
+			public MultivaluedMap<String, String> getQueryParameters(
+				boolean decode) {
+
+				return null;
+			}
+
+			@Override
+			public URI getRequestUri() {
+				return null;
+			}
+
+			@Override
+			public UriBuilder getRequestUriBuilder() {
+				return null;
+			}
+
+			@Override
+			public URI relativize(URI uri) {
+				return null;
+			}
+
+			@Override
+			public URI resolve(URI uri) {
+				return null;
+			}
+
+		};
+	}
+
+	private String _getVersion(String basePath, String openAPIPath) {
+		String subpath = openAPIPath.substring(
+			openAPIPath.indexOf(basePath) + basePath.length() + 1);
+
+		String version = StringUtil.extractFirst(subpath, StringPool.SLASH);
+
+		if (version != null) {
+			Matcher versionMatcher = _versionPattern.matcher(version);
+
+			if (versionMatcher.matches()) {
+				return version;
+			}
+		}
+
+		return null;
+	}
+
 	private void _populateCompanyIds(
 		ServiceReference<Application> serviceReference) {
 
@@ -210,6 +457,17 @@ public class HeadlessDiscoveryOpenAPIResourceImpl {
 		}
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		HeadlessDiscoveryOpenAPIResourceImpl.class);
+
+	private static final Pattern _versionPattern = Pattern.compile(
+		"v[0-9]+\\.[0-9]+");
+
+	private BundleContext _bundleContext;
+
+	@Context
+	private Company _company;
+
 	private final Map<String, List<String>> _companyIds = new HashMap<>();
 
 	@Context
@@ -219,8 +477,14 @@ public class HeadlessDiscoveryOpenAPIResourceImpl {
 	private JaxrsServiceRuntime _jaxrsServiceRuntime;
 
 	@Reference
+	private OpenAPIResource _openAPIResource;
+
+	@Reference
 	private Portal _portal;
 
 	private ServiceTrackerMap<String, Application> _serviceTrackerMap;
+
+	@Context
+	private UriInfo _uriInfo;
 
 }
