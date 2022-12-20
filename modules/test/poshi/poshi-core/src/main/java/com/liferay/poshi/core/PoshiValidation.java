2f3f91281cbdf5f6d74c96478ec2cfa03cd60e18
CalumR23 <searagan@gmail.com>
2022-12-19 17:30:33 -0800

    POSHI-449 Add antCommand to deprecation list

--- modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/PoshiValidation.java
+++ modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/PoshiValidation.java
@@ -2019,6 +2019,7 @@ public class PoshiValidation {
 	private static final Map<String, String> _deprecatedMethodNames =
 		new Hashtable<String, String>() {
 			{
+				put("antCommand", "\"AntCommands.runCommand\"");
 				put("assertAlert", "\"selenium.assertAlertText\"");
 				put("copyText", "\"selenium.getText\" (stored as a variable)");
 				put(
