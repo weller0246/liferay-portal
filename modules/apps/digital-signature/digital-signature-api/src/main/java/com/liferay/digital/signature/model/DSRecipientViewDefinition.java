d555dbd26a3cf647451afe21a22672e2e7037955
José Abelenda <jose.abelenda@liferay.com>
2022-12-23 01:18:08 -0300

    LPS-171656 Create model DSRecipientViewDefinition

---
+++ modules/apps/digital-signature/digital-signature-api/src/main/java/com/liferay/digital/signature/model/DSRecipientViewDefinition.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.model;
+
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.json.JSONUtil;
+
+/**
+ * @author José Abelenda
+ */
+public class DSRecipientViewDefinition {
+
+	public String getAuthenticationMethod() {
+		return authenticationMethod;
+	}
+
+	public String getClientUserId() {
+		return clientUserId;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getReturnUrl() {
+		return returnUrl;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setAuthenticationMethod(String authenticationMethod) {
+		this.authenticationMethod = authenticationMethod;
+	}
+
+	public void setClientUserId(String clientUserId) {
+		this.clientUserId = clientUserId;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setReturnUrl(String returnUrl) {
+		this.returnUrl = returnUrl;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public JSONObject toJSONObject() {
+		return JSONUtil.put(
+			"authenticationMethod", getAuthenticationMethod()
+		).put(
+			"clientUserId", getClientUserId()
+		).put(
+			"email", getEmail()
+		).put(
+			"returnUrl", getReturnUrl()
+		).put(
+			"userName", getUserName()
+		);
+	}
+
+	@Override
+	public String toString() {
+		return toJSONObject().toString();
+	}
+
+	protected String authenticationMethod;
+	protected String clientUserId;
+	protected String email;
+	protected String returnUrl;
+	protected String userName;
+
+}
