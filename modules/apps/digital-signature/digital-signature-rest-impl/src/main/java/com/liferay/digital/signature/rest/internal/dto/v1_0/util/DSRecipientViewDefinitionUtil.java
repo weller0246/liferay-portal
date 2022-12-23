9e55b2f066560b5c7700d8cfab4d690e7cfb183d
José Abelenda <jose.abelenda@liferay.com>
2022-12-23 02:40:40 -0300

    LPS-171656 DTO - create DSRecipientViewDefinitionUtil

---
+++ modules/apps/digital-signature/digital-signature-rest-impl/src/main/java/com/liferay/digital/signature/rest/internal/dto/v1_0/util/DSRecipientViewDefinitionUtil.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.digital.signature.rest.internal.dto.v1_0.util;
+
+import com.liferay.digital.signature.rest.dto.v1_0.DSEnvelopeSignUrl;
+import com.liferay.digital.signature.rest.dto.v1_0.DSRecipientViewDefinition;
+import com.liferay.portal.kernel.json.JSONObject;
+
+/**
+ * @author José Abelenda
+ */
+public class DSRecipientViewDefinitionUtil {
+
+	public static DSEnvelopeSignUrl toDSEnvelopeSignUrl(JSONObject jsonObject) {
+		return new DSEnvelopeSignUrl() {
+			{
+				url = jsonObject.getString("url");
+			}
+		};
+	}
+
+	public static DSRecipientViewDefinition toDSRecipientViewDefinition(
+		com.liferay.digital.signature.model.DSRecipientViewDefinition
+			dsRecipientViewDefinition) {
+
+		return new DSRecipientViewDefinition() {
+			{
+				authenticationMethod =
+					dsRecipientViewDefinition.getAuthenticationMethod();
+				clientUserId = dsRecipientViewDefinition.getClientUserId();
+				email = dsRecipientViewDefinition.getEmail();
+				returnUrl = dsRecipientViewDefinition.getReturnUrl();
+				userName = dsRecipientViewDefinition.getUserName();
+			}
+		};
+	}
+
+	public static com.liferay.digital.signature.model.DSRecipientViewDefinition
+		toDSRecipientViewDefinition(
+			DSRecipientViewDefinition dsRecipientViewDefinition) {
+
+		return new com.liferay.digital.signature.model.
+			DSRecipientViewDefinition() {
+
+			{
+				authenticationMethod =
+					dsRecipientViewDefinition.getAuthenticationMethod();
+				clientUserId = dsRecipientViewDefinition.getClientUserId();
+				email = dsRecipientViewDefinition.getEmail();
+				returnUrl = dsRecipientViewDefinition.getReturnUrl();
+				userName = dsRecipientViewDefinition.getUserName();
+			}
+		};
+	}
+
+}
