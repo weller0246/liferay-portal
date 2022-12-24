0f1e0625a888aee74952b78747a08a7bd9c80e67
Brian Chan <brian.chan@liferay.com>
2022-12-24 10:16:35 -0800

    LPS-169847 Other FF is no longer used

--- workspaces/refresh.sh
+++ workspaces/refresh.sh
@@ -69,7 +69,7 @@ function refresh_sample_default_workspace {
 
 	echo -en "\n**/dist\n**/node_modules_cache\n.DS_Store" >> .gitignore
 
-	echo -en "\n\nfeature.flag.LPS-153457=true\nfeature.flag.LPS-166479=true" >> configs/local/portal-ext.properties
+	echo -en "\n\nfeature.flag.LPS-166479=true" >> configs/local/portal-ext.properties
 
 	#echo -en "\nliferay.workspace.docker.image.liferay=liferay/dxp:7.4.13-u54-d5.0.5-20221208173455" >> gradle.properties
 	echo -en "\nliferay.workspace.node.package.manager=yarn" >> gradle.properties
