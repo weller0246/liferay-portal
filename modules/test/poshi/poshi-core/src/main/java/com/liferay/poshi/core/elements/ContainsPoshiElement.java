brandonyee333/test

POSHI-245 Allow for non quoted parsing in contains element

modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/ContainsPoshiElement.java
@@ -56,15 +56,39 @@ public class ContainsPoshiElement extends PoshiElement {
 
 		matcher.find();
 
-		addAttribute("string", matcher.group(1));
-		addAttribute("substring", matcher.group(2));
+		String string = matcher.group(1);
+
+		if (isQuotedContent(string)) {
+			string = getDoubleQuotedContent(string);
+		}
+
+		addAttribute("string", string);
+
+		String substring = matcher.group(2);
+
+		if (isQuotedContent(substring)) {
+			substring = getDoubleQuotedContent(string);
+		}
+
+		addAttribute("substring", substring);
 	}
 
 	@Override
 	public String toPoshiScript() {
+		String stringValue = attributeValue("string");
+
+		if (isQuotedContent(stringValue)) {
+			stringValue = "\"" + stringValue + "\"";
+		}
+
+		String substringValue = attributeValue("substring");
+
+		if (isQuotedContent(substringValue)) {
+			substringValue = "\"" + substringValue + "\"";
+		}
+
 		return StringUtil.combine(
-			_ELEMENT_NAME, "(\"" + attributeValue("string") + "\", \"",
-			attributeValue("substring"), "\")");
+			_ELEMENT_NAME, "(" + stringValue + ", ", substringValue, ")");
 	}
 
 	protected ContainsPoshiElement() {
@@ -102,6 +126,6 @@ public class ContainsPoshiElement extends PoshiElement {
 	private static final String _ELEMENT_NAME = "contains";
 
 	private static final Pattern _conditionPattern = Pattern.compile(
-		"^" + _ELEMENT_NAME + "\\(\"(.*)\"[\\s]*,[\\s]*\"(.*)\"\\)$");
+		"^" + _ELEMENT_NAME + "\\((.*)[\\s]*,[\\s]*(.*)\\)$");
 
 }

weller0246/liferay-portal

modules/test/poshi/poshi-core/src/main/java/com/liferay/poshi/core/elements/ContainsPoshiElement.java
@@ -56,15 +56,39 @@ public class ContainsPoshiElement extends PoshiElement {
 
 		matcher.find();
 
-		addAttribute("string", matcher.group(1));
-		addAttribute("substring", matcher.group(2));
+		String string = matcher.group(1);
+
+		if (isQuotedContent(string)) {
+			string = getDoubleQuotedContent(string);
+		}
+
+		addAttribute("string", string);
+
+		String substring = matcher.group(2);
+
+		if (isQuotedContent(substring)) {
+			substring = getDoubleQuotedContent(substring);
+		}
+
+		addAttribute("substring", substring);
 	}
 
 	@Override
 	public String toPoshiScript() {
+		String stringValue = attributeValue("string");
+
+		if (isQuotedContent(stringValue)) {
+			stringValue = "\"" + stringValue + "\"";
+		}
+
+		String substringValue = attributeValue("substring");
+
+		if (isQuotedContent(substringValue)) {
+			substringValue = "\"" + substringValue + "\"";
+		}
+
 		return StringUtil.combine(
-			_ELEMENT_NAME, "(\"" + attributeValue("string") + "\", \"",
-			attributeValue("substring"), "\")");
+			_ELEMENT_NAME, "(" + stringValue + ", ", substringValue, ")");
 	}
 
 	protected ContainsPoshiElement() {
@@ -102,6 +126,7 @@ public class ContainsPoshiElement extends PoshiElement {
 	private static final String _ELEMENT_NAME = "contains";
 
 	private static final Pattern _conditionPattern = Pattern.compile(
-		"^" + _ELEMENT_NAME + "\\(\"(.*)\"[\\s]*,[\\s]*\"(.*)\"\\)$");
+		"^" + _ELEMENT_NAME + "\\((.*)[\\s]*,[\\s]*(\\d+|(?:\\$\\{|\\\")" +
+			"[\\s\\S]*(?:\\}|\"))\\)$");
 
 }