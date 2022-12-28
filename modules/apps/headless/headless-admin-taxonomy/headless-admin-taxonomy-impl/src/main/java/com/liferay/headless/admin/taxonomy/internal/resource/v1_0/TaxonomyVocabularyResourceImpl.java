7183f8b730fa2c05864cfd17048424330d767532
Janis Zhang <janis.zhang@liferay.com>
2022-12-28 11:52:09 +0800

    LPS-171834 Remove Optional and Stream usages in TaxonomyVocabularyResourceImpl

--- modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/TaxonomyVocabularyResourceImpl.java
+++ modules/apps/headless/headless-admin-taxonomy/headless-admin-taxonomy-impl/src/main/java/com/liferay/headless/admin/taxonomy/internal/resource/v1_0/TaxonomyVocabularyResourceImpl.java
@@ -15,6 +15,7 @@
 package com.liferay.headless.admin.taxonomy.internal.resource.v1_0;
 
 import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetCategory;
 import com.liferay.asset.kernel.model.AssetCategoryConstants;
 import com.liferay.asset.kernel.model.AssetRendererFactory;
 import com.liferay.asset.kernel.model.AssetVocabulary;
@@ -67,8 +68,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.Stream;
 
 import javax.ws.rs.BadRequestException;
 import javax.ws.rs.InternalServerErrorException;
@@ -540,20 +539,21 @@ public class TaxonomyVocabularyResourceImpl
 					contextCompany.getCompanyId()),
 				AssetRendererFactory::isCategorizable);
 
-		Stream<AssetRendererFactory<?>> stream =
-			categorizableAssetRenderFactories.stream();
+		String className = null;
 
-		Optional<AssetRendererFactory<?>> assetRendererFactoryOptional =
-			stream.filter(
-				assetRendererFactory -> assetTypeType.equals(
-					_getModelResource(assetRendererFactory))
-			).findFirst();
+		for (AssetRendererFactory<?> assetRendererFactory :
+				categorizableAssetRenderFactories) {
 
-		String className = assetRendererFactoryOptional.map(
-			AssetRendererFactory::getClassName
-		).orElse(
-			_assetTypeTypeToClassNames.get(assetTypeType)
-		);
+			if (assetTypeType.equals(_getModelResource(assetRendererFactory))) {
+				className = assetRendererFactory.getClassName();
+
+				break;
+			}
+		}
+
+		if (className == null) {
+			className = _assetTypeTypeToClassNames.get(assetTypeType);
+		}
 
 		if (className == null) {
 			throw new BadRequestException(
@@ -723,13 +723,16 @@ public class TaxonomyVocabularyResourceImpl
 				name_i18n = LocalizedMapUtil.getI18nMap(
 					contextAcceptLanguage.isAcceptAllLanguages(),
 					assetVocabulary.getTitleMap());
-				numberOfTaxonomyCategories = Optional.ofNullable(
-					assetVocabulary.getCategories()
-				).map(
-					List::size
-				).orElse(
-					0
-				);
+
+				numberOfTaxonomyCategories = 0;
+
+				List<AssetCategory> assetCategories =
+					assetVocabulary.getCategories();
+
+				if (assetCategories != null) {
+					numberOfTaxonomyCategories = assetCategories.size();
+				}
+
 				siteId = GroupUtil.getSiteId(group);
 			}
 		};
