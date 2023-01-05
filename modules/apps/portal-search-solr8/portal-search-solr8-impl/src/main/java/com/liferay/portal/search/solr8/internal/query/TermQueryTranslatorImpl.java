5ab0bbc056ad683e92965cef1edb027b32323e6a
Bryan Engler <bryan.engler@liferay.com>
2023-01-04 16:28:22 -0800

    LPS-166987 LPS-167287 portal-search-solr8-impl: Apply same logic as TermFilterTranslatorImpl so that queries can be parsed
    https://issues.liferay.com/browse/LPS-167287

--- modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/query/TermQueryTranslatorImpl.java
+++ modules/apps/portal-search-solr8/portal-search-solr8-impl/src/main/java/com/liferay/portal/search/solr8/internal/query/TermQueryTranslatorImpl.java
@@ -14,12 +14,16 @@
 
 package com.liferay.portal.search.solr8.internal.query;
 
+import com.liferay.petra.string.CharPool;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.search.QueryTerm;
 import com.liferay.portal.kernel.search.TermQuery;
+import com.liferay.portal.kernel.util.StringUtil;
 
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.BoostQuery;
 import org.apache.lucene.search.Query;
+import org.apache.solr.client.solrj.util.ClientUtils;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -34,14 +38,27 @@ public class TermQueryTranslatorImpl implements TermQueryTranslator {
 	public Query translate(TermQuery termQuery) {
 		QueryTerm queryTerm = termQuery.getQueryTerm();
 
+		String value = queryTerm.getValue();
+
+		if (value.isEmpty()) {
+			value = StringPool.DOUBLE_APOSTROPHE;
+		}
+
 		Query query = new org.apache.lucene.search.TermQuery(
-			new Term(queryTerm.getField(), queryTerm.getValue()));
+			new Term(
+				_escape(queryTerm.getField()),
+				ClientUtils.escapeQueryChars(value)));
 
 		if (!termQuery.isDefaultBoost()) {
 			return new BoostQuery(query, termQuery.getBoost());
 		}
 
 		return query;
+	}
+
+	private String _escape(String value) {
+		return StringUtil.replace(
+			value, CharPool.SPACE, StringPool.BACK_SLASH + StringPool.SPACE);
 	}
 
 }
