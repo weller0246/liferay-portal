49ead3de83e21e0017ff96c53dc729d225deb29a
Jiaxu Wei <jiaxu.wei@liferay.com>
2023-01-09 11:22:23 +0800

    LPS-172335 HttpBindingsRegistrator: Register classes SamlBinding in a simple way

--- modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/binding/HttpBindingsRegistrator.java
+++ modules/dxp/apps/saml/saml-opensaml-integration/src/main/java/com/liferay/saml/opensaml/integration/internal/binding/HttpBindingsRegistrator.java
@@ -14,20 +14,16 @@
 
 package com.liferay.saml.opensaml.integration.internal.binding;
 
+import com.liferay.portal.kernel.util.HashMapBuilder;
+import com.liferay.saml.opensaml.integration.internal.servlet.profile.BaseProfile;
 import com.liferay.saml.opensaml.integration.internal.velocity.VelocityEngineFactory;
-
-import java.util.Hashtable;
-import java.util.Map;
 
 import net.shibboleth.utilities.java.support.xml.ParserPool;
 
 import org.apache.http.client.HttpClient;
 
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.ServiceRegistration;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Deactivate;
 import org.osgi.service.component.annotations.Reference;
 
 /**
@@ -37,39 +33,24 @@ import org.osgi.service.component.annotations.Reference;
 public class HttpBindingsRegistrator {
 
 	@Activate
-	protected void activate(
-		BundleContext bundleContext, Map<String, Object> propertiesMap) {
-
-		Hashtable<String, Object> properties = new Hashtable<>(propertiesMap);
-
-		HttpPostBinding httpPostBinding = new HttpPostBinding(
+	protected void activate() {
+		SamlBinding httpPostBinding = new HttpPostBinding(
 			_parserPool, _velocityEngineFactory.getVelocityEngine());
 
-		HttpRedirectBinding httpRedirectBinding = new HttpRedirectBinding(
-			_parserPool);
+		SamlBinding httpRedirectBinding = new HttpRedirectBinding(_parserPool);
 
-		HttpSoap11Binding httpSoap11Binding = new HttpSoap11Binding(
+		SamlBinding httpSoap11Binding = new HttpSoap11Binding(
 			_parserPool, _httpClient);
 
-		_samlHttpPostBindingServiceRegistration = bundleContext.registerService(
-			SamlBinding.class, httpPostBinding, properties);
-
-		_samlHttpRedirectBindingServiceRegistration =
-			bundleContext.registerService(
-				SamlBinding.class, httpRedirectBinding, properties);
-
-		_samlHttpSoap11BindingServiceRegistration =
-			bundleContext.registerService(
-				SamlBinding.class, httpSoap11Binding, properties);
-	}
-
-	@Deactivate
-	protected void deactivate() {
-		_samlHttpPostBindingServiceRegistration.unregister();
-
-		_samlHttpRedirectBindingServiceRegistration.unregister();
-
-		_samlHttpSoap11BindingServiceRegistration.unregister();
+		BaseProfile.setSamlBindings(
+			HashMapBuilder.put(
+				httpPostBinding.getCommunicationProfileId(), httpPostBinding
+			).put(
+				httpRedirectBinding.getCommunicationProfileId(),
+				httpRedirectBinding
+			).put(
+				httpSoap11Binding.getCommunicationProfileId(), httpSoap11Binding
+			).build());
 	}
 
 	@Reference
@@ -77,13 +58,6 @@ public class HttpBindingsRegistrator {
 
 	@Reference
 	private ParserPool _parserPool;
-
-	private ServiceRegistration<SamlBinding>
-		_samlHttpPostBindingServiceRegistration;
-	private ServiceRegistration<SamlBinding>
-		_samlHttpRedirectBindingServiceRegistration;
-	private ServiceRegistration<SamlBinding>
-		_samlHttpSoap11BindingServiceRegistration;
 
 	@Reference
 	private VelocityEngineFactory _velocityEngineFactory;
