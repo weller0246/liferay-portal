8622b60c559bbab12d6e69cfe3148bbe20b09278
Jorge Avalos <jorge.avalos@liferay.com>
2023-01-10 11:12:20 -0800

    LPS-170765 Use toString before toLowercase to ensure that it is a string

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/LengthField.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/common/components/LengthField.js
@@ -50,7 +50,7 @@ const getInitialValue = (value) => {
 		return {unit: UNITS[0], value: ''};
 	}
 
-	const match = value.toLowerCase().match(REGEX);
+	const match = value.toString().toLowerCase().match(REGEX);
 
 	if (match) {
 		const [, number, unit] = match;
@@ -236,7 +236,7 @@ export function LengthInput({
 			return;
 		}
 
-		const [, , unit] = value.toLowerCase().match(REGEX) || [];
+		const [, , unit] = value.toString().toLowerCase().match(REGEX) || [];
 
 		setNextUnit(unit || CUSTOM);
 	}, [value]);
