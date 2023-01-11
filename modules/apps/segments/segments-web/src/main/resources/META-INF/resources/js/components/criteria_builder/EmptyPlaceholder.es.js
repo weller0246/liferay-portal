9a0b90d0852e67bbc35873da6c5338e81023465f
Robert-Stefan Tanasie <stefan.tanasie@liferay.com>
2023-01-11 14:14:04 +0100

    LPS-168954 Use ClayEmptyState component instead of custom component

--- modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_builder/EmptyPlaceholder.es.js
+++ modules/apps/segments/segments-web/src/main/resources/META-INF/resources/js/components/criteria_builder/EmptyPlaceholder.es.js
@@ -12,24 +12,21 @@
  * details.
  */
 
+import ClayEmptyState from '@clayui/empty-state';
 import React from 'react';
 
 import {sub} from '../../utils/utils.es';
 
 export default function EmptyPlaceholder() {
 	return (
-		<div className="empty-contributors mb-0 p-5 rounded taglib-empty-result-message">
-			<div className="taglib-empty-result-message-header" />
-
-			<div className="sheet-text text-center">
-				<h1 className="mb-3 mt-4">
-					{sub(Liferay.Language.get('no-x-yet'), [
-						Liferay.Language.get('conditions'),
-					])}
-				</h1>
-
-				<p>{Liferay.Language.get('empty-conditions-message')}</p>
-			</div>
+		<div className="empty-contributors mb-0 p-4 rounded">
+			<ClayEmptyState
+				description={Liferay.Language.get('empty-conditions-message')}
+				imgSrc={`${themeDisplay.getPathThemeImages()}/states/empty_state.gif`}
+				title={sub(Liferay.Language.get('no-x-yet'), [
+					Liferay.Language.get('conditions'),
+				])}
+			></ClayEmptyState>
 		</div>
 	);
 }
