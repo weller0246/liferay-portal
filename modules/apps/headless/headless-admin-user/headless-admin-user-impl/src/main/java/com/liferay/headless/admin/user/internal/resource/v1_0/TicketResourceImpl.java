dc21a5cc76e9c22b39eef8704db961e01123681f
pei-jung <pei-jung.lan@liferay.com>
2023-01-11 12:19:37 -0800

    LPS-171650 (headless-admin-user-impl) - implement new endpoints

--- modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/resource/v1_0/TicketResourceImpl.java
+++ modules/apps/headless/headless-admin-user/headless-admin-user-impl/src/main/java/com/liferay/headless/admin/user/internal/resource/v1_0/TicketResourceImpl.java
@@ -14,9 +14,17 @@
 
 package com.liferay.headless.admin.user.internal.resource.v1_0;
 
+import com.liferay.headless.admin.user.dto.v1_0.Ticket;
 import com.liferay.headless.admin.user.resource.v1_0.TicketResource;
+import com.liferay.portal.kernel.model.TicketConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.TicketLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+
+import java.util.List;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -27,4 +35,52 @@ import org.osgi.service.component.annotations.ServiceScope;
 	scope = ServiceScope.PROTOTYPE, service = TicketResource.class
 )
 public class TicketResourceImpl extends BaseTicketResourceImpl {
+
+	@Override
+	public Ticket getUserAccountEmailVerificationTicket(Long userAccountId)
+		throws Exception {
+
+		return _getTicket(TicketConstants.TYPE_EMAIL_ADDRESS, userAccountId);
+	}
+
+	@Override
+	public Ticket getUserAccountPasswordResetTicket(Long userAccountId)
+		throws Exception {
+
+		return _getTicket(TicketConstants.TYPE_PASSWORD, userAccountId);
+	}
+
+	private Ticket _getTicket(int type, Long userAccountId) throws Exception {
+		_userLocalService.getUser(userAccountId);
+
+		List<com.liferay.portal.kernel.model.Ticket> tickets =
+			_ticketLocalService.getTickets(
+				User.class.getName(), userAccountId, type);
+
+		if (tickets.isEmpty()) {
+			return null;
+		}
+
+		return _toTicket(tickets.get(0));
+	}
+
+	private Ticket _toTicket(com.liferay.portal.kernel.model.Ticket ticket)
+		throws Exception {
+
+		return new Ticket() {
+			{
+				expirationDate = ticket.getExpirationDate();
+				extraInfo = ticket.getExtraInfo();
+				id = ticket.getTicketId();
+				key = ticket.getKey();
+			}
+		};
+	}
+
+	@Reference
+	private TicketLocalService _ticketLocalService;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
 }
