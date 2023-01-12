627aec81b8c9684cbd1033e0bdbc22cbb9c2ebc1
Mikel Lorza <mikel.lorza@liferay.com>
2023-01-12 12:58:04 +0100

    LPS-171060 Add saveVariantSegmentsExperienceURL attribute to save specific variant segments experience

--- modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
+++ modules/apps/layout/layout-content-page-editor-web/src/main/java/com/liferay/layout/content/page/editor/web/internal/display/context/ContentPageEditorDisplayContext.java
@@ -590,14 +590,7 @@ public class ContentPageEditorDisplayContext {
 			).put(
 				"portletNamespace", getPortletNamespace()
 			).put(
-				"publishURL",
-				() -> {
-					if (_isSegmentsExperimentVariant()) {
-						return getSaveVariantSegmentsExperienceURL();
-					}
-
-					return getPublishURL();
-				}
+				"publishURL", getPublishURL()
 			).put(
 				"redirectURL", _getRedirect()
 			).put(
@@ -609,6 +602,9 @@ public class ContentPageEditorDisplayContext {
 				getFragmentEntryActionURL(
 					"/layout_content_page_editor" +
 						"/restore_collection_display_config")
+			).put(
+				"saveVariantSegmentsExperienceURL",
+				getSaveVariantSegmentsExperienceURL()
 			).put(
 				"searchContainerPageMaxDelta",
 				PropsValues.SEARCH_CONTAINER_PAGE_MAX_DELTA
