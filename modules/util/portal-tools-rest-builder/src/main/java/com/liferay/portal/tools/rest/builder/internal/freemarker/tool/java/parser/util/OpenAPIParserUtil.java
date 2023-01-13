ba4ac3b7e1d820653dfab43592ce3d0f478290e3
Jiefeng Wu <jeffwu0724@gmail.com>
2023-01-13 09:19:02 -0800

    LPS-172794 remove the usage of optional in OpenAPIParserUtil

--- modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/java/parser/util/OpenAPIParserUtil.java
+++ modules/util/portal-tools-rest-builder/src/main/java/com/liferay/portal/tools/rest/builder/internal/freemarker/tool/java/parser/util/OpenAPIParserUtil.java
@@ -51,7 +51,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Queue;
 import java.util.Set;
 import java.util.TreeSet;
@@ -157,13 +156,13 @@ public class OpenAPIParserUtil {
 
 		Map<String, PathItem> pathItems = openAPIYAML.getPathItems();
 
-		Map<String, Schema> schemas = Optional.ofNullable(
-			openAPIYAML.getComponents()
-		).map(
-			Components::getSchemas
-		).orElse(
-			new HashMap<>()
-		);
+		Components components = openAPIYAML.getComponents();
+
+		Map<String, Schema> schemas = new HashMap<>();
+
+		if ((components != null) && (components.getSchemas() != null)) {
+			schemas = components.getSchemas();
+		}
 
 		if (pathItems != null) {
 			for (PathItem pathItem : pathItems.values()) {
