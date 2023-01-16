a1848e5cecbd61e949fb54b4aff505a3d0b0bf67
Joao Victor <joao.victor@liferay.com>
2023-01-16 13:04:00 -0300

    LPS-172916 Remove immediate from gradle-plugins module

--- modules/sdk/gradle-plugins/src/gradleTest/deployFast/src/main/java/com/liferay/example/internal/portlet/ExamplePortlet.java
+++ modules/sdk/gradle-plugins/src/gradleTest/deployFast/src/main/java/com/liferay/example/internal/portlet/ExamplePortlet.java
@@ -24,7 +24,6 @@ import org.osgi.service.component.annotations.Component;
  * @author Greg Amerson
  */
 @Component(
-	immediate = true,
 	property = {
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.header-portlet-css=/css/main.css",
