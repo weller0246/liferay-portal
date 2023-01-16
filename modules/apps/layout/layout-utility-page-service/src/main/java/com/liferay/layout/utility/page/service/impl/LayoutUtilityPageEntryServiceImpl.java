905e3059f8a393315b1bf35dbbbf9a48389ba53e
Jürgen Kappler <jurgen.kappler@liferay.com>
2023-01-16 16:13:02 +0100

    LPS-172865 We really need to check if we have the permissions to delete an entry. Also, if it's marked as default, don't let it be deleted unless we have the specific permission to do so

--- modules/apps/layout/layout-utility-page-service/src/main/java/com/liferay/layout/utility/page/service/impl/LayoutUtilityPageEntryServiceImpl.java
+++ modules/apps/layout/layout-utility-page-service/src/main/java/com/liferay/layout/utility/page/service/impl/LayoutUtilityPageEntryServiceImpl.java
@@ -79,6 +79,21 @@ public class LayoutUtilityPageEntryServiceImpl
 			long layoutUtilityPageEntryId)
 		throws PortalException {
 
+		LayoutUtilityPageEntry layoutUtilityPageEntry =
+			layoutUtilityPageEntryLocalService.getLayoutUtilityPageEntry(
+				layoutUtilityPageEntryId);
+
+		_layoutUtilityPageEntryModelResourcePermission.check(
+			getPermissionChecker(), layoutUtilityPageEntryId,
+			ActionKeys.DELETE);
+
+		if (layoutUtilityPageEntry.isDefaultLayoutUtilityPageEntry()) {
+			_groupPermission.check(
+				getPermissionChecker(), layoutUtilityPageEntry.getGroupId(),
+				LayoutUtilityPageActionKeys.
+					ASSIGN_DEFAULT_LAYOUT_UTILITY_PAGE_ENTRY);
+		}
+
 		return layoutUtilityPageEntryLocalService.deleteLayoutUtilityPageEntry(
 			layoutUtilityPageEntryId);
 	}
