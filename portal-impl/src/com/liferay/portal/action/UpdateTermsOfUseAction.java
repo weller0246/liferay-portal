0593a802c3d5bf2004395c9769ed567330c444ba
Jonathan McCann <jonathan.mccann@liferay.com>
2023-01-17 12:40:21 -0800

    LPS-170836 Handle the exception to avoid returning a 500 response code

--- portal-impl/src/com/liferay/portal/action/UpdateTermsOfUseAction.java
+++ portal-impl/src/com/liferay/portal/action/UpdateTermsOfUseAction.java
@@ -14,8 +14,12 @@
 
 package com.liferay.portal.action;
 
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.security.auth.AuthTokenUtil;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.service.UserServiceUtil;
+import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.util.PortalUtil;
 import com.liferay.portal.struts.Action;
 import com.liferay.portal.struts.constants.ActionConstants;
@@ -36,14 +40,33 @@ public class UpdateTermsOfUseAction implements Action {
 			HttpServletResponse httpServletResponse)
 		throws Exception {
 
-		AuthTokenUtil.checkCSRFToken(
-			httpServletRequest, UpdateTermsOfUseAction.class.getName());
+		try {
+			AuthTokenUtil.checkCSRFToken(
+				httpServletRequest, UpdateTermsOfUseAction.class.getName());
 
-		UserServiceUtil.updateAgreedToTermsOfUse(
-			PortalUtil.getUserId(httpServletRequest), true);
+			UserServiceUtil.updateAgreedToTermsOfUse(
+				PortalUtil.getUserId(httpServletRequest), true);
 
-		return actionMapping.getActionForward(
-			ActionConstants.COMMON_REFERER_JSP);
+			return actionMapping.getActionForward(
+				ActionConstants.COMMON_REFERER_JSP);
+		}
+		catch (Exception exception) {
+			if (exception instanceof PrincipalException) {
+				_log.error("The CSRF token is invalid", exception);
+
+				SessionErrors.add(httpServletRequest, exception.getClass());
+
+				return actionMapping.getActionForward("portal.error");
+			}
+
+			PortalUtil.sendError(
+				exception, httpServletRequest, httpServletResponse);
+
+			return null;
+		}
 	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		UpdateTermsOfUseAction.class);
 
 }
