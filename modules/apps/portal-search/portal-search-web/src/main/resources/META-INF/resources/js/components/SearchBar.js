701b07fee333a5c0a9654eb5895f0d7174058cff
Brian Chan <brian.chan@liferay.com>
2023-01-17 09:52:57 -0800

    LPS-172622 Sort

--- modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SearchBar.js
+++ modules/apps/portal-search/portal-search-web/src/main/resources/META-INF/resources/js/components/SearchBar.js
@@ -311,7 +311,7 @@ export default function SearchBar({
 	const _updateQueryString = (queryString) => {
 		const searchParams = new URLSearchParams(queryString);
 
-		if (inputValue || emptySearchEnabled) {
+		if (emptySearchEnabled || inputValue) {
 			searchParams.set(
 				keywordsParameterName,
 				inputValue.replace(/^\s+|\s+$/, '')
