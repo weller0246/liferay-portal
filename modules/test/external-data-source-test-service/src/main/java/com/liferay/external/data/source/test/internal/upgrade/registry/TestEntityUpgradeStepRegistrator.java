fc40d11d286c60f8cabc0d6e453538b01ff5f88a
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-01-19 15:10:50 -0300

    LPS-173291 Remove immediate from External Data Source Test Service module

--- modules/test/external-data-source-test-service/src/main/java/com/liferay/external/data/source/test/internal/upgrade/registry/TestEntityUpgradeStepRegistrator.java
+++ modules/test/external-data-source-test-service/src/main/java/com/liferay/external/data/source/test/internal/upgrade/registry/TestEntityUpgradeStepRegistrator.java
@@ -22,7 +22,7 @@ import org.osgi.service.component.annotations.Component;
 /**
  * @author Shuyang Zhou
  */
-@Component(immediate = true, service = UpgradeStepRegistrator.class)
+@Component(service = UpgradeStepRegistrator.class)
 public class TestEntityUpgradeStepRegistrator
 	implements UpgradeStepRegistrator {
 
