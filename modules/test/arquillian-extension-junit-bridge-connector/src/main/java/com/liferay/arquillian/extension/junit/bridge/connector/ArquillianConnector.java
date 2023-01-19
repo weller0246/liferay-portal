543fc21039d419b5345ac04cd10d2db7e9cc79f2
Renan Vasconcelos <renan.vasconcelos@liferay.com>
2023-01-19 15:09:46 -0300

    LPS-173291 Remove immediate from Arquillian Extension Junit Bridge Connector module

--- modules/test/arquillian-extension-junit-bridge-connector/src/main/java/com/liferay/arquillian/extension/junit/bridge/connector/ArquillianConnector.java
+++ modules/test/arquillian-extension-junit-bridge-connector/src/main/java/com/liferay/arquillian/extension/junit/bridge/connector/ArquillianConnector.java
@@ -33,7 +33,7 @@ import org.osgi.service.log.LoggerFactory;
  */
 @Component(
 	configurationPid = "com.liferay.arquillian.extension.junit.bridge.connector.ArquillianConnectorConfiguration",
-	immediate = true, service = {}
+	service = {}
 )
 public class ArquillianConnector {
 
