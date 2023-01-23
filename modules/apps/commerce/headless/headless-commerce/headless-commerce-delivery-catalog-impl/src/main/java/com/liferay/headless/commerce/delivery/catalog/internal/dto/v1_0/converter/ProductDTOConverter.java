4f1d20f5816fffae32a65850719a1c9808151722
Brian Chan <brian.chan@liferay.com>
2023-01-22 23:35:48 -0600

    COMMERCE-10579 SF

--- modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/dto/v1_0/converter/ProductDTOConverter.java
+++ modules/apps/commerce/headless/headless-commerce/headless-commerce-delivery-catalog-impl/src/main/java/com/liferay/headless/commerce/delivery/catalog/internal/dto/v1_0/converter/ProductDTOConverter.java
@@ -96,7 +96,6 @@ public class ProductDTOConverter
 
 						return cProduct.getExternalReferenceCode();
 					});
-
 				setUrlImage(
 					() -> {
 						Company company = _companyLocalService.getCompany(
