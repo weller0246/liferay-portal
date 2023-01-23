b01db626bc554f24e9965ba481acaacaf7089949
Brian Chan <brian.chan@liferay.com>
2023-01-22 22:06:29 -0600

    LPS-171998 Wordsmith

--- modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/odata/ExpressionVisitorImpl.java
+++ modules/apps/segments/segments-web/src/main/java/com/liferay/segments/web/internal/odata/ExpressionVisitorImpl.java
@@ -345,7 +345,7 @@ public class ExpressionVisitorImpl implements ExpressionVisitor<Object> {
 		}
 		catch (DateTimeParseException dateTimeParseException) {
 			throw new ExpressionVisitException(
-				"Invalid duration, use a correct format " +
+				"Invalid duration, use a correct format: " +
 					dateTimeParseException.getMessage());
 		}
 	}
@@ -424,7 +424,7 @@ public class ExpressionVisitorImpl implements ExpressionVisitor<Object> {
 
 		throw new UnsupportedOperationException(
 			StringBundler.concat(
-				"Unsupported types in _sub with Arithmetic Operator SUB with ",
+				"Unsupported types in _sub with arithmetic operator SUB with ",
 				"left type ", left.getClass(), "and right type ",
 				right.getClass()));
 	}
