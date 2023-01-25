75c6befe286b71f983d082e5e32fe591bfc32a52
Tina Tian <tina.tian@liferay.com>
2023-01-25 14:22:36 -0800

    LPS-173122 Unget service when removing

--- modules/dxp/apps/commerce/commerce-machine-learning-impl/src/main/java/com/liferay/commerce/machine/learning/internal/search/instance/lifecycle/CommerceMLIndexerPortalInstanceLifecycleListener.java
+++ modules/dxp/apps/commerce/commerce-machine-learning-impl/src/main/java/com/liferay/commerce/machine/learning/internal/search/instance/lifecycle/CommerceMLIndexerPortalInstanceLifecycleListener.java
@@ -99,6 +99,8 @@ public class CommerceMLIndexerPortalInstanceLifecycleListener
 				public void removedService(
 					ServiceReference<CommerceMLIndexer> serviceReference,
 					CommerceMLIndexer commerceMLIndexer) {
+
+					bundleContext.ungetService(serviceReference);
 				}
 
 			});
